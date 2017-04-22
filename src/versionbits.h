e3e49ee805974b27397cbb4b5c5f2e7343b12fa2
Amaury SECHET <deadalnix@gmail.com>
2017-04-21 20:43:22 -0400

    Format versionbits.h and versionbits.cpp

--- src/versionbits.h
+++ src/versionbits.h
@@ -28,7 +28,7 @@ enum ThresholdState {
 // A map that gives the state for blocks whose height is a multiple of Period().
 // The map is indexed by the block's parent, however, so all keys in the map
 // will either be NULL or a block with (height + 1) % Period() == 0.
-typedef std::map<const CBlockIndex*, ThresholdState> ThresholdConditionCache;
+typedef std::map<const CBlockIndex *, ThresholdState> ThresholdConditionCache;
 
 struct BIP9DeploymentInfo {
     /** Deployment name */
@@ -40,31 +40,44 @@ struct BIP9DeploymentInfo {
 extern const struct BIP9DeploymentInfo VersionBitsDeploymentInfo[];
 
 /**
- * Abstract class that implements BIP9-style threshold logic, and caches results.
+ * Abstract class that implements BIP9-style threshold logic, and caches
+ * results.
  */
 class AbstractThresholdConditionChecker {
 protected:
-    virtual bool Condition(const CBlockIndex* pindex, const Consensus::Params& params) const =0;
-    virtual int64_t BeginTime(const Consensus::Params& params) const =0;
-    virtual int64_t EndTime(const Consensus::Params& params) const =0;
-    virtual int Period(const Consensus::Params& params) const =0;
-    virtual int Threshold(const Consensus::Params& params) const =0;
+    virtual bool Condition(const CBlockIndex *pindex,
+                           const Consensus::Params &params) const = 0;
+    virtual int64_t BeginTime(const Consensus::Params &params) const = 0;
+    virtual int64_t EndTime(const Consensus::Params &params) const = 0;
+    virtual int Period(const Consensus::Params &params) const = 0;
+    virtual int Threshold(const Consensus::Params &params) const = 0;
 
 public:
-    // Note that the functions below take a pindexPrev as input: they compute information for block B based on its parent.
-    ThresholdState GetStateFor(const CBlockIndex* pindexPrev, const Consensus::Params& params, ThresholdConditionCache& cache) const;
-    int GetStateSinceHeightFor(const CBlockIndex* pindexPrev, const Consensus::Params& params, ThresholdConditionCache& cache) const;
+    // Note that the functions below take a pindexPrev as input: they compute
+    // information for block B based on its parent.
+    ThresholdState GetStateFor(const CBlockIndex *pindexPrev,
+                               const Consensus::Params &params,
+                               ThresholdConditionCache &cache) const;
+    int GetStateSinceHeightFor(const CBlockIndex *pindexPrev,
+                               const Consensus::Params &params,
+                               ThresholdConditionCache &cache) const;
 };
 
-struct VersionBitsCache
-{
+struct VersionBitsCache {
     ThresholdConditionCache caches[Consensus::MAX_VERSION_BITS_DEPLOYMENTS];
 
     void Clear();
 };
 
-ThresholdState VersionBitsState(const CBlockIndex* pindexPrev, const Consensus::Params& params, Consensus::DeploymentPos pos, VersionBitsCache& cache);
-int VersionBitsStateSinceHeight(const CBlockIndex* pindexPrev, const Consensus::Params& params, Consensus::DeploymentPos pos, VersionBitsCache& cache);
-uint32_t VersionBitsMask(const Consensus::Params& params, Consensus::DeploymentPos pos);
+ThresholdState VersionBitsState(const CBlockIndex *pindexPrev,
+                                const Consensus::Params &params,
+                                Consensus::DeploymentPos pos,
+                                VersionBitsCache &cache);
+int VersionBitsStateSinceHeight(const CBlockIndex *pindexPrev,
+                                const Consensus::Params &params,
+                                Consensus::DeploymentPos pos,
+                                VersionBitsCache &cache);
+uint32_t VersionBitsMask(const Consensus::Params &params,
+                         Consensus::DeploymentPos pos);
 
 #endif
