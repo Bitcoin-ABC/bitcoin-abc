ef78e27690a4b9487ff23fcdf9d500b8b4ddf307
Amaury SECHET <deadalnix@gmail.com>
2017-04-23 10:40:32 -0400

    Format undo.h

--- src/undo.h
+++ src/undo.h
@@ -6,7 +6,7 @@
 #ifndef BITCOIN_UNDO_H
 #define BITCOIN_UNDO_H
 
-#include "compressor.h" 
+#include "compressor.h"
 #include "primitives/transaction.h"
 #include "serialize.h"
 
@@ -16,62 +16,63 @@
  *  last output of the affected transaction, its metadata as well
  *  (coinbase or not, height, transaction version)
  */
-class CTxInUndo
-{
+class CTxInUndo {
 public:
-    CTxOut txout;         // the txout data before being spent
-    bool fCoinBase;       // if the outpoint was the last unspent: whether it belonged to a coinbase
-    unsigned int nHeight; // if the outpoint was the last unspent: its height
-    int nVersion;         // if the outpoint was the last unspent: its version
+    // The txout data before being spent
+    CTxOut txout;
+    // If the outpoint was the last unspent: whether it belonged to a coinbase
+    bool fCoinBase;
+    // If the outpoint was the last unspent: its height
+    unsigned int nHeight;
+    // If the outpoint was the last unspent: its version
+    int nVersion;
 
     CTxInUndo() : txout(), fCoinBase(false), nHeight(0), nVersion(0) {}
-    CTxInUndo(const CTxOut &txoutIn, bool fCoinBaseIn = false, unsigned int nHeightIn = 0, int nVersionIn = 0) : txout(txoutIn), fCoinBase(fCoinBaseIn), nHeight(nHeightIn), nVersion(nVersionIn) { }
+    CTxInUndo(const CTxOut &txoutIn, bool fCoinBaseIn = false,
+              unsigned int nHeightIn = 0, int nVersionIn = 0)
+        : txout(txoutIn), fCoinBase(fCoinBaseIn), nHeight(nHeightIn),
+          nVersion(nVersionIn) {}
 
-    template<typename Stream>
-    void Serialize(Stream &s) const {
-        ::Serialize(s, VARINT(nHeight*2+(fCoinBase ? 1 : 0)));
-        if (nHeight > 0)
-            ::Serialize(s, VARINT(this->nVersion));
+    template <typename Stream> void Serialize(Stream &s) const {
+        ::Serialize(s, VARINT(nHeight * 2 + (fCoinBase ? 1 : 0)));
+        if (nHeight > 0) ::Serialize(s, VARINT(this->nVersion));
         ::Serialize(s, CTxOutCompressor(REF(txout)));
     }
 
-    template<typename Stream>
-    void Unserialize(Stream &s) {
+    template <typename Stream> void Unserialize(Stream &s) {
         unsigned int nCode = 0;
         ::Unserialize(s, VARINT(nCode));
         nHeight = nCode / 2;
         fCoinBase = nCode & 1;
-        if (nHeight > 0)
-            ::Unserialize(s, VARINT(this->nVersion));
+        if (nHeight > 0) ::Unserialize(s, VARINT(this->nVersion));
         ::Unserialize(s, REF(CTxOutCompressor(REF(txout))));
     }
 };
 
 /** Undo information for a CTransaction */
-class CTxUndo
-{
+class CTxUndo {
 public:
-    // undo information for all txins
+    // Undo information for all txins
     std::vector<CTxInUndo> vprevout;
 
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action) {
+    inline void SerializationOp(Stream &s, Operation ser_action) {
         READWRITE(vprevout);
     }
 };
 
 /** Undo information for a CBlock */
-class CBlockUndo
-{
+class CBlockUndo {
 public:
-    std::vector<CTxUndo> vtxundo; // for all but the coinbase
+    // For all but the coinbase
+    std::vector<CTxUndo> vtxundo;
 
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action) {
+    inline void SerializationOp(Stream &s, Operation ser_action) {
         READWRITE(vtxundo);
     }
 };
