cf47329f69468c35ceefb46bcda14ee7994bccb0
Amaury SECHET <deadalnix@gmail.com>
2017-04-28 05:43:31 -0400

    CheckInputs do not need to handle coinbase: coinbase have no inputs.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1319,67 +1319,67 @@ bool CheckInputs(const CTransaction &tx, CValidationState &state,
                  unsigned int flags, bool cacheStore,
                  PrecomputedTransactionData &txdata,
                  std::vector<CScriptCheck> *pvChecks) {
-    if (!tx.IsCoinBase()) {
-        if (!Consensus::CheckTxInputs(tx, state, inputs,
-                                      GetSpendHeight(inputs)))
-            return false;
+    assert(!tx.IsCoinBase());
 
-        if (pvChecks) pvChecks->reserve(tx.vin.size());
+    if (!Consensus::CheckTxInputs(tx, state, inputs, GetSpendHeight(inputs))) {
+        return false;
+    }
 
-        // The first loop above does all the inexpensive checks.
-        // Only if ALL inputs pass do we perform expensive ECDSA signature
-        // checks.
-        // Helps prevent CPU exhaustion attacks.
+    if (pvChecks) {
+        pvChecks->reserve(tx.vin.size());
+    }
 
-        // Skip script verification when connecting blocks under the
-        // assumedvalid block. Assuming the assumedvalid block is valid this
-        // is safe because block merkle hashes are still computed and checked,
-        // Of course, if an assumed valid block is invalid due to false
-        // scriptSigs
-        // this optimization would allow an invalid chain to be accepted.
-        if (fScriptChecks) {
-            for (unsigned int i = 0; i < tx.vin.size(); i++) {
-                const COutPoint &prevout = tx.vin[i].prevout;
-                const CCoins *coins = inputs.AccessCoins(prevout.hash);
-                assert(coins);
+    // The first loop above does all the inexpensive checks. Only if ALL inputs
+    // pass do we perform expensive ECDSA signature checks. Helps prevent CPU
+    // exhaustion attacks.
 
-                // Verify signature
-                CScriptCheck check(*coins, tx, i, flags, cacheStore, &txdata);
-                if (pvChecks) {
-                    pvChecks->push_back(CScriptCheck());
-                    check.swap(pvChecks->back());
-                } else if (!check()) {
-                    if (flags & STANDARD_NOT_MANDATORY_VERIFY_FLAGS) {
-                        // Check whether the failure was caused by a
-                        // non-mandatory script verification check, such as
-                        // non-standard DER encodings or non-null dummy
-                        // arguments; if so, don't trigger DoS protection to
-                        // avoid splitting the network between upgraded and
-                        // non-upgraded nodes.
-                        CScriptCheck check2(
-                            *coins, tx, i,
-                            flags & ~STANDARD_NOT_MANDATORY_VERIFY_FLAGS,
-                            cacheStore, &txdata);
-                        if (check2())
-                            return state.Invalid(
-                                false, REJECT_NONSTANDARD,
-                                strprintf(
-                                    "non-mandatory-script-verify-flag (%s)",
-                                    ScriptErrorString(check.GetScriptError())));
-                    }
-                    // Failures of other flags indicate a transaction that is
-                    // invalid in new blocks, e.g. a invalid P2SH. We DoS ban
-                    // such nodes as they are not following the protocol. That
-                    // said during an upgrade careful thought should be taken
-                    // as to the correct behavior - we may want to continue
-                    // peering with non-upgraded nodes even after soft-fork
-                    // super-majority signaling has occurred.
-                    return state.DoS(
-                        100, false, REJECT_INVALID,
-                        strprintf("mandatory-script-verify-flag-failed (%s)",
+    // Skip script verification when connecting blocks under the assumedvalid
+    // block. Assuming the assumedvalid block is valid this is safe because
+    // block merkle hashes are still computed and checked, of course, if an
+    // assumed valid block is invalid due to false scriptSigs this optimization
+    // would allow an invalid chain to be accepted.
+    if (!fScriptChecks) {
+        return true;
+    }
+
+    for (unsigned int i = 0; i < tx.vin.size(); i++) {
+        const COutPoint &prevout = tx.vin[i].prevout;
+        const CCoins *coins = inputs.AccessCoins(prevout.hash);
+        assert(coins);
+
+        // Verify signature
+        CScriptCheck check(*coins, tx, i, flags, cacheStore, &txdata);
+        if (pvChecks) {
+            pvChecks->push_back(CScriptCheck());
+            check.swap(pvChecks->back());
+        } else if (!check()) {
+            if (flags & STANDARD_NOT_MANDATORY_VERIFY_FLAGS) {
+                // Check whether the failure was caused by a non-mandatory
+                // script verification check, such as non-standard DER encodings
+                // or non-null dummy arguments; if so, don't trigger DoS
+                // protection to avoid splitting the network between upgraded
+                // and non-upgraded nodes.
+                CScriptCheck check2(
+                    *coins, tx, i, flags & ~STANDARD_NOT_MANDATORY_VERIFY_FLAGS,
+                    cacheStore, &txdata);
+                if (check2()) {
+                    return state.Invalid(
+                        false, REJECT_NONSTANDARD,
+                        strprintf("non-mandatory-script-verify-flag (%s)",
                                   ScriptErrorString(check.GetScriptError())));
                 }
             }
+
+            // Failures of other flags indicate a transaction that is invalid in
+            // new blocks, e.g. a invalid P2SH. We DoS ban such nodes as they
+            // are not following the protocol. That said during an upgrade
+            // careful thought should be taken as to the correct behavior - we
+            // may want to continue peering with non-upgraded nodes even after
+            // soft-fork super-majority signaling has occurred.
+            return state.DoS(
+                100, false, REJECT_INVALID,
+                strprintf("mandatory-script-verify-flag-failed (%s)",
+                          ScriptErrorString(check.GetScriptError())));
         }
     }
 
