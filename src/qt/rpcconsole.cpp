a46b1d0e35ddcad8a09e4f37e1d6b9eb02b360de
Spencer Lievens <spencerlievens@users.noreply.github.com>
2017-05-08 21:39:34 +0200

    Add OSX keystroke to clear RPCConsole
    Summary:
    Currently only Ctrl-L is mentioned in help, but, (⌘)-L functions on OSX and isn't mentioned.
    
    Backport of Core PR #10362
    Completes T431
    
    Co-authored-by: Jason B. Cox <contact@jasonbcox.com>
    
    Test Plan: ninja check
    
    Reviewers: deadalnix, schancel, #bitcoin_abc
    
    Reviewed By: schancel, #bitcoin_abc
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1931

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -807,10 +807,18 @@ void RPCConsole::clear(bool clearHistory) {
                 "b { color: #006060; } ")
             .arg(fixedFontInfo.family(), QString("%1pt").arg(consoleFontSize)));
 
+#ifdef Q_OS_MAC
+    QString clsKey = "(⌘)-L";
+#else
+    QString clsKey = "Ctrl-L";
+#endif
+
     message(CMD_REPLY,
             (tr("Welcome to the %1 RPC console.").arg(tr(PACKAGE_NAME)) +
-             "<br>" + tr("Use up and down arrows to navigate history, and "
-                         "<b>Ctrl-L</b> to clear screen.") +
+             "<br>" +
+             tr("Use up and down arrows to navigate history, and "
+                "%1 to clear screen.")
+                 .arg("<b>" + clsKey + "</b>") +
              "<br>" +
              tr("Type <b>help</b> for an overview of available commands.")) +
                 "<br><span class=\"secwarning\">" +
