8cff65830dffd36fd433ba2d5cc60a1b62578310
Jonas Schnelli <dev@jonasschnelli.ch>
2017-05-10 09:35:36 +0200

    Implement BIP159 NODE_NETWORK_LIMITED (pruned peers) *signaling only*
    Summary:
    ```
    Does implement BIP159, but only the signalling part. No connections are
    made to NODE_NETWORK_LIMITED in this PR.
    ```
    
    Backport of core PR11740
    
    Depends on D2362
    
    Test Plan:
      ./test/functional/test_runner.py node_network_limited
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Subscribers: jasonbcox, teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2363

--- src/init.cpp
+++ src/init.cpp
@@ -1292,7 +1292,7 @@ namespace { // Variables internal to initialization process only
 int nMaxConnections;
 int nUserMaxConnections;
 int nFD;
-ServiceFlags nLocalServices = NODE_NETWORK;
+ServiceFlags nLocalServices = ServiceFlags(NODE_NETWORK | NODE_NETWORK_LIMITED);
 } // namespace
 
 [[noreturn]] static void new_handler_terminate() {
