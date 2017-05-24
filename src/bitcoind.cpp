41e08b12e886ef2def75b8a0a0eca591900d0525
Wladimir J. van der Laan <laanwj@gmail.com>
2017-05-24 09:28:51 +0200

    Make bitcoind invalid argument error message specific
    Summary:
    The current message is not helpful. Hardly anyone even remembers that
    bitcoind used to be a cli utility, let alone new users. Print what the
    actual problem is.
    
    This is a backport of Core's PR10447
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1828

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -21,7 +21,6 @@
 #include "util.h"
 #include "utilstrencodings.h"
 
-#include <boost/algorithm/string/predicate.hpp>
 #include <boost/thread.hpp>
 
 #include <cstdio>
@@ -126,19 +125,18 @@ bool AppInit(int argc, char *argv[]) {
             return false;
         }
 
-        // Command-line RPC
-        bool fCommandLine = false;
-        for (int i = 1; i < argc; i++)
-            if (!IsSwitchChar(argv[i][0]) &&
-                !boost::algorithm::istarts_with(argv[i], "bitcoin:"))
-                fCommandLine = true;
-
-        if (fCommandLine) {
-            fprintf(stderr, "Error: There is no RPC client functionality in "
-                            "bitcoind anymore. Use the bitcoin-cli utility "
-                            "instead.\n");
-            exit(EXIT_FAILURE);
+        // Error out when loose non-argument tokens are encountered on command
+        // line
+        for (int i = 1; i < argc; i++) {
+            if (!IsSwitchChar(argv[i][0])) {
+                fprintf(stderr, "Error: Command line contains unexpected token "
+                                "'%s', see bitcoind -h for a list of "
+                                "options.\n",
+                        argv[i]);
+                exit(EXIT_FAILURE);
+            }
         }
+
         // -server defaults to true for bitcoind but not for the GUI so do this
         // here
         gArgs.SoftSetBoolArg("-server", true);
