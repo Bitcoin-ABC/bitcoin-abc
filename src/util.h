571dda17c215d920ee10f416ff62d227c48663c6
Wladimir J. van der Laan <laanwj@gmail.com>
2017-05-31 17:15:38 +0200

    Merge #10479: [trivial] Fix comment for ForceSetArg()
    Summary:
    42a83e5 [trivial] Fix comment for ForceSetArg() (John Newbery)
    
    Tree-SHA512: 596db8b4bb01bca18908e254e5b364d3d4fcb0ea49d7d54314c65106e497efed65a877d80740bff88e090aeb772be1e46d4571ecca9b9263578f3f680f0bd0ce
    
    Backport of Core PR10479
    https://github.com/bitcoin/bitcoin/pull/10479/
    
    Test Plan:
      make check
    
    Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: Fabien, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D3506

--- src/util.h
+++ src/util.h
@@ -226,7 +226,8 @@ public:
      */
     bool SoftSetBoolArg(const std::string &strArg, bool fValue);
 
-    // Forces a arg setting, used only in testing
+    // Forces an arg setting. Called by SoftSetArg() if the arg hasn't already
+    // been set. Also called directly in testing.
     void ForceSetArg(const std::string &strArg, const std::string &strValue);
 
     // Forces a multi arg setting, used only in testing
