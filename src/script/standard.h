6d210d5d5a7b63b70afc2a3971967b4ba5ad380d
Amaury SECHET <deadalnix@gmail.com>
2017-06-04 12:46:28 +0200

    Format standard.h and standard.cpp
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: freetrader, #bitcoin_abc
    
    Reviewed By: freetrader, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D49

--- src/script/standard.h
+++ src/script/standard.h
@@ -11,7 +11,7 @@
 
 #include <boost/variant.hpp>
 
-#include <stdint.h>
+#include <cstdint>
 
 static const bool DEFAULT_ACCEPT_DATACARRIER = true;
 
@@ -19,15 +19,15 @@ class CKeyID;
 class CScript;
 
 /** A reference to a CScript: the Hash160 of its serialization (see script.h) */
-class CScriptID : public uint160
-{
+class CScriptID : public uint160 {
 public:
     CScriptID() : uint160() {}
-    CScriptID(const CScript& in);
-    CScriptID(const uint160& in) : uint160(in) {}
+    CScriptID(const CScript &in);
+    CScriptID(const uint160 &in) : uint160(in) {}
 };
 
-static const unsigned int MAX_OP_RETURN_RELAY = 83; //!< bytes (+1 for OP_RETURN, +2 for the pushdata opcodes)
+//!< bytes (+1 for OP_RETURN, +2 for the pushdata opcodes)
+static const unsigned int MAX_OP_RETURN_RELAY = 83;
 extern bool fAcceptDatacarrier;
 extern unsigned nMaxDatacarrierBytes;
 
@@ -36,14 +36,13 @@ extern unsigned nMaxDatacarrierBytes;
  * them to be valid. (but old blocks may not comply with) Currently just P2SH,
  * but in the future other flags may be added, such as a soft-fork to enforce
  * strict DER encoding.
- * 
+ *
  * Failing one of these tests may trigger a DoS ban - see CheckInputs() for
  * details.
  */
 static const unsigned int MANDATORY_SCRIPT_VERIFY_FLAGS = SCRIPT_VERIFY_P2SH;
 
-enum txnouttype
-{
+enum txnouttype {
     TX_NONSTANDARD,
     // 'standard' transaction types:
     TX_PUBKEY,
@@ -57,11 +56,15 @@ enum txnouttype
 
 class CNoDestination {
 public:
-    friend bool operator==(const CNoDestination &a, const CNoDestination &b) { return true; }
-    friend bool operator<(const CNoDestination &a, const CNoDestination &b) { return true; }
+    friend bool operator==(const CNoDestination &a, const CNoDestination &b) {
+        return true;
+    }
+    friend bool operator<(const CNoDestination &a, const CNoDestination &b) {
+        return true;
+    }
 };
 
-/** 
+/**
  * A txout script template with a specific destination. It is either:
  *  * CNoDestination: no destination set
  *  * CKeyID: TX_PUBKEYHASH destination
@@ -70,14 +73,18 @@ public:
  */
 typedef boost::variant<CNoDestination, CKeyID, CScriptID> CTxDestination;
 
-const char* GetTxnOutputType(txnouttype t);
+const char *GetTxnOutputType(txnouttype t);
 
-bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::vector<unsigned char> >& vSolutionsRet);
-bool ExtractDestination(const CScript& scriptPubKey, CTxDestination& addressRet);
-bool ExtractDestinations(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<CTxDestination>& addressRet, int& nRequiredRet);
+bool Solver(const CScript &scriptPubKey, txnouttype &typeRet,
+            std::vector<std::vector<unsigned char>> &vSolutionsRet);
+bool ExtractDestination(const CScript &scriptPubKey,
+                        CTxDestination &addressRet);
+bool ExtractDestinations(const CScript &scriptPubKey, txnouttype &typeRet,
+                         std::vector<CTxDestination> &addressRet,
+                         int &nRequiredRet);
 
-CScript GetScriptForDestination(const CTxDestination& dest);
-CScript GetScriptForRawPubKey(const CPubKey& pubkey);
-CScript GetScriptForMultisig(int nRequired, const std::vector<CPubKey>& keys);
+CScript GetScriptForDestination(const CTxDestination &dest);
+CScript GetScriptForRawPubKey(const CPubKey &pubkey);
+CScript GetScriptForMultisig(int nRequired, const std::vector<CPubKey> &keys);
 
 #endif // BITCOIN_SCRIPT_STANDARD_H
