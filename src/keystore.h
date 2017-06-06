4d6166c8f7d61cdd8bdd1ab6164f631f3db35227
Amaury SECHET <deadalnix@gmail.com>
2017-06-06 11:37:42 +0200

    Format keystore.h and keystore.cpp
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: freetrader, sickpig, #bitcoin_abc
    
    Reviewed By: freetrader, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D74

--- src/keystore.h
+++ src/keystore.h
@@ -16,8 +16,7 @@
 #include <boost/variant.hpp>
 
 /** A virtual base class for key stores */
-class CKeyStore
-{
+class CKeyStore {
 protected:
     mutable CCriticalSection cs_KeyStore;
 
@@ -25,35 +24,38 @@ public:
     virtual ~CKeyStore() {}
 
     //! Add a key to the store.
-    virtual bool AddKeyPubKey(const CKey &key, const CPubKey &pubkey) =0;
+    virtual bool AddKeyPubKey(const CKey &key, const CPubKey &pubkey) = 0;
     virtual bool AddKey(const CKey &key);
 
-    //! Check whether a key corresponding to a given address is present in the store.
-    virtual bool HaveKey(const CKeyID &address) const =0;
-    virtual bool GetKey(const CKeyID &address, CKey& keyOut) const =0;
-    virtual void GetKeys(std::set<CKeyID> &setAddress) const =0;
-    virtual bool GetPubKey(const CKeyID &address, CPubKey& vchPubKeyOut) const =0;
+    //! Check whether a key corresponding to a given address is present in the
+    //! store.
+    virtual bool HaveKey(const CKeyID &address) const = 0;
+    virtual bool GetKey(const CKeyID &address, CKey &keyOut) const = 0;
+    virtual void GetKeys(std::set<CKeyID> &setAddress) const = 0;
+    virtual bool GetPubKey(const CKeyID &address,
+                           CPubKey &vchPubKeyOut) const = 0;
 
-    //! Support for BIP 0013 : see https://github.com/bitcoin/bips/blob/master/bip-0013.mediawiki
-    virtual bool AddCScript(const CScript& redeemScript) =0;
-    virtual bool HaveCScript(const CScriptID &hash) const =0;
-    virtual bool GetCScript(const CScriptID &hash, CScript& redeemScriptOut) const =0;
+    //! Support for BIP 0013 : see
+    //! https://github.com/bitcoin/bips/blob/master/bip-0013.mediawiki
+    virtual bool AddCScript(const CScript &redeemScript) = 0;
+    virtual bool HaveCScript(const CScriptID &hash) const = 0;
+    virtual bool GetCScript(const CScriptID &hash,
+                            CScript &redeemScriptOut) const = 0;
 
     //! Support for Watch-only addresses
-    virtual bool AddWatchOnly(const CScript &dest) =0;
-    virtual bool RemoveWatchOnly(const CScript &dest) =0;
-    virtual bool HaveWatchOnly(const CScript &dest) const =0;
-    virtual bool HaveWatchOnly() const =0;
+    virtual bool AddWatchOnly(const CScript &dest) = 0;
+    virtual bool RemoveWatchOnly(const CScript &dest) = 0;
+    virtual bool HaveWatchOnly(const CScript &dest) const = 0;
+    virtual bool HaveWatchOnly() const = 0;
 };
 
 typedef std::map<CKeyID, CKey> KeyMap;
 typedef std::map<CKeyID, CPubKey> WatchKeyMap;
-typedef std::map<CScriptID, CScript > ScriptMap;
+typedef std::map<CScriptID, CScript> ScriptMap;
 typedef std::set<CScript> WatchOnlySet;
 
 /** Basic key store, that keeps keys in an address->secret map */
-class CBasicKeyStore : public CKeyStore
-{
+class CBasicKeyStore : public CKeyStore {
 protected:
     KeyMap mapKeys;
     WatchKeyMap mapWatchKeys;
@@ -61,10 +63,9 @@ protected:
     WatchOnlySet setWatchOnly;
 
 public:
-    bool AddKeyPubKey(const CKey& key, const CPubKey &pubkey);
-    bool GetPubKey(const CKeyID &address, CPubKey& vchPubKeyOut) const;
-    bool HaveKey(const CKeyID &address) const
-    {
+    bool AddKeyPubKey(const CKey &key, const CPubKey &pubkey);
+    bool GetPubKey(const CKeyID &address, CPubKey &vchPubKeyOut) const;
+    bool HaveKey(const CKeyID &address) const {
         bool result;
         {
             LOCK(cs_KeyStore);
@@ -72,35 +73,32 @@ public:
         }
         return result;
     }
-    void GetKeys(std::set<CKeyID> &setAddress) const
-    {
+    void GetKeys(std::set<CKeyID> &setAddress) const {
         setAddress.clear();
         {
             LOCK(cs_KeyStore);
             KeyMap::const_iterator mi = mapKeys.begin();
-            while (mi != mapKeys.end())
-            {
+            while (mi != mapKeys.end()) {
                 setAddress.insert((*mi).first);
                 mi++;
             }
         }
     }
-    bool GetKey(const CKeyID &address, CKey &keyOut) const
-    {
+    bool GetKey(const CKeyID &address, CKey &keyOut) const {
         {
             LOCK(cs_KeyStore);
             KeyMap::const_iterator mi = mapKeys.find(address);
-            if (mi != mapKeys.end())
-            {
+            if (mi != mapKeys.end()) {
                 keyOut = mi->second;
                 return true;
             }
         }
         return false;
     }
-    virtual bool AddCScript(const CScript& redeemScript);
+    virtual bool AddCScript(const CScript &redeemScript);
     virtual bool HaveCScript(const CScriptID &hash) const;
-    virtual bool GetCScript(const CScriptID &hash, CScript& redeemScriptOut) const;
+    virtual bool GetCScript(const CScriptID &hash,
+                            CScript &redeemScriptOut) const;
 
     virtual bool AddWatchOnly(const CScript &dest);
     virtual bool RemoveWatchOnly(const CScript &dest);
@@ -108,7 +106,9 @@ public:
     virtual bool HaveWatchOnly() const;
 };
 
-typedef std::vector<unsigned char, secure_allocator<unsigned char> > CKeyingMaterial;
-typedef std::map<CKeyID, std::pair<CPubKey, std::vector<unsigned char> > > CryptedKeyMap;
+typedef std::vector<unsigned char, secure_allocator<unsigned char>>
+    CKeyingMaterial;
+typedef std::map<CKeyID, std::pair<CPubKey, std::vector<unsigned char>>>
+    CryptedKeyMap;
 
 #endif // BITCOIN_KEYSTORE_H
