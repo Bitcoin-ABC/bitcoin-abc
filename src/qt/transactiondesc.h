b2c5ac47b36f8b99264b6d2f3f5f2d0df57e6c5e
Amaury SECHET <deadalnix@gmail.com>
2017-06-07 01:11:23 +0200

    Format transactiondesc.h and transactiondesc.cpp
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: freetrader, sickpig, #bitcoin_abc
    
    Reviewed By: freetrader, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D99

--- src/qt/transactiondesc.h
+++ src/qt/transactiondesc.h
@@ -13,19 +13,20 @@ class TransactionRecord;
 class CWallet;
 class CWalletTx;
 
-/** Provide a human-readable extended HTML description of a transaction.
+/**
+ * Provide a human-readable extended HTML description of a transaction.
  */
-class TransactionDesc: public QObject
-{
+class TransactionDesc : public QObject {
     Q_OBJECT
 
 public:
-    static QString toHTML(CWallet *wallet, CWalletTx &wtx, TransactionRecord *rec, int unit);
+    static QString toHTML(CWallet *wallet, CWalletTx &wtx,
+                          TransactionRecord *rec, int unit);
 
 private:
     TransactionDesc() {}
 
-    static QString FormatTxStatus(const CWalletTx& wtx);
+    static QString FormatTxStatus(const CWalletTx &wtx);
 };
 
 #endif // BITCOIN_QT_TRANSACTIONDESC_H
