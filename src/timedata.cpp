01f3d176a40da7aa7a2520c7eb902210a2200511
Amaury SECHET <deadalnix@gmail.com>
2017-06-06 21:23:31 +0200

    Format timedata.h and timedata.cpp
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: freetrader, sickpig, #bitcoin_abc
    
    Reviewed By: freetrader, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D85

--- src/timedata.cpp
+++ src/timedata.cpp
@@ -25,94 +25,92 @@ static int64_t nTimeOffset = 0;
  * Our three time sources are:
  *  - System clock
  *  - Median of other nodes clocks
- *  - The user (asking the user to fix the system clock if the first two disagree)
+ *  - The user (asking the user to fix the system clock if the first two
+ * disagree)
  */
-int64_t GetTimeOffset()
-{
+int64_t GetTimeOffset() {
     LOCK(cs_nTimeOffset);
     return nTimeOffset;
 }
 
-int64_t GetAdjustedTime()
-{
+int64_t GetAdjustedTime() {
     return GetTime() + GetTimeOffset();
 }
 
-static int64_t abs64(int64_t n)
-{
+static int64_t abs64(int64_t n) {
     return (n >= 0 ? n : -n);
 }
 
 #define BITCOIN_TIMEDATA_MAX_SAMPLES 200
 
-void AddTimeData(const CNetAddr& ip, int64_t nOffsetSample)
-{
+void AddTimeData(const CNetAddr &ip, int64_t nOffsetSample) {
     LOCK(cs_nTimeOffset);
     // Ignore duplicates
     static std::set<CNetAddr> setKnown;
-    if (setKnown.size() == BITCOIN_TIMEDATA_MAX_SAMPLES)
-        return;
-    if (!setKnown.insert(ip).second)
-        return;
+    if (setKnown.size() == BITCOIN_TIMEDATA_MAX_SAMPLES) return;
+    if (!setKnown.insert(ip).second) return;
 
     // Add data
     static CMedianFilter<int64_t> vTimeOffsets(BITCOIN_TIMEDATA_MAX_SAMPLES, 0);
     vTimeOffsets.input(nOffsetSample);
-    LogPrint("net","added time data, samples %d, offset %+d (%+d minutes)\n", vTimeOffsets.size(), nOffsetSample, nOffsetSample/60);
+    LogPrint("net", "added time data, samples %d, offset %+d (%+d minutes)\n",
+             vTimeOffsets.size(), nOffsetSample, nOffsetSample / 60);
 
     // There is a known issue here (see issue #4521):
     //
-    // - The structure vTimeOffsets contains up to 200 elements, after which
-    // any new element added to it will not increase its size, replacing the
-    // oldest element.
+    // - The structure vTimeOffsets contains up to 200 elements, after which any
+    // new element added to it will not increase its size, replacing the oldest
+    // element.
     //
     // - The condition to update nTimeOffset includes checking whether the
     // number of elements in vTimeOffsets is odd, which will never happen after
     // there are 200 elements.
     //
     // But in this case the 'bug' is protective against some attacks, and may
-    // actually explain why we've never seen attacks which manipulate the
-    // clock offset.
+    // actually explain why we've never seen attacks which manipulate the clock
+    // offset.
     //
-    // So we should hold off on fixing this and clean it up as part of
-    // a timing cleanup that strengthens it in a number of other ways.
+    // So we should hold off on fixing this and clean it up as part of a timing
+    // cleanup that strengthens it in a number of other ways.
     //
-    if (vTimeOffsets.size() >= 5 && vTimeOffsets.size() % 2 == 1)
-    {
+    if (vTimeOffsets.size() >= 5 && vTimeOffsets.size() % 2 == 1) {
         int64_t nMedian = vTimeOffsets.median();
         std::vector<int64_t> vSorted = vTimeOffsets.sorted();
         // Only let other nodes change our time by so much
-        if (abs64(nMedian) <= std::max<int64_t>(0, GetArg("-maxtimeadjustment", DEFAULT_MAX_TIME_ADJUSTMENT)))
-        {
+        if (abs64(nMedian) <=
+            std::max<int64_t>(
+                0, GetArg("-maxtimeadjustment", DEFAULT_MAX_TIME_ADJUSTMENT))) {
             nTimeOffset = nMedian;
-        }
-        else
-        {
+        } else {
             nTimeOffset = 0;
 
             static bool fDone;
-            if (!fDone)
-            {
-                // If nobody has a time different than ours but within 5 minutes of ours, give a warning
+            if (!fDone) {
+                // If nobody has a time different than ours but within 5 minutes
+                // of ours, give a warning
                 bool fMatch = false;
-                BOOST_FOREACH(int64_t nOffset, vSorted)
-                    if (nOffset != 0 && abs64(nOffset) < 5 * 60)
-                        fMatch = true;
+                BOOST_FOREACH (int64_t nOffset, vSorted)
+                    if (nOffset != 0 && abs64(nOffset) < 5 * 60) fMatch = true;
 
-                if (!fMatch)
-                {
+                if (!fMatch) {
                     fDone = true;
-                    std::string strMessage = strprintf(_("Please check that your computer's date and time are correct! If your clock is wrong, %s will not work properly."), _(PACKAGE_NAME));
+                    std::string strMessage =
+                        strprintf(_("Please check that your computer's date "
+                                    "and time are correct! If your clock is "
+                                    "wrong, %s will not work properly."),
+                                  _(PACKAGE_NAME));
                     SetMiscWarning(strMessage);
-                    uiInterface.ThreadSafeMessageBox(strMessage, "", CClientUIInterface::MSG_WARNING);
+                    uiInterface.ThreadSafeMessageBox(
+                        strMessage, "", CClientUIInterface::MSG_WARNING);
                 }
             }
         }
-        
-        BOOST_FOREACH(int64_t n, vSorted)
+
+        BOOST_FOREACH (int64_t n, vSorted)
             LogPrint("net", "%+d  ", n);
         LogPrint("net", "|  ");
-        
-        LogPrint("net", "nTimeOffset = %+d  (%+d minutes)\n", nTimeOffset, nTimeOffset/60);
+
+        LogPrint("net", "nTimeOffset = %+d  (%+d minutes)\n", nTimeOffset,
+                 nTimeOffset / 60);
     }
 }
--- src/timedata.h
+++ src/timedata.h
@@ -6,36 +6,32 @@
 #define BITCOIN_TIMEDATA_H
 
 #include <algorithm>
-#include <assert.h>
-#include <stdint.h>
+#include <cassert>
+#include <cstdint>
 #include <vector>
 
 static const int64_t DEFAULT_MAX_TIME_ADJUSTMENT = 70 * 60;
 
 class CNetAddr;
 
-/** 
+/**
  * Median filter over a stream of values.
  * Returns the median of the last N numbers
  */
-template <typename T>
-class CMedianFilter
-{
+template <typename T> class CMedianFilter {
 private:
     std::vector<T> vValues;
     std::vector<T> vSorted;
     unsigned int nSize;
 
 public:
-    CMedianFilter(unsigned int size, T initial_value) : nSize(size)
-    {
+    CMedianFilter(unsigned int size, T initial_value) : nSize(size) {
         vValues.reserve(size);
         vValues.push_back(initial_value);
         vSorted = vValues;
     }
 
-    void input(T value)
-    {
+    void input(T value) {
         if (vValues.size() == nSize) {
             vValues.erase(vValues.begin());
         }
@@ -46,33 +42,27 @@ public:
         std::sort(vSorted.begin(), vSorted.end());
     }
 
-    T median() const
-    {
+    T median() const {
         int size = vSorted.size();
         assert(size > 0);
-        if (size & 1) // Odd number of elements
-        {
+
+        if (size & 1) {
+            // Odd number of elements
             return vSorted[size / 2];
-        } else // Even number of elements
-        {
+        } else {
+            // Even number of elements
             return (vSorted[size / 2 - 1] + vSorted[size / 2]) / 2;
         }
     }
 
-    int size() const
-    {
-        return vValues.size();
-    }
+    int size() const { return vValues.size(); }
 
-    std::vector<T> sorted() const
-    {
-        return vSorted;
-    }
+    std::vector<T> sorted() const { return vSorted; }
 };
 
 /** Functions to keep track of adjusted P2P time */
 int64_t GetTimeOffset();
 int64_t GetAdjustedTime();
-void AddTimeData(const CNetAddr& ip, int64_t nTime);
+void AddTimeData(const CNetAddr &ip, int64_t nTime);
 
 #endif // BITCOIN_TIMEDATA_H
