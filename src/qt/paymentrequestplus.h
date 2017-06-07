9d0682fc6c07316d7f7088eaa08453d7833e384d
Amaury SECHET <deadalnix@gmail.com>
2017-06-07 14:16:14 +0200

    Format paymentrequestplus.h, paymentrequestplus.cpp, paymentserver.h and paymentserver.cpp
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: freetrader, sickpig, #bitcoin_abc
    
    Reviewed By: freetrader, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D106

--- src/qt/paymentrequestplus.h
+++ src/qt/paymentrequestplus.h
@@ -21,27 +21,25 @@
 static const bool DEFAULT_SELFSIGNED_ROOTCERTS = false;
 
 //
-// Wraps dumb protocol buffer paymentRequest
-// with extra methods
+// Wraps dumb protocol buffer paymentRequest with extra methods
 //
 
-class PaymentRequestPlus
-{
+class PaymentRequestPlus {
 public:
-    PaymentRequestPlus() { }
+    PaymentRequestPlus() {}
 
-    bool parse(const QByteArray& data);
-    bool SerializeToString(std::string* output) const;
+    bool parse(const QByteArray &data);
+    bool SerializeToString(std::string *output) const;
 
     bool IsInitialized() const;
-    // Returns true if merchant's identity is authenticated, and
-    // returns human-readable merchant identity in merchant
-    bool getMerchant(X509_STORE* certStore, QString& merchant) const;
+    // Returns true if merchant's identity is authenticated, and returns
+    // human-readable merchant identity in merchant
+    bool getMerchant(X509_STORE *certStore, QString &merchant) const;
 
     // Returns list of outputs, amount
-    QList<std::pair<CScript,CAmount> > getPayTo() const;
+    QList<std::pair<CScript, CAmount>> getPayTo() const;
 
-    const payments::PaymentDetails& getDetails() const { return details; }
+    const payments::PaymentDetails &getDetails() const { return details; }
 
 private:
     payments::PaymentRequest paymentRequest;
