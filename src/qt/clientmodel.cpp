c924c511fc38fd7f91c44ef8a0f1b339ba3fee9d
Andrea Suisani <sickpig@gmail.com>
2017-06-19 16:31:16 +0200

    Clang formatting of `qt/clientmodel.cpp` and `test/net_tests.cpp`
    Summary: Clang formatting of `qt/clientmodel.cpp` and `test/net_tests.cpp`
    
    Test Plan: cd src; clang-format -assume-filename=.clang-format qt/clientmodel.cpp and look for diff
    
    Reviewers: freetrader, deadalnix, kyuupichan, awemany, #bitcoin_abc
    
    Reviewed By: deadalnix, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D226

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -12,11 +12,11 @@
 #include "chainparams.h"
 #include "checkpoints.h"
 #include "clientversion.h"
-#include "validation.h"
 #include "net.h"
 #include "txmempool.h"
 #include "ui_interface.h"
 #include "util.h"
+#include "validation.h"
 
 #include <stdint.h>
 
@@ -29,13 +29,9 @@ static const int64_t nClientStartupTime = GetTime();
 static int64_t nLastHeaderTipUpdateNotification = 0;
 static int64_t nLastBlockTipUpdateNotification = 0;
 
-ClientModel::ClientModel(OptionsModel *_optionsModel, QObject *parent) :
-    QObject(parent),
-    optionsModel(_optionsModel),
-    peerTableModel(0),
-    banTableModel(0),
-    pollTimer(0)
-{
+ClientModel::ClientModel(OptionsModel *_optionsModel, QObject *parent)
+    : QObject(parent), optionsModel(_optionsModel), peerTableModel(0),
+      banTableModel(0), pollTimer(0) {
     peerTableModel = new PeerTableModel(this);
     banTableModel = new BanTableModel(this);
     pollTimer = new QTimer(this);
@@ -45,124 +41,102 @@ ClientModel::ClientModel(OptionsModel *_optionsModel, QObject *parent) :
     subscribeToCoreSignals();
 }
 
-ClientModel::~ClientModel()
-{
+ClientModel::~ClientModel() {
     unsubscribeFromCoreSignals();
 }
 
-int ClientModel::getNumConnections(unsigned int flags) const
-{
+int ClientModel::getNumConnections(unsigned int flags) const {
     CConnman::NumConnections connections = CConnman::CONNECTIONS_NONE;
 
-    if(flags == CONNECTIONS_IN)
+    if (flags == CONNECTIONS_IN)
         connections = CConnman::CONNECTIONS_IN;
     else if (flags == CONNECTIONS_OUT)
         connections = CConnman::CONNECTIONS_OUT;
     else if (flags == CONNECTIONS_ALL)
         connections = CConnman::CONNECTIONS_ALL;
 
-    if(g_connman)
-         return g_connman->GetNodeCount(connections);
+    if (g_connman) return g_connman->GetNodeCount(connections);
     return 0;
 }
 
-int ClientModel::getNumBlocks() const
-{
+int ClientModel::getNumBlocks() const {
     LOCK(cs_main);
     return chainActive.Height();
 }
 
-int ClientModel::getHeaderTipHeight() const
-{
+int ClientModel::getHeaderTipHeight() const {
     LOCK(cs_main);
-    if (!pindexBestHeader)
-        return 0;
+    if (!pindexBestHeader) return 0;
     return pindexBestHeader->nHeight;
 }
 
-int64_t ClientModel::getHeaderTipTime() const
-{
+int64_t ClientModel::getHeaderTipTime() const {
     LOCK(cs_main);
-    if (!pindexBestHeader)
-        return 0;
+    if (!pindexBestHeader) return 0;
     return pindexBestHeader->GetBlockTime();
 }
 
-quint64 ClientModel::getTotalBytesRecv() const
-{
-    if(!g_connman)
-        return 0;
+quint64 ClientModel::getTotalBytesRecv() const {
+    if (!g_connman) return 0;
     return g_connman->GetTotalBytesRecv();
 }
 
-quint64 ClientModel::getTotalBytesSent() const
-{
-    if(!g_connman)
-        return 0;
+quint64 ClientModel::getTotalBytesSent() const {
+    if (!g_connman) return 0;
     return g_connman->GetTotalBytesSent();
 }
 
-QDateTime ClientModel::getLastBlockDate() const
-{
+QDateTime ClientModel::getLastBlockDate() const {
     LOCK(cs_main);
 
     if (chainActive.Tip())
         return QDateTime::fromTime_t(chainActive.Tip()->GetBlockTime());
 
-    return QDateTime::fromTime_t(Params().GenesisBlock().GetBlockTime()); // Genesis block's time of current network
+    // Genesis block's time of current network
+    return QDateTime::fromTime_t(Params().GenesisBlock().GetBlockTime());
 }
 
-long ClientModel::getMempoolSize() const
-{
+long ClientModel::getMempoolSize() const {
     return mempool.size();
 }
 
-size_t ClientModel::getMempoolDynamicUsage() const
-{
+size_t ClientModel::getMempoolDynamicUsage() const {
     return mempool.DynamicMemoryUsage();
 }
 
-double ClientModel::getVerificationProgress(const CBlockIndex *tipIn) const
-{
+double ClientModel::getVerificationProgress(const CBlockIndex *tipIn) const {
     CBlockIndex *tip = const_cast<CBlockIndex *>(tipIn);
-    if (!tip)
-    {
+    if (!tip) {
         LOCK(cs_main);
         tip = chainActive.Tip();
     }
     return GuessVerificationProgress(Params().TxData(), tip);
 }
 
-void ClientModel::updateTimer()
-{
+void ClientModel::updateTimer() {
     // no locking required at this point
     // the following calls will acquire the required lock
     Q_EMIT mempoolSizeChanged(getMempoolSize(), getMempoolDynamicUsage());
     Q_EMIT bytesChanged(getTotalBytesRecv(), getTotalBytesSent());
 }
 
-void ClientModel::updateNumConnections(int numConnections)
-{
+void ClientModel::updateNumConnections(int numConnections) {
     Q_EMIT numConnectionsChanged(numConnections);
 }
 
-void ClientModel::updateNetworkActive(bool networkActive)
-{
+void ClientModel::updateNetworkActive(bool networkActive) {
     Q_EMIT networkActiveChanged(networkActive);
 }
 
-void ClientModel::updateAlert()
-{
+void ClientModel::updateAlert() {
     Q_EMIT alertsChanged(getStatusBarWarnings());
 }
 
-bool ClientModel::inInitialBlockDownload() const
-{
+bool ClientModel::inInitialBlockDownload() const {
     return IsInitialBlockDownload();
 }
 
-enum BlockSource ClientModel::getBlockSource() const
-{
+enum BlockSource ClientModel::getBlockSource() const {
     if (fReindex)
         return BLOCK_SOURCE_REINDEX;
     else if (fImporting)
@@ -173,148 +147,149 @@ enum BlockSource ClientModel::getBlockSource() const
     return BLOCK_SOURCE_NONE;
 }
 
-void ClientModel::setNetworkActive(bool active)
-{
+void ClientModel::setNetworkActive(bool active) {
     if (g_connman) {
-         g_connman->SetNetworkActive(active);
+        g_connman->SetNetworkActive(active);
     }
 }
 
-bool ClientModel::getNetworkActive() const
-{
+bool ClientModel::getNetworkActive() const {
     if (g_connman) {
         return g_connman->GetNetworkActive();
     }
     return false;
 }
 
-QString ClientModel::getStatusBarWarnings() const
-{
+QString ClientModel::getStatusBarWarnings() const {
     return QString::fromStdString(GetWarnings("gui"));
 }
 
-OptionsModel *ClientModel::getOptionsModel()
-{
+OptionsModel *ClientModel::getOptionsModel() {
     return optionsModel;
 }
 
-PeerTableModel *ClientModel::getPeerTableModel()
-{
+PeerTableModel *ClientModel::getPeerTableModel() {
     return peerTableModel;
 }
 
-BanTableModel *ClientModel::getBanTableModel()
-{
+BanTableModel *ClientModel::getBanTableModel() {
     return banTableModel;
 }
 
-QString ClientModel::formatFullVersion() const
-{
+QString ClientModel::formatFullVersion() const {
     return QString::fromStdString(FormatFullVersion());
 }
 
-QString ClientModel::formatSubVersion() const
-{
+QString ClientModel::formatSubVersion() const {
     return QString::fromStdString(strSubVersion);
 }
 
-bool ClientModel::isReleaseVersion() const
-{
+bool ClientModel::isReleaseVersion() const {
     return CLIENT_VERSION_IS_RELEASE;
 }
 
-QString ClientModel::formatClientStartupTime() const
-{
+QString ClientModel::formatClientStartupTime() const {
     return QDateTime::fromTime_t(nClientStartupTime).toString();
 }
 
-QString ClientModel::dataDir() const
-{
+QString ClientModel::dataDir() const {
     return GUIUtil::boostPathToQString(GetDataDir());
 }
 
-void ClientModel::updateBanlist()
-{
+void ClientModel::updateBanlist() {
     banTableModel->refresh();
 }
 
 // Handlers for core signals
-static void ShowProgress(ClientModel *clientmodel, const std::string &title, int nProgress)
-{
+static void ShowProgress(ClientModel *clientmodel, const std::string &title,
+                         int nProgress) {
     // emits signal "showProgress"
     QMetaObject::invokeMethod(clientmodel, "showProgress", Qt::QueuedConnection,
                               Q_ARG(QString, QString::fromStdString(title)),
                               Q_ARG(int, nProgress));
 }
 
-static void NotifyNumConnectionsChanged(ClientModel *clientmodel, int newNumConnections)
-{
-    // Too noisy: qDebug() << "NotifyNumConnectionsChanged: " + QString::number(newNumConnections);
-    QMetaObject::invokeMethod(clientmodel, "updateNumConnections", Qt::QueuedConnection,
+static void NotifyNumConnectionsChanged(ClientModel *clientmodel,
+                                        int newNumConnections) {
+    // Too noisy: qDebug() << "NotifyNumConnectionsChanged: " +
+    // QString::number(newNumConnections);
+    QMetaObject::invokeMethod(clientmodel, "updateNumConnections",
+                              Qt::QueuedConnection,
                               Q_ARG(int, newNumConnections));
 }
 
-static void NotifyNetworkActiveChanged(ClientModel *clientmodel, bool networkActive)
-{
-    QMetaObject::invokeMethod(clientmodel, "updateNetworkActive", Qt::QueuedConnection,
-                              Q_ARG(bool, networkActive));
+static void NotifyNetworkActiveChanged(ClientModel *clientmodel,
+                                       bool networkActive) {
+    QMetaObject::invokeMethod(clientmodel, "updateNetworkActive",
+                              Qt::QueuedConnection, Q_ARG(bool, networkActive));
 }
 
-static void NotifyAlertChanged(ClientModel *clientmodel)
-{
+static void NotifyAlertChanged(ClientModel *clientmodel) {
     qDebug() << "NotifyAlertChanged";
     QMetaObject::invokeMethod(clientmodel, "updateAlert", Qt::QueuedConnection);
 }
 
-static void BannedListChanged(ClientModel *clientmodel)
-{
+static void BannedListChanged(ClientModel *clientmodel) {
     qDebug() << QString("%1: Requesting update for peer banlist").arg(__func__);
-    QMetaObject::invokeMethod(clientmodel, "updateBanlist", Qt::QueuedConnection);
+    QMetaObject::invokeMethod(clientmodel, "updateBanlist",
+                              Qt::QueuedConnection);
 }
 
-static void BlockTipChanged(ClientModel *clientmodel, bool initialSync, const CBlockIndex *pIndex, bool fHeader)
-{
+static void BlockTipChanged(ClientModel *clientmodel, bool initialSync,
+                            const CBlockIndex *pIndex, bool fHeader) {
     // lock free async UI updates in case we have a new block tip
     // during initial sync, only update the UI if the last update
     // was > 250ms (MODEL_UPDATE_DELAY) ago
     int64_t now = 0;
-    if (initialSync)
-        now = GetTimeMillis();
+    if (initialSync) now = GetTimeMillis();
 
-    int64_t& nLastUpdateNotification = fHeader ? nLastHeaderTipUpdateNotification : nLastBlockTipUpdateNotification;
+    int64_t &nLastUpdateNotification = fHeader
+                                           ? nLastHeaderTipUpdateNotification
+                                           : nLastBlockTipUpdateNotification;
 
     // if we are in-sync, update the UI regardless of last update time
     if (!initialSync || now - nLastUpdateNotification > MODEL_UPDATE_DELAY) {
-        //pass a async signal to the UI thread
-        QMetaObject::invokeMethod(clientmodel, "numBlocksChanged", Qt::QueuedConnection,
-                                  Q_ARG(int, pIndex->nHeight),
-                                  Q_ARG(QDateTime, QDateTime::fromTime_t(pIndex->GetBlockTime())),
-                                  Q_ARG(double, clientmodel->getVerificationProgress(pIndex)),
-                                  Q_ARG(bool, fHeader));
+        // pass a async signal to the UI thread
+        QMetaObject::invokeMethod(
+            clientmodel, "numBlocksChanged", Qt::QueuedConnection,
+            Q_ARG(int, pIndex->nHeight),
+            Q_ARG(QDateTime, QDateTime::fromTime_t(pIndex->GetBlockTime())),
+            Q_ARG(double, clientmodel->getVerificationProgress(pIndex)),
+            Q_ARG(bool, fHeader));
         nLastUpdateNotification = now;
     }
 }
 
-void ClientModel::subscribeToCoreSignals()
-{
+void ClientModel::subscribeToCoreSignals() {
     // Connect signals to client
     uiInterface.ShowProgress.connect(boost::bind(ShowProgress, this, _1, _2));
-    uiInterface.NotifyNumConnectionsChanged.connect(boost::bind(NotifyNumConnectionsChanged, this, _1));
-    uiInterface.NotifyNetworkActiveChanged.connect(boost::bind(NotifyNetworkActiveChanged, this, _1));
-    uiInterface.NotifyAlertChanged.connect(boost::bind(NotifyAlertChanged, this));
+    uiInterface.NotifyNumConnectionsChanged.connect(
+        boost::bind(NotifyNumConnectionsChanged, this, _1));
+    uiInterface.NotifyNetworkActiveChanged.connect(
+        boost::bind(NotifyNetworkActiveChanged, this, _1));
+    uiInterface.NotifyAlertChanged.connect(
+        boost::bind(NotifyAlertChanged, this));
     uiInterface.BannedListChanged.connect(boost::bind(BannedListChanged, this));
-    uiInterface.NotifyBlockTip.connect(boost::bind(BlockTipChanged, this, _1, _2, false));
-    uiInterface.NotifyHeaderTip.connect(boost::bind(BlockTipChanged, this, _1, _2, true));
+    uiInterface.NotifyBlockTip.connect(
+        boost::bind(BlockTipChanged, this, _1, _2, false));
+    uiInterface.NotifyHeaderTip.connect(
+        boost::bind(BlockTipChanged, this, _1, _2, true));
 }
 
-void ClientModel::unsubscribeFromCoreSignals()
-{
+void ClientModel::unsubscribeFromCoreSignals() {
     // Disconnect signals from client
-    uiInterface.ShowProgress.disconnect(boost::bind(ShowProgress, this, _1, _2));
-    uiInterface.NotifyNumConnectionsChanged.disconnect(boost::bind(NotifyNumConnectionsChanged, this, _1));
-    uiInterface.NotifyNetworkActiveChanged.disconnect(boost::bind(NotifyNetworkActiveChanged, this, _1));
-    uiInterface.NotifyAlertChanged.disconnect(boost::bind(NotifyAlertChanged, this));
-    uiInterface.BannedListChanged.disconnect(boost::bind(BannedListChanged, this));
-    uiInterface.NotifyBlockTip.disconnect(boost::bind(BlockTipChanged, this, _1, _2, false));
-    uiInterface.NotifyHeaderTip.disconnect(boost::bind(BlockTipChanged, this, _1, _2, true));
+    uiInterface.ShowProgress.disconnect(
+        boost::bind(ShowProgress, this, _1, _2));
+    uiInterface.NotifyNumConnectionsChanged.disconnect(
+        boost::bind(NotifyNumConnectionsChanged, this, _1));
+    uiInterface.NotifyNetworkActiveChanged.disconnect(
+        boost::bind(NotifyNetworkActiveChanged, this, _1));
+    uiInterface.NotifyAlertChanged.disconnect(
+        boost::bind(NotifyAlertChanged, this));
+    uiInterface.BannedListChanged.disconnect(
+        boost::bind(BannedListChanged, this));
+    uiInterface.NotifyBlockTip.disconnect(
+        boost::bind(BlockTipChanged, this, _1, _2, false));
+    uiInterface.NotifyHeaderTip.disconnect(
+        boost::bind(BlockTipChanged, this, _1, _2, true));
 }
--- src/test/net_tests.cpp
+++ src/test/net_tests.cpp
@@ -1,45 +1,38 @@
 // Copyright (c) 2012-2016 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include "net.h"
 #include "addrman.h"
-#include "test/test_bitcoin.h"
-#include <string>
-#include <boost/test/unit_test.hpp>
+#include "chainparams.h"
 #include "hash.h"
+#include "netbase.h"
 #include "serialize.h"
 #include "streams.h"
-#include "net.h"
-#include "netbase.h"
-#include "chainparams.h"
+#include "test/test_bitcoin.h"
+#include <boost/test/unit_test.hpp>
+#include <string>
 
-class CAddrManSerializationMock : public CAddrMan
-{
+class CAddrManSerializationMock : public CAddrMan {
 public:
-    virtual void Serialize(CDataStream& s) const = 0;
+    virtual void Serialize(CDataStream &s) const = 0;
 
     //! Ensure that bucket placement is always the same for testing purposes.
-    void MakeDeterministic()
-    {
+    void MakeDeterministic() {
         nKey.SetNull();
         insecure_rand = FastRandomContext(true);
     }
 };
 
-class CAddrManUncorrupted : public CAddrManSerializationMock
-{
+class CAddrManUncorrupted : public CAddrManSerializationMock {
 public:
-    void Serialize(CDataStream& s) const
-    {
-        CAddrMan::Serialize(s);
-    }
+    void Serialize(CDataStream &s) const { CAddrMan::Serialize(s); }
 };
 
-class CAddrManCorrupted : public CAddrManSerializationMock
-{
+class CAddrManCorrupted : public CAddrManSerializationMock {
 public:
-    void Serialize(CDataStream& s) const
-    {
-        // Produces corrupt output that claims addrman has 20 addrs when it only has one addr.
+    void Serialize(CDataStream &s) const {
+        // Produces corrupt output that claims addrman has 20 addrs when it only
+        // has one addr.
         unsigned char nVersion = 1;
         s << nVersion;
         s << ((unsigned char)32);
@@ -60,8 +53,7 @@ public:
     }
 };
 
-CDataStream AddrmanToStream(CAddrManSerializationMock& _addrman)
-{
+CDataStream AddrmanToStream(CAddrManSerializationMock &_addrman) {
     CDataStream ssPeersIn(SER_DISK, CLIENT_VERSION);
     ssPeersIn << FLATDATA(Params().MessageStart());
     ssPeersIn << _addrman;
@@ -72,8 +64,7 @@ CDataStream AddrmanToStream(CAddrManSerializationMock& _addrman)
 
 BOOST_FIXTURE_TEST_SUITE(net_tests, BasicTestingSetup)
 
-BOOST_AUTO_TEST_CASE(caddrdb_read)
-{
+BOOST_AUTO_TEST_CASE(caddrdb_read) {
     CAddrManUncorrupted addrmanUncorrupted;
     addrmanUncorrupted.MakeDeterministic();
 
@@ -99,14 +90,15 @@ BOOST_AUTO_TEST_CASE(caddrdb_read)
         unsigned char pchMsgTmp[4];
         ssPeers1 >> FLATDATA(pchMsgTmp);
         ssPeers1 >> addrman1;
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         exceptionThrown = true;
     }
 
     BOOST_CHECK(addrman1.size() == 3);
     BOOST_CHECK(exceptionThrown == false);
 
-    // Test that CAddrDB::Read creates an addrman with the correct number of addrs.
+    // Test that CAddrDB::Read creates an addrman with the correct number of
+    // addrs.
     CDataStream ssPeers2 = AddrmanToStream(addrmanUncorrupted);
 
     CAddrMan addrman2;
@@ -116,9 +108,7 @@ BOOST_AUTO_TEST_CASE(caddrdb_read)
     BOOST_CHECK(addrman2.size() == 3);
 }
 
-
-BOOST_AUTO_TEST_CASE(caddrdb_read_corrupted)
-{
+BOOST_AUTO_TEST_CASE(caddrdb_read_corrupted) {
     CAddrManCorrupted addrmanCorrupted;
     addrmanCorrupted.MakeDeterministic();
 
@@ -131,14 +121,16 @@ BOOST_AUTO_TEST_CASE(caddrdb_read_corrupted)
         unsigned char pchMsgTmp[4];
         ssPeers1 >> FLATDATA(pchMsgTmp);
         ssPeers1 >> addrman1;
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         exceptionThrown = true;
     }
-    // Even through de-serialization failed addrman is not left in a clean state.
+    // Even through de-serialization failed addrman is not left in a clean
+    // state.
     BOOST_CHECK(addrman1.size() == 1);
     BOOST_CHECK(exceptionThrown);
 
-    // Test that CAddrDB::Read leaves addrman in a clean state if de-serialization fails.
+    // Test that CAddrDB::Read leaves addrman in a clean state if
+    // de-serialization fails.
     CDataStream ssPeers2 = AddrmanToStream(addrmanCorrupted);
 
     CAddrMan addrman2;
@@ -148,26 +140,27 @@ BOOST_AUTO_TEST_CASE(caddrdb_read_corrupted)
     BOOST_CHECK(addrman2.size() == 0);
 }
 
-BOOST_AUTO_TEST_CASE(cnode_simple_test)
-{
+BOOST_AUTO_TEST_CASE(cnode_simple_test) {
     SOCKET hSocket = INVALID_SOCKET;
     NodeId id = 0;
     int height = 0;
 
     in_addr ipv4Addr;
     ipv4Addr.s_addr = 0xa0b0c001;
-    
+
     CAddress addr = CAddress(CService(ipv4Addr, 7777), NODE_NETWORK);
     std::string pszDest = "";
     bool fInboundIn = false;
 
     // Test that fFeeler is false by default.
-    std::unique_ptr<CNode> pnode1(new CNode(id++, NODE_NETWORK, height, hSocket, addr, 0, 0, pszDest, fInboundIn));
+    std::unique_ptr<CNode> pnode1(new CNode(id++, NODE_NETWORK, height, hSocket,
+                                            addr, 0, 0, pszDest, fInboundIn));
     BOOST_CHECK(pnode1->fInbound == false);
     BOOST_CHECK(pnode1->fFeeler == false);
 
     fInboundIn = true;
-    std::unique_ptr<CNode> pnode2(new CNode(id++, NODE_NETWORK, height, hSocket, addr, 1, 1, pszDest, fInboundIn));
+    std::unique_ptr<CNode> pnode2(new CNode(id++, NODE_NETWORK, height, hSocket,
+                                            addr, 1, 1, pszDest, fInboundIn));
     BOOST_CHECK(pnode2->fInbound == true);
     BOOST_CHECK(pnode2->fFeeler == false);
 }
