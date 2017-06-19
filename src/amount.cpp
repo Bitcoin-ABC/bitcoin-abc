f5d30b1a71aefc70484ce567a4ab8328fa4e5202
Amaury SECHET <deadalnix@gmail.com>
2017-06-19 21:54:10 +0200

    Format amount.h and amount.cpp
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: freetrader, sickpig, awemany, kyuupichan, #bitcoin_abc
    
    Reviewed By: freetrader, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D236

--- src/amount.cpp
+++ src/amount.cpp
@@ -9,8 +9,7 @@
 
 const std::string CURRENCY_UNIT = "BTC";
 
-CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nBytes_)
-{
+CFeeRate::CFeeRate(const CAmount &nFeePaid, size_t nBytes_) {
     assert(nBytes_ <= uint64_t(std::numeric_limits<int64_t>::max()));
     int64_t nSize = int64_t(nBytes_);
 
@@ -20,24 +19,21 @@ CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nBytes_)
         nSatoshisPerK = 0;
 }
 
-CAmount CFeeRate::GetFee(size_t nBytes_) const
-{
+CAmount CFeeRate::GetFee(size_t nBytes_) const {
     assert(nBytes_ <= uint64_t(std::numeric_limits<int64_t>::max()));
     int64_t nSize = int64_t(nBytes_);
 
     CAmount nFee = nSatoshisPerK * nSize / 1000;
 
     if (nFee == 0 && nSize != 0) {
-        if (nSatoshisPerK > 0)
-            nFee = CAmount(1);
-        if (nSatoshisPerK < 0)
-            nFee = CAmount(-1);
+        if (nSatoshisPerK > 0) nFee = CAmount(1);
+        if (nSatoshisPerK < 0) nFee = CAmount(-1);
     }
 
     return nFee;
 }
 
-std::string CFeeRate::ToString() const
-{
-    return strprintf("%d.%08d %s/kB", nSatoshisPerK / COIN, nSatoshisPerK % COIN, CURRENCY_UNIT);
+std::string CFeeRate::ToString() const {
+    return strprintf("%d.%08d %s/kB", nSatoshisPerK / COIN,
+                     nSatoshisPerK % COIN, CURRENCY_UNIT);
 }
