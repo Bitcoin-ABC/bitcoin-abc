be6f171382d99bf9c649d78e91a7f425af0b9e13
Wladimir J. van der Laan <laanwj@gmail.com>
2017-06-22 18:35:39 +0200

    Merge #9517: [refactor] Switched httpserver.cpp to use RAII wrapped libevents.
    Summary:
    1ae86ec Changed event RAII helper functions to inline to deal with duplicate symbol linker errors. (Karl-Johan Alm)
    fd369d2 Switched httpserver.cpp to use RAII wrapped libevents. (Kalle Alm)
    
    Tree-SHA512: 877e431f211024d42a3b0800e860e02833398611433e8393f8d5d4970f47f4bd670b900443678c067fec110c087aaab7dc1981ccbf17f6057676fdbbda89aed9
    
    Backport of Core PR 9517
    https://github.com/bitcoin/bitcoin/pull/9517/files
    
    Test Plan:
    make check
    ninja check
    test_runner.py
    
    Reviewers: deadalnix, Fabien, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D2897

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -17,11 +17,11 @@
 
 #include <event2/buffer.h>
 #include <event2/bufferevent.h>
-#include <event2/event.h>
-#include <event2/http.h>
 #include <event2/keyvalq_struct.h>
 #include <event2/thread.h>
 #include <event2/util.h>
+
+#include <support/events.h>
 
 #ifdef EVENT__HAVE_NETINET_IN_H
 #include <netinet/in.h>
@@ -359,10 +359,9 @@ static void libevent_log_cb(int severity, const char *msg) {
 }
 
 bool InitHTTPServer(Config &config) {
-    struct evhttp *http = 0;
-    struct event_base *base = 0;
-
-    if (!InitHTTPAllowList()) return false;
+    if (!InitHTTPAllowList()) {
+        return false;
+    }
 
     if (gArgs.GetBoolArg("-rpcssl", false)) {
         uiInterface.ThreadSafeMessageBox(
@@ -388,18 +387,13 @@ bool InitHTTPServer(Config &config) {
     evthread_use_pthreads();
 #endif
 
-    // XXX RAII: Create a new event_base for Libevent use
-    base = event_base_new();
-    if (!base) {
-        LogPrintf("Couldn't create an event_base: exiting\n");
-        return false;
-    }
+    raii_event_base base_ctr = obtain_event_base();
 
-    // XXX RAII: Create a new evhttp object to handle requests
-    http = evhttp_new(base);
+    /* Create a new evhttp object to handle requests. */
+    raii_evhttp http_ctr = obtain_evhttp(base_ctr.get());
+    struct evhttp *http = http_ctr.get();
     if (!http) {
         LogPrintf("couldn't create evhttp. Exiting.\n");
-        event_base_free(base);
         return false;
     }
 
@@ -418,8 +412,6 @@ bool InitHTTPServer(Config &config) {
 
     if (!HTTPBindAddresses(http)) {
         LogPrintf("Unable to bind any endpoint for RPC server\n");
-        evhttp_free(http);
-        event_base_free(base);
         return false;
     }
 
@@ -429,8 +421,9 @@ bool InitHTTPServer(Config &config) {
     LogPrintf("HTTP: creating work queue of depth %d\n", workQueueDepth);
 
     workQueue = new WorkQueue<HTTPClosure>(workQueueDepth);
-    eventBase = base;
-    eventHTTP = http;
+    // tranfer ownership to eventBase/HTTP via .release()
+    eventBase = base_ctr.release();
+    eventHTTP = http_ctr.release();
     return true;
 }
 
