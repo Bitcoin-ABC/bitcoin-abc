b8500e6989310f530a8251ec0255ce12e4068f14
Amaury SECHET <deadalnix@gmail.com>
2017-06-23 23:31:03 +0200

    Format warning.{h,cpp}
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: freetrader, sickpig, awemany, kyuupichan, #bitcoin_abc
    
    Reviewed By: freetrader, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D267

--- src/warnings.cpp
+++ src/warnings.cpp
@@ -3,48 +3,42 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "sync.h"
-#include "clientversion.h"
-#include "util.h"
 #include "warnings.h"
+#include "clientversion.h"
+#include "sync.h"
+#include "util.h"
 
 CCriticalSection cs_warnings;
 std::string strMiscWarning;
 bool fLargeWorkForkFound = false;
 bool fLargeWorkInvalidChainFound = false;
 
-void SetMiscWarning(const std::string& strWarning)
-{
+void SetMiscWarning(const std::string &strWarning) {
     LOCK(cs_warnings);
     strMiscWarning = strWarning;
 }
 
-void SetfLargeWorkForkFound(bool flag)
-{
+void SetfLargeWorkForkFound(bool flag) {
     LOCK(cs_warnings);
     fLargeWorkForkFound = flag;
 }
 
-bool GetfLargeWorkForkFound()
-{
+bool GetfLargeWorkForkFound() {
     LOCK(cs_warnings);
     return fLargeWorkForkFound;
 }
 
-void SetfLargeWorkInvalidChainFound(bool flag)
-{
+void SetfLargeWorkInvalidChainFound(bool flag) {
     LOCK(cs_warnings);
     fLargeWorkInvalidChainFound = flag;
 }
 
-bool GetfLargeWorkInvalidChainFound()
-{
+bool GetfLargeWorkInvalidChainFound() {
     LOCK(cs_warnings);
     return fLargeWorkInvalidChainFound;
 }
 
-std::string GetWarnings(const std::string& strFor)
-{
+std::string GetWarnings(const std::string &strFor) {
     std::string strStatusBar;
     std::string strRPC;
     std::string strGUI;
@@ -53,29 +47,36 @@ std::string GetWarnings(const std::string& strFor)
     LOCK(cs_warnings);
 
     if (!CLIENT_VERSION_IS_RELEASE) {
-        strStatusBar = "This is a pre-release test build - use at your own risk - do not use for mining or merchant applications";
-        strGUI = _("This is a pre-release test build - use at your own risk - do not use for mining or merchant applications");
+        strStatusBar = "This is a pre-release test build - use at your own "
+                       "risk - do not use for mining or merchant applications";
+        strGUI = _("This is a pre-release test build - use at your own risk - "
+                   "do not use for mining or merchant applications");
     }
 
     if (GetBoolArg("-testsafemode", DEFAULT_TESTSAFEMODE))
         strStatusBar = strRPC = strGUI = "testsafemode enabled";
 
     // Misc warnings like out of disk space and clock is wrong
-    if (strMiscWarning != "")
-    {
+    if (strMiscWarning != "") {
         strStatusBar = strMiscWarning;
         strGUI += (strGUI.empty() ? "" : uiAlertSeperator) + strMiscWarning;
     }
 
-    if (fLargeWorkForkFound)
-    {
-        strStatusBar = strRPC = "Warning: The network does not appear to fully agree! Some miners appear to be experiencing issues.";
-        strGUI += (strGUI.empty() ? "" : uiAlertSeperator) + _("Warning: The network does not appear to fully agree! Some miners appear to be experiencing issues.");
-    }
-    else if (fLargeWorkInvalidChainFound)
-    {
-        strStatusBar = strRPC = "Warning: We do not appear to fully agree with our peers! You may need to upgrade, or other nodes may need to upgrade.";
-        strGUI += (strGUI.empty() ? "" : uiAlertSeperator) + _("Warning: We do not appear to fully agree with our peers! You may need to upgrade, or other nodes may need to upgrade.");
+    if (fLargeWorkForkFound) {
+        strStatusBar = strRPC = "Warning: The network does not appear to fully "
+                                "agree! Some miners appear to be experiencing "
+                                "issues.";
+        strGUI += (strGUI.empty() ? "" : uiAlertSeperator) +
+                  _("Warning: The network does not appear to fully agree! Some "
+                    "miners appear to be experiencing issues.");
+    } else if (fLargeWorkInvalidChainFound) {
+        strStatusBar = strRPC = "Warning: We do not appear to fully agree with "
+                                "our peers! You may need to upgrade, or other "
+                                "nodes may need to upgrade.";
+        strGUI +=
+            (strGUI.empty() ? "" : uiAlertSeperator) +
+            _("Warning: We do not appear to fully agree with our peers! You "
+              "may need to upgrade, or other nodes may need to upgrade.");
     }
 
     if (strFor == "gui")
--- src/warnings.h
+++ src/warnings.h
@@ -6,15 +6,15 @@
 #ifndef BITCOIN_WARNINGS_H
 #define BITCOIN_WARNINGS_H
 
-#include <stdlib.h>
+#include <cstdlib>
 #include <string>
 
-void SetMiscWarning(const std::string& strWarning);
+void SetMiscWarning(const std::string &strWarning);
 void SetfLargeWorkForkFound(bool flag);
 bool GetfLargeWorkForkFound();
 void SetfLargeWorkInvalidChainFound(bool flag);
 bool GetfLargeWorkInvalidChainFound();
-std::string GetWarnings(const std::string& strFor);
+std::string GetWarnings(const std::string &strFor);
 
 static const bool DEFAULT_TESTSAFEMODE = false;
 
