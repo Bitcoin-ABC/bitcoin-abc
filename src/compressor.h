0ebcdcbdd9cb85e33e819a58a19efc0a674813e8
Amaury SECHET <deadalnix@gmail.com>
2017-06-24 01:01:41 +0200

    Format compressor.{h,cpp}
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: freetrader, sickpig, awemany, kyuupichan, #bitcoin_abc
    
    Reviewed By: freetrader, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D276

--- src/compressor.h
+++ src/compressor.h
@@ -14,36 +14,37 @@ class CKeyID;
 class CPubKey;
 class CScriptID;
 
-/** Compact serializer for scripts.
+/**
+ * Compact serializer for scripts.
  *
- *  It detects common cases and encodes them much more efficiently.
- *  3 special cases are defined:
+ * It detects common cases and encodes them much more efficiently.
+ * 3 special cases are defined:
  *  * Pay to pubkey hash (encoded as 21 bytes)
  *  * Pay to script hash (encoded as 21 bytes)
  *  * Pay to pubkey starting with 0x02, 0x03 or 0x04 (encoded as 33 bytes)
  *
- *  Other scripts up to 121 bytes require 1 byte + script length. Above
- *  that, scripts up to 16505 bytes require 2 bytes + script length.
+ * Other scripts up to 121 bytes require 1 byte + script length. Above that,
+ * scripts up to 16505 bytes require 2 bytes + script length.
  */
-class CScriptCompressor
-{
+class CScriptCompressor {
 private:
     /**
-     * make this static for now (there are only 6 special scripts defined)
-     * this can potentially be extended together with a new nVersion for
-     * transactions, in which case this value becomes dependent on nVersion
-     * and nHeight of the enclosing transaction.
+     * make this static for now (there are only 6 special scripts defined) this
+     * can potentially be extended together with a new nVersion for
+     * transactions, in which case this value becomes dependent on nVersion and
+     * nHeight of the enclosing transaction.
      */
     static const unsigned int nSpecialScripts = 6;
 
     CScript &script;
+
 protected:
     /**
-     * These check for scripts for which a special case with a shorter encoding is defined.
-     * They are implemented separately from the CScript test, as these test for exact byte
-     * sequence correspondences, and are more strict. For example, IsToPubKey also verifies
-     * whether the public key is valid (as invalid ones cannot be represented in compressed
-     * form).
+     * These check for scripts for which a special case with a shorter encoding
+     * is defined. They are implemented separately from the CScript test, as
+     * these test for exact byte sequence correspondences, and are more strict.
+     * For example, IsToPubKey also verifies whether the public key is valid (as
+     * invalid ones cannot be represented in compressed form).
      */
     bool IsToKeyID(CKeyID &hash) const;
     bool IsToScriptID(CScriptID &hash) const;
@@ -52,11 +53,11 @@ protected:
     bool Compress(std::vector<unsigned char> &out) const;
     unsigned int GetSpecialSize(unsigned int nSize) const;
     bool Decompress(unsigned int nSize, const std::vector<unsigned char> &out);
-public:
-    CScriptCompressor(CScript &scriptIn) : script(scriptIn) { }
 
-    template<typename Stream>
-    void Serialize(Stream &s) const {
+public:
+    CScriptCompressor(CScript &scriptIn) : script(scriptIn) {}
+
+    template <typename Stream> void Serialize(Stream &s) const {
         std::vector<unsigned char> compr;
         if (Compress(compr)) {
             s << CFlatData(compr);
@@ -67,8 +68,7 @@ public:
         s << CFlatData(script);
     }
 
-    template<typename Stream>
-    void Unserialize(Stream &s) {
+    template <typename Stream> void Unserialize(Stream &s) {
         unsigned int nSize = 0;
         s >> VARINT(nSize);
         if (nSize < nSpecialScripts) {
@@ -90,8 +90,7 @@ public:
 };
 
 /** wrapper for CTxOut that provides a more compact serialization */
-class CTxOutCompressor
-{
+class CTxOutCompressor {
 private:
     CTxOut &txout;
 
@@ -99,12 +98,12 @@ public:
     static uint64_t CompressAmount(uint64_t nAmount);
     static uint64_t DecompressAmount(uint64_t nAmount);
 
-    CTxOutCompressor(CTxOut &txoutIn) : txout(txoutIn) { }
+    CTxOutCompressor(CTxOut &txoutIn) : txout(txoutIn) {}
 
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action) {
+    inline void SerializationOp(Stream &s, Operation ser_action) {
         if (!ser_action.ForRead()) {
             uint64_t nVal = CompressAmount(txout.nValue);
             READWRITE(VARINT(nVal));
