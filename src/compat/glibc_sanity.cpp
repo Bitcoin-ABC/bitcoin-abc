d907896b5f74be287d99258fdb7208c31ee6093b
Amaury SECHET <deadalnix@gmail.com>
2017-06-23 17:49:56 +0200

    Format the compat module
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: freetrader, sickpig, awemany, kyuupichan, #bitcoin_abc
    
    Reviewed By: freetrader, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D261

--- src/compat/glibc_sanity.cpp
+++ src/compat/glibc_sanity.cpp
@@ -12,22 +12,18 @@
 #include <sys/select.h>
 #endif
 
-extern "C" void* memcpy(void* a, const void* b, size_t c);
-void* memcpy_int(void* a, const void* b, size_t c)
-{
+extern "C" void *memcpy(void *a, const void *b, size_t c);
+void *memcpy_int(void *a, const void *b, size_t c) {
     return memcpy(a, b, c);
 }
 
-namespace
-{
+namespace {
 // trigger: Use the memcpy_int wrapper which calls our internal memcpy.
 //   A direct call to memcpy may be optimized away by the compiler.
 // test: Fill an array with a sequence of integers. memcpy to a new empty array.
 //   Verify that the arrays are equal. Use an odd size to decrease the odds of
 //   the call being optimized away.
-template <unsigned int T>
-bool sanity_test_memcpy()
-{
+template <unsigned int T> bool sanity_test_memcpy() {
     unsigned int memcpy_test[T];
     unsigned int memcpy_verify[T] = {};
     for (unsigned int i = 0; i != T; ++i)
@@ -36,8 +32,7 @@ bool sanity_test_memcpy()
     memcpy_int(memcpy_verify, memcpy_test, sizeof(memcpy_test));
 
     for (unsigned int i = 0; i != T; ++i) {
-        if (memcpy_verify[i] != i)
-            return false;
+        if (memcpy_verify[i] != i) return false;
     }
     return true;
 }
@@ -47,8 +42,7 @@ bool sanity_test_memcpy()
 //   as >0 and optimizations must be set to at least -O2.
 // test: Add a file descriptor to an empty fd_set. Verify that it has been
 //   correctly added.
-bool sanity_test_fdelt()
-{
+bool sanity_test_fdelt() {
     fd_set fds;
     FD_ZERO(&fds);
     FD_SET(0, &fds);
@@ -58,11 +52,9 @@ bool sanity_test_fdelt()
 
 } // anon namespace
 
-bool glibc_sanity_test()
-{
+bool glibc_sanity_test() {
 #if defined(HAVE_SYS_SELECT_H)
-    if (!sanity_test_fdelt())
-        return false;
+    if (!sanity_test_fdelt()) return false;
 #endif
     return sanity_test_memcpy<1025>();
 }
--- src/compat/glibcxx_sanity.cpp
+++ src/compat/glibcxx_sanity.cpp
@@ -6,14 +6,13 @@
 #include <locale>
 #include <stdexcept>
 
-namespace
-{
+namespace {
 // trigger: use ctype<char>::widen to trigger ctype<char>::_M_widen_init().
 // test: convert a char from narrow to wide and back. Verify that the result
 //   matches the original.
-bool sanity_test_widen(char testchar)
-{
-    const std::ctype<char>& test(std::use_facet<std::ctype<char> >(std::locale()));
+bool sanity_test_widen(char testchar) {
+    const std::ctype<char> &test(
+        std::use_facet<std::ctype<char>>(std::locale()));
     return test.narrow(test.widen(testchar), 'b') == testchar;
 }
 
@@ -21,18 +20,15 @@ bool sanity_test_widen(char testchar)
 //   _M_unhook.
 // test: Push a sequence of integers into a list. Pop them off and verify that
 //   they match the original sequence.
-bool sanity_test_list(unsigned int size)
-{
+bool sanity_test_list(unsigned int size) {
     std::list<unsigned int> test;
     for (unsigned int i = 0; i != size; ++i)
         test.push_back(i + 1);
 
-    if (test.size() != size)
-        return false;
+    if (test.size() != size) return false;
 
     while (!test.empty()) {
-        if (test.back() != test.size())
-            return false;
+        if (test.back() != test.size()) return false;
         test.pop_back();
     }
     return true;
@@ -40,22 +36,22 @@ bool sanity_test_list(unsigned int size)
 
 } // anon namespace
 
-// trigger: string::at(x) on an empty string to trigger __throw_out_of_range_fmt.
+// trigger: string::at(x) on an empty string to trigger
+// __throw_out_of_range_fmt.
 // test: force std::string to throw an out_of_range exception. Verify that
 //   it's caught correctly.
-bool sanity_test_range_fmt()
-{
+bool sanity_test_range_fmt() {
     std::string test;
     try {
         test.at(1);
-    } catch (const std::out_of_range&) {
+    } catch (const std::out_of_range &) {
         return true;
     } catch (...) {
     }
     return false;
 }
 
-bool glibcxx_sanity_test()
-{
-    return sanity_test_widen('a') && sanity_test_list(100) && sanity_test_range_fmt();
+bool glibcxx_sanity_test() {
+    return sanity_test_widen('a') && sanity_test_list(100) &&
+           sanity_test_range_fmt();
 }
