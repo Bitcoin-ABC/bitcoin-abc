5da70e52d2aac70e16b3a983c4e2dfa5bb28bdba
practicalswift <practicalswift@users.noreply.github.com>
2017-07-09 22:53:45 +0200

    Avoid reference to undefined name: stderr does not exist, sys.stderr does
    Summary:
    Backport 2-of-6 for Core PR10781
    T473
    
    Test Plan: I don't think anyone of us uses this, so I didn't really test it.
    
    Reviewers: deadalnix, schancel, Fabien, #bitcoin_abc
    
    Reviewed By: Fabien
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1999

--- contrib/linearize/linearize-hashes.py
+++ contrib/linearize/linearize-hashes.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
     if 'rev_hash_bytes' not in settings:
         settings['rev_hash_bytes'] = 'false'
     if 'rpcuser' not in settings or 'rpcpassword' not in settings:
-        print("Missing username and/or password in cfg file", file=stderr)
+        print("Missing username and/or password in cfg file", file=sys.stderr)
         sys.exit(1)
 
     settings['port'] = int(settings['port'])
