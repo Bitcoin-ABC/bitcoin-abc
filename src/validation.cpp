d2e256150504e1929a322417b61b7651c913f718
hanchon <guillermo@bitprim.org>
2017-07-25 11:59:23 -0300

    Indexes moved inside not coinbase code

--- src/validation.cpp
+++ src/validation.cpp
@@ -2129,40 +2129,40 @@ static bool ConnectBlock(const Config &config, const CBlock &block,
                                __func__),
                     REJECT_INVALID, "bad-txns-nonfinal");
             }
-        }
 
-        if (fAddressIndex || fSpentIndex)
-        {
-            for (size_t j = 0; j < tx.vin.size(); j++) {
+            if (fAddressIndex || fSpentIndex)
+            {
+                for (size_t j = 0; j < tx.vin.size(); j++) {
 
-                const CTxIn input = tx.vin[j];
-                const CTxOut &prevout = view.GetOutputFor(tx.vin[j]);
-                uint160 hashBytes;
-                int addressType;
+                    const CTxIn input = tx.vin[j];
+                    const CTxOut &prevout = view.GetOutputFor(tx.vin[j]);
+                    uint160 hashBytes;
+                    int addressType;
 
-                if (prevout.scriptPubKey.IsPayToScriptHash()) {
-                    hashBytes = uint160(std::vector <unsigned char>(prevout.scriptPubKey.begin()+2, prevout.scriptPubKey.begin()+22));
-                    addressType = 2;
-                } else if (prevout.scriptPubKey.IsPayToPublicKeyHash()) {
-                    hashBytes = uint160(std::vector <unsigned char>(prevout.scriptPubKey.begin()+3, prevout.scriptPubKey.begin()+23));
-                    addressType = 1;
-                } else {
-                    hashBytes.SetNull();
-                    addressType = 0;
-                }
+                    if (prevout.scriptPubKey.IsPayToScriptHash()) {
+                        hashBytes = uint160(std::vector <unsigned char>(prevout.scriptPubKey.begin()+2, prevout.scriptPubKey.begin()+22));
+                        addressType = 2;
+                    } else if (prevout.scriptPubKey.IsPayToPublicKeyHash()) {
+                        hashBytes = uint160(std::vector <unsigned char>(prevout.scriptPubKey.begin()+3, prevout.scriptPubKey.begin()+23));
+                        addressType = 1;
+                    } else {
+                        hashBytes.SetNull();
+                        addressType = 0;
+                    }
 
-                if (fAddressIndex && addressType > 0) {
-                    // record spending activity
-                    addressIndex.push_back(std::make_pair(CAddressIndexKey(addressType, hashBytes, pindex->nHeight, i, txhash, j, true), prevout.nValue * -1));
+                    if (fAddressIndex && addressType > 0) {
+                        // record spending activity
+                        addressIndex.push_back(std::make_pair(CAddressIndexKey(addressType, hashBytes, pindex->nHeight, i, txhash, j, true), prevout.nValue * -1));
 
-                    // remove address from unspent index
-                    addressUnspentIndex.push_back(std::make_pair(CAddressUnspentKey(addressType, hashBytes, input.prevout.hash, input.prevout.n), CAddressUnspentValue()));
-                }
+                        // remove address from unspent index
+                        addressUnspentIndex.push_back(std::make_pair(CAddressUnspentKey(addressType, hashBytes, input.prevout.hash, input.prevout.n), CAddressUnspentValue()));
+                    }
 
-                if (fSpentIndex) {
-                    // add the spent index to determine the txid and input that spent an output
-                    // and to find the amount and address from an input
-                    spentIndex.push_back(std::make_pair(CSpentIndexKey(input.prevout.hash, input.prevout.n), CSpentIndexValue(txhash, j, pindex->nHeight, prevout.nValue, addressType, hashBytes)));
+                    if (fSpentIndex) {
+                        // add the spent index to determine the txid and input that spent an output
+                        // and to find the amount and address from an input
+                        spentIndex.push_back(std::make_pair(CSpentIndexKey(input.prevout.hash, input.prevout.n), CSpentIndexValue(txhash, j, pindex->nHeight, prevout.nValue, addressType, hashBytes)));
+                    }
                 }
             }
         }
