f9d5ed9d7c6d66841decdb2ca3f56a959b8fc4f6
howelzy <howelzy@iMac-2.local>
2017-07-30 14:31:09 +0100

    more rebranding changes

--- src/init.cpp
+++ src/init.cpp
@@ -866,8 +866,8 @@ std::string HelpMessage(HelpMessageMode mode) {
 
 std::string LicenseInfo() {
     const std::string URL_SOURCE_CODE =
-        "<https://github.com/Bitcoin-ABC/bitcoin-abc>";
-    const std::string URL_WEBSITE = "<https://www.bitcoinabc.org>";
+        "<https://github.com/BitcoinXBT/bitcoinxbt>";
+    const std::string URL_WEBSITE = "<https://www.bitcoinxbt.org>";
 
     return CopyrightHolders(
                strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR) +
