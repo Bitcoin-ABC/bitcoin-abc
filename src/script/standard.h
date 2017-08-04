f89b82b0055433120a01e9624f9bdebfe2315e4b
Amaury SECHET <deadalnix@gmail.com>
2017-08-05 00:42:13 +0200

    Make SCRIPT_VERIFY_STRICTENC a mandatory flag
    Summary: This flag has been made mandatory by the HF.
    
    Test Plan:
      make check
      ../qa/pull-tester/rpc-tests.py
    
    Reviewers: freetrader, CCulianu, #bitcoin_abc, sickpig
    
    Reviewed By: freetrader, #bitcoin_abc, sickpig
    
    Differential Revision: https://reviews.bitcoinabc.org/D421

--- src/script/standard.h
+++ src/script/standard.h
@@ -40,7 +40,8 @@ extern unsigned nMaxDatacarrierBytes;
  * Failing one of these tests may trigger a DoS ban - see CheckInputs() for
  * details.
  */
-static const unsigned int MANDATORY_SCRIPT_VERIFY_FLAGS = SCRIPT_VERIFY_P2SH;
+static const unsigned int MANDATORY_SCRIPT_VERIFY_FLAGS =
+    SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_STRICTENC;
 
 enum txnouttype {
     TX_NONSTANDARD,
