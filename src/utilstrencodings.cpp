bc94722fc53951cc2631762a1363e98ce92c5d22
ftrader <freetrader@tuta.io>
2017-08-10 13:39:25 +0000

    Get rid of blanket std import from util*.cpp and script/ismine.cpp
    Summary: Together with D461, this gets rid of remaining blanket std imports in all but univalue.
    
    Test Plan:
    make check
    ../qa/pull-tester/rpc-tests.py -extended
    
    Reviewers: #bitcoin_abc, deadalnix, CCulianu
    
    Reviewed By: #bitcoin_abc, CCulianu
    
    Subscribers: CCulianu
    
    Differential Revision: https://reviews.bitcoinabc.org/D462

--- src/utilstrencodings.cpp
+++ src/utilstrencodings.cpp
@@ -12,20 +12,18 @@
 #include <cstring>
 #include <limits>
 
-using namespace std;
-
-static const string CHARS_ALPHA_NUM =
+static const std::string CHARS_ALPHA_NUM =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-static const string SAFE_CHARS[] = {
+static const std::string SAFE_CHARS[] = {
     // SAFE_CHARS_DEFAULT
     CHARS_ALPHA_NUM + " .,;-_/:?@()",
     // SAFE_CHARS_UA_COMMENT
     CHARS_ALPHA_NUM + " .,;-_?@",
 };
 
-string SanitizeString(const string &str, int rule) {
-    string strResult;
+std::string SanitizeString(const std::string &str, int rule) {
+    std::string strResult;
     for (std::string::size_type i = 0; i < str.size(); i++) {
         if (SAFE_CHARS[rule].find(str[i]) != std::string::npos)
             strResult.push_back(str[i]);
@@ -56,16 +54,16 @@ signed char HexDigit(char c) {
     return p_util_hexdigit[(unsigned char)c];
 }
 
-bool IsHex(const string &str) {
+bool IsHex(const std::string &str) {
     for (std::string::const_iterator it(str.begin()); it != str.end(); ++it) {
         if (HexDigit(*it) < 0) return false;
     }
     return (str.size() > 0) && (str.size() % 2 == 0);
 }
 
-vector<unsigned char> ParseHex(const char *psz) {
+std::vector<unsigned char> ParseHex(const char *psz) {
     // convert hex dump to vector
-    vector<unsigned char> vch;
+    std::vector<unsigned char> vch;
     while (true) {
         while (isspace(*psz))
             psz++;
@@ -80,15 +78,15 @@ vector<unsigned char> ParseHex(const char *psz) {
     return vch;
 }
 
-vector<unsigned char> ParseHex(const string &str) {
+std::vector<unsigned char> ParseHex(const std::string &str) {
     return ParseHex(str.c_str());
 }
 
-string EncodeBase64(const unsigned char *pch, size_t len) {
+std::string EncodeBase64(const unsigned char *pch, size_t len) {
     static const char *pbase64 =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-    string strRet = "";
+    std::string strRet = "";
     strRet.reserve((len + 2) / 3 * 4);
 
     int mode = 0, left = 0;
@@ -126,11 +124,11 @@ string EncodeBase64(const unsigned char *pch, size_t len) {
     return strRet;
 }
 
-string EncodeBase64(const string &str) {
+std::string EncodeBase64(const std::string &str) {
     return EncodeBase64((const unsigned char *)str.c_str(), str.size());
 }
 
-vector<unsigned char> DecodeBase64(const char *p, bool *pfInvalid) {
+std::vector<unsigned char> DecodeBase64(const char *p, bool *pfInvalid) {
     static const int decode64_table[256] = {
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
@@ -150,7 +148,7 @@ vector<unsigned char> DecodeBase64(const char *p, bool *pfInvalid) {
 
     if (pfInvalid) *pfInvalid = false;
 
-    vector<unsigned char> vchRet;
+    std::vector<unsigned char> vchRet;
     vchRet.reserve(strlen(p) * 3 / 4);
 
     int mode = 0;
@@ -209,16 +207,17 @@ vector<unsigned char> DecodeBase64(const char *p, bool *pfInvalid) {
     return vchRet;
 }
 
-string DecodeBase64(const string &str) {
-    vector<unsigned char> vchRet = DecodeBase64(str.c_str());
-    return (vchRet.size() == 0) ? string() : string((const char *)&vchRet[0],
-                                                    vchRet.size());
+std::string DecodeBase64(const std::string &str) {
+    std::vector<unsigned char> vchRet = DecodeBase64(str.c_str());
+    return (vchRet.size() == 0)
+               ? std::string()
+               : std::string((const char *)&vchRet[0], vchRet.size());
 }
 
-string EncodeBase32(const unsigned char *pch, size_t len) {
+std::string EncodeBase32(const unsigned char *pch, size_t len) {
     static const char *pbase32 = "abcdefghijklmnopqrstuvwxyz234567";
 
-    string strRet = "";
+    std::string strRet = "";
     strRet.reserve((len + 4) / 5 * 8);
 
     int mode = 0, left = 0;
@@ -270,11 +269,11 @@ string EncodeBase32(const unsigned char *pch, size_t len) {
     return strRet;
 }
 
-string EncodeBase32(const string &str) {
+std::string EncodeBase32(const std::string &str) {
     return EncodeBase32((const unsigned char *)str.c_str(), str.size());
 }
 
-vector<unsigned char> DecodeBase32(const char *p, bool *pfInvalid) {
+std::vector<unsigned char> DecodeBase32(const char *p, bool *pfInvalid) {
     static const int decode32_table[256] = {
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
@@ -294,7 +293,7 @@ vector<unsigned char> DecodeBase32(const char *p, bool *pfInvalid) {
 
     if (pfInvalid) *pfInvalid = false;
 
-    vector<unsigned char> vchRet;
+    std::vector<unsigned char> vchRet;
     vchRet.reserve((strlen(p)) * 5 / 8);
 
     int mode = 0;
@@ -390,10 +389,11 @@ vector<unsigned char> DecodeBase32(const char *p, bool *pfInvalid) {
     return vchRet;
 }
 
-string DecodeBase32(const string &str) {
-    vector<unsigned char> vchRet = DecodeBase32(str.c_str());
-    return (vchRet.size() == 0) ? string() : string((const char *)&vchRet[0],
-                                                    vchRet.size());
+std::string DecodeBase32(const std::string &str) {
+    std::vector<unsigned char> vchRet = DecodeBase32(str.c_str());
+    return (vchRet.size() == 0)
+               ? std::string()
+               : std::string((const char *)&vchRet[0], vchRet.size());
 }
 
 static bool ParsePrechecks(const std::string &str) {
