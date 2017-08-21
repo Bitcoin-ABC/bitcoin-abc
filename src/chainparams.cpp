7038e96d6d466eb90d62959fd353400d2d016166
Aaron Voisine <voisine@gmail.com>
2017-08-21 15:57:18 -0700

    Update chainparams.cpp
    Change the mainnet p2pkh address version prefix to 28 (addresses start with "C"), and the p2sh version to 40 (addresses start with "H")
    
    resolves issue #35

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -186,8 +186,8 @@ public:
         vSeeds.push_back(
             CDNSSeedData("criptolayer.net", "seeder.criptolayer.net", true));
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 0);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 5);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 28);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 40);
         base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 128);
         base58Prefixes[EXT_PUBLIC_KEY] =
             boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E)
