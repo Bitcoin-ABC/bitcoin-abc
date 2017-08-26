42a837318e8ca4792ce383f576f30310d797abe2
Amaury SECHET <deadalnix@gmail.com>
2017-08-26 19:13:00 +0200

    Add SipHashUint256Extra as a specialization of siphash for a 256bits int and a 32 bit one.
    Summary:
    This is going to be used to hash outpoints. This is part of the work started with D342 .
    
    Depends on 490
    
    Test Plan:
      make check
    
    Reviewers: freetrader, CCulianu, #bitcoin_abc
    
    Reviewed By: CCulianu, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D491

--- src/hash.cpp
+++ src/hash.cpp
@@ -140,7 +140,7 @@ CSipHasher &CSipHasher::Write(const uint8_t *data, size_t size) {
     int c = count;
 
     while (size--) {
-        t |= ((uint64_t)(*(data++))) << (8 * (c % 8));
+        t |= uint64_t(*(data++)) << (8 * (c % 8));
         c++;
         if ((c & 7) == 0) {
             v3 ^= t;
@@ -164,7 +164,7 @@ CSipHasher &CSipHasher::Write(const uint8_t *data, size_t size) {
 uint64_t CSipHasher::Finalize() const {
     uint64_t v0 = v[0], v1 = v[1], v2 = v[2], v3 = v[3];
 
-    uint64_t t = tmp | (((uint64_t)count) << 56);
+    uint64_t t = tmp | (uint64_t(count) << 56);
 
     v3 ^= t;
     SIPROUND;
@@ -205,10 +205,51 @@ uint64_t SipHashUint256(uint64_t k0, uint64_t k1, const uint256 &val) {
     SIPROUND;
     SIPROUND;
     v0 ^= d;
-    v3 ^= ((uint64_t)4) << 59;
+    v3 ^= uint64_t(4) << 59;
     SIPROUND;
     SIPROUND;
-    v0 ^= ((uint64_t)4) << 59;
+    v0 ^= uint64_t(4) << 59;
+    v2 ^= 0xFF;
+    SIPROUND;
+    SIPROUND;
+    SIPROUND;
+    SIPROUND;
+    return v0 ^ v1 ^ v2 ^ v3;
+}
+
+uint64_t SipHashUint256Extra(uint64_t k0, uint64_t k1, const uint256 &val,
+                             uint32_t extra) {
+    /* Specialized implementation for efficiency */
+    uint64_t d = val.GetUint64(0);
+
+    uint64_t v0 = 0x736f6d6570736575ULL ^ k0;
+    uint64_t v1 = 0x646f72616e646f6dULL ^ k1;
+    uint64_t v2 = 0x6c7967656e657261ULL ^ k0;
+    uint64_t v3 = 0x7465646279746573ULL ^ k1 ^ d;
+
+    SIPROUND;
+    SIPROUND;
+    v0 ^= d;
+    d = val.GetUint64(1);
+    v3 ^= d;
+    SIPROUND;
+    SIPROUND;
+    v0 ^= d;
+    d = val.GetUint64(2);
+    v3 ^= d;
+    SIPROUND;
+    SIPROUND;
+    v0 ^= d;
+    d = val.GetUint64(3);
+    v3 ^= d;
+    SIPROUND;
+    SIPROUND;
+    v0 ^= d;
+    d = (uint64_t(36) << 56) | extra;
+    v3 ^= d;
+    SIPROUND;
+    SIPROUND;
+    v0 ^= d;
     v2 ^= 0xFF;
     SIPROUND;
     SIPROUND;
