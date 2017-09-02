b651d377ebbb9a516b01dacc6007aabe212b8cc7
MarcoFalke <falke.marco@gmail.com>
2017-09-02 11:38:43 +0200

    Merge #11198: [Qt] Fix display of package name on 'open config file' tooltip
    Summary:
    14ccd4d8d [Qt] Fix display of package name on 'open config file' tooltip (Daniel Edgecumbe)
    
    Pull request description:
    
      This patch addresses https://github.com/bitcoin/bitcoin/issues/11197.
    
    Tree-SHA512: b826fa2eb69f2c8054d2e84614e067bb00d2176701992dadfea01c2ad63d29015acdb76c5b874f5f6bda52b1f05e3ccfa1b67a4ee2f1e0c26496d1ff1718f325
    
    Backport of Core PR11198
    https://github.com/bitcoin/bitcoin/pull/11198/files
    
    Depends on D3219
    
    Test Plan:
    ```
    # on master
    bitcoin-qt
    # Settings -> Options
    # Hover over open configuration file button
    # See the message contains '%1'
    
    # with this patch applied
    make check
    bitcoin-qt
    # See the same message as above, but with '%1' properly replaced with 'Bitcoin ABC'
    
    Reviewers: #bitcoin_abc, deadalnix, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D3220

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -80,6 +80,9 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet)
     ui->bitcoinAtStartup->setText(
         ui->bitcoinAtStartup->text().arg(tr(PACKAGE_NAME)));
 
+    ui->openBitcoinConfButton->setToolTip(
+        ui->openBitcoinConfButton->toolTip().arg(tr(PACKAGE_NAME)));
+
     ui->lang->setToolTip(ui->lang->toolTip().arg(tr(PACKAGE_NAME)));
     ui->lang->addItem(QString("(") + tr("default") + QString(")"),
                       QVariant(""));
