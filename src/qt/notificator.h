427bef94450824dcb7ec3a75534896335370af3a
Jonas Schnelli <dev@jonasschnelli.ch>
2017-09-07 10:11:44 -0700

    [macOS] remove Growl support, remove unused code
    Summary:
      Backport Core PR 11268: [macOS] remove Growl support, remove unused code
      https://github.com/bitcoin/bitcoin/pull/11268
    
      f151f5f50 [macOS] remove Growl support, remove unused code (Jonas Schnelli)
    
      Pull request description:
    
        There is no longer a reason to support Growl.
        A) It went to pay-ware since a couple of years
        B) Since OSX 10.8, the operating system has its own modal notification options (Notification Center).
    
        This PR removes support for Growl.
        OSX notification centre is still supported after this PR.
    
    Test Plan:
      make check
    
    confirm Qt GUI runs fine on MacOSX
    
    Reviewers: schancel, #bitcoin_abc
    
    Reviewed By: schancel, #bitcoin_abc
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1243

--- src/qt/notificator.h
+++ src/qt/notificator.h
@@ -60,8 +60,6 @@ private:
                  dialog for Critical notifications. */
         Freedesktop, /**< Use DBus org.freedesktop.Notifications */
         QSystemTray, /**< Use QSystemTray::showMessage */
-        Growl12,     /**< Use the Growl 1.2 notification system (Mac only) */
-        Growl13,     /**< Use the Growl 1.3 notification system (Mac only) */
         UserNotificationCenter /**< Use the 10.8+ User Notification Center (Mac
                                   only) */
     };
@@ -77,8 +75,6 @@ private:
     void notifySystray(Class cls, const QString &title, const QString &text,
                        const QIcon &icon, int millisTimeout);
 #ifdef Q_OS_MAC
-    void notifyGrowl(Class cls, const QString &title, const QString &text,
-                     const QIcon &icon);
     void notifyMacUserNotificationCenter(Class cls, const QString &title,
                                          const QString &text,
                                          const QIcon &icon);
