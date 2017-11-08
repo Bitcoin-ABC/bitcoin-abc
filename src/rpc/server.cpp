41abbec5fd78a59b020a5130a6e0833ff024ba25
Jason B. Cox <contact@jasonbcox.com>
2017-11-07 19:53:45 -0800

    Get rid of namespace imports & backporting univalue
    Summary: Remove namespace imports as they pollute namespace and are prone to symbol clashes.  Backport univalue to latest version, which also includes namespace removals.
    
    Test Plan: These changes build.  No new tests should be necessary.
    
    Reviewers: deadalnix, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: deadalnix, O1 Bitcoin ABC, #bitcoin_abc
    
    Maniphest Tasks: T6
    
    Differential Revision: https://reviews.bitcoinabc.org/D647

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -26,8 +26,6 @@
 #include <set>
 #include <unordered_map>
 
-using namespace RPCServer;
-
 static bool fRPCRunning = false;
 static bool fRPCInWarmup = true;
 static std::string rpcWarmupStatus("RPC server started");
