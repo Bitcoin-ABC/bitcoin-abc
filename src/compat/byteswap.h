6b08ed7779e1636db237ba5c0af3f3d6881f2e57
Wladimir J. van der Laan <laanwj@gmail.com>
2017-11-17 15:33:56 +0100

    [backport#11140] Trivial: Improve #endif comments
    Summary:
    ac1cf8d Trivial: Improve #endif comments (danra)
    
    Pull request description:
    
      Improve the #endif comments for the '#if HAVE_DECL_BSWAP_XX == 0' preprocessor conditions, so each shows the full condition which it closes.
    
    ---
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/11140 | PR11140]]
    
    Test Plan:
      ninja
    
    Reviewers: #bitcoin_abc, PiRK, Fabien
    
    Reviewed By: #bitcoin_abc, PiRK, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D8172

--- src/compat/byteswap.h
+++ src/compat/byteswap.h
@@ -37,14 +37,14 @@
 inline uint16_t bswap_16(uint16_t x) {
     return (x >> 8) | (x << 8);
 }
-#endif // HAVE_DECL_BSWAP16
+#endif // HAVE_DECL_BSWAP16 == 0
 
 #if HAVE_DECL_BSWAP_32 == 0
 inline uint32_t bswap_32(uint32_t x) {
     return (((x & 0xff000000U) >> 24) | ((x & 0x00ff0000U) >> 8) |
             ((x & 0x0000ff00U) << 8) | ((x & 0x000000ffU) << 24));
 }
-#endif // HAVE_DECL_BSWAP32
+#endif // HAVE_DECL_BSWAP32 == 0
 
 #if HAVE_DECL_BSWAP_64 == 0
 inline uint64_t bswap_64(uint64_t x) {
@@ -57,7 +57,7 @@ inline uint64_t bswap_64(uint64_t x) {
             ((x & 0x000000000000ff00ull) << 40) |
             ((x & 0x00000000000000ffull) << 56));
 }
-#endif // HAVE_DECL_BSWAP64
+#endif // HAVE_DECL_BSWAP64 == 0
 
 #endif // defined(MAC_OSX)
 
