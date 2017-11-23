6aeb1f7492051e3e119ed4669d9b5ab86250b85c
John Newbery <john@johnnewbery.com>
2017-11-23 09:36:35 -0500

    [tests] Remove mininode periodic (half-hour) ping messages
    Summary:
    Partial backport of core PR11712 (commit f2ae6f3)
    
    Depends on D2356
    
    Test Plan:
      ./test/functional/test_runner.py
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2357

--- test/functional/test_framework/mininode.py
+++ test/functional/test_framework/mininode.py
@@ -20,7 +20,6 @@ import logging
 import socket
 import struct
 import sys
-import time
 from threading import RLock, Thread
 
 from test_framework.messages import *
@@ -235,7 +234,6 @@ class NodeConn(asyncore.dispatcher):
         self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         self.sendbuf = b""
         self.recvbuf = b""
-        self.last_sent = 0
         self.state = "connecting"
         self.network = net
         self.cb = callback
@@ -336,8 +334,6 @@ class NodeConn(asyncore.dispatcher):
             raise
 
     def got_message(self, message):
-        if self.last_sent + 30 * 60 < time.time():
-            self.send_message(MESSAGEMAP[b'ping']())
         self._log_message("receive", message)
         self.cb.deliver(self, message)
 
@@ -389,7 +385,6 @@ class NodeConn(asyncore.dispatcher):
                     self.sendbuf = tmsg
             else:
                 self.sendbuf += tmsg
-            self.last_sent = time.time()
 
     # Class utility methods
 
