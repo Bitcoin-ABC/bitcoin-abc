6ef18a84bd269afdf5928fd4d8c93d180052f600
Amaury Séchet <deadalnix@gmail.com>
2017-12-12 01:30:03 +0100

    Add support to build leveldb using cmake
    Summary: As per title
    
    Test Plan:
      cmake -GNinja ..
      ninja
    
    Ensure that lebeldb is built properly. Repeat for windows build.
    
    Reviewers: schancel
    
    Reviewed By: schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D798

---
+++ cmake/modules/FindSHLWAPI.cmake
@@ -0,0 +1,15 @@
+# Try to find the SHLWAPI librairy
+# SHLWAPI_FOUND - system has SHLWAPI lib
+# SHLWAPI_INCLUDE_DIR - the SHLWAPI include directory
+# SHLWAPI_LIBRARY - Libraries needed to use SHLWAPI
+
+if(SHLWAPI_INCLUDE_DIR AND SHLWAPI_LIBRARY)
+	# Already in cache, be silent
+	set(SHLWAPI_FIND_QUIETLY TRUE)
+endif()
+
+find_path(SHLWAPI_INCLUDE_DIR NAMES shlwapi.h)
+find_library(SHLWAPI_LIBRARY NAMES shlwapi)
+message(STATUS "SHLWAPI lib: " ${SHLWAPI_LIBRARY})
+
+mark_as_advanced(SHLWAPI_INCLUDE_DIR SHLWAPI_LIBRARY)
