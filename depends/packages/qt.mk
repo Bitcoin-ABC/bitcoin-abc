6303bac238582ce35f6732080a3de23e6c4569d0
Amaury Séchet <deadalnix@gmail.com>
2017-12-19 18:54:06 +0100

    Update depends on qt to build the whole qtbase and preserve cmake/mkspecs
    Summary: This ensure that we can use Qt's cmake integration when using Qt from depends.
    
    Test Plan:
      cd build
      make -C ../depends HOST=i686-w64-mingw32
    
    Checks that cmake files and mkdepends are installed properly.
    
    Reviewers: schancel, #bitcoin_abc
    
    Reviewed By: schancel, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D830

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -159,18 +159,18 @@ define $(package)_config_cmds
   $(MAKE) sub-src-clean && \
   cd ../qttranslations && ../qtbase/bin/qmake qttranslations.pro -o Makefile && \
   cd translations && ../../qtbase/bin/qmake translations.pro -o Makefile && cd ../.. &&\
-  cd qttools/src/linguist/lrelease/ && ../../../../qtbase/bin/qmake lrelease.pro -o Makefile
+  cd qttools/src/linguist/ && ../../../qtbase/bin/qmake linguist.pro -o Makefile
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C src $(addprefix sub-,$($(package)_qt_libs)) && \
-  $(MAKE) -C ../qttools/src/linguist/lrelease && \
+  $(MAKE) && \
+  $(MAKE) -C ../qttools/src/linguist && \
   $(MAKE) -C ../qttranslations
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) -C src INSTALL_ROOT=$($(package)_staging_dir) $(addsuffix -install_subtargets,$(addprefix sub-,$($(package)_qt_libs))) && cd .. &&\
-  $(MAKE) -C qttools/src/linguist/lrelease INSTALL_ROOT=$($(package)_staging_dir) install_target && \
+  $(MAKE) INSTALL_ROOT=$($(package)_staging_dir) install && cd .. && \
+  $(MAKE) -C qttools/src/linguist INSTALL_ROOT=$($(package)_staging_dir) install && \
   $(MAKE) -C qttranslations INSTALL_ROOT=$($(package)_staging_dir) install_subtargets && \
   if `test -f qtbase/src/plugins/platforms/xcb/xcb-static/libxcb-static.a`; then \
     cp qtbase/src/plugins/platforms/xcb/xcb-static/libxcb-static.a $($(package)_staging_prefix_dir)/lib; \
@@ -178,6 +178,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf native/mkspecs/ native/lib/ lib/cmake/ && \
-  rm -f lib/lib*.la lib/*.prl plugins/*/*.prl
+  rm -rf native/lib/ lib/lib*.la lib/*.prl plugins/*/*.prl
 endef
