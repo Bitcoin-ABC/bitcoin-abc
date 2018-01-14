72d69ae906829d73466080c0a1a80783ca74da37
Jimmy Song <jaejoon@gmail.com>
2018-01-14 18:35:52 +0100

    Tests: Add test for getdifficulty
    Summary:
    Test added to blockchain.py as adding a new test to reduce test run time.
    
    Backport core's PR10229
    
    Test Plan:
      ./test/functional/test_runner.py blockchain.py
    
    Reviewers: #bitcoin_abc, schancel
    
    Reviewed By: #bitcoin_abc, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D951

--- test/functional/blockchain.py
+++ test/functional/blockchain.py
@@ -3,10 +3,18 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#
-# Test RPC calls related to blockchain state. Tests correspond to code in
-# rpc/blockchain.cpp.
-#
+"""Test RPCs related to blockchainstate.
+
+Test the following RPCs:
+    - gettxoutsetinfo
+    - getdifficulty
+    - getbestblockhash
+    - getblockhash
+    - getblockheader
+    - verifychain
+
+Tests correspond to code in rpc/blockchain.cpp.
+"""
 
 from decimal import Decimal
 
@@ -22,15 +30,6 @@ from test_framework.util import (
 
 
 class BlockchainTest(BitcoinTestFramework):
-
-    """
-    Test blockchain-related RPC calls:
-
-        - gettxoutsetinfo
-        - verifychain
-
-    """
-
     def __init__(self):
         super().__init__()
         self.setup_clean_chain = False
@@ -39,6 +38,7 @@ class BlockchainTest(BitcoinTestFramework):
     def run_test(self):
         self._test_gettxoutsetinfo()
         self._test_getblockheader()
+        self._test_getdifficulty()
         self.nodes[0].verifychain(4, 0)
 
     def _test_gettxoutsetinfo(self):
@@ -109,6 +109,12 @@ class BlockchainTest(BitcoinTestFramework):
         assert isinstance(header['version'], int)
         assert isinstance(int(header['versionHex'], 16), int)
         assert isinstance(header['difficulty'], Decimal)
+
+    def _test_getdifficulty(self):
+        difficulty = self.nodes[0].getdifficulty()
+        # 1 hash in 2 should be valid, so difficulty should be 1/2**31
+        # binary => decimal => binary math is why we do this check
+        assert abs(difficulty * 2**31 - 1) < 0.0001
 
 
 if __name__ == '__main__':
