bd65632d84f2eea05c24bb86caca205da0ded2dc
Russell Yanofsky <russ@yanofsky.org>
2018-01-18 13:15:00 -0500

    Don't allow relative -walletdir paths
    Summary:
    Also warn if bitcoind is configured to use a relative -datadir path.
    
    Specifying paths relative to the current working directory in a daemon
    process can be dangerous, because files can fail to be located even if
    the configuration doesn't change, but the daemon is started up
    differently.
    
    Specifying a relative -datadir now adds a warning to the debug log. It
    would not be backwards-compatible to forbid relative -datadir paths
    entirely, and it could also be also inconvenient for command line
    testing.
    
    Specifying a relative -walletdir now results in a startup error. But
    since the -walletdir option is new in 0.18.7, there should be no
    compatibility issues.
    Another reason not to use working directory paths for -walletdir
    specifically is that the default -walletdir is a "wallets" subdirectory
    inside the datadir, so it could be surprising that setting -walletdir
    manually would choose a directory rooted in a completely different
    location.
    
    Backport of core PR12220
    
    Depends on D2400
    
    Test Plan:
      make check
      ./test/functional/test_runner.py wallet_multiwallet
    
      mkdir mydata
      ./src/bitcoind -datadir=mydata
    Check in the log for a warning:
      Warning: relative datadir option 'mydata' specified, which will be
    interpreted relative to the current working directory
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2407

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -247,18 +247,18 @@ class BitcoinTestFramework():
             self.nodes.append(TestNode(i, self.options.tmpdir, extra_args[i], rpchost, rpc_port=rpc_port(i), p2p_port=p2p_port(i),
                                        timewait=timewait, binary=binary[i], stderr=None, mocktime=self.mocktime, coverage_dir=self.options.coveragedir, use_cli=self.options.usecli))
 
-    def start_node(self, i, extra_args=None, stderr=None):
+    def start_node(self, i, *args, **kwargs):
         """Start a bitcoind"""
 
         node = self.nodes[i]
 
-        node.start(extra_args, stderr)
+        node.start(*args, **kwargs)
         node.wait_for_rpc_connection()
 
         if self.options.coveragedir is not None:
             coverage.write_all_rpc_commands(self.options.coveragedir, node.rpc)
 
-    def start_nodes(self, extra_args=None):
+    def start_nodes(self, extra_args=None, *args, **kwargs):
         """Start multiple bitcoinds"""
 
         if extra_args is None:
@@ -266,7 +266,7 @@ class BitcoinTestFramework():
         assert_equal(len(extra_args), self.num_nodes)
         try:
             for i, node in enumerate(self.nodes):
-                node.start(extra_args[i])
+                node.start(extra_args[i], *args, **kwargs)
             for node in self.nodes:
                 node.wait_for_rpc_connection()
         except:
@@ -299,10 +299,11 @@ class BitcoinTestFramework():
         self.stop_node(i)
         self.start_node(i, extra_args)
 
-    def assert_start_raises_init_error(self, i, extra_args=None, expected_msg=None):
+    def assert_start_raises_init_error(self, i, extra_args=None, expected_msg=None, *args, **kwargs):
         with tempfile.SpooledTemporaryFile(max_size=2**16) as log_stderr:
             try:
-                self.start_node(i, extra_args, stderr=log_stderr)
+                self.start_node(
+                    i, extra_args, stderr=log_stderr, *args, **kwargs)
                 self.stop_node(i)
             except Exception as e:
                 assert 'bitcoind exited' in str(e)  # node must have shutdown
