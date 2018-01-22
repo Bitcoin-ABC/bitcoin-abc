8f53e13ce9001293484d55d44af96be66a8d6490
Matias Alejo Garcia <ematiu@gmail.com>
2018-01-21 22:25:43 -0300

    add MacOs pidof compatibility
    Summary:
    Adds MacOs `pidof` compatibility  for bitcoind detection.
    
    Otherwise, the warning is always shown:
    `WARNING! There is already a bitcoind process running on this system. Tests may fail unexpectedly due to resource contention!`
    
    if `pidof: stable 0.1.4 (bottled)` from `brew` is installed.
    
    Depends on D999
    
    Test Plan: ./test/functional/test_runner.py
    
    Reviewers: deadalnix, schancel, #bitcoin_abc
    
    Reviewed By: deadalnix, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D1008

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -275,7 +275,8 @@ def main():
 def run_tests(test_list, src_dir, build_dir, exeext, tmpdir, jobs=1, enable_coverage=False, args=[]):
     # Warn if bitcoind is already running (unix only)
     try:
-        if subprocess.check_output(["pidof", "bitcoind"]) is not None:
+        pidofOutput = subprocess.check_output(["pidof", "bitcoind"])
+        if pidofOutput is not None and pidofOutput != b'':
             print("%sWARNING!%s There is already a bitcoind process running on this system. Tests may fail unexpectedly due to resource contention!" % (
                 BOLD[1], BOLD[0]))
     except (OSError, subprocess.SubprocessError):
