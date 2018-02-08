3979bf03baa42f9874e47fcd129815c95dfae378
Wladimir J. van der Laan <laanwj@gmail.com>
2018-02-08 19:44:52 +0100

    Merge #12333: Make CWallet::ListCoins atomic
    Summary:
    1beea7a [wallet] Make CWallet::ListCoins atomic (João Barbosa)
    
    Pull request description:
    
      Fix a potencial race in `CWallet::ListCoins`.
    
      Replaces `cs_main` and `cs_wallet` locks by assertions in `CWallet::AvailableCoins`.
    
    Tree-SHA512: 09109f44a08b4b53f7605d950ab506d3f748490ab9aed474aa200e93f7b0b9f96f9bf60abe1c5f658240fd13d9e3267c0dd43fd3c1695d82384198ce1da8109f
    
    Backport of Core PR12333
    https://github.com/bitcoin/bitcoin/pull/12333/
    
    Note: 2f960b5 [wallet] Indent only change of CWallet::AvailableCoins (João Barbosa) was skipped because our linter made the changes unnecessary.  I also verified that cherry-picking this commit resulted in no change to the code.
    
    Test Plan:
      ../configure --enable-debug
      make check
      make check should not throw any locking errors.
    
    Reviewers: jasonbcox, deadalnix, Fabien, markblundeberg, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: jasonbcox, Fabien, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D3049

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2331,10 +2331,12 @@ void CWallet::AvailableCoins(std::vector<COutput> &vCoins, bool fOnlySafe,
                              const Amount nMinimumSumAmount,
                              const uint64_t nMaximumCount, const int nMinDepth,
                              const int nMaxDepth) const {
+    AssertLockHeld(cs_main);
+    AssertLockHeld(cs_wallet);
+
     vCoins.clear();
     Amount nTotal = Amount::zero();
 
-    LOCK2(cs_main, cs_wallet);
     for (const auto &entry : mapWallet) {
         const TxId &wtxid = entry.first;
         const CWalletTx *pcoin = &entry.second;
@@ -2454,11 +2456,11 @@ std::map<CTxDestination, std::vector<COutput>> CWallet::ListCoins() const {
     // avoid adding some extra complexity to the Qt code.
 
     std::map<CTxDestination, std::vector<COutput>> result;
-
     std::vector<COutput> availableCoins;
-    AvailableCoins(availableCoins);
 
     LOCK2(cs_main, cs_wallet);
+    AvailableCoins(availableCoins);
+
     for (auto &coin : availableCoins) {
         CTxDestination address;
         if (coin.fSpendable &&
