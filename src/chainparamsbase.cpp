784be9ae126bd384131a4cd79ef496c0da7b0c01
Wladimir J. van der Laan <laanwj@gmail.com>
2018-02-10 09:56:10 +0100

    Merge #12128: Refactor: One CBaseChainParams should be enough
    Summary:
    1687cb4 Refactor: One CBaseChainParams should be enough (Jorge Timón)
    
    Pull request description:
    
      There's no need for class hierarchy with CBaseChainParams, it is just a struct with 2 fields.
      This starts as a +10-43 diff
    
    Tree-SHA512: 0a7dd64ab785416550b541787c6083540e4962d76b6cffa806bb3593aec2daf1752dfe65ac5cd51b34ad5c31dd8292c422b483fdd2d37d0b7e68725498ed4c2d
    
    Backport of Core PR12128
    https://github.com/bitcoin/bitcoin/pull/12128/
    
    Test Plan:
      make check
    
    Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D3804

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -24,36 +24,6 @@ void SetupChainParamsBaseOptions() {
                  OptionsCategory::CHAINPARAMS);
 }
 
-/**
- * Main network
- */
-class CBaseMainParams : public CBaseChainParams {
-public:
-    CBaseMainParams() { nRPCPort = 8332; }
-};
-
-/**
- * Testnet (v3)
- */
-class CBaseTestNetParams : public CBaseChainParams {
-public:
-    CBaseTestNetParams() {
-        nRPCPort = 18332;
-        strDataDir = "testnet3";
-    }
-};
-
-/*
- * Regression test
- */
-class CBaseRegTestParams : public CBaseChainParams {
-public:
-    CBaseRegTestParams() {
-        nRPCPort = 18443;
-        strDataDir = "regtest";
-    }
-};
-
 static std::unique_ptr<CBaseChainParams> globalChainBaseParams;
 
 const CBaseChainParams &BaseParams() {
@@ -64,11 +34,11 @@ const CBaseChainParams &BaseParams() {
 std::unique_ptr<CBaseChainParams>
 CreateBaseChainParams(const std::string &chain) {
     if (chain == CBaseChainParams::MAIN)
-        return std::unique_ptr<CBaseChainParams>(new CBaseMainParams());
+        return std::make_unique<CBaseChainParams>("", 8332);
     else if (chain == CBaseChainParams::TESTNET)
-        return std::unique_ptr<CBaseChainParams>(new CBaseTestNetParams());
+        return std::make_unique<CBaseChainParams>("testnet3", 18332);
     else if (chain == CBaseChainParams::REGTEST)
-        return std::unique_ptr<CBaseChainParams>(new CBaseRegTestParams());
+        return std::make_unique<CBaseChainParams>("regtest", 18443);
     else
         throw std::runtime_error(
             strprintf("%s: Unknown chain %s.", __func__, chain));
