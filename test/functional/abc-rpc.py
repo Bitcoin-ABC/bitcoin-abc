3729302239e6378e867c43de37c13e32a5defcdf
Amaury Séchet <deadalnix@gmail.com>
2018-03-03 16:56:06 +0100

    Stop testing for cash service bit, it is not necessary anymore
    Summary: As per title.
    
    Test Plan:
      ./test/functional/test_runner.py abc-rpc
    
    Reviewers: #bitcoin_abc, schancel
    
    Reviewed By: #bitcoin_abc, schancel
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1153

--- test/functional/abc-rpc.py
+++ test/functional/abc-rpc.py
@@ -10,7 +10,6 @@ import random
 import re
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import *
-from test_framework.mininode import NODE_BITCOIN_CASH
 from test_framework.cdefs import (ONE_MEGABYTE,
                                   LEGACY_MAX_BLOCK_SIZE,
                                   DEFAULT_MAX_BLOCK_SIZE)
@@ -81,18 +80,9 @@ class ABC_RPC_Test (BitcoinTestFramework):
         # check for EB correctness in the subver string
         self.check_subversion("/Bitcoin ABC:.*\(EB13\.1; .*\)/")
 
-    def test_cashservicebit(self):
-        # Check that NODE_BITCOIN_CASH bit is set.
-        # This can be seen in the 'localservices' entry of getnetworkinfo RPC.
-        node = self.nodes[0]
-        nw_info = node.getnetworkinfo()
-        assert_equal(int(nw_info['localservices'], 16) & NODE_BITCOIN_CASH,
-                     NODE_BITCOIN_CASH)
-
     def run_test(self):
         self.genesis_hash = int(self.nodes[0].getbestblockhash(), 16)
         self.test_excessiveblock()
-        self.test_cashservicebit()
 
 
 if __name__ == '__main__':
