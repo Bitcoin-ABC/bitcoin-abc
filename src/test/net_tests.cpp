2ebe90a5f4700727b7602f82a6eac27752d5f3a3
Shammah Chancellor <shammah.chancellor@gmail.com>
2018-03-16 10:37:42 -0700

    Update release files for 0.17.0
    Summary:
    Following doc/release-process.md, update the files as per
    instructions for release 0.17.0
    
    Test Plan: Review files, build, and verify versioning
    
    Reviewers: jasonbcox, Mengerian, matiu, movrcx, #bitcoin_abc, deadalnix
    
    Reviewed By: jasonbcox, #bitcoin_abc, deadalnix
    
    Subscribers: deadalnix, teamcity
    
    Maniphest Tasks: T286
    
    Differential Revision: https://reviews.bitcoinabc.org/D1206

--- src/test/net_tests.cpp
+++ src/test/net_tests.cpp
@@ -195,7 +195,7 @@ BOOST_AUTO_TEST_CASE(test_userAgentLength) {
 
     BOOST_CHECK_EQUAL(userAgent(config).size(), MAX_SUBVERSION_LENGTH);
     BOOST_CHECK_EQUAL(userAgent(config),
-                      "/Bitcoin ABC:0.16.3(EB8.0; very very very very very "
+                      "/Bitcoin ABC:0.17.0(EB8.0; very very very very very "
                       "very very very very very very very very very very very "
                       "very very very very very very very very very very very "
                       "very very very very very very very very very very very "
