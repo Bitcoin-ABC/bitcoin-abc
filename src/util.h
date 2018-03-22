297d67419ba86e081ff0739a920de77a75859eb9
Wladimir J. van der Laan <laanwj@gmail.com>
2018-03-22 14:57:46 +0100

    Merge #12630: Provide useful error message if datadir is not writable.
    Summary:
    8674e74 Provide relevant error message if datadir is not writable. (murrayn)
    
    Pull request description:
    
      If the --datadir exists, but is not writable, the current error message on startup is 'Cannot obtain a lock on data directory foo. Bitcoin Core is probably already running.' This is misleading.
    
      I believe this PR addresses #11668, although the issue is not Windows-specific.
    
    Tree-SHA512: 10cbbaea433072aee4fb3e8938a72073c7a5c841f7a7685c9e12549c322b2925c7d34bac254ac33021b23132bfc352c058712bc9542298cf86f8fd9757f528b2
    
    Backport Core PR 12630
    https://github.com/bitcoin/bitcoin/pull/12630/files
    Completes T605
    Depends on D2809
    
    Test Plan: make check
    
    Reviewers: deadalnix, Fabien, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D2810

--- src/util.h
+++ src/util.h
@@ -74,6 +74,7 @@ void AllocateFileRange(FILE *file, unsigned int offset, unsigned int length);
 bool RenameOver(fs::path src, fs::path dest);
 bool LockDirectory(const fs::path &directory, const std::string lockfile_name,
                    bool probe_only = false);
+bool DirIsWritable(const fs::path &directory);
 
 /**
  * Release all directory locks. This is used for unit testing only, at runtime
