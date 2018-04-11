06ac73365f35de43f9119211b281a4fffc728f38
MarcoFalke <falke.marco@gmail.com>
2018-04-11 14:04:59 -0400

    bitcoin-tx: Remove unused for loop
    Summary:
    This is a backport of Core PR12950
    
    Depends on D3337
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D3338

--- src/bitcoin-tx.cpp
+++ src/bitcoin-tx.cpp
@@ -553,12 +553,10 @@ static void MutateTxSign(CMutableTransaction &tx, const std::string &flagStr) {
         throw std::runtime_error("unknown sighash flag/sign option");
     }
 
-    std::vector<CTransaction> txVariants;
-    txVariants.push_back(CTransaction(tx));
-
-    // mergedTx will end up with all the signatures; it starts as a clone of the
-    // raw tx:
-    CMutableTransaction mergedTx(txVariants[0]);
+    // mergedTx will end up with all the signatures; it
+    // starts as a clone of the raw tx:
+    CMutableTransaction mergedTx{tx};
+    const CTransaction txv{tx};
     CCoinsView viewDummy;
     CCoinsViewCache view(&viewDummy);
 
@@ -652,7 +650,7 @@ static void MutateTxSign(CMutableTransaction &tx, const std::string &flagStr) {
 
     // Sign what we can:
     for (size_t i = 0; i < mergedTx.vin.size(); i++) {
-        CTxIn &txin = mergedTx.vin[i];
+        const CTxIn &txin = mergedTx.vin[i];
         const Coin &coin = view.AccessCoin(txin.prevout);
         if (coin.IsSpent()) {
             continue;
@@ -671,13 +669,10 @@ static void MutateTxSign(CMutableTransaction &tx, const std::string &flagStr) {
         }
 
         // ... and merge in other signatures:
-        for (const CTransaction &txv : txVariants) {
-            sigdata = CombineSignatures(
-                prevPubKey,
-                MutableTransactionSignatureChecker(&mergedTx, i, amount),
-                sigdata, DataFromTransaction(txv, i));
-        }
-
+        sigdata = CombineSignatures(
+            prevPubKey,
+            MutableTransactionSignatureChecker(&mergedTx, i, amount), sigdata,
+            DataFromTransaction(txv, i));
         UpdateTransaction(mergedTx, i, sigdata);
     }
 
