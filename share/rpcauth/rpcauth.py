e760b402e97939712c333bfdca05b42e249a5651
Qasim Javed <qasimj@gmail.com>
2018-04-23 00:39:12 -0700

    [tests] Make rpcauth.py testable and add unit tests
    Summary:
    Backport of core PR13056 and PR13073
    https://github.com/bitcoin/bitcoin/pull/13056/files
    https://github.com/bitcoin/bitcoin/pull/13073/files
    
    Depends on D3758.
    
    Test Plan:
      make check
      ninja check
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D3759

--- share/rpcauth/rpcauth.py
+++ share/rpcauth/rpcauth.py
@@ -3,32 +3,47 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-import sys
-import os
-from random import SystemRandom
 import base64
 import hmac
+import os
+from random import SystemRandom
+import sys
 
-if len(sys.argv) < 2:
-    sys.stderr.write('Please include username as an argument.\n')
-    sys.exit(0)
 
-username = sys.argv[1]
+def generate_salt():
+    # This uses os.urandom() underneath
+    cryptogen = SystemRandom()
 
-# This uses os.urandom() underneath
-cryptogen = SystemRandom()
+    # Create 16 byte hex salt
+    salt_sequence = [cryptogen.randrange(256) for _ in range(16)]
+    return ''.join([format(r, 'x') for r in salt_sequence])
 
-# Create 16 byte hex salt
-salt_sequence = [cryptogen.randrange(256) for i in range(16)]
-hexseq = list(map(hex, salt_sequence))
-salt = "".join([x[2:] for x in hexseq])
 
-# Create 32 byte b64 password
-password = base64.urlsafe_b64encode(os.urandom(32)).decode("utf-8")
+def generate_password(salt):
+    """Create 32 byte b64 password"""
+    password = base64.urlsafe_b64encode(os.urandom(32)).decode('utf-8')
 
-m = hmac.new(bytearray(salt, 'utf-8'), bytearray(password, 'utf-8'), "SHA256")
-result = m.hexdigest()
+    m = hmac.new(bytearray(salt, 'utf-8'),
+                 bytearray(password, 'utf-8'), 'SHA256')
+    password_hmac = m.hexdigest()
 
-print("String to be appended to bitcoin.conf:")
-print("rpcauth=" + username + ":" + salt + "$" + result)
-print("Your password:\n" + password)
+    return password, password_hmac
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.stderr.write('Please include username as an argument.\n')
+        sys.exit(0)
+
+    username = sys.argv[1]
+
+    salt = generate_salt()
+    password, password_hmac = generate_password(salt)
+
+    print('String to be appended to bitcoin.conf:')
+    print('rpcauth={0}:{1}${2}'.format(username, salt, password_hmac))
+    print('Your password:\n{0}'.format(password))
+
+
+if __name__ == '__main__':
+    main()
