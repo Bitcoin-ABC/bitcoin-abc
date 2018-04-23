6475813563ebfb34dcd9057a6fb69b16123dcd74
Wladimir J. van der Laan <laanwj@gmail.com>
2018-04-23 20:58:33 +0200

    Merge #12999: qt: Show the Window when double clicking the taskbar icon
    Summary:
    67bf2aa qt:Show the entire Window when double clicking on taskbar (Chun Kuan Lee)
    
    Pull request description:
    
      fix #12838
    
    Tree-SHA512: 4498bc1fe52efeed3768d6bdd7941e4b036d52ae80d9c1b6679c6daa3a62aeda4cff0c91fe1c65afaea6049c59cb0296fdcbff63ecb342d518615a92361fda7f
    
    Backport Core PR12999
    https://github.com/bitcoin/bitcoin/pull/12999/files
    
    Test Plan:
    ```
    make check
    # No good way to test this since I don't have a Windows dev machine.
    ```
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D3203

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -999,6 +999,10 @@ void BitcoinGUI::changeEvent(QEvent *e) {
             if (!(wsevt->oldState() & Qt::WindowMinimized) && isMinimized()) {
                 QTimer::singleShot(0, this, SLOT(hide()));
                 e->ignore();
+            } else if ((wsevt->oldState() & Qt::WindowMinimized) &&
+                       !isMinimized()) {
+                QTimer::singleShot(0, this, SLOT(show()));
+                e->ignore();
             }
         }
     }
