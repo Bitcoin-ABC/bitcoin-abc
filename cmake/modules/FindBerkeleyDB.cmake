2e059ee431516826b4ca036efe6e859c277b69f0
Amaury Séchet <deadalnix@gmail.com>
2018-05-01 17:17:48 +0200

    [cmake] Improve the way BerkeleyDB++ is looked for and used.
    Summary: As per title.
    
    Test Plan:
      ninja
    
    Reviewers: #bitcoin_abc, schancel
    
    Reviewed By: #bitcoin_abc, schancel
    
    Subscribers: teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D1333

--- cmake/modules/FindBerkeleyDB.cmake
+++ cmake/modules/FindBerkeleyDB.cmake
@@ -1,23 +1,22 @@
-# Try to find the GMP librairies
+# Try to find the BerkeleyDB librairies
 # BDB_FOUND - system has Berkeley DB lib
 # BDB_INCLUDE_DIR - the Berkeley DB include directory
 # BDB_LIBRARY - Library needed to use Berkeley DB
+# BDBXX_INCLUDE_DIR - the Berkeley DB include directory for C++
 # BDBXX_LIBRARY - Library needed to use Berkeley DB C++ API
-
-if(BDB_INCLUDE_DIR AND BDB_LIBRARY)
-	# Already in cache, be silent
-	set(BDB_FIND_QUIETLY TRUE)
-endif()
 
 find_path(BDB_INCLUDE_DIR NAMES db.h)
 find_library(BDB_LIBRARY NAMES db libdb)
+
+find_path(BDBXX_INCLUDE_DIR NAMES db_cxx.h)
 find_library(BDBXX_LIBRARY NAMES db_cxx libdb_cxx)
-MESSAGE(STATUS "Berkeley DB libs: " ${BDB_LIBRARY} " " ${BDBXX_LIBRARY})
+
+MESSAGE(STATUS "BerkeleyDB libs: " ${BDB_LIBRARY} " " ${BDBXX_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS("Berkeley DB" DEFAULT_MSG BDB_INCLUDE_DIR BDB_LIBRARY BDBXX_LIBRARY)
+find_package_handle_standard_args(BerkeleyDB DEFAULT_MSG BDB_INCLUDE_DIR BDB_LIBRARY BDBXX_INCLUDE_DIR BDBXX_LIBRARY)
 
-mark_as_advanced(BDB_INCLUDE_DIR BDB_LIBRARY)
-if(BDB_INCLUDE_DIR AND BDB_LIBRARY)
-	set(BDB_FOUND TRUE)
-endif()
+mark_as_advanced(BDB_INCLUDE_DIR BDB_LIBRARY BDBXX_INCLUDE_DIR BDBXX_LIBRARY)
+
+set(BerkeleyDB_LIBRARIES ${BDB_LIBRARY} ${BDBXX_LIBRARY})
+set(BerkeleyDB_INCLUDE_DIRS ${BDB_INCLUDE_DIR} ${BDBXX_INCLUDE_DIR})
