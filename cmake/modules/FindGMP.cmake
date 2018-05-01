b4404c90ab624f4b21db04510d9858c8de4364bb
Amaury Séchet <deadalnix@gmail.com>
2018-05-01 18:01:07 +0200

    [cmake] Find GMP properly
    Summary: This fix standard parameters handling
    
    Test Plan:
      cmake -GNinja ..
    
    Reviewers: #bitcoin_abc, schancel
    
    Reviewed By: #bitcoin_abc, schancel
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1339

--- cmake/modules/FindGMP.cmake
+++ cmake/modules/FindGMP.cmake
@@ -1,23 +1,24 @@
 # Try to find the GMP librairies
 # GMP_FOUND - system has GMP lib
 # GMP_INCLUDE_DIR - the GMP include directory
-# GMP_LIBRARIES - Libraries needed to use GMP
-# GMPXX_LIBRARIES - Libraries needed to use GMP C++ API
+# GMP_LIBRARY - Library needed to use GMP
+# GMPXX_LIBRARY - Library needed to use GMP C++ API
 
-if(GMP_INCLUDE_DIR AND GMP_LIBRARIES)
+if(GMP_INCLUDE_DIR AND GMP_LIBRARY)
 	# Already in cache, be silent
 	set(GMP_FIND_QUIETLY TRUE)
 endif()
 
 find_path(GMP_INCLUDE_DIR NAMES gmp.h)
-find_library(GMP_LIBRARIES NAMES gmp libgmp)
-find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx)
-MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES})
+find_library(GMP_LIBRARY NAMES gmp libgmp)
+find_library(GMPXX_LIBRARY NAMES gmpxx libgmpxx)
+
+message(STATUS "GMP libs: " ${GMP_LIBRARY} " " ${GMPXX_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARIES)
+find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARY)
 
-mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARIES)
-if(GMP_INCLUDE_DIR AND GMP_LIBRARIES)
-	set(GMP_FOUND TRUE)
-endif()
+mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARY GMPXX_LIBRARY)
+
+set(GMP_LIBRARIES ${GMP_LIBRARY} ${GMPXX_LIBRARY})
+set(GMP_INCLUDE_DIRS ${GMP_INCLUDE_DIR})
