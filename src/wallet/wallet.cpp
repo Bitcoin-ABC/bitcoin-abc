d559f01c776950a4be6d1ca4700fb1d7fdb8739c
Jonas Schnelli <dev@jonasschnelli.ch>
2018-05-07 15:03:28 +0200

    Merge #13079: Fix rescanblockchain rpc to properly report progress
    Summary:
    16be13345 Fix rescanblockchain rpc to property report progress (Ben Woosley)
    
    Pull request description:
    
      Previously it assumed tip in all cases. This also extracts a RescanVerificationProgress helper object whose role is to manage reporting, in order to simplify ScanForWalletTransactions - more lines in total, but much simpler to follow the core logic.
    
    Tree-SHA512: 5ebed0c56fae4ccfe613ff1d7082cb6da5a86635a8993ed3af70b500a4ea43074121aea9219b2f0321fbfeb7efcb964bdc2199297a64ca0fa85d9d07aa637d40
    
    Backport of Core PR13079
    https://github.com/bitcoin/bitcoin/pull/13079/
    
    Test Plan:
      make check
      ./bitcoind --printtoconsole
      ./bitcoin-cli rescanblockchain 0 1300000
    
    Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: deadalnix, Fabien, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D4112

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1816,30 +1816,37 @@ CBlockIndex *CWallet::ScanForWalletTransactions(
         // on startup.
         ShowProgress(_("Rescanning..."), 0);
         CBlockIndex *tip = nullptr;
-        double dProgressStart;
-        double dProgressTip;
+        double progress_begin;
+        double progress_end;
         {
             LOCK(cs_main);
-            tip = chainActive.Tip();
-            dProgressStart =
+            progress_begin =
                 GuessVerificationProgress(chainParams.TxData(), pindex);
-            dProgressTip = GuessVerificationProgress(chainParams.TxData(), tip);
+            if (pindexStop == nullptr) {
+                tip = chainActive.Tip();
+                progress_end =
+                    GuessVerificationProgress(chainParams.TxData(), tip);
+            } else {
+                progress_end =
+                    GuessVerificationProgress(chainParams.TxData(), pindexStop);
+            }
         }
-        double gvp = dProgressStart;
+        double progress_current = progress_begin;
         while (pindex && !fAbortRescan && !ShutdownRequested()) {
             if (pindex->nHeight % 100 == 0 &&
-                dProgressTip - dProgressStart > 0.0) {
+                progress_end - progress_begin > 0.0) {
                 ShowProgress(
                     _("Rescanning..."),
                     std::max(
-                        1, std::min(99, (int)((gvp - dProgressStart) /
-                                              (dProgressTip - dProgressStart) *
-                                              100))));
+                        1,
+                        std::min(99, (int)((progress_current - progress_begin) /
+                                           (progress_end - progress_begin) *
+                                           100))));
             }
             if (GetTime() >= nNow + 60) {
                 nNow = GetTime();
                 LogPrintf("Still rescanning. At block %d. Progress=%f\n",
-                          pindex->nHeight, gvp);
+                          pindex->nHeight, progress_current);
             }
 
             CBlock block;
@@ -1866,11 +1873,12 @@ CBlockIndex *CWallet::ScanForWalletTransactions(
             {
                 LOCK(cs_main);
                 pindex = chainActive.Next(pindex);
-                gvp = GuessVerificationProgress(chainParams.TxData(), pindex);
-                if (tip != chainActive.Tip()) {
+                progress_current =
+                    GuessVerificationProgress(chainParams.TxData(), pindex);
+                if (pindexStop == nullptr && tip != chainActive.Tip()) {
                     tip = chainActive.Tip();
                     // in case the tip has changed, update progress max
-                    dProgressTip =
+                    progress_end =
                         GuessVerificationProgress(chainParams.TxData(), tip);
                 }
             }
@@ -1878,11 +1886,11 @@ CBlockIndex *CWallet::ScanForWalletTransactions(
 
         if (pindex && fAbortRescan) {
             LogPrintf("Rescan aborted at block %d. Progress=%f\n",
-                      pindex->nHeight, gvp);
+                      pindex->nHeight, progress_current);
         } else if (pindex && ShutdownRequested()) {
             LogPrintf("Rescan interrupted by shutdown request at block %d. "
                       "Progress=%f\n",
-                      pindex->nHeight, gvp);
+                      pindex->nHeight, progress_current);
         }
 
         // Hide progress dialog in GUI.
