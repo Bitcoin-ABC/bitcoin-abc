9f681de70a78538960b9e86af05ac8361414401e
Amaury Séchet <deadalnix@gmail.com>
2018-05-09 02:36:27 +0200

    Update core_write.cpp to use COutPoint's accessors
    Summary:
    As per title.
    
    Depends on D1388
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1390

--- src/core_write.cpp
+++ src/core_write.cpp
@@ -196,8 +196,8 @@ void TxToUniv(const CTransaction &tx, const uint256 &hashBlock,
             in.pushKV("coinbase",
                       HexStr(txin.scriptSig.begin(), txin.scriptSig.end()));
         } else {
-            in.pushKV("txid", txin.prevout.hash.GetHex());
-            in.pushKV("vout", (int64_t)txin.prevout.n);
+            in.pushKV("txid", txin.prevout.GetTxId().GetHex());
+            in.pushKV("vout", int64_t(txin.prevout.GetN()));
             UniValue o(UniValue::VOBJ);
             o.pushKV("asm", ScriptToAsmStr(txin.scriptSig, true));
             o.pushKV("hex",
