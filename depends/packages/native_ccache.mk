0654a539663738f79b8478ad997b8afb5ef11948
Jason B. Cox <contact@jasonbcox.com>
2018-05-14 19:32:26 -0700

    Bumped ccache to 3.3.6 for minor bug fixes
    Summary: Bug fixes in ccache, especially file descriptor leaks from 3.3.5 release.
    
    Test Plan:
    gitian build:
    ```
    export URL=/vagrant/
    COMMIT=ccc5f3e
    ./bin/gbuild --commit bitcoin=${COMMIT} --url bitcoin=${URL} /vagrant/contrib/gitian-descriptors/gitian-linux.yml
    ```
    
    Reviewers: deadalnix, schancel, #bitcoin_abc
    
    Reviewed By: schancel, #bitcoin_abc
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1418

--- depends/packages/native_ccache.mk
+++ depends/packages/native_ccache.mk
@@ -1,8 +1,8 @@
 package=native_ccache
-$(package)_version=3.3.3
+$(package)_version=3.3.6
 $(package)_download_path=https://samba.org/ftp/ccache
 $(package)_file_name=ccache-$($(package)_version).tar.bz2
-$(package)_sha256_hash=2985bc5e32ebe38d2958d508eb54ddcad39eed909489c0c2988035214597ca54
+$(package)_sha256_hash=eac8d2a5055014bebae1434d9b7c66c25d64323800808c27a4534cee167e6bea
 
 define $(package)_set_vars
 $(package)_config_opts=
