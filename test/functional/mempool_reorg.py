4c623ad0b671cacc39558131215b0fc3e59f7836
Shammah Chancellor <shammah.chancellor@bitcoinabc.org>
2018-05-16 19:37:21 -0700

    [qa] add monolithactivationtime to mempool reorg to ensure it works
    Summary:
    The existing test framework mines 200 blocks starting Jan 1 2014.
    Then this test mines some additiona blocks in the current time,
    then it invalidates one. This causes monolith activation to trip
    and clear the mempool during the test, when one of the current-time
    blocks is invalidated.
    
    Test Plan:
      ./test/functional/test_runner.py mempool_reorg.py
    
    Reviewers: #bitcoin_abc, jasonbcox, deadalnix
    
    Reviewed By: #bitcoin_abc, jasonbcox, deadalnix
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1423

--- test/functional/mempool_reorg.py
+++ test/functional/mempool_reorg.py
@@ -17,7 +17,7 @@ from test_framework.util import *
 class MempoolCoinbaseTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
-        self.extra_args = [["-checkmempool"]] * 2
+        self.extra_args = [["-checkmempool", "-monolithactivationtime=0"]] * 2
 
     alert_filename = None  # Set by setup_network
 
