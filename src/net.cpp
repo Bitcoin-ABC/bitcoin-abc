3951fbe93e0c9630e16905ac18279dc75c55d25e
Shammah Chancellor <shammah.chancellor@bitcoinabc.org>
2018-05-30 11:02:45 -0700

    Add BCLog::NET category to disconnect ban message
    Summary: Continuous reconnecting currently causes banned nodes to spam logs and fill them up with excessive amounts of data.  This commit makes such messages goto the NET logging category, which is non-default.  This should remove the DoS vector unless individuals are explicitly interested in such messages.
    
    Test Plan:
      NA
    
    Reviewers: #bitcoin_abc, jasonbcox, deadalnix
    
    Reviewed By: #bitcoin_abc, jasonbcox, deadalnix
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1472

--- src/net.cpp
+++ src/net.cpp
@@ -1176,7 +1176,8 @@ void CConnman::AcceptConnection(const ListenSocket &hListenSocket) {
 #endif
 
     if (IsBanned(addr) && !whitelisted) {
-        LogPrintf("connection from %s dropped (banned)\n", addr.ToString());
+        LogPrint(BCLog::NET, "connection from %s dropped (banned)\n",
+                 addr.ToString());
         CloseSocket(hSocket);
         return;
     }
