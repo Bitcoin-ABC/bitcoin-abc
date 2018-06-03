b5b67a808386a40ccbdb0aa4ec1554df6d01c9ff
practicalswift <practicalswift@users.noreply.github.com>
2018-06-03 21:33:32 +0200

    build: Guard against accidental introduction of new Boost dependencies
    Summary:
    ```
    Guard against accidental introduction of new Boost dependencies.
    
    Context: #13383  the usage of boost::lexical_cast was introduced in
    #11517 from December 2017
    ```
    
    Backport of core PR13385
    https://github.com/bitcoin/bitcoin/pull/13385/files
    
    The `lint-include.sh` has been renamed as all the other parts of it
    (apart from the boost dependency checks) have been ported as arcanist
    linters.
    
    The expected list has been updated to reflect our codebase.
    
    Test Plan:
    Should return no error:
      ./test/lint/lint-boost-dependencies.sh
    Comment one of the expected boost include and run again. Check the error
    is found.
    Add a `boost/dummy.hpp` to the expected list and run again. Check the
    linter displays a `Good job!` message.
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D3723

---
+++ test/lint/lint-boost-dependencies.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2018 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#
+# Guard against accidental introduction of new Boost dependencies.
+
+EXIT_CODE=0
+
+EXPECTED_BOOST_INCLUDES=(
+    boost/algorithm/string.hpp
+    boost/algorithm/string/classification.hpp
+    boost/algorithm/string/predicate.hpp
+    boost/algorithm/string/replace.hpp
+    boost/algorithm/string/split.hpp
+    boost/assign/std/vector.hpp
+    boost/bind.hpp
+    boost/chrono/chrono.hpp
+    boost/date_time/posix_time/posix_time.hpp
+    boost/filesystem.hpp
+    boost/filesystem/fstream.hpp
+    boost/interprocess/sync/file_lock.hpp
+    boost/lexical_cast.hpp
+    boost/multi_index/composite_key.hpp
+    boost/multi_index/hashed_index.hpp
+    boost/multi_index/member.hpp
+    boost/multi_index/ordered_index.hpp
+    boost/multi_index/sequenced_index.hpp
+    boost/multi_index_container.hpp
+    boost/noncopyable.hpp
+    boost/optional.hpp
+    boost/preprocessor/cat.hpp
+    boost/preprocessor/stringize.hpp
+    boost/program_options/detail/config_file.hpp
+    boost/range/iterator.hpp
+    boost/range/adaptor/sliced.hpp
+    boost/scoped_array.hpp
+    boost/signals2/connection.hpp
+    boost/signals2/last_value.hpp
+    boost/signals2/signal.hpp
+    boost/test/unit_test.hpp
+    boost/thread.hpp
+    boost/thread/condition_variable.hpp
+    boost/thread/locks.hpp
+    boost/thread/mutex.hpp
+    boost/thread/shared_mutex.hpp
+    boost/thread/thread.hpp
+    boost/variant.hpp
+    boost/variant/apply_visitor.hpp
+    boost/variant/static_visitor.hpp
+)
+
+for BOOST_INCLUDE in $(git grep '^#include <boost/' -- "*.cpp" "*.h" | cut -f2 -d: | cut -f2 -d'<' | cut -f1 -d'>' | sort -u); do
+    IS_EXPECTED_INCLUDE=0
+    for EXPECTED_BOOST_INCLUDE in "${EXPECTED_BOOST_INCLUDES[@]}"; do
+        if [[ "${BOOST_INCLUDE}" == "${EXPECTED_BOOST_INCLUDE}" ]]; then
+            IS_EXPECTED_INCLUDE=1
+            break
+        fi
+    done
+    if [[ ${IS_EXPECTED_INCLUDE} == 0 ]]; then
+        EXIT_CODE=1
+        echo "A new Boost dependency in the form of \"${BOOST_INCLUDE}\" appears to have been introduced:"
+        git grep "${BOOST_INCLUDE}" -- "*.cpp" "*.h"
+        echo
+    fi
+done
+
+for EXPECTED_BOOST_INCLUDE in "${EXPECTED_BOOST_INCLUDES[@]}"; do
+    if ! git grep -q "^#include <${EXPECTED_BOOST_INCLUDE}>" -- "*.cpp" "*.h"; then
+        echo "Good job! The Boost dependency \"${EXPECTED_BOOST_INCLUDE}\" is no longer used."
+        echo "Please remove it from EXPECTED_BOOST_INCLUDES in $0"
+        echo "to make sure this dependency is not accidentally reintroduced."
+        echo
+        EXIT_CODE=1
+    fi
+done
+
+exit ${EXIT_CODE}
