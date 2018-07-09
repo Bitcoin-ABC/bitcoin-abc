087b39c0e869a073a57634ef7f8e707b80fd7d1b
Amaury Séchet <deadalnix@gmail.com>
2018-07-09 16:41:55 +0200

    Do not use BIP9 to set block version anymore.
    Summary:
    As per title. This is part of deprecating BIP9.
    
    Depends on D1546
    
    Test Plan:
      make check
      ./test/functional/test_runner.py --extended
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1551

--- src/validation.cpp
+++ src/validation.cpp
@@ -1907,17 +1907,7 @@ VersionBitsCache versionbitscache;
 
 int32_t ComputeBlockVersion(const CBlockIndex *pindexPrev,
                             const Consensus::Params &params) {
-    LOCK(cs_main);
     int32_t nVersion = VERSIONBITS_TOP_BITS;
-
-    for (int i = 0; i < (int)Consensus::MAX_VERSION_BITS_DEPLOYMENTS; i++) {
-        ThresholdState state = VersionBitsState(
-            pindexPrev, params, (Consensus::DeploymentPos)i, versionbitscache);
-        if (state == THRESHOLD_LOCKED_IN || state == THRESHOLD_STARTED) {
-            nVersion |= VersionBitsMask(params, (Consensus::DeploymentPos)i);
-        }
-    }
-
     return nVersion;
 }
 
