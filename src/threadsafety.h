e057440b5892aef7c1f7e9a438d4f01075658938
Wladimir J. van der Laan <laanwj@gmail.com>
2018-09-04 12:29:13 +0200

    Merge #14129: Trivial: update clang thread-safety docs url
    Summary:
    c7f7fa467ec571d3b3fd7ae1d32429b74b18ad9c Trivial: update clang thread-safety docs url (Ben Woosley)
    
    Pull request description:
    
      From the defunct http://clang.llvm.org/docs/LanguageExtensions.html#threadsafety to https://clang.llvm.org/docs/ThreadSafetyAnalysis.html
    
    Tree-SHA512: 5113c3933fccee7b45cace5d8dffa38b46ed9ad1422795d57843a20b276ed0e513bbf8d3d2bd28f55a46baf14a349871d7a635485785b93cb21baf8b8720c15a
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/14129 | PR14129]]
    
    Test Plan: Verify old link does not lead to information on thread safety whereas the new link does lead to a page about clang thread safety
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D6226

--- src/threadsafety.h
+++ src/threadsafety.h
@@ -10,9 +10,9 @@
 // TL;DR Add GUARDED_BY(mutex) to member variables. The others are rarely
 // necessary. Ex: int nFoo GUARDED_BY(cs_foo);
 //
-// See http://clang.llvm.org/docs/LanguageExtensions.html#threadsafety for
-// documentation. The clang compiler can do advanced static analysis of locking
-// when given the -Wthread-safety option.
+// See https://clang.llvm.org/docs/ThreadSafetyAnalysis.html for documentation.
+// The clang compiler can do advanced static analysis of locking when given the
+// -Wthread-safety option.
 #define LOCKABLE __attribute__((lockable))
 #define SCOPED_LOCKABLE __attribute__((scoped_lockable))
 #define GUARDED_BY(x) __attribute__((guarded_by(x)))
