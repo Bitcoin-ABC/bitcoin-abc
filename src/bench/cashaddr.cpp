ad4c6453a9ef462777053713dbee38d9ef7ee63e
Jason B. Cox <contact@jasonbcox.com>
2018-10-01 14:22:46 -0700

    Added CashAddr benchmarks for decoding/encoding
    Summary:
    Added CashAddr benchmarks similar to the bech32 benchmarks in Core's PR 13586
    Completes T419
    
    Test Plan: make check && bench_bitcoin
    
    Reviewers: deadalnix, schancel, #bitcoin_abc
    
    Reviewed By: deadalnix, #bitcoin_abc
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1856

---
+++ src/bench/cashaddr.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2018 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "cashaddr.h"
+#include "bench.h"
+
+#include <string>
+#include <vector>
+
+static void CashAddrEncode(benchmark::State &state) {
+    std::vector<uint8_t> buffer = {17,  79, 8,   99,  150, 189, 208, 162,
+                                   22,  23, 203, 163, 36,  58,  147, 227,
+                                   139, 2,  215, 100, 91,  38,  11,  141,
+                                   253, 40, 117, 21,  16,  90,  200, 24};
+    while (state.KeepRunning()) {
+        cashaddr::Encode("bitcoincash", buffer);
+    }
+}
+
+static void CashAddrDecode(benchmark::State &state) {
+    const char *addrWithPrefix =
+        "bitcoincash:qprnwmr02d7ky9m693qufj5mgkpf4wvssv0w86tkjd";
+    const char *addrNoPrefix = "qprnwmr02d7ky9m693qufj5mgkpf4wvssv0w86tkjd";
+    while (state.KeepRunning()) {
+        cashaddr::Decode(addrWithPrefix, "bitcoincash");
+        cashaddr::Decode(addrNoPrefix, "bitcoincash");
+    }
+}
+
+BENCHMARK(CashAddrEncode);
+BENCHMARK(CashAddrDecode);
