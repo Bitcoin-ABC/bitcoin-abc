8a8007128fedba8cc7464984b0f8d7548086b513
John Newbery <john@johnnewbery.com>
2018-10-07 09:30:45 +0900

    [tests] Remove rpc_zmq.py
    Summary:
    ```
    rpc_zmq.py is racy and fails intermittently. Remove that test file and
    move the getzmqnotifications RPC test into interface_zmq.py
    ```
    
    Backport of core [[https://github.com/bitcoin/bitcoin/pull/14419 | PR14419]].
    
    Before this diff, running `interface_zmq` and `rpc_zmq` at the same time
    would fail, because they use the same address and port.
    
    Test Plan:
      ./test/functional/test_runner.py interface_zmq
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D5600

--- test/functional/interface_zmq.py
+++ test/functional/interface_zmq.py
@@ -13,6 +13,8 @@ from test_framework.util import (
     hash256,
 )
 
+ADDRESS = "tcp://127.0.0.1:28332"
+
 
 class ZMQSubscriber:
     def __init__(self, socket, topic):
@@ -50,11 +52,10 @@ class ZMQTest (BitcoinTestFramework):
         # test fails if the publishing order changes.
         # Note that the publishing order is not defined in the documentation and
         # is subject to change.
-        address = "tcp://127.0.0.1:28332"
         self.zmq_context = zmq.Context()
         socket = self.zmq_context.socket(zmq.SUB)
         socket.set(zmq.RCVTIMEO, 60000)
-        socket.connect(address)
+        socket.connect(ADDRESS)
 
         # Subscribe to all available topics.
         self.hashblock = ZMQSubscriber(socket, b"hashblock")
@@ -63,7 +64,7 @@ class ZMQTest (BitcoinTestFramework):
         self.rawtx = ZMQSubscriber(socket, b"rawtx")
 
         self.extra_args = [
-            ["-zmqpub{}={}".format(sub.topic.decode(), address) for sub in [
+            ["-zmqpub{}={}".format(sub.topic.decode(), ADDRESS) for sub in [
                 self.hashblock, self.hashtx, self.rawblock, self.rawtx]],
             [],
         ]
@@ -118,6 +119,16 @@ class ZMQTest (BitcoinTestFramework):
         # Should receive the broadcasted raw transaction.
         hex = self.rawtx.receive()
         assert_equal(payment_txid, hash256(hex).hex())
+
+        self.log.info("Test the getzmqnotifications RPC")
+        assert_equal(self.nodes[0].getzmqnotifications(), [
+            {"type": "pubhashblock", "address": ADDRESS},
+            {"type": "pubhashtx", "address": ADDRESS},
+            {"type": "pubrawblock", "address": ADDRESS},
+            {"type": "pubrawtx", "address": ADDRESS},
+        ])
+
+        assert_equal(self.nodes[1].getzmqnotifications(), [])
 
 
 if __name__ == '__main__':
