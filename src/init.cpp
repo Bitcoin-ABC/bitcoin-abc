6f41fa19a3cac196b49f53701ded65d556887360
Patrick Strateman <patrick.strateman@gmail.com>
2018-11-13 14:09:05 -0500

    Increase maxconnections limit when using poll.
    Summary:
    Partial backport of Core [[https://github.com/bitcoin/bitcoin/pull/14336 | PR14336]]
    https://github.com/bitcoin/bitcoin/pull/14336/commits/4927bf2f257ac53569978980eaf1f61c2c6b04cc
    
    Depends on D6470
    
    Test Plan:
      ninja
      ninja check-all
      src/bitcoind --debug=net
    Verify normal node behavior.
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, majcosta
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, majcosta
    
    Subscribers: majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D6471

--- src/init.cpp
+++ src/init.cpp
@@ -1735,13 +1735,18 @@ bool AppInitParameterInteraction(Config &config) {
     // Trim requested connection counts, to fit into system limitations
     // <int> in std::min<int>(...) to work around FreeBSD compilation issue
     // described in #2695
+    nFD = RaiseFileDescriptorLimit(nMaxConnections + MIN_CORE_FILEDESCRIPTORS +
+                                   MAX_ADDNODE_CONNECTIONS);
+#ifdef USE_POLL
+    int fd_max = nFD;
+#else
+    int fd_max = FD_SETSIZE;
+#endif
     nMaxConnections =
-        std::max(std::min<int>(nMaxConnections, FD_SETSIZE - nBind -
+        std::max(std::min<int>(nMaxConnections, fd_max - nBind -
                                                     MIN_CORE_FILEDESCRIPTORS -
                                                     MAX_ADDNODE_CONNECTIONS),
                  0);
-    nFD = RaiseFileDescriptorLimit(nMaxConnections + MIN_CORE_FILEDESCRIPTORS +
-                                   MAX_ADDNODE_CONNECTIONS);
     if (nFD < MIN_CORE_FILEDESCRIPTORS) {
         return InitError(
             _("Not enough file descriptors available.").translated);
