e0797f6dbefea651420f474940963b470a0931fd
Jason B. Cox <contact@jasonbcox.com>
2018-11-18 21:17:41 -0800

    Fix pad_tx off by one error + nits
    Summary: See title
    
    Test Plan: test_runner.py
    
    Reviewers: deadalnix, schancel, #bitcoin_abc
    
    Reviewed By: schancel, #bitcoin_abc
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D2096

--- test/functional/test_framework/txtools.py
+++ test/functional/test_framework/txtools.py
@@ -23,18 +23,17 @@ def pad_tx(tx, min_size=None):
             tx.vout.append(CTxOut(0, CScript([OP_RETURN])))
         else:
             padding = random.randrange(
-                1 << 8 * padding_len - 1, 1 << 8 * padding_len)
+                1 << 8 * padding_len - 2, 1 << 8 * padding_len - 1)
             tx.vout.append(
                 CTxOut(0, CScript([padding, OP_RETURN])))
         curr_size = len(tx.serialize())
 
     tx.rehash()
 
-# Pad  outputs until it reaches at least min_size
+# Pad outputs until it reaches at least min_size
 
 
 def pad_raw_tx(rawtx_hex, min_size=None):
-
     tx = CTransaction()
     FromHex(tx, rawtx_hex)
     pad_tx(tx, min_size)
