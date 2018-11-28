f4f3364e9f9b023d25882c0691744c9c1e533138
MarcoFalke <falke.marco@gmail.com>
2018-11-28 11:13:59 -0500

    Merge #14822: bench: Destroy wallet txs instead of leaking their memory
    Summary:
    [[https://github.com/bitcoin/bitcoin/pull/14822/files | PR14822]] backport
    
    fa5cef0f78 bench: Destroy wallet txs instead of leaking their memory (MarcoFalke)
    
    Pull request description:
    
      This should destroy the wallet txs when the benchmark ends to avoid having to hold them when the following benchmarks run.
    
    Depends on D5251 (only for having updated valgrind suppressions, besides this it is landable anytime)
    
    Test Plan:
    `ninja bench-bitcoin`
    
    Run the following and observe there are no more leaks--
    
        ninja src/bench/bitcoin-bench
        valgrind --suppressions=../contrib/valgrind.supp src/bench/bitcoin-bench -evals=1 -scaling=0.0
    
    Reviewers: Fabien, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D5252

--- src/bench/coin_selection.cpp
+++ src/bench/coin_selection.cpp
@@ -12,21 +12,15 @@
 #include <set>
 
 static void addCoin(const Amount nValue, const CWallet &wallet,
-                    std::vector<OutputGroup> &groups) {
-    int nInput = 0;
-
+                    std::vector<std::unique_ptr<CWalletTx>> &wtxs) {
     static int nextLockTime = 0;
     CMutableTransaction tx;
     // so all transactions get different hashes
     tx.nLockTime = nextLockTime++;
-    tx.vout.resize(nInput + 1);
-    tx.vout[nInput].nValue = nValue;
-    CWalletTx *wtx = new CWalletTx(&wallet, MakeTransactionRef(std::move(tx)));
-
-    int nAge = 6 * 24;
-    COutput output(wtx, nInput, nAge, true /* spendable */, true /* solvable */,
-                   true /* safe */);
-    groups.emplace_back(output.GetInputCoin(), 6, false, 0, 0);
+    tx.vout.resize(1);
+    tx.vout[0].nValue = nValue;
+    wtxs.push_back(std::make_unique<CWalletTx>(
+        &wallet, MakeTransactionRef(std::move(tx))));
 }
 
 // Simple benchmark for wallet coin selection. Note that it maybe be necessary
@@ -42,15 +36,23 @@ static void CoinSelection(benchmark::State &state) {
     auto chain = interfaces::MakeChain();
     const CWallet wallet(Params(), *chain, WalletLocation(),
                          WalletDatabase::CreateDummy());
-
+    std::vector<std::unique_ptr<CWalletTx>> wtxs;
     LOCK(wallet.cs_wallet);
 
     // Add coins.
-    std::vector<OutputGroup> groups;
     for (int i = 0; i < 1000; ++i) {
-        addCoin(1000 * COIN, wallet, groups);
+        addCoin(1000 * COIN, wallet, wtxs);
     }
-    addCoin(3 * COIN, wallet, groups);
+    addCoin(3 * COIN, wallet, wtxs);
+
+    // Create groups
+    std::vector<OutputGroup> groups;
+    for (const auto &wtx : wtxs) {
+        COutput output(wtx.get(), 0 /* iIn */, 6 * 24 /* nDepthIn */,
+                       true /* spendable */, true /* solvable */,
+                       true /* safe */);
+        groups.emplace_back(output.GetInputCoin(), 6, false, 0, 0);
+    }
 
     const CoinEligibilityFilter filter_standard(1, 6, 0);
     const CoinSelectionParams coin_selection_params(
