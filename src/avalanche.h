c7b844fb409da93426a775b0c4571c020e80956c
Amaury Séchet <deadalnix@gmail.com>
2018-11-29 16:27:29 +0100

    [avalanche] Use black magic for popcount.
    Summary:
    Or compiler intrinsic if these are supported.
    
    The black magic algorithm is from 'Owen Anderson <resistor@mac.com>', as far as I know.
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, jasonbcox, Fabien, Mengerian
    
    Reviewed By: #bitcoin_abc, jasonbcox, Fabien, Mengerian
    
    Subscribers: Mengerian, jasonbcox, teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2148

--- src/avalanche.h
+++ src/avalanche.h
@@ -55,24 +55,6 @@ private:
     // score.
     uint16_t confidence;
 
-    /**
-     * Return the number of bits set in an integer value.
-     * TODO: There are compiler intrinsics to do that, but we'd need to get them
-     * detected so this will do for now.
-     */
-    static uint32_t countBits(uint32_t value) {
-        uint32_t count = 0;
-        while (value) {
-            // If the value is non zero, then at least one bit is set.
-            count++;
-
-            // Clear the rightmost bit set.
-            value &= (value - 1);
-        }
-
-        return count;
-    }
-
 public:
     VoteRecord(bool accepted) : votes(0xaaaa), confidence(accepted) {}
 
@@ -87,29 +69,7 @@ public:
      * Register a new vote for an item and update confidence accordingly.
      * Returns true if the acceptance or finalization state changed.
      */
-    bool registerVote(bool vote) {
-        votes = (votes << 1) | vote;
-
-        auto bits = countBits(votes & 0xff);
-        bool yes = bits > 6;
-        bool no = bits < 2;
-        if (!yes && !no) {
-            // The vote is inconclusive.
-            return false;
-        }
-
-        if (isAccepted() == yes) {
-            // If the vote is in agreement with our internal status, increase
-            // confidence.
-            confidence += 2;
-            return getConfidence() == AVALANCHE_FINALIZATION_SCORE;
-        }
-
-        // The vote did not agree with our internal state, in that case, reset
-        // confidence.
-        confidence = yes;
-        return true;
-    }
+    bool registerVote(bool vote);
 };
 
 class AvalancheVote {
