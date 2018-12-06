8a84cdd66548c2ec16eff155dfcb34d119f9e930
ludete <yyxyong@163.com>
2018-12-06 23:07:12 +0800

    Modify integration test

--- test/functional/whc_tokengrantnegtive.py
+++ test/functional/whc_tokengrantnegtive.py
@@ -9,60 +9,67 @@ import time
 
 class WHC_TOKEN_MANAGE(BitcoinTestFramework):
     def set_test_params(self):
-        self.num_nodes = 2
+        self.num_nodes = 1
         self.tip = None
         self.setup_clean_chain = True
 
     def token_manage_test(self):
         # generate 200whc for node[0]
         address = self.nodes[0].getnewaddress("")
-        address_dst = self.nodes[1].getnewaddress("")
+        address_dst = self.nodes[0].getnewaddress("")
+        self.nodes[0].generatetoaddress(110, address)
+        self.nodes[0].whc_burnbchgetwhc(4)
+        self.nodes[0].sendtoaddress(address_dst, 20)
         self.nodes[0].generatetoaddress(1, address)
-        self.nodes[0].generatetoaddress(1, address_dst)
-        time.sleep(5)
-        self.nodes[0].generatetoaddress(100, address)
-        time.sleep(5)
-        trans_id = self.nodes[0].whc_burnbchgetwhc(4)
-        trans_id1 = self.nodes[1].whc_burnbchgetwhc(4)
-        self.nodes[0].generatetoaddress(1, address)
-        time.sleep(1)
 
-        fixed_trans_id = self.nodes[0].whc_sendissuancefixed(address, 1, 1, 0, "", "", "whctoken", "", "", "500")
+        # create fix property And manager property
+        self.nodes[0].whc_sendissuancefixed(address, 1, 1, 0, "", "", "whctoken", "", "", "500")
         managed_trans_id = self.nodes[0].whc_sendissuancemanaged(address, 1, 1, 0, "", "", "managede token", "", "")
         self.nodes[0].generatetoaddress(1, address)
-        time.sleep(1)
-
-        fixed_trans = self.nodes[0].whc_gettransaction(fixed_trans_id)
-        fixed_property_id = fixed_trans["propertyid"]
 
         managed_trans = self.nodes[0].whc_gettransaction(managed_trans_id)
         managed_property_id = managed_trans["propertyid"]
 
         # exp1: token type is not managed
-        ret = self.nodes[0].whc_createrawtx_input("", managed_trans_id, 1)
-        payload = self.nodes[0].whc_createpayload_grant(managed_property_id, "50", "")
-        p = payload[:15] + '3' + payload[16:]
-        ret = self.nodes[0].whc_createrawtx_opreturn(ret, p)
-        ret = self.nodes[0].whc_createrawtx_reference(ret, address, 49.99)
-        ret = self.nodes[0].signrawtransaction(ret)
-        ret = self.nodes[0].sendrawtransaction(ret["hex"])
-        self.nodes[0].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[0].whc_gettransaction(ret)
-        assert trans["valid"] is False
+        item = self.getSpent(address)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_grant(managed_property_id, "50", "")
+            p = payload[:15] + '9' + payload[16:]
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, p)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].signrawtransaction(ret)
+            ret = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(ret)
+            assert trans["valid"] is False
+        else:
+            assert False
 
         # exp2: token granter is not token issuer
-        ret = self.nodes[1].whc_createrawtx_input("", trans_id1, 2)
-        payload = self.nodes[1].whc_createpayload_grant(managed_property_id, "50", "")
-        ret = self.nodes[1].whc_createrawtx_opreturn(ret, payload)
-        ret = self.nodes[1].whc_createrawtx_reference(ret, address, 45.99)
-        ret = self.nodes[1].signrawtransaction(ret)
-        ret = self.nodes[1].sendrawtransaction(ret["hex"])
-        self.nodes[1].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[1].whc_gettransaction(ret)
-        assert trans["valid"] is False
-        assert trans["invalidreason"] == "Sender is not the issuer of the property"
+        item = self.getSpent(address_dst)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_grant(managed_property_id, "50", "")
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, payload)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].signrawtransaction(ret)
+            ret = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(ret)
+            assert trans["valid"] is False
+            assert trans["invalidreason"] == "Sender is not the issuer of the property"
+        else:
+            assert False
+
+    def getSpent(self, addr):
+        item = None
+        ret = self.nodes[0].listunspent()
+        for it in ret:
+            if it["address"] == addr and it["amount"] > 1:
+                item = it
+                break
+        return item
 
     def run_test(self):
         self.token_manage_test()
--- test/functional/whc_tokenrevokenegtive.py
+++ test/functional/whc_tokenrevokenegtive.py
@@ -9,141 +9,164 @@ import time
 
 class WHC_TOKEN_MANAGE(BitcoinTestFramework):
     def set_test_params(self):
-        self.num_nodes = 2
+        self.num_nodes = 1
         self.tip = None
         self.setup_clean_chain = True
 
     def token_manage_test(self):
         # generate 200whc for node[0]
         address = self.nodes[0].getnewaddress("")
-        address_dst = self.nodes[1].getnewaddress("")
+        address_dst = self.nodes[0].getnewaddress("")
+        self.nodes[0].generatetoaddress(110, address)
+        self.nodes[0].whc_burnbchgetwhc(4)
+        self.nodes[0].sendtoaddress(address_dst, 10)
         self.nodes[0].generatetoaddress(1, address)
-        self.nodes[0].generatetoaddress(1, address_dst)
-        time.sleep(5)
-        self.nodes[0].generatetoaddress(100, address)
-        time.sleep(5)
-        trans_id = self.nodes[0].whc_burnbchgetwhc(4)
-        trans_id1 = self.nodes[1].whc_burnbchgetwhc(4)
-        self.nodes[0].generatetoaddress(1, address)
-        time.sleep(1)
 
-        fixed_trans_id = self.nodes[0].whc_sendissuancefixed(address, 1, 1, 0, "", "", "whctoken", "", "", "500")
+        self.nodes[0].whc_sendissuancefixed(address, 1, 1, 0, "", "", "whctoken", "", "", "500")
         managed_trans_id = self.nodes[0].whc_sendissuancemanaged(address, 1, 1, 0, "", "", "managede token", "", "")
         self.nodes[0].generatetoaddress(1, address)
-        time.sleep(1)
-
-        fixed_trans = self.nodes[0].whc_gettransaction(fixed_trans_id)
-        fixed_property_id = fixed_trans["propertyid"]
 
         managed_trans = self.nodes[0].whc_gettransaction(managed_trans_id)
         managed_property_id = managed_trans["propertyid"]
 
         # exp3: token value exceed max
-        ret = self.nodes[0].whc_createrawtx_input("", managed_trans_id, 1)
-        payload = self.nodes[0].whc_createpayload_grant(managed_property_id, "50", "")
-        p = payload[:16] + "ffffffffffffffff" + payload[32:]
-        ret = self.nodes[0].whc_createrawtx_opreturn(ret, p)
-        ret = self.nodes[0].whc_createrawtx_reference(ret, address, 49.99)
-        ret = self.nodes[0].signrawtransaction(ret)
-        trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
-        self.nodes[0].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[0].whc_gettransaction(trans_id)
-        assert trans["valid"] is False
-        assert trans["invalidreason"] == "Value out of range or zero"
+        item = self.getSpent(address)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_grant(managed_property_id, "50", "")
+            p = payload[:16] + "ffffffffffffffff" + payload[32:]
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, p)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].signrawtransaction(ret)
+            trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(trans_id)
+            assert trans["valid"] is False
+            assert trans["invalidreason"] == "Value out of range or zero"
+        else:
+            assert False
 
         # exp3: token value exceed max
-        ret = self.nodes[0].whc_createrawtx_input("", trans_id, 1)
-        payload = self.nodes[0].whc_createpayload_grant(managed_property_id, "50", "")
-        ret = self.nodes[0].whc_createrawtx_opreturn(ret, payload)
-        ret = self.nodes[0].whc_createrawtx_reference(ret, address, 49.989)
-        ret = self.nodes[0].signrawtransaction(ret)
-        trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
-        self.nodes[0].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[0].whc_gettransaction(trans_id)
-        assert trans["valid"] is True
+        item = self.getSpent(address)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_grant(managed_property_id, "50", "")
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, payload)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].signrawtransaction(ret)
+            trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(trans_id)
+            assert trans["valid"] is True
+        else:
+            assert False
 
         # not managed token
-        ret = self.nodes[0].whc_createrawtx_input("", trans_id, 1)
-        payload = self.nodes[0].whc_createpayload_revoke(managed_property_id, "20", "")
-        p = payload[:15] + '3' + payload[16:]
-        ret = self.nodes[0].whc_createrawtx_opreturn(ret, p)
-        ret = self.nodes[0].whc_createrawtx_reference(ret, address, 49.988)
-        ret = self.nodes[0].signrawtransaction(ret)
-        trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
-        self.nodes[0].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[0].whc_gettransaction(trans_id)
-        assert trans["valid"] is False
+        item = self.getSpent(address)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_revoke(managed_property_id, "20", "")
+            p = payload[:15] + '6' + payload[16:]
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, p)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].signrawtransaction(ret)
+            trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(trans_id)
+            assert trans["valid"] is False
+        else:
+            assert False
 
         # not managed token issuer
-        ret = self.nodes[1].whc_createrawtx_input("", trans_id1, 2)
-        payload = self.nodes[1].whc_createpayload_revoke(managed_property_id, "20", "")
-        ret = self.nodes[1].whc_createrawtx_opreturn(ret, payload)
-        ret = self.nodes[1].whc_createrawtx_reference(ret, address_dst, 45.987)
-        ret = self.nodes[1].signrawtransaction(ret)
-        trans_id2 = self.nodes[1].sendrawtransaction(ret["hex"])
-        self.nodes[1].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[1].whc_gettransaction(trans_id2)
-        assert trans["valid"] is False
-        assert trans["invalidreason"] == "Sender is not the issuer of the property"
+        item = self.getSpent(address_dst)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_revoke(managed_property_id, "20", "")
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, payload)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].signrawtransaction(ret)
+            trans_id2 = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(trans_id2)
+            assert trans["valid"] is False
+            assert trans["invalidreason"] == "Sender is not the issuer of the property"
+        else:
+            assert False
 
         # no issuer raise a change issuer action
-        ret = self.nodes[1].whc_createrawtx_input("", trans_id2, 1)
-        payload = self.nodes[1].whc_createpayload_changeissuer(managed_property_id)
-        ret = self.nodes[1].whc_createrawtx_opreturn(ret, payload)
-        ret = self.nodes[1].whc_createrawtx_reference(ret, address, 45.986)
-        ret = self.nodes[1].signrawtransaction(ret)
-        ret = self.nodes[1].sendrawtransaction(ret["hex"])
-        self.nodes[1].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[1].whc_gettransaction(ret)
-        assert trans["valid"] is False
-        assert trans["invalidreason"] == "Sender is not the issuer of the property"
+        item = self.getSpent(address_dst)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_changeissuer(managed_property_id)
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, payload)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].signrawtransaction(ret)
+            ret = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(ret)
+            assert trans["valid"] is False
+            assert trans["invalidreason"] == "Sender is not the issuer of the property"
+        else:
+            assert False
 
         # issuer is himself
-        ret = self.nodes[0].whc_createrawtx_input("", trans_id, 1)
-        payload = self.nodes[0].whc_createpayload_changeissuer(managed_property_id)
-        ret = self.nodes[0].whc_createrawtx_opreturn(ret, payload)
-        ret = self.nodes[0].whc_createrawtx_reference(ret, address, 49.987)
-        ret = self.nodes[0].whc_createrawtx_reference(ret, address)
-        ret = self.nodes[0].signrawtransaction(ret)
-        trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
-        self.nodes[0].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[0].whc_gettransaction(trans_id)
-        # assert trans["valid"] is True
+        item = self.getSpent(address)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_changeissuer(managed_property_id)
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, payload)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].whc_createrawtx_reference(ret, address)
+            ret = self.nodes[0].signrawtransaction(ret)
+            trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(trans_id)
+            assert trans["valid"] is True
+        else:
+            assert False
 
         # property id not exit
-        ret = self.nodes[0].whc_createrawtx_input("", trans_id, 1)
-        payload = self.nodes[0].whc_createpayload_changeissuer(managed_property_id)
-        p = payload[:15] + 'f'
-        ret = self.nodes[0].whc_createrawtx_opreturn(ret, p)
-        ret = self.nodes[0].whc_createrawtx_reference(ret, address, 49.986)
-        ret = self.nodes[0].whc_createrawtx_reference(ret, address_dst)
-        ret = self.nodes[0].signrawtransaction(ret)
-        trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
-        self.nodes[0].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[0].whc_gettransaction(trans_id)
-        assert trans["valid"] is False
-        assert trans["invalidreason"] == "Property does not exist"
+        item = self.getSpent(address)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_changeissuer(managed_property_id)
+            p = payload[:15] + 'f'
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, p)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].whc_createrawtx_reference(ret, address_dst)
+            ret = self.nodes[0].signrawtransaction(ret)
+            trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(trans_id)
+            assert trans["valid"] is False
+            assert trans["invalidreason"] == "Property does not exist"
+        else:
+            assert False
 
         # token revoke > token left
-        ret = self.nodes[0].whc_createrawtx_input("", trans_id, 1)
-        payload = self.nodes[0].whc_createpayload_revoke(managed_property_id, "60", "")
-        ret = self.nodes[0].whc_createrawtx_opreturn(ret, payload)
-        ret = self.nodes[0].whc_createrawtx_reference(ret, address, 49.985)
-        ret = self.nodes[0].signrawtransaction(ret)
-        trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
-        self.nodes[0].generatetoaddress(10, address)
-        time.sleep(1)
-        trans = self.nodes[0].whc_gettransaction(trans_id)
-        assert trans["valid"] is False
-        assert trans["invalidreason"] == "Sender has insufficient balance"
+        item = self.getSpent(address)
+        if item:
+            ret = self.nodes[0].whc_createrawtx_input("", item["txid"], item["vout"])
+            payload = self.nodes[0].whc_createpayload_revoke(managed_property_id, "60", "")
+            ret = self.nodes[0].whc_createrawtx_opreturn(ret, payload)
+            ret = self.nodes[0].whc_createrawtx_reference(ret, item["address"], round(float(item["amount"]) - 0.01, 8))
+            ret = self.nodes[0].signrawtransaction(ret)
+            trans_id = self.nodes[0].sendrawtransaction(ret["hex"])
+            self.nodes[0].generatetoaddress(1, address)
+            trans = self.nodes[0].whc_gettransaction(trans_id)
+            assert trans["valid"] is False
+            assert trans["invalidreason"] == "Sender has insufficient balance"
+        else:
+            assert False
 
+    def getSpent(self, addr):
+        item = None
+        ret = self.nodes[0].listunspent()
+        for it in ret:
+            if it["address"] == addr and it["amount"] > 1:
+                item = it
+                break
+        return item
     def run_test(self):
         self.token_manage_test()
 
