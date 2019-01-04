ccec0cfd7d8745d20e8d67cbd254043126c918f6
Matias Alejo Garcia <ematiu@gmail.com>
2019-01-04 16:18:58 +0100

    update IsValidSignatureEncoding doc
    Summary:
    update IsValidSignatureEncoding documentation, removing hashtype
    from the signature description
    
    Test Plan: n/a
    
    Reviewers: #bitcoin_abc, deadalnix, matiu
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D2095

--- src/script/sigencoding.cpp
+++ src/script/sigencoding.cpp
@@ -15,9 +15,9 @@ typedef boost::sliced_range<const valtype> slicedvaltype;
 
 /**
  * A canonical signature exists of: <30> <total len> <02> <len R> <R> <02> <len
- * S> <S> <hashtype>, where R and S are not negative (their first byte has its
- * highest bit not set), and not excessively padded (do not start with a 0 byte,
- * unless an otherwise negative number follows, in which case a single 0 byte is
+ * S> <S>, where R and S are not negative (their first byte has its highest bit
+ * not set), and not excessively padded (do not start with a 0 byte, unless an
+ * otherwise negative number follows, in which case a single 0 byte is
  * necessary and even required).
  *
  * See https://bitcointalk.org/index.php?topic=8392.msg127623#msg127623
