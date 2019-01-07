6d889411eeb7a19ba06d907c2436acd147bc8a95
MarcoFalke <falke.marco@gmail.com>
2019-01-07 12:58:55 +0100

    [backport#15102] test: Run invalid_txs.InputMissing test in feature_block
    Summary:
    test: Run invalid_txs.InputMissing test in feature_block (MarcoFalke)
    
    https://github.com/bitcoin/bitcoin/pull/15102/commits/fac4e731a8e5f140dc3bbca4c9af6e23fc1cd107
    
    ---
    
    SegWit is _not_ active in our RegTest, so I just removed the conditional branch that refers to it.
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/15102 | PR15102]]
    
    Test Plan:
      ninja
      ./test/functional/test_runner.py feature_block
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D7318

--- test/functional/feature_block.py
+++ test/functional/feature_block.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright (c) 2015-2017 The Bitcoin Core developers
-# Copyright (c) 2017 The Bitcoin developers
+# Copyright (c) 2019 The Bitcoin developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test block processing."""
@@ -133,12 +133,6 @@ class FullBlockTest(BitcoinTestFramework):
         for TxTemplate in invalid_txs.iter_all_templates():
             template = TxTemplate(spend_tx=attempt_spend_tx)
 
-            # Something about the serialization code for missing inputs creates
-            # a different hash in the test client than on bitcoind, resulting
-            # in a mismatching merkle root during block validation.
-            # Skip until we figure out what's going on.
-            if TxTemplate == invalid_txs.InputMissing:
-                continue
             if template.valid_in_block:
                 continue
 
@@ -148,7 +142,8 @@ class FullBlockTest(BitcoinTestFramework):
             blockname = "for_invalid.{}".format(TxTemplate.__name__)
             badblock = self.next_block(blockname)
             badtx = template.get_tx()
-            self.sign_tx(badtx, attempt_spend_tx)
+            if TxTemplate != invalid_txs.InputMissing:
+                self.sign_tx(badtx, attempt_spend_tx)
             badtx.rehash()
             badblock = self.update_block(blockname, [badtx])
             self.send_blocks(
