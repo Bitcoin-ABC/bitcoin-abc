6ed34f9cc7ccbea7aba7d626c861c5f46c1dd7be
Glenn Willen <gwillen@nerdnet.org>
2019-01-08 22:16:50 -0800

    Factor BroadcastTransaction out of sendrawtransaction
    Summary:
    ```
    Factor out a new BroadcastTransaction function, performing the core work
    of the sendrawtransaction rpc, so that it can be used from the GUI code.
    Move it from src/rpc/ to src/node/.
    ```
    
    Partial backport of core [[https://github.com/bitcoin/bitcoin/pull/14978 | PR14978]]:
    https://github.com/bitcoin/bitcoin/pull/14978/commits/81cd9588484cb4f4050ea4e239da0681111795db
    
    Test Plan:
      ninja all check
      ./test/functional/test_runner.py rpc_rawtransaction
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D5706

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -4,6 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <chain.h>
+#include <chainparams.h>
 #include <coins.h>
 #include <compat/byteswap.h>
 #include <config.h>
@@ -14,7 +15,7 @@
 #include <key_io.h>
 #include <keystore.h>
 #include <merkleblock.h>
-#include <net.h>
+#include <node/transaction.h>
 #include <policy/policy.h>
 #include <primitives/transaction.h>
 #include <rpc/rawtransaction.h>
@@ -31,7 +32,6 @@
 #include <validationinterface.h>
 
 #include <cstdint>
-#include <future>
 
 #include <univalue.h>
 
@@ -1205,8 +1205,6 @@ static UniValue sendrawtransaction(const Config &config,
             HelpExampleRpc("sendrawtransaction", "\"signedhex\""));
     }
 
-    std::promise<void> promise;
-
     RPCTypeCheck(request.params, {UniValue::VSTR, UniValue::VBOOL});
 
     // parse hex string from parameter
@@ -1216,73 +1214,13 @@ static UniValue sendrawtransaction(const Config &config,
     }
 
     CTransactionRef tx(MakeTransactionRef(std::move(mtx)));
-    const TxId &txid = tx->GetId();
 
-    Amount nMaxRawTxFee = maxTxFee;
-    if (!request.params[1].isNull() && request.params[1].get_bool()) {
-        nMaxRawTxFee = Amount::zero();
+    bool allowhighfees = false;
+    if (!request.params[1].isNull()) {
+        allowhighfees = request.params[1].get_bool();
     }
 
-    { // cs_main scope
-        LOCK(cs_main);
-        CCoinsViewCache &view = *pcoinsTip;
-        bool fHaveChain = false;
-        for (size_t o = 0; !fHaveChain && o < tx->vout.size(); o++) {
-            const Coin &existingCoin = view.AccessCoin(COutPoint(txid, o));
-            fHaveChain = !existingCoin.IsSpent();
-        }
-
-        bool fHaveMempool = g_mempool.exists(txid);
-        if (!fHaveMempool && !fHaveChain) {
-            // Push to local node and sync with wallets.
-            CValidationState state;
-            bool fMissingInputs;
-            if (!AcceptToMemoryPool(config, g_mempool, state, std::move(tx),
-                                    &fMissingInputs, false /* bypass_limits */,
-                                    nMaxRawTxFee)) {
-                if (state.IsInvalid()) {
-                    throw JSONRPCError(RPC_TRANSACTION_REJECTED,
-                                       FormatStateMessage(state));
-                }
-
-                if (fMissingInputs) {
-                    throw JSONRPCError(RPC_TRANSACTION_ERROR, "Missing inputs");
-                }
-
-                throw JSONRPCError(RPC_TRANSACTION_ERROR,
-                                   FormatStateMessage(state));
-            } else {
-                // If wallet is enabled, ensure that the wallet has been made
-                // aware of the new transaction prior to returning. This
-                // prevents a race where a user might call sendrawtransaction
-                // with a transaction to/from their wallet, immediately call
-                // some wallet RPC, and get a stale result because callbacks
-                // have not yet been processed.
-                CallFunctionInValidationInterfaceQueue(
-                    [&promise] { promise.set_value(); });
-            }
-        } else if (fHaveChain) {
-            throw JSONRPCError(RPC_TRANSACTION_ALREADY_IN_CHAIN,
-                               "transaction already in block chain");
-        } else {
-            // Make sure we don't block forever if re-sending a transaction
-            // already in mempool.
-            promise.set_value();
-        }
-    } // cs_main
-
-    promise.get_future().wait();
-
-    if (!g_connman) {
-        throw JSONRPCError(
-            RPC_CLIENT_P2P_DISABLED,
-            "Error: Peer-to-peer functionality missing or disabled");
-    }
-
-    CInv inv(MSG_TX, txid);
-    g_connman->ForEachNode([&inv](CNode *pnode) { pnode->PushInventory(inv); });
-
-    return txid.GetHex();
+    return BroadcastTransaction(config, tx, allowhighfees).GetHex();
 }
 
 static UniValue testmempoolaccept(const Config &config,
