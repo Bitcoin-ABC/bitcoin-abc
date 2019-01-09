db4aa8ecde0a52542dba52a3b5da4eb5abe5ce81
Fabien <fabcien@gmail.com>
2019-01-09 10:44:36 +0000

    Fix user agent sanitizer
    Summary:
    The current user agent sanitizer behavior is to drop invalid characters
    in the comment section and truncate its length if necessary, along with
    putting warnings in log.
    Note that due to a bug, invalid chars are actually not dropped from the
    comment section despite the log states it.
    
    The current behavior is not really user friend, and the bug could lead
    to BIP0014 violation.
    
    This diff moves the checks at init time, to allow for the node startup
    to fail with an error message if the requirements on the user agent are
    not met. This is actually the same behavior as bitcoin core.
    
    Depends on D2290
    
    Test Plan:
      make check
      ./test/functional/test_runner.py
    
    `./src/bitcoind -uacomment=tara:tata`, check it returns an error message
    at startup and fails to start
    
    `./src/qt/bitcoin-qt -uacomment=tara:tata`, check it displays an error message in
    a popup then exits when the OK button is clicked
    
    `./src/qt/bitcoin-qt -uacomment=tara:tata`, go to the debug window and check the
    user agent matches `/Bitcoin ABC:0.18.6(EB32.0; taratata)/`
    
    ```
    ./src/bitcoind -uacomment=taratata
    ./src/bticoin-cli getnetworkinfo
    ```
    Check the "subversion" field matches `/Bitcoin ABC:0.18.6(EB32.0;
    taratata)/`
    
    ```
    echo "uacomment=taratata" > uacomment.conf
    ./src/bitcoind -conf=$PWD/uacomment.conf
    ./src/bticoin-cli getnetworkinfo
    ```
    Check the "subversion" field matches `/Bitcoin ABC:0.18.6(EB32.0;
    taratata)/`
    
    ```
    echo "uacomment=tara:tata" > uacomment.conf
    ./src/bitcoind -conf=$PWD/uacomment.conf
    ```
    Check it returns an error message at startup and fails to start, then
    `rm uacomment.conf`
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, deadalnix, jasonbcox
    
    Subscribers: jasonbcox, teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2275

--- src/test/net_tests.cpp
+++ src/test/net_tests.cpp
@@ -13,7 +13,6 @@
 #include "streams.h"
 #include "test/test_bitcoin.h"
 
-#include <sstream>
 #include <string>
 
 #include <boost/test/unit_test.hpp>
@@ -57,6 +56,18 @@ public:
         CAddrInfo info = CAddrInfo(addr, resolved);
         s << info;
     }
+};
+
+class NetTestConfig : public DummyConfig {
+public:
+    bool SetMaxBlockSize(uint64_t maxBlockSize) override {
+        nMaxBlockSize = maxBlockSize;
+        return true;
+    }
+    uint64_t GetMaxBlockSize() const override { return nMaxBlockSize; }
+
+private:
+    uint64_t nMaxBlockSize;
 };
 
 CDataStream AddrmanToStream(CAddrManSerializationMock &_addrman) {
@@ -184,31 +195,19 @@ BOOST_AUTO_TEST_CASE(test_getSubVersionEB) {
     BOOST_CHECK_EQUAL(getSubVersionEB(0), "0.0");
 }
 
-BOOST_AUTO_TEST_CASE(test_userAgentLength) {
-    GlobalConfig config;
+BOOST_AUTO_TEST_CASE(test_userAgent) {
+    NetTestConfig config;
 
     config.SetMaxBlockSize(8000000);
-    std::string long_uacomment = "very very very very very very very very very "
-                                 "very very very very very very very very very "
-                                 "very very very very very very very very very "
-                                 "very very very very very very very very very "
-                                 "very very very very very very very very very "
-                                 "very very very very very very very very very "
-                                 "very very very very very very very very very "
-                                 "very very very very very very long comment";
-    gArgs.ForceSetMultiArg("-uacomment", long_uacomment);
+    const std::string uacomment = "A very nice comment";
+    gArgs.ForceSetMultiArg("-uacomment", uacomment);
 
-    std::ostringstream versionMessage;
-    versionMessage << "/Bitcoin ABC:" << CLIENT_VERSION_MAJOR << "."
-                   << CLIENT_VERSION_MINOR << "." << CLIENT_VERSION_REVISION
-                   << "(EB8.0; very very very very very "
-                      "very very very very very very very very very very very "
-                      "very very very very very very very very very very very "
-                      "very very very very very very very very very very very "
-                      "very very very very very very very ve)/";
+    const std::string versionMessage =
+        "/Bitcoin ABC:" + std::to_string(CLIENT_VERSION_MAJOR) + "." +
+        std::to_string(CLIENT_VERSION_MINOR) + "." +
+        std::to_string(CLIENT_VERSION_REVISION) + "(EB8.0; " + uacomment + ")/";
 
-    BOOST_CHECK_EQUAL(userAgent(config).size(), MAX_SUBVERSION_LENGTH);
-    BOOST_CHECK_EQUAL(userAgent(config), versionMessage.str());
+    BOOST_CHECK_EQUAL(userAgent(config), versionMessage);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
