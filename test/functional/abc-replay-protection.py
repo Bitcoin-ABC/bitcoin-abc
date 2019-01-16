96cc3af114b7bfc6f285abc79ea0b2fbd6fc07b7
Fabien <fabcien@gmail.com>
2019-01-16 09:19:55 +0000

    Remove -magneticanomalyactivationtime from functional tests
    Summary:
    The tests that checked for magnetic activation are removed, the others
    are updated.
    
    Test Plan:
      ./test/functional/test_runner.py abc-magnetic-anomaly-mining
    abc-replay-protection abc-transaction-ordering
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2324

--- test/functional/abc-replay-protection.py
+++ test/functional/abc-replay-protection.py
@@ -19,7 +19,6 @@ from test_framework.script import *
 
 # far into the future
 REPLAY_PROTECTION_START_TIME = 2000000000
-MAGNETIC_ANOMALY_START_TIME = 4000000000
 
 # Error due to invalid signature
 INVALID_SIGNATURE_ERROR = b'mandatory-script-verify-flag-failed (Signature must be zero for failed CHECK(MULTI)SIG operation)'
@@ -43,7 +42,6 @@ class ReplayProtectionTest(ComparisonTestFramework):
         self.tip = None
         self.blocks = {}
         self.extra_args = [['-whitelist=127.0.0.1',
-                            "-magneticanomalyactivationtime=%d" % MAGNETIC_ANOMALY_START_TIME,
                             "-replayprotectionactivationtime=%d" % REPLAY_PROTECTION_START_TIME]]
 
     def run_test(self):
@@ -109,6 +107,7 @@ class ReplayProtectionTest(ComparisonTestFramework):
             block = self.blocks[block_number]
             block.vtx.extend(new_transactions)
             old_sha256 = block.sha256
+            make_conform_to_ctor(block)
             block.hashMerkleRoot = block.calc_merkle_root()
             block.solve()
             # Update the internal state just like in next_block
