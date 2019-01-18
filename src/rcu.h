df55cbfc5a76c8c3ed25ec7897c1b02e4f76ed12
Amaury Séchet <deadalnix@gmail.com>
2019-01-18 15:49:49 +0100

    [RCU] Implement RCUPtr, a smart pointer that free asynchronousely when other threads did synchronize.
    Summary:
    This will make the memory managaement for the RadixTree significantly easier and even allow the tree to share some of its innard with the outside world.
    
    Depends on D2317
    
    Test Plan: Adde unt tests for the various behaviors implemented.
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien, teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2340

--- src/rcu.h
+++ src/rcu.h
@@ -12,6 +12,8 @@
 #include <cstdint>
 #include <functional>
 #include <map>
+#include <type_traits>
+#include <utility>
 
 class RCUInfos;
 class RCUReadLock;
@@ -71,5 +73,91 @@ public:
 
     static void synchronize() { RCUInfos::infos.synchronize(); }
 };
+
+template <typename T> class RCUPtr {
+    T *ptr;
+
+public:
+    RCUPtr() : ptr(nullptr) {}
+    explicit RCUPtr(T *ptrIn) : ptr(ptrIn) {}
+
+    ~RCUPtr() {
+        if (ptr != nullptr) {
+            ptr->release();
+        }
+    }
+
+    /**
+     * Construct a new object that is owned by the pointer.
+     */
+    template <typename... Args> static RCUPtr make(Args &&... args) {
+        return RCUPtr(new T(std::forward<Args>(args)...));
+    }
+
+    /**
+     * Copy semantic.
+     */
+    RCUPtr(const RCUPtr &src) : ptr(src.ptr) {
+        if (ptr != nullptr) {
+            ptr->acquire();
+        }
+    }
+
+    RCUPtr &operator=(const RCUPtr &rhs) {
+        RCUPtr tmp(rhs);
+        std::swap(ptr, tmp.ptr);
+        return *this;
+    }
+
+    /**
+     * Move semantic.
+     */
+    RCUPtr(RCUPtr &&src) : RCUPtr() { std::swap(ptr, src.ptr); }
+    RCUPtr &operator=(RCUPtr &&rhs) {
+        std::swap(ptr, rhs.ptr);
+        return *this;
+    }
+
+    /**
+     * Accessors
+     */
+    T *operator->() { return ptr; }
+};
+
+#define IMPLEMENT_RCU_REFCOUNT(T)                                              \
+private:                                                                       \
+    std::atomic<T> refcount{0};                                                \
+                                                                               \
+    void acquire() { refcount++; }                                             \
+                                                                               \
+    bool tryDecrement() {                                                      \
+        T count = refcount.load();                                             \
+        while (count > 0) {                                                    \
+            if (refcount.compare_exchange_weak(count, count - 1)) {            \
+                return true;                                                   \
+            }                                                                  \
+        }                                                                      \
+                                                                               \
+        return false;                                                          \
+    }                                                                          \
+                                                                               \
+    void release() {                                                           \
+        if (tryDecrement()) {                                                  \
+            return;                                                            \
+        }                                                                      \
+                                                                               \
+        RCULock::registerCleanup([this] {                                      \
+            if (tryDecrement()) {                                              \
+                return;                                                        \
+            }                                                                  \
+                                                                               \
+            delete this;                                                       \
+        });                                                                    \
+    }                                                                          \
+                                                                               \
+    static_assert(std::is_integral<T>::value, "T must be an integral type.");  \
+    static_assert(std::is_unsigned<T>::value, "T must be unsigned.");          \
+                                                                               \
+    template <typename> friend class ::RCUPtr
 
 #endif // BITCOIN_RCU_H
