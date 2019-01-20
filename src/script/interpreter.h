5aac30498f9624411ff8b065ef310d191e6ec814
Mark Lundeberg <unknown>
2019-01-19 21:47:06 -0800

    fix confusingly named function parameter 'scriptSig'
    Summary:
    scriptSig normally refers to a script, in this case it
    was referring to a serialized signature.
    
    Test Plan: trivial change
    
    Reviewers: #bitcoin_abc, deadalnix, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: teamcity, schancel
    
    Maniphest Tasks: T527
    
    Differential Revision: https://reviews.bitcoinabc.org/D2349

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -33,7 +33,7 @@ public:
                                  const CPubKey &vchPubKey,
                                  const uint256 &sighash) const;
 
-    virtual bool CheckSig(const std::vector<uint8_t> &scriptSig,
+    virtual bool CheckSig(const std::vector<uint8_t> &vchSigIn,
                           const std::vector<uint8_t> &vchPubKey,
                           const CScript &scriptCode, uint32_t flags) const {
         return false;
@@ -67,7 +67,7 @@ public:
         : txTo(txToIn), nIn(nInIn), amount(amountIn), txdata(&txdataIn) {}
 
     // The overriden functions are now final.
-    bool CheckSig(const std::vector<uint8_t> &scriptSig,
+    bool CheckSig(const std::vector<uint8_t> &vchSigIn,
                   const std::vector<uint8_t> &vchPubKey,
                   const CScript &scriptCode,
                   uint32_t flags) const final override;
