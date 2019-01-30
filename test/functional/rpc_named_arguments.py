9f8e0ae427e0b9731522daad73a48522ce3748c4
Fabien <fabcien@gmail.com>
2019-01-30 14:29:58 +0000

    Fix unused imports in rpc_* functional tests
    Summary: Preparation for a new linter on unused imports
    
    Test Plan:
      ./test/functional/test_runner.py rpc_fundrawtransaction
    rpc_listtransaction rpc_named_arguments rpc_signmessage
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Subscribers: teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2448

--- test/functional/rpc_named_arguments.py
+++ test/functional/rpc_named_arguments.py
@@ -3,16 +3,8 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-from decimal import Decimal
-
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.authproxy import JSONRPCException
-from test_framework.util import (
-    assert_equal,
-    assert_raises_rpc_error,
-    assert_is_hex_string,
-    assert_is_hash_string,
-)
+from test_framework.util import assert_equal, assert_raises_rpc_error
 
 
 class NamedArgumentTest(BitcoinTestFramework):
--- test/functional/rpc_signmessage.py
+++ test/functional/rpc_signmessage.py
@@ -4,7 +4,6 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import *
 
 
 class SignMessagesTest(BitcoinTestFramework):
