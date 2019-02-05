cc37dd10864ee3ad3054a38e664ad1fa3efa5c14
Jason B. Cox <contact@jasonbcox.com>
2019-02-05 14:31:20 -0800

    Move NULLPTR macro so that it can be used by all unit tests
    Summary: Rather than copy-paste this to another test that I'm working on, it should be available to the whole unit test suite.
    
    Test Plan: ninja check + CI diff should build (it uses an older version of Boost)
    
    Reviewers: deadalnix, Fabien, #bitcoin_abc
    
    Reviewed By: deadalnix, #bitcoin_abc
    
    Subscribers: teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2500

--- src/test/radix_tests.cpp
+++ src/test/radix_tests.cpp
@@ -14,15 +14,6 @@
 
 BOOST_FIXTURE_TEST_SUITE(radix_tests, BasicTestingSetup)
 
-/**
- * Version of Boost::test prior to 1.64 have issues when dealing with nullptr_t.
- * In order to work around this, we ensure that the null pointers are typed in a
- * way that Boost will like better.
- *
- * TODO: Use nullptr directly once the minimum version of boost is 1.64 or more.
- */
-#define NULLPTR(T) static_cast<T *>(nullptr)
-
 template <typename K> struct TestElement {
     K key;
 
