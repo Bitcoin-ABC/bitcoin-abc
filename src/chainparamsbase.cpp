ajtowns/bitcoin

ArgsManager: support config file sections

src/chainparamsbase.cpp
@@ -48,4 +48,5 @@ std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain
 void SelectBaseParams(const std::string& chain)
 {
     globalChainBaseParams = CreateBaseChainParams(chain);
+    gArgs.SelectConfigSection(chain);
 }

src/util.h
@@ -228,9 +228,15 @@ protected:
     mutable CCriticalSection cs_args;
     std::map<std::string, std::vector<std::string>> m_override_args;
     std::map<std::string, std::vector<std::string>> m_config_args;
+    std::string m_section;
     void ReadConfigStream(std::istream& stream);
 
 public:
+    /**
+     * Select the network in use
+     */
+    void SelectConfigSection(const std::string& section);
+
     void ParseParameters(int argc, const char*const argv[]);
     void ReadConfigFile(const std::string& confPath);
 

Bitcoin-ABC/bitcoin-abc

Summary:
This adds support for network sections in the configuration file.
There is no special case management yet.
Partial backport of core PR11862
Commits 95eb66d, 30f9407 and 8a9817d
Depends on D2433
Test Plan:
  make check
  ./test/functional/test_runner.py feature_config_args
Reviewers: #bitcoin_abc, deadalnix, jasonbcox
Reviewed By: #bitcoin_abc, jasonbcox
Subscribers: teamcity, schancel
Differential Revision: https://reviews.bitcoinabc.org/D2434

src/chainparamsbase.cpp
@@ -78,4 +78,5 @@ CreateBaseChainParams(const std::string &chain) {
 
 void SelectBaseParams(const std::string &chain) {
     globalChainBaseParams = CreateBaseChainParams(chain);
+    gArgs.SelectConfigNetwork(chain);
 }

src/util.h
@@ -100,10 +100,15 @@ protected:
     mutable CCriticalSection cs_args;
     std::map<std::string, std::vector<std::string>> m_override_args;
     std::map<std::string, std::vector<std::string>> m_config_args;
-
+    std::string m_network;
     void ReadConfigStream(std::istream &stream);
 
 public:
+    /**
+     * Select the network in use
+     */
+    void SelectConfigNetwork(const std::string &network);
+
     void ParseParameters(int argc, const char *const argv[]);
     void ReadConfigFile(const std::string &confPath);
 