Bitcoin-ABC/bitcoin-abc

commit 76c64df03947f6ffde8e2f3f214dda3eb565f139

refactor TxToJSON() and ScriptPubKeyToJSON()
Summary:
This PR removes almost all of the TxToJSON() and ScriptPubKeyToJSON() code from bitcoin-rpc and places it in bitcoin-common, removing about 80 lines of duplicate code. The only part that can't be moved into bitcoin-common is parsing block contextual information (confirmations and blocktime), which are not available to bitcoin-common code.

Backports Core PR8824 https://github.com/bitcoin/bitcoin/pull/8824

Completes T500.

Test Plan: make check

Reviewers: jasonbcox, deadalnix, O1 Bitcoin ABC, #bitcoin_abc, Fabien

Reviewed By: deadalnix, O1 Bitcoin ABC, #bitcoin_abc

Subscribers: teamcity

Differential Revision: https://reviews.bitcoinabc.org/D2499

src/rpc/rawtransaction.cpp
@@ -36,93 +36,14 @@
 
 #include <univalue.h>
 
-void ScriptPubKeyToJSON(const Config &config, const CScript &scriptPubKey,
-                        UniValue &out, bool fIncludeHex) {
-    txnouttype type;
-    std::vector<CTxDestination> addresses;
-    int nRequired;
-
-    out.pushKV("asm", ScriptToAsmStr(scriptPubKey));
-    if (fIncludeHex) {
-        out.pushKV("hex", HexStr(scriptPubKey.begin(), scriptPubKey.end()));
-    }
-
-    if (!ExtractDestinations(scriptPubKey, type, addresses, nRequired)) {
-        out.pushKV("type", GetTxnOutputType(type));
-        return;
-    }
-
-    out.pushKV("reqSigs", nRequired);
-    out.pushKV("type", GetTxnOutputType(type));
-
-    UniValue a(UniValue::VARR);
-    for (const CTxDestination &addr : addresses) {
-        a.push_back(EncodeDestination(addr));
-    }
-
-    out.pushKV("addresses", a);
-}
-
-void TxToJSON(const Config &config, const CTransaction &tx,
-              const uint256 hashBlock, UniValue &entry) {
-    entry.pushKV("txid", tx.GetId().GetHex());
-    entry.pushKV("hash", tx.GetHash().GetHex());
-    entry.pushKV("size",
-                 int(::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION)));
-    entry.pushKV("version", tx.nVersion);
-    entry.pushKV("locktime", int64_t(tx.nLockTime));
-
-    UniValue vin(UniValue::VARR);
-    for (unsigned int i = 0; i < tx.vin.size(); i++) {
-        const CTxIn &txin = tx.vin[i];
-        UniValue in(UniValue::VOBJ);
-        if (tx.IsCoinBase()) {
-            in.pushKV("coinbase",
-                      HexStr(txin.scriptSig.begin(), txin.scriptSig.end()));
-        } else {
-            in.pushKV("txid", txin.prevout.GetTxId().GetHex());
-            in.pushKV("vout", int64_t(txin.prevout.GetN()));
-            UniValue o(UniValue::VOBJ);
-            o.pushKV("asm", ScriptToAsmStr(txin.scriptSig, true));
-            o.pushKV("hex",
-                     HexStr(txin.scriptSig.begin(), txin.scriptSig.end()));
-            in.pushKV("scriptSig", o);
-        }
-
-        in.pushKV("sequence", int64_t(txin.nSequence));
-        vin.push_back(in);
-    }
-
-    entry.pushKV("vin", vin);
-    UniValue vout(UniValue::VARR);
-    for (unsigned int i = 0; i < tx.vout.size(); i++) {
-        const CTxOut &txout = tx.vout[i];
-        UniValue out(UniValue::VOBJ);
-        out.pushKV("value", ValueFromAmount(txout.nValue));
-        out.pushKV("n", int64_t(i));
-        UniValue o(UniValue::VOBJ);
-        ScriptPubKeyToJSON(config, txout.scriptPubKey, o, true);
-        out.pushKV("scriptPubKey", o);
-        vout.push_back(out);
-    }
-
-    entry.pushKV("vout", vout);
-
-    if (!hashBlock.IsNull()) {
-        entry.pushKV("blockhash", hashBlock.GetHex());
-        BlockMap::iterator mi = mapBlockIndex.find(hashBlock);
-        if (mi != mapBlockIndex.end() && (*mi).second) {
-            CBlockIndex *pindex = (*mi).second;
-            if (chainActive.Contains(pindex)) {
-                entry.pushKV("confirmations",
-                             1 + chainActive.Height() - pindex->nHeight);
-                entry.pushKV("time", pindex->GetBlockTime());
-                entry.pushKV("blocktime", pindex->GetBlockTime());
-            } else {
-                entry.pushKV("confirmations", 0);
-            }
-        }
-    }
+void TxToJSON(const CTransaction &tx, const uint256 hashBlock,
+              UniValue &entry) {
+    // Call into TxToUniv() in bitcoin-common to decode the transaction hex.
+    //
+    // Blockchain contextual information (confirmations and blocktime) is not
+    // available to code in bitcoin-common, so we query them here and push the
+    // data into the returned UniValue.
+    TxToUniv(tx, uint256(), entry);
 }
 
 static UniValue getrawtransaction(const Config &config,
@@ -252,7 +173,7 @@ static UniValue getrawtransaction(const Config &config,
 
     UniValue result(UniValue::VOBJ);
     result.pushKV("hex", strHex);
-    TxToJSON(config, *tx, hashBlock, result);
+    TxToUniv(*tx, hashBlock, result);
     return result;
 }
 
@@ -649,7 +570,7 @@ static UniValue decoderawtransaction(const Config &config,
     }
 
     UniValue result(UniValue::VOBJ);
-    TxToJSON(config, CTransaction(std::move(mtx)), uint256(), result);
+    TxToUniv(CTransaction(std::move(mtx)), uint256(), result);
 
     return result;
 }
@@ -693,7 +614,7 @@ static UniValue decodescript(const Config &config,
         // Empty scripts are valid.
     }
 
-    ScriptPubKeyToJSON(config, script, r, false);
+    ScriptPubKeyToUniv(script, r, false);
 
     UniValue type;
     type = find_value(r, "type");

src/core_write.cpp
@@ -189,6 +189,8 @@ void TxToUniv(const CTransaction &tx, const uint256 &hashBlock,
     entry.pushKV("txid", tx.GetId().GetHex());
     entry.pushKV("hash", tx.GetHash().GetHex());
     entry.pushKV("version", tx.nVersion);
+    entry.pushKV("size",
+                 (int)::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION));
     entry.pushKV("locktime", (int64_t)tx.nLockTime);
 
     UniValue vin(UniValue::VARR);

src/rest.cpp
@@ -6,6 +6,7 @@
 #include "chain.h"
 #include "chainparams.h"
 #include "config.h"
+#include "core_io.h"
 #include "httpserver.h"
 #include "primitives/block.h"
 #include "primitives/transaction.h"
@@ -430,7 +431,7 @@ static bool rest_tx(Config &config, HTTPRequest *req,
 
         case RF_JSON: {
             UniValue objTx(UniValue::VOBJ);
-            TxToJSON(config, *tx, hashBlock, objTx);
+            TxToUniv(*tx, hashBlock, objTx);
             std::string strJSON = objTx.write() + "\n";
             req->WriteHeader("Content-Type", "application/json");
             req->WriteReply(HTTP_OK, strJSON);
@@ -645,7 +646,7 @@ static bool rest_getutxos(Config &config, HTTPRequest *req,
 
                 // include the script in a json output
                 UniValue o(UniValue::VOBJ);
-                ScriptPubKeyToJSON(config, coin.out.scriptPubKey, o, true);
+                ScriptPubKeyToUniv(coin.out.scriptPubKey, o, true);
                 utxo.pushKV("scriptPubKey", o);
                 utxos.push_back(utxo);
             }

src/rpc/blockchain.cpp
@@ -12,11 +12,11 @@
 #include "coins.h"
 #include "config.h"
 #include "consensus/validation.h"
+#include "core_io.h"
 #include "hash.h"
 #include "policy/policy.h"
 #include "primitives/transaction.h"
 #include "rpc/server.h"
-#include "rpc/tojson.h"
 #include "streams.h"
 #include "sync.h"
 #include "txdb.h"
@@ -118,7 +118,7 @@ UniValue blockToJSON(const Config &config, const CBlock &block,
     for (const auto &tx : block.vtx) {
         if (txDetails) {
             UniValue objTx(UniValue::VOBJ);
-            TxToJSON(config, *tx, uint256(), objTx);
+            TxToUniv(*tx, uint256(), objTx);
             txs.push_back(objTx);
         } else {
             txs.push_back(tx->GetId().GetHex());
@@ -1147,7 +1147,7 @@ UniValue gettxout(const Config &config, const JSONRPCRequest &request) {
     }
     ret.pushKV("value", ValueFromAmount(coin.GetTxOut().nValue));
     UniValue o(UniValue::VOBJ);
-    ScriptPubKeyToJSON(config, coin.GetTxOut().scriptPubKey, o, true);
+    ScriptPubKeyToUniv(coin.GetTxOut().scriptPubKey, o, true);
     ret.pushKV("scriptPubKey", o);
     ret.pushKV("coinbase", coin.IsCoinBase());
 

src/rpc/tojson.h
@@ -11,8 +11,6 @@
 
 class CScript;
 
-void ScriptPubKeyToJSON(const Config &config, const CScript &scriptPubKey,
-                        UniValue &out, bool fIncludeHex);
 void TxToJSON(const Config &config, const CTransaction &tx,
               const uint256 hashBlock, UniValue &entry);
 UniValue blockToJSON(const Config &config, const CBlock &block,

commit 2ba3ac5b795d4e58b3a7c215ad30d05f6ff6a4af

Fix regression in getrawtransaction
Summary:
Fixes a regression from D2499 where `confirmations`, `time`, and `blocktime` are no longer present in getrawtransaction.  Also add sanity checks to rpc_rawtransaction.py to ensure these verbose-only values are included in the future.

This fixes this issue reported on Github: https://github.com/Bitcoin-ABC/bitcoin-abc/issues/303

Test Plan:
```
ninja check
test_runner.py
```

Reviewers: deadalnix, Fabien, #bitcoin_abc

Reviewed By: deadalnix, #bitcoin_abc

Subscribers: teamcity, schancel

Differential Revision: https://reviews.bitcoinabc.org/D2546

src/rpc/rawtransaction.cpp
@@ -44,6 +44,22 @@ void TxToJSON(const CTransaction &tx, const uint256 hashBlock,
     // available to code in bitcoin-common, so we query them here and push the
     // data into the returned UniValue.
     TxToUniv(tx, uint256(), entry);
+
+    if (!hashBlock.IsNull()) {
+        entry.pushKV("blockhash", hashBlock.GetHex());
+        BlockMap::iterator mi = mapBlockIndex.find(hashBlock);
+        if (mi != mapBlockIndex.end() && (*mi).second) {
+            CBlockIndex *pindex = (*mi).second;
+            if (chainActive.Contains(pindex)) {
+                entry.pushKV("confirmations",
+                             1 + chainActive.Height() - pindex->nHeight);
+                entry.pushKV("time", pindex->GetBlockTime());
+                entry.pushKV("blocktime", pindex->GetBlockTime());
+            } else {
+                entry.pushKV("confirmations", 0);
+            }
+        }
+    }
 }
 
 static UniValue getrawtransaction(const Config &config,
@@ -173,7 +189,7 @@ static UniValue getrawtransaction(const Config &config,
 
     UniValue result(UniValue::VOBJ);
     result.pushKV("hex", strHex);
-    TxToUniv(*tx, hashBlock, result);
+    TxToJSON(*tx, hashBlock, result);
     return result;
 }
 

test/functional/rpc_rawtransaction.py
@@ -141,6 +141,8 @@ class RawTransactionsTest(BitcoinTestFramework):
         assert_equal(self.nodes[0].getbalance(), bal + Decimal(
             '50.00000000') + Decimal('2.19000000'))  # block reward + tx
 
+        rawTxBlock = self.nodes[0].getblock(self.nodes[0].getbestblockhash())
+
         # 2of2 test for combining transactions
         bal = self.nodes[2].getbalance()
         addr1 = self.nodes[1].getnewaddress()
@@ -233,6 +235,21 @@ class RawTransactionsTest(BitcoinTestFramework):
         # 8. invalid parameters - supply txid and empty dict
         assert_raises_rpc_error(
             -3, "Invalid type", self.nodes[0].getrawtransaction, txHash, {})
+
+        # Sanity checks on verbose getrawtransaction output
+        rawTxOutput = self.nodes[0].getrawtransaction(txHash, True)
+        assert_equal(rawTxOutput["hex"], rawTxSigned["hex"])
+        assert_equal(rawTxOutput["txid"], txHash)
+        assert_equal(rawTxOutput["hash"], txHash)
+        assert_greater_than(rawTxOutput["size"], 300)
+        assert_equal(rawTxOutput["version"], 0x02)
+        assert_equal(rawTxOutput["locktime"], 0)
+        assert_equal(len(rawTxOutput["vin"]), 1)
+        assert_equal(len(rawTxOutput["vout"]), 1)
+        assert_equal(rawTxOutput["blockhash"], rawTxBlock["hash"])
+        assert_equal(rawTxOutput["confirmations"], 3)
+        assert_equal(rawTxOutput["time"], rawTxBlock["time"])
+        assert_equal(rawTxOutput["blocktime"], rawTxBlock["time"])
 
         inputs = [
             {'txid': "1d1d4e24ed99057e84c3f80fd8fbec79ed9e1acee37da269356ecea000000000", 'sequence': 1000}]