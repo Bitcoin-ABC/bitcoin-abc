8a09e705ccb35dbef5ff6a00c5923f507a4ee26e
Amaury Séchet <deadalnix@gmail.com>
2019-02-14 15:34:54 +0100

    Various nits in key.{h|cpp}
    Summary: Format comments
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2551

--- src/key.h
+++ src/key.h
@@ -183,12 +183,16 @@ struct CExtKey {
     }
 };
 
-/** Initialize the elliptic curve support. May not be called twice without
- * calling ECC_Stop first. */
+/**
+ * Initialize the elliptic curve support. May not be called twice without
+ * calling ECC_Stop first.
+ */
 void ECC_Start(void);
 
-/** Deinitialize the elliptic curve support. No-op if ECC_Start wasn't called
- * first. */
+/**
+ * Deinitialize the elliptic curve support. No-op if ECC_Start wasn't called
+ * first.
+ */
 void ECC_Stop(void);
 
 /** Check that required EC support is available at runtime. */
