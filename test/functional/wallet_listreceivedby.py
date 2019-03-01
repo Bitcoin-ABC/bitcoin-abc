95fc067d27cfb7ae0b9e822ea991304d705ffa92
Fabien <fabcien@gmail.com>
2019-03-01 12:21:34 +0000

    Remove enable_mocktime from wallet_listreceivedby
    Summary:
    The `enable_mocktime()` call has been resurrected in D2465.
    However this line is not necessary and should have been removed with
    D1983 because it is part of core PR11055.
    
    Test Plan:
      ./test/functional/test_runner.py wallet_listreceivedby
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2635

--- test/functional/wallet_listreceivedby.py
+++ test/functional/wallet_listreceivedby.py
@@ -17,7 +17,6 @@ from test_framework.util import (
 class ReceivedByTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
-        self.enable_mocktime()
 
     def run_test(self):
         # Generate block to get out of IBD
