32f04379b4e558eb52a9ef27391188394985ebed
practicalswift <practicalswift@users.noreply.github.com>
2019-02-28 16:36:07 -0800

    Remove redundant check (!ecc is always true)
    Summary:
    Partial backport of Core PR10898 (commit 7466991)
    https://github.com/bitcoin/bitcoin/pull/10898/commits/74669916709e5429990df120c6ba84ac60791b30
    
    Test Plan: ninja check
    
    Reviewers: deadalnix, Fabien, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Subscribers: teamcity, schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2621

--- src/bitcoin-tx.cpp
+++ src/bitcoin-tx.cpp
@@ -715,24 +715,17 @@ static void MutateTx(CMutableTransaction &tx, const std::string &command,
     } else if (command == "outaddr") {
         MutateTxAddOutAddr(tx, commandVal, chainParams);
     } else if (command == "outpubkey") {
-        if (!ecc) {
-            ecc.reset(new Secp256k1Init());
-        }
+        ecc.reset(new Secp256k1Init());
         MutateTxAddOutPubKey(tx, commandVal);
     } else if (command == "outmultisig") {
-        if (!ecc) {
-            ecc.reset(new Secp256k1Init());
-        }
+        ecc.reset(new Secp256k1Init());
         MutateTxAddOutMultiSig(tx, commandVal);
     } else if (command == "outscript") {
         MutateTxAddOutScript(tx, commandVal);
     } else if (command == "outdata") {
         MutateTxAddOutData(tx, commandVal);
     } else if (command == "sign") {
-        if (!ecc) {
-            ecc.reset(new Secp256k1Init());
-        }
-
+        ecc.reset(new Secp256k1Init());
         MutateTxSign(tx, commandVal);
     } else if (command == "load") {
         RegisterLoad(commandVal);
