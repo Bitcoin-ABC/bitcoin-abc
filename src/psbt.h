aa96a2979b5500fd4b0ce3e5186b787135354c3c
Glenn Willen <gwillen@nerdnet.org>
2019-03-12 16:08:35 -0700

    [backport#15508] Add documentation of struct PSBTAnalysis et al
    Summary:
    https://github.com/bitcoin/bitcoin/pull/15508/commits/892eff05f115c0b002d0e0b6ffc3ab418480d25c
    
    ---
    
    Depends on D6079
    
    This completes the backport of Core [[https://github.com/bitcoin/bitcoin/pull/15508 | PR15508]]
    
    Test Plan: none
    
    Reviewers: #bitcoin_abc, nakihito
    
    Reviewed By: nakihito
    
    Differential Revision: https://reviews.bitcoinabc.org/D6080

--- src/psbt.h
+++ src/psbt.h
@@ -489,21 +489,38 @@ enum class PSBTRole {
     EXTRACTOR,
 };
 
+/**
+ * Holds an analysis of one input from a PSBT
+ */
 struct PSBTInputAnalysis {
+    //! Whether we have UTXO information for this input
     bool has_utxo;
+    //! Whether the input has all required information including signatures
     bool is_final;
+    //! Which of the BIP 174 roles needs to handle this input next
     PSBTRole next;
 
+    //! Pubkeys whose BIP32 derivation path is missing
     std::vector<CKeyID> missing_pubkeys;
+    //! Pubkeys whose signatures are missing
     std::vector<CKeyID> missing_sigs;
+    //! Hash160 of redeem script, if missing
     uint160 missing_redeem_script;
 };
 
+/**
+ * Holds the results of AnalyzePSBT (miscellaneous information about a PSBT)
+ */
 struct PSBTAnalysis {
+    //! Estimated weight of the transaction
     Optional<size_t> estimated_vsize;
+    //! Estimated feerate (fee / weight) of the transaction
     Optional<CFeeRate> estimated_feerate;
+    //! Amount of fee being paid by the transaction
     Optional<Amount> fee;
+    //! More information about the individual inputs of the transaction
     std::vector<PSBTInputAnalysis> inputs;
+    //! Which of the BIP 174 roles needs to handle the transaction next
     PSBTRole next;
 };
 
