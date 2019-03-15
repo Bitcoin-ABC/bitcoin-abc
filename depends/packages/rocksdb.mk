e3ff4d805f93a27034b88e6caaaec6387606e617
Proteus <pro@proteanx.com>
2019-03-14 23:26:30 -0400

    rocksdb mk fix

--- depends/packages/rocksdb.mk
+++ depends/packages/rocksdb.mk
@@ -1,4 +1,4 @@
-package=librocksdb
+package=rocksdb
 $(package)_version=5.18.3
 $(package)_download_path=https://github.com/facebook/rocksdb/archive/
 $(package)_file_name=v$($(package)_version).tar.gz
@@ -8,18 +8,17 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_set_vars
-  $(package)_config_opts_linux=--with-pic
 endef
 
 define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) static_lib
+  $(MAKE) shared_lib
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) INSTALL_PATH=$(host_prefix) install
 endef
 
 define $(package)_postprocess_cmds
