44f00a8e46d602648f5eb1a171f517b84239e50d
Proteus <pro@proteanx.com>
2019-03-14 20:15:02 -0400

    Add rocksdb.mk
    (in case we decide to use later)

---
+++ depends/packages/librocksdb.mk
@@ -0,0 +1,26 @@
+package=librocksdb
+$(package)_version=5.18.3
+$(package)_download_path=https://github.com/facebook/rocksdb/archive/
+$(package)_file_name=v$($(package)_version).tar.gz
+$(package)_sha256_hash=7fb6738263d3f2b360d7468cf2ebe333f3109f3ba1ff80115abd145d75287254
+
+define $(package)_preprocess_cmds
+endef
+
+define $(package)_set_vars
+  $(package)_config_opts_linux=--with-pic
+endef
+
+define $(package)_config_cmds
+endef
+
+define $(package)_build_cmds
+  $(MAKE) static_lib
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+endef
