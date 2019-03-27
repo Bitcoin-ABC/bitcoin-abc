b1b46a3606a158572aa1aa2deca8a2542e7308ec
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-03-27 17:00:08 +0000

    [backport#15652] wallet: Update transactions with current mempool after load
    Summary:
    https://github.com/bitcoin/bitcoin/pull/15652/commits/2ebf650b2eb7a078ab60c8c4d5c726823686f549
    
    ---
    
    Depends on D6246
    
    Partial backport of Core [[https://github.com/bitcoin/bitcoin/pull/15652 | PR15652]]
    
    Test Plan:
      ninja check
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D6247

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4843,6 +4843,9 @@ void CWallet::postInitProcess() {
     // Add wallet transactions that aren't already in a block to mempool.
     // Do this here as mempool requires genesis block to be loaded.
     ReacceptWalletTransactions(*locked_chain);
+
+    // Update wallet transactions with current mempool transactions.
+    chain().requestMempoolTransactions(*this);
 }
 
 bool CWallet::BackupWallet(const std::string &strDest) {
