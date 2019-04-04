646b7a6ae44dbd6b2638b0ad7b15cba25ce6470c
Fabien <fabcien@gmail.com>
2019-04-04 15:48:18 +0000

    [CI] Add an option to build and run unit tests with debug enabled
    Summary:
    Calling the script with `ENABLE_DEBUG=1` will build and run the unit
    tests with debug enabled.
    Functional tests are known failing under debug, they will be skipped.
    
    Test Plan:
      ENABLE_DEBUG=1 ./contrib/teamcity/build.sh
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: schancel
    
    Differential Revision: https://reviews.bitcoinabc.org/D2776

--- contrib/teamcity/build.sh
+++ contrib/teamcity/build.sh
@@ -30,6 +30,14 @@ if [[ ! -z "${DISABLE_WALLET}" ]]; then
 	CONFIGURE_FLAGS+=("--disable-wallet")
 fi
 
+# Default to nothing
+: ${ENABLE_DEBUG:=}
+
+if [[ ! -z "${ENABLE_DEBUG}" ]]; then
+	echo "*** Building with debug"
+	CONFIGURE_FLAGS+=("--enable-debug")
+fi
+
 ../configure "${CONFIGURE_FLAGS[@]}"
 
 # Run build
@@ -45,16 +53,21 @@ if [[ -z "${DISABLE_TESTS}" ]]; then
 	# Run unit tests
 	make -j ${THREADS} check
 
-	mkdir -p output/
-	BRANCH=$(git rev-parse --abbrev-ref HEAD)
-	if [[ ! -z "${DISABLE_WALLET}" ]]; then
-		echo "Skipping rpc testing due to disabled wallet functionality."
-	elif [[ "${BRANCH}" == "master" ]]; then
-		./test/functional/test_runner.py --cutoff=600 --tmpdirprefix=output
-		./test/functional/test_runner.py --cutoff=600 --tmpdirprefix=output --with-greatwallactivation
+	# FIXME Remove when the functional tests are running with debug enabled
+	if [[ -z "${ENABLE_DEBUG}" ]]; then
+		mkdir -p output/
+		BRANCH=$(git rev-parse --abbrev-ref HEAD)
+		if [[ ! -z "${DISABLE_WALLET}" ]]; then
+			echo "Skipping rpc testing due to disabled wallet functionality."
+		elif [[ "${BRANCH}" == "master" ]]; then
+			./test/functional/test_runner.py --cutoff=600 --tmpdirprefix=output
+			./test/functional/test_runner.py --cutoff=600 --tmpdirprefix=output --with-greatwallactivation
+		else
+			./test/functional/test_runner.py --tmpdirprefix=output
+			./test/functional/test_runner.py --tmpdirprefix=output --with-greatwallactivation
+		fi
 	else
-		./test/functional/test_runner.py --tmpdirprefix=output
-		./test/functional/test_runner.py --tmpdirprefix=output --with-greatwallactivation
+		echo "*** Functional tests have been skipped"
 	fi
 else
 	echo "*** Tests have been skipped"
