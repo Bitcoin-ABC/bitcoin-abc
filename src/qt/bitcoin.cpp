Bitcoin-ABC/bitcoin-abc

commit 256ef1e899be702aabe6f44cf431d1f5a4e45273

Use static_cast instead of C-style casts for non-fundamental types
Summary:
A C-style cast is equivalent to try casting in the following order:

1. const_cast(...)
2. static_cast(...)
3. const_cast(static_cast(...))
4. reinterpret_cast(...)
5. const_cast(reinterpret_cast(...))

By using static_cast<T>(...) explicitly we avoid the possibility
of an unintentional and dangerous reinterpret_cast. Furthermore
static_cast<T>(...) allows for easier grepping of casts.  Please enter the commit message for your changes. Lines starting

Backport of Core PR10498
https://github.com/bitcoin/bitcoin/pull/10498

Completes T555

Test Plan:
make check
test_runner.py

Reviewers: jasonbcox, deadalnix, Fabien, O1 Bitcoin ABC, #bitcoin_abc

Reviewed By: jasonbcox, O1 Bitcoin ABC, #bitcoin_abc

Subscribers: schancel

Differential Revision: https://reviews.bitcoinabc.org/D2765

src/qt/bitcoin.cpp
@@ -793,7 +793,7 @@ int main(int argc, char *argv[]) {
         WinShutdownMonitor::registerShutdownBlockReason(
             QObject::tr("%1 didn't yet exit safely...")
                 .arg(QObject::tr(PACKAGE_NAME)),
-            (HWND)app.getMainWinId());
+            static_cast<HWND>(app.getMainWinId()));
 #endif
         app.exec();
         app.requestShutdown(config);

src/qt/winshutdownmonitor.cpp
@@ -56,8 +56,9 @@ bool WinShutdownMonitor::nativeEventFilter(const QByteArray &eventType,
 void WinShutdownMonitor::registerShutdownBlockReason(const QString &strReason,
                                                      const HWND &mainWinId) {
     typedef BOOL(WINAPI * PSHUTDOWNBRCREATE)(HWND, LPCWSTR);
-    PSHUTDOWNBRCREATE shutdownBRCreate = (PSHUTDOWNBRCREATE)GetProcAddress(
-        GetModuleHandleA("User32.dll"), "ShutdownBlockReasonCreate");
+    PSHUTDOWNBRCREATE shutdownBRCreate =
+        static_cast<PSHUTDOWNBRCREATE>(GetProcAddress(
+            GetModuleHandleA("User32.dll"), "ShutdownBlockReasonCreate"));
     if (shutdownBRCreate == nullptr) {
         qWarning() << "registerShutdownBlockReason: GetProcAddress for "
                       "ShutdownBlockReasonCreate failed";

src/addrman.h
@@ -55,7 +55,7 @@ public:
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream &s, Operation ser_action) {
-        READWRITE(*(CAddress *)this);
+        READWRITE(*static_cast<CAddress *>(this));
         READWRITE(source);
         READWRITE(nLastSuccess);
         READWRITE(nAttempts);

src/core_read.cpp
@@ -31,16 +31,16 @@ CScript ParseScript(const std::string &s) {
                 continue;
             }
 
-            const char *name = GetOpName((opcodetype)op);
+            const char *name = GetOpName(static_cast<opcodetype>(op));
             if (strcmp(name, "OP_UNKNOWN") == 0) {
                 continue;
             }
 
             std::string strName(name);
-            mapOpNames[strName] = (opcodetype)op;
+            mapOpNames[strName] = static_cast<opcodetype>(op);
             // Convenience: OP_ADD and just ADD are both recognized:
             boost::algorithm::replace_first(strName, "OP_", "");
-            mapOpNames[strName] = (opcodetype)op;
+            mapOpNames[strName] = static_cast<opcodetype>(op);
         }
     }
 

src/httpserver.cpp
@@ -513,7 +513,7 @@ struct event_base *EventBase() {
 
 static void httpevent_callback_fn(evutil_socket_t, short, void *data) {
     // Static handler: simply call inner handler
-    HTTPEvent *self = ((HTTPEvent *)data);
+    HTTPEvent *self = static_cast<HTTPEvent *>(data);
     self->handler();
     if (self->deleteWhenTriggered) delete self;
 }

src/net.cpp
@@ -2109,7 +2109,8 @@ void CConnman::OpenNetworkConnection(const CAddress &addrConnect,
         return;
     }
     if (!pszDest) {
-        if (IsLocal(addrConnect) || FindNode((CNetAddr)addrConnect) ||
+        if (IsLocal(addrConnect) ||
+            FindNode(static_cast<CNetAddr>(addrConnect)) ||
             IsBanned(addrConnect) || FindNode(addrConnect.ToStringIPPort())) {
             return;
         }

src/netaddress.cpp
@@ -513,7 +513,8 @@ unsigned short CService::GetPort() const {
 }
 
 bool operator==(const CService &a, const CService &b) {
-    return (CNetAddr)a == (CNetAddr)b && a.port == b.port;
+    return static_cast<CNetAddr>(a) == static_cast<CNetAddr>(b) &&
+           a.port == b.port;
 }
 
 bool operator<(const CService &a, const CService &b) {

src/primitives/block.h
@@ -70,14 +70,14 @@ public:
 
     CBlock(const CBlockHeader &header) {
         SetNull();
-        *((CBlockHeader *)this) = header;
+        *(static_cast<CBlockHeader *>(this)) = header;
     }
 
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream &s, Operation ser_action) {
-        READWRITE(*(CBlockHeader *)this);
+        READWRITE(*static_cast<CBlockHeader *>(this));
         READWRITE(vtx);
     }
 

commit 193d2b9c296a05f64db546093df127f463b36490

Fix Windows build errors introduced in D2765
Summary:
See core issue #12386:
https://github.com/bitcoin/bitcoin/issues/12386

Backport of core PR12416

Test Plan:
  make check
Build the windows binaries

Reviewers: #bitcoin_abc, deadalnix, jasonbcox

Reviewed By: #bitcoin_abc, jasonbcox

Subscribers: schancel

Differential Revision: https://reviews.bitcoinabc.org/D2779

src/qt/bitcoin.cpp
@@ -793,7 +793,7 @@ int main(int argc, char *argv[]) {
         WinShutdownMonitor::registerShutdownBlockReason(
             QObject::tr("%1 didn't yet exit safely...")
                 .arg(QObject::tr(PACKAGE_NAME)),
-            static_cast<HWND>(app.getMainWinId()));
+            (HWND)app.getMainWinId());
 #endif
         app.exec();
         app.requestShutdown(config);

src/qt/winshutdownmonitor.cpp
@@ -56,9 +56,8 @@ bool WinShutdownMonitor::nativeEventFilter(const QByteArray &eventType,
 void WinShutdownMonitor::registerShutdownBlockReason(const QString &strReason,
                                                      const HWND &mainWinId) {
     typedef BOOL(WINAPI * PSHUTDOWNBRCREATE)(HWND, LPCWSTR);
-    PSHUTDOWNBRCREATE shutdownBRCreate =
-        static_cast<PSHUTDOWNBRCREATE>(GetProcAddress(
-            GetModuleHandleA("User32.dll"), "ShutdownBlockReasonCreate"));
+    PSHUTDOWNBRCREATE shutdownBRCreate = (PSHUTDOWNBRCREATE)GetProcAddress(
+        GetModuleHandleA("User32.dll"), "ShutdownBlockReasonCreate");
     if (shutdownBRCreate == nullptr) {
         qWarning() << "registerShutdownBlockReason: GetProcAddress for "
                       "ShutdownBlockReasonCreate failed";