11faedfb873a71bb9e56f837af48414e69db0e6f
Emil <emu@emuadmin.com>
2019-04-17 17:32:05 +0000

    Filter IPv6 by ASN
    Summary:
    Backport of Core PR15840
    https://github.com/bitcoin/bitcoin/pull/15840/files
    
    Test Plan: Run `makeseeds.py` according to `contrib/seeds/README.md`
    
    Reviewers: #bitcoin_abc, deadalnix, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D4779

--- contrib/seeds/makeseeds.py
+++ contrib/seeds/makeseeds.py
@@ -130,19 +130,36 @@ def filtermultiport(ips):
 
 def filterbyasn(ips, max_per_asn, max_total):
     # Sift out ips by type
-    ips_ipv4 = [ip for ip in ips if ip['net'] == 'ipv4']
-    ips_ipv6 = [ip for ip in ips if ip['net'] == 'ipv6']
+    ips_ipv46 = [ip for ip in ips if ip['net'] in ['ipv4', 'ipv6']]
     ips_onion = [ip for ip in ips if ip['net'] == 'onion']
 
-    # Filter IPv4 by ASN
+    # Filter IPv46 by ASN
     result = []
     asn_count = {}
-    for ip in ips_ipv4:
+    for ip in ips_ipv46:
         if len(result) == max_total:
             break
         try:
-            asn = int([x.to_text()
-                       for x in dns.resolver.query('.'.join(reversed(ip['ip'].split('.'))) + '.origin.asn.cymru.com', 'TXT').response.answer][0].split('\"')[1].split(' ')[0])
+            if ip['net'] == 'ipv4':
+                ipaddr = ip['ip']
+                prefix = '.origin'
+            else:
+                # http://www.team-cymru.com/IP-ASN-mapping.html
+                # 2001:4860:b002:23::68
+                res = str()
+                # pick the first 4 nibbles
+                for nb in ip['ip'].split(':')[:4]:
+                    # right padded with '0'
+                    for c in nb.zfill(4):
+                        # 2001 4860 b002 0023
+                        res += c + '.'
+                # 2.0.0.1.4.8.6.0.b.0.0.2.0.0.2.3
+                ipaddr = res.rstrip('.')
+                prefix = '.origin6'
+
+            asn = int([x.to_text() for x in dns.resolver.query('.'.join(
+                       reversed(ipaddr.split('.'))) + prefix + '.asn.cymru.com',
+                'TXT').response.answer][0].split('\"')[1].split(' ')[0])
             if asn not in asn_count:
                 asn_count[asn] = 0
             if asn_count[asn] == max_per_asn:
@@ -153,10 +170,7 @@ def filterbyasn(ips, max_per_asn, max_total):
             sys.stderr.write(
                 'ERR: Could not resolve ASN for "' + ip['ip'] + '"\n')
 
-    # TODO: filter IPv6 by ASN
-
-    # Add back non-IPv4
-    result.extend(ips_ipv6)
+    # Add back Onions
     result.extend(ips_onion)
     return result
 
