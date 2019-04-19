bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Remove direct bitcoin calls from qt/utilitydialog.cpp

src/qt/bitcoingui.cpp
@@ -153,7 +153,7 @@ BitcoinGUI::BitcoinGUI(ipc::Node& _ipcNode, const PlatformStyle *_platformStyle,
 #endif
 
     rpcConsole = new RPCConsole(_platformStyle, 0);
-    helpMessageDialog = new HelpMessageDialog(this, false);
+    helpMessageDialog = new HelpMessageDialog(ipcNode, this, false);
 #ifdef ENABLE_WALLET
     if(enableWallet)
     {
@@ -646,7 +646,7 @@ void BitcoinGUI::aboutClicked()
     if(!clientModel)
         return;
 
-    HelpMessageDialog dlg(this, true);
+    HelpMessageDialog dlg(ipcNode, this, true);
     dlg.exec();
 }
 

Bitcoin-ABC/bitcoin-abc

Summary:
Partial backport for Core PR 10244
https://github.com/bitcoin/bitcoin/pull/10244/commits/c2f672fb1960399389dea9cdd8f76d7156c2c88b
Progress towards T417
Depends on D2815, D2826
Test Plan:
```
make check
ninja check
bitcoin-qt # sanity checking
bitcoin-qt -help
bitcoin-qt -version
Reviewers: deadalnix, Fabien, #bitcoin_abc
Reviewed By: deadalnix, Fabien, #bitcoin_abc
Differential Revision: https://reviews.bitcoinabc.org/D2816

src/qt/bitcoingui.cpp
@@ -103,7 +103,7 @@ BitcoinGUI::BitcoinGUI(interfaces::Node &node, const Config *configIn,
     setWindowTitle(windowTitle);
 
     rpcConsole = new RPCConsole(_platformStyle, 0);
-    helpMessageDialog = new HelpMessageDialog(this, false);
+    helpMessageDialog = new HelpMessageDialog(node, this, false);
 #ifdef ENABLE_WALLET
     if (enableWallet) {
         /** Create wallet frame and make it the central widget */
@@ -692,7 +692,7 @@ void BitcoinGUI::optionsClicked() {
 void BitcoinGUI::aboutClicked() {
     if (!clientModel) return;
 
-    HelpMessageDialog dlg(this, true);
+    HelpMessageDialog dlg(m_node, this, true);
     dlg.exec();
 }
 