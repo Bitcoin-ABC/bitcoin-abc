eed263423f869f16895de759262c648bccffccd3
MarcoFalke <falke.marco@gmail.com>
2019-04-19 10:20:14 -0400

    [backport#15853] wallet: Remove unused import checkpoints.h
    Summary:
    fadf7d1390 wallet: Remove unused import checkpoints.h (MarcoFalke)
    
    Pull request description:
    
      Yet another silent merge conflict. This one was caused by unsorted includes.
    
    ---
    
    https://github.com/bitcoin/bitcoin/pull/15853/commits/fadf7d13905d76ef4d2d8b9b96a6b9269f78fbba
    
    Backport of Core PR15853
    
    Test Plan:
      ninja
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D7029

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2016 The Bitcoin Core developers
+// Copyright (c) 2009-2019 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -7,7 +7,6 @@
 
 #include <chain.h>
 #include <chainparams.h>
-#include <checkpoints.h>
 #include <config.h>
 #include <consensus/consensus.h>
 #include <consensus/validation.h>
