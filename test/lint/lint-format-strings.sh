93e2cadf33ffa5656b30606a036559d266ade494
Fabien <fabcien@gmail.com>
2019-04-26 15:15:25 +0000

    [LINTER] Move the string formatting function list to the python script
    Summary:
    This change the call stack when running the `lint-format-strings.sh`
    linter. The python script is now called once per file instead of running
    once per function per file. The python script does loop internally
    resulting in a large performance improvement (about 80% time cut on my
    machine).
    
    Depends on D2881.
    
    Test Plan:
      arc lint --everything
    Should return no error
    
    Comment one of the false positive at the beginning of the
    `lint-format-strings.py` file.
    Add a comment in the file from the commented false positive.
      arc lint
    The false positive should be catched by the linter.
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D2882

--- test/lint/lint-format-strings.sh
+++ test/lint/lint-format-strings.sh
@@ -10,29 +10,11 @@
 
 export LC_ALL=C
 
-FUNCTION_NAMES_AND_NUMBER_OF_LEADING_ARGUMENTS=(
-    FatalError,0
-    fprintf,1
-    LogConnectFailure,1
-    LogPrint,1
-    LogPrintf,0
-    printf,0
-    snprintf,2
-    sprintf,1
-    strprintf,0
-    vfprintf,1
-    vprintf,1
-    vsnprintf,1
-    vsprintf,1
-)
-
 EXIT_CODE=0
 if ! python3 -m doctest test/lint/lint-format-strings.py; then
     EXIT_CODE=1
 fi
-for S in "${FUNCTION_NAMES_AND_NUMBER_OF_LEADING_ARGUMENTS[@]}"; do
-    IFS="," read -r FUNCTION_NAME SKIP_ARGUMENTS <<< "${S}"
-    test/lint/lint-format-strings.py --skip-arguments "${SKIP_ARGUMENTS}" "${FUNCTION_NAME}" "${1}"
-done
+
+test/lint/lint-format-strings.py "${1}"
 
 exit ${EXIT_CODE}
