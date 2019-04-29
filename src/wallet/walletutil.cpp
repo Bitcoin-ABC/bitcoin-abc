8ac51748d64ffacf3c0bcebedac073417a3681d2
MarcoFalke <falke.marco@gmail.com>
2019-04-29 08:06:53 -0400

    [backport#15917] wallet: Avoid logging no_such_file_or_directory error
    Summary:
    wallet: Avoid logging no_such_file_or_directory error (João Barbosa)
    
    Pull request description:
    
      Avoid logging `IsBerkeleyBtree: No such file or directory ...`. The result of `IsBerkeleyBtree` is the same since `fs::file_size()` returns 0 for non existent files.
    
      Fix #15912.
    
    ---
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/15917 | PR15917]]
    
    Test Plan:
      ninja check
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D7351

--- src/wallet/walletutil.cpp
+++ src/wallet/walletutil.cpp
@@ -30,6 +30,10 @@ fs::path GetWalletDir() {
 }
 
 static bool IsBerkeleyBtree(const fs::path &path) {
+    if (!fs::exists(path)) {
+        return false;
+    }
+
     // A Berkeley DB Btree file has at least 4K.
     // This check also prevents opening lock files.
     boost::system::error_code ec;
