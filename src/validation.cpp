77d79c034ec9556163d2bbbabb16248ce07ad6bf
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-04-30 20:16:45 +0300

    [backport#15894 3/3] Make AbortNode() aware of MSG_NOPREFIX flag
    Summary:
    https://github.com/bitcoin/bitcoin/pull/15894/commits/f724f31401b963c75bd64f5e2c5b9d9561a9a9dd
    
    ---
    
    Depends on D7125
    
    Concludes backport of Core [[https://github.com/bitcoin/bitcoin/pull/15894 | PR15894]]
    
    Test Plan:
      ninja check check-functional
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D7126

--- src/validation.cpp
+++ src/validation.cpp
@@ -1196,23 +1196,30 @@ bool UndoReadFromDisk(CBlockUndo &blockundo, const CBlockIndex *pindex) {
 
 /** Abort with a message */
 static bool AbortNode(const std::string &strMessage,
-                      const std::string &userMessage = "") {
+                      const std::string &userMessage = "",
+                      unsigned int prefix = 0) {
     SetMiscWarning(strMessage);
     LogPrintf("*** %s\n", strMessage);
-    uiInterface.ThreadSafeMessageBox(
-        userMessage.empty() ? _("Error: A fatal internal error occurred, see "
-                                "debug.log for details")
-                                  .translated
-                            : userMessage,
-        "", CClientUIInterface::MSG_ERROR);
+    if (!userMessage.empty()) {
+        uiInterface.ThreadSafeMessageBox(
+            userMessage, "", CClientUIInterface::MSG_ERROR | prefix);
+    } else {
+        uiInterface.ThreadSafeMessageBox(
+            _("Error: A fatal internal error occurred, see debug.log for "
+              "details")
+                .translated,
+            "",
+            CClientUIInterface::MSG_ERROR | CClientUIInterface::MSG_NOPREFIX);
+    }
     StartShutdown();
     return false;
 }
 
 static bool AbortNode(BlockValidationState &state,
                       const std::string &strMessage,
-                      const std::string &userMessage = "") {
-    AbortNode(strMessage, userMessage);
+                      const std::string &userMessage = "",
+                      unsigned int prefix = 0) {
+    AbortNode(strMessage, userMessage, prefix);
     return state.Error(strMessage);
 }
 
@@ -1928,8 +1935,10 @@ bool CChainState::FlushStateToDisk(const CChainParams &chainparams,
             if (fDoFullFlush || fPeriodicWrite) {
                 // Depend on nMinDiskSpace to ensure we can write block index
                 if (!CheckDiskSpace(GetBlocksDir())) {
-                    return AbortNode(state, "Disk space is low!",
-                                     _("Error: Disk space is low!").translated);
+                    return AbortNode(
+                        state, "Disk space is too low!",
+                        _("Error: Disk space is too low!").translated,
+                        CClientUIInterface::MSG_NOPREFIX);
                 }
 
                 // First make sure all block and undo data is flushed to disk.
@@ -1977,8 +1986,10 @@ bool CChainState::FlushStateToDisk(const CChainParams &chainparams,
                 // factor of 2.
                 if (!CheckDiskSpace(GetDataDir(),
                                     48 * 2 * 2 * pcoinsTip->GetCacheSize())) {
-                    return AbortNode(state, "Disk space is low!",
-                                     _("Error: Disk space is low!").translated);
+                    return AbortNode(
+                        state, "Disk space is too low!",
+                        _("Error: Disk space is too low!").translated,
+                        CClientUIInterface::MSG_NOPREFIX);
                 }
 
                 // Flush the chainstate (which may refer to block index
@@ -3339,8 +3350,9 @@ static bool FindBlockPos(FlatFilePos &pos, unsigned int nAddSize,
         size_t bytes_allocated =
             BlockFileSeq().Allocate(pos, nAddSize, out_of_space);
         if (out_of_space) {
-            return AbortNode("Disk space is low!",
-                             _("Error: Disk space is low!").translated);
+            return AbortNode("Disk space is too low!",
+                             _("Error: Disk space is too low!").translated,
+                             CClientUIInterface::MSG_NOPREFIX);
         }
         if (bytes_allocated != 0 && fPruneMode) {
             fCheckForPruning = true;
@@ -3365,8 +3377,9 @@ static bool FindUndoPos(BlockValidationState &state, int nFile,
     size_t bytes_allocated =
         UndoFileSeq().Allocate(pos, nAddSize, out_of_space);
     if (out_of_space) {
-        return AbortNode(state, "Disk space is low!",
-                         _("Error: Disk space is low!").translated);
+        return AbortNode(state, "Disk space is too low!",
+                         _("Error: Disk space is too low!").translated,
+                         CClientUIInterface::MSG_NOPREFIX);
     }
     if (bytes_allocated != 0 && fPruneMode) {
         fCheckForPruning = true;
