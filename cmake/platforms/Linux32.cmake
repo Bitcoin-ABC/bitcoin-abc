a6d4a5e5d4a901771c40870cc55ed5e810270d6f
Fabien <fabcien@gmail.com>
2019-04-30 09:44:25 +0000

    [CMAKE] Add cross build toolchain files for PC Linux platform
    Summary:
    This allows to use the depends system for building a deterministic linux
    binary.
    
    Depends on D3017
    
    Test Plan:
    Note: see dependencies in `contrib/gitian-descriptors/gitian-linus.yml`
    
      mkdir buildcmake && cd buildcmake
      cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/Linux64.cmake \
      -GNinja ..
      ninja
    Run `src/bitcoind` and `src/qt/bitcoin-qt` on a 64 bits Linux
    
      rm -rf *
      cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/Linux32.cmake \
      -GNinja ..
      ninja
    Run `src/bitcoind` and `src/qt/bitcoin-qt` on a 32 bits Linux
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D3019

---
+++ cmake/platforms/Linux32.cmake
@@ -0,0 +1,25 @@
+# Copyright (c) 2019 The Bitcoin developers
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(TOOLCHAIN_PREFIX i686-pc-linux-gnu)
+
+# Cross compilers to use for C and C++
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+
+# Target environment on the build host system
+# Set 1st to directory with the cross compiler's C/C++ headers/libs
+set(CMAKE_FIND_ROOT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/depends/${TOOLCHAIN_PREFIX}")
+
+# We also may have built dependencies for the native platform.
+set(CMAKE_PREFIX_PATH "${CMAKE_CURRENT_SOURCE_DIR}/depends/${TOOLCHAIN_PREFIX}/native")
+
+# Modify default behavior of FIND_XXX() commands to
+# search for headers/libs in the target environment and
+# search for programs in the build host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+string(APPEND CMAKE_C_FLAGS_INIT " -m32")
+string(APPEND CMAKE_CXX_FLAGS_INIT " -m32")
