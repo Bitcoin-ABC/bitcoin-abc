6bb649a50f6a017dd3b7ab26bbda4ed8cc9551ec
Fabien <fabcien@gmail.com>
2019-05-03 22:15:58 +0200

    test: Plug memory leaks and stack-use-after-scope
    Summary:
    Backport of core PR12477
    https://github.com/bitcoin/bitcoin/pull/12477/files
    
    The modifications in `checkqueue_tests.cpp` are already part of our
    codebase.
    
    The modifications of `test_bitcoin` are only required to clear memory
    from `avalanche_tests` and `DoS_tests`. Avalanche tests use
    `std::_unique_ptr`, making this fix useless, and so does DoS tests
    since D2919.
    
    Depends on D2919
    
    Test Plan:
      ./src/bench/bitcoin-bench
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, deadalnix, jasonbcox
    
    Subscribers: jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D2918

--- src/bench/coin_selection.cpp
+++ src/bench/coin_selection.cpp
@@ -40,13 +40,6 @@ static void CoinSelection(benchmark::State &state) {
     LOCK(wallet.cs_wallet);
 
     while (state.KeepRunning()) {
-        // Empty wallet.
-        for (COutput output : vCoins) {
-            delete output.tx;
-        }
-
-        vCoins.clear();
-
         // Add coins.
         for (int i = 0; i < 1000; i++)
             addCoin(1000 * COIN, wallet, vCoins);
@@ -59,6 +52,12 @@ static void CoinSelection(benchmark::State &state) {
         assert(success);
         assert(nValueRet == 1003 * COIN);
         assert(setCoinsRet.size() == 2);
+
+        // Empty wallet.
+        for (COutput output : vCoins) {
+            delete output.tx;
+        }
+        vCoins.clear();
     }
 }
 
