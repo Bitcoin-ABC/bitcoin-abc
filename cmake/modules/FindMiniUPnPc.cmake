c234874c598851d9279937139ad82de547e7bcdd
Fabien <fabcien@gmail.com>
2019-05-08 15:04:33 +0000

    [CMAKE] Add support for Miniupnpc
    Summary:
    This diff adds support for UPnP and an option to disable it by setting
    `-DENABLE_UPNP=OFF` on the cmake command line.
    
    It also adds an option `START_WITH_UPNP` which can be turned ON to set
    UPnP as the default to map ports (OFF by default).
    
    Test Plan:
    With `libminiupnpc` installed:
      mkdir buildcmake && cd buildcmake
      cmake -GNinja .. -DENABLE_UPNP=OFF
      ninja
      ./src/qt/bitcoin-qt
    Go to `Settings->Options...`
    Click on the `Network` tab.
    Check the `Map port using UPnP` checkbox is dimmed.
    Close `bitcoin-qt`.
    
      rm -rf *
      cmake -GNinja ..
      ninja
      ./src/qt/bitcoin-qt
    Go to `Settings->Options...`
    Click on the `Network` tab.
    Check the `Map port using UPnP` checkbox is not dimmed.
    Uncheck `Map port using UPnP` and close `bitcoin-qt`.
    
    Remove the line `fUseUPnP=false` from `~/.config/BitcoinABC/BitcoinABC-Qt.conf`.
      rm -rf *
      cmake -GNinja -DSTART_WITH_UPNP=ON ..
      ninja
      ./src/qt/bitcoin-qt
    Go to `Settings->Options...`
    Click on the `Network` tab.
    Check the `Map port using UPnP` checkbox is not dimmed and is checked.
    Uncheck `Map port using UPnP` and close `bitcoin-qt`.
    
    Uninstall libminiupnpc. On Debian `sudo apt remove libminiupnpc*`
      rm -rf *
      cmake -GNinja ..
    Should return a CMake Error:
      Could NOT find MiniUPnPc (missing: MINIUPNPC_INCLUDE_DIR MINIUPNPC_LIBRARY)
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D2997

---
+++ cmake/modules/FindMiniUPnPc.cmake
@@ -0,0 +1,45 @@
+# Try to find libminiupnpc
+# MINIUPNPC_FOUND - system has libminiupnpc
+# MINIUPNPC_INCLUDE_DIR - the libminiupnpc include directory
+# MINIUPNPC_LIBRARY - Library needed to use libminiupnpc
+
+if (MINIUPNPC_INCLUDE_DIR AND MINIUPNPC_LIBRARY)
+	# Already in cache, be silent
+	set(MINIUPNPC_FIND_QUIETLY TRUE)
+endif()
+
+find_path(MINIUPNPC_INCLUDE_DIR miniupnpc/miniupnpc.h)
+
+set(MINIUPNPC_REQUIRED_HEADERS
+	miniupnpc/miniwget.h
+	miniupnpc/upnpcommands.h
+	miniupnpc/upnperrors.h
+)
+
+include(SanitizeHelper)
+set(CMAKE_REQUIRED_INCLUDES ${MINIUPNPC_INCLUDE_DIR})
+foreach(_miniupnpc_header ${MINIUPNPC_REQUIRED_HEADERS})
+	sanitize_variable(HAVE_MINIUPNPC_ ${_miniupnpc_header} HEADER_FOUND)
+	check_include_files(${_miniupnpc_header} ${HEADER_FOUND})
+	if(NOT ${HEADER_FOUND})
+		set(MINIUPNPC_MISSING_HEADER ON)
+	endif()
+endforeach()
+
+if(NOT MINIUPNPC_MISSING_HEADER)
+	find_library(MINIUPNPC_LIBRARY NAMES miniupnpc libminiupnpc)
+
+	message(STATUS "MiniUPnPc lib: " ${MINIUPNPC_LIBRARY})
+
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(
+		MiniUPnPc DEFAULT_MSG
+		MINIUPNPC_INCLUDE_DIR
+		MINIUPNPC_LIBRARY
+	)
+
+	mark_as_advanced(MINIUPNPC_INCLUDE_DIR MINIUPNPC_LIBRARY)
+
+	set(MiniUPnPc_LIBRARIES ${MINIUPNPC_LIBRARY})
+	set(MiniUPnPc_INCLUDE_DIRS ${MINIUPNPC_INCLUDE_DIR})
+endif()
