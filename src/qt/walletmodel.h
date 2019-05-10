deb7d1c0ff41f3a70a13dc5a299cada2c7f72059
Pieter Wuille <pieter.wuille@gmail.com>
2019-05-09 18:07:33 -0700

    [backport]Give WalletModel::UnlockContext move semantics
    Summary:
    WalletModel::UnlockContext seems to implement "move upon copy" semantics; with C++11 this can be done more safely using move semantics (making attempts to actually copy fail instead).
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/16001 | PR16001]]
    
    Test Plan: `ninja check`
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D7567

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -201,10 +201,12 @@ public:
 
         bool isValid() const { return valid; }
 
-        // Copy operator and constructor transfer the context
-        UnlockContext(const UnlockContext &obj) { CopyFrom(obj); }
-        UnlockContext &operator=(const UnlockContext &rhs) {
-            CopyFrom(rhs);
+        // Copy constructor is disabled.
+        UnlockContext(const UnlockContext &) = delete;
+        // Move operator and constructor transfer the context
+        UnlockContext(UnlockContext &&obj) { CopyFrom(std::move(obj)); }
+        UnlockContext &operator=(UnlockContext &&rhs) {
+            CopyFrom(std::move(rhs));
             return *this;
         }
 
@@ -214,7 +216,8 @@ public:
         // mutable, as it can be set to false by copying
         mutable bool relock;
 
-        void CopyFrom(const UnlockContext &rhs);
+        UnlockContext &operator=(const UnlockContext &) = default;
+        void CopyFrom(UnlockContext &&rhs);
     };
 
     UnlockContext requestUnlock();
