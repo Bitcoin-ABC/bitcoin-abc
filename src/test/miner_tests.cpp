819b0da6cb91fd3a40ab541f001a137983a2b7c6
Shammah Chancellor <shammah.chancellor@gmail.com>
2019-05-30 20:12:00 -0700

    [mining] Add a test for TestCBlockTemplateEntry
    Summary:
    Add the skeleton of a test to ensure that the constructor is setting the expected
    fields, and can be filled in later for more complicated logic which will be added.
    
    Test Plan:
      make check
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D3162

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -770,4 +770,14 @@ BOOST_AUTO_TEST_CASE(BlockAssembler_construction) {
     }
 }
 
+BOOST_AUTO_TEST_CASE(TestCBlockTemplateEntry) {
+    const CTransaction tx;
+    CTransactionRef txRef = MakeTransactionRef(tx);
+    CBlockTemplateEntry txEntry(txRef, 1 * SATOSHI, 200, 10);
+    BOOST_CHECK_MESSAGE(txEntry.tx == txRef, "Transactions did not match");
+    BOOST_CHECK_EQUAL(txEntry.txFee, 1 * SATOSHI);
+    BOOST_CHECK_EQUAL(txEntry.txSize, 200);
+    BOOST_CHECK_EQUAL(txEntry.txSigOps, 10);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
