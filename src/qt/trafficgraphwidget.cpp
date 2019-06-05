aa80b9c746c65084bc4e85ff8c01e1a4585b1ccb
Josu Goñi <josu_z@hotmail.com>
2019-06-05 18:43:59 +0200

    Add antialiasing to traffic graph widget
    Summary: Backport of Core [[https://github.com/bitcoin/bitcoin/pull/16153 | PR16153]]
    
    Test Plan:
    Visual improvement on network traffic plot:
    `src/qt/bitcoin-qt -> Help -> Debug window -> Network Traffic`
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D7819

--- src/qt/trafficgraphwidget.cpp
+++ src/qt/trafficgraphwidget.cpp
@@ -100,6 +100,7 @@ void TrafficGraphWidget::paintEvent(QPaintEvent *) {
         }
     }
 
+    painter.setRenderHint(QPainter::Antialiasing);
     if (!vSamplesIn.empty()) {
         QPainterPath p;
         paintPath(p, vSamplesIn);
