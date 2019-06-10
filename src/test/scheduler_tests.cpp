Bitcoin-ABC/bitcoin-abc

commit aa668d1eb2404fbd9c2cb4401e02aa2d17596aca

Avoid spurious boost output in scheduler_tests
Summary:
Boost test macros try to output some message, which leads to a messy
debug output when called in intricated threads.
Use `assert` in these cases solves the issue.

Backport of Core PR14069
https://github.com/bitcoin/bitcoin/pull/14069/files

Test Plan:
  ./src/test/test_bitcoin -t scheduler_tests
Without the patch it will sometime output partial boost debug info.
With the patch the output will remain clean.

Reviewers: #bitcoin_abc, deadalnix, jasonbcox

Reviewed By: #bitcoin_abc, jasonbcox

Subscribers: jasonbcox

Differential Revision: https://reviews.bitcoinabc.org/D3232

src/test/scheduler_tests.cpp
@@ -202,11 +202,9 @@ BOOST_AUTO_TEST_CASE(singlethreadedscheduler_ordered) {
     // then the callbacks should run in exactly the order in which they were
     // enqueued
     for (int i = 0; i < 100; ++i) {
-        queue1.AddToProcessQueue(
-            [i, &counter1]() { BOOST_CHECK_EQUAL(i, counter1++); });
+        queue1.AddToProcessQueue([i, &counter1]() { assert(i == counter1++); });
 
-        queue2.AddToProcessQueue(
-            [i, &counter2]() { BOOST_CHECK_EQUAL(i, counter2++); });
+        queue2.AddToProcessQueue([i, &counter2]() { assert(i == counter2++); });
     }
 
     // finish up

commit 3a0a0966a1d1739dad8f6c427a6801c8e7d833f2

Don't assert(...) with side effects
Summary:
This diffs fixes the assertions introduced in D3232 and adds a linter to
avoid reproducing the issue.

The fix is a backport of core PR14088
https://github.com/bitcoin/bitcoin/pull/14088/files
The linter is based on the second commit, but integrated into arcanist.

Test Plan:
  ./src/test/test_bitcoin -t scheduler_tests
Revert the changes in `scheduler_tests` and run:
  arc lint -- src/test/scheduler_tests.cpp
The linter should catch the errors

Reviewers: #bitcoin_abc, deadalnix, jasonbcox

Reviewed By: #bitcoin_abc, jasonbcox

Subscribers: jasonbcox

Differential Revision: https://reviews.bitcoinabc.org/D3277

.arclint
@@ -80,6 +80,13 @@
             "spelling.dictionaries": [
                 "test/lint/dictionary/english.json"
             ]
+        },
+        "lint-assert-with-side-effects": {
+            "type": "lint-assert-with-side-effects",
+            "include": "(^src/.*\\.(h|cpp)$)",
+            "exclude": [
+                "(^src/(secp256k1|univalue|leveldb)/)"
+            ]
         }
     }
 }

arcanist/.phutil_module_cache
@@ -1,1 +1,1 @@
-{"__symbol_cache_version__":11,"90a8b110dc475955f15bb81d37268cb5":{"have":{"class":{"AutoPEP8FormatLinter":75}},"need":{"function":{"pht":297,"execx":769,"id":1903},"class":{"ArcanistExternalLinter":104,"ArcanistLintMessage":1910,"Filesystem":1754,"ArcanistLinter":2017,"ArcanistLintSeverity":2095}},"xmap":{"AutoPEP8FormatLinter":["ArcanistExternalLinter"]}},"bf0805c02029a7226e8c0d7dee039b3c":{"have":{"class":{"CheckDocLinter":106}},"need":{"function":{"pht":323,"id":1847},"class":{"ArcanistExternalLinter":129,"ArcanistLintMessage":1854,"Filesystem":731,"ArcanistLinter":1902,"ArcanistLintSeverity":1988}},"xmap":{"CheckDocLinter":["ArcanistExternalLinter"]}},"6af7410cfea496ff1d4dcc2624b6b8ea":{"have":{"class":{"ClangFormatLinter":79}},"need":{"function":{"pht":302,"execx":781,"id":1653},"class":{"ArcanistExternalLinter":105,"ArcanistLintMessage":1660,"Filesystem":1504,"ArcanistLinter":1767,"ArcanistLintSeverity":1845}},"xmap":{"ClangFormatLinter":["ArcanistExternalLinter"]}},"9285ad9415f8ebe564f7119e5a72c559":{"have":{"class":{"FormatStringLinter":146}},"need":{"function":{"pht":377,"csprintf":1492,"id":1872},"class":{"ArcanistExternalLinter":173,"ArcanistLintMessage":1879,"Filesystem":827,"ArcanistLinter":1956,"ArcanistLintSeverity":2044}},"xmap":{"FormatStringLinter":["ArcanistExternalLinter"]}},"224d394856b17878058b4c14acb7178b":{"have":{"class":{"LocaleDependenceLinter":160}},"need":{"function":{"pht":5400},"class":{"ArcanistLinter":191,"ArcanistLintSeverity":5903,"Filesystem":6149}},"xmap":{"LocaleDependenceLinter":["ArcanistLinter"]}},"6f2f22dd0f259fb2eaa284b4fab3bc29":{"have":{"class":{"PythonFormatLinter":123}},"need":{"function":{"pht":353,"id":1838},"class":{"ArcanistExternalLinter":150,"ArcanistLintMessage":1845,"Filesystem":776,"ArcanistLinter":1970,"ArcanistLintSeverity":2053}},"xmap":{"PythonFormatLinter":["ArcanistExternalLinter"]}},"25781df78f6eebfb223296b8265e9d19":{"have":{"class":{"TestsLinter":103}},"need":{"function":{"pht":318,"id":2629},"class":{"ArcanistExternalLinter":123,"ArcanistLintMessage":2636,"Filesystem":776,"ArcanistLinter":2684,"ArcanistLintSeverity":2792}},"xmap":{"TestsLinter":["ArcanistExternalLinter"]}},"38f0c676bff5192a344464142caaa253":{"have":{"class":{"CHeaderLinter":99}},"need":{"function":{"pht":611},"class":{"ArcanistLinter":121,"ArcanistLintSeverity":1060,"Filesystem":1307}},"xmap":{"CHeaderLinter":["ArcanistLinter"]}}}
+{"__symbol_cache_version__":11,"90a8b110dc475955f15bb81d37268cb5":{"have":{"class":{"AutoPEP8FormatLinter":75}},"need":{"function":{"pht":297,"execx":769,"id":1903},"class":{"ArcanistExternalLinter":104,"ArcanistLintMessage":1910,"Filesystem":1754,"ArcanistLinter":2017,"ArcanistLintSeverity":2095}},"xmap":{"AutoPEP8FormatLinter":["ArcanistExternalLinter"]}},"38f0c676bff5192a344464142caaa253":{"have":{"class":{"CHeaderLinter":99}},"need":{"function":{"pht":611},"class":{"ArcanistLinter":121,"ArcanistLintSeverity":1060,"Filesystem":1307}},"xmap":{"CHeaderLinter":["ArcanistLinter"]}},"a30e4e25376ca05d4ae719915441be9e":{"have":{"class":{"CheckDocLinter":106}},"need":{"function":{"pht":323,"id":1848},"class":{"ArcanistExternalLinter":129,"ArcanistLintMessage":1855,"Filesystem":731,"ArcanistLinter":1903,"ArcanistLintSeverity":1989}},"xmap":{"CheckDocLinter":["ArcanistExternalLinter"]}},"6af7410cfea496ff1d4dcc2624b6b8ea":{"have":{"class":{"ClangFormatLinter":79}},"need":{"function":{"pht":302,"execx":781,"id":1653},"class":{"ArcanistExternalLinter":105,"ArcanistLintMessage":1660,"Filesystem":1504,"ArcanistLinter":1767,"ArcanistLintSeverity":1845}},"xmap":{"ClangFormatLinter":["ArcanistExternalLinter"]}},"9285ad9415f8ebe564f7119e5a72c559":{"have":{"class":{"FormatStringLinter":146}},"need":{"function":{"pht":377,"csprintf":1492,"id":1872},"class":{"ArcanistExternalLinter":173,"ArcanistLintMessage":1879,"Filesystem":827,"ArcanistLinter":1956,"ArcanistLintSeverity":2044}},"xmap":{"FormatStringLinter":["ArcanistExternalLinter"]}},"224d394856b17878058b4c14acb7178b":{"have":{"class":{"LocaleDependenceLinter":160}},"need":{"function":{"pht":5400},"class":{"ArcanistLinter":191,"ArcanistLintSeverity":5903,"Filesystem":6149}},"xmap":{"LocaleDependenceLinter":["ArcanistLinter"]}},"6f2f22dd0f259fb2eaa284b4fab3bc29":{"have":{"class":{"PythonFormatLinter":123}},"need":{"function":{"pht":353,"id":1838},"class":{"ArcanistExternalLinter":150,"ArcanistLintMessage":1845,"Filesystem":776,"ArcanistLinter":1970,"ArcanistLintSeverity":2053}},"xmap":{"PythonFormatLinter":["ArcanistExternalLinter"]}},"25781df78f6eebfb223296b8265e9d19":{"have":{"class":{"TestsLinter":103}},"need":{"function":{"pht":318,"id":2629},"class":{"ArcanistExternalLinter":123,"ArcanistLintMessage":2636,"Filesystem":776,"ArcanistLinter":2684,"ArcanistLintSeverity":2792}},"xmap":{"TestsLinter":["ArcanistExternalLinter"]}},"2809b09d2021203b43c57da33d1fe8bf":{"have":{"class":{"AssertWithSideEffectsLinter":210}},"need":{"function":{"pht":439},"class":{"ArcanistLinter":246,"ArcanistLintSeverity":926,"Filesystem":1170}},"xmap":{"AssertWithSideEffectsLinter":["ArcanistLinter"]}}}

arcanist/__phutil_library_map__.php
@@ -9,6 +9,7 @@
 phutil_register_library_map(array(
   '__library_version__' => 2,
   'class' => array(
+    'AssertWithSideEffectsLinter' => 'linter/AssertWithSideEffectsLinter.php',
     'AutoPEP8FormatLinter' => 'linter/AutoPEP8Linter.php',
     'CHeaderLinter' => 'linter/CHeaderLinter.php',
     'CheckDocLinter' => 'linter/CheckDocLinter.php',
@@ -20,6 +21,7 @@ phutil_register_library_map(array(
   ),
   'function' => array(),
   'xmap' => array(
+    'AssertWithSideEffectsLinter' => 'ArcanistLinter',
     'AutoPEP8FormatLinter' => 'ArcanistExternalLinter',
     'CHeaderLinter' => 'ArcanistLinter',
     'CheckDocLinter' => 'ArcanistExternalLinter',

arcanist/linter/AssertWithSideEffectsLinter.php
@@ -1,0 +1,70 @@
+<?php
+
+/**
+ * Check that there is no side effect in assertions.
+ *
+ * PRE31-C (SEI CERT C Coding Standard):
+ * "Assertions should not contain assignments, increment, or decrement
+ * operators."
+ */
+final class AssertWithSideEffectsLinter extends ArcanistLinter {
+
+  const ASSERT_SIDE_EFFECTS_FOUND = 1;
+
+  public function getInfoName() {
+    return 'lint-assert-with-side-effects';
+  }
+
+  public function getInfoDescription() {
+    return pht('Check that there is no side effect in assertions.');
+  }
+
+  public function getLinterName() {
+    return 'ASSERT_WITH_SIDE_EFFECTS';
+  }
+
+  public function getLinterConfigurationName() {
+    return 'lint-assert-with-side-effects';
+  }
+
+  public function getLinterConfigurationOptions() {
+    $options = array();
+    return $options + parent::getLinterConfigurationOptions();
+  }
+
+  public function getLintSeverityMap() {
+    return array(
+        self::ASSERT_SIDE_EFFECTS_FOUND => ArcanistLintSeverity::SEVERITY_ERROR,
+    );
+  }
+
+  public function getLintNameMap() {
+    return array(
+        self::ASSERT_SIDE_EFFECTS_FOUND => pht('Assertion has side-effects.'),
+    );
+  }
+
+  public function lintPath($path) {
+    $path = Filesystem::resolvePath($path, $this->getProjectRoot());
+    $fileContent = Filesystem::readFile($path);
+
+    if (!preg_match_all("/[^_]assert\(.*(\+\+|\-\-|[^=!<>]=[^=!<>]).*\);/",
+      $fileContent, $matches, PREG_OFFSET_CAPTURE)) {
+      return;
+    }
+
+    foreach ($matches[1] as $match) {
+      list($sideEffect, $offset) = $match;
+
+      $this->raiseLintAtOffset(
+        $offset,
+          self::ASSERT_SIDE_EFFECTS_FOUND,
+        pht('Having side-effects in assertions is unexpected and makes the '.
+            'code harder to understand. From PRE31-C (SEI CERT C Coding '.
+            'Standard): "Assertions should not contain assignments, '.
+            'increment, or decrement operators."'),
+        $sideEffect,
+        null);
+    }
+  }
+}

src/test/scheduler_tests.cpp
@@ -202,9 +202,15 @@ BOOST_AUTO_TEST_CASE(singlethreadedscheduler_ordered) {
     // then the callbacks should run in exactly the order in which they were
     // enqueued
     for (int i = 0; i < 100; ++i) {
-        queue1.AddToProcessQueue([i, &counter1]() { assert(i == counter1++); });
+        queue1.AddToProcessQueue([i, &counter1]() {
+            bool expectation = i == counter1++;
+            assert(expectation);
+        });
 
-        queue2.AddToProcessQueue([i, &counter2]() { assert(i == counter2++); });
+        queue2.AddToProcessQueue([i, &counter2]() {
+            bool expectation = i == counter2++;
+            assert(expectation);
+        });
     }
 
     // finish up