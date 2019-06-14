0f49f7a5185d4d7de35ee4c5bb651e0df7f1a7a9
Amaury Séchet <deadalnix@gmail.com>
2019-06-15 00:07:43 +0200

    Kill MAX_FREE_TRANSACTION_CREATE_SIZE
    Summary: It's not used anywhere
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D3345

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -51,8 +51,6 @@ static const Amount WALLET_INCREMENTAL_RELAY_FEE(5000 * SATOSHI);
 static const bool DEFAULT_SPEND_ZEROCONF_CHANGE = true;
 //! Default for -walletrejectlongchains
 static const bool DEFAULT_WALLET_REJECT_LONG_CHAINS = false;
-//! Largest (in bytes) free transaction we're willing to create
-static const unsigned int MAX_FREE_TRANSACTION_CREATE_SIZE = 1000;
 static const bool DEFAULT_WALLETBROADCAST = true;
 static const bool DEFAULT_DISABLE_WALLET = false;
 
