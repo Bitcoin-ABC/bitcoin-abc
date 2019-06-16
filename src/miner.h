Bitcoin-ABC/bitcoin-abc

commit af600301e61b373a543c9f6b2957fb6a1fcd969e

mempool: Fix missing locking in CTxMemPool::check(…) and CTxMemPool::setSanityCheck(…)
Summary:
This is a backport of Core PR11689

Fix missing locking in CTxMemPool::check(const CCoinsViewCache *pcoins)

* reading variable 'mapTx' requires holding mutex 'cs'
* reading variable 'mapNextTx' requires holding mutex 'cs'
* reading variable 'nCheckFrequency' requires holding mutex 'cs'

Fix missing locking in CTxMemPool::setSanityCheck(double dFrequency)

* writing variable 'nCheckFrequency' requires holding mutex 'cs'

Add Clang thread safety analysis annotations

Test Plan:
  make check

Reviewers: #bitcoin_abc, jasonbcox

Reviewed By: #bitcoin_abc, jasonbcox

Subscribers: Fabien

Differential Revision: https://reviews.bitcoinabc.org/D3307

src/miner.h
@@ -190,7 +190,8 @@ private:
      * Increments nPackagesSelected / nDescendantsUpdated with corresponding
      * statistics from the package selection (for logging statistics).
      */
-    void addPackageTxs(int &nPackagesSelected, int &nDescendantsUpdated);
+    void addPackageTxs(int &nPackagesSelected, int &nDescendantsUpdated)
+        EXCLUSIVE_LOCKS_REQUIRED(mempool.cs);
 
     /** Enum for the results from TestForBlock */
     enum class TestForBlockResult : uint8_t {
@@ -223,7 +224,8 @@ private:
      */
     bool SkipMapTxEntry(CTxMemPool::txiter it,
                         indexed_modified_transaction_set &mapModifiedTx,
-                        CTxMemPool::setEntries &failedTx);
+                        CTxMemPool::setEntries &failedTx)
+        EXCLUSIVE_LOCKS_REQUIRED(mempool.cs);
     /** Sort the package in an order that is valid to appear in a block */
     void SortForBlock(const CTxMemPool::setEntries &package,
                       CTxMemPool::txiter entry,
@@ -234,7 +236,8 @@ private:
      * updated descendants.
      */
     int UpdatePackagesForAdded(const CTxMemPool::setEntries &alreadyAdded,
-                               indexed_modified_transaction_set &mapModifiedTx);
+                               indexed_modified_transaction_set &mapModifiedTx)
+        EXCLUSIVE_LOCKS_REQUIRED(mempool.cs);
 };
 
 /** Modify the extranonce in a block */

src/test/mempool_tests.cpp
@@ -251,8 +251,9 @@ BOOST_AUTO_TEST_CASE(MempoolClearTest) {
 }
 
 template <typename name>
-void CheckSort(CTxMemPool &pool, std::vector<std::string> &sortedOrder,
-               std::string &&testcase) {
+static void CheckSort(CTxMemPool &pool, std::vector<std::string> &sortedOrder,
+                      const std::string &testcase)
+    EXCLUSIVE_LOCKS_REQUIRED(pool.cs) {
     BOOST_CHECK_EQUAL(pool.size(), sortedOrder.size());
     typename CTxMemPool::indexed_transaction_set::index<name>::type::iterator
         it = pool.mapTx.get<name>().begin();

src/txmempool.cpp
@@ -682,6 +682,7 @@ void CTxMemPool::clear() {
 }
 
 void CTxMemPool::check(const CCoinsViewCache *pcoins) const {
+    LOCK(cs);
     if (nCheckFrequency == 0) {
         return;
     }
@@ -700,7 +701,6 @@ void CTxMemPool::check(const CCoinsViewCache *pcoins) const {
     CCoinsViewCache mempoolDuplicate(const_cast<CCoinsViewCache *>(pcoins));
     const int64_t nSpendHeight = GetSpendHeight(mempoolDuplicate);
 
-    LOCK(cs);
     std::list<const CTxMemPoolEntry *> waitingOnDependants;
     for (indexed_transaction_set::const_iterator it = mapTx.begin();
          it != mapTx.end(); it++) {

src/txmempool.h
@@ -470,7 +470,7 @@ typedef std::pair<double, Amount> TXModifier;
 class CTxMemPool {
 private:
     //!< Value n means that n times in 2^32 we check.
-    uint32_t nCheckFrequency;
+    uint32_t nCheckFrequency GUARDED_BY(cs);
     unsigned int nTransactionsUpdated;
 
     //!< sum of all mempool tx's virtual sizes.
@@ -518,7 +518,7 @@ public:
         indexed_transaction_set;
 
     mutable CCriticalSection cs;
-    indexed_transaction_set mapTx;
+    indexed_transaction_set mapTx GUARDED_BY(cs);
 
     typedef indexed_transaction_set::nth_index<0>::type::iterator txiter;
     //!< All tx hashes/entries in mapTx, in random order
@@ -531,8 +531,10 @@ public:
     };
     typedef std::set<txiter, CompareIteratorByHash> setEntries;
 
-    const setEntries &GetMemPoolParents(txiter entry) const;
-    const setEntries &GetMemPoolChildren(txiter entry) const;
+    const setEntries &GetMemPoolParents(txiter entry) const
+        EXCLUSIVE_LOCKS_REQUIRED(cs);
+    const setEntries &GetMemPoolChildren(txiter entry) const
+        EXCLUSIVE_LOCKS_REQUIRED(cs);
 
 private:
     typedef std::map<txiter, setEntries, CompareIteratorByHash> cacheMap;
@@ -552,10 +554,11 @@ private:
     GetSortedDepthAndScore() const EXCLUSIVE_LOCKS_REQUIRED(cs);
 
 public:
-    indirectmap<COutPoint, const CTransaction *> mapNextTx;
+    indirectmap<COutPoint, const CTransaction *> mapNextTx GUARDED_BY(cs);
     std::map<uint256, TXModifier> mapDeltas;
 
-    /** Create a new CTxMemPool.
+    /**
+     * Create a new CTxMemPool.
      */
     CTxMemPool();
     ~CTxMemPool();
@@ -568,6 +571,7 @@ public:
      */
     void check(const CCoinsViewCache *pcoins) const;
     void setSanityCheck(double dFrequency = 1.0) {
+        LOCK(cs);
         nCheckFrequency = static_cast<uint32_t>(dFrequency * 4294967295.0);
     }
 
@@ -593,7 +597,7 @@ public:
 
     void clear();
     // lock free
-    void _clear();
+    void _clear() EXCLUSIVE_LOCKS_REQUIRED(cs);
     bool CompareDepthAndScore(const uint256 &hasha, const uint256 &hashb);
     void queryHashes(std::vector<uint256> &vtxid);
     bool isSpent(const COutPoint &outpoint);
@@ -662,7 +666,8 @@ public:
      * Assumes that setDescendants includes all in-mempool descendants of
      * anything already in it.
      */
-    void CalculateDescendants(txiter it, setEntries &setDescendants) const;
+    void CalculateDescendants(txiter it, setEntries &setDescendants) const
+        EXCLUSIVE_LOCKS_REQUIRED(cs);
 
     /**
      * The minimum fee to get into the mempool, which may itself not be enough
@@ -748,22 +753,26 @@ private:
      * transaction again, if encountered in another transaction chain.
      */
     void UpdateForDescendants(txiter updateIt, cacheMap &cachedDescendants,
-                              const std::set<TxId> &setExclude);
+                              const std::set<TxId> &setExclude)
+        EXCLUSIVE_LOCKS_REQUIRED(cs);
     /**
      * Update ancestors of hash to add/remove it as a descendant transaction.
      */
-    void UpdateAncestorsOf(bool add, txiter hash, setEntries &setAncestors);
+    void UpdateAncestorsOf(bool add, txiter hash, setEntries &setAncestors)
+        EXCLUSIVE_LOCKS_REQUIRED(cs);
     /** Set ancestor state for an entry */
-    void UpdateEntryForAncestors(txiter it, const setEntries &setAncestors);
+    void UpdateEntryForAncestors(txiter it, const setEntries &setAncestors)
+        EXCLUSIVE_LOCKS_REQUIRED(cs);
     /**
      * For each transaction being removed, update ancestors and any direct
      * children. If updateDescendants is true, then also update in-mempool
      * descendants' ancestor state.
      */
     void UpdateForRemoveFromMempool(const setEntries &entriesToRemove,
-                                    bool updateDescendants);
+                                    bool updateDescendants)
+        EXCLUSIVE_LOCKS_REQUIRED(cs);
     /** Sever link between specified transaction and direct children. */
-    void UpdateChildrenForRemoval(txiter entry);
+    void UpdateChildrenForRemoval(txiter entry) EXCLUSIVE_LOCKS_REQUIRED(cs);
 
     /**
      * Before calling removeUnchecked for a given transaction,
@@ -773,8 +782,10 @@ private:
      * transaction that is removed, so we can't remove intermediate transactions
      * in a chain before we've updated all the state for the removal.
      */
-    void removeUnchecked(txiter entry, MemPoolRemovalReason reason =
-                                           MemPoolRemovalReason::UNKNOWN);
+    void
+    removeUnchecked(txiter entry,
+                    MemPoolRemovalReason reason = MemPoolRemovalReason::UNKNOWN)
+        EXCLUSIVE_LOCKS_REQUIRED(cs);
 };
 
 /**

commit 0965d72948457077b22cd3659dc6c0ab43802488

Fix lock reference in miner.h
Summary: Fix syntax error introduced in D3307.

Test Plan:
  make check

Reviewers: O1 Bitcoin ABC, #bitcoin_abc, deadalnix

Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, deadalnix

Differential Revision: https://reviews.bitcoinabc.org/D3351

src/miner.h
@@ -191,7 +191,7 @@ private:
      * statistics from the package selection (for logging statistics).
      */
     void addPackageTxs(int &nPackagesSelected, int &nDescendantsUpdated)
-        EXCLUSIVE_LOCKS_REQUIRED(mempool.cs);
+        EXCLUSIVE_LOCKS_REQUIRED(mempool->cs);
 
     /** Enum for the results from TestForBlock */
     enum class TestForBlockResult : uint8_t {
@@ -225,7 +225,7 @@ private:
     bool SkipMapTxEntry(CTxMemPool::txiter it,
                         indexed_modified_transaction_set &mapModifiedTx,
                         CTxMemPool::setEntries &failedTx)
-        EXCLUSIVE_LOCKS_REQUIRED(mempool.cs);
+        EXCLUSIVE_LOCKS_REQUIRED(mempool->cs);
     /** Sort the package in an order that is valid to appear in a block */
     void SortForBlock(const CTxMemPool::setEntries &package,
                       CTxMemPool::txiter entry,
@@ -237,7 +237,7 @@ private:
      */
     int UpdatePackagesForAdded(const CTxMemPool::setEntries &alreadyAdded,
                                indexed_modified_transaction_set &mapModifiedTx)
-        EXCLUSIVE_LOCKS_REQUIRED(mempool.cs);
+        EXCLUSIVE_LOCKS_REQUIRED(mempool->cs);
 };
 
 /** Modify the extranonce in a block */