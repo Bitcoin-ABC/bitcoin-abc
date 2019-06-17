88a45c98694fc57ccc14bc5e398584c7405ca55b
Jason B. Cox <contact@jasonbcox.com>
2019-06-17 15:45:06 -0700

    Replumb ibd.sh to prepare for better post-IBD checks
    Summary:
    With the current design of ibd.sh, exiting the IBD subshell early (in the
    future case of adding additional tests/checks) would result in bitcoind not exiting
    as expected.  This patch replumbs ibd.sh such that failures in the IBD subshell or
    bitcoind properly fail the script as expected.
    
    Post IBD checks will be added in an upcoming diff.
    
    Test Plan: TeamCity IBD
    
    Reviewers: #bitcoin_abc, deadalnix, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D3359

--- contrib/teamcity/ibd.sh
+++ contrib/teamcity/ibd.sh
@@ -16,27 +16,57 @@ mkdir -p ibd
 touch ibd/debug.log
 chmod +x bitcoind
 
-# On shutdown, cleanup background processes spawned by this script
-cleanup() {
-    pkill -P ${MYPID} tail || true
-}
-trap "cleanup" EXIT
-
 # Launch bitcoind using this script's parameters
 ./bitcoind -datadir=ibd $* &
-bitcoin_pid=$!
+BITCOIND_PID=$!
 
-# Wait for IBD to finish and kill the daemon
-(
-    set +o pipefail
-    tail -f ibd/debug.log | grep -m 1 'progress=1.000000'
-    echo "Initial block download complete, killing bitcoin daemon (PID: ${bitcoin_pid})."
-    kill ${bitcoin_pid}
-) &
+cleanup() {
+  # Cleanup background processes spawned by this script.
+  pkill -P ${MYPID} tail || true
+}
+trap "cleanup" EXIT
 
 # Show some progress
 tail -f ibd/debug.log | grep 'UpdateTip' | awk 'NR % 10000 == 0' &
 
-# Wait for bitcoind to exit
-wait ${bitcoin_pid}
-exit $?
+# Wait for IBD to finish and kill the daemon
+(
+  (
+    # Ignore the broken pipe when tail tries to write pipe closed by grep
+    set +o pipefail
+    tail -f ibd/debug.log | grep -m 1 'progress=1.000000'
+  )
+  echo "Initial block download complete."
+
+  # TODO Add more checks to see if IBD completed as expected,
+  # These checks will exit the subshell with a non-zero exit code.
+) &
+IBD_PID=$!
+
+# When the IBD subshell finishes, kill bitcoind
+(
+  while [ -e /proc/${IBD_PID} ]; do sleep 0.1; done
+
+  echo "Cleaning up bitcoin daemon (PID: ${BITCOIND_PID})."
+  kill ${BITCOIND_PID}
+) &
+
+# Wait for bitcoind to exit, whether it exited on its own or IBD finished
+wait ${BITCOIND_PID}
+BITCOIND_EXIT_CODE=$?
+
+if [ "${BITCOIND_EXIT_CODE}" -ne "0" ]; then
+  echo "bitcoind exited unexpectedly with code: ${BITCOIND_EXIT_CODE}"
+  exit ${BITCOIND_EXIT_CODE}
+fi
+
+# Get the exit code for the IBD subshell, which should have exited already
+wait ${IBD_PID}
+IBD_EXIT_CODE=$?
+
+# The IBD subshell should only exit with a non-zero code if one of the tests
+# failed.
+if [ "${IBD_EXIT_CODE}" -ne "0" ]; then
+  echo "IBD tests failed with code: ${IBD_EXIT_CODE}"
+  exit ${IBD_EXIT_CODE}
+fi
