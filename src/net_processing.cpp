aab89ee14ae456e4f844d9fc87593be50ef5e128
nicolas.dorier <nicolas.dorier@gmail.com>
2019-06-21 11:31:12 +0900

    Do not disconnect peer for asking mempool if it has NO_BAN permission
    Summary:
    This is a partial backport of Core [[https://github.com/bitcoin/bitcoin/pull/16248 | PR16248]] : https://github.com/bitcoin/bitcoin/pull/16248/commits/ecd5cf7ea4c3644a30092100ffc399e30e193275
    
    Depends on D5928
    
    Test Plan:
      ninja all check-all
    
    Reviewers: #bitcoin_abc, nakihito
    
    Reviewed By: nakihito
    
    Differential Revision: https://reviews.bitcoinabc.org/D5929

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3558,20 +3558,24 @@ static bool ProcessMessage(const Config &config, CNode *pfrom,
 
     if (strCommand == NetMsgType::MEMPOOL) {
         if (!(pfrom->GetLocalServices() & NODE_BLOOM) && !pfrom->fWhitelisted) {
-            LogPrint(BCLog::NET,
-                     "mempool request with bloom filters disabled, disconnect "
-                     "peer=%d\n",
-                     pfrom->GetId());
-            pfrom->fDisconnect = true;
+            if (!pfrom->HasPermission(PF_NOBAN)) {
+                LogPrint(BCLog::NET,
+                         "mempool request with bloom filters disabled, "
+                         "disconnect peer=%d\n",
+                         pfrom->GetId());
+                pfrom->fDisconnect = true;
+            }
             return true;
         }
 
         if (connman->OutboundTargetReached(false) && !pfrom->fWhitelisted) {
-            LogPrint(BCLog::NET,
-                     "mempool request with bandwidth limit reached, disconnect "
-                     "peer=%d\n",
-                     pfrom->GetId());
-            pfrom->fDisconnect = true;
+            if (!pfrom->HasPermission(PF_NOBAN)) {
+                LogPrint(BCLog::NET,
+                         "mempool request with bandwidth limit reached, "
+                         "disconnect peer=%d\n",
+                         pfrom->GetId());
+                pfrom->fDisconnect = true;
+            }
             return true;
         }
 
