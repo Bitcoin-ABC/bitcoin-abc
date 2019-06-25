afffef17a64886fff6695bf2f4abcdbb5f24c343
Jason B. Cox <contact@jasonbcox.com>
2019-06-24 17:08:37 -0700

    Merge #11210: Stop test_bitcoin-qt touching ~/.bitcoin
    Summary:
    dea086f49 Stop test_bitcoin-qt touching ~/.bitcoin (MeshCollider)
    
    Pull request description:
    
      Fixes https://github.com/bitcoin/bitcoin/issues/11192
    
      The directory remains unused, but this stops the tests touching ~/.bitcoin at all (namely creating it if it doesn't exist)
    
    Tree-SHA512: e59ad6b83dbc5ea2fb2761994c09933721d29668b0eef09b9d938a4ee1c67871c5125c57483ee0ea25f2385e308d275d86bcb9087dd4d502923013b4f3dbac82
    
    Backport of Core PR11210
    
    https://github.com/bitcoin/bitcoin/pull/11210/files
    
    Test Plan: `make check`
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D3442

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -49,6 +49,13 @@ int main(int argc, char *argv[]) {
     SetupNetworking();
     SelectParams(CBaseChainParams::MAIN);
     noui_connect();
+    ClearDatadirCache();
+    fs::path pathTemp =
+        fs::temp_directory_path() / strprintf("test_bitcoin-qt_%lu_%i",
+                                              (unsigned long)GetTime(),
+                                              (int)GetRand(100000));
+    fs::create_directories(pathTemp);
+    gArgs.ForceSetArg("-datadir", pathTemp.string());
 
     bool fInvalid = false;
 
@@ -98,6 +105,8 @@ int main(int argc, char *argv[]) {
         fInvalid = true;
     }
 #endif
+
+    fs::remove_all(pathTemp);
 
     return fInvalid;
 }
