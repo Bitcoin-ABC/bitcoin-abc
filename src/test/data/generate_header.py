da0377490053366d3fa02bc0f5004f914f12b3db
Fabien <fabcien@gmail.com>
2019-06-26 08:56:18 +0200

    Fix exit in generate_header.py and some formatting nits
    Summary:
    Possible exit solutions are `sys.exit()` or `os._exit()`, but
    `os.exit()` doesn't exist. Using `sys.exit()` incidentally removes the
    dependency to `os`.
    Also fix some spaces in the generated code so it matches our
    `clang-format` rules, as it is trivial and saves as much as 3 bytes.
    
    Test Plan:
      ninja check
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D3418

--- src/test/data/generate_header.py
+++ src/test/data/generate_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # Copyright (c) 2018 The Bitcoin developers
 
-import os
 import sys
 
 
@@ -9,16 +8,16 @@ def main(test_name, input_file):
     with open(input_file, "rb") as f:
         contents = f.read()
 
-    print("namespace json_tests{")
-    print("   static unsigned const char {}[] = {{".format(test_name))
+    print("namespace json_tests {")
+    print("static unsigned const char {}[] = {{".format(test_name))
     print(", ".join(map(lambda x: "0x{:02x}".format(ord(x)), contents)))
-    print(" };")
+    print("};")
     print("};")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("We need additional pylons!")
-        os.exit(1)
+        sys.exit(1)
 
     main(sys.argv[1], sys.argv[2])
