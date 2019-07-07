816d31b4f1da4ef1a9126ab464b7c4bd982dff39
Amaury Séchet <deadalnix@gmail.com>
2019-07-07 14:58:09 +0200

    Use cstdint in generated test headers
    Summary: It is more in line with the style we are adopting.
    
    Test Plan:
      make clean
      make check
    
    Reviewers: #bitcoin_abc, markblundeberg, Fabien
    
    Reviewed By: #bitcoin_abc, markblundeberg, Fabien
    
    Subscribers: markblundeberg
    
    Differential Revision: https://reviews.bitcoinabc.org/D3574

--- src/test/data/generate_header.py
+++ src/test/data/generate_header.py
@@ -8,8 +8,9 @@ def main(test_name, input_file):
     with open(input_file, "rb") as f:
         contents = f.read()
 
+    print("#include <cstdint>\n")
     print("namespace json_tests {")
-    print("static unsigned const char {}[] = {{".format(test_name))
+    print("static const uint8_t {}[] = {{".format(test_name))
     print(", ".join(map(lambda x: "0x{:02x}".format(ord(x)), contents)))
     print("};")
     print("};")
