3f54b658373385b086ce0c3a0e020351f124fac6
Andrew Chow <achow101-github@achow101.com>
2019-07-08 15:34:37 -0400

    Store WalletDescriptor in DescriptorScriptPubKeyMan
    Summary:
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/16528 | PR16528]] [8/43] : https://github.com/bitcoin/bitcoin/pull/16528/commits/834de0300cde57ca3f662fb7aa5b1bdaed68bc8f
    
    Depends on D8404 and D8405
    
    Test Plan:
      ninja all check-all
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D8406

--- src/wallet/scriptpubkeyman.h
+++ src/wallet/scriptpubkeyman.h
@@ -618,8 +618,18 @@ public:
 };
 
 class DescriptorScriptPubKeyMan : public ScriptPubKeyMan {
+private:
+    WalletDescriptor m_wallet_descriptor GUARDED_BY(cs_desc_man);
+
+    // Map of scripts to descriptor range index
+    using ScriptPubKeyMap = std::map<CScript, int32_t>;
+
+    ScriptPubKeyMap m_map_script_pub_keys GUARDED_BY(cs_desc_man);
+
 public:
-    using ScriptPubKeyMan::ScriptPubKeyMan;
+    DescriptorScriptPubKeyMan(WalletStorage &storage,
+                              WalletDescriptor &descriptor)
+        : ScriptPubKeyMan(storage), m_wallet_descriptor(descriptor) {}
 
     mutable RecursiveMutex cs_desc_man;
 
