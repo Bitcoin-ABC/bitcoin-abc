0f6ecdd44c0c836b680dbd0bcc8206f0db5d8449
Jason B. Cox <contact@jasonbcox.com>
2019-07-11 11:13:41 -0700

    Update chainparams
    Summary: See title.
    
    Test Plan:
    `make check`
    + TeamCity IBD
    
    Reviewers should verify these hashes against their own nodes.
    
    Reviewers: #bitcoin_abc, deadalnix, Fabien, markblundeberg
    
    Reviewed By: #bitcoin_abc, deadalnix, Fabien, markblundeberg
    
    Subscribers: markblundeberg
    
    Differential Revision: https://reviews.bitcoinabc.org/D3653

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -105,12 +105,12 @@ public:
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S(
-            "000000000000000000000000000000000000000000f22fbd89943b5f5104e4ec");
+            "000000000000000000000000000000000000000000f436d6f0fe3133f4c823c8");
 
         // By default assume that the signatures in ancestors of this block are
         // valid.
         consensus.defaultAssumeValid = uint256S(
-            "00000000000000000401095ca2933cc4729484965e66e6a9f8e937070cc8e971");
+            "000000000000000004425aeeb553758650da20031643fbfff0f53ac5dd1b39c3");
 
         // August 1, 2017 hard fork
         consensus.uahfHeight = 478558;
@@ -268,12 +268,12 @@ public:
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S(
-            "00000000000000000000000000000000000000000000004f587a0e52b7984751");
+            "00000000000000000000000000000000000000000000004fea236d62ac9a9f50");
 
         // By default assume that the signatures in ancestors of this block are
         // valid.
         consensus.defaultAssumeValid = uint256S(
-            "00000000001b618c015c41cc218a60a5a94bc42e16e30f1426cfc138615201c3");
+            "000000000000037b3419370d2306ada6b4f30ac9f01a1bec513eef69b8cd4eab");
 
         // August 1, 2017 hard fork
         consensus.uahfHeight = 1155875;
