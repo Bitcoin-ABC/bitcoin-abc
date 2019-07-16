b4bccba1f5f31a3ea7b46be072a898da0c69da2d
Amaury Séchet <deadalnix@gmail.com>
2019-07-16 15:19:40 +0200

    Nits in Qt test
    Summary: As per title.
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Subscribers: jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D3678

--- src/qt/test/util.cpp
+++ src/qt/test/util.cpp
@@ -8,11 +8,13 @@
 #include <QWidget>
 
 void ConfirmMessage(QString *text, int msec) {
-    QTimer::singleShot(msec, [text]() {
+    QTimer::singleShot(msec, [text] {
         for (QWidget *widget : QApplication::topLevelWidgets()) {
             if (widget->inherits("QMessageBox")) {
                 QMessageBox *messageBox = qobject_cast<QMessageBox *>(widget);
-                if (text) *text = messageBox->text();
+                if (text) {
+                    *text = messageBox->text();
+                }
                 messageBox->defaultButton()->click();
             }
         }
