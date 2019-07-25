hebasto/bitcoin-qtw

Fix bitcoin-cli --version
By declaring the relevant option.

Note contrib/devtools/gen-manpages.sh relies on this version information.

test/functional/interface_bitcoin_cli.py
@@ -15,6 +15,9 @@ class TestBitcoinCli(BitcoinTestFramework):
     def run_test(self):
         """Main test logic"""
 
+        cli_response = self.nodes[0].cli("-version").send_cli()
+        assert("Bitcoin Core RPC client version" in cli_response)
+
         self.log.info("Compare responses from gewalletinfo RPC and `bitcoin-cli getwalletinfo`")
         cli_response = self.nodes[0].cli.getwalletinfo()
         rpc_response = self.nodes[0].getwalletinfo()

Bitcoin-ABC/bitcoin-abc

Summary:
This is a backport of Core PR13725
Test Plan:
  make check
  ./test/functional/test_runner.py
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Subscribers: Fabien
Differential Revision: https://reviews.bitcoinabc.org/D3728

test/functional/interface_bitcoin_cli.py
@@ -20,6 +20,9 @@ class TestBitcoinCli(BitcoinTestFramework):
     def run_test(self):
         """Main test logic"""
 
+        cli_response = self.nodes[0].cli("-version").send_cli()
+        assert("Bitcoin ABC RPC client version" in cli_response)
+
         self.log.info(
             "Compare responses from gewalletinfo RPC and `bitcoin-cli getwalletinfo`")
         cli_response = self.nodes[0].cli.getwalletinfo()