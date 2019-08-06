b44d73a64c2018c03b477c40597b4a072efca6e9
Fabien <fabcien@gmail.com>
2019-08-06 10:53:06 +0200

    Enforce clang-format version 7.x
    Summary:
    `llvm` 8.0 and 7.1 have been released recently.
    
    As an example, Ubuntu recently updated to clang-format 7.1, causing
    arcanist to return an error on `arc lint` due to version conflict.
    
    I tested our codebase against clang-format 7.1 and 8.0, there is
    a minor difference in the output (some edge cases are causing
    bad indents with clang-format-7, which is fixed by clang-format-8).
    
    Therefore both versions cannot be allowed at the same time
    as it would cause `arc lint` to format back and forth when
    using different versions.
    
    I would have preferred to enforce >= 7.0 and < 8 in the
    `.arclint`file but this is not supported by arcanist.
    As a workaround an additional check is performed at the linter
    level to enforce using version 7.x only.
    
    Test Plan:
    With `clang-format` 7.0, 7.1 and 8.0:
      arc lint --everything
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, deadalnix, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D3806

--- arcanist/linter/ClangFormatLinter.php
+++ arcanist/linter/ClangFormatLinter.php
@@ -40,10 +40,25 @@ final class ClangFormatLinter extends ArcanistExternalLinter {
     $matches = array();
     $regex = '/^clang-format version (?P<version>\d+\.\d+)\./';
     if (preg_match($regex, $stdout, $matches)) {
-      return $matches['version'];
+      $version = $matches['version'];
     } else {
       return false;
     }
+
+    /*
+     * FIXME: This is a hack to only allow for clang-format version 7.x.
+     * The .arclint `version` field only allow to filter versions using `=`,
+     * `>`, `<`, `>=` or `<=`. There is no facility to define that the required
+     * version should be >= 7.0 and < 8.0.
+     */
+    if ($version[0] != '7') {
+      throw new Exception(pht('Linter %s requires clang-format version 7.x. '.
+                              'You have version %s.',
+                              ClangFormatLinter::class,
+                              $version));
+    }
+
+    return $version;
   }
 
   public function getInstallInstructions() {
