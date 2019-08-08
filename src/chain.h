4ee94ecbed17b35b07309dce4b58223d4e6967f8
“Deisler-JJ” <zhangxing3134@163.com>
2019-08-08 13:21:51 +0800

    config: adjust configration MAX_FUTURE_BLOCK_TIME

--- src/chain.h
+++ src/chain.h
@@ -24,7 +24,7 @@
  * Maximum amount of time that a block timestamp is allowed to exceed the
  * current network-adjusted time before the block will be accepted.
  */
-static const int64_t MAX_FUTURE_BLOCK_TIME = 2 * 60 * 60;
+static const int64_t MAX_FUTURE_BLOCK_TIME = 12 * 60;
 
 /**
  * Timestamp window used as a grace period by code that compares external
