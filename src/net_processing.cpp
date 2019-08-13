da361cf611fc2f6eab062c9c2617e9e1a3de3791
Suhas Daftuar <sdaftuar@gmail.com>
2019-08-13 10:08:48 -0400

    doc: improve comments relating to block-relay-only peers
    Summary:
    This is a partial backport of Core [[https://github.com/bitcoin/bitcoin/pull/15759 | PR15759]] : https://github.com/bitcoin/bitcoin/pull/15759/commits/937eba91e1550bc3038dc541c236ac83e0a0e6d5
    
    Depends on D6426
    
    Test Plan:
      ninja all check-all
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D6434

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1690,6 +1690,11 @@ static void ProcessGetData(const Config &config, CNode *pfrom,
     std::deque<CInv>::iterator it = pfrom->vRecvGetData.begin();
     std::vector<CInv> vNotFound;
     const CNetMsgMaker msgMaker(pfrom->GetSendVersion());
+
+    // Note that if we receive a getdata for a MSG_TX or MSG_WITNESS_TX from a
+    // block-relay-only outbound peer, we will stop processing further getdata
+    // messages from this peer (likely resulting in our peer eventually
+    // disconnecting us).
     if (pfrom->m_tx_relay != nullptr) {
         LOCK(cs_main);
 
