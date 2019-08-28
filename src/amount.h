1cf3629759b9ae694cead011dc4394fa5a96e3f2
Amaury Séchet <deadalnix@gmail.com>
2019-08-28 17:52:06 +0200

    Fix deprecated copy warnings in amount.h
    Summary: This is a warning that is enabled by default on modern versions of GCC. Out code is tripping this in the Amount class.
    
    Test Plan: Build and ensure that warnings in Amount when building with GCC9.
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D3963

--- src/amount.h
+++ src/amount.h
@@ -22,7 +22,15 @@ private:
 
 public:
     constexpr Amount() : amount(0) {}
-    constexpr Amount(const Amount &_camount) : amount(_camount.amount) {}
+    constexpr Amount(const Amount &other) : amount(other.amount) {}
+
+    /**
+     * Assignement operator.
+     */
+    constexpr Amount &operator=(const Amount &other) {
+        amount = other.amount;
+        return *this;
+    }
 
     static constexpr Amount zero() { return Amount(0); }
     static constexpr Amount satoshi() { return Amount(1); }
