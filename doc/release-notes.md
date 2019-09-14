567961d85770aff5af389ca634c9c18c99fa228a
Samuel Dobson <dobsonsa68@gmail.com>
2019-09-14 21:42:34 +1200

    [backport#16866] wallet: Rename 'decode' argument in gettransaction method to 'verbose'
    Summary:
    [wallet] Rename 'decode' argument in gettransaction method to 'verbose' (John Newbery)
    
    Pull request description:
    
      This makes the RPC method consistent with other RPC methods that have a
      'verbose' option.
    
      Change the name of the return object from 'decoded' to details.
    
      Update help text.
    
    ---
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/16866 | PR16866]] and [[https://github.com/bitcoin/bitcoin/pull/16873 | PR16873]] (required to unbreak [[https://github.com/bitcoin/bitcoin/pull/16866 | PR16866]])
    
    Test Plan:
      ninja check check-functional
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D7378

--- doc/release-notes.md
+++ doc/release-notes.md
@@ -8,4 +8,7 @@ This release includes the following features and fixes:
 
 RPC changes
 -----------
-The `gettransaction` RPC now accepts a third (boolean) argument `decode`. If set to `true`, a new `decoded` field will be added to the response containing the decoded transaction.
+The `gettransaction` RPC now accepts a third (boolean) argument `verbose`. If
+set to `true`, a new `decoded` field will be added to the response containing
+the decoded transaction. This field is equivalent to RPC `decoderawtransaction`,
+or RPC `getrawtransaction` when `verbose` is passed.
--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -1929,8 +1929,9 @@ static UniValue gettransaction(const Config &config,
              /* default */ "true for watch-only wallets, otherwise false",
              "Whether to include watch-only addresses in balance calculation "
              "and details[]"},
-            {"decode", RPCArg::Type::BOOL, /* default */ "false",
-             "Whether to add a field with the decoded transaction"},
+            {"verbose", RPCArg::Type::BOOL, /* default */ "false",
+             "Whether to include a `decoded` field containing the decoded "
+             "transaction (equivalent to RPC decoderawtransaction)"},
         },
         RPCResult{
             "{\n"
@@ -1993,7 +1994,11 @@ static UniValue gettransaction(const Config &config,
             "  ],\n"
             "  \"hex\" : \"data\"         (string) Raw data for transaction\n"
             "  \"decoded\" : transaction         (json object) Optional, the "
-            "decoded transaction\n"
+            "decoded transaction (only present when `verbose` is passed), "
+            "equivalent to the\n"
+            "                                                RPC "
+            "decoderawtransaction method, or the RPC getrawtransaction method "
+            "when `verbose` is passed.\n"
             "}\n"},
         RPCExamples{HelpExampleCli("gettransaction",
                                    "\"1075db55d416d3ca199f55b6084e2115b9345e16c"
@@ -2024,7 +2029,7 @@ static UniValue gettransaction(const Config &config,
         filter |= ISMINE_WATCH_ONLY;
     }
 
-    bool decode_tx =
+    bool verbose =
         request.params[2].isNull() ? false : request.params[2].get_bool();
 
     UniValue entry(UniValue::VOBJ);
@@ -2057,7 +2062,7 @@ static UniValue gettransaction(const Config &config,
         EncodeHexTx(*wtx.tx, pwallet->chain().rpcSerializationFlags());
     entry.pushKV("hex", strHex);
 
-    if (decode_tx) {
+    if (verbose) {
         UniValue decoded(UniValue::VOBJ);
         TxToUniv(*wtx.tx, uint256(), decoded, false);
         entry.pushKV("decoded", decoded);
@@ -4751,7 +4756,7 @@ static const CRPCCommand commands[] = {
     { "wallet",             "getrawchangeaddress",          getrawchangeaddress,          {"address_type"} },
     { "wallet",             "getreceivedbyaddress",         getreceivedbyaddress,         {"address","minconf"} },
     { "wallet",             "getreceivedbylabel",           getreceivedbylabel,           {"label","minconf"} },
-    { "wallet",             "gettransaction",               gettransaction,               {"txid","include_watchonly","decode"} },
+    { "wallet",             "gettransaction",               gettransaction,               {"txid","include_watchonly","verbose"} },
     { "wallet",             "getunconfirmedbalance",        getunconfirmedbalance,        {} },
     { "wallet",             "getbalances",                  getbalances,                  {} },
     { "wallet",             "getwalletinfo",                getwalletinfo,                {} },
