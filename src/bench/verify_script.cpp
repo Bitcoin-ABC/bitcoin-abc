31616ea1191dab41dda15f81f129299edd3a1977
Pieter Wuille <pieter.wuille@gmail.com>
2019-09-17 19:34:51 -0700

    Benchmark script verification with 100 nested IFs
    Summary:
    This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/16902 | PR16902]] [1/3]
    https://github.com/bitcoin/bitcoin/pull/16902/commits/89fb241c54fc85befacfa3703d8e21bf3b8a76eb
    
    `src/bench/verify_script.cpp`  was removed from BitcoinABC in https://reviews.bitcoinabc.org/rSTAGING1ff7b561eb7c6f30f1ec788e9503c30bc6485752
    
    Test Plan: ninja all && src/bench/bitcoin-bench -filter=VerifyNestedIfScript
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D8835

---
+++ src/bench/verify_script.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2016-2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <bench/bench.h>
+#include <key.h>
+#if defined(HAVE_CONSENSUS_LIB)
+#include <script/bitcoinconsensus.h>
+#endif
+#include <script/interpreter.h>
+#include <script/script.h>
+#include <script/script_error.h>
+#include <script/standard.h>
+#include <streams.h>
+#include <test/util/transaction_utils.h>
+
+#include <array>
+
+static void VerifyNestedIfScript(benchmark::State &state) {
+    std::vector<std::vector<uint8_t>> stack;
+    CScript script;
+    for (int i = 0; i < 100; ++i) {
+        script << OP_1 << OP_IF;
+    }
+    for (int i = 0; i < 1000; ++i) {
+        script << OP_1;
+    }
+    for (int i = 0; i < 100; ++i) {
+        script << OP_ENDIF;
+    }
+    while (state.KeepRunning()) {
+        auto stack_copy = stack;
+        ScriptExecutionMetrics metrics = {};
+        ScriptError error;
+        bool ret = EvalScript(stack_copy, script, 0, BaseSignatureChecker(),
+                              metrics, &error);
+        assert(ret);
+    }
+}
+
+BENCHMARK(VerifyNestedIfScript, 100);
