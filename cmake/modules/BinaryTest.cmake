a7b6fd85643385870502028d95a008e89034dec2
Fabien <fabcien@gmail.com>
2019-09-20 18:24:42 +0200

    [CMAKE] Add the check-symbols target
    Summary:
    This target runs the symbol-check.py scripts on the executables.
    This is the equivalent of the autotools `check-symbols` rule.
    
    Test Plan:
    Build the depends subsystem for linux 64, then:
      mkdir buildcmake && cd buildcmake
      cmake -GNinja .. \
        -DENABLE_GLIBC_BACK_COMPAT=ON \
        -DENABLE_STATIC_LIBSTDCXX=ON \
        -DENABLE_REDUCE_EXPORTS=ON \
        -DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/Linux64.cmake
      ninja check-symbols
    If should be successful.
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Subscribers: jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D4136

---
+++ cmake/modules/BinaryTest.cmake
@@ -0,0 +1,19 @@
+# Facilities to run tests on the executable binaries.
+
+macro(add_to_symbols_check BINARY)
+	set(CUSTOM_TARGET_NAME "check-symbols-${BINARY}")
+	add_custom_target("${CUSTOM_TARGET_NAME}"
+		COMMAND
+			${CMAKE_COMMAND} -E echo "Running symbol-check.py on ${BINARY}..."
+		COMMAND
+			"${PYTHON_EXECUTABLE}"
+			"${CMAKE_SOURCE_DIR}/contrib/devtools/symbol-check.py"
+			"${BINARY}"
+		DEPENDS
+			"${BINARY}"
+	)
+
+	if(TARGET check-symbols)
+		add_dependencies(check-symbols "${CUSTOM_TARGET_NAME}")
+	endif()
+endmacro()
