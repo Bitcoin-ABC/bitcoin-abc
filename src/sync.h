333492095cafc506050b1b5b4d89f3ee87df9198
James O'Beirne <james.obeirne@gmail.com>
2019-09-25 10:55:52 -0400

    refactor: consolidate PASTE macros
    Summary: This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/17073 | PR17073]]
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D5845

--- src/sync.h
+++ src/sync.h
@@ -7,6 +7,7 @@
 #define BITCOIN_SYNC_H
 
 #include <threadsafety.h>
+#include <util/macros.h>
 
 #include <condition_variable>
 #include <mutex>
@@ -172,9 +173,6 @@ public:
 template <typename MutexArg>
 using DebugLock = UniqueLock<typename std::remove_reference<
     typename std::remove_pointer<MutexArg>::type>::type>;
-
-#define PASTE(x, y) x##y
-#define PASTE2(x, y) PASTE(x, y)
 
 #define LOCK(cs)                                                               \
     DebugLock<decltype(cs)> PASTE2(criticalblock,                              \
