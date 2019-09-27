98415c84aa38692ecf9fb4ad356b2b40e3e6e02e
Fabien <fabcien@gmail.com>
2019-09-27 11:37:21 +0200

    Add support for Glibc version 2.28
    Summary:
    The Glibc backward compatibility feature handles up to version 2.27
    (used by Ubuntu Bionic). This diffs adds support for the version 2.28
    (used by Debian Buster).
    
    There is an additional compatibility fix involved due to the update of
    `fcntl()` and the introduction of the new `fcntl64()` function.
    These functions are linked against their previous version through a
    compatibility symbol.
    
    Test Plan:
    Cross build for all the linux platforms on an OS that ships Glibc 2.28
    (tested on Debian Buster). Use both `autotools` and `cmake` for the
    build.
    Run the `symbol-check.py` script against the binaries and check there is
    no complaint on `fcntl` or `fcntl64` symbol being from a too recent
    Glibc.
    Check the binaries run on OS with a `libc.so.6` version < 2.28
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D4167

--- src/compat/glibc_compat.cpp
+++ src/compat/glibc_compat.cpp
@@ -6,6 +6,7 @@
 #include <config/bitcoin-config.h>
 #endif
 
+#include <cstdarg>
 #include <cstddef>
 #include <cstdint>
 
@@ -66,3 +67,48 @@ __asm(".symver log2f_old,log2f@GLIBC_2.17");
 extern "C" float __wrap_log2f(float x) {
     return log2f_old(x);
 }
+
+/*
+ * Starting with GLIBC_2.28, the `fcntl()` function has a new variant
+ * `fcntl64()`:
+ * https://gitlab.com/freedesktop-sdk/mirrors/glibc/commit/06ab719d30b01da401150068054d3b8ea93dd12f
+ * (link to a mirror, glibc has no online source browser).
+ *
+ * See also the release notes for the 2.28 version:
+ * https://www.sourceware.org/ml/libc-alpha/2018-08/msg00003.html
+ *
+ * This is intended to fix a bug related to large file support on architectures
+ * where off_t and off64_t are not the same underlying type.
+ * To remain compatible with the previous versions, the GLIBC offers a
+ * compatibility symbol for these architectures. We can link the new `fcntl()`
+ * and `fcntl64()` against this symbol with the help of a wrapper.
+ */
+#if defined(HAVE_CONFIG_H) && !defined(OFF_T_EQUALS_OFF64_T)
+extern "C" int fcntl_old(int fd, int cmd, ...);
+#ifdef __i386__
+__asm(".symver fcntl_old,fcntl@GLIBC_2.0");
+#elif defined(__amd64__)
+__asm(".symver fcntl_old,fcntl@GLIBC_2.2.5");
+#elif defined(__arm__)
+__asm(".symver fcntl_old,fcntl@GLIBC_2.4");
+#elif defined(__aarch64__)
+__asm(".symver fcntl_old,fcntl@GLIBC_2.17");
+#endif
+
+extern "C" int __wrap_fcntl(int fd, int cmd, ...) {
+    int ret;
+    va_list vargs;
+    va_start(vargs, cmd);
+    ret = fcntl_old(fd, cmd, va_arg(vargs, void *));
+    va_end(vargs);
+    return ret;
+}
+extern "C" int __wrap_fcntl64(int fd, int cmd, ...) {
+    int ret;
+    va_list vargs;
+    va_start(vargs, cmd);
+    ret = fcntl_old(fd, cmd, va_arg(vargs, void *));
+    va_end(vargs);
+    return ret;
+}
+#endif
