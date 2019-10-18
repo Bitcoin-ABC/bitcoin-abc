d2a40c0a60e2816653e226e3280c620758b2c362
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2019-10-18 11:24:01 -0300

    [mempool] Mark mempool import fails that were found in mempool as 'already there'
    Summary:
    I was investigating the reasons for failed imports in mempool and noticed that `LoadMempool()` and `pwallet->postInitProcess()` (for all wallets) are executed concurrently. The wallet will end up importing transactions that `LoadMempool()` later tries to import; the latter will fail due to the tx already being in the mempool.
    
    Backport of Bitcoin Core PR11062
    https://github.com/bitcoin/bitcoin/pull/11062
    
    Test Plan:
    ```
    make check-all
    ```
    
    Reviewers: Fabien, #bitcoin_abc, deadalnix
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D4266

--- src/validation.cpp
+++ src/validation.cpp
@@ -5602,8 +5602,9 @@ bool LoadMempool(const Config &config) {
     }
 
     int64_t count = 0;
-    int64_t skipped = 0;
+    int64_t expired = 0;
     int64_t failed = 0;
+    int64_t already_there = 0;
     int64_t nNow = GetTime();
 
     try {
@@ -5640,10 +5641,18 @@ bool LoadMempool(const Config &config) {
                 if (state.IsValid()) {
                     ++count;
                 } else {
-                    ++failed;
+                    // mempool may contain the transaction already, e.g. from
+                    // wallet(s) having loaded it while we were processing
+                    // mempool transactions; consider these as valid, instead of
+                    // failed, but mark them as 'already there'
+                    if (g_mempool.exists(tx->GetHash())) {
+                        ++already_there;
+                    } else {
+                        ++failed;
+                    }
                 }
             } else {
-                ++skipped;
+                ++expired;
             }
 
             if (ShutdownRequested()) {
@@ -5663,9 +5672,9 @@ bool LoadMempool(const Config &config) {
         return false;
     }
 
-    LogPrintf("Imported mempool transactions from disk: %i successes, %i "
-              "failed, %i expired\n",
-              count, failed, skipped);
+    LogPrintf("Imported mempool transactions from disk: %i succeeded, %i "
+              "failed, %i expired, %i already there\n",
+              count, failed, expired, already_there);
     return true;
 }
 
