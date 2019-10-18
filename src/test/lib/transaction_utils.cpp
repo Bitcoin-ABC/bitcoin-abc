69616da09d8b4a6db78f76236e648c0e32681f10
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2019-10-18 02:05:05 +0200

    refactor: test/bench: dedup Build{Crediting,Spending}Transaction()
    Summary:
    prototypes used in src/test/script_tests.cpp:
    - CMutableTransaction BuildCreditingTransaction(const CScript& scriptPubKey, int nValue = 0);
    - CMutableTransaction BuildSpendingTransaction(const CScript& scriptSig, const CScriptWitness& scriptWitness, const CTransaction& txCredit);
    
    prototypes used in bench/verify_script.cpp:
    - CMutableTransaction BuildCreditingTransaction(const CScript& scriptPubKey);
    - CMutableTransaction BuildSpendingTransaction(const CScript& scriptSig, const CMutableTransaction& txCredit);
    
    The more generic versions from the script tests are moved into a new file pair
    transaction_utils.cpp/h and the calls are adapted accordingly in the
    verify_script benchmark (passing the nValue of 1 explicitely for
    BuildCreditingTransaction(), passing empty scriptWitness explicitely and
    converting txCredit parameter to CTransaction in BuildSpendingTransaction()).
    
    This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/17183 | PR17183]]
    
    The benchmark in question was deleted because segwit dependent, but it may be a good time to resurrect it.
    
    Test Plan:
      make check
      ninja check
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D5806

---
+++ src/test/lib/transaction_utils.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <test/lib/transaction_utils.h>
+
+CMutableTransaction BuildCreditingTransaction(const CScript &scriptPubKey,
+                                              const Amount nValue) {
+    CMutableTransaction txCredit;
+    txCredit.nVersion = 1;
+    txCredit.nLockTime = 0;
+    txCredit.vin.resize(1);
+    txCredit.vout.resize(1);
+    txCredit.vin[0].prevout = COutPoint();
+    txCredit.vin[0].scriptSig = CScript() << CScriptNum(0) << CScriptNum(0);
+    txCredit.vin[0].nSequence = CTxIn::SEQUENCE_FINAL;
+    txCredit.vout[0].scriptPubKey = scriptPubKey;
+    txCredit.vout[0].nValue = nValue;
+
+    return txCredit;
+}
+
+CMutableTransaction BuildSpendingTransaction(const CScript &scriptSig,
+                                             const CTransaction &txCredit) {
+    CMutableTransaction txSpend;
+    txSpend.nVersion = 1;
+    txSpend.nLockTime = 0;
+    txSpend.vin.resize(1);
+    txSpend.vout.resize(1);
+    txSpend.vin[0].prevout = COutPoint(txCredit.GetId(), 0);
+    txSpend.vin[0].scriptSig = scriptSig;
+    txSpend.vin[0].nSequence = CTxIn::SEQUENCE_FINAL;
+    txSpend.vout[0].scriptPubKey = CScript();
+    txSpend.vout[0].nValue = txCredit.vout[0].nValue;
+
+    return txSpend;
+}
--- src/test/script_tests.cpp
+++ src/test/script_tests.cpp
@@ -22,6 +22,7 @@
 
 #include <test/data/script_tests.json.h>
 #include <test/jsonutil.h>
+#include <test/lib/transaction_utils.h>
 #include <test/scriptflags.h>
 #include <test/setup_common.h>
 #include <test/sigutil.h>
@@ -118,39 +119,6 @@ static ScriptError ParseScriptError(const std::string &name) {
 }
 
 BOOST_FIXTURE_TEST_SUITE(script_tests, BasicTestingSetup)
-
-static CMutableTransaction
-BuildCreditingTransaction(const CScript &scriptPubKey, const Amount nValue) {
-    CMutableTransaction txCredit;
-    txCredit.nVersion = 1;
-    txCredit.nLockTime = 0;
-    txCredit.vin.resize(1);
-    txCredit.vout.resize(1);
-    txCredit.vin[0].prevout = COutPoint();
-    txCredit.vin[0].scriptSig = CScript() << CScriptNum(0) << CScriptNum(0);
-    txCredit.vin[0].nSequence = CTxIn::SEQUENCE_FINAL;
-    txCredit.vout[0].scriptPubKey = scriptPubKey;
-    txCredit.vout[0].nValue = nValue;
-
-    return txCredit;
-}
-
-static CMutableTransaction
-BuildSpendingTransaction(const CScript &scriptSig,
-                         const CTransaction &txCredit) {
-    CMutableTransaction txSpend;
-    txSpend.nVersion = 1;
-    txSpend.nLockTime = 0;
-    txSpend.vin.resize(1);
-    txSpend.vout.resize(1);
-    txSpend.vin[0].prevout = COutPoint(txCredit.GetId(), 0);
-    txSpend.vin[0].scriptSig = scriptSig;
-    txSpend.vin[0].nSequence = CTxIn::SEQUENCE_FINAL;
-    txSpend.vout[0].scriptPubKey = CScript();
-    txSpend.vout[0].nValue = txCredit.vout[0].nValue;
-
-    return txSpend;
-}
 
 static void DoTest(const CScript &scriptPubKey, const CScript &scriptSig,
                    uint32_t flags, const std::string &message,
