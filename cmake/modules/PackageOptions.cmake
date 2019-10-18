6dd33291c9a8dcb71a477bd6aafac4154a784d15
Fabien <fabcien@gmail.com>
2019-10-18 17:28:10 +0200

    [CMAKE] Create a windows installer
    Summary:
    This diff adds NSIS to the windows package generators, and configure it
    to create a windows graphical installer/uninstaller.
    
    The generated installer is very similar to the one from autotools, the
    main difference is that this installer asks for licence agreement.
    
    The installed files differ from autotools, which installs a lot of files
    in a custom subtree, including documentation on how to build the
    software and a lot of things which are unexpected output from a
    graphical installer. This one only installs the files from the package,
    identical to what is distributed as part of a release.
    
    Depends on D4284.
    
    Test Plan:
      mkdir buildcmake && cd buildcmake
      cmake -GNinja .. -DBUILD_BITCOIN_SEEDER=OFF \
        -DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/Win64.cmake
      ninja package
    Check a `bitcoin-abc-<version>-x86_64-w64-mingw32.exe` installer is
    created.
    Run the installer, check all the steps are correct.
    After installation run `Bitcoin ABC` and `Bitcoin ABC (testnet)` from
    the start menu folder.
    Uninstall the software using then windows settings panel and check all
    the files are deleted.
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, deadalnix, jasonbcox
    
    Subscribers: jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D4274

--- cmake/modules/PackageOptions.cmake
+++ cmake/modules/PackageOptions.cmake
@@ -12,7 +12,7 @@ endif()
 
 if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
 	set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/share/pixmaps/nsis-header.bmp")
-	set(CPACK_GENERATOR "ZIP")
+	set(CPACK_GENERATOR "NSIS;ZIP")
 else()
 	set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/share/pixmaps/bitcoin-abc128.png")
 	set(CPACK_GENERATOR "TGZ")
@@ -21,3 +21,39 @@ endif()
 # CPack source package options
 set(CPACK_SOURCE_PACKAGE_FILE_NAME "${PROJECT_NAME}-${PROJECT_VERSION}")
 set(CPACK_SOURCE_GENERATOR "TGZ")
+
+# CPack NSIS installer options
+Include(InstallationHelper)
+set(CPACK_NSIS_EXECUTABLES_DIRECTORY "${CMAKE_INSTALL_BINDIR}")
+set(_nsis_bitcoin_qt "bitcoin-qt.exe")
+
+set(CPACK_NSIS_URL_INFO_ABOUT "${BITCOINABC_HOMEPAGE_URL}")
+set(CPACK_NSIS_CONTACT "info@bitcoinabc.org")
+
+set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/share/pixmaps/bitcoin-abc.ico")
+set(CPACK_NSIS_MUI_UNIICON "${CPACK_NSIS_MUI_ICON}")
+set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP "${CMAKE_SOURCE_DIR}/share/pixmaps/nsis-wizard.bmp")
+set(CPACK_NSIS_MUI_UNWELCOMEFINISHPAGE_BITMAP "${CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP}")
+set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${_nsis_bitcoin_qt}")
+set(CPACK_NSIS_INSTALLED_ICON_NAME "${CMAKE_INSTALL_BINDIR}/${_nsis_bitcoin_qt}")
+
+set(CPACK_NSIS_COMPRESSOR "/SOLID lzma")
+set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
+set(CPACK_NSIS_DISPLAY_NAME "${PACKAGE_NAME}")
+
+macro(add_start_menu_link LINK_NAME EXE PARAMETERS ICON_EXE ICON_INDEX)
+	list(APPEND CPACK_NSIS_CREATE_ICONS_EXTRA
+		"CreateShortCut '$SMPROGRAMS\\\\$STARTMENU_FOLDER\\\\${LINK_NAME}.lnk' '$INSTDIR\\\\${CMAKE_INSTALL_BINDIR}\\\\${EXE}' '${PARAMETERS}' '$INSTDIR\\\\${CMAKE_INSTALL_BINDIR}\\\\${ICON_EXE}' '${ICON_INDEX}'"
+	)
+	list(APPEND CPACK_NSIS_DELETE_ICONS_EXTRA
+		"Delete '$SMPROGRAMS\\\\$START_MENU\\\\${LINK_NAME}.lnk'"
+	)
+endmacro()
+
+set(CPACK_NSIS_MENU_LINKS "${CMAKE_INSTALL_BINDIR}/${_nsis_bitcoin_qt}" "Bitcoin ABC")
+add_start_menu_link("${PACKAGE_NAME} (testnet)"
+	"${_nsis_bitcoin_qt}"
+	"-testnet"
+	"${_nsis_bitcoin_qt}"
+	1
+)
