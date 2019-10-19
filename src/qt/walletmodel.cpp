hebasto/bitcoin-qtw

ui: Support wallets unloaded dynamically

src/qt/walletmodel.cpp
@@ -364,6 +364,12 @@ bool WalletModel::changePassphrase(const SecureString &oldPass, const SecureStri
 }
 
 // Handlers for core signals
+static void NotifyUnload(WalletModel* walletModel)
+{
+    qDebug() << "NotifyUnload";
+    QMetaObject::invokeMethod(walletModel, "unload", Qt::QueuedConnection);
+}
+
 static void NotifyKeyStoreStatusChanged(WalletModel *walletmodel)
 {
     qDebug() << "NotifyKeyStoreStatusChanged";
@@ -411,6 +417,7 @@ static void NotifyWatchonlyChanged(WalletModel *walletmodel, bool fHaveWatchonly
 void WalletModel::subscribeToCoreSignals()
 {
     // Connect signals to wallet
+    m_handler_unload = m_wallet->handleUnload(boost::bind(&NotifyUnload, this));
     m_handler_status_changed = m_wallet->handleStatusChanged(boost::bind(&NotifyKeyStoreStatusChanged, this));
     m_handler_address_book_changed = m_wallet->handleAddressBookChanged(boost::bind(NotifyAddressBookChanged, this, _1, _2, _3, _4, _5));
     m_handler_transaction_changed = m_wallet->handleTransactionChanged(boost::bind(NotifyTransactionChanged, this, _1, _2));
@@ -421,6 +428,7 @@ void WalletModel::subscribeToCoreSignals()
 void WalletModel::unsubscribeFromCoreSignals()
 {
     // Disconnect signals from wallet
+    m_handler_unload->disconnect();
     m_handler_status_changed->disconnect();
     m_handler_address_book_changed->disconnect();
     m_handler_transaction_changed->disconnect();

Bitcoin-ABC/bitcoin-abc

Summary:
Wallets unloaded dynamically will be reflected in the gui.
Partial backport of Core PR13111
https://github.com/bitcoin/bitcoin/pull/13111/commits/0ee77b20771fe34f8dbde6b16d7e2637859baec3
Reviewer note: we migrated from `boost::bind` to `std::bind` in D4133.
Depends on D4253
Test Plan:
  make
  ./bitcoin-qt
  Help -> Debug -> Console
  createwallet test
On the main window and the debug window you should have the multiwallet drop down menu.
Select the test wallet in the main window.  Select `test` in the debug window.
  unloadwallet
This should remove the drop down menu from both windows.  Additionally the main window should should change to displaying the information for `[default_wallet]`.
The main window should no longer display any wallet information as no wallets have been loaded.
  loadwallet test
The `test` wallet's information should be displayed in the main window.
Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
Reviewed By: jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
Differential Revision: https://reviews.bitcoinabc.org/D4254

src/qt/walletmodel.cpp
@@ -338,6 +338,11 @@ bool WalletModel::changePassphrase(const SecureString &oldPass,
 }
 
 // Handlers for core signals
+static void NotifyUnload(WalletModel *walletModel) {
+    qDebug() << "NotifyUnload";
+    QMetaObject::invokeMethod(walletModel, "unload", Qt::QueuedConnection);
+}
+
 static void NotifyKeyStoreStatusChanged(WalletModel *walletmodel) {
     qDebug() << "NotifyKeyStoreStatusChanged";
     QMetaObject::invokeMethod(walletmodel, "updateStatus",
@@ -389,6 +394,7 @@ static void NotifyWatchonlyChanged(WalletModel *walletmodel,
 
 void WalletModel::subscribeToCoreSignals() {
     // Connect signals to wallet
+    m_handler_unload = m_wallet->handleUnload(std::bind(&NotifyUnload, this));
     m_handler_status_changed = m_wallet->handleStatusChanged(
         std::bind(&NotifyKeyStoreStatusChanged, this));
     m_handler_address_book_changed = m_wallet->handleAddressBookChanged(
@@ -406,6 +412,7 @@ void WalletModel::subscribeToCoreSignals() {
 
 void WalletModel::unsubscribeFromCoreSignals() {
     // Disconnect signals from wallet
+    m_handler_unload->disconnect();
     m_handler_status_changed->disconnect();
     m_handler_address_book_changed->disconnect();
     m_handler_transaction_changed->disconnect();