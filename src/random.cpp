8d6b4a37ad9d65038937a9a5cf997ea89637498b
fanquake <fanquake@gmail.com>
2019-10-26 08:15:43 -0400

    random: stop feeding RNG output back into OpenSSL
    Summary:
    ```
    On the ::SLOW or ::SLEEP paths, we would feed our RNG output back into
    OpenSSL using RAND_add. This commit removes that functionality.
    
    RAND_add(): https://www.openssl.org/docs/manmaster/man3/RAND_add.html
    
    RAND_add() mixes the num bytes at buf into the internal state of the
    random generator. This function will not normally be needed, as
    mentioned above. The randomness argument is an estimate of how much
    randomness is contained in buf, in bytes, and should be a number
    between zero and num.
    ```
    
    Partial backport of core [[https://github.com/bitcoin/bitcoin/pull/17265 | PR17265]]:
    https://github.com/bitcoin/bitcoin/pull/17265/commits/5624ab0b4f844dc7c17aeb1b009f002c33c38fb3
    
    Test Plan:
      ninja all check-all
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D6240

--- src/random.cpp
+++ src/random.cpp
@@ -628,15 +628,6 @@ static void ProcRand(uint8_t *out, int num, RNGLevel level) {
         SeedStartup(startup_hasher, rng);
         rng.MixExtract(out, num, std::move(startup_hasher), true);
     }
-
-    // For anything but the 'fast' level, feed the resulting RNG output (after
-    // an additional hashing step) back into OpenSSL.
-    if (level != RNGLevel::FAST) {
-        uint8_t buf[64];
-        CSHA512().Write(out, num).Finalize(buf);
-        RAND_add(buf, sizeof(buf), num);
-        memory_cleanse(buf, 64);
-    }
 }
 
 void GetRandBytes(uint8_t *buf, int num) noexcept {
