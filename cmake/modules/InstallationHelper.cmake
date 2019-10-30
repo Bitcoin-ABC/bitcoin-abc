eb1d98d212078e22f9e2549a36db7b48d56e3b1f
Fabien <fabcien@gmail.com>
2019-10-30 15:09:55 +0100

    [CMAKE] Build bitcoinconsensus library both static and shared
    Summary:
    The bitcoinconsensus library exports the functions from
    `src/script/bitcoinconsensus.h`:
     - `bitcoinconsensus_verify_script`
     - `bitcoinconsensus_verify_script_with_amount`
     - `bitcoinconsensus_version`
    
    This diff adds a facility to build (if required) and install a shared
    library, independently of whether the library already exists as static and/or
    shared.
    
    Note that building with BUILD_SHARED_LIBS=ON set will fail and needs a
    fix which is outside the scope of this diff (the failure is not related
    to this diff).
    
    This is a replacement for D4197 and D4198.
    
    Test Plan:
      mkdir -p buildcmake/install && cd buildcmake
      cmake -GNinja .. -DCMAKE_INSTALL_PREFIX=install
      ninja install
    
      ls install/lib
    
    Should return 1 file and 2 symbolic links:
     - `libbitcoinconsensus.so.0.0.0`
     - `libbitcoinconsensus.so.0` (link to `libbitcoinconsensus.so.0.0.0`)
     - `libbitcoinconsensus.so` (link to `libbitcoinconsensus.so.0`)
    
      ls install/include
    
    Should return `bitcoinconsensus.h`.
    
      nm -D --defined-only install/lib/libbitcoinconsensus.so
    
    Check the exported symbols contain the 3 above mentioned functions.
    
      ninja check # There are additional test cases in `script_tests.cpp`
    
      rm -rf *
      mkdir install
      cmake -GNinja .. -DCMAKE_INSTALL_PREFIX=install \
        -DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/OSX.cmake
      ninja install
    
      ls install/lib
    
    Should return 1 file and 1 symbolic link:
     - `libbitcoinconsensus.0.dylib`
     - `libbitcoinconsensus.dylib` (link to `libbitcoinconsensus.0.dylib`)
    
      ls install/include
    
    Should return `bitcoinconsensus.h`.
    
      rm -rf *
      mkdir install
      cmake -GNinja .. -DCMAKE_INSTALL_PREFIX=install \
        -DBUILD_BITCOIN_SEEDER=OFF \
        -DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/Win64.cmake
      ninja install
    
      ls install/lib
    
    Should return 1 file :
     - `libbitcoinconsensus-0.dll`
    
      ls install/include
    
    Should return `bitcoinconsensus.h`.
    
      rm -rf *
      cmake -GNinja .. -DCMAKE_INSTALL_PREFIX=install \
        -DBUILD_LIBBITCOINCONSENSUS=OFF
      ninja install
    
      ls install
    Should have no `lib` nor `include` directory.
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D4229

--- cmake/modules/InstallationHelper.cmake
+++ cmake/modules/InstallationHelper.cmake
@@ -2,11 +2,72 @@
 
 include(GNUInstallDirs)
 
-macro(install_target _target)
+function(install_target _target)
+	set(RUNTIME_DESTINATION "${CMAKE_INSTALL_BINDIR}")
+	# CMake installs Windows shared libraries to the RUNTIME destination folder,
+	# but autotools install them into the LIBRARY destination folder.
+	# This special case only purpose is to provide identical installation trees
+	# between CMake and autotools.
+	get_target_property(_target_type ${_target} TYPE)
+	if(${CMAKE_SYSTEM_NAME} MATCHES "Windows" AND _target_type STREQUAL "SHARED_LIBRARY")
+		set(RUNTIME_DESTINATION "${CMAKE_INSTALL_LIBDIR}")
+	endif()
+
 	install(
 		TARGETS ${_target}
-		RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}"
+		RUNTIME DESTINATION "${RUNTIME_DESTINATION}"
 		LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
 		PUBLIC_HEADER DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
 	)
-endmacro()
+endfunction()
+
+function(install_shared_library NAME)
+	cmake_parse_arguments(ARG
+		""
+		""
+		"PUBLIC_HEADER"
+		${ARGN}
+	)
+
+	set(_sources ${ARG_UNPARSED_ARGUMENTS})
+
+	get_target_property(_target_type ${NAME} TYPE)
+	if(_target_type STREQUAL "SHARED_LIBRARY")
+		set(_shared_name "${NAME}")
+		target_sources(${NAME} PRIVATE ${_sources})
+	else()
+		set(_shared_name "${NAME}-shared")
+		add_library(${_shared_name} SHARED ${_sources})
+		target_link_libraries(${_shared_name} ${NAME})
+	endif()
+
+	if(ARG_PUBLIC_HEADER)
+		list(APPEND _properties PUBLIC_HEADER ${ARG_PUBLIC_HEADER})
+	endif()
+
+	if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+		# FIXME For compatibility reason with autotools, the version is set
+		# to 0.0.0 (major being actually 0). This is obviously wrong and the
+		# version of the library should reflect the version of the release.
+		# On platforms other than linux, only the major version (0) is used.
+		# Replace the VERSION line with the statement below to set the
+		# correct version:
+		# set(_properties VERSION "${bitcoin-abc_VERSION}")
+		list(APPEND _properties VERSION "${bitcoin-abc_VERSION_MAJOR}.0.0")
+	else()
+		list(APPEND _properties VERSION "${bitcoin-abc_VERSION_MAJOR}")
+	endif()
+
+	# For autotools compatibility, rename the library to ${OUTPUT_NAME}-0.dll
+	if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+		list(APPEND _properties OUTPUT_NAME "${NAME}-${bitcoin-abc_VERSION_MAJOR}")
+	else()
+		list(APPEND _properties OUTPUT_NAME "${NAME}")
+	endif()
+
+	list(APPEND _properties SOVERSION "${bitcoin-abc_VERSION_MAJOR}")
+
+	set_target_properties(${_shared_name} PROPERTIES ${_properties})
+
+	install_target(${_shared_name})
+endfunction()
