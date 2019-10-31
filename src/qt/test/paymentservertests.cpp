7b315185cc0d8d6e5d7f8ff6882d3c217357b16e
Nico Guiton <nico@bitframe.org>
2019-10-31 21:18:43 +0000

    Update qt/test/paymentservertests.cpp to use Qt5 connect syntax
    Summary:
    Updates to use Qt5 connect syntax over Qt4.
    See https://wiki.qt.io/New_Signal_Slot_Syntax for details.
    
    Partial backport of Core PR13529
    https://github.com/bitcoin/bitcoin/pull/13529/commits/f78558f1e39198779bdb17e2b0e256fb99ad4b28#diff-86ee984762bcf229d495727d769c156b
    
    Test Plan:
      make check
      src/qt/test/test_bitcoin-qt
    
    Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D4349

--- src/qt/test/paymentservertests.cpp
+++ src/qt/test/paymentservertests.cpp
@@ -36,8 +36,8 @@ X509 *parse_b64der_cert(const char *cert_data) {
 static SendCoinsRecipient handleRequest(PaymentServer *server,
                                         std::vector<uint8_t> &data) {
     RecipientCatcher sigCatcher;
-    QObject::connect(server, SIGNAL(receivedPaymentRequest(SendCoinsRecipient)),
-                     &sigCatcher, SLOT(getRecipient(SendCoinsRecipient)));
+    QObject::connect(server, &PaymentServer::receivedPaymentRequest,
+                     &sigCatcher, &RecipientCatcher::getRecipient);
 
     // Write data to a temp file:
     QTemporaryFile f;
@@ -54,9 +54,8 @@ static SendCoinsRecipient handleRequest(PaymentServer *server,
     // will lead to a test failure anyway.
     QCoreApplication::sendEvent(&object, &event);
 
-    QObject::disconnect(server,
-                        SIGNAL(receivedPaymentRequest(SendCoinsRecipient)),
-                        &sigCatcher, SLOT(getRecipient(SendCoinsRecipient)));
+    QObject::disconnect(server, &PaymentServer::receivedPaymentRequest,
+                        &sigCatcher, &RecipientCatcher::getRecipient);
 
     // Return results from sigCatcher
     return sigCatcher.recipient;
