8fc1ee3ece76fe40a91643fd074188decd53d987
Nico Guiton <nico@bitframe.org>
2019-11-01 00:50:17 +0000

    Add a separate executable for seeder tests
    Summary:
    As requested in discussion in D4276.  This allows us to run seeder unit tests
    independently from the other unit tests run by `test_bitcoin`
    
    Depends on D4432
    
    Test Plan:
      ../configure
      make check
      src/seeder/test_bitcoin-seeder
      ../configure CXX=clang++ CC=clang
      make clean
      make check
      src/seeder/test_bitcoin-seeder
    
      ninja
      ninja check
    These should build everything including the seeder and seeder tests.
    
      ninja test_bitcoin-seeder
    This should only build `test_bitcoin-seeder`
      ninja check-bitcoin-seeder
    This should only run the `bitcoin-seeder` tests.
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, deadalnix, Fabien, jasonbcox
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, deadalnix, Fabien, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D4417

---
+++ src/seeder/test/seeder_tests.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#define BOOST_TEST_MODULE Bitcoin Seeder Test Suite
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(seeder_tests)
+
+BOOST_AUTO_TEST_CASE(test_stub) {
+    BOOST_CHECK_EQUAL(true, true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
