709706b9d79f98c68b1812ab4b0bd2b8ba5ea90b
Russell Yanofsky <russ@yanofsky.org>
2019-11-05 10:47:07 -0500

    [backport#17381 4/5] Clean up nested scope in GetReservedDestination
    Summary:
    Suggested https://github.com/bitcoin/bitcoin/pull/17304#discussion_r341194391
    by Gregory Sanders <gsanders87@gmail.com>
    
    Reason for keeping the `return true` `return false` verbosity is that more code
    will be added after the ReserveKeyFromKeyPool() call before returning.
    
    ---
    
    https://github.com/bitcoin/bitcoin/pull/17381/commits/bfd826a675445801adec86a469040f3ceb8172ee
    
    Depends on D7418
    
    Partial backport of Core [[https://github.com/bitcoin/bitcoin/pull/17381 | PR17381]]
    
    Test Plan:
      ninja check check-functional
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D7419

--- src/wallet/scriptpubkeyman.cpp
+++ src/wallet/scriptpubkeyman.cpp
@@ -217,10 +217,8 @@ bool LegacyScriptPubKeyMan::GetReservedDestination(const OutputType type,
                                                    bool internal,
                                                    int64_t &index,
                                                    CKeyPool &keypool) {
-    {
-        if (!ReserveKeyFromKeyPool(index, keypool, internal)) {
-            return false;
-        }
+    if (!ReserveKeyFromKeyPool(index, keypool, internal)) {
+        return false;
     }
     return true;
 }
