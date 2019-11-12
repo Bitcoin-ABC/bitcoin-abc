e5777f5302dd7e5e64b95495fe170c5fa94e8f3f
Nico Guiton <nico@bitframe.org>
2019-11-12 21:08:19 +0000

    Add/update copyright lines to top of seeder code
    Summary:
    Seeder files had either no copyright or only the first line of copyright
    information and therefore needed updating.
    
    Test Plan:
      make
    
    Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D4434

--- src/seeder/main.cpp
+++ src/seeder/main.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2017-2019 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <clientversion.h>
 #include <fs.h>
 #include <logging.h>
--- src/seeder/util.h
+++ src/seeder/util.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2017-2019 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef BITCOIN_SEEDER_UTIL_H
 #define BITCOIN_SEEDER_UTIL_H
 
