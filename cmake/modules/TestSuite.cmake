e06c742b1b12caa87c66ee24e73a33659c80e18e
Nico Guiton <nico@bitframe.org>
2019-11-12 00:33:46 +0000

    Added CMake function to detect BOOST_TEST_DYN_LINK
    Summary:
    Avoid code duplication by making `BOOST_TEST_DYN_LINK` detection into a CMake
    module.
    
    Test Plan:
      ninja check
    
    Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: Fabien, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D4432

--- cmake/modules/TestSuite.cmake
+++ cmake/modules/TestSuite.cmake
@@ -16,3 +16,24 @@ function(add_test_to_suite SUITE NAME)
 	add_test(${NAME} ${NAME})
 	add_dependencies("check-${SUITE}" ${NAME})
 endfunction(add_test_to_suite)
+
+function(add_boost_unit_tests_to_suite SUITE NAME)
+	add_test_to_suite(${SUITE} ${NAME} ${ARGN})
+
+	find_package(Boost 1.58 REQUIRED unit_test_framework)
+	target_link_libraries(${NAME} Boost::unit_test_framework)
+
+	# We need to detect if the BOOST_TEST_DYN_LINK flag is required
+	include(CheckCXXSourceCompiles)
+	set(CMAKE_REQUIRED_LIBRARIES Boost::unit_test_framework)
+
+	check_cxx_source_compiles("
+		#define BOOST_TEST_DYN_LINK
+		#define BOOST_TEST_MAIN
+		#include <boost/test/unit_test.hpp>
+	" BOOST_TEST_DYN_LINK)
+
+	if(BOOST_TEST_DYN_LINK)
+		target_compile_definitions(${NAME} PRIVATE BOOST_TEST_DYN_LINK)
+	endif(BOOST_TEST_DYN_LINK)
+endfunction(add_boost_unit_tests_to_suite)
