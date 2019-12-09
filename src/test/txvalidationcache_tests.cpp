6516515d7bbbedad99cee95ba1371797175a0961
Mark Lundeberg <36528214+markblundeberg@users.noreply.github.com>
2019-12-10 03:23:15 +0900

    fix incomplete txvalidationcache_tests
    Summary:
    This test was missed in the LCG updates (see D2475, D2481, D2482).
    The lack of flag range was partly responsible for the problem behind D4451
    being hidden until activation.
    
    I've also removed the `| MANDATORY` from the flag scanning and restored the
    original "Filter out incompatible flag choices" approach (reversing the
    liberty taken in backport D534). Since we have so many mandatory flags
    (soon to be even more) this makes sure the tests cover as many flag
    combinations as possible. But, since we don't just have soft-forking flags,
    it is important that tests can define the required_flags that they rely
    on for function.
    
    Test Plan: `make check`
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D4671

--- src/test/txvalidationcache_tests.cpp
+++ src/test/txvalidationcache_tests.cpp
@@ -18,6 +18,7 @@
 #include <util/time.h>
 #include <validation.h>
 
+#include <test/lcg.h>
 #include <test/sigutil.h>
 #include <test/test_bitcoin.h>
 
@@ -104,15 +105,22 @@ BOOST_FIXTURE_TEST_CASE(tx_mempool_block_doublespend, TestChain100Setup) {
 // evaluating any script flag that is implemented as an upgraded NOP code.
 static void ValidateCheckInputsForAllFlags(const CTransaction &tx,
                                            uint32_t failing_flags,
+                                           uint32_t required_flags,
                                            bool add_to_cache, bool upgraded_nop)
     EXCLUSIVE_LOCKS_REQUIRED(cs_main) {
     PrecomputedTransactionData txdata(tx);
-    // If we add many more flags, this loop can get too expensive, but we can
-    // rewrite in the future to randomly pick a set of flags to evaluate.
-    for (uint32_t test_flags = 0; test_flags < (1U << 17); test_flags += 1) {
+
+    MMIXLinearCongruentialGenerator lcg;
+    for (int i = 0; i < 4096; i++) {
+        uint32_t test_flags = lcg.next() | required_flags;
         CValidationState state;
-        // Make sure the mandatory flags are enabled.
-        test_flags |= MANDATORY_SCRIPT_VERIFY_FLAGS;
+
+        // Filter out incompatible flag choices
+        if ((test_flags & SCRIPT_VERIFY_CLEANSTACK)) {
+            // CLEANSTACK requires P2SH, see VerifyScript() in
+            // script/interpreter.cpp
+            test_flags |= SCRIPT_VERIFY_P2SH;
+        }
 
         bool ret = CheckInputs(tx, state, pcoinsTip.get(), true, test_flags,
                                true, add_to_cache, txdata, nullptr);
@@ -261,7 +269,7 @@ BOOST_FIXTURE_TEST_CASE(checkinputs_test, TestChain100Setup) {
         // later that block validation works fine in the absence of cached
         // successes.
         ValidateCheckInputsForAllFlags(
-            tx, SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS, false, false);
+            tx, SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS, 0, false, false);
     }
 
     // And if we produce a block with this tx, it should be valid, even though
@@ -288,7 +296,7 @@ BOOST_FIXTURE_TEST_CASE(checkinputs_test, TestChain100Setup) {
         invalid_under_p2sh_tx.vin[0].scriptSig << vchSig2;
 
         ValidateCheckInputsForAllFlags(CTransaction(invalid_under_p2sh_tx),
-                                       SCRIPT_VERIFY_P2SH, true, false);
+                                       SCRIPT_VERIFY_P2SH, 0, true, false);
     }
 
     // Test CHECKLOCKTIMEVERIFY
@@ -312,9 +320,10 @@ BOOST_FIXTURE_TEST_CASE(checkinputs_test, TestChain100Setup) {
         vchSig.push_back(uint8_t(SIGHASH_ALL | SIGHASH_FORKID));
         invalid_with_cltv_tx.vin[0].scriptSig = CScript() << vchSig << 101;
 
-        ValidateCheckInputsForAllFlags(CTransaction(invalid_with_cltv_tx),
-                                       SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY, true,
-                                       true);
+        ValidateCheckInputsForAllFlags(
+            CTransaction(invalid_with_cltv_tx),
+            SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY | SCRIPT_ENABLE_REPLAY_PROTECTION,
+            SCRIPT_ENABLE_SIGHASH_FORKID, true, true);
 
         // Make it valid, and check again
         invalid_with_cltv_tx.vin[0].scriptSig = CScript() << vchSig << 100;
@@ -349,9 +358,10 @@ BOOST_FIXTURE_TEST_CASE(checkinputs_test, TestChain100Setup) {
         vchSig.push_back(uint8_t(SIGHASH_ALL | SIGHASH_FORKID));
         invalid_with_csv_tx.vin[0].scriptSig = CScript() << vchSig << 101;
 
-        ValidateCheckInputsForAllFlags(CTransaction(invalid_with_csv_tx),
-                                       SCRIPT_VERIFY_CHECKSEQUENCEVERIFY, true,
-                                       true);
+        ValidateCheckInputsForAllFlags(
+            CTransaction(invalid_with_csv_tx),
+            SCRIPT_VERIFY_CHECKSEQUENCEVERIFY | SCRIPT_ENABLE_REPLAY_PROTECTION,
+            SCRIPT_ENABLE_SIGHASH_FORKID, true, true);
 
         // Make it valid, and check again
         invalid_with_csv_tx.vin[0].scriptSig = CScript() << vchSig << 100;
@@ -400,8 +410,11 @@ BOOST_FIXTURE_TEST_CASE(checkinputs_test, TestChain100Setup) {
             UpdateInput(tx.vin[1], sigdata);
         }
 
-        // This should be valid under all script flags
-        ValidateCheckInputsForAllFlags(CTransaction(tx), 0, true, false);
+        // This should be valid under all script flags that support our sighash
+        // convention.
+        ValidateCheckInputsForAllFlags(
+            CTransaction(tx), SCRIPT_ENABLE_REPLAY_PROTECTION,
+            SCRIPT_ENABLE_SIGHASH_FORKID | SCRIPT_VERIFY_P2SH, true, false);
 
         // Check that if the second input is invalid, but the first input is
         // valid, the transaction is not cached.
