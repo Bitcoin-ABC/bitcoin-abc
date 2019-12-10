a7812e1d32322b6c8e46a232f1a24d1a7a2b0dbf
Fabien <fabcien@gmail.com>
2019-12-10 17:16:02 +0100

    [CMAKE] Override default flags
    Summary:
    This is a bit of simplification for setting the default optimization
    flags value. The `OverrideInitFlags.cmake` is loaded at the `project()`
    call and is used to write the `CMAKE_<LANG>_FLAGS_<CONFIGURATION>` to
    the cache.
    This allow for removing the `NDEBUG` flag for release type
    configurations, as well as overriding the default CFLAGS for the debug
    configuration.
    
    This is a first step toward refactoring the compilation flag assignment.
    This does not change the current behavior.
    
    Test Plan:
      ninja check
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D4765

---
+++ cmake/modules/OverrideInitFlags.cmake
@@ -0,0 +1,15 @@
+# This will set the initial value for the C/CXX flags.
+# It is included at the time a project language is enabled.
+
+# This mimics the autotools behavior by setting the CFLAGS to '-g -O2`, which
+# are not well suited for debugging.
+# FIXME: update CFLAGS with better debug oriented optimization flags
+set(CMAKE_C_FLAGS_DEBUG_INIT "-g -O2")
+set(CMAKE_C_FLAGS_MINSIZEREL_INIT "-Os")
+set(CMAKE_C_FLAGS_RELEASE_INIT "-O3")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "-g -O2")
+
+set(CMAKE_CXX_FLAGS_DEBUG_INIT "-O0")
+set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT "-Os")
+set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-g -O2")
