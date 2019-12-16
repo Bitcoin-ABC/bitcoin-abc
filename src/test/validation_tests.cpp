0016b3eb56265b2370f97c215a71c6b97e261760
Jason B. Cox <contact@jasonbcox.com>
2019-12-16 15:54:31 -0800

    test: Move main_tests to validation_tests
    Summary:
    Partial backport of Core PR15504: https://github.com/bitcoin/bitcoin/pull/15504/commits/fa85468cd252f51ad5328272a1bc19b7517e204e
    
    validation_tests.cpp was already added, but those changes do not overlap with Core: D189
    
    Test Plan:
    ```
    ninja check
    make check
    ```
    
    Reviewers: #bitcoin_abc, deadalnix, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D4738

--- src/test/validation_tests.cpp
+++ src/test/validation_tests.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2011-2016 The Bitcoin Core developers
-// Copyright (c) 2017 The Bitcoin developers
+// Copyright (c) 2011-2019 The Bitcoin Core developers
+// Copyright (c) 2017-2019 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -9,17 +9,70 @@
 #include <clientversion.h>
 #include <config.h>
 #include <consensus/consensus.h>
+#include <net.h>
 #include <primitives/transaction.h>
 #include <streams.h>
 #include <util/system.h>
+#include <validation.h>
 
 #include <test/test_bitcoin.h>
 
+#include <boost/signals2/signal.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include <cstdint>
 #include <cstdio>
 #include <vector>
+
+BOOST_FIXTURE_TEST_SUITE(validation_tests, TestingSetup)
+
+static void TestBlockSubsidyHalvings(const Consensus::Params &consensusParams) {
+    int maxHalvings = 64;
+    Amount nInitialSubsidy = 50 * COIN;
+
+    // for height == 0
+    Amount nPreviousSubsidy = 2 * nInitialSubsidy;
+    BOOST_CHECK_EQUAL(nPreviousSubsidy, 2 * nInitialSubsidy);
+    for (int nHalvings = 0; nHalvings < maxHalvings; nHalvings++) {
+        int nHeight = nHalvings * consensusParams.nSubsidyHalvingInterval;
+        Amount nSubsidy = GetBlockSubsidy(nHeight, consensusParams);
+        BOOST_CHECK(nSubsidy <= nInitialSubsidy);
+        BOOST_CHECK_EQUAL(nSubsidy, nPreviousSubsidy / 2);
+        nPreviousSubsidy = nSubsidy;
+    }
+    BOOST_CHECK_EQUAL(
+        GetBlockSubsidy(maxHalvings * consensusParams.nSubsidyHalvingInterval,
+                        consensusParams),
+        Amount::zero());
+}
+
+static void TestBlockSubsidyHalvings(int nSubsidyHalvingInterval) {
+    Consensus::Params consensusParams;
+    consensusParams.nSubsidyHalvingInterval = nSubsidyHalvingInterval;
+    TestBlockSubsidyHalvings(consensusParams);
+}
+
+BOOST_AUTO_TEST_CASE(block_subsidy_test) {
+    const auto chainParams = CreateChainParams(CBaseChainParams::MAIN);
+    // As in main
+    TestBlockSubsidyHalvings(chainParams->GetConsensus());
+    // As in regtest
+    TestBlockSubsidyHalvings(150);
+    // Just another interval
+    TestBlockSubsidyHalvings(1000);
+}
+
+BOOST_AUTO_TEST_CASE(subsidy_limit_test) {
+    const auto chainParams = CreateChainParams(CBaseChainParams::MAIN);
+    Amount nSum = Amount::zero();
+    for (int nHeight = 0; nHeight < 14000000; nHeight += 1000) {
+        Amount nSubsidy = GetBlockSubsidy(nHeight, chainParams->GetConsensus());
+        BOOST_CHECK(nSubsidy <= 50 * COIN);
+        nSum += 1000 * nSubsidy;
+        BOOST_CHECK(MoneyRange(nSum));
+    }
+    BOOST_CHECK_EQUAL(nSum, int64_t(2099999997690000LL) * SATOSHI);
+}
 
 static CBlock makeLargeDummyBlock(const size_t num_tx) {
     CBlock block;
@@ -32,12 +85,12 @@ static CBlock makeLargeDummyBlock(const size_t num_tx) {
     return block;
 }
 
-BOOST_FIXTURE_TEST_SUITE(validation_tests, TestingSetup)
-
-/** Test that LoadExternalBlockFile works with the buffer size set
-below the size of a large block. Currently, LoadExternalBlockFile has the
-buffer size for CBufferedFile set to 2 * MAX_TX_SIZE. Test with a value
-of 10 * MAX_TX_SIZE. */
+/**
+ * Test that LoadExternalBlockFile works with the buffer size set below the
+ * size of a large block. Currently, LoadExternalBlockFile has the buffer size
+ * for CBufferedFile set to 2 * MAX_TX_SIZE. Test with a value of
+ * 10 * MAX_TX_SIZE.
+ */
 BOOST_AUTO_TEST_CASE(validation_load_external_block_file) {
     fs::path tmpfile_name =
         SetDataDir("validation_load_external_block_file") / "block.dat";
