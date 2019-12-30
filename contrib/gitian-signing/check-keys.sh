bc8443f73ff6ee1be1adef96cf983529a5fd62d8
Jason B. Cox <contact@jasonbcox.com>
2019-12-30 11:59:10 -0800

    Fix nits in check-keys.sh
    Summary:
    See title.
    
    Depends on D4805 for the test plan.
    
    Test Plan: `check-keys.sh`
    
    Reviewers: #bitcoin_abc, deadalnix, nakihito, Fabien
    
    Reviewed By: #bitcoin_abc, nakihito, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D4806

--- contrib/gitian-signing/check-keys.sh
+++ contrib/gitian-signing/check-keys.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+# Copyright (c) 2018-2019 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 export LC_ALL=C
 
+set -euo pipefail
+
 # Fetch latest signers' keys. We do this in order to check if a key was revoked.
-while read fingerprint _
+while read FINGERPRINT _
 do
-  gpg --recv-keys ${fingerprint}
+  gpg --recv-keys ${FINGERPRINT}
 done < ./keys.txt
