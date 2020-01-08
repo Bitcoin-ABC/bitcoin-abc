2953167fb5e4e68b9c02cc811bdbb2d0586b2140
Pieter Wuille <pieter.wuille@gmail.com>
2020-01-08 09:05:44 -0800

    Convert VARINT to the formatter/Using approach
    Summary:
    This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/17896 | PR17896]] [2/3]
    https://github.com/bitcoin/bitcoin/pull/17896/commits/2f1b2f4ed044fe005e5a6c1b55e95822e83c16df
    
    Depends on D8626
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D8627

--- src/serialize.h
+++ src/serialize.h
@@ -575,23 +575,20 @@ static inline Wrapper<Formatter, T &> Using(T &&t) {
     return Wrapper<Formatter, T &>(t);
 }
 
-#define VARINT(obj, ...) WrapVarInt<__VA_ARGS__>(REF(obj))
+#define VARINT(obj, ...) Using<VarIntFormatter<__VA_ARGS__>>(obj)
 #define COMPACTSIZE(obj) CCompactSize(REF(obj))
 #define LIMITED_STRING(obj, n) LimitedString<n>(REF(obj))
 
-template <VarIntMode Mode, typename I> class CVarInt {
-protected:
-    I &n;
-
-public:
-    explicit CVarInt(I &nIn) : n(nIn) {}
-
-    template <typename Stream> void Serialize(Stream &s) const {
-        WriteVarInt<Stream, Mode, I>(s, n);
+/**
+ * Serialization wrapper class for integers in VarInt format.
+ */
+template <VarIntMode Mode = VarIntMode::DEFAULT> struct VarIntFormatter {
+    template <typename Stream, typename I> void Ser(Stream &s, I v) {
+        WriteVarInt<Stream, Mode, typename std::remove_cv<I>::type>(s, v);
     }
 
-    template <typename Stream> void Unserialize(Stream &s) {
-        n = ReadVarInt<Stream, Mode, I>(s);
+    template <typename Stream, typename I> void Unser(Stream &s, I &v) {
+        v = ReadVarInt<Stream, Mode, typename std::remove_cv<I>::type>(s);
     }
 };
 
@@ -668,11 +665,6 @@ public:
         }
     }
 };
-
-template <VarIntMode Mode = VarIntMode::DEFAULT, typename I>
-CVarInt<Mode, I> WrapVarInt(I &n) {
-    return CVarInt<Mode, I>{n};
-}
 
 template <typename I> BigEndian<I> WrapBigEndian(I &n) {
     return BigEndian<I>(n);
