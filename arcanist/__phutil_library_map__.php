025472d9a046d89c36e7cae6ada59cdbc0300145
Fabien <fabcien@gmail.com>
2020-02-07 12:52:05 +0100

    Run the linters as part of arc land
    Summary:
    The default `arc land` workflow does not run the linters before landing.
    This can cause issues when the user issues a rebase before landing, such
    as D5192.
    
    This diff runs `arc lint --severity autofix --amend-autofix` before
    `arc land` to try to catch these issues.
    
    A `--bypass-linters` options is added to `arc land` to avoid deadlocks.
    
    Test Plan:
    Add a trailing whitespace to an arcanist PHP file, then:
      arc land --preview
    Check the linter error is returned.
      arc land --preview --bypass-linters
    Check no linter error is returned. Arc land will fail due to dirty tree.
    
    Create a local commit with a clang-format violation (such as bad header
    ordering).
      arc land --preview
    See the auto amend message. `arc land` should succeed.
    
      arc help land
    Check the `--bypass-linters` is added to the list.
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: deadalnix, markblundeberg
    
    Differential Revision: https://reviews.bitcoinabc.org/D5196

--- arcanist/__phutil_library_map__.php
+++ arcanist/__phutil_library_map__.php
@@ -9,6 +9,7 @@
 phutil_register_library_map(array(
   '__library_version__' => 2,
   'class' => array(
+    'ArcanistBitcoinABCConfiguration' => 'configuration/ArcanistBitcoinABCConfiguration.php',
     'AssertWithSideEffectsLinter' => 'linter/AssertWithSideEffectsLinter.php',
     'AutoPEP8FormatLinter' => 'linter/AutoPEP8Linter.php',
     'BashShebangLinter' => 'linter/BashShebangLinter.php',
@@ -40,6 +41,7 @@ phutil_register_library_map(array(
   ),
   'function' => array(),
   'xmap' => array(
+    'ArcanistBitcoinABCConfiguration' => 'ArcanistConfiguration',
     'AssertWithSideEffectsLinter' => 'ArcanistLinter',
     'AutoPEP8FormatLinter' => 'ArcanistExternalLinter',
     'BashShebangLinter' => 'ArcanistLinter',
---
+++ arcanist/configuration/ArcanistBitcoinABCConfiguration.php
@@ -0,0 +1,51 @@
+<?php
+
+class ArcanistBitcoinABCConfiguration extends ArcanistConfiguration {
+  public function getCustomArgumentsForCommand($command) {
+    if ($command === 'land') {
+      return array(
+        'bypass-linters' => array(
+          'help' => pht('Do not run the linters before landing.'),
+        ),
+      );
+    }
+
+    return array();
+  }
+
+  public function willRunWorkflow($command, ArcanistWorkflow $workflow) {
+    if ($command === 'land') {
+      /* Offer an way to force landing even if there is a linter issue */
+      if ($workflow->getArgument('bypass-linters')) {
+        return 0;
+      }
+
+      /*
+       * Arguments:
+       *
+       * --severity autofix: we are only interested in autofix/warning/error
+       * messages, not advice messages.
+       *
+       * --amend-autofix: since arc land expects a clean worktree, try to avoid
+       * asking the user to git add the file and run arc land again.
+       */
+      $lintWorkflow = $workflow->buildChildWorkflow(
+        'lint', array('--severity', 'autofix', '--amend-autofixes'));
+
+      $lintResult = $lintWorkflow->run();
+      $lintMessages = $lintWorkflow->getUnresolvedMessages();
+
+      /*
+       * Autofix messages return RESULT_OKAY, so check there is no remaining
+       * unresolved message.
+       */
+      if ($lintResult != ArcanistLintWorkflow::RESULT_OKAY ||
+          !empty($lintMessages)) {
+        throw new Exception(
+          pht("Please fix the linter issues before landing."));
+      }
+    }
+
+    return 0;
+  }
+}
