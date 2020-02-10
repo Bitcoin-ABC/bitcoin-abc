d5771bf781e3d36799c986fa61aa4ff4d3cef44e
Jason B. Cox <contact@jasonbcox.com>
2020-02-10 15:12:28 -0800

    Add update-seeds to automated-commits
    Summary:
    This will allow us to automatically update seeds and detangle it from our release process.
    
    Depends on D5246 and D5331
    
    Test Plan:
    ```
    curl seederdump.bitframe.org/seeds_main.txt > ~/Downloads/seeds_main.txt
    curl seederdump.bitframe.org/seeds_test.txt > ~/Downloads/seeds_test.txt
    SEEDS_MAIN=~/Downloads/seeds_main.txt SEEDS_TEST=~/Downloads/seeds_test.txt COMMIT_TYPE=update-seeds ./automated-commits.sh  # dry-run
    ```
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D5248

--- contrib/source-control-tools/automated-commits.sh
+++ contrib/source-control-tools/automated-commits.sh
@@ -78,7 +78,6 @@ export BUILD_DIR
 
 DEVTOOLS_DIR="${TOPLEVEL}"/contrib/devtools
 CHAINPARAMS_SCRIPTS_DIR="${DEVTOOLS_DIR}"/chainparams
-TEAMCITY_SCRIPTS_DIR="${TOPLEVEL}"/contrib/teamcity
 
 # Make sure tree is clean
 git checkout master
@@ -102,6 +101,31 @@ case "${COMMIT_TYPE}" in
     popd
 
     git commit -m "${BOT_PREFIX} Update chainparams"
+    ;;
+
+  update-seeds)
+    # Assumes seeder instances are already running on mainnet and testnet
+    pushd "${TOPLEVEL}"/contrib/seeds
+    : "${SEEDS_MAIN:=seeds_main.txt}"
+    ./makeseeds.py < "${SEEDS_MAIN}" > nodes_main.txt
+    git add nodes_main.txt
+
+    : "${SEEDS_TEST:=seeds_test.txt}"
+    ./makeseeds.py < "${SEEDS_TEST}" > nodes_test.txt
+    git add nodes_test.txt
+
+    SEEDS_HEADER="${TOPLEVEL}"/src/chainparamsseeds.h
+    ./generate-seeds.py . > "${SEEDS_HEADER}"
+    git add "${SEEDS_HEADER}"
+    popd
+
+    # Check that seeds have good connectivity
+    "${DEVTOOLS_DIR}"/build_cmake.sh
+    SEEDS_DIR="${TOPLEVEL}"/contrib/seeds
+    RPC_PORT=18832 "${SEEDS_DIR}"/check-seeds.sh main 80
+    RPC_PORT=18833 "${SEEDS_DIR}"/check-seeds.sh test 70
+
+    git commit -m "${BOT_PREFIX} Update seeds"
     ;;
 
   *)
