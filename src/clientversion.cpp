216cabf31fcb3756c4af5c428136185fec27ad93
wanglin5659643@gmail.com <wanglin5659643@gmail.com>
2020-02-23 16:05:24 +0800

    alter application name
    Summary:  See title
    
    Test Plan:
    ```
    make check
    ```

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -46,8 +46,8 @@ const std::string CLIENT_NAME("Freecash");
 //! 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "195a085ba"
-#define GIT_COMMIT_DATE "Sun, 16 Feb 2020 14:34:50 +0800"
+#define GIT_COMMIT_ID "$Format:%h$"
+#define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix)                   \
