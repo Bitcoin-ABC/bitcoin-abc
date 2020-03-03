ca0a03351c0dd1caff57c45a56addf90b34bc544
Andrew Chow <achow101-github@achow101.com>
2020-03-03 17:16:40 -0500

    Rename BIP32PubkeyProvider.m_extkey to m_root_extkey
    Summary:
    Renaming clarifies that m_extkey is actually the root
    extkey that keys are derived from.
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/18204 | PR18204]] part [3/7] : https://github.com/bitcoin/bitcoin/pull/18204/commits/66c2cadc91d26074b89e5ada68350b5c8676efac
    
    Depends on D8382
    
    Test Plan:
      ninja all check
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D8383

--- src/script/descriptor.cpp
+++ src/script/descriptor.cpp
@@ -307,21 +307,22 @@ enum class DeriveType {
 
 /** An object representing a parsed extended public key in a descriptor. */
 class BIP32PubkeyProvider final : public PubkeyProvider {
-    CExtPubKey m_extkey;
+    // Root xpub, path, and final derivation step type being used, if any
+    CExtPubKey m_root_extkey;
     KeyPath m_path;
     DeriveType m_derive;
 
     bool GetExtKey(const SigningProvider &arg, CExtKey &ret) const {
         CKey key;
-        if (!arg.GetKey(m_extkey.pubkey.GetID(), key)) {
+        if (!arg.GetKey(m_root_extkey.pubkey.GetID(), key)) {
             return false;
         }
-        ret.nDepth = m_extkey.nDepth;
-        std::copy(m_extkey.vchFingerprint,
-                  m_extkey.vchFingerprint + sizeof(ret.vchFingerprint),
+        ret.nDepth = m_root_extkey.nDepth;
+        std::copy(m_root_extkey.vchFingerprint,
+                  m_root_extkey.vchFingerprint + sizeof(ret.vchFingerprint),
                   ret.vchFingerprint);
-        ret.nChild = m_extkey.nChild;
-        ret.chaincode = m_extkey.chaincode;
+        ret.nChild = m_root_extkey.nChild;
+        ret.chaincode = m_root_extkey.chaincode;
         ret.key = key;
         return true;
     }
@@ -341,8 +342,8 @@ class BIP32PubkeyProvider final : public PubkeyProvider {
 public:
     BIP32PubkeyProvider(uint32_t exp_index, const CExtPubKey &extkey,
                         KeyPath path, DeriveType derive)
-        : PubkeyProvider(exp_index), m_extkey(extkey), m_path(std::move(path)),
-          m_derive(derive) {}
+        : PubkeyProvider(exp_index), m_root_extkey(extkey),
+          m_path(std::move(path)), m_derive(derive) {}
     bool IsRange() const override { return m_derive != DeriveType::NO; }
     size_t GetSize() const override { return 33; }
     bool GetPubKey(int pos, const SigningProvider &arg, CPubKey *key,
@@ -356,7 +357,7 @@ public:
                 *key = priv_key.GetPubKey();
             } else {
                 // TODO: optimize by caching
-                CExtPubKey extkey = m_extkey;
+                CExtPubKey extkey = m_root_extkey;
                 for (auto entry : m_path) {
                     extkey.Derive(extkey, entry);
                 }
@@ -367,7 +368,7 @@ public:
                 *key = extkey.pubkey;
             }
         }
-        CKeyID keyid = m_extkey.pubkey.GetID();
+        CKeyID keyid = m_root_extkey.pubkey.GetID();
         std::copy(keyid.begin(), keyid.begin() + sizeof(info.fingerprint),
                   info.fingerprint);
         info.path = m_path;
@@ -380,7 +381,8 @@ public:
         return true;
     }
     std::string ToString() const override {
-        std::string ret = EncodeExtPubKey(m_extkey) + FormatHDKeypath(m_path);
+        std::string ret =
+            EncodeExtPubKey(m_root_extkey) + FormatHDKeypath(m_path);
         if (IsRange()) {
             ret += "/*";
             if (m_derive == DeriveType::HARDENED) {
