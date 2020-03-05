f1473deabdf63aa09e1c93d18ecc8998d73c873e
Fabien <fabcien@gmail.com>
2020-03-05 16:57:16 +0100

    [CMAKE] Improve FindSHLWAPI
    Summary:
    This makes the `FindSHLWAPI` consistent with the other find modules.
    There are few benefits right now because the imported target cannot be
    used due to cmake bugs
    (https://gitlab.kitware.com/cmake/cmake/issues/16291,
    https://gitlab.kitware.com/cmake/cmake/issues/19095) but makes it future
    proof when cmake > 3.14.1 get enforced.
    
    Test Plan:
      ninja check
    
    Run the Gitian builds.
    
    I also tested using the imported target with cmake 3.14.1.
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D5453

--- cmake/modules/FindSHLWAPI.cmake
+++ cmake/modules/FindSHLWAPI.cmake
@@ -1,22 +1,41 @@
-# Try to find the SHLWAPI librairy
-# SHLWAPI_FOUND - system has SHLWAPI lib
-# SHLWAPI_INCLUDE_DIR - the SHLWAPI include directory
-# SHLWAPI_LIBRARY - Libraries needed to use SHLWAPI
+# Copyright (c) 2017-2020 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-if(SHLWAPI_INCLUDE_DIR AND SHLWAPI_LIBRARY)
-	# Already in cache, be silent
-	set(SHLWAPI_FIND_QUIETLY TRUE)
-endif()
+# .rst:
+# FindSHLWAPI
+# --------------
+#
+# Find the SHLWAPI library. The following conponents are
+# available::
+#   shlwapi
+#
+# This will define the following variables::
+#
+#   SHLWAPI_FOUND - True if the SHLWAPI library is found.
+#   SHLWAPI_INCLUDE_DIRS - List of the header include directories.
+#   SHLWAPI_LIBRARIES - List of the libraries.
+#
+# And the following imported targets::
+#
+#   SHLWAPI::shlwapi
 
-find_path(SHLWAPI_INCLUDE_DIR NAMES shlwapi.h)
-find_library(SHLWAPI_LIBRARY NAMES shlwapi)
+find_path(SHLWAPI_INCLUDE_DIR
+	NAMES shlwapi.h
+)
 
-message(STATUS "SHLWAPI lib: " ${SHLWAPI_LIBRARY})
+set(SHLWAPI_INCLUDE_DIRS "${SHLWAPI_INCLUDE_DIR}")
+mark_as_advanced(SHLWAPI_INCLUDE_DIR)
+
+include(ExternalLibraryHelper)
+find_component(SHLWAPI shlwapi
+	NAMES shlwapi
+	INCLUDE_DIRS ${SHLWAPI_INCLUDE_DIRS}
+)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP DEFAULT_MSG SHLWAPI_INCLUDE_DIR SHLWAPI_LIBRARY)
-
-mark_as_advanced(SHLWAPI_INCLUDE_DIR SHLWAPI_LIBRARY)
-
-set(SHLWAPI_LIBRARIES ${SHLWAPI_LIBRARY})
-set(SHLWAPI_INCLUDE_DIRS ${SHLWAPI_INCLUDE_DIR})
+find_package_handle_standard_args(SHLWAPI
+	REQUIRED_VARS
+		SHLWAPI_INCLUDE_DIR
+	HANDLE_COMPONENTS
+)
