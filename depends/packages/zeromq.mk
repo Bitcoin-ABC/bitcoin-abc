bcdc1c061edf73e863597329112b97884fc98275
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-03-22 20:26:06 +0200

    build: Drop all of the ZeroMQ patches
    Summary:
    ```
    This PR gets rid of the all patches for ZeroMQ:
     - the Mingw-w64 5.0 (Ubuntu 18.04 bionic) is used to build the Windows
    binaries
     - it is safe to use pthread_setname_np since #17538 when the minimal
    glibc version is set to 2.17; see: #11986 (comment)
    ```
    
    Note: Debian Buster has MinGw 6.0 and we enforce glibc >= 2.19.
    
    Backport of core [[https://github.com/bitcoin/bitcoin/pull/18405 | PR18405]].
    
    Test Plan:
      make build-all
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D7760

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -3,7 +3,6 @@ $(package)_version=4.3.1
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d835cd21eb
-$(package)_patches=0001-fix-build-with-older-mingw64.patch 0002-disable-pthread_set_name_np.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--without-docs --disable-shared --disable-curve --disable-curve-keygen --disable-perf
@@ -12,11 +11,6 @@ define $(package)_set_vars
   $(package)_config_opts += --disable-Werror --disable-drafts --enable-option-checking
   $(package)_config_opts_linux=--with-pic
   $(package)_cxxflags=-std=c++11
-endef
-
-define $(package)_preprocess_cmds
-   patch -p1 < $($(package)_patch_dir)/0001-fix-build-with-older-mingw64.patch && \
-   patch -p1 < $($(package)_patch_dir)/0002-disable-pthread_set_name_np.patch
 endef
 
 define $(package)_config_cmds
