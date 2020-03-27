e409be178eae211ac377a1bbddaffa969421cdba
Fabien <fabcien@gmail.com>
2020-03-27 13:15:44 +0100

    Disable thread_local for i686-mingw
    Summary:
    This is a follow-up for D5540. Despite the tests passing, the diff is
    causing bitcoin-qt to fail quickly. Disabling thread_local for this
    compiler fixes the issue.
    
    The code is extracted from [[https://github.com/bitcoin/bitcoin/pull/15849 | PR15849]], but the tests for thread_local is a
    simplified version.
    
    Test Plan:
    Build for win32 with autotools and cmake. Run bitcoin-qt and check it
    doesn't crash.
    
    As a sanity check, build for win64 and any linux and check
    `thread_local` is enabled.
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Subscribers: jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D5587

--- src/util/threadnames.cpp
+++ src/util/threadnames.cpp
@@ -35,6 +35,10 @@ static void SetThreadName(const char *name) {
 #endif
 }
 
+// If we have thread_local, just keep thread ID and name in a thread_local
+// global.
+#if defined(HAVE_THREAD_LOCAL)
+
 static thread_local std::string g_thread_name;
 const std::string &util::ThreadGetInternalName() {
     return g_thread_name;
@@ -44,6 +48,17 @@ const std::string &util::ThreadGetInternalName() {
 static void SetInternalName(std::string name) {
     g_thread_name = std::move(name);
 }
+
+// Without thread_local available, don't handle internal name at all.
+#else
+
+static const std::string empty_string;
+const std::string &util::ThreadGetInternalName() {
+    return empty_string;
+}
+static void SetInternalName(std::string name) {}
+
+#endif // defined(HAVE_THREAD_LOCAL)
 
 void util::ThreadRename(std::string &&name) {
     SetThreadName(("b-" + name).c_str());
