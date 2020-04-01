6330d8ccc8b1b720c42c8c9239dadc8240ca5025
Fabien <fabcien@gmail.com>
2020-04-01 15:08:03 +0200

    Revert "Disable thread_local for i686-mingw"
    Summary:
    This reverts commit e409be178eae211ac377a1bbddaffa969421cdba.
    
    It is no longer required after D5612.
    Depends on D5612.
    
    Test Plan:
    Build for windows (64 bits obviously).
    Run bitcoin-qt.exe and test_bitcoin.exe.
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D5635

--- src/util/threadnames.cpp
+++ src/util/threadnames.cpp
@@ -35,10 +35,6 @@ static void SetThreadName(const char *name) {
 #endif
 }
 
-// If we have thread_local, just keep thread ID and name in a thread_local
-// global.
-#if defined(HAVE_THREAD_LOCAL)
-
 static thread_local std::string g_thread_name;
 const std::string &util::ThreadGetInternalName() {
     return g_thread_name;
@@ -48,17 +44,6 @@ const std::string &util::ThreadGetInternalName() {
 static void SetInternalName(std::string name) {
     g_thread_name = std::move(name);
 }
-
-// Without thread_local available, don't handle internal name at all.
-#else
-
-static const std::string empty_string;
-const std::string &util::ThreadGetInternalName() {
-    return empty_string;
-}
-static void SetInternalName(std::string name) {}
-
-#endif // defined(HAVE_THREAD_LOCAL)
 
 void util::ThreadRename(std::string &&name) {
     SetThreadName(("b-" + name).c_str());
