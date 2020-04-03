1eea7d56ce9968b6f5b1ad2fb60947ca147d1992
Amaury Séchet <deadalnix@gmail.com>
2020-04-03 17:58:17 +0200

    [avalanche] Add test for the parking scenario
    Summary: Now that we poll all blocks, the parking code is reachable and deserve to be tested.
    
    Test Plan:
      ./test/functional/test_runner.py abc_p2p_avalanche
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Subscribers: majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D6731

--- test/functional/abc_p2p_avalanche.py
+++ test/functional/abc_p2p_avalanche.py
@@ -77,7 +77,7 @@ class AvalancheTest(BitcoinTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 2
         self.extra_args = [
-            ['-enableavalanche=1', '-avacooldown=0'],
+            ['-enableavalanche=1', '-avacooldown=0', '-automaticunparking=0'],
             ['-enableavalanche=1', '-avacooldown=0', '-noparkdeepreorg', '-maxreorgdepth=-1']]
 
     def run_test(self):
@@ -245,6 +245,23 @@ class AvalancheTest(BitcoinTestFramework):
 
         # Because everybody answers yes, the node will accept that block.
         wait_until(has_accepted_new_tip, timeout=15)
+        assert_equal(node.getbestblockhash(), fork_tip)
+
+        self.log.info("Answer all polls to park...")
+        node.generate(1)
+
+        tip_to_park = node.getbestblockhash()
+        self.log.info(tip_to_park)
+
+        hash_to_find = int(tip_to_park, 16)
+        assert(tip_to_park != fork_tip)
+
+        def has_parked_new_tip():
+            can_find_block_in_poll(hash_to_find, BLOCK_REJECTED)
+            return node.getbestblockhash() == fork_tip
+
+        # Because everybody answers no, the node will park that block.
+        wait_until(has_parked_new_tip, timeout=15)
         assert_equal(node.getbestblockhash(), fork_tip)
 
 
