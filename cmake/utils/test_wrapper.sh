5c45fb1195209a40048922198313eed37861f4f2
Fabien <fabcien@gmail.com>
2020-04-29 12:18:56 +0200

    [CMAKE] Use the crosscompiling emulator to run the tests
    Summary:
    This makes the `test_bitcoin` check targets honor the
    `CMAKE_CROSSCOMPILER_EMULATOR` if given. This is useful for running the
    ARM/AArch64 binaries with qemu.
    
    Test Plan:
    Prerequisite
      sudo apt install qemu-user-static
    Then:
      cmake -GNinja .. \
        -DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/LinuxAArch64.cmake \
        -DCMAKE_CROSSCOMPILING_EMULATOR=$(command -v qemu-aarch64-static)
      QEMU_LD_PREFIX=/usr/aarch64-linux-gnu ninja check-bitcoin
    
    Sanity check for regression:
      cmake -GNinja ..
      ninja check
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D5877

--- cmake/utils/test_wrapper.sh
+++ cmake/utils/test_wrapper.sh
@@ -7,8 +7,7 @@ export LC_ALL=C.UTF-8
 # The stdout and stderr outputs are redirected to the <log> file, which is only
 # printed on error.
 
-EXECUTABLE="$1"
-LOG="$2"
-shift 2
+LOG="$1"
+shift 1
 
-"${EXECUTABLE}" "$@" > "${LOG}" 2>&1 || (cat "${LOG}" && exit 1)
+"$@" > "${LOG}" 2>&1 || (cat "${LOG}" && exit 1)
