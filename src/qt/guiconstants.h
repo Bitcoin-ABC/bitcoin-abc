hebasto/bitcoin-qtw

gui: Fix for Incorrect application name when passing -regtest

src/qt/guiconstants.h
@@ -50,5 +50,6 @@ static const int MAX_URI_LENGTH = 255;
 #define QAPP_ORG_DOMAIN "bitcoin.org"
 #define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
 #define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
+#define QAPP_APP_NAME_REGTEST "Bitcoin-Qt-regtest"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H

Bitcoin-ABC/bitcoin-abc

Summary: This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/15085 | PR15085]]
Test Plan: Run bitcoin-qt on regtest on win64
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D5887

src/qt/guiconstants.h
@@ -50,5 +50,6 @@ static const int MAX_URI_LENGTH = 255;
 #define QAPP_ORG_DOMAIN "bitcoinabc.org"
 #define QAPP_APP_NAME_DEFAULT "BitcoinABC-Qt"
 #define QAPP_APP_NAME_TESTNET "BitcoinABC-Qt-testnet"
+#define QAPP_APP_NAME_REGTEST "BitcoinABC-Qt-regtest"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H