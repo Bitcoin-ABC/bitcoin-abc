e2c542d2fafa3a9e1570660caa6da2a21cec2c21
Amaury Séchet <deadalnix@gmail.com>
2020-05-10 16:15:30 +0200

    Remove last vestige of the alert system
    Summary: Node supporting that feature cannot sync our chain anwyays.
    
    Test Plan:
      ninja all check-all
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D6021

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2289,22 +2289,6 @@ static bool ProcessMessage(const Config &config, CNode *pfrom,
         pfrom->nTimeOffset = nTimeOffset;
         AddTimeData(pfrom->addr, nTimeOffset);
 
-        // If the peer is old enough to have the old alert system, send it the
-        // final alert.
-        if (pfrom->nVersion <= 70012) {
-            CDataStream finalAlert(
-                ParseHex(
-                    "60010000000000000000000000ffffff7f00000000ffffff7ffeffff7f"
-                    "01ffffff7f00000000ffffff7f00ffffff7f002f555247454e543a2041"
-                    "6c657274206b657920636f6d70726f6d697365642c2075706772616465"
-                    "207265717569726564004630440220653febd6410f470f6bae11cad19c"
-                    "48413becb1ac2c17f908fd0fd53bdc3abd5202206d0e9c96fe88d4a0f0"
-                    "1ed9dedae2b6f9e00da94cad0fecaae66ecf689bf71b50"),
-                SER_NETWORK, PROTOCOL_VERSION);
-            connman->PushMessage(
-                pfrom, CNetMsgMaker(nSendVersion).Make("alert", finalAlert));
-        }
-
         // Feeler connections exist only to verify if address is online.
         if (pfrom->fFeeler) {
             assert(pfrom->fInbound == false);
