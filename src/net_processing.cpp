db81f26de6541e01c8cc8ec5bd4f8747266b5aad
fanquake <fanquake@gmail.com>
2020-05-19 14:50:07 +0800

    [backport#18861] Do not answer GETDATA for to-be-announced tx
    Summary:
    2896c412fadbc03916a33028f4f50fd87ac48edb Do not answer GETDATA for to-be-announced tx (Pieter Wuille)
    f2f32a3dee9a965c8198f9ddd3aaebc627c273e4 Push down use of cs_main into FindTxForGetData (Pieter Wuille)
    c6131bf407c1ada78a0e5509a702bc7da0bfd57d Abstract logic to determine whether to answer tx GETDATA (Pieter Wuille)
    
    Pull request description:
    
      This PR intends to improve transaction-origin privacy.
    
      In general, we should try to not leak information about what transactions we have (recently) learned about before deciding to announce them to our peers. There is a controlled transaction dissemination process that reveals our transactions to peers that has various safeguards for privacy (it's rate-limited, delayed & batched, deterministically sorted, ...), and ideally there is no way to test which transactions we have before that controlled process reveals them. The handling of the `mempool` BIP35 message has protections in this regard as well, as it would be an obvious way to bypass these protections (handled asynchronously after a delay, also deterministically sorted).
    
      However, currently, if we receive a GETDATA for a transaction that we have not yet announced to the requester, we will still respond to it if it was announced to *some* other peer already (because it needs to be in `mapRelay`, which only happens on the first announcement). This is a slight privacy leak.
    
      Thankfully, this seems easy to solve: `setInventontoryTxToSend` keeps track of the txids we have yet to announce to a peer - which almost(*) exactly corresponds to the transactions we know of that we haven't revealed to that peer. By checking whether a txid is in that set before responding to a GETDATA, we can filter these out.
    
      (*) Locally resubmitted or rebroadcasted transactions may end up in setInventoryTxToSend while the peer already knows we have them, which could result in us incorrectly claiming we don't have such transactions if coincidentally requested right after we schedule reannouncing them, but before they're actually INVed. This is made even harder by the fact that filterInventoryKnown will generally keep known reannouncements out of setInventoryTxToSend unless it overflows (which needs 50000 INVs in either direction before it happens).
    
      The condition for responding now becomes:
    
      ```
        (not in setInventoryTxToSend) AND
        (
          (in relay map) OR
          (
            (in mempool) AND
            (old enough that it could have expired from relay map) AND
            (older than our last getmempool response)
          )
        )
      ```
    
    ---
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/18861 | PR18861]]
    
    notes:
    * uses references to CNode rather than pointers as in the original PR, see D8480
    * changed the CNode* parameter in FindTxForGet Data to const CNode& because that's what it is
    
    Test Plan:
      CC=clang CXX=clang++ cmake -GNinja -DENABLE_SANITIZERS=thread
      ninja check-all
    
      cmake -GNinja -DCMAKE_BUILD_TYPE=Debug
      ninja check-all
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D9162

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1971,6 +1971,44 @@ static void ProcessGetBlockData(const Config &config, CNode &pfrom,
     }
 }
 
+//! Determine whether or not a peer can request a transaction, and return it (or
+//! nullptr if not found or not allowed).
+CTransactionRef static FindTxForGetData(
+    const CNode &peer, const TxId &txid, const std::chrono::seconds mempool_req,
+    const std::chrono::seconds longlived_mempool_time) LOCKS_EXCLUDED(cs_main) {
+    // Check if the requested transaction is so recent that we're just
+    // about to announce it to the peer; if so, they certainly shouldn't
+    // know we already have it.
+    {
+        LOCK(peer.m_tx_relay->cs_tx_inventory);
+        if (peer.m_tx_relay->setInventoryTxToSend.count(txid)) {
+            return {};
+        }
+    }
+
+    {
+        LOCK(cs_main);
+        // Look up transaction in relay pool
+        auto mi = mapRelay.find(txid);
+        if (mi != mapRelay.end()) {
+            return mi->second;
+        }
+    }
+
+    auto txinfo = g_mempool.info(txid);
+    if (txinfo.tx) {
+        // To protect privacy, do not answer getdata using the mempool when
+        // that TX couldn't have been INVed in reply to a MEMPOOL request,
+        // or when it's too recent to have expired from mapRelay.
+        if ((mempool_req.count() && txinfo.m_time <= mempool_req) ||
+            txinfo.m_time <= longlived_mempool_time) {
+            return txinfo.tx;
+        }
+    }
+
+    return {};
+}
+
 static void ProcessGetData(const Config &config, CNode &pfrom,
                            CConnman &connman, CTxMemPool &mempool,
                            const std::atomic<bool> &interruptMsgProc)
@@ -1990,65 +2028,38 @@ static void ProcessGetData(const Config &config, CNode &pfrom,
             ? pfrom.m_tx_relay->m_last_mempool_req.load()
             : std::chrono::seconds::min();
 
-    {
-        LOCK(cs_main);
-
-        // Process as many TX items from the front of the getdata queue as
-        // possible, since they're common and it's efficient to batch process
-        // them.
-        while (it != pfrom.vRecvGetData.end() && it->type == MSG_TX) {
-            if (interruptMsgProc) {
-                return;
-            }
-            // The send buffer provides backpressure. If there's no space in
-            // the buffer, pause processing until the next call.
-            if (pfrom.fPauseSend) {
-                break;
-            }
-
-            const CInv &inv = *it++;
-
-            if (pfrom.m_tx_relay == nullptr) {
-                // Ignore GETDATA requests for transactions from blocks-only
-                // peers.
-                continue;
-            }
-
-            // Send stream from relay memory
-            bool push = false;
-            auto mi = mapRelay.find(inv.hash);
-            int nSendFlags = 0;
-            if (mi != mapRelay.end()) {
-                connman.PushMessage(
-                    &pfrom,
-                    msgMaker.Make(nSendFlags, NetMsgType::TX, *mi->second));
-                push = true;
-            } else {
-                auto txinfo = mempool.info(TxId(inv.hash));
-                // To protect privacy, do not answer getdata using the mempool
-                // when that TX couldn't have been INVed in reply to a MEMPOOL
-                // request, or when it's too recent to have expired from
-                // mapRelay.
-                if (txinfo.tx &&
-                    ((mempool_req.count() && txinfo.m_time <= mempool_req) ||
-                     (txinfo.m_time <= longlived_mempool_time))) {
-                    connman.PushMessage(
-                        &pfrom,
-                        msgMaker.Make(nSendFlags, NetMsgType::TX, *txinfo.tx));
-                    push = true;
-                }
-            }
-
-            if (push) {
-                // We interpret fulfilling a GETDATA for a transaction as a
-                // successful initial broadcast and remove it from our
-                // unbroadcast set.
-                mempool.RemoveUnbroadcastTx(TxId(inv.hash));
-            } else {
-                vNotFound.push_back(inv);
-            }
+    // Process as many TX items from the front of the getdata queue as
+    // possible, since they're common and it's efficient to batch process
+    // them.
+    while (it != pfrom.vRecvGetData.end() && it->type == MSG_TX) {
+        if (interruptMsgProc) {
+            return;
         }
-    } // release cs_main
+        // The send buffer provides backpressure. If there's no space in
+        // the buffer, pause processing until the next call.
+        if (pfrom.fPauseSend) {
+            break;
+        }
+
+        const CInv &inv = *it++;
+
+        if (pfrom.m_tx_relay == nullptr) {
+            // Ignore GETDATA requests for transactions from blocks-only
+            // peers.
+            continue;
+        }
+
+        CTransactionRef tx = FindTxForGetData(
+            pfrom, TxId{inv.hash}, mempool_req, longlived_mempool_time);
+        if (tx) {
+            int nSendFlags = 0;
+            connman.PushMessage(&pfrom,
+                                msgMaker.Make(nSendFlags, NetMsgType::TX, *tx));
+            mempool.RemoveUnbroadcastTx(TxId(inv.hash));
+        } else {
+            vNotFound.push_back(inv);
+        }
+    }
 
     // Only process one BLOCK item per call, since they're uncommon and can be
     // expensive to process.
