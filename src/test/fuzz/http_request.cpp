dafd2b7224b5c05879deaf31f7071a18495343f8
practicalswift <practicalswift@users.noreply.github.com>
2020-06-02 09:34:12 +0000

    tests: Avoid fuzzer-specific nullptr dereference in libevent when handling PROXY requests
    Summary:
    ```
    The dereference (req->evcon->http_server) takes place in
    evhttp_parse_request_line and is a consequence of our hacky but
    necessary use of the internal function evhttp_parse_firstline_ in the
    http_request fuzzing harness.
    
    The suggested workaround is not aesthetically pleasing, but it
    successfully avoids the troublesome code path.
    
    " http:// HTTP/1.1\n" was a crashing input prior to this workaround.
    ```
    
    Backport of core [[https://github.com/bitcoin/bitcoin/pull/19140 | PR19140]].
    
    Depends on D9001.
    
    Test Plan:
      ninja bitcoin-fuzzers
      echo " http:// HTTP/1.1" > input
      ./src/test/fuzz/http_request input
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D9002

--- src/test/fuzz/http_request.cpp
+++ src/test/fuzz/http_request.cpp
@@ -4,6 +4,7 @@
 
 #include <httpserver.h>
 #include <netaddress.h>
+#include <util/strencodings.h>
 
 #include <test/fuzz/FuzzedDataProvider.h>
 #include <test/fuzz/fuzz.h>
@@ -51,7 +52,19 @@ void test_one_input(const std::vector<uint8_t> &buffer) {
     const std::vector<uint8_t> http_buffer =
         ConsumeRandomLengthByteVector(fuzzed_data_provider, 4096);
     evbuffer_add(evbuf, http_buffer.data(), http_buffer.size());
-    if (evhttp_parse_firstline_(evreq, evbuf) != 1 ||
+    // Avoid constructing requests that will be interpreted by libevent as PROXY
+    // requests to avoid triggering a nullptr dereference. The dereference
+    // (req->evcon->http_server) takes place in evhttp_parse_request_line and is
+    // a consequence of our hacky but necessary use of the internal function
+    // evhttp_parse_firstline_ in this fuzzing harness. The workaround is not
+    // aesthetically pleasing, but it successfully avoids the troublesome code
+    // path. " http:// HTTP/1.1\n" was a crashing input prior to this
+    // workaround.
+    const std::string http_buffer_str =
+        ToLower({http_buffer.begin(), http_buffer.end()});
+    if (http_buffer_str.find(" http://") != std::string::npos ||
+        http_buffer_str.find(" https://") != std::string::npos ||
+        evhttp_parse_firstline_(evreq, evbuf) != 1 ||
         evhttp_parse_headers_(evreq, evbuf) != 1) {
         evbuffer_free(evbuf);
         evhttp_request_free(evreq);
