4d0d32567acc74e5cbbbc7f01748aff0723dbf07
Pieter Wuille <pieter@wuille.net>
2020-06-08 11:24:42 -0700

    Call SHA256AutoDetect in benchmark setup
    Summary:
    > It seems SHA256AutoDetect() was not being called in benchmarks, making the numbers only reflect the naive implementation. Fix this by calling it in bench_bitcoin's setup.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/19214 | core#19214]]
    
    Test Plan:
    `ninja bench-bitcoin`
    
    I don't see a significant difference in the SHA256 becnhmarks before or
    after this commit.
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D10004

--- src/bench/bench_bitcoin.cpp
+++ src/bench/bench_bitcoin.cpp
@@ -4,6 +4,7 @@
 
 #include <bench/bench.h>
 
+#include <crypto/sha256.h>
 #include <util/strencodings.h>
 #include <util/system.h>
 
@@ -50,6 +51,7 @@ static std::vector<double> parseAsymptote(const std::string &str) {
 int main(int argc, char **argv) {
     ArgsManager argsman;
     SetupBenchArgs(argsman);
+    SHA256AutoDetect();
     std::string error;
     if (!argsman.ParseParameters(argc, argv, error)) {
         tfm::format(std::cerr, "Error parsing command line arguments: %s\n",
