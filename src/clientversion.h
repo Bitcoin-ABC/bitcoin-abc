d2d485e2b1a0f12a2b9f9575212e6e8e73eb3617
Amaury Séchet <deadalnix@gmail.com>
2020-06-14 15:34:07 +0200

    Make CLIENT_VERSION constexpr
    Summary: As per title.
    
    Test Plan:
      ninja
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D6565

--- src/clientversion.h
+++ src/clientversion.h
@@ -41,9 +41,9 @@
 #include <string>
 #include <vector>
 
-static const int CLIENT_VERSION = 1000000 * CLIENT_VERSION_MAJOR +
-                                  10000 * CLIENT_VERSION_MINOR +
-                                  100 * CLIENT_VERSION_REVISION;
+static constexpr int CLIENT_VERSION = 1000000 * CLIENT_VERSION_MAJOR +
+                                      10000 * CLIENT_VERSION_MINOR +
+                                      100 * CLIENT_VERSION_REVISION;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
