ff275be8629a3d7c4777fe4710f5738dcee4bbac
Fabien <fabcien@gmail.com>
2020-06-15 16:40:12 +0200

    [Qt] Fix deprecated QDateTime(const QDate &)
    Summary:
    Qt 5.15 has deprecated the construction of a `QDateTime` object from a
    `QDate`. This was used in our codebase to build a `QDateTime`
    corresponding to the start of the day for the given `QDate`, but Qt
    introduced a dedicated method for this use case since 5.14.
    
    Test Plan:
    With Qt 5.5.1 and Qt 5.15:
      ninja all check
    
    Run bitcoin-qt and check the transaction view filter still works as
    expected (including with an arbitrary range input).
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D6575

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -311,7 +311,19 @@ void TransactionView::chooseDate(int idx) {
         return;
     }
 
-    QDate current = QDate::currentDate();
+    const QDate currentDate = QDate::currentDate();
+
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
+    const QDateTime startofDay = currentDate.startOfDay();
+#else
+    const QDateTime startofDay = QDateTime(currentDate);
+#endif
+    const QDateTime startOfWeek =
+        startofDay.addDays(-(currentDate.dayOfWeek() - 1));
+    const QDateTime startOfMonth = startofDay.addDays(-(currentDate.day() - 1));
+    const QDateTime startOfYear =
+        startofDay.addDays(-(currentDate.dayOfYear() - 1));
+
     dateRangeWidget->setVisible(false);
     switch (dateWidget->itemData(idx).toInt()) {
         case All:
@@ -321,30 +333,28 @@ void TransactionView::chooseDate(int idx) {
             break;
         case Today:
             transactionProxyModel->setDateRange(
-                QDateTime(current), TransactionFilterProxy::MAX_DATE);
+                startofDay, TransactionFilterProxy::MAX_DATE);
             break;
         case ThisWeek: {
             // Find last Monday
-            QDate startOfWeek = current.addDays(-(current.dayOfWeek() - 1));
+
             transactionProxyModel->setDateRange(
-                QDateTime(startOfWeek), TransactionFilterProxy::MAX_DATE);
+                startOfWeek, TransactionFilterProxy::MAX_DATE);
 
         } break;
         case ThisMonth:
+
             transactionProxyModel->setDateRange(
-                QDateTime(QDate(current.year(), current.month(), 1)),
-                TransactionFilterProxy::MAX_DATE);
+                startOfMonth, TransactionFilterProxy::MAX_DATE);
             break;
         case LastMonth:
-            transactionProxyModel->setDateRange(
-                QDateTime(
-                    QDate(current.year(), current.month(), 1).addMonths(-1)),
-                QDateTime(QDate(current.year(), current.month(), 1)));
+            transactionProxyModel->setDateRange(startOfMonth.addMonths(-1),
+                                                startOfMonth);
             break;
         case ThisYear:
+
             transactionProxyModel->setDateRange(
-                QDateTime(QDate(current.year(), 1, 1)),
-                TransactionFilterProxy::MAX_DATE);
+                startOfYear, TransactionFilterProxy::MAX_DATE);
             break;
         case Range:
             dateRangeWidget->setVisible(true);
@@ -630,8 +640,15 @@ void TransactionView::dateRangeChanged() {
         return;
     }
 
-    transactionProxyModel->setDateRange(QDateTime(dateFrom->date()),
-                                        QDateTime(dateTo->date()).addDays(1));
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
+    const QDateTime rangeFrom = dateFrom->date().startOfDay();
+    const QDateTime rangeTo = dateTo->date().endOfDay();
+#else
+    const QDateTime rangeFrom = QDateTime(dateFrom->date());
+    const QDateTime rangeTo = QDateTime(dateTo->date()).addDays(1);
+#endif
+
+    transactionProxyModel->setDateRange(rangeFrom, rangeTo);
 }
 
 void TransactionView::focusTransaction(const QModelIndex &idx) {
