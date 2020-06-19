a9803b74b01853a13ef35ec5cba4cf943dc1621f
Vasil Dimov <vd@FreeBSD.org>
2020-06-19 20:51:16 +0200

    sync: detect double lock from the same thread
    Summary:
    > sync: make EnterCritical() & push_lock() type safe
    >
    > The functions `EnterCritical()` and `push_lock()` take a pointer to a
    > mutex, but that pointer used to be of type `void*` because we use a few
    > different types for mutexes. This `void*` argument was not type safe
    > because somebody could have send a pointer to anything that is not a
    > mutex. Furthermore it wouldn't allow to check whether the passed mutex
    > is recursive or not.
    >
    > Thus, change the functions to templated ones so that we can implement
    > stricter checks for non-recursive mutexes. This also simplifies the
    > callers of `EnterCritical()`.
    
    > sync: detect double lock from the same thread
    >
    > Double lock of the same (non-recursive) mutex from the same thread
    > is producing an undefined behavior. Detect this from DEBUG_LOCKORDER
    > and react similarly to the deadlock detection.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/19337 | core#19337]] and [[https://github.com/bitcoin/bitcoin/pull/20507 | core#20507]]
    
    Note that `boost::mutex` is no longer in use since D13966
    
    Test Plan:
    ```
    cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
    ninja all check-all
    ```
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14388

--- src/sync.h
+++ src/sync.h
@@ -53,8 +53,9 @@ LEAVE_CRITICAL_SECTION(mutex); // no RAII
 ///////////////////////////////
 
 #ifdef DEBUG_LOCKORDER
+template <typename MutexType>
 void EnterCritical(const char *pszName, const char *pszFile, int nLine,
-                   void *cs, bool fTry = false);
+                   MutexType *cs, bool fTry = false);
 void LeaveCritical();
 void CheckLastCritical(void *cs, std::string &lockname, const char *guardname,
                        const char *file, int line);
@@ -75,8 +76,9 @@ bool LockStackEmpty();
  */
 extern bool g_debug_lockorder_abort;
 #else
+template <typename MutexType>
 inline void EnterCritical(const char *pszName, const char *pszFile, int nLine,
-                          void *cs, bool fTry = false) {}
+                          MutexType *cs, bool fTry = false) {}
 inline void LeaveCritical() {}
 inline void CheckLastCritical(void *cs, std::string &lockname,
                               const char *guardname, const char *file,
@@ -137,7 +139,7 @@ template <typename Mutex, typename Base = typename Mutex::UniqueLock>
 class SCOPED_LOCKABLE UniqueLock : public Base {
 private:
     void Enter(const char *pszName, const char *pszFile, int nLine) {
-        EnterCritical(pszName, pszFile, nLine, (void *)(Base::mutex()));
+        EnterCritical(pszName, pszFile, nLine, Base::mutex());
 #ifdef DEBUG_LOCKCONTENTION
         if (Base::try_lock()) {
             return;
@@ -150,7 +152,7 @@ private:
     }
 
     bool TryEnter(const char *pszName, const char *pszFile, int nLine) {
-        EnterCritical(pszName, pszFile, nLine, (void *)(Base::mutex()), true);
+        EnterCritical(pszName, pszFile, nLine, Base::mutex(), true);
         if (Base::try_lock()) {
             return true;
         }
@@ -214,8 +216,7 @@ public:
 
         ~reverse_lock() {
             templock.swap(lock);
-            EnterCritical(lockname.c_str(), file.c_str(), line,
-                          (void *)lock.mutex());
+            EnterCritical(lockname.c_str(), file.c_str(), line, lock.mutex());
             lock.lock();
         }
 
@@ -253,7 +254,7 @@ using DebugLock = UniqueLock<typename std::remove_reference<
 
 #define ENTER_CRITICAL_SECTION(cs)                                             \
     {                                                                          \
-        EnterCritical(#cs, __FILE__, __LINE__, (void *)(&cs));                 \
+        EnterCritical(#cs, __FILE__, __LINE__, &cs);                           \
         (cs).lock();                                                           \
     }
 
