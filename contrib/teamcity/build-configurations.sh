2d11352f940d5eef07ad32958423244ddf4b6eff
Fabien <fabcien@gmail.com>
2020-06-23 15:56:26 +0200

    [CI] Make build-configurations.sh take the build name as an argument
    Summary:
    This is a much more natural way to call the script:
      ./build-configurations.sh build-asan
    rather than:
      ABC_BUILD_NAME=build-asan ./build-configurations.sh
    
    The environment variable is still accepted in order to maintain
    compatibility with existing infra.
    
    Test Plan:
      ./build-configurations.sh build-clang-10
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D6681

--- contrib/teamcity/build-configurations.sh
+++ contrib/teamcity/build-configurations.sh
@@ -5,6 +5,12 @@ export LC_ALL=C.UTF-8
 set -euxo pipefail
 
 : "${ABC_BUILD_NAME:=""}"
+
+if [ $# -ge 1 ]; then
+  ABC_BUILD_NAME="$1"
+  shift
+fi
+
 if [ -z "${ABC_BUILD_NAME}" ]; then
   echo "Error: Environment variable ABC_BUILD_NAME must be set"
   exit 1
