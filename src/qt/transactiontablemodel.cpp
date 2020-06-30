c3833ecf947cf40f9ff8653b043966cba1e13d7d
MarcoFalke <falke.marco@gmail.com>
2020-06-30 19:40:41 -0400

    doc: Remove outdated comment in TransactionTablePriv
    Summary: This is a backport of [[https://github.com/bitcoin-core/gui/pull/17 | core-gui#17]]
    
    Test Plan: Proofreading
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D9973

--- src/qt/transactiontablemodel.cpp
+++ src/qt/transactiontablemodel.cpp
@@ -176,13 +176,9 @@ public:
         if (idx >= 0 && idx < cachedWallet.size()) {
             TransactionRecord *rec = &cachedWallet[idx];
 
-            // Get required locks upfront. This avoids the GUI from getting
-            // stuck if the core is holding the locks for a longer time - for
-            // example, during a wallet rescan.
-            //
             // If a status update is needed (blocks came in since last check),
-            // update the status of this transaction from the wallet. Otherwise,
-            // simply re-use the cached status.
+            // try to update the status of this transaction from the wallet.
+            // Otherwise, simply re-use the cached status.
             interfaces::WalletTxStatus wtx;
             int numBlocks;
             int64_t block_time;
