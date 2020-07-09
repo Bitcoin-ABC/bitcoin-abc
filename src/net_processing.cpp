bitcoin/bitcoin

[validation] Remove unused first_invalid parameter from ProcessNewBlockHeaders()
No callers use the returned value in first_invalid. Remove it from the
function signature and don't set it in the function.

src/net_processing.cpp
@@ -1688,8 +1688,7 @@ bool static ProcessHeadersMessage(CNode *pfrom, CConnman *connman, const std::ve
     }
 
     BlockValidationState state;
-    CBlockHeader first_invalid_header;
-    if (!ProcessNewBlockHeaders(headers, state, chainparams, &pindexLast, &first_invalid_header)) {
+    if (!ProcessNewBlockHeaders(headers, state, chainparams, &pindexLast)) {
         if (state.IsInvalid()) {
             MaybePunishNodeForBlock(pfrom->GetId(), state, via_compact_block, "invalid header received");
             return false;

Bitcoin-ABC/bitcoin-abc

Summary:
Partial backport of Core [[https://github.com/bitcoin/bitcoin/pull/15921 | PR15921]] : https://github.com/bitcoin/bitcoin/pull/15921/commits/c428622a5bb1e37b2e6ab2c52791ac05d9271238
Depends on D6873 only to be lint-free.
Test Plan: `ninja check`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D6874

src/net_processing.cpp
@@ -1988,9 +1988,7 @@ static bool ProcessHeadersMessage(const Config &config, CNode *pfrom,
     }
 
     CValidationState state;
-    CBlockHeader first_invalid_header;
-    if (!ProcessNewBlockHeaders(config, headers, state, &pindexLast,
-                                &first_invalid_header)) {
+    if (!ProcessNewBlockHeaders(config, headers, state, &pindexLast)) {
         if (state.IsInvalid()) {
             MaybePunishNode(pfrom->GetId(), state, via_compact_block,
                             "invalid header received");