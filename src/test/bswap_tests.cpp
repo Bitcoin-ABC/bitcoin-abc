3af9381d54337c0676366e89415fa831da3b8f7b
Jason B. Cox <contact@jasonbcox.com>
2020-07-21 02:28:27 +0000

    Renaming bswap_tests test case so that the name does not collide with the test suite name
    Summary:
    This is the only test where the suite and test names are the same. It results in
    strange `<no class>` output in the Teamcity UI: https://build.bitcoinabc.org/viewLog.html?currentGroup=class&scope=Bitcoin_ABC_unit_tests%23teamcity%23*%23teamcity%23*&pager.currentPage=1&order=NAME_ASC&recordsPerPage=20&filterText=&status=&buildTypeId=BitcoinABCMasterLinux&buildId=29086&tab=testsInfo
    
    Test Plan: Check that CI shows bswap_tests class name correctly with this patch
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D6982

--- src/test/bswap_tests.cpp
+++ src/test/bswap_tests.cpp
@@ -10,7 +10,7 @@
 
 BOOST_FIXTURE_TEST_SUITE(bswap_tests, BasicTestingSetup)
 
-BOOST_AUTO_TEST_CASE(bswap_tests) {
+BOOST_AUTO_TEST_CASE(basic_byteswaps) {
     // Sibling in bitcoin/src/qt/test/compattests.cpp
     uint16_t u1 = 0x1234;
     uint32_t u2 = 0x56789abc;
