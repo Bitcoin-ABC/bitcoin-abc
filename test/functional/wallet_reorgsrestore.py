f7b92034f258d2d2868bee5c79a4c3bbd4c5fa9a
Fabien <fabcien@gmail.com>
2020-07-30 12:29:35 +0000

    Fix wallet_reorgsrestore functional test flakiness
    Summary:
    The test is not designed to account for the parking feature, so disable
    it.
    
    Test Plan:
    ```
    for i in range {1..100}; do
      if ! ./test/functional/test_runner.py wallet_reorgsrestore; then
        break
      fi
    done
    ```
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D7088

--- test/functional/wallet_reorgsrestore.py
+++ test/functional/wallet_reorgsrestore.py
@@ -28,6 +28,7 @@ from test_framework.util import (
 class ReorgsRestoreTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 3
+        self.extra_args = [["-noparkdeepreorg"]] * self.num_nodes
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
