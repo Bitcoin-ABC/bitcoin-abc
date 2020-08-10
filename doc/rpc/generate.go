148c4f7f071e98d610cb8311b409afed7ad80c80
Jason B. Cox <contact@jasonbcox.com>
2020-08-10 15:07:23 -0700

    [RPC docs] Remove unused index files
    Summary: These index files are not used and contain nothing useful.  Now that D7130 and D7131 are landed, we know these index files will never be used.
    
    Test Plan:
    ```
    ninja doc-rpc
    ```
    Verify `doc/rpc/en/0.22.0/index.html` and `doc/rpc/en/0.22.0/rpc/index.html` are no longer generated.
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D6596

--- doc/rpc/generate.go
+++ doc/rpc/generate.go
@@ -117,10 +117,9 @@ func main() {
 
 	tmpl := template.Must(template.ParseFiles(path.Join(srcdir, "doc", "rpc", "command-template.md")))
 
-	rpcdocdir := path.Join("doc", "rpc", "en")
 	for _, group := range groups {
 		groupname := group.Name
-		dirname := path.Join(rpcdocdir, version, "rpc", groupname)
+		dirname := path.Join("doc", "rpc", "en", version, "rpc", groupname)
 		err := os.MkdirAll(dirname, 0777)
 		if err != nil {
 			log.Fatalf("Cannot make directory %s: %s", dirname, err.Error())
@@ -140,32 +139,6 @@ func main() {
 				log.Fatalf("Cannot make command file %s: %s", name, err.Error())
 			}
 		}
-	}
-
-	address := path.Join(rpcdocdir, version, "rpc", "index.md")
-	permalink := fmt.Sprintf("doc/%s/rpc/", version)
-	err := tmpl.Execute(open(address), CommandData{
-		Version:     version,
-		Name:        "rpcindex",
-		Description: "",
-		Group:       "index",
-		Permalink:   permalink,
-	})
-	if err != nil {
-		log.Fatalf("Cannot make index file: %s", err.Error())
-	}
-
-	address = path.Join(rpcdocdir, version, "index.md")
-	permalink = fmt.Sprintf("doc/%s/", version)
-	err = tmpl.Execute(open(address), CommandData{
-		Version:     version,
-		Name:        "index",
-		Description: "",
-		Group:       "index",
-		Permalink:   permalink,
-	})
-	if err != nil {
-		log.Fatalf("Cannot make index file: %s", err.Error())
 	}
 }
 
