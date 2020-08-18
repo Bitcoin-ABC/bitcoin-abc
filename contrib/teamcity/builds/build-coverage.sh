d6fdcaa63ebf72ac886e910852f27e989965e854
Fabien <fabcien@gmail.com>
2020-08-18 14:59:56 +0200

    [CI] Push coverage data to Teamcity statistics
    Summary:
    This will allow for graphing the coverage values and trigger events on
    thresholds.
    
    Test Plan:
      ../contrib/teamcity/build-configurations.py build-coverage
    
    Reviewers: #bitcoin_abc, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D7211

--- contrib/teamcity/builds/build-coverage.sh
+++ contrib/teamcity/builds/build-coverage.sh
@@ -15,6 +15,53 @@ build_with_cmake --gcc coverage-check-extended
 
 # Publish the coverage report in a format that Teamcity understands
 pushd check-extended.coverage
+
+# Send some message to Teamcity containing the coverage data.
+# This make it possible to track coverage variation and trigger events.
+function extract_coverage() {
+  SCOPE=$1
+
+  case "${SCOPE}" in
+    lines)
+      KEY_LETTER="L"
+      ;;
+    functions)
+      KEY_LETTER="M"
+      ;;
+    branches)
+      KEY_LETTER="R"
+      ;;
+    *)
+      echo "Unsupported coverage scope: ${SCOPE}"
+      exit 1
+  esac
+
+  REGEX="([0-9]+) of ([0-9]+) ${SCOPE}"
+
+  HIT=0
+  TOTAL=0
+
+  while IFS= read -r LINE
+  do
+    if [[ "${LINE}" =~ ${REGEX} ]]
+    then
+      HIT="${BASH_REMATCH[1]}"
+      TOTAL="${BASH_REMATCH[2]}"
+      break
+    fi
+  done < coverage-summary.txt
+
+  echo "##teamcity[buildStatisticValue key='CodeCoverageAbs${KEY_LETTER}Covered' value='${HIT}']"
+  echo "##teamcity[buildStatisticValue key='CodeCoverageAbs${KEY_LETTER}Total' value='${TOTAL}']"
+}
+
+# Lines hit/total
+extract_coverage lines
+# Functions hit/total
+extract_coverage functions
+# Branches hit/total
+extract_coverage branches
+
 # Run from the coverage directory to prevent tar from creating a top level
 # folder in the generated archive.
 tar -czf ../coverage.tar.gz -- *
