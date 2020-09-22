39063a6f203fcd8fd33053b3b4be61f167916dd3
Jason B. Cox <contact@jasonbcox.com>
2020-09-22 16:44:21 +0000

    Decouple updating timings from automated commit pipeline
    Summary: See title.
    
    Test Plan:
    ```
    COMMIT_TYPE=dummy ./automated-commits.sh -p HEAD ./patch-recipes/update-timings.sh
    git show    # verify timings are updated
    ```
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D7504

---
+++ contrib/source-control-tools/patch-recipes/update-timings.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+export LC_ALL=C.UTF-8
+
+set -euxo pipefail
+
+TOPLEVEL=$(git rev-parse --show-toplevel)
+"${TOPLEVEL}"/contrib/devtools/build_cmake.sh
+pushd "${BUILD_DIR}"
+ninja check-functional-extended
+TIMING_SRC_FILE="${TOPLEVEL}"/test/functional/timing.json
+mv timing.json "${TIMING_SRC_FILE}"
+popd
+
+# Check that all tests are included in timing.json
+pushd "${TOPLEVEL}"/test/functional
+NON_TESTS=$(python3 -c 'from test_runner import NON_SCRIPTS; print(" ".join(NON_SCRIPTS))')
+export NON_TESTS
+check_missing() {
+  # Exclude non-tests from the check
+  if [[ "${NON_TESTS}" =~ $1 ]]; then
+    exit 0
+  fi
+
+  if ! grep -q $1 timing.json ; then
+    echo "Error: Test file '$1' is missing from timing.json"
+    exit 1
+  fi
+}
+export -f check_missing
+find . -maxdepth 1 -name '*.py' | cut -c 3- | xargs -I'{}' -n1 bash -c 'check_missing {}'
+popd
+
+git add "${TIMING_SRC_FILE}"
+
+git commit -m "[Automated] Update timing.json"
