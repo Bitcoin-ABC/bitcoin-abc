Bitcoin-ABC/bitcoin-abc

commit 357a4cd67bde5e8a39467b84fd947b2acc9884bb

[backport#12173] [Qt] Use flexible font size for QRCode image address
Summary:
Use flexible font size for QRCode image address (Jonas Schnelli)

Pull request description:

  Bech32 addresses are currently cut off in the QRCode image in the GUI receive tab.
  This adds a simple font size calculation logic that "must" (down to 4pt) fix into the given image width.

  Examples OSX HiDPI:
  <img width="332" alt="bildschirmfoto 2018-01-12 um 11 25 40" src="https://user-images.githubusercontent.com/178464/34896144-c0c65d76-f78c-11e7-93e1-94dc8e203269.png">
  <img width="322" alt="bildschirmfoto 2018-01-12 um 11 25 46" src="https://user-images.githubusercontent.com/178464/34896145-c0edfe1c-f78c-11e7-8c09-c15155e2160e.png">

  Examples Ubuntu non HIDPI:
  <img width="314" alt="bildschirmfoto 2018-01-12 um 11 27 51" src="https://user-images.githubusercontent.com/178464/34896151-c88347f4-f78c-11e7-8a03-df8049dcfed6.png">
  <img width="322" alt="bildschirmfoto 2018-01-12 um 11 27 42" src="https://user-images.githubusercontent.com/178464/34896152-c8bb881c-f78c-11e7-89d2-6f04ec608a19.png">

---

Backport of Core [[https://github.com/bitcoin/bitcoin/pull/12173 | PR12173]]

Test Plan:
  ninja
  bitcoin-qt -regtest, check the receive tab for correctness

Reviewers: #bitcoin_abc, deadalnix

Reviewed By: #bitcoin_abc, deadalnix

Differential Revision: https://reviews.bitcoinabc.org/D7524

src/qt/guiutil.cpp
@@ -949,6 +949,19 @@ bool ClickableLabel::hasPixmap() const {
 #endif
 }
 
+qreal calculateIdealFontSize(int width, const QString &text, QFont font,
+                             qreal minPointSize, qreal font_size) {
+    while (font_size >= minPointSize) {
+        font.setPointSizeF(font_size);
+        QFontMetrics fm(font);
+        if (fm.horizontalAdvance(text) < width) {
+            break;
+        }
+        font_size -= 0.5;
+    }
+    return font_size;
+}
+
 void ClickableLabel::mouseReleaseEvent(QMouseEvent *event) {
     Q_EMIT clicked(event->pos());
 }

src/qt/guiutil.h
@@ -244,6 +244,9 @@ QString formatNiceTimeOffset(qint64 secs);
 
 QString formatBytes(uint64_t bytes);
 
+qreal calculateIdealFontSize(int width, const QString &text, QFont font,
+                             qreal minPointSize = 4, qreal startPointSize = 14);
+
 class ClickableLabel : public QLabel {
     Q_OBJECT
 

src/qt/receiverequestdialog.cpp
@@ -168,8 +168,6 @@ void ReceiveRequestDialog::update() {
     ui->outUri->setText(html);
 
 #ifdef USE_QRCODE
-    int fontSize = 10;
-
     ui->lblQRCode->setText("");
     if (!uri.isEmpty()) {
         // limit URI length
@@ -202,9 +200,13 @@ void ReceiveRequestDialog::update() {
             painter.drawImage(0, 0,
                               qrImage.scaled(QR_IMAGE_SIZE, QR_IMAGE_SIZE));
             QFont font = GUIUtil::fixedPitchFont();
-            font.setPixelSize(fontSize);
-            painter.setFont(font);
             QRect paddedRect = qrAddrImage.rect();
+
+            // calculate ideal font size
+            qreal font_size = GUIUtil::calculateIdealFontSize(
+                paddedRect.width() - 20, info.address, font);
+            font.setPointSizeF(font_size);
+
             paddedRect.setHeight(QR_IMAGE_SIZE + 12);
             painter.drawText(paddedRect, Qt::AlignBottom | Qt::AlignCenter,
                              info.address);

commit 131a31b642fa30253ac717a37f71dc7b97c95987

[fix] only use horizontalAdvance for Qt versions which support it
Summary:
Our cross-builds build against Qt 5.10.x or lower and were broken by D7524:

  [00:44:34][Step 1/1] ../../src/qt/guiutil.cpp: In function ‘qreal GUIUtil::calculateIdealFontSize(int, const QString&, QFont, qreal, qreal)’:
  [00:44:34][Step 1/1] ../../src/qt/guiutil.cpp:957:16: error: ‘class QFontMetrics’ has no member named ‘horizontalAdvance’
  [00:44:34][Step 1/1]          if (fm.horizontalAdvance(text) < width) {
  [00:44:34][Step 1/1]                 ^~~~~~~~~~~~~~~~~

This diff fixes that ~~without an additional preprocessor conditional block~~

Test Plan:
  ninja
  bitcoin-qt -regtest

Check if receive tab shows everything right

Reviewers: #bitcoin_abc, deadalnix, Fabien

Reviewed By: #bitcoin_abc, deadalnix, Fabien

Subscribers: deadalnix

Differential Revision: https://reviews.bitcoinabc.org/D7533

src/qt/guiutil.cpp
@@ -954,7 +954,7 @@ qreal calculateIdealFontSize(int width, const QString &text, QFont font,
     while (font_size >= minPointSize) {
         font.setPointSizeF(font_size);
         QFontMetrics fm(font);
-        if (fm.horizontalAdvance(text) < width) {
+        if (GUIUtil::TextWidth(fm, text) < width) {
             break;
         }
         font_size -= 0.5;