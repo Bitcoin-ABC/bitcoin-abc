e7f8845c0cdea7bec17ac6cb6d82b4507ce7f86b
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-09-24 21:11:48 +0300

    net: Add alternative port for onion service
    Summary:
    This change allows to distinguish incoming Tor connections from others.
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/19991 | core#19991]] [2/8]
    https://github.com/bitcoin/bitcoin/pull/19991/commits/a5266d4546c444cfd6d36cb63d2df52ce9e689e2
    
    Depends on D10444
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D10445

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -38,18 +38,22 @@ const CBaseChainParams &BaseParams() {
     return *globalChainBaseParams;
 }
 
+/**
+ * Port numbers for incoming Tor connections (8334, 18334, 38334, 18445) have
+ * been chosen arbitrarily to keep ranges of used ports tight.
+ */
 std::unique_ptr<CBaseChainParams>
 CreateBaseChainParams(const std::string &chain) {
     if (chain == CBaseChainParams::MAIN) {
-        return std::make_unique<CBaseChainParams>("", 8332);
+        return std::make_unique<CBaseChainParams>("", 8332, 8334);
     }
 
     if (chain == CBaseChainParams::TESTNET) {
-        return std::make_unique<CBaseChainParams>("testnet3", 18332);
+        return std::make_unique<CBaseChainParams>("testnet3", 18332, 18334);
     }
 
     if (chain == CBaseChainParams::REGTEST) {
-        return std::make_unique<CBaseChainParams>("regtest", 18443);
+        return std::make_unique<CBaseChainParams>("regtest", 18443, 18445);
     }
 
     throw std::runtime_error(
