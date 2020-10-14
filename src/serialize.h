4b12d4863d5e09712debc8b21978ee05ad4b3586
Pieter Wuille <pieter.wuille@gmail.com>
2020-10-14 13:18:57 +0000

    Support serialization of std::vector<bool>
    Summary:
    PR description:
    > This adds support for serialization of std::vector<bool>, as a prerequisite of [[https://github.com/bitcoin/bitcoin/pull/16702 | PR16702]].
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/16730 | PR16730]].
    
    Test Plan: `ninja && ninja check`
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, deadalnix
    
    Subscribers: deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D7907

--- src/serialize.h
+++ src/serialize.h
@@ -653,6 +653,8 @@ inline void Unserialize(Stream &is, prevector<N, T> &v);
  */
 template <typename Stream, typename T, typename A>
 void Serialize_impl(Stream &os, const std::vector<T, A> &v, const uint8_t &);
+template <typename Stream, typename T, typename A>
+void Serialize_impl(Stream &os, const std::vector<T, A> &v, const bool &);
 template <typename Stream, typename T, typename A, typename V>
 void Serialize_impl(Stream &os, const std::vector<T, A> &v, const V &);
 template <typename Stream, typename T, typename A>
@@ -807,6 +809,17 @@ void Serialize_impl(Stream &os, const std::vector<T, A> &v, const uint8_t &) {
     WriteCompactSize(os, v.size());
     if (!v.empty()) {
         os.write((char *)v.data(), v.size() * sizeof(T));
+    }
+}
+
+template <typename Stream, typename T, typename A>
+void Serialize_impl(Stream &os, const std::vector<T, A> &v, const bool &) {
+    // A special case for std::vector<bool>, as dereferencing
+    // std::vector<bool>::const_iterator does not result in a const bool&
+    // due to std::vector's special casing for bool arguments.
+    WriteCompactSize(os, v.size());
+    for (bool elem : v) {
+        ::Serialize(os, elem);
     }
 }
 
