ryanofsky/bitcoin

[wallet] Move getBlockHash from Chain::Lock interface to simple Chain

src/interfaces/chain.h
@@ -87,9 +87,6 @@ public:
     public:
         virtual ~Lock() {}
 
-        //! Get block hash. Height must be valid or this function will abort.
-        virtual uint256 getBlockHash(int height) = 0;
-
         //! Check that the block is available on disk (i.e. has not been
         //! pruned), and contains transactions.
         virtual bool haveBlockOnDisk(int height) = 0;
@@ -134,6 +131,9 @@ public:
     //! 1 for following block, and so on. Returns nullopt for a block not
     //! included in the current chain.
     virtual Optional<int> getBlockHeight(const uint256& hash) = 0;
+
+    //! Get block hash. Height must be valid or this function will abort.
+    virtual uint256 getBlockHash(int height) = 0;
 
     //! Return whether node has the block and optionally return block metadata
     //! or contents.

Bitcoin-ABC/bitcoin-abc

Summary:
Backport of Core [[https://github.com/bitcoin/bitcoin/pull/16426 | PR16426]] part [3/5] : https://github.com/bitcoin/bitcoin/pull/16426/commits/0a76287387950bc9c5b634e95c5cd5fb1029f42d
Depends on D7935
Test Plan:
  ninja all check-all
Reviewers: #bitcoin_abc, majcosta
Reviewed By: #bitcoin_abc, majcosta
Differential Revision: https://reviews.bitcoinabc.org/D7936

src/interfaces/chain.h
@@ -108,9 +108,6 @@ public:
     public:
         virtual ~Lock() {}
 
-        //! Get block hash. Height must be valid or this function will abort.
-        virtual BlockHash getBlockHash(int height) = 0;
-
         //! Check that the block is available on disk (i.e. has not been
         //! pruned), and contains transactions.
         virtual bool haveBlockOnDisk(int height) = 0;
@@ -160,6 +157,9 @@ public:
     //! 1 for following block, and so on. Returns nullopt for a block not
     //! included in the current chain.
     virtual Optional<int> getBlockHeight(const BlockHash &hash) = 0;
+
+    //! Get block hash. Height must be valid or this function will abort.
+    virtual BlockHash getBlockHash(int height) = 0;
 
     //! Return whether node has the block and optionally return block metadata
     //! or contents.