f0a959057a41b883b95b61d89075b8174026e1f2
MarcoFalke <falke.marco@gmail.com>
2020-10-16 13:05:56 +0200

    test: Remove unused -blockversion from tests
    Summary:
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/20167 | core#20167]] [1/3]
    https://github.com/bitcoin/bitcoin/pull/20167/commits/fa2b778d0cf723925af8710d52cae211d4f036df
    
    Test Plan: `ninja check-functional`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D10677

--- test/functional/feature_csv_activation.py
+++ test/functional/feature_csv_activation.py
@@ -182,7 +182,7 @@ class BIP68_112_113Test(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
         self.setup_clean_chain = True
-        self.extra_args = [['-whitelist=noban@127.0.0.1', '-blockversion=4']]
+        self.extra_args = [['-whitelist=noban@127.0.0.1']]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
