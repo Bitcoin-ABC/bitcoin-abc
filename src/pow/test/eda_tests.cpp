a87c1a8f23120a5bfdd65586a9251c3ef55edf1f
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-10-20 15:52:33 +0000

    Merge #16931: test: add unittests for CheckProofOfWork
    Summary:
    0cc7dd74e0af735dddf7e786f4ed136c382a4ad5 test: add unittests for CheckProofOfWork (soroosh-sdi)
    
    Pull request description:
    
      following situations are covered:
      - negative target
      - overflow target
      - target easier then powLimit
      - invalid hash (hash > target)
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/16931 | PR16931]]
    
    Test Plan: `ninja check`
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D7995

--- src/pow/test/eda_tests.cpp
+++ src/pow/test/eda_tests.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <pow/eda.h>
+#include <pow/pow.h>
 
 #include <chain.h>
 #include <chainparams.h>
@@ -72,6 +73,60 @@ BOOST_AUTO_TEST_CASE(get_next_work_upper_limit_actual) {
         CalculateNextWorkRequired(&pindexLast, nLastRetargetTime,
                                   config.GetChainParams().GetConsensus()),
         0x1d00e1fdU);
+}
+
+BOOST_AUTO_TEST_CASE(CheckProofOfWork_test_negative_target) {
+    const auto consensus =
+        CreateChainParams(CBaseChainParams::MAIN)->GetConsensus();
+    BlockHash hash;
+    unsigned int nBits;
+    nBits = UintToArith256(consensus.powLimit).GetCompact(true);
+    hash.SetHex("0x1");
+    BOOST_CHECK(!CheckProofOfWork(hash, nBits, consensus));
+}
+
+BOOST_AUTO_TEST_CASE(CheckProofOfWork_test_overflow_target) {
+    const auto consensus =
+        CreateChainParams(CBaseChainParams::MAIN)->GetConsensus();
+    BlockHash hash;
+    unsigned int nBits = ~0x00800000;
+    hash.SetHex("0x1");
+    BOOST_CHECK(!CheckProofOfWork(hash, nBits, consensus));
+}
+
+BOOST_AUTO_TEST_CASE(CheckProofOfWork_test_too_easy_target) {
+    const auto consensus =
+        CreateChainParams(CBaseChainParams::MAIN)->GetConsensus();
+    BlockHash hash;
+    unsigned int nBits;
+    arith_uint256 nBits_arith = UintToArith256(consensus.powLimit);
+    nBits_arith *= 2;
+    nBits = nBits_arith.GetCompact();
+    hash.SetHex("0x1");
+    BOOST_CHECK(!CheckProofOfWork(hash, nBits, consensus));
+}
+
+BOOST_AUTO_TEST_CASE(CheckProofOfWork_test_biger_hash_than_target) {
+    const auto consensus =
+        CreateChainParams(CBaseChainParams::MAIN)->GetConsensus();
+    BlockHash hash;
+    unsigned int nBits;
+    arith_uint256 hash_arith = UintToArith256(consensus.powLimit);
+    nBits = hash_arith.GetCompact();
+    hash_arith *= 2; // hash > nBits
+    hash = BlockHash(ArithToUint256(hash_arith));
+    BOOST_CHECK(!CheckProofOfWork(hash, nBits, consensus));
+}
+
+BOOST_AUTO_TEST_CASE(CheckProofOfWork_test_zero_target) {
+    const auto consensus =
+        CreateChainParams(CBaseChainParams::MAIN)->GetConsensus();
+    BlockHash hash;
+    unsigned int nBits;
+    arith_uint256 hash_arith{0};
+    nBits = hash_arith.GetCompact();
+    hash = BlockHash(ArithToUint256(hash_arith));
+    BOOST_CHECK(!CheckProofOfWork(hash, nBits, consensus));
 }
 
 BOOST_AUTO_TEST_CASE(GetBlockProofEquivalentTime_test) {
