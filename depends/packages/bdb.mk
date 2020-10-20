8fc7a3d8a370b34f758b3215a7fb56fe9eb55553
fanquake <fanquake@gmail.com>
2020-10-20 17:10:45 +0800

    build: fix mutex detection when building bdb on macOS
    Summary:
    ```
    Starting with the Clang shipped with Xcode 12, Apple has enabled
    -Werror=implicit-function-declaration by default. This causes bdbs mutex
    detection to fail when building on macOS (not cross-compiling):
    
    checking for mutexes... UNIX/fcntl
    configure: WARNING: NO SHARED LATCH IMPLEMENTATION FOUND FOR THIS PLATFORM.
    configure: error: Unable to find a mutex implementation
    ```
    
     Backport of [[https://github.com/bitcoin/bitcoin/pull/20195 | core#20195]].
    
    Test Plan:  Run the guix osx build.
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D17253

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -10,6 +10,7 @@ $(package)_patches+=winioctl.patch
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --enable-cxx --disable-replication --enable-option-checking
 $(package)_config_opts_mingw32=--enable-mingw
+$(package)_cflags+=-Wno-error=implicit-function-declaration
 $(package)_cppflags_mingw32=-DUNICODE -D_UNICODE
 endef
 
