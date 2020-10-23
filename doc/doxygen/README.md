9c2f408c3cb4c0ea39e3d138dfef0a5a30168a13
Jon Layton <me@jonl.io>
2020-10-23 14:40:25 +0000

    [backport#16912] doc: Move doxygen intro to file for USE_MDFILE_AS_MANPAGE
    Summary:
    With USE_MDFILE_AS_MAINPAGE, this moves the introductory Doxygen comment to its own file. This makes bitcoind.cpp cleaner.
    
    It also removes the \mainpage header text, which was smaller than the section titles, and improves the Navigation section.
    
    This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/16912 | PR16912]]
    
    Test Plan: `ninja doc-doxygen`
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, Fabien
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D8061

---
+++ doc/doxygen/README.md
@@ -0,0 +1,21 @@
+\mainpage notitle
+
+\section intro_sec Introduction
+
+This is the developer documentation of Bitcoin ABC
+(https://www.bitcoinabc.org/). Bitcoin ABC is a client for the digital
+currency called Bitcoin Cash (https://www.bitcoincash.org/), which enables
+instant payments to anyone, anywhere in the world. Bitcoin Cash uses
+peer-to-peer technology to operate with no central authority: managing
+transactions and issuing money are carried out collectively by the network.
+
+The software is a community-driven open source project, released under the
+MIT license.
+
+\section Navigation
+Use <a href="modules.html"><code>Modules</code></a>,
+<a href="namespaces.html"><code>Namespaces</code></a>,
+<a href="classes.html"><code>Classes</code></a>, or
+<a href="files.html"><code>Files</code></a> at the top of the page
+to start navigating the code.
+
--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -28,27 +28,6 @@
 
 const std::function<std::string(const char *)> G_TRANSLATION_FUN = nullptr;
 
-/* Introduction text for doxygen: */
-
-/*! \mainpage Developer documentation
- *
- * \section intro_sec Introduction
- *
- * This is the developer documentation of Bitcoin ABC
- * (https://www.bitcoinabc.org/). Bitcoin ABC is a client for the digital
- * currency called Bitcoin Cash (https://www.bitcoincash.org/), which enables
- * instant payments to anyone, anywhere in the world. Bitcoin Cash uses
- * peer-to-peer technology to operate with no central authority: managing
- * transactions and issuing money are carried out collectively by the network.
- *
- * The software is a community-driven open source project, released under the
- * MIT license.
- *
- * \section Navigation
- * Use the buttons <code>Namespaces</code>, <code>Classes</code> or
- * <code>Files</code> at the top of the page to start navigating the code.
- */
-
 static void WaitForShutdown(NodeContext &node) {
     while (!ShutdownRequested()) {
         UninterruptibleSleep(std::chrono::milliseconds{200});
