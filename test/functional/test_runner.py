5b31979a5c3d1c8e244915d6cdef190acb15b042
Andrew Chow <achow101-github@achow101.com>
2020-10-27 14:01:12 -0400

    Disable some tests for tool_wallet when descriptors
    Summary:
    Some tests are legacy wallet only (and make legacy wallets) so they shouldn't be run when doing descriptor wallet tests.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/18788 | core#18788]] [10/15]
    https://github.com/bitcoin/bitcoin/pull/18788/commits/388053e1722632c2e485c56a444bc75cf0152188
    
    Test Plan: `test/functional/test_runner.py tool_wallet`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D10672

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -110,6 +110,7 @@ TEST_PARAMS = {
     "rpc_signrawtransaction.py": [["--descriptors"]],
     # FIXME: "rpc_psbt.py": [["--descriptors"]],
     "wallet_address_types.py": [["--descriptors"]],
+    "tool_wallet.py": [["--descriptors"]],
     "wallet_avoidreuse.py": [["--descriptors"]],
     "wallet_balance.py": [["--descriptors"]],
     # FIXME: "wallet_basic.py": [["--descriptors"]],
