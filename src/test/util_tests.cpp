d8117150219cfe4bcc534aa32a0bfbe1a5bf8288
Russell Yanofsky <russ@yanofsky.org>
2020-11-04 17:30:10 +0000

    Add missing newline in util_ChainMerge test
    Summary:
    > This was causing a lot of test cases not not be very meaningful because
    > multiple configuration options were combined into one line.
    >
    > The changes in test output with this fix make sense and look like:
    >
    > ```diff
    > - testnet=1 regtest=1 || test
    > + testnet=1 regtest=1 || error: Invalid combination of -regtest, -testnet and -chain. Can use at >  > most one.
    > ```
    > Issue was reported and debugged by
    > Wladimir J. van der Laan <laanwj@protonmail.com> in
    > https://github.com/bitcoin/bitcoin/pull/17385#issuecomment-550033222
    
    This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/17388 | PR17388]]
    
    Test Plan: `ninja && ninja check`
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, majcosta
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, majcosta
    
    Differential Revision: https://reviews.bitcoinabc.org/D8257

--- src/test/util_tests.cpp
+++ src/test/util_tests.cpp
@@ -1229,6 +1229,7 @@ BOOST_FIXTURE_TEST_CASE(util_ChainMerge, ChainMergeTestingSetup) {
             desc += " ";
             desc += argstr + 1;
             conf += argstr + 1;
+            conf += "\n";
         }
         std::istringstream conf_stream(conf);
         BOOST_CHECK(parser.ReadConfigStream(conf_stream, "filepath", error));
@@ -1276,7 +1277,7 @@ BOOST_FIXTURE_TEST_CASE(util_ChainMerge, ChainMergeTestingSetup) {
     //   <input> || <output>
     BOOST_CHECK_EQUAL(
         out_sha_hex,
-        "94b4ad55c8ac639a56b93e36f7e32e4c611fd7d7dd7b2be6a71707b1eadcaec7");
+        "f0b3a3c29869edc765d579c928f7f1690a71fbb673b49ccf39cbc4de18156a0d");
 }
 
 BOOST_AUTO_TEST_CASE(util_ReadWriteSettings) {
