98a48af32926f9cb7d9820db2968bdfe494fd3fc
Troy Giorshev <troygiorshev@gmail.com>
2020-11-05 05:05:37 -0500

    [net] Replace GetID() with id in TransportDeserializer constructor
    Summary:
    `GetId() ` relies on `CNode.id` being initialized
    
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/22735 | core#22735]]
    
    https://github.com/bitcoin/bitcoin/pull/22735/changes/f3e451bebfe2e2d8de901d8ac29c064a51d3b746
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D20050

--- src/net.cpp
+++ src/net.cpp
@@ -3009,7 +3009,7 @@ CNode::CNode(NodeId idIn, std::shared_ptr<Sock> sock, const CAddress &addrIn,
              const std::string &addrNameIn, ConnectionType conn_type_in,
              bool inbound_onion, CNodeOptions &&node_opts)
     : m_deserializer{std::make_unique<V1TransportDeserializer>(
-          V1TransportDeserializer(GetConfig(), GetId()))},
+          V1TransportDeserializer(GetConfig(), idIn))},
       m_serializer{
           std::make_unique<V1TransportSerializer>(V1TransportSerializer())},
       m_permission_flags{node_opts.permission_flags}, m_sock{sock},
