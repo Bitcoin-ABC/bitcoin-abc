194b03e77907e2e24b27adf14df562cfdcbc1eb4
Jason B. Cox <contact@jasonbcox.com>
2020-11-11 11:20:38 -0800

    Add prefix argument to make-sha256sums.sh
    Summary:
    This fixes the script so that we can generate separate sha256sums
    for Gitian outputs based on some prefix (ABC or BCHN).
    
    Test Plan:
      ./make-sha256sums.sh ~/path/to/gitian-results/0.22.6/ ABC
      ./make-sha256sums.sh ~/path/to/gitian-results/0.22.6/ BCHN
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D8370

--- contrib/release/make-sha256sums.sh
+++ contrib/release/make-sha256sums.sh
@@ -5,14 +5,13 @@ export LC_ALL=C
 set -euo pipefail
 
 help_message() {
-  echo "Output sha256sums from Gitian build output."
-  echo "Usage: $0 AssetDirectory > sha256sums"
+  (cat <<EOF
+Output sha256sums from Gitian build output.
+Usage: $0 AssetDirectory Prefix > sha256sums
+Prefix will be matched in the manifest and then removed from the final output.
+EOF
+)
 }
-
-if [ "$#" -ne 1 ]; then
-  echo "Error: Expects 1 argument: AssetDirectory"
-  exit 1
-fi
 
 case $1 in
   -h|--help)
@@ -21,15 +20,37 @@ case $1 in
     ;;
 esac
 
+if [ "$#" -ne 2 ]; then
+  echo "Error: Expects 2 arguments"
+  echo
+  help_message
+  exit 1
+fi
+
+ASSET_DIR="$1"
+PREFIX="$2"
+
 # Trim off preceding whitespace that exists in the manifest
 trim() {
   sed 's/^\s*//'
 }
 
 # Get the hash of the source tarball and output that first
-cat "$1"/linux/bitcoin-abc-*linux-res.yml | grep -E "bitcoin-abc-[0-9.]+.tar.gz" | trim
+cat "${ASSET_DIR}"/linux/bitcoin-abc-*linux-res.yml | grep -E "bitcoin-abc-[0-9.]+.tar.gz" | trim
+
+strip_prefix() {
+  sed -E "s/ $1.?bitcoin-/ bitcoin-/"
+}
 
 # Output hashes of all of the binaries
-cat "$1"/linux/bitcoin-abc-*linux-res.yml | grep -E "bitcoin-abc-[0-9.]+.*-linux-.*.tar.gz" | trim
-cat "$1"/win/bitcoin-abc-*win-res.yml | grep -E -- "bitcoin-abc-[0-9.]+-win.*.(exe|tar.gz|zip)" | trim
-cat "$1"/osx/bitcoin-abc-*osx-res.yml | grep -E -- "bitcoin-abc-[0-9.]+-osx.*.(dmg|tar.gz)" | trim
+cat "${ASSET_DIR}"/linux/bitcoin-abc-*linux-res.yml | \
+  grep -E "${PREFIX}.?bitcoin-abc-[0-9.]+.*-linux-.*.tar.gz" | \
+  strip_prefix "${PREFIX}" | trim
+
+cat "${ASSET_DIR}"/win/bitcoin-abc-*win-res.yml | \
+  grep -E -- "${PREFIX}.?bitcoin-abc-[0-9.]+-win.*.(exe|tar.gz|zip)" | \
+  strip_prefix "${PREFIX}" | trim
+
+cat "${ASSET_DIR}"/osx/bitcoin-abc-*osx-res.yml | \
+  grep -E -- "${PREFIX}.?bitcoin-abc-[0-9.]+-osx.*.(dmg|tar.gz)" | \
+  strip_prefix "${PREFIX}" | trim
