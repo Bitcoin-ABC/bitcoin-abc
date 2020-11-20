ad746f3c3989f48ba2a45f52a0a44022b247191a
Jason B. Cox <contact@jasonbcox.com>
2020-11-20 00:38:19 +0000

    Split chainparams by network
    Summary:
    See title.
    
    Depends on D8289 and D8297 and D8306
    
    Test Plan:
    `ninja check`
    Run mainnet and testnet nodes. Updated ABC and BCHN chains using these nodes.
    ```
    bitcoind -daemon
    bitcoind -daemon -testnet
    ./contrib/source-control-tools/patch-recipes/update-chainparams.sh abc 8332 18332 bchn 8332 18332
    git show   # verify chainparams updated as expected
    ```
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Subscribers: deadalnix, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D8298

--- contrib/source-control-tools/patch-recipes/update-chainparams.sh
+++ contrib/source-control-tools/patch-recipes/update-chainparams.sh
@@ -7,19 +7,35 @@ set -euxo pipefail
 TOPLEVEL=$(git rev-parse --show-toplevel)
 CHAINPARAMS_SCRIPTS_DIR="${TOPLEVEL}"/contrib/devtools/chainparams
 
+# ARGS should be triplets of network, mainnet port, testnet port, ...
+ARGS=("$@")
+if [ "$#" == 0 ]; then
+  ARGS=("abc" 8332 18332)
+fi
+
 # Assumes bitcoind instances are already running on mainnet and testnet
 pushd "${CHAINPARAMS_SCRIPTS_DIR}"
-CHAINPARAMS_MAINNET_TXT="chainparams_main.txt"
-./make_chainparams.py > "${CHAINPARAMS_MAINNET_TXT}"
-git add "${CHAINPARAMS_MAINNET_TXT}"
+INDEX=0
+while [ "${INDEX}" -lt "${#ARGS[@]}" ]; do
+  NETWORK="${ARGS[${INDEX}]}"
+  ((INDEX+=1))
+  MAINNET_PORT="${ARGS[${INDEX}]}"
+  ((INDEX+=1))
+  TESTNET_PORT="${ARGS[${INDEX}]}"
+  ((INDEX+=1))
 
-CHAINPARAMS_TESTNET_TXT="chainparams_test.txt"
-./make_chainparams.py -a 127.0.0.1:18332 > "${CHAINPARAMS_TESTNET_TXT}"
-git add "${CHAINPARAMS_TESTNET_TXT}"
+  CHAINPARAMS_MAINNET_TXT="${NETWORK}/chainparams_main.txt"
+  ./make_chainparams.py -a 127.0.0.1:"${MAINNET_PORT}"> "${CHAINPARAMS_MAINNET_TXT}"
+  git add "${CHAINPARAMS_MAINNET_TXT}"
 
-CHAINPARAMS_CONSTANTS="${TOPLEVEL}"/src/chainparamsconstants.cpp
-./generate_chainparams_constants.py . > "${CHAINPARAMS_CONSTANTS}"
-git add "${CHAINPARAMS_CONSTANTS}"
+  CHAINPARAMS_TESTNET_TXT="${NETWORK}/chainparams_test.txt"
+  ./make_chainparams.py -a 127.0.0.1:"${TESTNET_PORT}" > "${CHAINPARAMS_TESTNET_TXT}"
+  git add "${CHAINPARAMS_TESTNET_TXT}"
+
+  CHAINPARAMS_CONSTANTS="${TOPLEVEL}"/src/networks/"${NETWORK}"/chainparamsconstants.cpp
+  ./generate_chainparams_constants.py "${NETWORK}" > "${CHAINPARAMS_CONSTANTS}"
+  git add "${CHAINPARAMS_CONSTANTS}"
+done
 popd
 
 git commit -m "[Automated] Update chainparams"
