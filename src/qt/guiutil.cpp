hebasto/bitcoin-qtw

Consolidate service flag bit-to-name conversion to a shared serviceFlagToStr function
Side effect: this results in the RPC showing unknown service bits as "UNKNOWN[n]" like the GUI.

Note that there is no common mask-to-vector<string> function because both GUI and RPC would need to iterate through it to convert to their desired target formats.

src/qt/guiutil.cpp
@@ -737,24 +737,6 @@ QString formatDurationStr(int secs)
     return strList.join(" ");
 }
 
-QString serviceFlagToStr(const quint64 mask, const int bit)
-{
-    switch (ServiceFlags(mask)) {
-    case NODE_NONE: abort();  // impossible
-    case NODE_NETWORK:         return "NETWORK";
-    case NODE_GETUTXO:         return "GETUTXO";
-    case NODE_BLOOM:           return "BLOOM";
-    case NODE_WITNESS:         return "WITNESS";
-    case NODE_NETWORK_LIMITED: return "NETWORK_LIMITED";
-    // Not using default, so we get warned when a case is missing
-    }
-    if (bit < 8) {
-        return QString("%1[%2]").arg("UNKNOWN").arg(mask);
-    } else {
-        return QString("%1[2^%2]").arg("UNKNOWN").arg(bit);
-    }
-}
-
 QString formatServicesStr(quint64 mask)
 {
     QStringList strList;
@@ -763,7 +745,7 @@ QString formatServicesStr(quint64 mask)
         uint64_t check = 1ull << i;
         if (mask & check)
         {
-            strList.append(serviceFlagToStr(check, i));
+            strList.append(QString::fromStdString(serviceFlagToStr(mask, i)));
         }
     }
 

Bitcoin-ABC/bitcoin-abc

Summary:
```
Side effect: this results in the RPC showing unknown service bits as
"UNKNOWN[n]" like the GUI.
Note that there is no common mask-to-vector<string> function because
both GUI and RPC would need to iterate through it to convert to their
desired target formats.
Backport of core [[https://github.com/bitcoin/bitcoin/pull/18165 | PR18165]].
Depends on D8516.
Test Plan:
  ninja all check-all
  ./src/qt/bitcoin-qt -server
Check the service bits are displayed correctly in the peers window
  ./src/bitcoin-cli getnetworkinfo
Check the service bits are displayed correctly
Reviewers: #bitcoin_abc, deadalnix
Reviewed By: #bitcoin_abc, deadalnix
Subscribers: deadalnix
Differential Revision: https://reviews.bitcoinabc.org/D8518

src/qt/guiutil.cpp
@@ -751,30 +751,6 @@ QString formatDurationStr(int secs) {
     return strList.join(" ");
 }
 
-QString serviceFlagToStr(const quint64 mask, const int bit) {
-    switch (ServiceFlags(mask)) {
-        case NODE_NONE:
-            // impossible
-            abort();
-        case NODE_NETWORK:
-            return "NETWORK";
-        case NODE_GETUTXO:
-            return "GETUTXO";
-        case NODE_BLOOM:
-            return "BLOOM";
-        case NODE_XTHIN:
-            return "XTHIN";
-        case NODE_NETWORK_LIMITED:
-            return "NETWORK_LIMITED";
-        default:
-            if (bit < 8) {
-                return QString("%1[%2]").arg("UNKNOWN").arg(mask);
-            } else {
-                return QString("%1[2^%2]").arg("UNKNOWN").arg(bit);
-            }
-    }
-}
-
 QString formatServicesStr(quint64 mask) {
     QStringList strList;
 
@@ -785,7 +761,7 @@ QString formatServicesStr(quint64 mask) {
             break;
         }
         if (mask & check) {
-            strList.append(serviceFlagToStr(check, i));
+            strList.append(QString::fromStdString(serviceFlagToStr(check, i)));
         }
     }
 