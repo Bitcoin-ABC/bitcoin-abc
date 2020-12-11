Bitcoin-ABC/bitcoin-abc

commit 5c70631e078bc6ba1b074c99c885bf0c89a67b12

refactor: Pass NodeContext to RPC and REST methods through util::Ref
Summary:
```
This commit does not change behavior
```

Partial backport of core [[https://github.com/bitcoin/bitcoin/pull/18740 | PR18740]]:
https://github.com/bitcoin/bitcoin/pull/18740/commits/6fca33b2edc09ed62dab2323c780b31585de1750#diff-2ae094f9329ce9498e1b4a9cb7767945b54a33b4ee9fed4f0ba401a98e1683c1

Depends on D7988 and D7989.

Test Plan:
  ninja all check-all

Reviewers: #bitcoin_abc, jasonbcox, deadalnix

Reviewed By: #bitcoin_abc, jasonbcox, deadalnix

Subscribers: jasonbcox

Differential Revision: https://reviews.bitcoinabc.org/D7990

src/qt/bitcoin.cpp
@@ -23,6 +23,7 @@
 #include <qt/winshutdownmonitor.h>
 #include <ui_interface.h>
 #include <uint256.h>
+#include <util/ref.h>
 #include <util/system.h>
 #include <util/threadnames.h>
 
@@ -728,7 +729,8 @@ int GuiMain(int argc, char *argv[]) {
     }
 
     RPCServer rpcServer;
-    HTTPRPCRequestProcessor httpRPCRequestProcessor(config, rpcServer);
+    util::Ref context{node};
+    HTTPRPCRequestProcessor httpRPCRequestProcessor(config, rpcServer, context);
 
     try {
         app.createWindow(&config, networkStyle.data());

src/bitcoind.cpp
@@ -18,6 +18,7 @@
 #include <noui.h>
 #include <shutdown.h>
 #include <ui_interface.h>
+#include <util/ref.h>
 #include <util/strencodings.h>
 #include <util/system.h>
 #include <util/threadnames.h>
@@ -64,10 +65,14 @@ static bool AppInit(int argc, char *argv[]) {
     // not possible as the whole application has too many global state. However,
     // this is a first step.
     auto &config = const_cast<Config &>(GetConfig());
+
     RPCServer rpcServer;
-    HTTPRPCRequestProcessor httpRPCRequestProcessor(config, rpcServer);
 
     NodeContext node;
+    util::Ref context{node};
+
+    HTTPRPCRequestProcessor httpRPCRequestProcessor(config, rpcServer, context);
+
     bool fRet = false;
 
     util::ThreadSetInternalName("init");

src/httprpc.cpp
@@ -9,6 +9,7 @@
 #include <crypto/hmac_sha256.h>
 #include <rpc/protocol.h>
 #include <ui_interface.h>
+#include <util/ref.h>
 #include <util/strencodings.h>
 #include <util/system.h>
 #include <util/translation.h>
@@ -305,7 +306,7 @@ bool HTTPRPCRequestProcessor::ProcessHTTPRequest(HTTPRequest *req) {
         return false;
     }
 
-    JSONRPCRequest jreq;
+    JSONRPCRequest jreq(context);
     jreq.peerAddr = req->GetPeer().ToString();
     if (!RPCAuthorized(authHeader.second, jreq.authUser)) {
         LogPrintf("ThreadRPCServer incorrect password attempt from %s\n",

src/httprpc.h
@@ -10,6 +10,10 @@
 
 class Config;
 
+namespace util {
+class Ref;
+} // namespace util
+
 class HTTPRPCRequestProcessor {
 private:
     Config &config;
@@ -18,8 +22,11 @@ private:
     bool ProcessHTTPRequest(HTTPRequest *request);
 
 public:
-    HTTPRPCRequestProcessor(Config &configIn, RPCServer &rpcServerIn)
-        : config(configIn), rpcServer(rpcServerIn) {}
+    const util::Ref &context;
+
+    HTTPRPCRequestProcessor(Config &configIn, RPCServer &rpcServerIn,
+                            const util::Ref &contextIn)
+        : config(configIn), rpcServer(rpcServerIn), context(contextIn) {}
 
     static bool DelegateHTTPRequest(HTTPRPCRequestProcessor *requestProcessor,
                                     HTTPRequest *request) {
@@ -46,7 +53,7 @@ void StopHTTPRPC();
  * Start HTTP REST subsystem.
  * Precondition; HTTP and RPC has been started.
  */
-void StartREST();
+void StartREST(const util::Ref &context);
 
 /** Interrupt RPC REST subsystem */
 void InterruptREST();

src/init.cpp
@@ -1413,7 +1413,7 @@ static bool AppInitServers(Config &config,
         return false;
     }
     if (gArgs.GetBoolArg("-rest", DEFAULT_REST_ENABLE)) {
-        StartREST();
+        StartREST(httpRPCRequestProcessor.context);
     }
 
     StartHTTPServer();

src/init.h
@@ -22,6 +22,9 @@ class RPCServer;
 namespace boost {
 class thread_group;
 } // namespace boost
+namespace util {
+class Ref;
+} // namespace util
 
 /** Interrupt threads */
 void Interrupt(NodeContext &node);

src/interfaces/node.cpp
@@ -27,6 +27,7 @@
 #include <sync.h>
 #include <txmempool.h>
 #include <ui_interface.h>
+#include <util/ref.h>
 #include <util/system.h>
 #include <util/translation.h>
 #include <validation.h>
@@ -257,7 +258,7 @@ namespace {
         UniValue executeRpc(Config &config, const std::string &command,
                             const UniValue &params,
                             const std::string &uri) override {
-            JSONRPCRequest req;
+            JSONRPCRequest req(m_context_ref);
             req.params = params;
             req.strMethod = command;
             req.URI = uri;
@@ -364,6 +365,7 @@ namespace {
         }
         NodeContext *context() override { return &m_context; }
         NodeContext m_context;
+        util::Ref m_context_ref{m_context};
     };
 } // namespace
 

src/qt/test/apptests.cpp
@@ -14,6 +14,7 @@
 #include <qt/rpcconsole.h>
 #include <rpc/server.h>
 #include <shutdown.h>
+#include <util/ref.h>
 #include <validation.h>
 
 #if defined(HAVE_CONFIG_H)
@@ -93,7 +94,8 @@ void AppTests::appTests() {
     m_app.baseInitialize(config);
 
     RPCServer rpcServer;
-    HTTPRPCRequestProcessor httpRPCRequestProcessor(config, rpcServer);
+    util::Ref context{test.m_node};
+    HTTPRPCRequestProcessor httpRPCRequestProcessor(config, rpcServer, context);
     m_app.requestInitialize(config, rpcServer, httpRPCRequestProcessor);
     m_app.exec();
     m_app.requestShutdown(config);

commit d0207bfcd35f39f2690cfe9afa639fd265cdbdf1

Fix bitcoin-qt RPC when -server is used
Summary:
The bug was introduced in D7990. The `node` name usually designates a
node context, but not in this case.

Test Plan:
  ninja all check-all

Without this diff, returns a `Node context not found` error:
  ./src/qt/bitcoin-qt -server
  ./src/bitcoin-cli getnetworkinfo

Reviewers: #bitcoin_abc, jasonbcox

Reviewed By: #bitcoin_abc, jasonbcox

Subscribers: jasonbcox

Differential Revision: https://reviews.bitcoinabc.org/D8652

src/qt/bitcoin.cpp
@@ -748,7 +748,7 @@ int GuiMain(int argc, char *argv[]) {
     }
 
     RPCServer rpcServer;
-    util::Ref context{node};
+    util::Ref context{node_context};
     HTTPRPCRequestProcessor httpRPCRequestProcessor(config, rpcServer, context);
 
     try {