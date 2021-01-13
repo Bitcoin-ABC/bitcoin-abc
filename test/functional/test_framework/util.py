a05e018b9578668c6a5efcdb1608e87e16ad9eb5
MarcoFalke <falke.marco@gmail.com>
2021-01-13 12:44:31 +0000

    test: check that peer is connected when calling sync_*
    Summary:
    > Without a connection there is no way to sync, so we can fail early and don't have to wait for the timeout
    
    This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/18474 | PR18474]]
    
    Test Plan: `ninja && ninja check-functional-extended`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D8897

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -469,6 +469,8 @@ def sync_blocks(rpc_connections, *, wait=1, timeout=60):
         best_hash = [x.getbestblockhash() for x in rpc_connections]
         if best_hash.count(best_hash[0]) == len(rpc_connections):
             return
+        # Check that each peer has at least one connection
+        assert (all([len(x.getpeerinfo()) for x in rpc_connections]))
         time.sleep(wait)
     raise AssertionError("Block sync timed out:{}".format(
         "".join("\n  {!r}".format(b) for b in best_hash)))
@@ -488,6 +490,8 @@ def sync_mempools(rpc_connections, *, wait=1,
                 for r in rpc_connections:
                     r.syncwithvalidationinterfacequeue()
             return
+        # Check that each peer has at least one connection
+        assert (all([len(x.getpeerinfo()) for x in rpc_connections]))
         time.sleep(wait)
     raise AssertionError("Mempool sync timed out:{}".format(
         "".join("\n  {!r}".format(m) for m in pool)))
