bitcoin-ghost/bitcoin-core-ladder-script

[trivial] Remove misleading 'const'
Returning by const value is only meaningful in a specific circumstance around
user defined types. In this case, the const is not enforcing any restrictions
on the call site, so is misleading.

src/txmempool.h
@@ -714,7 +714,7 @@ public:
     void RemoveUnbroadcastTx(const uint256& txid, const bool unchecked = false);
 
     /** Returns transactions in unbroadcast set */
-    const std::set<uint256> GetUnbroadcastTxs() const {
+    std::set<uint256> GetUnbroadcastTxs() const {
         LOCK(cs);
         return m_unbroadcast_txids;
     }

Bitcoin-ABC/bitcoin-abc

Summary:
This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/18807 | PR18807]] [7/9]
https://github.com/bitcoin/bitcoin/pull/18807/commits/750456d6f29c63d57af05bfbdd6035bb9c965de2
Test Plan: ninja all check-all
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D9034

src/txmempool.h
@@ -796,7 +796,7 @@ public:
     void RemoveUnbroadcastTx(const TxId &txid, const bool unchecked = false);
 
     /** Returns transactions in unbroadcast set */
-    const std::set<TxId> GetUnbroadcastTxs() const {
+    std::set<TxId> GetUnbroadcastTxs() const {
         LOCK(cs);
         return m_unbroadcast_txids;
     }