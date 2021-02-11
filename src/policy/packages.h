74054628d1a9b9a30c8c42d78e9cb946ac3d49e4
glozow <gzhao408@berkeley.edu>
2021-02-11 09:50:42 -0800

    [policy] Define packages
    Summary:
    Define the Package type as an alias for a vector of transactions for now.
    Add PackageValidationResult, similar to TxValidationResult and
    BlockValidationResult for package-wide errors that cannot be reported
    within a single transaction result, such as having too many
    transactions in the package. We can update the concept of
    what a package is and have different logic for packages vs lists of
    transactions in the future, e.g. for package relay.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/20833 | core#20833]] [4/13]
    https://github.com/bitcoin/bitcoin/pull/20833/commits/b88d77aec5e7bef5305a668d15031351c0548b4d
    
    Test Plan: `ninja`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D12122

---
+++ src/policy/packages.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_POLICY_PACKAGES_H
+#define BITCOIN_POLICY_PACKAGES_H
+
+#include <consensus/validation.h>
+#include <primitives/transaction.h>
+
+#include <vector>
+
+/**
+ * A "reason" why a package was invalid. It may be that one or more of the
+ * included transactions is invalid or the package itself violates our rules.
+ * We don't distinguish between consensus and policy violations right now.
+ */
+enum class PackageValidationResult {
+    //! Initial value. The package has not yet been rejected.
+    PCKG_RESULT_UNSET = 0,
+    //! The package itself is invalid (e.g. too many transactions).
+    PCKG_POLICY,
+    //! At least one tx is invalid.
+    PCKG_TX,
+};
+
+/**
+ * A package is an ordered list of transactions. The transactions cannot
+ * conflict with (spend the same inputs as) one another.
+ */
+using Package = std::vector<CTransactionRef>;
+
+class PackageValidationState : public ValidationState<PackageValidationResult> {
+};
+
+#endif // BITCOIN_POLICY_PACKAGES_H
