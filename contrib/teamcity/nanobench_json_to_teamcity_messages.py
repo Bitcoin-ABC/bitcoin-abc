c3ac2ec43dd3ab1a22765ce7a297c47e8a15c802
Fabien <fabcien@gmail.com>
2021-02-12 16:38:10 +0100

    [CI] Integrate nanobench with Teamcity
    Summary:
    The new nanobench benchmarking framework introduced in Dxxxx has no
    built-in output format compatible with Teamcity. But since it provides a
    very complete json output, it is easy to parse it and generate the
    messages we want for teamcity.
    
    This diff makes the `bench-bitcoin` target generate csv and json output
    files, and adds a simple python script to parse the json output and send
    teamcity messages when run on CI.
    
    Depends on D9226.
    
    Test Plan:
      ninja bench-bitcoin
    
      ../contrib/teamcity/build-configurations.py build-bench
    Check the csv and json files are in the artifacts, and it echoes a lot
    of teamcity messages with the correct values.
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D9227

---
+++ contrib/teamcity/nanobench_json_to_teamcity_messages.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# Copyright (c) 2021 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from datetime import timedelta
+import json
+import sys
+from teamcity.messages import TeamcityServiceMessages
+
+if len(sys.argv) != 3:
+    print(
+        """
+    Usage:
+        {} <benchmark suite name> <path to nanobench json file>
+
+    Print the teamcity service messages for associating op/s benchmark result
+    to the tests.
+
+    Requires the teamcity-messages python library:
+      pip3 install teamcity-messages
+""".format(sys.argv[0]))
+    sys.exit(1)
+
+suite_name = sys.argv[1]
+with open(sys.argv[2], encoding='utf-8') as f:
+    json_results = json.load(f)
+
+teamcity_messages = TeamcityServiceMessages()
+
+teamcity_messages.testSuiteStarted(
+    suite_name
+)
+
+
+def testMeta_number_message(test_name, param_name, param_value):
+    teamcity_messages.message(
+        'testMeta',
+        type='number',
+        testName=test_name,
+        name=param_name,
+        value='{:.2f}'.format(param_value),
+    )
+
+
+for result in json_results.get('results', []):
+    test_name = result['name']
+
+    teamcity_messages.testStarted(
+        test_name
+    )
+
+    testMeta_number_message(
+        test_name,
+        'ns/{}'.format(result['unit']),
+        1e9 * result['median(elapsed)'] / result['batch'],
+    )
+
+    testMeta_number_message(
+        test_name,
+        '{}/s'.format(result['unit']),
+        result['batch'] / result['median(elapsed)'],
+    )
+
+    testMeta_number_message(
+        test_name,
+        'err%',
+        100 * result['medianAbsolutePercentError(elapsed)'],
+    )
+
+    testMeta_number_message(
+        test_name,
+        'ins/{}'.format(result['unit']),
+        result['median(instructions)'] / result['batch'],
+    )
+
+    teamcity_messages.testFinished(
+        test_name,
+        testDuration=timedelta(seconds=result['totalTime']),
+    )
+
+teamcity_messages.testSuiteFinished(
+    suite_name
+)
