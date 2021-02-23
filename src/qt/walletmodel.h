bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

qt: Fix regression in TransactionTableModel
Since #17993 a crash is possible on exit.

Co-authored-by: Russell Yanofsky <russ@yanofsky.org>

src/qt/walletmodel.h
@@ -155,6 +155,9 @@ public:
     AddressTableModel* getAddressTableModel() const { return addressTableModel; }
 
     void refresh(bool pk_hash_only = false);
+
+    uint256 getLastBlockProcessed() const;
+
 private:
     std::unique_ptr<interfaces::Wallet> m_wallet;
     std::unique_ptr<interfaces::Handler> m_handler_unload;

Bitcoin-ABC/bitcoin-abc

Summary:
Since PR17993 a crash is possible on exit.
This is a backport of [[https://github.com/bitcoin-core/gui/pull/8 | core-gui#8]]
Depends on D9254
Test Plan:
start bitcoin-qt
wait until sync
on main window: Menu -> File -> Quit
Reviewers: #bitcoin_abc, majcosta
Reviewed By: #bitcoin_abc, majcosta
Subscribers: majcosta
Differential Revision: https://reviews.bitcoinabc.org/D9257

src/qt/walletmodel.h
@@ -162,6 +162,8 @@ public:
         return addressTableModel;
     }
 
+    BlockHash getLastBlockProcessed() const;
+
 private:
     std::unique_ptr<interfaces::Wallet> m_wallet;
     std::unique_ptr<interfaces::Handler> m_handler_unload;