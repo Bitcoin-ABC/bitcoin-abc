c714c4dfb1da10cab9caf920501c2be8b3093da8
Joey King <joseph.roy.king@gmail.com>
2021-03-19 10:14:19 -0700

    [Cashtab] Upgrading pkg dependencies
    Summary: Upgrading npm pkgs. Because react-app is ejected in Cashtab, also modifying `webpackDevServer.config.js` to correct type error; see `https://stackoverflow.com/questions/60234640/typeerror-err-invalid-arg-type-the-path-argument-must-be-of-type-string-re`
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D9337

--- web/cashtab/config/webpackDevServer.config.js
+++ web/cashtab/config/webpackDevServer.config.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
 const evalSourceMapMiddleware = require('react-dev-utils/evalSourceMapMiddleware');
 const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
@@ -98,7 +96,7 @@ module.exports = function (proxy, allowedHost) {
             // We do this in development to avoid hitting the production cache if
             // it used the same host and port.
             // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-            app.use(noopServiceWorkerMiddleware());
+            app.use(noopServiceWorkerMiddleware('/'));
         },
     };
 };
