64ab2b37d24845f99cda13be8cc96ae9caadd517
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-03-21 23:45:26 +0200

    build: Do not build unused CoreWLAN stuff in depends for macOS
    Summary:
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/21497 | core#21497]]
    Depends on D13933
    
    Test Plan: gitian-osx
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D13934

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -108,6 +108,7 @@ $(package)_config_opts += -no-feature-xml
 
 $(package)_config_opts_darwin = -no-dbus
 $(package)_config_opts_darwin += -pch
+$(package)_config_opts_darwin += -no-feature-corewlan
 $(package)_config_opts_darwin += QMAKE_MACOSX_DEPLOYMENT_TARGET=$(OSX_MIN_VERSION)
 
 ifneq ($(build_os),darwin)
