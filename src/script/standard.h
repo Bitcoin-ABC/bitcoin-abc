af6a2dfd8daef9a96936abaca60476cc80ddd927
MarcoFalke <falke.marco@gmail.com>
2021-04-04 12:25:56 +0200

    move-only: Add util/hash_type
    Summary:
    Can be reviewed with --color-moved=dimmed-zebra
    
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/21584 | core#21584]]
    https://github.com/bitcoin/bitcoin/pull/21584/commits/faa921f78788be0f236c6b10ed3f92dcf148d1cb
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D11330

--- src/script/standard.h
+++ src/script/standard.h
@@ -10,6 +10,7 @@
 #include <pubkey.h>
 #include <script/script_flags.h>
 #include <uint256.h>
+#include <util/hash_type.h>
 
 #include <boost/variant.hpp>
 
@@ -20,46 +21,6 @@ static const bool DEFAULT_ACCEPT_DATACARRIER = true;
 class CKeyID;
 class CScript;
 struct ScriptHash;
-
-template <typename HashType> class BaseHash {
-protected:
-    HashType m_hash;
-
-public:
-    BaseHash() : m_hash() {}
-    BaseHash(const HashType &in) : m_hash(in) {}
-
-    uint8_t *begin() { return m_hash.begin(); }
-
-    const uint8_t *begin() const { return m_hash.begin(); }
-
-    uint8_t *end() { return m_hash.end(); }
-
-    const uint8_t *end() const { return m_hash.end(); }
-
-    operator std::vector<uint8_t>() const {
-        return std::vector<uint8_t>{m_hash.begin(), m_hash.end()};
-    }
-
-    std::string ToString() const { return m_hash.ToString(); }
-
-    bool operator==(const BaseHash<HashType> &other) const noexcept {
-        return m_hash == other.m_hash;
-    }
-
-    bool operator!=(const BaseHash<HashType> &other) const noexcept {
-        return !(m_hash == other.m_hash);
-    }
-
-    bool operator<(const BaseHash<HashType> &other) const noexcept {
-        return m_hash < other.m_hash;
-    }
-
-    size_t size() const { return m_hash.size(); }
-
-    uint8_t *data() { return m_hash.data(); }
-    const uint8_t *data() const { return m_hash.data(); }
-};
 
 /** A reference to a CScript: the Hash160 of its serialization (see script.h) */
 class CScriptID : public BaseHash<uint160> {
---
+++ src/util/hash_type.h
@@ -0,0 +1,48 @@
+// Copyright (c) 2020-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_HASH_TYPE_H
+#define BITCOIN_UTIL_HASH_TYPE_H
+
+template <typename HashType> class BaseHash {
+protected:
+    HashType m_hash;
+
+public:
+    BaseHash() : m_hash() {}
+    BaseHash(const HashType &in) : m_hash(in) {}
+
+    uint8_t *begin() { return m_hash.begin(); }
+
+    const uint8_t *begin() const { return m_hash.begin(); }
+
+    uint8_t *end() { return m_hash.end(); }
+
+    const uint8_t *end() const { return m_hash.end(); }
+
+    operator std::vector<uint8_t>() const {
+        return std::vector<uint8_t>{m_hash.begin(), m_hash.end()};
+    }
+
+    std::string ToString() const { return m_hash.ToString(); }
+
+    bool operator==(const BaseHash<HashType> &other) const noexcept {
+        return m_hash == other.m_hash;
+    }
+
+    bool operator!=(const BaseHash<HashType> &other) const noexcept {
+        return !(m_hash == other.m_hash);
+    }
+
+    bool operator<(const BaseHash<HashType> &other) const noexcept {
+        return m_hash < other.m_hash;
+    }
+
+    size_t size() const { return m_hash.size(); }
+
+    uint8_t *data() { return m_hash.data(); }
+    const uint8_t *data() const { return m_hash.data(); }
+};
+
+#endif // BITCOIN_UTIL_HASH_TYPE_H
