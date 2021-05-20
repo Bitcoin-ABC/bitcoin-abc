ariard/bitcoin

Report and verify expirations

src/test/fuzz/txrequest.cpp
@@ -243,11 +243,13 @@ public:
 
         //! list of (sequence number, txhash, is_wtxid) tuples.
         std::vector<std::tuple<uint64_t, int, bool>> result;
+        std::vector<std::pair<NodeId, GenTxid>> expected_expired;
         for (int txhash = 0; txhash < MAX_TXHASHES; ++txhash) {
             // Mark any expired REQUESTED announcements as COMPLETED.
             for (int peer2 = 0; peer2 < MAX_PEERS; ++peer2) {
                 Announcement& ann2 = m_announcements[txhash][peer2];
                 if (ann2.m_state == State::REQUESTED && ann2.m_time <= m_now) {
+                    expected_expired.emplace_back(peer2, GenTxid{ann2.m_is_wtxid, TXHASHES[txhash]});
                     ann2.m_state = State::COMPLETED;
                     break;
                 }
@@ -262,9 +264,13 @@ public:
         }
         // Sort the results by sequence number.
         std::sort(result.begin(), result.end());
+        std::sort(expected_expired.begin(), expected_expired.end());
 
         // Compare with TxRequestTracker's implementation.
-        const auto actual = m_tracker.GetRequestable(peer, m_now);
+        std::vector<std::pair<NodeId, GenTxid>> expired;
+        const auto actual = m_tracker.GetRequestable(peer, m_now, &expired);
+        std::sort(expired.begin(), expired.end());
+        assert(expired == expected_expired);
 
         m_tracker.PostGetRequestableSanityCheck(m_now);
         assert(result.size() == actual.size());

Bitcoin-ABC/bitcoin-abc

Summary:
Completes backport of [[https://github.com/bitcoin/bitcoin/pull/19988 | core#19988]]:
https://github.com/bitcoin/bitcoin/pull/19988/commits/fd9a0060f028a4c01bd88f58777dea34bdcbafd1
Depends on D9553.
Test Plan:
  ninja all check-all
  ./test/functional/test_runner.py p2p_tx_download
  ninja bitcoin-fuzzers
Reviewers: #bitcoin_abc, PiRK
Reviewed By: #bitcoin_abc, PiRK
Differential Revision: https://reviews.bitcoinabc.org/D9554

src/test/fuzz/txrequest.cpp
@@ -262,11 +262,13 @@ public:
 
         //! list of (sequence number, txid) tuples.
         std::vector<std::tuple<uint64_t, int>> result;
+        std::vector<std::pair<NodeId, TxId>> expected_expired;
         for (int txid = 0; txid < MAX_TXIDS; ++txid) {
             // Mark any expired REQUESTED announcements as COMPLETED.
             for (int peer2 = 0; peer2 < MAX_PEERS; ++peer2) {
                 Announcement &ann2 = m_announcements[txid][peer2];
                 if (ann2.m_state == State::REQUESTED && ann2.m_time <= m_now) {
+                    expected_expired.emplace_back(peer2, TXIDS[txid]);
                     ann2.m_state = State::COMPLETED;
                     break;
                 }
@@ -282,9 +284,13 @@ public:
         }
         // Sort the results by sequence number.
         std::sort(result.begin(), result.end());
+        std::sort(expected_expired.begin(), expected_expired.end());
 
         // Compare with TxRequestTracker's implementation.
-        const auto actual = m_tracker.GetRequestable(peer, m_now);
+        std::vector<std::pair<NodeId, TxId>> expired;
+        const auto actual = m_tracker.GetRequestable(peer, m_now, &expired);
+        std::sort(expired.begin(), expired.end());
+        assert(expired == expected_expired);
 
         m_tracker.PostGetRequestableSanityCheck(m_now);
         assert(result.size() == actual.size());