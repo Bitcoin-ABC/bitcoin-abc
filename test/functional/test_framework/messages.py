cf92888087fb826483cbd4e1756803a570ba04c0
Marco Antonio Jaguaribe Costa <marco.antonio.costa@gmail.com>
2021-06-11 07:26:54 -0300

    [refactor] use MAX_MONEY constant instead of magic number
    Summary: as title
    
    Test Plan:
      ninja all check-functional
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D9666

--- test/functional/test_framework/messages.py
+++ test/functional/test_framework/messages.py
@@ -494,7 +494,7 @@ class CTransaction:
     def is_valid(self):
         self.calc_sha256()
         for tout in self.vout:
-            if tout.nValue < 0 or tout.nValue > 21000000 * COIN:
+            if tout.nValue < 0 or tout.nValue > MAX_MONEY:
                 return False
         return True
 
