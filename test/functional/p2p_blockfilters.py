196f3dd317c50fe464c2f1f2e20d37c22566b8ed
MarcoFalke <falke.marco@gmail.com>
2021-06-22 19:43:02 +0200

    test: Add missing syncwithvalidationinterfacequeue
    Summary:
    The index on the block filters is running in the background on the validation interface. To avoid intermittent test failures, it needs to be synced.
    
    This concludes backport of [[https://github.com/bitcoin/bitcoin/pull/22311 | core#22311]]
    https://github.com/bitcoin/bitcoin/pull/22311/commits/fadddd13eef4428f5fa7237583d4be41a9335cd9
    
    Depends on D11445
    
    Test Plan: `test/functional/test_runner.py p2p_blockfilters`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D11446

--- test/functional/p2p_blockfilters.py
+++ test/functional/p2p_blockfilters.py
@@ -62,6 +62,7 @@ class CompactFiltersTest(BitcoinTestFramework):
         self.disconnect_nodes(0, 1)
 
         stale_block_hash = self.nodes[0].generate(1)[0]
+        self.nodes[0].syncwithvalidationinterfacequeue()
         assert_equal(self.nodes[0].getblockcount(), 1000)
 
         self.nodes[1].generate(1001)
@@ -93,6 +94,7 @@ class CompactFiltersTest(BitcoinTestFramework):
         self.log.info("Reorg node 0 to a new chain.")
         self.connect_nodes(0, 1)
         self.sync_blocks(timeout=600)
+        self.nodes[0].syncwithvalidationinterfacequeue()
 
         main_block_hash = self.nodes[0].getblockhash(1000)
         assert main_block_hash != stale_block_hash, "node 0 chain did not reorganize"
