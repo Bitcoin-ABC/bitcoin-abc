Bitcoin-ABC/bitcoin-abc

commit 7cb36e0d697594ee63e1729ff5f180294ed6f6b0

[avalanche] Do not bucket brigade proof registration
Summary: There are few callsites, it can easily be avoided.

Test Plan:
  ninja all check-all

Reviewers: #bitcoin_abc, PiRK, majcosta

Reviewed By: #bitcoin_abc, PiRK, majcosta

Differential Revision: https://reviews.bitcoinabc.org/D9768

src/net_processing.cpp
@@ -4372,22 +4372,25 @@ void PeerManager::ProcessMessage(const Config &config, CNode &pfrom,
 
         // addProof should not be called while cs_proofrequest because it holds
         // cs_main and that creates a potential deadlock during shutdown
-        if (g_avalanche->addProof(proof)) {
-            WITH_LOCK(cs_proofrequest, m_proofrequest.ForgetInvId(proofid));
-            RelayProof(proofid, m_connman);
+        g_avalanche->withPeerManager([&](avalanche::PeerManager &pm) {
+            if (pm.registerProof(proof)) {
+                WITH_LOCK(cs_proofrequest, m_proofrequest.ForgetInvId(proofid));
+                RelayProof(proofid, m_connman);
 
-            LogPrint(BCLog::NET, "New avalanche proof: peer=%d, proofid %s\n",
-                     nodeid, proofid.ToString());
-        } else {
-            // If the proof couldn't be added, it can be either orphan or
-            // invalid. In the latter case we should increase the ban score.
-            // TODO improve the ban reason by printing the validation state
-            if (!g_avalanche->getOrphan(proofid)) {
-                WITH_LOCK(cs_rejectedProofs, rejectedProofs->insert(proofid));
-                Misbehaving(nodeid, 100, "invalid-avaproof");
+                LogPrint(BCLog::NET,
+                         "New avalanche proof: peer=%d, proofid %s\n", nodeid,
+                         proofid.ToString());
+            } else {
+                // If the proof couldn't be added, it can be either orphan or
+                // invalid. In the latter case we should increase the ban score.
+                // TODO improve the ban reason by printing the validation state
+                if (!pm.getOrphan(proofid)) {
+                    WITH_LOCK(cs_rejectedProofs,
+                              rejectedProofs->insert(proofid));
+                    Misbehaving(nodeid, 100, "invalid-avaproof");
+                }
             }
-        }
-
+        });
         return;
     }
 

src/avalanche/processor.cpp
@@ -558,11 +558,6 @@ bool Processor::sendHello(CNode *pfrom) const {
     return true;
 }
 
-bool Processor::addProof(const std::shared_ptr<Proof> &proof) {
-    LOCK(cs_peerManager);
-    return peerManager->registerProof(proof);
-}
-
 std::shared_ptr<Proof> Processor::getProof(const ProofId &proofid) const {
     LOCK(cs_peerManager);
     return peerManager->getProof(proofid);

src/avalanche/processor.h
@@ -288,7 +288,6 @@ public:
     CPubKey getSessionPubKey() const;
     bool sendHello(CNode *pfrom) const;
 
-    bool addProof(const std::shared_ptr<Proof> &proof);
     std::shared_ptr<Proof> getProof(const ProofId &proofid) const;
     std::shared_ptr<Proof> getLocalProof() const;
     std::shared_ptr<Proof> getOrphan(const ProofId &proofid) const;

src/avalanche/test/processor_tests.cpp
@@ -133,13 +133,18 @@ struct AvalancheTestingSetup : public TestChain100Setup {
 
     bool addNode(NodeId nodeid) {
         auto proof = GetProof();
-        BOOST_CHECK(m_processor->addProof(proof));
-        return addNode(nodeid, proof->getId());
+        return m_processor->withPeerManager([&](avalanche::PeerManager &pm) {
+            return pm.registerProof(proof) &&
+                   pm.addNode(nodeid, proof->getId());
+        });
     }
 
     std::array<CNode *, 8> ConnectNodes() {
         auto proof = GetProof();
-        BOOST_CHECK(m_processor->addProof(proof));
+        BOOST_CHECK(
+            m_processor->withPeerManager([&](avalanche::PeerManager &pm) {
+                return pm.registerProof(proof);
+            }));
         const ProofId &proofid = proof->getId();
 
         std::array<CNode *, 8> nodes;
@@ -744,7 +749,8 @@ BOOST_AUTO_TEST_CASE(poll_inflight_timeout, *boost::unit_test::timeout(60)) {
 BOOST_AUTO_TEST_CASE(poll_inflight_count) {
     // Create enough nodes so that we run into the inflight request limit.
     auto proof = GetProof();
-    BOOST_CHECK(m_processor->addProof(proof));
+    BOOST_CHECK(m_processor->withPeerManager(
+        [&](avalanche::PeerManager &pm) { return pm.registerProof(proof); }));
 
     std::array<CNode *, AVALANCHE_MAX_INFLIGHT_POLL + 1> nodes;
     for (auto &n : nodes) {

src/rpc/avalanche.cpp
@@ -52,6 +52,13 @@ static CPubKey ParsePubKey(const UniValue &param) {
     return HexToPubKey(keyHex);
 }
 
+static bool registerProofIfNeeded(std::shared_ptr<avalanche::Proof> proof) {
+    return g_avalanche->withPeerManager([&](avalanche::PeerManager &pm) {
+        return pm.getProof(proof->getId()) ||
+               pm.registerProof(std::move(proof));
+    });
+}
+
 static UniValue addavalanchenode(const Config &config,
                                  const JSONRPCRequest &request) {
     RPCHelpMan{
@@ -95,7 +102,7 @@ static UniValue addavalanchenode(const Config &config,
     }
 
     const avalanche::ProofId &proofid = proof->getId();
-    if (!g_avalanche->getProof(proofid) && !g_avalanche->addProof(proof)) {
+    if (!registerProofIfNeeded(proof)) {
         return false;
     }
 
@@ -576,7 +583,7 @@ static UniValue sendavalancheproof(const Config &config,
     // verification has already been done, a failure likely indicates that there
     // already is a proof with conflicting utxos.
     const avalanche::ProofId &proofid = proof->getId();
-    if (!g_avalanche->getProof(proofid) && !g_avalanche->addProof(proof)) {
+    if (!registerProofIfNeeded(proof)) {
         throw JSONRPCError(
             RPC_INVALID_PARAMETER,
             "The proof has conflicting utxo with an existing proof");

commit 3e097cd624915ba69a51ccf3ce5301315dcced99

[avalanche] Fix a lock order inversion on shutdown
Summary: The lock order is touchy during shutdown and was broken by D9768.

Test Plan:
With debug:
  ./test/functional/test_runner.py abc_p2p_proof_inventory

Reviewers: #bitcoin_abc, PiRK

Reviewed By: #bitcoin_abc, PiRK

Differential Revision: https://reviews.bitcoinabc.org/D9791

src/net_processing.cpp
@@ -4394,25 +4394,26 @@ void PeerManager::ProcessMessage(const Config &config, CNode &pfrom,
 
         // addProof should not be called while cs_proofrequest because it holds
         // cs_main and that creates a potential deadlock during shutdown
-        g_avalanche->withPeerManager([&](avalanche::PeerManager &pm) {
-            if (pm.registerProof(proof)) {
-                WITH_LOCK(cs_proofrequest, m_proofrequest.ForgetInvId(proofid));
-                RelayProof(proofid, m_connman);
 
-                LogPrint(BCLog::NET,
-                         "New avalanche proof: peer=%d, proofid %s\n", nodeid,
-                         proofid.ToString());
-            } else {
-                // If the proof couldn't be added, it can be either orphan or
-                // invalid. In the latter case we should increase the ban score.
-                // TODO improve the ban reason by printing the validation state
-                if (!pm.getOrphan(proofid)) {
-                    WITH_LOCK(cs_rejectedProofs,
-                              rejectedProofs->insert(proofid));
-                    Misbehaving(nodeid, 100, "invalid-avaproof");
-                }
+        if (g_avalanche->withPeerManager([&](avalanche::PeerManager &pm) {
+                return pm.registerProof(proof);
+            })) {
+            WITH_LOCK(cs_proofrequest, m_proofrequest.ForgetInvId(proofid));
+            RelayProof(proofid, m_connman);
+
+            LogPrint(BCLog::NET, "New avalanche proof: peer=%d, proofid %s\n",
+                     nodeid, proofid.ToString());
+        } else {
+            // If the proof couldn't be added, it can be either orphan or
+            // invalid. In the latter case we should increase the ban score.
+            // TODO improve the ban reason by printing the validation state
+            if (!g_avalanche->withPeerManager([&](avalanche::PeerManager &pm) {
+                    return pm.getOrphan(proofid);
+                })) {
+                WITH_LOCK(cs_rejectedProofs, rejectedProofs->insert(proofid));
+                Misbehaving(nodeid, 100, "invalid-avaproof");
             }
-        });
+        }
         return;
     }
 