5e1ee2503a55cee0fe096b9c3e41b47a1614a486
PiRK <pierrekn@gmail.com>
2021-07-20 11:03:31 +0200

    make copyright headers more consistent
    Summary:
    "The Bitcoin developers" and "The Bitcoin Core developers" is the
    dominant use. Fix the few occurences of "Developers" (capital D).
    
    Remove one  occurence of a "-" between the authors and the year, and one extra "-" after the first year.
    
    Replace "Bitcoin developers" with "The Bitcoin developers" in one file.
    
    Test Plan:
    Before:
    ```
    $./contrib/devtools/copyright_header.py report .
    34 with unexpected copyright holder names
    ```
    
    After:
    ```
    $./contrib/devtools/copyright_header.py report .
    27 with unexpected copyright holder names
    ```
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D9816

--- src/test/sigcache_tests.cpp
+++ src/test/sigcache_tests.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2012-2015 The Bitcoin Core developers
-// Copyright (c) 2019- The Bitcoin developers
+// Copyright (c) 2019 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 // (based on key_tests.cpp)
