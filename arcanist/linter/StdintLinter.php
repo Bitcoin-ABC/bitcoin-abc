b75e910beeb3b9e05a19cab08b4a5fbf40390d1a
Fabien <fabcien@gmail.com>
2021-08-31 18:18:29 +0200

    [LINTER] Replace unsigned short with uint16_t
    Summary:
    Similar to uint8_t, suggested by @deadalnix.
    
    Depends on D9987.
    
    Test Plan:
      arc lint --everything
    Undo any change from D9987 and Check the linter proposes a replacement.
    
    Reviewers: #bitcoin_abc, majcosta
    
    Reviewed By: #bitcoin_abc, majcosta
    
    Subscribers: majcosta, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D9998

--- arcanist/linter/StdintLinter.php
+++ arcanist/linter/StdintLinter.php
@@ -6,6 +6,13 @@
 final class StdintLinter extends ArcanistLinter {
 
   const UNSIGNED_CHAR_FOUND = 1;
+  const UNSIGNED_SHORT_FOUND = 2;
+
+  /* Mapping the original type to a tuple (error code, stdint replacement) */
+  const TYPE_MAPPING = array(
+    'unsigned char' => array(self::UNSIGNED_CHAR_FOUND, 'uint8_t'),
+    'unsigned short' => array(self::UNSIGNED_SHORT_FOUND, 'uint16_t'),
+  );
 
   public function getInfoName() {
     return 'lint-stdint';
@@ -26,6 +33,7 @@ final class StdintLinter extends ArcanistLinter {
   public function getLintSeverityMap() {
     return array(
       self::UNSIGNED_CHAR_FOUND => ArcanistLintSeverity::SEVERITY_WARNING,
+      self::UNSIGNED_SHORT_FOUND => ArcanistLintSeverity::SEVERITY_WARNING,
     );
   }
 
@@ -33,6 +41,8 @@ final class StdintLinter extends ArcanistLinter {
     return array(
       self::UNSIGNED_CHAR_FOUND => pht('`uint8_t should be preferred over '.
                                        '`unsigned char``.'),
+      self::UNSIGNED_SHORT_FOUND => pht('`uint16_t should be preferred over '.
+                                       '`unsigned short``.'),
     );
   }
 
@@ -40,17 +50,20 @@ final class StdintLinter extends ArcanistLinter {
     $abspath = Filesystem::resolvePath($path, $this->getProjectRoot());
     $fileContent = Filesystem::readFile($abspath);
 
-    if (preg_match_all('/unsigned char/', $fileContent, $matches,
+    if (preg_match_all('/unsigned (?:char|short)/', $fileContent, $matches,
       PREG_OFFSET_CAPTURE)) {
       foreach ($matches[0] as $match) {
-        list($unsignedChar, $offset) = $match;
+        list($unsignedType, $offset) = $match;
+        list($errorCode, $replacement) = self::TYPE_MAPPING[$unsignedType];
 
         $this->raiseLintAtOffset(
           $offset,
-          self::UNSIGNED_CHAR_FOUND,
-          pht('`uint8_t should be preferred over `unsigned char`'),
-          $unsignedChar,
-          'uint8_t');
+          $errorCode,
+          pht(
+            '`'.$replacement.'` should be preferred over `'.$unsignedType.'`'),
+          $unsignedType,
+          $replacement,
+        );
       }
     }
   }
