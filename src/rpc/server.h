bitcoin-ghost/bitcoin-core-ladder-script

rpc: Assert that passed arg names are equal to hardcoded ones

src/rpc/server.h
@@ -112,6 +112,8 @@ public:
               fn().GetArgNames(),
               intptr_t(fn))
     {
+        CHECK_NONFATAL(fn().m_name == name_in);
+        CHECK_NONFATAL(fn().GetArgNames() == args_in);
     }
 
     //! Simplified constructor taking plain rpcfn_type function pointer.

Bitcoin-ABC/bitcoin-abc

Summary:
This is a backport of [[https://github.com/bitcoin/bitcoin/pull/19386 | core#19386]] [3/5]
https://github.com/bitcoin/bitcoin/pull/19386/commits/fa9708f94c01cb8bf2971bdf404af38c38fa341b
Depends on D10007
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, majcosta
Reviewed By: #bitcoin_abc, majcosta
Differential Revision: https://reviews.bitcoinabc.org/D10008

src/rpc/server.h
@@ -174,7 +174,10 @@ public:
                   result = _fn().HandleRequest(config, request);
                   return true;
               },
-              _fn().GetArgNames(), intptr_t(_fn)) {}
+              _fn().GetArgNames(), intptr_t(_fn)) {
+        CHECK_NONFATAL(_fn().m_name == name_in);
+        CHECK_NONFATAL(_fn().GetArgNames() == args_in);
+    }
 
     //! Simplified constructor taking plain rpcfn_type function pointer.
     CRPCCommand(const char *_category, const char *_name, rpcfn_type _fn,