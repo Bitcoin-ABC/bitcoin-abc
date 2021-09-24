181d570e4a3cb106e33237c1c42c344546b26acd
Jon Atack <jon@atack.com>
2021-09-24 12:26:59 +0200

    test: avoid non-determinism in asmap-addrman test
    Summary:
    ```
    The probability of collision when adding an addrman entry is
    expected to be 1/2^16 = 1/65536 for an address from a different /16.
    
    This change hopes to avoid these collisions by adding 1 tried entry before adding 1 new table one, instead of 2 tried entries followed by 2 new entries, which appears to have caused a collision in the CI.
    ```
    
    Backport of [[https://github.com/bitcoin/bitcoin/pull/23084 | core#23084]].
    
    Test Plan:
    In a loop:
      ./test/functional/test_runner.py feature_asmap
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D12363

--- test/functional/feature_asmap.py
+++ test/functional/feature_asmap.py
@@ -47,8 +47,8 @@ class AsmapTest(BitcoinTestFramework):
         ]]
 
     def fill_addrman(self, node_id):
-        """Add 2 tried addresses to the addrman, followed by 2 new addresses."""
-        for addr, tried in [[0, True], [1, True], [2, False], [3, False]]:
+        """Add 1 tried address to the addrman, followed by 1 new address."""
+        for addr, tried in [[0, True], [1, False]]:
             self.nodes[node_id].addpeeraddress(
                 address=f"101.{addr}.0.0", tried=tried, port=8333)
 
@@ -97,7 +97,7 @@ class AsmapTest(BitcoinTestFramework):
         self.restart_node(0, ["-asmap", "-checkaddrman=1"])
         with self.node.assert_debug_log(
             expected_msgs=[
-                "Addrman checks started: new 2, tried 2, total 4",
+                "Addrman checks started: new 1, tried 1, total 2",
                 "Addrman checks completed successfully",
             ]
         ):
