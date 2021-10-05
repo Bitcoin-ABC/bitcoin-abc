Bitcoin-ABC/bitcoin-abc

commit 35feef8b43812af554ca1fe218dd99593bc261bc

wallet: Remove Verify and IsLoaded methods
Summary:
Checks are now consolidated in MakeBerkeleyDatabase function instead of
happening in higher level code.

This commit does not change behavior except for error messages which now
include more complete information.

This is a backport of [[https://github.com/bitcoin/bitcoin/pull/19619 | core#19619]] [4/8]
https://github.com/bitcoin/bitcoin/pull/19619/commits/3c815cfe54087fd139169161d2fd175e99840e6a

Depends on D10226

Test Plan: `ninja all check-all`

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D10227

src/wallet/db.h
@@ -160,9 +160,6 @@ public:
     unsigned int nLastFlushed;
     int64_t nLastWalletUpdate;
 
-    /** Verifies the environment and database file */
-    virtual bool Verify(bilingual_str &error) = 0;
-
     std::string m_file_path;
 
     /** Make a DatabaseBatch connected to this database */
@@ -214,7 +211,6 @@ public:
     bool PeriodicFlush() override { return true; }
     void IncrementUpdateCounter() override { ++nUpdateCounter; }
     void ReloadDbEnv() override {}
-    bool Verify(bilingual_str &errorStr) override { return true; }
     std::unique_ptr<DatabaseBatch>
     MakeBatch(const char *mode = "r+", bool flush_on_close = true) override {
         return std::make_unique<DummyBatch>();

src/wallet/wallet.cpp
@@ -227,7 +227,7 @@ LoadWalletInternal(interfaces::Chain &chain, const std::string &name,
                    const DatabaseOptions &options, DatabaseStatus &status,
                    bilingual_str &error, std::vector<bilingual_str> &warnings) {
     try {
-        if (!CWallet::Verify(chain, name, error, warnings)) {
+        if (!MakeWalletDatabase(name, options, status, error)) {
             error = Untranslated("Wallet file verification failed.") +
                     Untranslated(" ") + error;
             return nullptr;
@@ -299,7 +299,7 @@ CreateWallet(interfaces::Chain &chain, const std::string &name,
 
     // Wallet::Verify will check if we're trying to create a wallet with a
     // duplicate name.
-    if (!CWallet::Verify(chain, name, error, warnings)) {
+    if (!MakeWalletDatabase(name, options, status, error)) {
         error = Untranslated("Wallet file verification failed.") +
                 Untranslated(" ") + error;
         status = DatabaseStatus::FAILED_VERIFY;
@@ -4214,16 +4214,15 @@ CWallet::GetDestValues(const std::string &prefix) const {
     return values;
 }
 
-bool CWallet::Verify(interfaces::Chain &chain, const std::string &name,
-                     bilingual_str &error_string,
-                     std::vector<bilingual_str> &warnings) {
+std::unique_ptr<WalletDatabase>
+MakeWalletDatabase(const std::string &name, const DatabaseOptions &options,
+                   DatabaseStatus &status, bilingual_str &error_string) {
     // Do some checking on wallet path. It should be either a:
     //
     // 1. Path where a directory can be created.
     // 2. Path to an existing directory.
     // 3. Path to a symlink to a directory.
     // 4. For backwards compatibility, the name of a data file in -walletdir.
-    LOCK(cs_wallets);
     const fs::path &wallet_path = fs::absolute(name, GetWalletDir());
     fs::file_type path_type = fs::symlink_status(wallet_path).type();
     if (!(path_type == fs::file_not_found || path_type == fs::directory_file ||
@@ -4238,30 +4237,10 @@ bool CWallet::Verify(interfaces::Chain &chain, const std::string &name,
                       "or (for backwards compatibility) the name of an "
                       "existing data file in -walletdir (%s)",
                       name, GetWalletDir()));
-        return false;
+        status = DatabaseStatus::FAILED_BAD_PATH;
+        return nullptr;
     }
-
-    // Make sure that the wallet path doesn't clash with an existing wallet path
-    if (IsWalletLoaded(wallet_path)) {
-        error_string = Untranslated(strprintf(
-            "Error loading wallet %s. Duplicate -wallet filename specified.",
-            name));
-        return false;
-    }
-
-    // Keep same database environment instance across Verify/Recover calls
-    // below.
-    std::unique_ptr<WalletDatabase> database =
-        CreateWalletDatabase(wallet_path);
-
-    try {
-        return database->Verify(error_string);
-    } catch (const fs::filesystem_error &e) {
-        error_string =
-            Untranslated(strprintf("Error loading wallet %s. %s", name,
-                                   fsbridge::get_filesystem_error_message(e)));
-        return false;
-    }
+    return MakeDatabase(wallet_path, options, status, error_string);
 }
 
 std::shared_ptr<CWallet> CWallet::CreateWalletFromFile(

src/wallet/bdb.cpp
@@ -61,21 +61,6 @@ bool WalletDatabaseFileId::operator==(const WalletDatabaseFileId &rhs) const {
     return memcmp(value, &rhs.value, sizeof(value)) == 0;
 }
 
-bool IsBDBWalletLoaded(const fs::path &wallet_path) {
-    fs::path env_directory;
-    std::string database_filename;
-    SplitWalletPath(wallet_path, env_directory, database_filename);
-
-    LOCK(cs_db);
-    auto env = g_dbenvs.find(env_directory.string());
-    if (env == g_dbenvs.end()) {
-        return false;
-    }
-
-    auto database = env->second.lock();
-    return database && database->IsDatabaseLoaded(database_filename);
-}
-
 /**
  * @param[in] wallet_path Path to wallet directory. Or (for backwards
  * compatibility only) a path to a berkeley btree data file inside a wallet

src/wallet/bdb.h
@@ -57,9 +57,6 @@ public:
     void MakeMock();
     bool IsMock() const { return fMockDb; }
     bool IsInitialized() const { return fDbEnvInit; }
-    bool IsDatabaseLoaded(const std::string &db_filename) const {
-        return m_databases.find(db_filename) != m_databases.end();
-    }
     fs::path Directory() const { return strPath; }
 
     bool Open(bilingual_str &error);
@@ -83,9 +80,6 @@ public:
 /** Get BerkeleyEnvironment and database filename given a wallet path. */
 std::shared_ptr<BerkeleyEnvironment>
 GetWalletEnv(const fs::path &wallet_path, std::string &database_filename);
-
-/** Return whether a BDB wallet database is currently loaded. */
-bool IsBDBWalletLoaded(const fs::path &wallet_path);
 
 /** Check format of database file */
 bool IsBerkeleyBtree(const fs::path &path);
@@ -161,7 +155,7 @@ public:
     void ReloadDbEnv() override;
 
     /** Verifies the environment and database file */
-    bool Verify(bilingual_str &error) override;
+    bool Verify(bilingual_str &error);
 
     /**
      * Pointer to shared database environment.

src/wallet/load.cpp
@@ -62,14 +62,11 @@ bool VerifyWallets(interfaces::Chain &chain,
             return false;
         }
 
+        DatabaseOptions options;
+        DatabaseStatus status;
+        options.verify = true;
         bilingual_str error_string;
-        std::vector<bilingual_str> warnings;
-        bool verify_success =
-            CWallet::Verify(chain, wallet_file, error_string, warnings);
-        if (!warnings.empty()) {
-            chain.initWarning(Join(warnings, Untranslated("\n")));
-        }
-        if (!verify_success) {
+        if (!MakeWalletDatabase(wallet_file, options, status, error_string)) {
             chain.initError(error_string);
             return false;
         }

src/wallet/wallet.h
@@ -73,6 +73,9 @@ CreateWallet(interfaces::Chain &chain, const std::string &name,
              DatabaseStatus &status, bilingual_str &error,
              std::vector<bilingual_str> &warnings);
 std::unique_ptr<interfaces::Handler> HandleLoadWallet(LoadWalletFn load_wallet);
+std::unique_ptr<WalletDatabase>
+MakeWalletDatabase(const std::string &name, const DatabaseOptions &options,
+                   DatabaseStatus &status, bilingual_str &error);
 
 //! -paytxfee default
 constexpr Amount DEFAULT_PAY_TX_FEE = Amount::zero();
@@ -1377,11 +1380,6 @@ public:
      * inputs may be respent.
      */
     bool AbandonTransaction(const TxId &txid);
-
-    //! Verify wallet naming and perform salvage on the wallet if required
-    static bool Verify(interfaces::Chain &chain, const std::string &name,
-                       bilingual_str &error_string,
-                       std::vector<bilingual_str> &warnings);
 
     /**
      * Initializes the wallet, returns a new CWallet instance or a null pointer

src/wallet/walletdb.cpp
@@ -1158,10 +1158,6 @@ std::unique_ptr<WalletDatabase> MakeDatabase(const fs::path &path,
     return MakeBerkeleyDatabase(path, options, status, error);
 }
 
-bool IsWalletLoaded(const fs::path &wallet_path) {
-    return IsBDBWalletLoaded(wallet_path);
-}
-
 /** Return object for accessing database at specified path. */
 std::unique_ptr<WalletDatabase> CreateWalletDatabase(const fs::path &path) {
     std::string filename;

src/wallet/walletdb.h
@@ -301,9 +301,6 @@ bool ReadKeyValue(CWallet *pwallet, CDataStream &ssKey, CDataStream &ssValue,
                   std::string &strType, std::string &strErr,
                   const KeyFilterFn &filter_fn = nullptr);
 
-/** Return whether a wallet database is currently loaded. */
-bool IsWalletLoaded(const fs::path &wallet_path);
-
 /** Return object for accessing database at specified path. */
 std::unique_ptr<WalletDatabase> CreateWalletDatabase(const fs::path &path);
 

commit 0ddd20024bf78ba488530ba06e28914dfc479be6

bugfix: make LoadWallet assigns status always
Summary:
Although loadwallet() in rpcwallet.cpp assumes LoadWallet() always
assign some value to the 'status', but LoadWallet() does not do so
in some situation.

This fixes above and prevends loadwallet() returns ambiguous error code.

This is a backport of [[https://github.com/bitcoin/bitcoin/pull/19919 | core#19919]]

The problem is probably introduced in D10227 or D10229, but I wasn't able to reproduce
it.

Depends on D10231

Test Plan:
```
ninja
for i in $(seq 20); do test/functional/test_runner.py wallet_multiwallet; done
```

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D10240

src/wallet/db.h
@@ -234,6 +234,7 @@ enum class DatabaseStatus {
     FAILED_ALREADY_EXISTS,
     FAILED_NOT_FOUND,
     FAILED_CREATE,
+    FAILED_LOAD,
     FAILED_VERIFY,
     FAILED_ENCRYPT,
 };

src/wallet/wallet.cpp
@@ -241,6 +241,7 @@ LoadWalletInternal(interfaces::Chain &chain, const std::string &name,
         if (!wallet) {
             error = Untranslated("Wallet loading failed.") + Untranslated(" ") +
                     error;
+            status = DatabaseStatus::FAILED_LOAD;
             return nullptr;
         }
         AddWallet(wallet);
@@ -252,6 +253,7 @@ LoadWalletInternal(interfaces::Chain &chain, const std::string &name,
         return wallet;
     } catch (const std::runtime_error &e) {
         error = Untranslated(e.what());
+        status = DatabaseStatus::FAILED_LOAD;
         return nullptr;
     }
 }
@@ -266,6 +268,7 @@ LoadWallet(interfaces::Chain &chain, const std::string &name,
                             return g_loading_wallet_set.insert(name));
     if (!result.second) {
         error = Untranslated("Wallet already being loading.");
+        status = DatabaseStatus::FAILED_LOAD;
         return nullptr;
     }
     auto wallet = LoadWalletInternal(chain, name, load_on_start, options,