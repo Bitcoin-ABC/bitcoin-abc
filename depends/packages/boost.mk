e7f92999d26d59ab7de4ab0c4ca960cbc54089d4
Pasta <pasta@dashboost.org>
2021-10-22 17:33:25 -0400

    build: Update Boost to 1.81.0 in depends
    Summary: This is a backport of [[https://github.com/bitcoin/bitcoin/pull/24383 | core#24383]], [[https://github.com/bitcoin/bitcoin/pull/25873 | core#25873]] and [[https://github.com/bitcoin/bitcoin/pull/26557 | core#26557]]
    
    Test Plan: guix and gitian builds
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D13985

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1.71.0
+$(package)_version=1.81.0
 $(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$($(package)_version)/source/
 $(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.bz2
-$(package)_sha256_hash=d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee
+$(package)_sha256_hash=71feeed900fbccca04a3b4f2f84a7c217186f28a940ed8b7ed4725986baf99fa
 $(package)_dependencies=native_b2
 
 define $(package)_set_vars
