2096754c7dddb9dd68fe711bf256405d62a1db7b
John Newbery <john@johnnewbery.com>
2021-11-03 12:06:40 +0000

    [logging/documentation] Remove reference to AcceptToMemoryPool from error string
    Summary:
    User-facing error messages should not leak internal implementation
    details like function names. Update the MEMPOOL_REJECTED error string
    from "Transaction rejected by AcceptToMemoryPool" to the more generic
    "Transaction rejected by mempool". Also update the MEMPOOL_ERROR error
    message from "AcceptToMemoryPool failed" to the more precise "Mempool
    internal error" since this error indicates and internal (e.g.
    logic/hardware/etc) failure, and not a transaction rejection.
    
    These error messages are not covered by tests because they are overwritten with more precise error messages from the ATMP validation state in `sendrawtransaction`.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/23173 | core#23173]]
    https://github.com/bitcoin/bitcoin/pull/23173/commits/36167faea92c97ddea7403280a5074073c8e5f90
    
    Test Plan: `ninja all check check-functional-extended`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D12234

--- src/util/error.cpp
+++ src/util/error.cpp
@@ -20,9 +20,9 @@ bilingual_str TransactionErrorString(const TransactionError error) {
             return Untranslated(
                 "Peer-to-peer functionality missing or disabled");
         case TransactionError::MEMPOOL_REJECTED:
-            return Untranslated("Transaction rejected by AcceptToMemoryPool");
+            return Untranslated("Transaction rejected by mempool");
         case TransactionError::MEMPOOL_ERROR:
-            return Untranslated("AcceptToMemoryPool failed");
+            return Untranslated("Mempool internal error");
         case TransactionError::INVALID_PSBT:
             return Untranslated("PSBT is not sane");
         case TransactionError::PSBT_MISMATCH:
