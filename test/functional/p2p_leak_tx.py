hebasto/bitcoin-qtw

test: add p2p_lock acquires in p2p_leak_tx.py

test/functional/p2p_leak_tx.py
@@ -5,7 +5,7 @@
 """Test that we don't leak txs to inbound peers that we haven't yet announced to"""
 
 from test_framework.messages import msg_getdata, CInv, MSG_TX
-from test_framework.p2p import P2PDataStore
+from test_framework.p2p import p2p_lock, P2PDataStore
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
     assert_equal,
@@ -39,14 +39,16 @@ class P2PLeakTxTest(BitcoinTestFramework):
 
             want_tx = msg_getdata()
             want_tx.inv.append(CInv(t=MSG_TX, h=int(txid, 16)))
-            inbound_peer.last_message.pop('notfound', None)
+            with p2p_lock:
+                inbound_peer.last_message.pop('notfound', None)
             inbound_peer.send_and_ping(want_tx)
 
             if inbound_peer.last_message.get('notfound'):
                 self.log.debug('tx {} was not yet announced to us.'.format(txid))
                 self.log.debug("node has responded with a notfound message. End test.")
                 assert_equal(inbound_peer.last_message['notfound'].vec[0].hash, int(txid, 16))
-                inbound_peer.last_message.pop('notfound')
+                with p2p_lock:
+                    inbound_peer.last_message.pop('notfound')
                 break
             else:
                 self.log.debug('tx {} was already announced to us. Try test again.'.format(txid))

Bitcoin-ABC/bitcoin-abc

Summary:
This is a backport of [[https://github.com/bitcoin/bitcoin/pull/20126 | core#20126]] [2/2]
https://github.com/bitcoin/bitcoin/pull/20126/commits/5b77f8098de537898151ab116d0e547fd6ff9466
Test Plan: `ninja check-functional`
Reviewers: #bitcoin_abc, majcosta
Reviewed By: #bitcoin_abc, majcosta
Differential Revision: https://reviews.bitcoinabc.org/D10462

test/functional/p2p_leak_tx.py
@@ -5,7 +5,7 @@
 """Test that we don't leak txs to inbound peers that we haven't yet announced to"""
 
 from test_framework.messages import MSG_TX, CInv, msg_getdata
-from test_framework.p2p import P2PDataStore
+from test_framework.p2p import P2PDataStore, p2p_lock
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal
 from test_framework.wallet import MiniWallet
@@ -40,7 +40,8 @@ class P2PLeakTxTest(BitcoinTestFramework):
 
             want_tx = msg_getdata()
             want_tx.inv.append(CInv(t=MSG_TX, h=int(txid, 16)))
-            inbound_peer.last_message.pop('notfound', None)
+            with p2p_lock:
+                inbound_peer.last_message.pop('notfound', None)
             inbound_peer.send_and_ping(want_tx)
 
             if inbound_peer.last_message.get('notfound'):
@@ -50,7 +51,8 @@ class P2PLeakTxTest(BitcoinTestFramework):
                     "node has responded with a notfound message. End test.")
                 assert_equal(
                     inbound_peer.last_message['notfound'].vec[0].hash, int(txid, 16))
-                inbound_peer.last_message.pop('notfound')
+                with p2p_lock:
+                    inbound_peer.last_message.pop('notfound')
                 break
             else:
                 self.log.debug(