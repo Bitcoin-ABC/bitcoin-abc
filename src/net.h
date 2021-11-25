bitcoin-ghost/bitcoin-core-ladder-script

[Rename only] Rename orphan_work_set to m_orphan_work_set.
This helps distinguish the member from any local variables.

src/net.h
@@ -1042,7 +1042,7 @@ public:
     // Whether a ping is requested.
     std::atomic<bool> fPingQueued{false};
 
-    std::set<uint256> orphan_work_set;
+    std::set<uint256> m_orphan_work_set;
 
     CNode(NodeId id, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn, SOCKET hSocketIn, const CAddress &addrIn, uint64_t nKeyedNetGroupIn, uint64_t nLocalHostNonceIn, const CAddress &addrBindIn, const std::string &addrNameIn, ConnectionType conn_type_in, bool inbound_onion = false);
     ~CNode();

Bitcoin-ABC/bitcoin-abc

Summary:
This is a backport of [[https://github.com/bitcoin/bitcoin/pull/19911 | core#19911]] [1/6]
https://github.com/bitcoin/bitcoin/pull/19911/commits/9c47cb29f9f525ee58acc629825a97075156d764
Test Plan: `ninja`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D10526

src/net.h
@@ -1232,7 +1232,7 @@ public:
     // Whether a ping is requested.
     std::atomic<bool> fPingQueued{false};
 
-    std::set<TxId> orphan_work_set;
+    std::set<TxId> m_orphan_work_set;
 
     CNode(NodeId id, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn,
           SOCKET hSocketIn, const CAddress &addrIn, uint64_t nKeyedNetGroupIn,