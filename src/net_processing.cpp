bitcoin-ghost/bitcoin-core-ladder-script

Lock before checking if orphan_work_set is empty; indicate it is guarded

src/net_processing.cpp
@@ -513,7 +513,7 @@ struct Peer {
     bool m_should_discourage GUARDED_BY(m_misbehavior_mutex){false};
 
     /** Set of txids to reconsider once their parent transactions have been accepted **/
-    std::set<uint256> m_orphan_work_set;
+    std::set<uint256> m_orphan_work_set GUARDED_BY(g_cs_orphans);
 
     Peer(NodeId id) : m_id(id) {}
 };
@@ -3876,9 +3876,11 @@ bool PeerManager::ProcessMessages(CNode* pfrom, std::atomic<bool>& interruptMsgP
     if (!pfrom->vRecvGetData.empty())
         ProcessGetData(*pfrom, m_chainparams, m_connman, m_mempool, interruptMsgProc);
 
-    if (!peer->m_orphan_work_set.empty()) {
+    {
         LOCK2(cs_main, g_cs_orphans);
-        ProcessOrphanTx(peer->m_orphan_work_set);
+        if (!peer->m_orphan_work_set.empty()) {
+            ProcessOrphanTx(peer->m_orphan_work_set);
+        }
     }
 
     if (pfrom->fDisconnect)
@@ -3887,7 +3889,10 @@ bool PeerManager::ProcessMessages(CNode* pfrom, std::atomic<bool>& interruptMsgP
     // this maintains the order of responses
     // and prevents vRecvGetData to grow unbounded
     if (!pfrom->vRecvGetData.empty()) return true;
-    if (!peer->m_orphan_work_set.empty()) return true;
+    {
+        LOCK(g_cs_orphans);
+        if (!peer->m_orphan_work_set.empty()) return true;
+    }
 
     // Don't bother if send buffer is too full to respond anyway
     if (pfrom->fPauseSend)

Bitcoin-ABC/bitcoin-abc

Summary:
This is a backport of [[https://github.com/bitcoin/bitcoin/pull/19911 | core#19911]] [3/6]
https://github.com/bitcoin/bitcoin/pull/19911/commits/673247b58cd1252ab7e99f7d63ead05cc100cef2
Depends on D10527
Test Plan:
```
cmake .. -GNinja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DENABLE_SANITIZERS=thread
ninja && ninja check check-functional
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D10528

src/net_processing.cpp
@@ -611,7 +611,7 @@ struct Peer {
      * Set of txids to reconsider once their parent transactions have been
      * accepted
      */
-    std::set<TxId> m_orphan_work_set;
+    std::set<TxId> m_orphan_work_set GUARDED_BY(g_cs_orphans);
 
     Peer(NodeId id) : m_id(id) {}
 };
@@ -4860,9 +4860,11 @@ bool PeerManager::ProcessMessages(const Config &config, CNode *pfrom,
         ProcessGetData(config, *pfrom, m_connman, m_mempool, interruptMsgProc);
     }
 
-    if (!peer->m_orphan_work_set.empty()) {
+    {
         LOCK2(cs_main, g_cs_orphans);
-        ProcessOrphanTx(config, peer->m_orphan_work_set);
+        if (!peer->m_orphan_work_set.empty()) {
+            ProcessOrphanTx(config, peer->m_orphan_work_set);
+        }
     }
 
     if (pfrom->fDisconnect) {
@@ -4874,8 +4876,11 @@ bool PeerManager::ProcessMessages(const Config &config, CNode *pfrom,
     if (!pfrom->vRecvGetData.empty()) {
         return true;
     }
-    if (!peer->m_orphan_work_set.empty()) {
-        return true;
+    {
+        LOCK(g_cs_orphans);
+        if (!peer->m_orphan_work_set.empty()) {
+            return true;
+        }
     }
 
     // Don't bother if send buffer is too full to respond anyway