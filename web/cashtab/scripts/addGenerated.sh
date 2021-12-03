8c8d185ef5163da7f5c7f6aa80f5d816aeb49d42
Fabien <fabcien@gmail.com>
2021-12-04 00:42:32 +0100

    [Cashtab] Overhaul the addGenerated.sh script
    Summary:
    This script just doesn't work. Here are some of the fixes:
     - Set bash to exit on failure
     - Fix the array of files creation/appending
     - Use an absolute path for the find root so the script can be run from anywhere
     - Only search in src/ to not override the node_modules files
     - Use proper quoting to prevent globbing expansion issues in the file names
     - Rewrite the array loop so it actually runs on each element, not only the first one
     - Build the @generated mark in a variable so the script is not marked as generated
     - Use proper indentation
     - Add the missing newline at the end of the file
     - Make sed compatible with BSD/OSX variant
     - Remove the unnecessary function
     - Remove the unnecessary exit code
    
    The file being no longer marked as generated, this will let the linter do its magic and help preventing new issues from being introduced.
    
    Ref T2030.
    
    Test Plan:
    On both OSX (Catalina) and Linux (Arch):
    Remove the @generated mark from some of the files and run the script several times. Check the mark is added where missing but never duplicated.
    
    Reviewers: #bitcoin_abc, bytesofman
    
    Reviewed By: #bitcoin_abc, bytesofman
    
    Subscribers: bytesofman, hungsam
    
    Maniphest Tasks: T2030
    
    Differential Revision: https://reviews.bitcoinabc.org/D10629

--- web/cashtab/scripts/addGenerated.sh
+++ web/cashtab/scripts/addGenerated.sh
@@ -2,18 +2,27 @@
 
 export LC_ALL=C
 
-auto_add_generated_to_snapshots(){
-snapshot_file_array=() 
-snapshot_file_array+=$(find -name "*.test.js.snap")
-for i in $snapshot_file_array
-do
-if ! grep -q @generated $i
-then
-sed -i '1 s/$/ @generated/' $i
-fi
+set -euo pipefail
+
+# Build the generated marker without having this script being marked as
+# generated itself.
+AT=@
+AT_GENERATED="${AT}generated"
+
+TOPLEVEL=$(git rev-parse --show-toplevel)
+
+# Note that this won't work if the with files containing a space in their name.
+# This can be solved by using a null char delimiter (-d '\0') for read in
+# combination with find's option -print0 to get a compatible output, but this is
+# not supported by all find variants so we'll do without it for now.
+find "${TOPLEVEL}/web/cashtab/src" -name "*.test.js.snap" | while read i; do
+  # It is possible to avoid the grep and have sed do it all, but:
+  #  1. This is a more complex sed usage
+  #  2. It will add the generated mark in files that have it already on another
+  #     line.
+  if ! grep -q "${AT_GENERATED}" "${i}"; then
+    # The -i.bak option tells sed to do in-place replacement and create a backup
+    # file with a .bak suffix. This is mandatory for BSD/OSX compatibility.
+    sed -i.bak "1 s/$/ ${AT_GENERATED}/" "${i}" && rm -f "${i}.bak"
+  fi
 done
-}
-
-auto_add_generated_to_snapshots
-
-exit 0
