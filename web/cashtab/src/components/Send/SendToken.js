08eca63147ed379fe2dcf5d6efe044f3a8389f3c
Kieran <kieranpoconnell@gmail.com>
2021-12-08 13:23:37 -0330

    [Cashtab] Added eToken icon to send page
    Summary:
    Using the same logic as TokenListItem, eToken icon added to send page. Related to task T1988.
    
    Custom Token:
    {F5186811}
    
    Generated Token:
    {F5186810}
    
    Test Plan:
    cd web/cashtab
    npm start
    navigate to eTokens tab
    select a token with a custom icon
    ensure icon displays at top of token table
    navigate back to eTokens tab
    select a token with a generated icon
    ensure icon displays at top of token table
    
    Reviewers: bytesofman, #bitcoin_abc
    
    Reviewed By: bytesofman, #bitcoin_abc
    
    Subscribers: emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D10649

--- web/cashtab/src/components/Send/SendToken.js
+++ web/cashtab/src/components/Send/SendToken.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { WalletContext } from '@utils/context';
-import { Form, message, Row, Col, Alert, Descriptions } from 'antd';
+import { Form, message, Row, Col, Alert, Descriptions, Popover } from 'antd';
 import TokenIconAlert from '@components/Common/Alerts.js';
 import PrimaryButton, {
     SecondaryButton,
@@ -362,6 +362,52 @@ const SendToken = ({ tokenId, jestBCH, passLoadingStatus }) => {
                                     bordered
                                     title={`Token info for "${token.info.tokenName}"`}
                                 >
+                                    <Descriptions.Item label="Icon">
+                                        {currency.tokenIconsUrl !== '' ? (
+                                            <Popover
+                                                style={{ width: 256 }}
+                                                content={
+                                                    <Img
+                                                        src={`${currency.tokenIconsUrl}/256/${tokenId}.png`}
+                                                    />
+                                                }
+                                                trigger="click"
+                                                color="transparent"
+                                            >
+                                                <Img
+                                                    src={`${currency.tokenIconsUrl}/32/${tokenId}.png`}
+                                                    width={32}
+                                                    height={32}
+                                                    unloader={
+                                                        <img
+                                                            alt={`identicon of tokenId ${tokenId} `}
+                                                            height="32"
+                                                            width="32"
+                                                            style={{
+                                                                borderRadius:
+                                                                    '50%',
+                                                            }}
+                                                            key={`identicon-${tokenId}`}
+                                                            src={makeBlockie(
+                                                                tokenId,
+                                                            )}
+                                                        />
+                                                    }
+                                                />
+                                            </Popover>
+                                        ) : (
+                                            <img
+                                                alt={`identicon of tokenId ${tokenId} `}
+                                                height="32"
+                                                width="32"
+                                                style={{
+                                                    borderRadius: '50%',
+                                                }}
+                                                key={`identicon-${tokenId}`}
+                                                src={makeBlockie(tokenId)}
+                                            />
+                                        )}
+                                    </Descriptions.Item>
                                     <Descriptions.Item label="Decimals">
                                         {token.info.decimals}
                                     </Descriptions.Item>
