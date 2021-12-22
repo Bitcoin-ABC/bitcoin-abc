fcf196f63c2a68065adf4a013d04cebe8c40a690
PiRK <pierrekn@gmail.com>
2021-12-22 17:21:44 +0100

    cast bool operands to int to silence compiler warning
    Summary:
    I noticed the compiler warning while running the test plan for D10725.
    
    ```
    ../src/interfaces/chain.cpp:291:20: warning: use of bitwise '&' with boolean operands [-Wbitwise-instead-of-logical]
                return FillBlock(ancestor, ancestor_out, lock) &
                       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ```
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/23573 | core#23573]]
    
    Test Plan:
    ```
    cmake .. -GNinja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
    ninja all check-all
    ```
    I'm using clang 14.0.0
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D10726

--- src/interfaces/chain.cpp
+++ src/interfaces/chain.cpp
@@ -287,10 +287,11 @@ namespace {
             const CBlockIndex *ancestor =
                 block1 && block2 ? LastCommonAncestor(block1, block2) : nullptr;
             // Using & instead of && below to avoid short circuiting and leaving
-            // output uninitialized.
-            return FillBlock(ancestor, ancestor_out, lock) &
-                   FillBlock(block1, block1_out, lock) &
-                   FillBlock(block2, block2_out, lock);
+            // output uninitialized. Cast bool to int to avoid
+            // -Wbitwise-instead-of-logical compiler warnings.
+            return int{FillBlock(ancestor, ancestor_out, lock)} &
+                   int{FillBlock(block1, block1_out, lock)} &
+                   int{FillBlock(block2, block2_out, lock)};
         }
         void findCoins(std::map<COutPoint, Coin> &coins) override {
             return FindCoins(m_node, coins);
