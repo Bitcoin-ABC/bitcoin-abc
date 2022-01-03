bitcoin-ghost/bitcoin-core-ladder-script

CConnman: initialise at declaration rather than in Start()
Ensure nMaxOutboundTotalBytesSentInCycle and nMaxOutboundCycleStartTime
are initialized even if CConnman::Start() is not called.

src/net.h
@@ -478,8 +478,8 @@ private:
     uint64_t nTotalBytesSent GUARDED_BY(cs_totalBytesSent) {0};
 
     // outbound limit & stats
-    uint64_t nMaxOutboundTotalBytesSentInCycle GUARDED_BY(cs_totalBytesSent);
-    uint64_t nMaxOutboundCycleStartTime GUARDED_BY(cs_totalBytesSent);
+    uint64_t nMaxOutboundTotalBytesSentInCycle GUARDED_BY(cs_totalBytesSent) {0};
+    uint64_t nMaxOutboundCycleStartTime GUARDED_BY(cs_totalBytesSent) {0};
     uint64_t nMaxOutboundLimit GUARDED_BY(cs_totalBytesSent);
     uint64_t nMaxOutboundTimeframe GUARDED_BY(cs_totalBytesSent);
 

Bitcoin-ABC/bitcoin-abc

Summary:
Ensure `nMaxOutboundTotalBytesSentInCycle` and `nMaxOutboundCycleStartTime` are initialized even if `CConnman::Start()` is not called.  Prevents failures in test/fuzz/connman when run under valgrind.
`nTotalBytesRecv` and `nTotalBytesSent` are already initialized in the class definition, and `CConnman::Start` is only called once in `AppInitMain`, so removing the reinitialization in `Start` has no impact.
This is a backport of [[https://github.com/bitcoin/bitcoin/pull/20408 | core#20408]]
Test Plan:
With TSAN:
`ninja && ninja check check-functional`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D10740

src/net.h
@@ -537,8 +537,8 @@ private:
     uint64_t nTotalBytesSent GUARDED_BY(cs_totalBytesSent){0};
 
     // outbound limit & stats
-    uint64_t nMaxOutboundTotalBytesSentInCycle GUARDED_BY(cs_totalBytesSent);
-    uint64_t nMaxOutboundCycleStartTime GUARDED_BY(cs_totalBytesSent);
+    uint64_t nMaxOutboundTotalBytesSentInCycle GUARDED_BY(cs_totalBytesSent){0};
+    uint64_t nMaxOutboundCycleStartTime GUARDED_BY(cs_totalBytesSent){0};
     uint64_t nMaxOutboundLimit GUARDED_BY(cs_totalBytesSent);
     uint64_t nMaxOutboundTimeframe GUARDED_BY(cs_totalBytesSent);
 