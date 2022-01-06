7d75d11530e2257e341a2143ef739f381298162b
Joey King <joseph.roy.king@gmail.com>
2022-01-06 04:43:29 -0600

    [Cashtab] Add correct dev dependency for ESlint types in jest
    Summary:
    Prelim for T2088
    
    Cashtab devs will see linting errors in their IDE for all test files as the correct dev dependency for the jest types used is not installed. This diff corrects the issue.
    
    Test Plan:
    1. Open any `.test.js` file in VS code
    2. Confirm jest terms are not highlighted in red as lint errors (e.g. `it`, `describe`, `expect`)
    
    Reviewers: Fabien, emack, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D10770

--- web/cashtab/.eslintrc.js
+++ web/cashtab/.eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:jest/recommended',
+    ],
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
@@ -12,6 +16,7 @@ module.exports = {
         ecmaVersion: 12,
         sourceType: 'module',
     },
-    plugins: ['react'],
-    rules: {},
+    plugins: ['react', 'jest'],
+    rules: { 'jest/no-mocks-import': 'off' },
+    settings: { react: { version: 'detect' } },
 };
