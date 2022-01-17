Bitcoin-ABC/bitcoin-abc

commit 01d89460d99421353f57d16df74c989b58d9d531

qt, wallet: Remove unused AskPassphraseDialog::Decrypt
Summary:
Co-authored-by: Sehyun Chung <sehyun@berkeley.edu>

This is a backport of [[https://github.com/bitcoin-core/gui/pull/109 | core-gui#109]] [1/3]
https://github.com/bitcoin-core/gui/pull/109/commits/6e950118a31fd6a85026d934fc6adb6255e47e23

Test Plan: `ninja all check-all`

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D10737

src/qt/askpassphrasedialog.cpp
@@ -56,15 +56,6 @@ AskPassphraseDialog::AskPassphraseDialog(Mode _mode, QWidget *parent,
             ui->passEdit3->hide();
             setWindowTitle(tr("Unlock wallet"));
             break;
-        case Decrypt: // Ask passphrase
-            ui->warningLabel->setText(tr("This operation needs your wallet "
-                                         "passphrase to decrypt the wallet."));
-            ui->passLabel2->hide();
-            ui->passEdit2->hide();
-            ui->passLabel3->hide();
-            ui->passEdit3->hide();
-            setWindowTitle(tr("Decrypt wallet"));
-            break;
         case ChangePass: // Ask old passphrase + new passphrase x2
             setWindowTitle(tr("Change passphrase"));
             ui->warningLabel->setText(tr(
@@ -186,15 +177,6 @@ void AskPassphraseDialog::accept() {
                                       e.what());
             }
             break;
-        case Decrypt:
-            if (!model->setWalletEncrypted(false, oldpass)) {
-                QMessageBox::critical(this, tr("Wallet decryption failed"),
-                                      tr("The passphrase entered for the "
-                                         "wallet decryption was incorrect."));
-            } else {
-                QDialog::accept(); // Success
-            }
-            break;
         case ChangePass:
             if (newpass1 == newpass2) {
                 if (model->changePassphrase(oldpass, newpass1)) {
@@ -225,10 +207,7 @@ void AskPassphraseDialog::textChanged() {
             acceptable = !ui->passEdit2->text().isEmpty() &&
                          !ui->passEdit3->text().isEmpty();
             break;
-        case Unlock: // Old passphrase x1
-        case Decrypt:
-            acceptable = !ui->passEdit1->text().isEmpty();
-            break;
+        case Unlock:     // Old passphrase x1
         case ChangePass: // Old passphrase x1, new passphrase x2
             acceptable = !ui->passEdit1->text().isEmpty() &&
                          !ui->passEdit2->text().isEmpty() &&

src/qt/askpassphrasedialog.h
@@ -26,7 +26,6 @@ public:
         Encrypt,    /**< Ask passphrase twice and encrypt */
         Unlock,     /**< Ask passphrase and unlock */
         ChangePass, /**< Ask old passphrase + new passphrase twice */
-        Decrypt     /**< Ask passphrase and decrypt wallet */
     };
 
     explicit AskPassphraseDialog(Mode mode, QWidget *parent,

src/qt/walletview.cpp
@@ -350,10 +350,7 @@ void WalletView::encryptWallet(bool status) {
     if (!walletModel) {
         return;
     }
-
-    AskPassphraseDialog dlg(status ? AskPassphraseDialog::Encrypt
-                                   : AskPassphraseDialog::Decrypt,
-                            this);
+    AskPassphraseDialog dlg(AskPassphraseDialog::Encrypt, this);
     dlg.setModel(walletModel);
     dlg.exec();
 

commit 4fc92bdfa77df101642adeaa5ef870c89bce5b99

qt: unlock wallet "OK" button bugfix
Summary:
When trying to send a transaction from an encrypted wallet, the ask passphrase dialog would not allow the user to click the "OK" button and proceed. Therefore it was impossible to send a transaction through the gui. It was not enabling the "OK" button after the passphrase was entered by the user, because it was using the same form validation logic as the "Change passphrase" flow.

This bug was introduced in D10737 (v0.24.7).

This is a backport of [[https://github.com/bitcoin-core/gui/pull/138 | core-gui#138]]

Test Plan: In `bitcoin-qt`, try to send a transaction using an encrypted wallet. Verify that the "OK" button is not disabled in the passphrase dialog after typing at least one character in the passphrase field.

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Subscribers: Fabien

Differential Revision: https://reviews.bitcoinabc.org/D10831

src/qt/askpassphrasedialog.cpp
@@ -207,7 +207,9 @@ void AskPassphraseDialog::textChanged() {
             acceptable = !ui->passEdit2->text().isEmpty() &&
                          !ui->passEdit3->text().isEmpty();
             break;
-        case Unlock:     // Old passphrase x1
+        case Unlock: // Old passphrase x1
+            acceptable = !ui->passEdit1->text().isEmpty();
+            break;
         case ChangePass: // Old passphrase x1, new passphrase x2
             acceptable = !ui->passEdit1->text().isEmpty() &&
                          !ui->passEdit2->text().isEmpty() &&