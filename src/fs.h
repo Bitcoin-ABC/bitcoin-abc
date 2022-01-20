Bitcoin-ABC/bitcoin-abc

commit c4dad634abbf9b071f114799ff66b5822a54afb1

Unbreak the build with boost 1.78
Summary:
The recent fs.h changes broke the build with boost 1.78 (see https://github.com/bitcoin/bitcoin/issues/23846), but the piece of code causing the issue is currently unused in our codebase. This diff comments it out until a fix is merged that can be backported (expected to happen soon).

Note: boost 1.78 introduces another issue (change of behavior for boost::filsystem::canonical) which can be worked around by disabling the wallet, so I leave it for now. This will be fixed at the same time as this issue.

Test Plan:
With boost 1.78:
  ninja all check

Reviewers: #bitcoin_abc, PiRK

Reviewed By: #bitcoin_abc, PiRK

Differential Revision: https://reviews.bitcoinabc.org/D10822

src/fs.h
@@ -100,11 +100,17 @@ static inline auto quoted(const std::string &s) {
     return boost::io::quoted(s, '&');
 }
 
+// FIXME The below method is failing with Boost 1.78 (see
+// https://github.com/bitcoin/bitcoin/issues/23846) and is currently unused due
+// to out-of-order backports. Since boost::filesystem is expected to be removed
+// completely by https://github.com/bitcoin/bitcoin/pull/20744, it's fine to
+// keep it commented out and make sure it's fixed before being in use.
+//
 // Allow safe path append operations.
-static inline path operator+(path p1, path p2) {
-    p1 += std::move(p2);
-    return p1;
-}
+// static inline path operator+(path p1, path p2) {
+//     p1 += std::move(p2);
+//     return p1;
+// }
 
 /**
  * Convert path object to byte string. On POSIX, paths natively are byte

commit b9928812c94250c844d7b650accdd7e6d2d0115d

fs: Make compatible with boost 1.78
Summary:
```
Boost 1.78 removed operator+ in a way that breaks our usage of it in a subclass. A proposed workaround for this is to cast the argument to boost::filesystem::path, and this is backwards compatible with older versions of boost.

Additionally, it appears that fs::canonical no longer removes trailing slashes. This was causing a test to fail. The solution is to explicitly remove the trailing separator in the one place that fs::canonical is used.

Lastly, fs::create_directories now has an error message saying create_directories instead of create_directory. This caused wallet_multiwallet.py to fail. The error message check has been updated to be able accept either string.
```

Backport of [[https://github.com/bitcoin/bitcoin/pull/24104 | core#24104]].

This replaces the build failure work around introduced in D10822.

Test Plan:
With boost 1.78:
  ninja all check-all

Reviewers: #bitcoin_abc, PiRK

Reviewed By: #bitcoin_abc, PiRK

Differential Revision: https://reviews.bitcoinabc.org/D10850

src/fs.h
@@ -100,17 +100,11 @@ static inline auto quoted(const std::string &s) {
     return boost::io::quoted(s, '&');
 }
 
-// FIXME The below method is failing with Boost 1.78 (see
-// https://github.com/bitcoin/bitcoin/issues/23846) and is currently unused due
-// to out-of-order backports. Since boost::filesystem is expected to be removed
-// completely by https://github.com/bitcoin/bitcoin/pull/20744, it's fine to
-// keep it commented out and make sure it's fixed before being in use.
-//
 // Allow safe path append operations.
-// static inline path operator+(path p1, path p2) {
-//     p1 += std::move(p2);
-//     return p1;
-// }
+static inline path operator+(path p1, path p2) {
+    p1 += static_cast<boost::filesystem::path &&>(p2);
+    return p1;
+}
 
 /**
  * Convert path object to byte string. On POSIX, paths natively are byte

src/wallet/load.cpp
@@ -23,7 +23,8 @@ bool VerifyWallets(interfaces::Chain &chain) {
         boost::system::error_code error;
         // The canonical path cleans the path, preventing >1 Berkeley
         // environment instances for the same directory
-        fs::path canonical_wallet_dir = fs::canonical(wallet_dir, error);
+        fs::path canonical_wallet_dir =
+            fs::canonical(wallet_dir, error).remove_trailing_separator();
         if (error || !fs::exists(wallet_dir)) {
             chain.initError(
                 strprintf(_("Specified -walletdir \"%s\" does not exist"),

test/functional/wallet_multiwallet.py
@@ -131,7 +131,7 @@ class MultiWalletTest(BitcoinTestFramework):
             assert_equal(os.path.isfile(wallet_file(wallet_name)), True)
 
         # should not initialize if wallet path can't be created
-        exp_stderr = "boost::filesystem::create_directory:"
+        exp_stderr = "boost::filesystem::create_director"
         self.nodes[0].assert_start_raises_init_error(
             ['-wallet=w8/bad'], exp_stderr, match=ErrorMatch.PARTIAL_REGEX)
 
b9928812c94250c844d7b650accdd7e6d2d0115d
Andrew Chow <achow101-github@achow101.com>
2022-01-19 15:21:25 -0500

    fs: Make compatible with boost 1.78
    Summary:
    ```
    Boost 1.78 removed operator+ in a way that breaks our usage of it in a subclass. A proposed workaround for this is to cast the argument to boost::filesystem::path, and this is backwards compatible with older versions of boost.
    
    Additionally, it appears that fs::canonical no longer removes trailing slashes. This was causing a test to fail. The solution is to explicitly remove the trailing separator in the one place that fs::canonical is used.
    
    Lastly, fs::create_directories now has an error message saying create_directories instead of create_directory. This caused wallet_multiwallet.py to fail. The error message check has been updated to be able accept either string.
    ```
    
    Backport of [[https://github.com/bitcoin/bitcoin/pull/24104 | core#24104]].
    
    This replaces the build failure work around introduced in D10822.
    
    Test Plan:
    With boost 1.78:
      ninja all check-all
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D10850

--- src/wallet/load.cpp
+++ src/wallet/load.cpp
@@ -23,7 +23,8 @@ bool VerifyWallets(interfaces::Chain &chain) {
         boost::system::error_code error;
         // The canonical path cleans the path, preventing >1 Berkeley
         // environment instances for the same directory
-        fs::path canonical_wallet_dir = fs::canonical(wallet_dir, error);
+        fs::path canonical_wallet_dir =
+            fs::canonical(wallet_dir, error).remove_trailing_separator();
         if (error || !fs::exists(wallet_dir)) {
             chain.initError(
                 strprintf(_("Specified -walletdir \"%s\" does not exist"),
