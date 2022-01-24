bitcoin-ghost/bitcoin-core-ladder-script

[net processing] Guard m_continuation_block with m_block_inv_mutex

src/net_processing.h
@@ -73,14 +73,14 @@ struct Peer {
      * message. If we can't announce via a `headers` message, we'll fall back to
      * announcing via `inv`. */
     std::vector<uint256> m_blocks_for_headers_relay GUARDED_BY(m_block_inv_mutex);
-
-    /** This peer's reported block height when we connected */
-    std::atomic<int> m_starting_height{-1};
     /** The final block hash that we sent in an `inv` message to this peer.
      * When the peer requests this block, we send an `inv` message to trigger
      * the peer to request the next sequence of block hashes.
      * Most peers use headers-first syncing, which doesn't use this mechanism */
-    uint256 m_continuation_block{};
+    uint256 m_continuation_block GUARDED_BY(m_block_inv_mutex) {};
+
+    /** This peer's reported block height when we connected */
+    std::atomic<int> m_starting_height{-1};
 
     /** Set of txids to reconsider once their parent transactions have been accepted **/
     std::set<uint256> m_orphan_work_set GUARDED_BY(g_cs_orphans);

Bitcoin-ABC/bitcoin-abc

Summary:
Completes backport of [[https://github.com/bitcoin/bitcoin/pull/19829 | core#19829]]:
https://github.com/bitcoin/bitcoin/pull/19829/commits/3002b4af2b4fde63026f8f7c575452a5c989c662
Depends on D10871.
Ref T1696.
Test Plan:
With Clang and debug:
  ninja all check-all
Reviewers: #bitcoin_abc, PiRK
Reviewed By: #bitcoin_abc, PiRK
Maniphest Tasks: T1696
Differential Revision: https://reviews.bitcoinabc.org/D10872

src/net_processing.h
@@ -91,15 +91,16 @@ struct Peer {
     std::vector<BlockHash>
         m_blocks_for_headers_relay GUARDED_BY(m_block_inv_mutex);
 
-    /** This peer's reported block height when we connected */
-    std::atomic<int> m_starting_height{-1};
     /**
      * The final block hash that we sent in an `inv` message to this peer.
      * When the peer requests this block, we send an `inv` message to trigger
      * the peer to request the next sequence of block hashes.
      * Most peers use headers-first syncing, which doesn't use this mechanism
      */
-    BlockHash m_continuation_block{};
+    BlockHash m_continuation_block GUARDED_BY(m_block_inv_mutex){};
+
+    /** This peer's reported block height when we connected */
+    std::atomic<int> m_starting_height{-1};
 
     /**
      * Set of txids to reconsider once their parent transactions have been