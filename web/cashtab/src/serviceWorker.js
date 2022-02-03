acc98ce6271766888f0f307aa17e804e90d049d7
Hưng Sam <minhhung.sam@gmail.com>
2022-02-03 12:18:50 +1100

    [Cashtab] fix stale cached data
    Summary:
    1. Uncofirmed Transaction
    - Unconfirmed tx has blocktime set to "undefined"
    - Cashtab shows unconfirmed tx as being created "Today"
    - Caching unconfirmed tx with CacheFirst strategy will cause the tx to always shown as "Today"
    - This fix make sure that only confirmed txs are cached.
    
    2. SLP Token Stats
    - SLP Token Stats (totalMinted, totalBurned) is specific to a particular moment in time. It does not make sense to cache them
    - This fix remove the caching of SLP Token Stats
    
    3. Removing old caches
    - simply changing the suffix to 'v1.0.1' will discard the old caches
    
    Test Plan:
    - cd web/cashtab
    - npm start
    - navigate to localhost:3000
    - check that the following caches are created:
    	+ cashtab-static-assets-v1.0.1
    	+ cashtab-tx-data-v1.0.1
    	+ cashtab-slp-tx-data-v1.0.1 (if you have token tx)
    - send some XECs and Tokens
    - inspect the caches to verify that unconfirmed txs are not cached
    - wait for the txs become confirmed
    - inspect the caches again to verify that the newly confirmed txs are cached
    
    Reviewers: bytesofman, #bitcoin_abc
    
    Reviewed By: bytesofman, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D10969

--- web/cashtab/src/serviceWorker.js
+++ web/cashtab/src/serviceWorker.js
@@ -3,7 +3,6 @@ import { setCacheNameDetails } from 'workbox-core';
 import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { CacheFirst } from 'workbox-strategies';
-import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { ExpirationPlugin } from 'workbox-expiration';
 
 clientsClaim();
@@ -11,7 +10,7 @@ self.skipWaiting();
 
 // cofingure prefix, suffix, and cacheNames
 const prefix = 'cashtab';
-const suffix = 'v1.0.0';
+const suffix = 'v1.0.1';
 const staticAssetsCache = `static-assets`;
 
 // configure prefix and suffix for default cache names
@@ -24,22 +23,68 @@ setCacheNameDetails({
 // injection point for static assets caching
 precacheAndRoute(self.__WB_MANIFEST);
 
+// A response is only cacheable if
+//  - status code is 200
+//  - it has a blockhash - meaning it has been confirmed
+const isResponseCacheable = async (
+    response,
+    checkResponseDataForCacheableConditons,
+) => {
+    // TODO: add error checking
+    //  response must be of type Response
+    //  checkResponseDataForCacheableConditons() must be a function
+    let cachable = false;
+    if (response && response.status === 200) {
+        const clonedResponse = response.clone();
+        const clonedResponseData = await clonedResponse.json();
+        if (checkResponseDataForCacheableConditons(clonedResponseData)) {
+            cachable = true;
+        }
+    }
+
+    return cachable;
+};
+
+const createCustomPlugin = checkResponseDataForCacheableConditons => {
+    return {
+        cacheWillUpdate: async ({ response }) => {
+            const cacheable = await isResponseCacheable(
+                response,
+                checkResponseDataForCacheableConditons,
+            );
+            if (cacheable) {
+                return response;
+            }
+            return null;
+        },
+    };
+};
+
+const blockhashExistsInTxResponse = responseBodyJson => {
+    return responseBodyJson && responseBodyJson.blockhash;
+};
+
+const blockhashExistsInSlpTxResponse = responseBodyJson => {
+    return (
+        responseBodyJson &&
+        responseBodyJson.retData &&
+        responseBodyJson.retData.blockhash
+    );
+};
+
 // Caching TX and Token Details using CacheFirst Strategy
 const txDetailsCaches = [
     {
         // ecash tx
         path: '/rawtransactions/getRawTransaction/',
         name: `${prefix}-tx-data-${suffix}`,
+        customPlugin: createCustomPlugin(blockhashExistsInTxResponse),
     },
     {
         // slp tx
         path: '/slp/txDetails/',
         name: `${prefix}-slp-tx-data-${suffix}`,
-    },
-    {
-        // slp token
-        path: '/slp/tokenStats/',
-        name: `${prefix}-slp-token-stats-${suffix}`,
+        customPlugin: createCustomPlugin(blockhashExistsInSlpTxResponse),
     },
 ];
 
@@ -49,9 +94,7 @@ txDetailsCaches.forEach(cache => {
         new CacheFirst({
             cacheName: cache.name,
             plugins: [
-                new CacheableResponsePlugin({
-                    statuses: [200],
-                }),
+                cache.customPlugin,
                 new ExpirationPlugin({
                     maxEntries: 1000,
                     maxAgeSeconds: 365 * 24 * 60 * 60,
