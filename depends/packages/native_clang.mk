cef329cd0fb1a84455f6f141f137c0b7f8fb174b
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-02-08 11:06:01 +0200

    depends: update the native_*.mk files
    Summary:
    This brings the following files up to date with the current Bitcoin Core master branch:
    - native_cctools.mk
    - native_clang.mk
    - native_ds_store.mk
    - native_mac_alias.mk
    - native_libtapi.mk
    
    It has at least one fix needed for ARM64 MacOS support.
    
    ----
    
    [[https://github.com/bitcoin/bitcoin/pull/24288 | core#24288]]
    
    > build, refactor: Drop redundant `$(package)_download_file` assignments
    >
    > No need to specify `$(package)_download_file` when it is equal to
    > `$(package)_file_name`.
    
    ----
    
    [[https://github.com/bitcoin/bitcoin/pull/24326 | core#24326]]
    
    > build: remove unused include dir from clang package
    
    > build: remove -f from clang cp invocation
    
    ----
    
    [[https://github.com/bitcoin/bitcoin/pull/25633 | core#25633]]
    
    > cctools: fixup building with LTO
    >
    > Use lto.h from clang+llvm not libtapi. The later is older,
    > and comes bundled with the libtapi repo.
    >
    > Copy libLTO.so when building with FORCE_USE_SYSTEM_CLANG.
    
    > depends: don't restrict --enable-lto to non-guix cctools
    >
    > This wasn't actually disabling LTO support anyways, because it's enabled
    > by default.
    
    ----
    
    [[https://github.com/bitcoin/bitcoin/pull/21658 | core#21658]]
    
    > build: mac_alias 2.2.0
    >
    > Bumps mac_alias version to one that's arm64-darwin compatible
    
    ----
    
    [[https://github.com/bitcoin/bitcoin/pull/25605 | core#25605]]
    
    > depends: update urls for dmg tools
    >
    > These repos have migrated from https://github.com/al45tair/ to
    > https://github.com/dmgbuild/, so update our URLs to point to the new
    > location. Note that GitHub is also already performing the redirect
    > automatically.
    >
    
    ----
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/24288 | core#24288]], [[https://github.com/bitcoin/bitcoin/pull/24326 | core#24326]], [[https://github.com/bitcoin/bitcoin/pull/25633 | core#25633]], [[https://github.com/bitcoin/bitcoin/pull/21658 | core#21658]] and [[https://github.com/bitcoin/bitcoin/pull/25605 | core#25605]]
    
    Test Plan: gitian builds
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14027

--- depends/packages/native_clang.mk
+++ depends/packages/native_clang.mk
@@ -1,7 +1,6 @@
 package=native_clang
 $(package)_version=10.0.1
 $(package)_download_path=https://github.com/llvm/llvm-project/releases/download/llvmorg-$($(package)_version)
-$(package)_download_file=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 $(package)_file_name=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 $(package)_sha256_hash=48b83ef827ac2c213d5b64f5ad7ed082c8bcb712b46644e0dc5045c6f462c231
 
@@ -12,15 +11,13 @@ endef
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_prefix_dir)/lib/clang/$($(package)_version)/include && \
   mkdir -p $($(package)_staging_prefix_dir)/bin && \
-  mkdir -p $($(package)_staging_prefix_dir)/include && \
+  mkdir -p $($(package)_staging_prefix_dir)/include/llvm-c && \
   cp bin/clang $($(package)_staging_prefix_dir)/bin/ && \
   cp -P bin/clang++ $($(package)_staging_prefix_dir)/bin/ && \
   cp bin/dsymutil $($(package)_staging_prefix_dir)/bin/$(host)-dsymutil && \
   cp bin/llvm-config $($(package)_staging_prefix_dir)/bin/ && \
+  cp include/llvm-c/ExternC.h $($(package)_staging_prefix_dir)/include/llvm-c && \
+  cp include/llvm-c/lto.h $($(package)_staging_prefix_dir)/include/llvm-c && \
   cp lib/libLTO.so $($(package)_staging_prefix_dir)/lib/ && \
-  cp -rf lib/clang/$($(package)_version)/include/* $($(package)_staging_prefix_dir)/lib/clang/$($(package)_version)/include/
-endef
-
-define $(package)_postprocess_cmds
-  rmdir include
+  cp -r lib/clang/$($(package)_version)/include/* $($(package)_staging_prefix_dir)/lib/clang/$($(package)_version)/include/
 endef
