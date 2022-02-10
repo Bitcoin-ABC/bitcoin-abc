afbc199feaa202cc96a8456337cc3e400428a231
MarcoFalke <falke.marco@gmail.com>
2022-02-10 18:02:05 +0100

    addrman: Log too low compat value
    Summary:
    ```
    Also remove uint8_t{} casts from values that are already of the same type.
    ```
    
    Backport of [[https://github.com/bitcoin/bitcoin/pull/24312 | core#24312]].
    
    Depends on D12348.
    
    Test Plan:
      ninja all check-all
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D12349

--- test/functional/feature_addrman.py
+++ test/functional/feature_addrman.py
@@ -71,6 +71,18 @@ class AddrmanTest(BitcoinTestFramework):
         assert_equal(self.nodes[0].getnodeaddresses(), [])
 
         self.log.info(
+            "Check that addrman with negative lowest_compatible cannot be read")
+        self.stop_node(0)
+        write_addrman(peers_dat, lowest_compatible=-32)
+        self.nodes[0].assert_start_raises_init_error(
+            expected_msg=init_error(
+                "Corrupted addrman database: The compat value \\(0\\) is lower "
+                "than the expected minimum value 32.: (.+)"
+            ),
+            match=ErrorMatch.FULL_REGEX,
+        )
+
+        self.log.info(
             "Check that addrman from future is overwritten with new addrman")
         self.stop_node(0)
         write_addrman(peers_dat, lowest_compatible=111)
