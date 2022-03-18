dongcarl/bitcoin

validation: Move LoadBlockIndexDB to CChainState
CChainState needed cuz setBlockIndexCandidates

src/validation.cpp
@@ -4133,11 +4133,12 @@ void BlockManager::Unload() {
     m_block_index.clear();
 }
 
-bool static LoadBlockIndexDB(ChainstateManager& chainman, const CChainParams& chainparams) EXCLUSIVE_LOCKS_REQUIRED(cs_main)
+bool CChainState::LoadBlockIndexDB(const CChainParams& chainparams) EXCLUSIVE_LOCKS_REQUIRED(cs_main)
 {
-    if (!chainman.m_blockman.LoadBlockIndex(
+    assert(std::addressof(::ChainstateActive()) == std::addressof(*this));
+    if (!m_blockman.LoadBlockIndex(
             chainparams.GetConsensus(), *pblocktree,
-            ::ChainstateActive().setBlockIndexCandidates)) {
+            setBlockIndexCandidates)) {
         return false;
     }
 
@@ -4161,7 +4162,7 @@ bool static LoadBlockIndexDB(ChainstateManager& chainman, const CChainParams& ch
     // Check presence of blk files
     LogPrintf("Checking all blk files are present...\n");
     std::set<int> setBlkDataFiles;
-    for (const std::pair<const uint256, CBlockIndex*>& item : chainman.BlockIndex()) {
+    for (const std::pair<const uint256, CBlockIndex*>& item : m_blockman.m_block_index) {
         CBlockIndex* pindex = item.second;
         if (pindex->nStatus & BLOCK_HAVE_DATA) {
             setBlkDataFiles.insert(pindex->nFile);
@@ -4591,7 +4592,7 @@ bool ChainstateManager::LoadBlockIndex(const CChainParams& chainparams)
     // Load block index from databases
     bool needs_init = fReindex;
     if (!fReindex) {
-        bool ret = LoadBlockIndexDB(*this, chainparams);
+        bool ret = ActiveChainstate().LoadBlockIndexDB(chainparams);
         if (!ret) return false;
         needs_init = m_blockman.m_block_index.empty();
     }

Bitcoin-ABC/bitcoin-abc

Summary:
This is a backport of [[https://github.com/bitcoin/bitcoin/pull/21055 | core#21055]] [9/16]
https://github.com/bitcoin/bitcoin/pull/21055/commits/8cdb2f7e58dfd9a631a8cbb8f0ee7e8c0c304eb4#
Depends on D11213
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D11214

src/validation.cpp
@@ -4817,12 +4817,10 @@ void BlockManager::Unload() {
     m_block_index.clear();
 }
 
-static bool LoadBlockIndexDB(ChainstateManager &chainman,
-                             const Consensus::Params &params)
-    EXCLUSIVE_LOCKS_REQUIRED(cs_main) {
-    if (!chainman.m_blockman.LoadBlockIndex(
-            params, *pblocktree,
-            ::ChainstateActive().setBlockIndexCandidates)) {
+bool CChainState::LoadBlockIndexDB(const Consensus::Params &params) {
+    assert(std::addressof(::ChainstateActive()) == std::addressof(*this));
+    if (!m_blockman.LoadBlockIndex(params, *pblocktree,
+                                   setBlockIndexCandidates)) {
         return false;
     }
 
@@ -4848,7 +4846,7 @@ static bool LoadBlockIndexDB(ChainstateManager &chainman,
     LogPrintf("Checking all blk files are present...\n");
     std::set<int> setBlkDataFiles;
     for (const std::pair<const BlockHash, CBlockIndex *> &item :
-         chainman.BlockIndex()) {
+         m_blockman.m_block_index) {
         CBlockIndex *pindex = item.second;
         if (pindex->nStatus.hasData()) {
             setBlkDataFiles.insert(pindex->nFile);
@@ -5260,7 +5258,7 @@ bool ChainstateManager::LoadBlockIndex(const Consensus::Params &params) {
     // Load block index from databases
     bool needs_init = fReindex;
     if (!fReindex) {
-        bool ret = LoadBlockIndexDB(*this, params);
+        bool ret = ActiveChainstate().LoadBlockIndexDB(params);
         if (!ret) {
             return false;
         }