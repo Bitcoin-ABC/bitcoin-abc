488cdcd7aa9416efce22fbd6084d1e23cb7dc830
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-03-25 21:37:28 +0100

    util, refactor: Use GetPathArg to read "-rpccookiefile" value
    Summary:
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/24675 | core#24675]]
    https://github.com/bitcoin/bitcoin/pull/24675/commits/138c668e2b4d64279ddefbe07c1d9b7c3d3c537c
    
    Depends on D16199
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D16200

--- src/rpc/request.cpp
+++ src/rpc/request.cpp
@@ -68,15 +68,15 @@ UniValue JSONRPCError(int code, const std::string &message) {
  */
 static const std::string COOKIEAUTH_USER = "__cookie__";
 /** Default name for auth cookie file */
-static const std::string COOKIEAUTH_FILE = ".cookie";
+static const char *const COOKIEAUTH_FILE = ".cookie";
 
 /** Get name of RPC authentication cookie file */
 static fs::path GetAuthCookieFile(bool temp = false) {
-    std::string arg = gArgs.GetArg("-rpccookiefile", COOKIEAUTH_FILE);
+    fs::path arg = gArgs.GetPathArg("-rpccookiefile", COOKIEAUTH_FILE);
     if (temp) {
         arg += ".tmp";
     }
-    return AbsPathForConfigVal(gArgs, fs::PathFromString(arg));
+    return AbsPathForConfigVal(gArgs, arg);
 }
 
 bool GenerateAuthCookie(std::string *cookie_out) {
