b75642178a71088e0524f93a94ba742fb0dda431
bytesofman <joey@bitcoinabc.org>
2022-03-25 09:36:40 -0700

    [Cashtab] Run tests in UTC timezone
    Summary:
    T2346
    
    This feature is already in Cashtab, needs to be copied over here to the upgraded test script in new CRA base
    
    Test Plan: `npm test` and all tests pass
    
    Reviewers: #bitcoin_abc, kieran709
    
    Reviewed By: #bitcoin_abc, kieran709
    
    Differential Revision: https://reviews.bitcoinabc.org/D11263

--- web/cashtab-v2/scripts/test.js
+++ web/cashtab-v2/scripts/test.js
@@ -4,6 +4,7 @@
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
+process.env.TZ = 'UTC';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
