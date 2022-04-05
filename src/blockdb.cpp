2543f05bf02b14bb74c5e1208a200c6335f4075b
PiRK <pierrekn@gmail.com>
2022-04-05 11:21:26 +0200

    add a copyright header in blockdb.cpp
    Summary: As per title
    
    Test Plan: NA
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D11304

--- src/blockdb.cpp
+++ src/blockdb.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2020-2022 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <blockdb.h>
 
 #include <blockindex.h>
