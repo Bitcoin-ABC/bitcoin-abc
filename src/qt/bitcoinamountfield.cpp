cdc0de237e2060f2c0fa0b44e78a6efb0f5aea5f
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-04-08 23:56:40 +0200

    qt: Remove `QApplication::globalStrut()` call
    Summary:
    This function has been deprecated in Qt 5.15.0, and has been removed in
    Qt 6 (see 033d01bd6e2aef740ad1408a04d3ca0ae3b9ba9b upstream commit).
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/579 | core#579]]
    
    Depends on D18138
    
    Test Plan:
    `ninja all check-all`
    
    Run bitcoin-qt, observe that nothing absurd happens to the Amount field (Send tab)
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D18139

--- src/qt/bitcoinamountfield.cpp
+++ src/qt/bitcoinamountfield.cpp
@@ -137,9 +137,7 @@ public:
             opt.rect = rect();
 
             cachedMinimumSizeHint =
-                style()
-                    ->sizeFromContents(QStyle::CT_SpinBox, &opt, hint, this)
-                    .expandedTo(QApplication::globalStrut());
+                style()->sizeFromContents(QStyle::CT_SpinBox, &opt, hint, this);
         }
         return cachedMinimumSizeHint;
     }
