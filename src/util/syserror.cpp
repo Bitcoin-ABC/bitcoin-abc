50efdc2baaa0fe4c42c36565b306bb48f9d7db26
laanwj <126646+laanwj@users.noreply.github.com>
2022-04-20 16:43:07 +0200

    util: Refactor SysErrorString logic
    Summary:
    > util: Use strerror_s for SysErrorString on Windows
    
    https://github.com/bitcoin/bitcoin/pull/24933/commits/e7f2f77756d33c6be9c8998a575b263ff2d39270
    
    > util: Refactor SysErrorString logic
    >
    > Deduplicate code and error checks by making sure `s` stays `nullptr`
    > in case of error. Return "Unknown error" instead of an empty string in
    > this case.
    
    https://github.com/bitcoin/bitcoin/pull/24933/commits/718da302c7b11b375042c3000d421fd93348c199
    
    > util: Increase buffer size to 1024 in SysErrorString
    >
    > Increase the error message buffer to 1024 as recommended in the manual
    > page (Thanks Jon Atack)
    
    https://github.com/bitcoin/bitcoin/pull/24933/commits/f00fb1265a8bc26e1612c771173325dbe49b3612
    
    This concludes backport of [[https://github.com/bitcoin/bitcoin/pull/24933 | core#24933]]
    Depends on D16209
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D16210

--- src/util/syserror.cpp
+++ src/util/syserror.cpp
@@ -12,22 +12,30 @@
 #include <cstring>
 
 std::string SysErrorString(int err) {
-    char buf[256];
-    buf[0] = 0;
+    char buf[1024];
     /**
-     * Too bad there are two incompatible implementations of the
+     * Too bad there are three incompatible implementations of the
      * thread-safe strerror.
      */
-    const char *s;
+    const char *s = nullptr;
+#ifdef WIN32
+    if (strerror_s(buf, sizeof(buf), err) == 0) {
+        s = buf;
+    }
+#else
 #ifdef STRERROR_R_CHAR_P
     /* GNU variant can return a pointer outside the passed buffer */
     s = strerror_r(err, buf, sizeof(buf));
 #else
-    s = buf;
     /* POSIX variant always returns message in buffer */
-    if (strerror_r(err, buf, sizeof(buf))) {
-        buf[0] = 0;
+    if (strerror_r(err, buf, sizeof(buf)) == 0) {
+        s = buf;
     }
 #endif
-    return strprintf("%s (%d)", s, err);
+#endif
+    if (s != nullptr) {
+        return strprintf("%s (%d)", s, err);
+    } else {
+        return strprintf("Unknown error (%d)", err);
+    }
 }
