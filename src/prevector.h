c3445eb7f5e8fa9a4862fbb9072f4e6fd7fb22ed
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-04-24 12:55:57 +0200

    prevector: only allow trivially copyable types
    Summary:
    The prevector implementation currently can't be used with types that are
    not trivially copyable, due to the use of memmove. Trivially copyable
    implies that it is trivially destructible, see
    https://eel.is/c++draft/class.prop#1.3
    
    That means that the checks for std::is_trivially_destructible are not
    necessary, and in fact where used it wouldn't be enough. E.g. in
    `erase(iterator, iterator)` the elements in range first-last are destructed,
    but it does not destruct elements left after `memmove`.
    
    This commit removes the checks for `std::is_trivially_destructible`
    and instead adds a `static_assert(std::is_trivially_copyable_v<T>);` to
    make sure `prevector` is only used with supported types.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/24962 | core#24962]]
    
    Test Plan: `ninja check`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19241

--- src/prevector.h
+++ src/prevector.h
@@ -36,6 +36,8 @@
 template <unsigned int N, typename T, typename Size = uint32_t,
           typename Diff = int32_t>
 class prevector {
+    static_assert(std::is_trivially_copyable_v<T>);
+
 public:
     static constexpr unsigned int STATIC_SIZE{N};
 
@@ -516,15 +518,7 @@ public:
         // representation (with capacity N and size <= N).
         iterator p = first;
         char *endp = (char *)&(*end());
-        if (!std::is_trivially_destructible<T>::value) {
-            while (p != last) {
-                (*p).~T();
-                _size--;
-                ++p;
-            }
-        } else {
-            _size -= last - p;
-        }
+        _size -= last - p;
         memmove(&(*first), &(*last), endp - ((char *)(&(*last))));
         return first;
     }
@@ -556,9 +550,6 @@ public:
     }
 
     ~prevector() {
-        if (!std::is_trivially_destructible<T>::value) {
-            clear();
-        }
         if (!is_direct()) {
             free(_union.indirect_contents.indirect);
             _union.indirect_contents.indirect = nullptr;
