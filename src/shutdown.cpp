Bitcoin-ABC/bitcoin-abc

commit 1ec7ba83469f293f0e418be61865d4683396f47e

Pass bilingual_str argument to AbortNode()
Summary:
```
This PR is a followup of #16224, and it adds bilingual_str type argument
support to the AbortNode() functions.
```

Backport of core [[https://github.com/bitcoin/bitcoin/pull/18927 | PR18927]].

Depends on D8648.

Test Plan:
  ninja all check-all

Reviewers: #bitcoin_abc, jasonbcox

Reviewed By: #bitcoin_abc, jasonbcox

Differential Revision: https://reviews.bitcoinabc.org/D8649

src/validation.cpp
@@ -1288,32 +1288,26 @@ bool UndoReadFromDisk(CBlockUndo &blockundo, const CBlockIndex *pindex) {
     return true;
 }
 
+constexpr auto AbortError = InitError;
+
 /** Abort with a message */
 static bool AbortNode(const std::string &strMessage,
-                      const std::string &userMessage = "",
-                      unsigned int prefix = 0) {
+                      bilingual_str user_message = bilingual_str()) {
     SetMiscWarning(strMessage);
     LogPrintf("*** %s\n", strMessage);
-    if (!userMessage.empty()) {
-        uiInterface.ThreadSafeMessageBox(Untranslated(userMessage), "",
-                                         CClientUIInterface::MSG_ERROR |
-                                             prefix);
-    } else {
-        uiInterface.ThreadSafeMessageBox(
-            _("Error: A fatal internal error occurred, see debug.log for "
-              "details"),
-            "",
-            CClientUIInterface::MSG_ERROR | CClientUIInterface::MSG_NOPREFIX);
+    if (!user_message.empty()) {
+        user_message =
+            _("A fatal internal error occurred, see debug.log for details");
     }
+    AbortError(user_message);
     StartShutdown();
     return false;
 }
 
 static bool AbortNode(BlockValidationState &state,
                       const std::string &strMessage,
-                      const std::string &userMessage = "",
-                      unsigned int prefix = 0) {
-    AbortNode(strMessage, userMessage, prefix);
+                      const bilingual_str &userMessage = bilingual_str()) {
+    AbortNode(strMessage, userMessage);
     return state.Error(strMessage);
 }
 
@@ -2118,10 +2112,8 @@ bool CChainState::FlushStateToDisk(const CChainParams &chainparams,
             if (fDoFullFlush || fPeriodicWrite) {
                 // Depend on nMinDiskSpace to ensure we can write block index
                 if (!CheckDiskSpace(GetBlocksDir())) {
-                    return AbortNode(
-                        state, "Disk space is too low!",
-                        _("Error: Disk space is too low!").translated,
-                        CClientUIInterface::MSG_NOPREFIX);
+                    return AbortNode(state, "Disk space is too low!",
+                                     _("Disk space is too low!"));
                 }
 
                 // First make sure all block and undo data is flushed to disk.
@@ -2169,10 +2161,8 @@ bool CChainState::FlushStateToDisk(const CChainParams &chainparams,
                 // factor of 2.
                 if (!CheckDiskSpace(GetDataDir(),
                                     48 * 2 * 2 * CoinsTip().GetCacheSize())) {
-                    return AbortNode(
-                        state, "Disk space is too low!",
-                        _("Error: Disk space is too low!").translated,
-                        CClientUIInterface::MSG_NOPREFIX);
+                    return AbortNode(state, "Disk space is too low!",
+                                     _("Disk space is too low!"));
                 }
 
                 // Flush the chainstate (which may refer to block index
@@ -3651,8 +3641,7 @@ static bool FindBlockPos(FlatFilePos &pos, unsigned int nAddSize,
             BlockFileSeq().Allocate(pos, nAddSize, out_of_space);
         if (out_of_space) {
             return AbortNode("Disk space is too low!",
-                             _("Error: Disk space is too low!").translated,
-                             CClientUIInterface::MSG_NOPREFIX);
+                             _("Disk space is too low!"));
         }
         if (bytes_allocated != 0 && fPruneMode) {
             fCheckForPruning = true;
@@ -3678,8 +3667,7 @@ static bool FindUndoPos(BlockValidationState &state, int nFile,
         UndoFileSeq().Allocate(pos, nAddSize, out_of_space);
     if (out_of_space) {
         return AbortNode(state, "Disk space is too low!",
-                         _("Error: Disk space is too low!").translated,
-                         CClientUIInterface::MSG_NOPREFIX);
+                         _("Disk space is too low!"));
     }
     if (bytes_allocated != 0 && fPruneMode) {
         fCheckForPruning = true;

src/noui.cpp
@@ -21,11 +21,8 @@ bool noui_ThreadSafeMessageBox(const bilingual_str &message,
                                const std::string &caption, unsigned int style) {
     bool fSecure = style & CClientUIInterface::SECURE;
     style &= ~CClientUIInterface::SECURE;
-    bool prefix = !(style & CClientUIInterface::MSG_NOPREFIX);
-    style &= ~CClientUIInterface::MSG_NOPREFIX;
 
     std::string strCaption;
-    if (prefix) {
         switch (style) {
             case CClientUIInterface::MSG_ERROR:
                 strCaption = "Error: ";
@@ -40,7 +37,6 @@ bool noui_ThreadSafeMessageBox(const bilingual_str &message,
                 // Use supplied caption (can be empty)
                 strCaption = caption + ": ";
         }
-    }
 
     if (!fSecure) {
         LogPrintf("%s%s\n", strCaption, message.original);

src/qt/bitcoingui.cpp
@@ -1207,9 +1207,6 @@ void BitcoinGUI::message(const QString &title, QString message,
     int nMBoxIcon = QMessageBox::Information;
     int nNotifyIcon = Notificator::Information;
 
-    bool prefix = !(style & CClientUIInterface::MSG_NOPREFIX);
-    style &= ~CClientUIInterface::MSG_NOPREFIX;
-
     QString msgType;
     if (!title.isEmpty()) {
         msgType = title;
@@ -1217,15 +1214,11 @@ void BitcoinGUI::message(const QString &title, QString message,
         switch (style) {
             case CClientUIInterface::MSG_ERROR:
                 msgType = tr("Error");
-                if (prefix) {
-                    message = tr("Error: %1").arg(message);
-                }
+                message = tr("Error: %1").arg(message);
                 break;
             case CClientUIInterface::MSG_WARNING:
                 msgType = tr("Warning");
-                if (prefix) {
-                    message = tr("Warning: %1").arg(message);
-                }
+                message = tr("Warning: %1").arg(message);
                 break;
             case CClientUIInterface::MSG_INFORMATION:
                 msgType = tr("Information");

src/qt/walletcontroller.cpp
@@ -243,7 +243,7 @@ void CreateWalletActivity::createWallet() {
 void CreateWalletActivity::finish() {
     m_progress_dialog->hide();
 
-    if (!m_error_message.original.empty()) {
+    if (!m_error_message.empty()) {
         QMessageBox::critical(
             m_parent_widget, tr("Create wallet failed"),
             QString::fromStdString(m_error_message.translated));
@@ -287,7 +287,7 @@ OpenWalletActivity::OpenWalletActivity(WalletController *wallet_controller,
 void OpenWalletActivity::finish() {
     m_progress_dialog->hide();
 
-    if (!m_error_message.original.empty()) {
+    if (!m_error_message.empty()) {
         QMessageBox::critical(
             m_parent_widget, tr("Open wallet failed"),
             QString::fromStdString(m_error_message.translated));

src/ui_interface.h
@@ -67,9 +67,6 @@ public:
          */
         MODAL = 0x10000000U,
 
-        /** Do not prepend error/warning prefix */
-        MSG_NOPREFIX = 0x20000000U,
-
         /** Do not print contents of message to debug log */
         SECURE = 0x40000000U,
 

commit d4ff766e57a43c98925e33ad8e6132c6c1ad5d43

Move AbortNode to shutdown and fix an error message
Summary:
Can be reviewed with the git option `--color-moved=dimmed-zebra`

Also fix a `if (user_message.empty())` clause which was previously negated by mistake (introduced in D8649). This caused the specific user message to be overwritten by a more generic and less useful message.

This is a backport of [[https://github.com/bitcoin/bitcoin/pull/21575 | core#21575]] [3/6]
https://github.com/bitcoin/bitcoin/pull/21575/commits/fa91b2b2b3447a3645e7958c7dc4e1946a69cb9c

Depends on D11348

Test Plan: `ninja all check-all`

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D11349

src/shutdown.cpp
@@ -5,7 +5,23 @@
 
 #include <shutdown.h>
 
+#include <logging.h>
+#include <node/ui_interface.h>
+#include <warnings.h>
+
 #include <atomic>
+
+bool AbortNode(const std::string &strMessage, bilingual_str user_message) {
+    SetMiscWarning(Untranslated(strMessage));
+    LogPrintf("*** %s\n", strMessage);
+    if (user_message.empty()) {
+        user_message =
+            _("A fatal internal error occurred, see debug.log for details");
+    }
+    AbortError(user_message);
+    StartShutdown();
+    return false;
+}
 
 static std::atomic<bool> fRequestShutdown(false);
 

src/shutdown.h
@@ -6,6 +6,12 @@
 #ifndef BITCOIN_SHUTDOWN_H
 #define BITCOIN_SHUTDOWN_H
 
+#include <util/translation.h> // For bilingual_str
+
+/** Abort with a message */
+bool AbortNode(const std::string &strMessage,
+               bilingual_str user_message = bilingual_str{});
+
 void StartShutdown();
 void AbortShutdown();
 bool ShutdownRequested();

src/validation.cpp
@@ -1337,20 +1337,6 @@ bool UndoReadFromDisk(CBlockUndo &blockundo, const CBlockIndex *pindex) {
     return true;
 }
 
-/** Abort with a message */
-static bool AbortNode(const std::string &strMessage,
-                      bilingual_str user_message = bilingual_str()) {
-    SetMiscWarning(Untranslated(strMessage));
-    LogPrintf("*** %s\n", strMessage);
-    if (!user_message.empty()) {
-        user_message =
-            _("A fatal internal error occurred, see debug.log for details");
-    }
-    AbortError(user_message);
-    StartShutdown();
-    return false;
-}
-
 static bool AbortNode(BlockValidationState &state,
                       const std::string &strMessage,
                       const bilingual_str &userMessage = bilingual_str()) {