ea998195ee6ed9d877485be005afbe6e01d9ddd8
fanquake <fanquake@gmail.com>
2022-06-15 09:16:57 +0100

    depends: sqlite 3.50.4
    Summary:
    These are some of the relevant commit messages for this stack of backports. Some messages are omitted because they are reverted later, or because they are intermediate version bumps.
    
    > And use more recommended sqlite3 compile options
    >
    > See https://www.sqlite.org/compile.html.
    
    > pass --enable-debug to sqlite when DEBUG=1
    
    > depends: add -g to DEBUG=1 flags
    
    > depends: sqlite 3.50.4
    >
    > Migrate to the new Autosetup build system (introduced in 3.48.0).
    > See https://msteveb.github.io/autosetup/.
    > Also: https://sqlite.org/releaselog/3_50_0.html.
    
    > depends: Propagate native C compiler to `sqlite` package
    >
    > This ensures that autosetup can build the local bootstrap `jimsh0` when
    > neither `jimsh` nor `tclsh` is available on the system.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/25378 | core#25378]], [[https://github.com/bitcoin/bitcoin/pull/25987 | core#25987]], [[https://github.com/bitcoin/bitcoin/pull/26998 | core#26998]], [[https://github.com/bitcoin/bitcoin/pull/29287 | core#29287]], [[https://github.com/bitcoin/bitcoin/pull/29527 | core#29527]], [[https://github.com/bitcoin/bitcoin/pull/29991 | core#29991]], [[https://github.com/bitcoin/bitcoin/pull/32655 | core#32655]], [[https://github.com/bitcoin/bitcoin/pull/32656 | core#32656]] and [[https://github.com/bitcoin/bitcoin/pull/33995 | core#33995]]
    (bypasses a change from [[https://github.com/bitcoin/bitcoin/pull/31276 | core#31276]])
    
    Depends on D19650
    
    Test Plan: `contrib/guix/guix-build`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19650

--- depends/hosts/linux.mk
+++ depends/hosts/linux.mk
@@ -4,7 +4,7 @@ linux_CXXFLAGS=-pipe -std=$(CXX_STANDARD)
 linux_release_CFLAGS=-O2
 linux_release_CXXFLAGS=$(linux_release_CFLAGS)
 
-linux_debug_CFLAGS=-O1
+linux_debug_CFLAGS=-O1 -g
 linux_debug_CXXFLAGS=$(linux_debug_CFLAGS)
 
 linux_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
