5615fb72f8975f89162d6cb333d6f3877574e0f0
Tobias Ruck <ruck.tobias@gmail.com>
2022-06-27 14:27:08 +0200

    [Chronik] Add minimal bitcoinsuite-core Rust crate + CMake to `cargo test` it
    Summary:
    - Add empty bitcoinsuite-core crate. We keep it empty as we don't have any lints yet, which will be added next.
    - Add CMake files to enable Chronik via `BUILD_BITCOIN_CHRONIK` CMake flag
    - Find Rust via Corrosion
    - Add check-bitcoinsuite target to run `cargo test` on all the rust crates starting with bitcoinsuite-.
    
    Test Plan:
    # Install Rust 1.61 and Corrosion 0.2, see D11663
    # `cmake .. -GNinja -DBUILD_BITCOIN_CHRONIK=on`
    # `ninja check-bitcoinsuite`
    # Optionally (will be automated next): in `src/rust`: None of `cargo check`, `cargo doc` and `cargo clippy` raise warnings
    
    Reviewers: Fabien, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Subscribers: Mengerian
    
    Differential Revision: https://reviews.bitcoinabc.org/D11668

---
+++ chronik/bitcoinsuite-core/src/lib.rs
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#![deny(unsafe_code)]
+
+//! Core primitives for dealing with Bitcoin-like chains.
+//!
+//! Note: This is a general purpose library, but has been optimized for the
+//! usage in Chronik, an indexer for Bitcoin ABC.
