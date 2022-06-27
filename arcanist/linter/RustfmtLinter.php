c451a96ffc0b51bbcbf8c7c71a23133d0a740d59
Tobias Ruck <ruck.tobias@gmail.com>
2022-06-27 15:58:28 +0200

    [Chronik] Add `rustfmt` linter, to style check Rust files
    Summary:
    Check formatting via `rustfmt`. Configuration is kept rusty, instead of mimicking clang-format.
    
    During development, `cargo +nightly fmt` should be used to wrap comments, strings, group imports, etc. As these features are unstable and might have false-positives, we don't enforce on them in the CI yet.
    
    Stable rustfmt currently doesn't enforce line length limits strictly, therefore we have to verify length outselves. We skip length limits that are fixed by rustfmt, however, the current approach leaves some false positives:
    
    - If two lines within one rustfmt diff exceed the limit,
    - one of them is fixed by rustfmt,
    - then for *both* will be issued a warning.
    
    Since this both doesn't occur too often, and also should be fixed by the "recommended" (aka `cargo +nightly fmt`) approach, the trade-off of having a simple implementation seems worth it.
    
    Also, if `cargo fmt` wraps more and more code successfully, these false positives should disappear.
    
    Test Plan:
    1. `arc lint` displays four errors, one being a false positive in line 12.
    2. `arc lint` will have only fit the spaces after `struct`.
    3. `cargo +nightly fmt` fixes all errors except the line having way too many x's.
    
    Reviewers: Fabien, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D11671

---
+++ arcanist/linter/RustfmtLinter.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * Run rustfmt, a formatter shipped with Rust.
+ */
+final class RustfmtLinter extends ArcanistExternalLinter {
+
+  public function getInfoName() {
+    return 'rustfmt';
+  }
+
+  public function getInfoURI() {
+    return 'https://github.com/rust-lang/rustfmt';
+  }
+
+  public function getInfoDescription() {
+    return pht('Formats Rust *.rs files.');
+  }
+
+  public function getInstallInstructions() {
+    return pht('You have to install a nightly Rust toolchain for this ' .
+               'linter, and name it "abc-nightly". You can do so via ' .
+               '`rustup install nightly-2022-06-29` and then ' .
+               '`ABC_NIGHTLY="$(rustc +nightly-2022-06-29 --print sysroot)"` ' .
+               'and then `rustup toolchain link abc-nightly "${ABC_NIGHTLY}"`');
+  }
+
+  public function getLinterName() {
+    return 'rustfmt';
+  }
+
+  public function getLinterConfigurationName() {
+    return 'rustfmt';
+  }
+
+  public function getDefaultBinary() {
+    return 'rustfmt';
+  }
+
+  protected function getMandatoryFlags() {
+    return array(
+      '+abc-nightly',
+      '--emit', 'stdout',
+      '--unstable-features',
+      '--skip-children',
+      '--quiet',
+    );
+  }
+
+  public function getVersion() {
+    list($err, $stdout, $stderr) = exec_manual('%C +abc-nightly --version',
+                                               $this->getExecutableCommand());
+
+    if ($err) {
+      throw new Exception(pht('Linter %s: %s', RustfmtLinter::class, $stderr));
+    }
+
+    $matches = array();
+    // example output: rustfmt 1.5.1-nightly (83088064 2022-06-28)
+    $regex = '/^rustfmt (?P<version>\d+\.\d+\.\d+)-.*$/';
+    if (preg_match($regex, $stdout, $matches)) {
+      return $matches['version'];
+    }
+
+    throw new Exception(pht('Linter %s unexpected output: %s',
+                            RustfmtLinter::class, $stderr));
+  }
+
+  protected function parseLinterOutput($path, $err, $stdout, $stderr) {
+    $root = $this->getProjectRoot();
+    $path = Filesystem::resolvePath($path, $root);
+
+    if ($err != 0) {
+      $message = id(new ArcanistLintMessage())
+        ->setPath($path)
+        ->setGranularity(ArcanistLinter::GRANULARITY_FILE)
+        ->setCode('RUSTFMT')
+        ->setSeverity(ArcanistLintSeverity::SEVERITY_ERROR)
+        ->setName('Linter error')
+        ->setDescription($stderr);
+
+      return array($message);
+    }
+
+    $messages = array();
+    $lines = preg_split("/\r\n|\n|\r/", $stdout);
+    // Sometimes it's impossible to wrap code, add an error in this case
+    foreach ($lines as $lineIdx => $line) {
+      if (strlen($line) > 80) {
+        $messages[] = id(new ArcanistLintMessage())
+        ->setPath($path)
+        ->setLine($lineIdx + 1)
+        ->setChar(1)
+        ->setGranularity(ArcanistLinter::GRANULARITY_FILE)
+        ->setCode('RUSTFMT')
+        ->setSeverity(ArcanistLintSeverity::SEVERITY_ERROR)
+        ->setName('Line limit exceeded')
+        ->setDescription('Could not wrap this line with rustfmt');
+      }
+    }
+
+    $orig = file_get_contents($path);
+
+    if ($orig == $stdout) {
+      return $messages;
+    }
+
+    $messages[] = id(new ArcanistLintMessage())
+      ->setPath($path)
+      ->setLine(1)
+      ->setChar(1)
+      ->setGranularity(ArcanistLinter::GRANULARITY_FILE)
+      ->setCode('RUSTFMT')
+      ->setSeverity(ArcanistLintSeverity::SEVERITY_AUTOFIX)
+      ->setName('Code style violation')
+      ->setDescription("'$path' has code style errors.")
+      ->setOriginalText($orig)
+      ->setReplacementText($stdout);
+
+    return $messages;
+  }
+}
