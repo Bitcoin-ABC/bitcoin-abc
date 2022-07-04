023ec18fb65e146a7c685db692112b9625e13033
Tobias Ruck <ruck.tobias@gmail.com>
2022-07-04 21:44:02 +0200

    [Chronik] Add clippy linter as build target
    Summary:
    `cargo clippy` catches common mistakes and improves the Rust code.
    
    1. Refactor: Add `add_cargo_custom_target` function to invoke a cargo command as custom target.
    2. Rename `check-bitcoinsuite` to `check-bitcoinsuite-test` to test `bitcoinsuite` crates.
    3. Add `check-bitcoinsuite-clippy` to lint `bitcoinsuite` crates.
    4. Add `check-bitcoinsuite` custom target that has the above two as dependencies.
    
    Test Plan:
    1. `ninja check-bitcoinsuite` runs cargo clippy and cargo test successfully.
    2. Adding `pub struct X;` at the end of `chronik/bitcoinsuite-core/src/lib.rs`, then running `ninja check-bitcoinsuite` makes the build fail.
    
    Reviewers: Fabien, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D11706

--- chronik/bitcoinsuite-core/src/lib.rs
+++ chronik/bitcoinsuite-core/src/lib.rs
@@ -3,6 +3,31 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #![deny(unsafe_code)]
+#![warn(
+    const_err,
+    dead_code,
+    elided_lifetimes_in_paths,
+    explicit_outlives_requirements,
+    improper_ctypes,
+    keyword_idents,
+    missing_debug_implementations,
+    missing_docs,
+    no_mangle_generic_items,
+    non_ascii_idents,
+    non_shorthand_field_patterns,
+    noop_method_call,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_in_public,
+    rust_2018_idioms,
+    single_use_lifetimes,
+    unconditional_recursion,
+    unreachable_pub,
+    unused_comparisons,
+    unused,
+    while_true
+)]
 
 //! Core primitives for dealing with Bitcoin-like chains.
 //!
