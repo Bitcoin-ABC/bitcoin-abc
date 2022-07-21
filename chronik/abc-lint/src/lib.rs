db165ec2c333e8aee6b32bba9f078004f7ee97b6
Tobias Ruck <ruck.tobias@gmail.com>
2022-07-21 23:26:30 +0200

    [Chronik] Add lint macro to prevent having to repeat common lints in every crate
    Summary:
    Manually checking that all lints are the same for all the crates in the repo is an unnecessary burden on developers and reviewers.
    
    With this macro, common lints are applied to all the items contained in it. This way lints can be defined in one place.
    
    Naturally, it doesn't cover anything outside the macro (e.g. the docs for the crate itself are not enforced anymore), however this is
    something that is reasonable to be checked manually by a reviewer.
    
    Test Plan:
    1. `ninja check-bitcoinsuite`
    2. Add `pub struct S;` into hash.rs
    3. Make sure `ninja check-bitcoinsuite` gives a bunch of lints (missing docs, missing Debug)
    4. Revert
    
    Reviewers: Fabien, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D11787

---
+++ chronik/abc-lint/src/lib.rs
@@ -0,0 +1,49 @@
+// Copyright (c) 2022 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+//! Lints for Bitcoin ABC Rust code.
+
+/// Adds common lints to the items in this macro.
+///
+/// Intended to be used in the lib.rs of a crate, where all the modules and
+/// other items are defined.
+///
+/// Usage:
+/// ```
+/// abc_lint::lint! {
+///     /// docs for `a`.
+///     pub mod a {}
+/// }
+/// ```
+#[macro_export]
+macro_rules! lint {
+    ($($lint_item:item)*) => {
+        $(
+            #[deny(unsafe_code)]
+            #[warn(
+                const_err,
+                explicit_outlives_requirements,
+                improper_ctypes,
+                keyword_idents,
+                missing_debug_implementations,
+                missing_docs,
+                no_mangle_generic_items,
+                non_ascii_idents,
+                non_shorthand_field_patterns,
+                noop_method_call,
+                overflowing_literals,
+                path_statements,
+                patterns_in_fns_without_body,
+                private_in_public,
+                rust_2018_idioms,
+                unconditional_recursion,
+                unreachable_pub,
+                unused_comparisons,
+                unused,
+                while_true
+            )]
+            $lint_item
+        )*
+    };
+}
--- chronik/bitcoinsuite-core/src/lib.rs
+++ chronik/bitcoinsuite-core/src/lib.rs
@@ -2,34 +2,12 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#![deny(unsafe_code)]
-#![warn(
-    const_err,
-    explicit_outlives_requirements,
-    improper_ctypes,
-    keyword_idents,
-    missing_debug_implementations,
-    missing_docs,
-    no_mangle_generic_items,
-    non_ascii_idents,
-    non_shorthand_field_patterns,
-    noop_method_call,
-    overflowing_literals,
-    path_statements,
-    patterns_in_fns_without_body,
-    private_in_public,
-    rust_2018_idioms,
-    unconditional_recursion,
-    unreachable_pub,
-    unused_comparisons,
-    unused,
-    while_true
-)]
-
 //! Core primitives for dealing with Bitcoin-like chains.
 //!
 //! Note: This is a general purpose library, but has been optimized for the
 //! usage in Chronik, an indexer for Bitcoin ABC.
 
-pub mod block;
-pub mod hash;
+abc_lint::lint! {
+    pub mod block;
+    pub mod hash;
+}
