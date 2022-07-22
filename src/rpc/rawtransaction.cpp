bitcoin-ghost/bitcoin-core-ladder-script

[refactor] clean up logic in testmempoolaccept
Cleans up reundant code and reduces the diff of the next commit.

src/rpc/rawtransaction.cpp
@@ -955,35 +955,30 @@ static RPCHelpMan testmempoolaccept()
             nullptr /* plTxnReplaced */, false /* bypass_limits */, /* test_accept */ true, &fee);
     }
 
-    // Check that fee does not exceed maximum fee
-    if (test_accept_res && max_raw_tx_fee && fee > max_raw_tx_fee) {
-        result_0.pushKV("allowed", false);
-        result_0.pushKV("reject-reason", "max-fee-exceeded");
-        result.push_back(std::move(result_0));
-        return result;
-    }
-    result_0.pushKV("allowed", test_accept_res);
-
     // Only return the fee and vsize if the transaction would pass ATMP.
     // These can be used to calculate the feerate.
     if (test_accept_res) {
-        result_0.pushKV("vsize", virtual_size);
-        UniValue fees(UniValue::VOBJ);
-        fees.pushKV("base", ValueFromAmount(fee));
-        result_0.pushKV("fees", fees);
+        // Check that fee does not exceed maximum fee
+        if (max_raw_tx_fee && fee > max_raw_tx_fee) {
+            result_0.pushKV("allowed", false);
+            result_0.pushKV("reject-reason", "max-fee-exceeded");
+        } else {
+            result_0.pushKV("allowed", true);
+            result_0.pushKV("vsize", virtual_size);
+            UniValue fees(UniValue::VOBJ);
+            fees.pushKV("base", ValueFromAmount(fee));
+            result_0.pushKV("fees", fees);
+        }
+        result.push_back(std::move(result_0));
     } else {
-        if (state.IsInvalid()) {
-            if (state.GetResult() == TxValidationResult::TX_MISSING_INPUTS) {
-                result_0.pushKV("reject-reason", "missing-inputs");
-            } else {
-                result_0.pushKV("reject-reason", strprintf("%s", state.GetRejectReason()));
-            }
+        result_0.pushKV("allowed", false);
+        if (state.GetResult() == TxValidationResult::TX_MISSING_INPUTS) {
+            result_0.pushKV("reject-reason", "missing-inputs");
         } else {
             result_0.pushKV("reject-reason", state.GetRejectReason());
         }
+        result.push_back(std::move(result_0));
     }
-
-    result.push_back(std::move(result_0));
     return result;
 },
     };

Bitcoin-ABC/bitcoin-abc

Summary:
This is a backport of [[https://github.com/bitcoin/bitcoin/pull/21062 | core#21062]] [1/4]
https://github.com/bitcoin/bitcoin/pull/21062/commits/9db10a55061e09021ff8ea1d6637d99f7959035f
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, sdulfari
Reviewed By: #bitcoin_abc, sdulfari
Differential Revision: https://reviews.bitcoinabc.org/D11778

src/rpc/rawtransaction.cpp
@@ -1167,39 +1167,31 @@ static RPCHelpMan testmempoolaccept() {
                              state, std::move(tx), false /* bypass_limits */,
                              true /* test_accept */, &fee));
 
-            // Check that fee does not exceed maximum fee
-            if (test_accept_res && max_raw_tx_fee != Amount::zero() &&
-                fee > max_raw_tx_fee) {
-                result_0.pushKV("allowed", false);
-                result_0.pushKV("reject-reason", "max-fee-exceeded");
-                result.push_back(std::move(result_0));
-                return result;
-            }
-            result_0.pushKV("allowed", test_accept_res);
-
             // Only return the fee and size if the transaction would pass ATMP.
             // These can be used to calculate the feerate.
             if (test_accept_res) {
-                result_0.pushKV("size", virtual_size);
-                UniValue fees(UniValue::VOBJ);
-                fees.pushKV("base", fee);
-                result_0.pushKV("fees", fees);
+                // Check that fee does not exceed maximum fee
+                if (max_raw_tx_fee != Amount::zero() && fee > max_raw_tx_fee) {
+                    result_0.pushKV("allowed", false);
+                    result_0.pushKV("reject-reason", "max-fee-exceeded");
+                } else {
+                    result_0.pushKV("allowed", true);
+                    result_0.pushKV("size", virtual_size);
+                    UniValue fees(UniValue::VOBJ);
+                    fees.pushKV("base", fee);
+                    result_0.pushKV("fees", fees);
+                }
+                result.push_back(std::move(result_0));
             } else {
-                if (state.IsInvalid()) {
-                    if (state.GetResult() ==
-                        TxValidationResult::TX_MISSING_INPUTS) {
-                        result_0.pushKV("reject-reason", "missing-inputs");
-                    } else {
-                        result_0.pushKV(
-                            "reject-reason",
-                            strprintf("%s", state.GetRejectReason()));
-                    }
+                result_0.pushKV("allowed", false);
+                if (state.GetResult() ==
+                    TxValidationResult::TX_MISSING_INPUTS) {
+                    result_0.pushKV("reject-reason", "missing-inputs");
                 } else {
                     result_0.pushKV("reject-reason", state.GetRejectReason());
                 }
+                result.push_back(std::move(result_0));
             }
-
-            result.push_back(std::move(result_0));
             return result;
         },
     };