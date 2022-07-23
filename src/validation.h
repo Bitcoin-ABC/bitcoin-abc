bitcoin/bitcoin

doc/style followups in MempoolAcceptResult

src/validation.h
@@ -196,14 +196,14 @@ struct MempoolAcceptResult {
         VALID, //!> Fully validated, valid.
         INVALID, //!> Invalid.
     };
-    ResultType m_result_type;
-    TxValidationState m_state;
+    const ResultType m_result_type;
+    const TxValidationState m_state;
 
     // The following fields are only present when m_result_type = ResultType::VALID
     /** Mempool transactions replaced by the tx per BIP 125 rules. */
-    std::optional<std::list<CTransactionRef>> m_replaced_transactions;
-    /** Raw base fees. */
-    std::optional<CAmount> m_base_fees;
+    const std::optional<std::list<CTransactionRef>> m_replaced_transactions;
+    /** Raw base fees in satoshis. */
+    const std::optional<CAmount> m_base_fees;
 
     /** Constructor for failure case */
     explicit MempoolAcceptResult(TxValidationState state)
@@ -214,7 +214,7 @@ struct MempoolAcceptResult {
 
     /** Constructor for success case */
     explicit MempoolAcceptResult(std::list<CTransactionRef>&& replaced_txns, CAmount fees)
-        : m_result_type(ResultType::VALID), m_state(TxValidationState{}),
+        : m_result_type(ResultType::VALID), m_state{},
         m_replaced_transactions(std::move(replaced_txns)), m_base_fees(fees) {}
 };
 

Bitcoin-ABC/bitcoin-abc

Summary: This is a backport of [[https://github.com/bitcoin/bitcoin/pull/21783 | core#21783]]
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D11794

src/validation.h
@@ -289,13 +289,13 @@ struct MempoolAcceptResult {
         //! Invalid.
         INVALID,
     };
-    ResultType m_result_type;
-    TxValidationState m_state;
+    const ResultType m_result_type;
+    const TxValidationState m_state;
 
     // The following fields are only present when m_result_type =
     // ResultType::VALID
-    /** Raw base fees. */
-    std::optional<Amount> m_base_fees;
+    /** Raw base fees in satoshis. */
+    const std::optional<Amount> m_base_fees;
 
     /** Constructor for failure case */
     explicit MempoolAcceptResult(TxValidationState state)
@@ -307,8 +307,7 @@ struct MempoolAcceptResult {
 
     /** Constructor for success case */
     explicit MempoolAcceptResult(Amount fees)
-        : m_result_type(ResultType::VALID), m_state(TxValidationState{}),
-          m_base_fees(fees) {}
+        : m_result_type(ResultType::VALID), m_base_fees(fees) {}
 };
 
 /**