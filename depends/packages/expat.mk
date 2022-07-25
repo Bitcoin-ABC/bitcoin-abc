e69506305d1889ea73c5cdaf16c877d64ecf90fc
fanquake <fanquake@gmail.com>
2022-07-25 12:18:11 +0100

    depends: expat 2.7.3,  switch to building expat with CMake
    Summary:
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/25697 | core#25697]], [[https://github.com/bitcoin/bitcoin/pull/29878 | core#29878]] and a partial backport of [[https://github.com/bitcoin/bitcoin/pull/33851 | core#33851]]
    https://github.com/bitcoin/bitcoin/pull/33851/changes/f541b92cf2bb7f8f708f147bd320becf48587d14
    
    Test Plan: `contrib/guix/guix-build`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19678

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,27 +1,28 @@
 package=expat
-$(package)_version=2.4.1
+$(package)_version=2.7.3
 $(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$($(package)_version))/
-$(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=cf032d0dba9b928636548e32b327a2d66b1aab63c4f4a13dd132c2d1d2f2fb6a
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash=821ac9710d2c073eaf13e1b1895a9c9aa66c1157a99635c639fbff65cdbdd732
+$(package)_build_subdir=build
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --without-docbook --without-tests --without-examples
-  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
-  $(package)_config_opts += --without-xmlwf
+  $(package)_config_opts := -DCMAKE_BUILD_TYPE=None -DEXPAT_BUILD_TOOLS=OFF
+  $(package)_config_opts += -DEXPAT_BUILD_EXAMPLES=OFF -DEXPAT_BUILD_TESTS=OFF
+  $(package)_config_opts += -DBUILD_SHARED_LIBS=OFF
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  $($(package)_cmake) -S .. -B .
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -j$(JOBS)
+  ninja -j$(JOBS)
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  cmake --install . --prefix $($(package)_staging_prefix_dir)
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf share lib/cmake lib/*.la
+  rm -rf share lib/cmake
 endef
