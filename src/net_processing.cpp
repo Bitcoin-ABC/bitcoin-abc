3a33b4bd41563e14e26db0710f67617b9dd2a8f3
Martin Zumsande <mzumsande@gmail.com>
2022-08-23 13:47:04 -0400

    p2p: Make block stalling timeout adaptive
    Summary:
    This makes the stalling detection mechanism (previously a fixed
    timeout of 2s) adaptive:
    If we disconnect a peer for stalling, double the timeout for the
    next peer - and let it slowly relax back to its default
    value each time the tip advances. (Idea by Pieter Wuille)
    
    This makes situations more unlikely in which we'd keep on
    disconnecting many of our peers for stalling, even though our
    own bandwidth is insufficient to download a block in 2 seconds.
    
    Co-authored-by: Vasil Dimov <vd@FreeBSD.org>
    
    This is a backport of [ [[https://github.com/bitcoin/bitcoin/pull/25880 | core#25880]] and [[https://github.com/bitcoin/bitcoin/pull/26982 | core#26982]]
    Note that we need to specify `version=4` when calling `create_block` in the test because we miss [[https://github.com/bitcoin/bitcoin/pull/16333 | core#16333]] (which make 4 the default version for create_block)
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D15080

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -186,10 +186,13 @@ static const unsigned int MAX_GETDATA_SZ = 1000;
  */
 static const int MAX_BLOCKS_IN_TRANSIT_PER_PEER = 16;
 /**
- * Time during which a peer must stall block download progress before being
- * disconnected.
+ * Default time during which a peer must stall block download progress before
+ * being disconnected. The actual timeout is increased temporarily if peers are
+ * disconnected for hitting the timeout
  */
-static constexpr auto BLOCK_STALLING_TIMEOUT{2s};
+static constexpr auto BLOCK_STALLING_TIMEOUT_DEFAULT{2s};
+/** Maximum timeout for stalling block download. */
+static constexpr auto BLOCK_STALLING_TIMEOUT_MAX{64s};
 /**
  * Number of headers sent in one getheaders result. We rely on the assumption
  * that if a peer sends
@@ -1045,6 +1048,10 @@ private:
 
     /** Number of preferable block download peers. */
     int m_num_preferred_download_peers GUARDED_BY(cs_main){0};
+
+    /** Stalling timeout for blocks in IBD */
+    std::atomic<std::chrono::seconds> m_block_stalling_timeout{
+        BLOCK_STALLING_TIMEOUT_DEFAULT};
 
     bool AlreadyHaveTx(const TxId &txid)
         EXCLUSIVE_LOCKS_REQUIRED(cs_main,
@@ -2466,7 +2473,8 @@ void PeerManagerImpl::StartScheduledTasks(CScheduler &scheduler) {
 /**
  * Evict orphan txn pool entries based on a newly connected
  * block, remember the recently confirmed transactions, and delete tracked
- * announcements for them. Also save the time of the last tip update.
+ * announcements for them. Also save the time of the last tip update and
+ * possibly reduce dynamic block stalling timeout.
  */
 void PeerManagerImpl::BlockConnected(
     const std::shared_ptr<const CBlock> &pblock, const CBlockIndex *pindex) {
@@ -2483,6 +2491,22 @@ void PeerManagerImpl::BlockConnected(
         LOCK(cs_main);
         for (const auto &ptx : pblock->vtx) {
             m_txrequest.ForgetInvId(ptx->GetId());
+        }
+    }
+
+    // In case the dynamic timeout was doubled once or more, reduce it slowly
+    // back to its default value
+    auto stalling_timeout = m_block_stalling_timeout.load();
+    Assume(stalling_timeout >= BLOCK_STALLING_TIMEOUT_DEFAULT);
+    if (stalling_timeout != BLOCK_STALLING_TIMEOUT_DEFAULT) {
+        const auto new_timeout =
+            std::max(std::chrono::duration_cast<std::chrono::seconds>(
+                         stalling_timeout * 0.85),
+                     BLOCK_STALLING_TIMEOUT_DEFAULT);
+        if (m_block_stalling_timeout.compare_exchange_strong(stalling_timeout,
+                                                             new_timeout)) {
+            LogPrint(BCLog::NET, "Decreased stalling timeout to %d seconds\n",
+                     count_seconds(new_timeout));
         }
     }
 }
@@ -7500,8 +7524,9 @@ bool PeerManagerImpl::SendMessages(const Config &config, CNode *pto) {
         CNodeState &state = *State(pto->GetId());
 
         // Detect whether we're stalling
+        auto stalling_timeout = m_block_stalling_timeout.load();
         if (state.m_stalling_since.count() &&
-            state.m_stalling_since < current_time - BLOCK_STALLING_TIMEOUT) {
+            state.m_stalling_since < current_time - stalling_timeout) {
             // Stalling only triggers when the block download window cannot
             // move. During normal steady state, the download window should be
             // much larger than the to-be-downloaded set of blocks, so
@@ -7509,6 +7534,18 @@ bool PeerManagerImpl::SendMessages(const Config &config, CNode *pto) {
             LogPrintf("Peer=%d is stalling block download, disconnecting\n",
                       pto->GetId());
             pto->fDisconnect = true;
+            // Increase timeout for the next peer so that we don't disconnect
+            // multiple peers if our own bandwidth is insufficient.
+            const auto new_timeout =
+                std::min(2 * stalling_timeout, BLOCK_STALLING_TIMEOUT_MAX);
+            if (stalling_timeout != new_timeout &&
+                m_block_stalling_timeout.compare_exchange_strong(
+                    stalling_timeout, new_timeout)) {
+                LogPrint(
+                    BCLog::NET,
+                    "Increased stalling timeout temporarily to %d seconds\n",
+                    count_seconds(new_timeout));
+            }
             return true;
         }
         // In case there is a block that has been in flight from this peer for
