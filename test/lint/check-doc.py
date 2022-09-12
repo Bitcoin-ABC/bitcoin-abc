ca75385868b9f61c99ee80a7af305d86fa798922
PiRK <pierrekn@gmail.com>
2022-09-12 09:55:41 +0200

    [linter] fix check-doc
    Summary: I somehow did a rebase mistake before landing D11947, so this change was not included. This breaks the check-doc linter, which fails to recognize `GetIntArg` as a valid use of a documented parameter.
    
    Test Plan: `arc lint`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D11951

--- test/lint/check-doc.py
+++ test/lint/check-doc.py
@@ -21,7 +21,7 @@ FOLDER_SRC = '/src/**/'
 FOLDER_TEST = '/src/**/test/'
 
 EXTENSIONS = ["*.c", "*.h", "*.cpp", "*.cc", "*.hpp"]
-REGEX_ARG = r'(?:ForceSet|SoftSet|Get|Is)(?:Bool)?Args?(?:Set)?\(\s*"(-[^"]+)"'
+REGEX_ARG = r'(?:ForceSet|SoftSet|Get|Is)(?:Bool|Int)?Args?(?:Set)?\(\s*"(-[^"]+)"'
 REGEX_DOC = r'AddArg\(\s*"(-[^"=]+?)(?:=|")'
 
 # list false positive unknows arguments
