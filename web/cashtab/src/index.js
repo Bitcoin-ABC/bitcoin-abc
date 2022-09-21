43e8a2c2b55b1576f7436d2a71d96894f303e97f
bytesofman <joey@bitcoinabc.org>
2022-09-21 08:45:33 -0700

    [Cashtab] [service worker fix p2] install create react app default service worker
    Summary:
    T2684
    
    Depends on D12020
    
    This diff replaces the service worker removed in D12020 with the create react app default service worker.
    
    Test Plan:
    Review `https://create-react-app.dev/docs/making-a-progressive-web-app/`
    
    This diff was created by running `npx create-react-app my-app --template cra-template-pwa` and then copying the relevant service worker files. Confirm that the files match, with the exception that `index.js` has been modified to register the service worker.
    
    `npm start`
    In the dev console, navigate to 'Application' and 'Service Workers." Note that there is no service worker as the new `serviceWorkerRegistration.js` disables service workers for localhost.
    Navigate to `https://632b314c324c805f8240173f--cashtab-local-dev.netlify.app/#/wallet` where this app has been deployed at this commit. Note that there is a service worker enabled.
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Subscribers: emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D12021

--- web/cashtab/src/index.js
+++ web/cashtab/src/index.js
@@ -6,6 +6,7 @@ import { AuthenticationProvider, WalletProvider } from './utils/context';
 import { HashRouter as Router } from 'react-router-dom';
 import GA from './utils/GoogleAnalytics';
 import 'antd/dist/antd.min.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
     <AuthenticationProvider>
@@ -18,6 +19,11 @@ ReactDOM.render(
     </AuthenticationProvider>,
     document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 if (module.hot) {
     module.hot.accept();
