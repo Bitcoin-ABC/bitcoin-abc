Bitcoin-ABC/bitcoin-abc

commit 5ae93395eb73756d4938d83de2ad6dc3488851c8

refactor: Move block storage globals to blockstorage
Summary:
However, keep a declaration in validation to make it possible to move
smaller chunks to blockstorage without breaking compilation.

Also, expose AbortNode in the header.

Can be reviewed with --color-moved=dimmed-zebra --color-moved-ws=ignore-all-space

This is a backport of [[https://github.com/bitcoin/bitcoin/pull/21727 | core#21727]] [2/4]
https://github.com/bitcoin/bitcoin/pull/21727/commits/fa247a327fc7c7cea6bc8f93637b8babd3015ffa

Depends on D12032

Test Plan: `ninja all check-all`

Reviewers: #bitcoin_abc, sdulfari

Reviewed By: #bitcoin_abc, sdulfari

Differential Revision: https://reviews.bitcoinabc.org/D12033

src/node/blockstorage.cpp
@@ -23,6 +23,25 @@ bool fHavePruned = false;
 bool fPruneMode = false;
 uint64_t nPruneTarget = 0;
 
+// TODO make namespace {
+RecursiveMutex cs_LastBlockFile;
+std::vector<CBlockFileInfo> vinfoBlockFile;
+int nLastBlockFile = 0;
+
+/**
+ * Global flag to indicate we should check to see if there are
+ *  block/undo files that should be deleted.  Set on startup
+ *  or if we allocate more file space when we're in prune mode
+ */
+bool fCheckForPruning = false;
+
+/** Dirty block index entries. */
+std::set<CBlockIndex *> setDirtyBlockIndex;
+
+/** Dirty block file entries. */
+std::set<int> setDirtyFileInfo;
+// } // namespace
+
 bool IsBlockPruned(const CBlockIndex *pblockindex) {
     return (fHavePruned && !pblockindex->nStatus.hasData() &&
             pblockindex->nTx > 0);

src/validation.cpp
@@ -109,23 +109,16 @@ CFeeRate minRelayTxFee = CFeeRate(DEFAULT_MIN_RELAY_TX_FEE_PER_KB);
 namespace {
 CBlockIndex *pindexBestInvalid = nullptr;
 CBlockIndex *pindexBestParked = nullptr;
-
-RecursiveMutex cs_LastBlockFile;
-std::vector<CBlockFileInfo> vinfoBlockFile;
-int nLastBlockFile = 0;
-/**
- * Global flag to indicate we should check to see if there are block/undo files
- * that should be deleted. Set on startup or if we allocate more file space when
- * we're in prune mode.
- */
-bool fCheckForPruning = false;
-
-/** Dirty block index entries. */
-std::set<const CBlockIndex *> setDirtyBlockIndex;
-
-/** Dirty block file entries. */
-std::set<int> setDirtyFileInfo;
 } // namespace
+
+// Internal stuff from blockstorage ...
+extern RecursiveMutex cs_LastBlockFile;
+extern std::vector<CBlockFileInfo> vinfoBlockFile;
+extern int nLastBlockFile;
+extern bool fCheckForPruning;
+extern std::set<const CBlockIndex *> setDirtyBlockIndex;
+extern std::set<int> setDirtyFileInfo;
+// ... TODO move fully to blockstorage
 
 BlockValidationOptions::BlockValidationOptions(const Config &config)
     : excessiveBlockSize(config.GetMaxBlockSize()), checkPoW(true),
@@ -1258,9 +1251,8 @@ bool UndoReadFromDisk(CBlockUndo &blockundo, const CBlockIndex *pindex) {
     return true;
 }
 
-static bool AbortNode(BlockValidationState &state,
-                      const std::string &strMessage,
-                      const bilingual_str &userMessage = bilingual_str()) {
+bool AbortNode(BlockValidationState &state, const std::string &strMessage,
+               const bilingual_str &userMessage) {
     AbortNode(strMessage, userMessage);
     return state.Error(strMessage);
 }

src/validation.h
@@ -29,6 +29,7 @@
 #include <txdb.h>
 #include <txmempool.h> // For CTxMemPool::cs
 #include <util/check.h>
+#include <util/translation.h>
 #include <versionbits.h>
 
 #include <atomic>
@@ -244,6 +245,9 @@ CTransactionRef GetTransaction(const CBlockIndex *const block_index,
                                const Consensus::Params &consensusParams,
                                BlockHash &hashBlock);
 Amount GetBlockSubsidy(int nHeight, const Consensus::Params &consensusParams);
+
+bool AbortNode(BlockValidationState &state, const std::string &strMessage,
+               const bilingual_str &userMessage = bilingual_str{});
 
 /**
  * Guess verification progress (as a fraction between 0.0=genesis and

commit 5bb6c5912018e068525d31a8d4a0b3ad3dbc028d

Fix mismatching types for setDirtyBlockIndex
Summary:
The extern declaration and the definition don't match, triggering a -Wodr warning.
This issue was introduced in D12033.

Test Plan:
  ninja all check-all

Reviewers: #bitcoin_abc, PiRK

Reviewed By: #bitcoin_abc, PiRK

Differential Revision: https://reviews.bitcoinabc.org/D12078

src/node/blockstorage.cpp
@@ -38,7 +38,7 @@ int nLastBlockFile = 0;
 bool fCheckForPruning = false;
 
 /** Dirty block index entries. */
-std::set<CBlockIndex *> setDirtyBlockIndex;
+std::set<const CBlockIndex *> setDirtyBlockIndex;
 
 /** Dirty block file entries. */
 std::set<int> setDirtyFileInfo;