bitcoin-ghost/bitcoin-core-ladder-script

prefer to use txindex if available for GetTransaction
Fixes #22382

src/validation.cpp
@@ -1159,6 +1159,20 @@ CTransactionRef GetTransaction(const CBlockIndex* const block_index, const CTxMe
 {
     LOCK(cs_main);
 
+    if (mempool && !block_index) {
+        CTransactionRef ptx = mempool->get(hash);
+        if (ptx) return ptx;
+    }
+    if (g_txindex) {
+        CTransactionRef tx;
+        uint256 block_hash;
+        if (g_txindex->FindTx(hash, block_hash, tx)) {
+            if (!block_index || block_index->GetBlockHash() == block_hash) {
+                hashBlock = block_hash;
+                return tx;
+            }
+        }
+    }
     if (block_index) {
         CBlock block;
         if (ReadBlockFromDisk(block, block_index, consensusParams)) {
@@ -1169,15 +1183,6 @@ CTransactionRef GetTransaction(const CBlockIndex* const block_index, const CTxMe
                 }
             }
         }
-        return nullptr;
-    }
-    if (mempool) {
-        CTransactionRef ptx = mempool->get(hash);
-        if (ptx) return ptx;
-    }
-    if (g_txindex) {
-        CTransactionRef tx;
-        if (g_txindex->FindTx(hash, hashBlock, tx)) return tx;
     }
     return nullptr;
 }

Bitcoin-ABC/bitcoin-abc

Summary:
```
Motivation: prevent excessive disk reads if txindex is enabled.
This will also benefit to `isfinaltransaction`.
Backport of [[https://github.com/bitcoin/bitcoin/pull/22383 | core#22383]].
Test Plan:
  ninja all check-all
Reviewers: #bitcoin_abc, PiRK
Reviewed By: #bitcoin_abc, PiRK
Differential Revision: https://reviews.bitcoinabc.org/D12208

src/validation.cpp
@@ -875,6 +875,22 @@ CTransactionRef GetTransaction(const CBlockIndex *const block_index,
                                BlockHash &hashBlock) {
     LOCK(cs_main);
 
+    if (mempool && !block_index) {
+        CTransactionRef ptx = mempool->get(txid);
+        if (ptx) {
+            return ptx;
+        }
+    }
+    if (g_txindex) {
+        CTransactionRef tx;
+        BlockHash block_hash;
+        if (g_txindex->FindTx(txid, block_hash, tx)) {
+            if (!block_index || block_index->GetBlockHash() == block_hash) {
+                hashBlock = block_hash;
+                return tx;
+            }
+        }
+    }
     if (block_index) {
         CBlock block;
         if (ReadBlockFromDisk(block, block_index, consensusParams)) {
@@ -884,19 +900,6 @@ CTransactionRef GetTransaction(const CBlockIndex *const block_index,
                     return tx;
                 }
             }
-        }
-        return nullptr;
-    }
-    if (mempool) {
-        CTransactionRef ptx = mempool->get(txid);
-        if (ptx) {
-            return ptx;
-        }
-    }
-    if (g_txindex) {
-        CTransactionRef tx;
-        if (g_txindex->FindTx(txid, hashBlock, tx)) {
-            return tx;
         }
     }
     return nullptr;