0dc0af8aa30237ef946b7dc52f8dd43bdce3f0ab
Fabien <fabcien@gmail.com>
2022-10-17 14:08:15 +0200

    [MOVEONLY] [tests] Move addrman ser/deser tests to addrman_tests.cpp
    Summary:
    ```
    Addrman serialization/deserialization tests are currently in net_tests.cpp.
    Move them to addrman_tests.cpp with the rest of the addrman tests.
    ```
    
    Backport of [[https://github.com/bitcoin/bitcoin/pull/22725 | core#22725]].
    
    Depends on D12272.
    
    Test Plan:
      ninja check
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D12274

--- src/test/net_tests.cpp
+++ src/test/net_tests.cpp
@@ -4,8 +4,6 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <net.h>
 
-#include <addrdb.h>
-#include <addrman.h>
 #include <avalanche/avalanche.h>
 #include <avalanche/processor.h>
 #include <chainparams.h>
@@ -235,44 +233,6 @@ struct CConnmanTest : public CConnman {
 };
 } // namespace
 
-class CAddrManSerializationMock : public CAddrMan {
-public:
-    virtual void Serialize(CDataStream &s) const = 0;
-
-    CAddrManSerializationMock()
-        : CAddrMan(/* consistency_check_ratio= */ 100) {}
-};
-
-class CAddrManUncorrupted : public CAddrManSerializationMock {
-public:
-    void Serialize(CDataStream &s) const override { CAddrMan::Serialize(s); }
-};
-
-class CAddrManCorrupted : public CAddrManSerializationMock {
-public:
-    void Serialize(CDataStream &s) const override {
-        // Produces corrupt output that claims addrman has 20 addrs when it only
-        // has one addr.
-        uint8_t nVersion = 1;
-        s << nVersion;
-        s << uint8_t(32);
-        s << nKey;
-        s << 10; // nNew
-        s << 10; // nTried
-
-        int nUBuckets = ADDRMAN_NEW_BUCKET_COUNT ^ (1 << 30);
-        s << nUBuckets;
-
-        CService serv;
-        BOOST_CHECK(Lookup("252.1.1.1", serv, 7777, false));
-        CAddress addr = CAddress(serv, NODE_NONE);
-        CNetAddr resolved;
-        BOOST_CHECK(LookupHost("252.2.2.2", resolved, false));
-        CAddrInfo info = CAddrInfo(addr, resolved);
-        s << info;
-    }
-};
-
 class NetTestConfig : public DummyConfig {
 public:
     bool SetMaxBlockSize(uint64_t maxBlockSize) override {
@@ -284,15 +244,6 @@ public:
 private:
     uint64_t nMaxBlockSize;
 };
-
-static CDataStream AddrmanToStream(const CAddrManSerializationMock &_addrman) {
-    CDataStream ssPeersIn(SER_DISK, CLIENT_VERSION);
-    ssPeersIn << Params().DiskMagic();
-    ssPeersIn << _addrman;
-    std::string str = ssPeersIn.str();
-    std::vector<uint8_t> vchData(str.begin(), str.end());
-    return CDataStream(vchData, SER_DISK, CLIENT_VERSION);
-}
 
 // Use TestingSetup or a daughter class so that m_node.addrman is non-null
 BOOST_FIXTURE_TEST_SUITE(net_tests, RegTestingSetup)
@@ -306,84 +257,6 @@ BOOST_AUTO_TEST_CASE(cnode_listen_port) {
     BOOST_CHECK(gArgs.SoftSetArg("-port", ToString(altPort)));
     port = GetListenPort();
     BOOST_CHECK(port == altPort);
-}
-
-BOOST_AUTO_TEST_CASE(caddrdb_read) {
-    CAddrManUncorrupted addrmanUncorrupted;
-    addrmanUncorrupted.MakeDeterministic();
-
-    CService addr1, addr2, addr3;
-    BOOST_CHECK(Lookup("250.7.1.1", addr1, 8333, false));
-    BOOST_CHECK(Lookup("250.7.2.2", addr2, 9999, false));
-    BOOST_CHECK(Lookup("250.7.3.3", addr3, 9999, false));
-    BOOST_CHECK(Lookup("250.7.3.3"s, addr3, 9999, false));
-    BOOST_CHECK(!Lookup("250.7.3.3\0example.com"s, addr3, 9999, false));
-
-    // Add three addresses to new table.
-    CService source;
-    BOOST_CHECK(Lookup("252.5.1.1", source, 8333, false));
-    BOOST_CHECK(addrmanUncorrupted.Add(CAddress(addr1, NODE_NONE), source));
-    BOOST_CHECK(addrmanUncorrupted.Add(CAddress(addr2, NODE_NONE), source));
-    BOOST_CHECK(addrmanUncorrupted.Add(CAddress(addr3, NODE_NONE), source));
-
-    // Test that the de-serialization does not throw an exception.
-    CDataStream ssPeers1 = AddrmanToStream(addrmanUncorrupted);
-    bool exceptionThrown = false;
-    CAddrMan addrman1(/* consistency_check_ratio= */ 100);
-
-    BOOST_CHECK(addrman1.size() == 0);
-    try {
-        uint8_t pchMsgTmp[4];
-        ssPeers1 >> pchMsgTmp;
-        ssPeers1 >> addrman1;
-    } catch (const std::exception &) {
-        exceptionThrown = true;
-    }
-
-    BOOST_CHECK(addrman1.size() == 3);
-    BOOST_CHECK(exceptionThrown == false);
-
-    // Test that CAddrDB::Read creates an addrman with the correct number of
-    // addrs.
-    CDataStream ssPeers2 = AddrmanToStream(addrmanUncorrupted);
-
-    CAddrMan addrman2(/* consistency_check_ratio= */ 100);
-    CAddrDB adb(Params());
-    BOOST_CHECK(addrman2.size() == 0);
-    BOOST_CHECK(adb.Read(addrman2, ssPeers2));
-    BOOST_CHECK(addrman2.size() == 3);
-}
-
-BOOST_AUTO_TEST_CASE(caddrdb_read_corrupted) {
-    CAddrManCorrupted addrmanCorrupted;
-    addrmanCorrupted.MakeDeterministic();
-
-    // Test that the de-serialization of corrupted addrman throws an exception.
-    CDataStream ssPeers1 = AddrmanToStream(addrmanCorrupted);
-    bool exceptionThrown = false;
-    CAddrMan addrman1(/* consistency_check_ratio= */ 100);
-    BOOST_CHECK(addrman1.size() == 0);
-    try {
-        uint8_t pchMsgTmp[4];
-        ssPeers1 >> pchMsgTmp;
-        ssPeers1 >> addrman1;
-    } catch (const std::exception &) {
-        exceptionThrown = true;
-    }
-    // Even through de-serialization failed addrman is not left in a clean
-    // state.
-    BOOST_CHECK(addrman1.size() == 1);
-    BOOST_CHECK(exceptionThrown);
-
-    // Test that CAddrDB::Read leaves addrman in a clean state if
-    // de-serialization fails.
-    CDataStream ssPeers2 = AddrmanToStream(addrmanCorrupted);
-
-    CAddrMan addrman2(/* consistency_check_ratio= */ 100);
-    CAddrDB adb(Params());
-    BOOST_CHECK(addrman2.size() == 0);
-    BOOST_CHECK(!adb.Read(addrman2, ssPeers2));
-    BOOST_CHECK(addrman2.size() == 0);
 }
 
 BOOST_AUTO_TEST_CASE(cnode_simple_test) {
