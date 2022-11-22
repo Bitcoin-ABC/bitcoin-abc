bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

test/setup: Use LoadChainstate
This commit coalesces the chainstate loading sequence between our unit
test and non-unit test init codepaths.

src/test/util/setup_common.h
@@ -7,6 +7,7 @@
 
 #include <chainparamsbase.h>
 #include <fs.h>
+#include <init/caches.h>
 #include <key.h>
 #include <util/system.h>
 #include <node/context.h>
@@ -89,6 +90,7 @@ struct BasicTestingSetup {
  * initialization behaviour.
  */
 struct ChainTestingSetup : public BasicTestingSetup {
+    CacheSizes m_cache_sizes{};
 
     explicit ChainTestingSetup(const std::string& chainName = CBaseChainParams::MAIN, const std::vector<const char*>& extra_args = {});
     ~ChainTestingSetup();

Bitcoin-ABC/bitcoin-abc

Summary:
```
This commit coalesces the chainstate loading sequence between our unit test and non-unit test init codepaths.
Partial backport of [[https://github.com/bitcoin/bitcoin/pull/23280 | core#23280]]:
https://github.com/bitcoin/bitcoin/pull/23280/commits/9a5a5a3d08b2c130ab9147914739ff3583b0dc84
Depends on D12582.
Test Plan:
  ninja all check
Reviewers: #bitcoin_abc, PiRK
Reviewed By: #bitcoin_abc, PiRK
Differential Revision: https://reviews.bitcoinabc.org/D12583

src/test/util/setup_common.h
@@ -9,6 +9,7 @@
 #include <consensus/amount.h>
 #include <fs.h>
 #include <key.h>
+#include <node/caches.h>
 #include <node/context.h>
 #include <primitives/transaction.h>
 #include <pubkey.h>
@@ -117,6 +118,8 @@ struct BasicTestingSetup {
  * initialization behaviour.
  */
 struct ChainTestingSetup : public BasicTestingSetup {
+    CacheSizes m_cache_sizes{};
+
     explicit ChainTestingSetup(
         const std::string &chainName = CBaseChainParams::MAIN,
         const std::vector<const char *> &extra_args = {});