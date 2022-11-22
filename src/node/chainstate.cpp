bitcoin-ghost/bitcoin-core-ladder-script

style-only: Remove redundant scope in *Chainstate
I strongly recommend reviewing with the following git-diff flags:
  --ignore-space-change

src/node/chainstate.cpp
@@ -26,100 +26,98 @@ std::optional<ChainstateLoadingError> LoadChainstate(bool fReset,
         return fReset || fReindexChainState || chainstate->CoinsTip().GetBestBlock().IsNull();
     };
 
-    {
-        LOCK(cs_main);
-        chainman.InitializeChainstate(mempool);
-        chainman.m_total_coinstip_cache = nCoinCacheUsage;
-        chainman.m_total_coinsdb_cache = nCoinDBCache;
+    LOCK(cs_main);
+    chainman.InitializeChainstate(mempool);
+    chainman.m_total_coinstip_cache = nCoinCacheUsage;
+    chainman.m_total_coinsdb_cache = nCoinDBCache;
 
-        UnloadBlockIndex(mempool, chainman);
+    UnloadBlockIndex(mempool, chainman);
 
-        auto& pblocktree{chainman.m_blockman.m_block_tree_db};
-        // new CBlockTreeDB tries to delete the existing file, which
-        // fails if it's still open from the previous loop. Close it first:
-        pblocktree.reset();
-        pblocktree.reset(new CBlockTreeDB(nBlockTreeDBCache, block_tree_db_in_memory, fReset));
+    auto& pblocktree{chainman.m_blockman.m_block_tree_db};
+    // new CBlockTreeDB tries to delete the existing file, which
+    // fails if it's still open from the previous loop. Close it first:
+    pblocktree.reset();
+    pblocktree.reset(new CBlockTreeDB(nBlockTreeDBCache, block_tree_db_in_memory, fReset));
 
-        if (fReset) {
-            pblocktree->WriteReindexing(true);
-            //If we're reindexing in prune mode, wipe away unusable block files and all undo data files
-            if (fPruneMode)
-                CleanupBlockRevFiles();
-        }
+    if (fReset) {
+        pblocktree->WriteReindexing(true);
+        //If we're reindexing in prune mode, wipe away unusable block files and all undo data files
+        if (fPruneMode)
+            CleanupBlockRevFiles();
+    }
 
+    if (shutdown_requested && shutdown_requested()) return ChainstateLoadingError::SHUTDOWN_PROBED;
+
+    // LoadBlockIndex will load fHavePruned if we've ever removed a
+    // block file from disk.
+    // Note that it also sets fReindex based on the disk flag!
+    // From here on out fReindex and fReset mean something different!
+    if (!chainman.LoadBlockIndex()) {
         if (shutdown_requested && shutdown_requested()) return ChainstateLoadingError::SHUTDOWN_PROBED;
+        return ChainstateLoadingError::ERROR_LOADING_BLOCK_DB;
+    }
 
-        // LoadBlockIndex will load fHavePruned if we've ever removed a
-        // block file from disk.
-        // Note that it also sets fReindex based on the disk flag!
-        // From here on out fReindex and fReset mean something different!
-        if (!chainman.LoadBlockIndex()) {
-            if (shutdown_requested && shutdown_requested()) return ChainstateLoadingError::SHUTDOWN_PROBED;
-            return ChainstateLoadingError::ERROR_LOADING_BLOCK_DB;
+    if (!chainman.BlockIndex().empty() &&
+            !chainman.m_blockman.LookupBlockIndex(consensus_params.hashGenesisBlock)) {
+        return ChainstateLoadingError::ERROR_BAD_GENESIS_BLOCK;
+    }
+
+    // Check for changed -prune state.  What we are concerned about is a user who has pruned blocks
+    // in the past, but is now trying to run unpruned.
+    if (fHavePruned && !fPruneMode) {
+        return ChainstateLoadingError::ERROR_PRUNED_NEEDS_REINDEX;
+    }
+
+    // At this point blocktree args are consistent with what's on disk.
+    // If we're not mid-reindex (based on disk + args), add a genesis block on disk
+    // (otherwise we use the one already on disk).
+    // This is called again in ThreadImport after the reindex completes.
+    if (!fReindex && !chainman.ActiveChainstate().LoadGenesisBlock()) {
+        return ChainstateLoadingError::ERROR_LOAD_GENESIS_BLOCK_FAILED;
+    }
+
+    // At this point we're either in reindex or we've loaded a useful
+    // block tree into BlockIndex()!
+
+    for (CChainState* chainstate : chainman.GetAll()) {
+        chainstate->InitCoinsDB(
+            /* cache_size_bytes */ nCoinDBCache,
+            /* in_memory */ coins_db_in_memory,
+            /* should_wipe */ fReset || fReindexChainState);
+
+        if (coins_error_cb) {
+            chainstate->CoinsErrorCatcher().AddReadErrCallback(coins_error_cb);
         }
 
-        if (!chainman.BlockIndex().empty() &&
-                !chainman.m_blockman.LookupBlockIndex(consensus_params.hashGenesisBlock)) {
-            return ChainstateLoadingError::ERROR_BAD_GENESIS_BLOCK;
+        // If necessary, upgrade from older database format.
+        // This is a no-op if we cleared the coinsviewdb with -reindex or -reindex-chainstate
+        if (!chainstate->CoinsDB().Upgrade()) {
+            return ChainstateLoadingError::ERROR_CHAINSTATE_UPGRADE_FAILED;
         }
 
-        // Check for changed -prune state.  What we are concerned about is a user who has pruned blocks
-        // in the past, but is now trying to run unpruned.
-        if (fHavePruned && !fPruneMode) {
-            return ChainstateLoadingError::ERROR_PRUNED_NEEDS_REINDEX;
+        // ReplayBlocks is a no-op if we cleared the coinsviewdb with -reindex or -reindex-chainstate
+        if (!chainstate->ReplayBlocks()) {
+            return ChainstateLoadingError::ERROR_REPLAYBLOCKS_FAILED;
         }
 
-        // At this point blocktree args are consistent with what's on disk.
-        // If we're not mid-reindex (based on disk + args), add a genesis block on disk
-        // (otherwise we use the one already on disk).
-        // This is called again in ThreadImport after the reindex completes.
-        if (!fReindex && !chainman.ActiveChainstate().LoadGenesisBlock()) {
-            return ChainstateLoadingError::ERROR_LOAD_GENESIS_BLOCK_FAILED;
+        // The on-disk coinsdb is now in a good state, create the cache
+        chainstate->InitCoinsCache(nCoinCacheUsage);
+        assert(chainstate->CanFlushToDisk());
+
+        if (!is_coinsview_empty(chainstate)) {
+            // LoadChainTip initializes the chain based on CoinsTip()'s best block
+            if (!chainstate->LoadChainTip()) {
+                return ChainstateLoadingError::ERROR_LOADCHAINTIP_FAILED;
+            }
+            assert(chainstate->m_chain.Tip() != nullptr);
         }
+    }
 
-        // At this point we're either in reindex or we've loaded a useful
-        // block tree into BlockIndex()!
-
-        for (CChainState* chainstate : chainman.GetAll()) {
-            chainstate->InitCoinsDB(
-                /* cache_size_bytes */ nCoinDBCache,
-                /* in_memory */ coins_db_in_memory,
-                /* should_wipe */ fReset || fReindexChainState);
-
-            if (coins_error_cb) {
-                chainstate->CoinsErrorCatcher().AddReadErrCallback(coins_error_cb);
-            }
-
-            // If necessary, upgrade from older database format.
-            // This is a no-op if we cleared the coinsviewdb with -reindex or -reindex-chainstate
-            if (!chainstate->CoinsDB().Upgrade()) {
-                return ChainstateLoadingError::ERROR_CHAINSTATE_UPGRADE_FAILED;
-            }
-
-            // ReplayBlocks is a no-op if we cleared the coinsviewdb with -reindex or -reindex-chainstate
-            if (!chainstate->ReplayBlocks()) {
-                return ChainstateLoadingError::ERROR_REPLAYBLOCKS_FAILED;
-            }
-
-            // The on-disk coinsdb is now in a good state, create the cache
-            chainstate->InitCoinsCache(nCoinCacheUsage);
-            assert(chainstate->CanFlushToDisk());
-
-            if (!is_coinsview_empty(chainstate)) {
-                // LoadChainTip initializes the chain based on CoinsTip()'s best block
-                if (!chainstate->LoadChainTip()) {
-                    return ChainstateLoadingError::ERROR_LOADCHAINTIP_FAILED;
-                }
-                assert(chainstate->m_chain.Tip() != nullptr);
-            }
-        }
-
-        if (!fReset) {
-            auto chainstates{chainman.GetAll()};
-            if (std::any_of(chainstates.begin(), chainstates.end(),
-                            [](const CChainState* cs) EXCLUSIVE_LOCKS_REQUIRED(cs_main) { return cs->NeedsRedownload(); })) {
-                return ChainstateLoadingError::ERROR_BLOCKS_WITNESS_INSUFFICIENTLY_VALIDATED;
-            }
+    if (!fReset) {
+        auto chainstates{chainman.GetAll()};
+        if (std::any_of(chainstates.begin(), chainstates.end(),
+                        [](const CChainState* cs) EXCLUSIVE_LOCKS_REQUIRED(cs_main) { return cs->NeedsRedownload(); })) {
+            return ChainstateLoadingError::ERROR_BLOCKS_WITNESS_INSUFFICIENTLY_VALIDATED;
         }
     }
 
@@ -138,22 +136,20 @@ std::optional<ChainstateLoadVerifyError> VerifyLoadedChainstate(ChainstateManage
         return fReset || fReindexChainState || chainstate->CoinsTip().GetBestBlock().IsNull();
     };
 
-    {
-        LOCK(cs_main);
+    LOCK(cs_main);
 
-        for (CChainState* chainstate : chainman.GetAll()) {
-            if (!is_coinsview_empty(chainstate)) {
-                const CBlockIndex* tip = chainstate->m_chain.Tip();
-                if (tip && tip->nTime > get_unix_time_seconds() + 2 * 60 * 60) {
-                    return ChainstateLoadVerifyError::ERROR_BLOCK_FROM_FUTURE;
-                }
+    for (CChainState* chainstate : chainman.GetAll()) {
+        if (!is_coinsview_empty(chainstate)) {
+            const CBlockIndex* tip = chainstate->m_chain.Tip();
+            if (tip && tip->nTime > get_unix_time_seconds() + 2 * 60 * 60) {
+                return ChainstateLoadVerifyError::ERROR_BLOCK_FROM_FUTURE;
+            }
 
-                if (!CVerifyDB().VerifyDB(
-                        *chainstate, consensus_params, chainstate->CoinsDB(),
-                        check_level,
-                        check_blocks)) {
-                    return ChainstateLoadVerifyError::ERROR_CORRUPTED_BLOCK_DB;
-                }
+            if (!CVerifyDB().VerifyDB(
+                    *chainstate, consensus_params, chainstate->CoinsDB(),
+                    check_level,
+                    check_blocks)) {
+                return ChainstateLoadVerifyError::ERROR_CORRUPTED_BLOCK_DB;
             }
         }
     }

Bitcoin-ABC/bitcoin-abc

Summary:
Completes backport of [[https://github.com/bitcoin/bitcoin/pull/23280 | core#23280]]:
https://github.com/bitcoin/bitcoin/pull/23280/commits/7f15eff2ddd86034e84a19413e1a42883987f660
Depends on D12584.
Test Plan:
  ninja all check-all
Reviewers: #bitcoin_abc, PiRK
Reviewed By: #bitcoin_abc, PiRK
Differential Revision: https://reviews.bitcoinabc.org/D12585

src/node/chainstate.cpp
@@ -23,112 +23,109 @@ LoadChainstate(bool fReset, ChainstateManager &chainman, CTxMemPool *mempool,
                    chainstate->CoinsTip().GetBestBlock().IsNull();
         };
 
-    {
-        LOCK(cs_main);
-        chainman.InitializeChainstate(mempool);
-        chainman.m_total_coinstip_cache = nCoinCacheUsage;
-        chainman.m_total_coinsdb_cache = nCoinDBCache;
+    LOCK(cs_main);
+    chainman.InitializeChainstate(mempool);
+    chainman.m_total_coinstip_cache = nCoinCacheUsage;
+    chainman.m_total_coinsdb_cache = nCoinDBCache;
 
-        UnloadBlockIndex(mempool, chainman);
+    UnloadBlockIndex(mempool, chainman);
 
-        auto &pblocktree{chainman.m_blockman.m_block_tree_db};
-        // new CBlockTreeDB tries to delete the existing file, which
-        // fails if it's still open from the previous loop. Close it first:
-        pblocktree.reset();
-        pblocktree.reset(new CBlockTreeDB(nBlockTreeDBCache,
-                                          block_tree_db_in_memory, fReset));
+    auto &pblocktree{chainman.m_blockman.m_block_tree_db};
+    // new CBlockTreeDB tries to delete the existing file, which
+    // fails if it's still open from the previous loop. Close it first:
+    pblocktree.reset();
+    pblocktree.reset(
+        new CBlockTreeDB(nBlockTreeDBCache, block_tree_db_in_memory, fReset));
 
-        if (fReset) {
-            pblocktree->WriteReindexing(true);
-            // If we're reindexing in prune mode, wipe away unusable block
-            // files and all undo data files
-            if (fPruneMode_) {
-                CleanupBlockRevFiles();
-            }
+    if (fReset) {
+        pblocktree->WriteReindexing(true);
+        // If we're reindexing in prune mode, wipe away unusable block
+        // files and all undo data files
+        if (fPruneMode_) {
+            CleanupBlockRevFiles();
         }
+    }
 
-        // If necessary, upgrade from older database format.
-        // This is a no-op if we cleared the block tree db with -reindex
-        // or -reindex-chainstate
-        if (!pblocktree->Upgrade(consensus_params)) {
-            return ChainstateLoadingError::ERROR_UPGRADING_BLOCK_DB;
-        }
+    // If necessary, upgrade from older database format.
+    // This is a no-op if we cleared the block tree db with -reindex
+    // or -reindex-chainstate
+    if (!pblocktree->Upgrade(consensus_params)) {
+        return ChainstateLoadingError::ERROR_UPGRADING_BLOCK_DB;
+    }
 
+    if (shutdown_requested && shutdown_requested()) {
+        return ChainstateLoadingError::SHUTDOWN_PROBED;
+    }
+
+    // LoadBlockIndex will load fHavePruned if we've ever removed a
+    // block file from disk.
+    // Note that it also sets fReindex based on the disk flag!
+    // From here on out fReindex and fReset mean something different!
+    if (!chainman.LoadBlockIndex()) {
         if (shutdown_requested && shutdown_requested()) {
             return ChainstateLoadingError::SHUTDOWN_PROBED;
         }
+        return ChainstateLoadingError::ERROR_LOADING_BLOCK_DB;
+    }
 
-        // LoadBlockIndex will load fHavePruned if we've ever removed a
-        // block file from disk.
-        // Note that it also sets fReindex based on the disk flag!
-        // From here on out fReindex and fReset mean something different!
-        if (!chainman.LoadBlockIndex()) {
-            if (shutdown_requested && shutdown_requested()) {
-                return ChainstateLoadingError::SHUTDOWN_PROBED;
-            }
-            return ChainstateLoadingError::ERROR_LOADING_BLOCK_DB;
+    if (!chainman.BlockIndex().empty() &&
+        !chainman.m_blockman.LookupBlockIndex(
+            consensus_params.hashGenesisBlock)) {
+        return ChainstateLoadingError::ERROR_BAD_GENESIS_BLOCK;
+    }
+
+    // Check for changed -prune state.  What we are concerned about is a
+    // user who has pruned blocks in the past, but is now trying to run
+    // unpruned.
+    if (fHavePruned && !fPruneMode_) {
+        return ChainstateLoadingError::ERROR_PRUNED_NEEDS_REINDEX;
+    }
+
+    // At this point blocktree args are consistent with what's on disk.
+    // If we're not mid-reindex (based on disk + args), add a genesis
+    // block on disk (otherwise we use the one already on disk). This is
+    // called again in ThreadImport after the reindex completes.
+    if (!fReindex && !chainman.ActiveChainstate().LoadGenesisBlock()) {
+        return ChainstateLoadingError::ERROR_LOAD_GENESIS_BLOCK_FAILED;
+    }
+
+    // At this point we're either in reindex or we've loaded a useful
+    // block tree into BlockIndex()!
+
+    for (CChainState *chainstate : chainman.GetAll()) {
+        chainstate->InitCoinsDB(
+            /* cache_size_bytes */ nCoinDBCache,
+            /* in_memory */ coins_db_in_memory,
+            /* should_wipe */ fReset || fReindexChainState);
+
+        if (coins_error_cb) {
+            chainstate->CoinsErrorCatcher().AddReadErrCallback(coins_error_cb);
         }
 
-        if (!chainman.BlockIndex().empty() &&
-            !chainman.m_blockman.LookupBlockIndex(
-                consensus_params.hashGenesisBlock)) {
-            return ChainstateLoadingError::ERROR_BAD_GENESIS_BLOCK;
+        // If necessary, upgrade from older database format.
+        // This is a no-op if we cleared the coinsviewdb with -reindex
+        // or -reindex-chainstate
+        if (!chainstate->CoinsDB().Upgrade()) {
+            return ChainstateLoadingError::ERROR_CHAINSTATE_UPGRADE_FAILED;
         }
 
-        // Check for changed -prune state.  What we are concerned about is a
-        // user who has pruned blocks in the past, but is now trying to run
-        // unpruned.
-        if (fHavePruned && !fPruneMode_) {
-            return ChainstateLoadingError::ERROR_PRUNED_NEEDS_REINDEX;
+        // ReplayBlocks is a no-op if we cleared the coinsviewdb with
+        // -reindex or -reindex-chainstate
+        if (!chainstate->ReplayBlocks()) {
+            return ChainstateLoadingError::ERROR_REPLAYBLOCKS_FAILED;
         }
 
-        // At this point blocktree args are consistent with what's on disk.
-        // If we're not mid-reindex (based on disk + args), add a genesis
-        // block on disk (otherwise we use the one already on disk). This is
-        // called again in ThreadImport after the reindex completes.
-        if (!fReindex && !chainman.ActiveChainstate().LoadGenesisBlock()) {
-            return ChainstateLoadingError::ERROR_LOAD_GENESIS_BLOCK_FAILED;
-        }
+        // The on-disk coinsdb is now in a good state, create the cache
+        chainstate->InitCoinsCache(nCoinCacheUsage);
+        assert(chainstate->CanFlushToDisk());
 
-        // At this point we're either in reindex or we've loaded a useful
-        // block tree into BlockIndex()!
-
-        for (CChainState *chainstate : chainman.GetAll()) {
-            chainstate->InitCoinsDB(
-                /* cache_size_bytes */ nCoinDBCache,
-                /* in_memory */ coins_db_in_memory,
-                /* should_wipe */ fReset || fReindexChainState);
-
-            if (coins_error_cb) {
-                chainstate->CoinsErrorCatcher().AddReadErrCallback(
-                    coins_error_cb);
+        if (!is_coinsview_empty(chainstate)) {
+            // LoadChainTip initializes the chain based on CoinsTip()'s
+            // best block
+            if (!chainstate->LoadChainTip()) {
+                return ChainstateLoadingError::ERROR_LOADCHAINTIP_FAILED;
             }
-
-            // If necessary, upgrade from older database format.
-            // This is a no-op if we cleared the coinsviewdb with -reindex
-            // or -reindex-chainstate
-            if (!chainstate->CoinsDB().Upgrade()) {
-                return ChainstateLoadingError::ERROR_CHAINSTATE_UPGRADE_FAILED;
-            }
-
-            // ReplayBlocks is a no-op if we cleared the coinsviewdb with
-            // -reindex or -reindex-chainstate
-            if (!chainstate->ReplayBlocks()) {
-                return ChainstateLoadingError::ERROR_REPLAYBLOCKS_FAILED;
-            }
-
-            // The on-disk coinsdb is now in a good state, create the cache
-            chainstate->InitCoinsCache(nCoinCacheUsage);
-            assert(chainstate->CanFlushToDisk());
-
-            if (!is_coinsview_empty(chainstate)) {
-                // LoadChainTip initializes the chain based on CoinsTip()'s
-                // best block
-                if (!chainstate->LoadChainTip()) {
-                    return ChainstateLoadingError::ERROR_LOADCHAINTIP_FAILED;
-                }
-                assert(chainstate->m_chain.Tip() != nullptr);
-            }
+            assert(chainstate->m_chain.Tip() != nullptr);
         }
     }
 
@@ -146,22 +143,20 @@ VerifyLoadedChainstate(ChainstateManager &chainman, bool fReset,
                    chainstate->CoinsTip().GetBestBlock().IsNull();
         };
 
-    {
-        LOCK(cs_main);
+    LOCK(cs_main);
 
-        for (CChainState *chainstate : chainman.GetAll()) {
-            if (!is_coinsview_empty(chainstate)) {
-                const CBlockIndex *tip = chainstate->m_chain.Tip();
-                if (tip && tip->nTime > get_unix_time_seconds() +
-                                            MAX_FUTURE_BLOCK_TIME) {
-                    return ChainstateLoadVerifyError::ERROR_BLOCK_FROM_FUTURE;
-                }
+    for (CChainState *chainstate : chainman.GetAll()) {
+        if (!is_coinsview_empty(chainstate)) {
+            const CBlockIndex *tip = chainstate->m_chain.Tip();
+            if (tip &&
+                tip->nTime > get_unix_time_seconds() + MAX_FUTURE_BLOCK_TIME) {
+                return ChainstateLoadVerifyError::ERROR_BLOCK_FROM_FUTURE;
+            }
 
-                if (!CVerifyDB().VerifyDB(*chainstate, config,
-                                          chainstate->CoinsDB(), check_level,
-                                          check_blocks)) {
-                    return ChainstateLoadVerifyError::ERROR_CORRUPTED_BLOCK_DB;
-                }
+            if (!CVerifyDB().VerifyDB(*chainstate, config,
+                                      chainstate->CoinsDB(), check_level,
+                                      check_blocks)) {
+                return ChainstateLoadVerifyError::ERROR_CORRUPTED_BLOCK_DB;
             }
         }
     }