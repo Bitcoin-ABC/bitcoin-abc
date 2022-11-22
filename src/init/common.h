bitcoin-ghost/bitcoin-core-ladder-script

Move common logging GetArgs code to init/common

src/init/common.h
@@ -19,6 +19,8 @@ void UnsetGlobals();
  */
 bool SanityChecks();
 void AddLoggingArgs(ArgsManager& args);
+void SetLoggingOptions(const ArgsManager& args);
+void SetLoggingCategories(const ArgsManager& args);
 } // namespace init
 
 #endif // BITCOIN_INIT_COMMON_H

Bitcoin-ABC/bitcoin-abc

Summary:
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/21732 | core#21732]]
 https://github.com/bitcoin/bitcoin/pull/21732/commits/1fb7fcfa52569a652d3ea55c210b725e60b7d86f
Depends on D12561
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D12562

src/init/common.h
@@ -19,6 +19,8 @@ void UnsetGlobals();
  */
 bool SanityChecks();
 void AddLoggingArgs(ArgsManager &args);
+void SetLoggingOptions(const ArgsManager &args);
+void SetLoggingCategories(const ArgsManager &args);
 } // namespace init
 
 #endif // BITCOIN_INIT_COMMON_H