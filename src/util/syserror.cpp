4951d6b7dd52bbca81453f6803335e9656d34097
John Moffett <john.moff@gmail.com>
2022-12-05 12:59:15 -0500

    Show descriptive error messages when FileCommit fails
    Summary:
    Only raw errno codes are logged if FileCommit fails. These are
    implementation-specific, so it makes it harder to debug based on
    user reports. Instead, use SysErrorString to display both the
    raw int value and the descriptive message.
    
    Consolidate Win32-specific error formatting
    
    GetErrorReason()'s Win32 implementation does the same thing as
    Win32ErrorString(int err) from syserror.cpp, so call the latter.
    
    Also remove now-unnecessary headers from sock.cpp and less verbose
    handling of #ifdefs.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/26654 | core#26654]]
    Depends on D19669
    
    Test Plan:
    `ninja all check-all`
    Win cross-build
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19670

--- src/util/syserror.cpp
+++ src/util/syserror.cpp
@@ -11,6 +11,12 @@
 
 #include <cstring>
 
+#if defined(WIN32)
+#include <codecvt>
+#include <locale>
+#include <windows.h>
+#endif
+
 std::string SysErrorString(int err) {
     char buf[1024];
     /**
@@ -39,3 +45,23 @@ std::string SysErrorString(int err) {
         return strprintf("Unknown error (%d)", err);
     }
 }
+
+#if defined(WIN32)
+std::string Win32ErrorString(int err) {
+    wchar_t buf[256];
+    buf[0] = 0;
+    if (FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM |
+                           FORMAT_MESSAGE_IGNORE_INSERTS |
+                           FORMAT_MESSAGE_MAX_WIDTH_MASK,
+                       nullptr, err, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                       buf, ARRAYSIZE(buf), nullptr)) {
+        return strprintf(
+            "%s (%d)",
+            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>()
+                .to_bytes(buf),
+            err);
+    } else {
+        return strprintf("Unknown error (%d)", err);
+    }
+}
+#endif
