73a81c3931a4f02ca930233312f4894806f0de7f
bytesofman <joey@bitcoinabc.org>
2022-12-19 10:51:08 -0800

    [Cashtab Docs] Removing docs from the monorepo
    Summary:
    T2841
    
    Remove the docs subdirectory from the monorepo. docs.cashtab.com is currently unavailable and 301s to cashtab. Removing this from the monorepo as it is not being maintained.
    
    Test Plan: Confirm this diff removes the `docs/` directory. Confirm in slack that this diff and D12918 can be landed i.e. confirm teamcity builds are configured correctly to take this change.
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D12919

--- web/docs/sidebars.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    sidebar: {
-        Cashtab: ['cashtab-overview'],
-        Extension: ['extension-overview'],
-        Components: ['components-overview'],
-    },
-};
--- web/docs/src/common/Ticker.js
+++
@@ -1,6 +0,0 @@
-export const currency = {
-    name: 'Bitcoin ABC',
-    ticker: 'BCHA',
-    tokenName: 'Bitcoin ABC SLP',
-    tokenTicker: 'SLPA',
-};
