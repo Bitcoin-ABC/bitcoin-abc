ajtowns/bitcoin

refactor: more const annotations for uses of CBlockIndex*

src/rest.cpp
@@ -283,8 +283,8 @@ static bool rest_block(const std::any& context,
         return RESTERR(req, HTTP_BAD_REQUEST, "Invalid hash: " + hashStr);
 
     CBlock block;
-    CBlockIndex* pblockindex = nullptr;
-    CBlockIndex* tip = nullptr;
+    const CBlockIndex* pblockindex = nullptr;
+    const CBlockIndex* tip = nullptr;
     {
         ChainstateManager* maybe_chainman = GetChainman(context, req);
         if (!maybe_chainman) return false;

Bitcoin-ABC/bitcoin-abc

Summary:
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/24515 | core#24515]]
https://github.com/bitcoin/bitcoin/pull/24515/commits/5be9ee3c54dcb396ff52fc8c8b7e4e6e39ec4a3b
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D13020

src/rest.cpp
@@ -296,8 +296,8 @@ static bool rest_block(const Config &config, const std::any &context,
     const BlockHash hash(rawHash);
 
     CBlock block;
-    CBlockIndex *pblockindex = nullptr;
-    CBlockIndex *tip = nullptr;
+    const CBlockIndex *pblockindex = nullptr;
+    const CBlockIndex *tip = nullptr;
     {
         ChainstateManager *maybe_chainman = GetChainman(context, req);
         if (!maybe_chainman) {