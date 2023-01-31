43800e59c2bcf86bf0f1e37da89269f745fa607b
bytesofman <joey@bitcoinabc.org>
2023-01-30 16:27:30 -0800

    [alias-server] [p6] outputScriptToAddress function and unit tests
    Summary:
    T2922
    
    Depends on D13078
    
    Add a function to get P2SH or P2PKH ecash: address from an outputScript. Add unit tests.
    
    The next diff in this stack will implement this function in `getAliases` and add unit tests there.
    
    Test Plan:
    Review function and unit tests
    `npm test`
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D13079

---
+++ web/alias-server/test/utils.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const { outputScriptToAddress } = require('../utils');
+
+describe('alias-server utils.js', function () {
+    it('Converts a P2PKH output script to a valid P2PKH ecash: address', function () {
+        assert.strictEqual(
+            outputScriptToAddress(
+                '76a9149846b6b38ff713334ac19fe3cf851a1f98c07b0088ac',
+            ),
+            'ecash:qzvydd4n3lm3xv62cx078nu9rg0e3srmqq0knykfed',
+        );
+    });
+    it('Converts a P2SH output script to a valid P2SH ecash: address', function () {
+        assert.strictEqual(
+            outputScriptToAddress(
+                'a914c5e60aad8d98f298a76434750630dc1b46a2382187',
+            ),
+            'ecash:prz7vz4d3kv09x98vs682p3smsd5dg3cyykjye6grt',
+        );
+    });
+    it('Returns false when input is invalid (in this case, an inputScript', function () {
+        assert.strictEqual(
+            outputScriptToAddress(
+                '483045022100dfad6c871d101eade422ef6c1c8aca951a43c65c6dd7cccc44d56b04a6b1b60602200c82ac4c06523fdb7ee455f1903c95a37c6fc1bb2c35c080778b869bb63f8f784121031e9483074a9f0ee7380131a870edbe9403e7b807a4b5611b01540a150f6aa4',
+            ),
+            false,
+        );
+    });
+    it('Returns false when input is an empty string', function () {
+        assert.strictEqual(outputScriptToAddress(''), false);
+    });
+});
