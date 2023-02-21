RadiantBlockchain/radiant-node

mempool: Save & restore mempool entry acceptance height on reorg
Summary

MR !1128 introduced a regression whereby mempool entry acceptance height
would be lost across reorgs and just set to the new tip height after the
block unwind is done.  This MR fixes that and restores previous behavior
where a mempool entry's acceptance height is remembered until a
node restart (at which point it is lost since it is not serialized).

The `mempool_reorg.py` functional test was modified to also test that the
height is preserved properly across reorgs.

Test Plan

- `ninja all check`
- `test/functional/test_runner.py mempool_reorg`
- `ninja bench_bitcoin && src/bench/bench_bitcoin -filter=Reorg.\*`

test/functional/mempool_reorg.py
@@ -102,6 +102,10 @@ class MempoolCoinbaseTest(BitcoinTestFramework):
         assert_equal(set(self.nodes[0].getrawmempool()), {
                      spend_101_id, spend_102_1_id, timelock_tx_id})
 
+        # save acceptance heights of 2 of the txs to later test that they are preserved across reorgs
+        spend_101_height = self.nodes[0].getmempoolentry(spend_101_id)["height"]
+        spend_102_1_height = self.nodes[0].getmempoolentry(spend_102_1_id)["height"]
+
         for node in self.nodes:
             node.invalidateblock(last_block[0])
         # Time-locked transaction is now too immature and has been removed from the mempool
@@ -109,6 +113,19 @@ class MempoolCoinbaseTest(BitcoinTestFramework):
         # mempool
         assert_equal(set(self.nodes[0].getrawmempool()), {
                      spend_101_id, spend_102_1_id, spend_103_1_id})
+
+        # now ensure that the acceptance height of the two txs was preserved across reorgs
+        # (and is not the same as the current tip height)
+        tip_height = self.nodes[0].getblockchaininfo()["blocks"]
+        assert spend_101_height != tip_height
+        assert spend_102_1_height != tip_height
+        assert_equal(
+            spend_101_height, self.nodes[0].getmempoolentry(spend_101_id)["height"])
+        assert_equal(
+            spend_102_1_height, self.nodes[0].getmempoolentry(spend_102_1_id)["height"])
+        # The new resurrected tx should just have height equal to current tip height
+        assert_equal(
+            tip_height, self.nodes[0].getmempoolentry(spend_103_1_id)["height"])
 
         # Use invalidateblock to re-org back and make all those coinbase spends
         # immature/invalid:

Bitcoin-ABC/bitcoin-abc

Summary:
```
Port of [[https://gitlab.com/bitcoin-cash-node/bitcoin-cash-node/merge_requests/1162 | bchn#1162]]. Depends on D13133.
Test Plan:
  ninja all check-all
Reviewers: #bitcoin_abc, sdulfari
Reviewed By: #bitcoin_abc, sdulfari
Differential Revision: https://reviews.bitcoinabc.org/D13141

test/functional/mempool_reorg.py
@@ -115,6 +115,12 @@ class MempoolCoinbaseTest(BitcoinTestFramework):
                      spend_1_id, spend_2_1_id, timelock_tx_id})
         self.sync_all()
 
+        # save acceptance heights of 2 of the txs to later test that they are
+        # preserved across reorgs
+        spend_1_height = self.nodes[0].getmempoolentry(spend_1_id)["height"]
+        spend_2_1_height = self.nodes[0].getmempoolentry(spend_2_1_id)[
+            "height"]
+
         self.log.info("invalidate the last block")
         for node in self.nodes:
             node.invalidateblock(last_block[0])
@@ -124,6 +130,20 @@ class MempoolCoinbaseTest(BitcoinTestFramework):
             "spend_3_1 has been re-orged out of the chain and is back in the mempool")
         assert_equal(set(self.nodes[0].getrawmempool()), {
                      spend_1_id, spend_2_1_id, spend_3_1_id})
+
+        # now ensure that the acceptance height of the two txs was preserved
+        # across reorgs (and is not the same as the current tip height)
+        tip_height = self.nodes[0].getblockchaininfo()["blocks"]
+        assert spend_1_height != tip_height
+        assert spend_2_1_height != tip_height
+        assert_equal(
+            spend_1_height, self.nodes[0].getmempoolentry(spend_1_id)["height"])
+        assert_equal(
+            spend_2_1_height, self.nodes[0].getmempoolentry(spend_2_1_id)["height"])
+        # The new resurrected tx should just have height equal to current tip
+        # height
+        assert_equal(
+            tip_height, self.nodes[0].getmempoolentry(spend_3_1_id)["height"])
 
         self.log.info(
             "Use invalidateblock to re-org back and make all those coinbase spends immature/invalid")