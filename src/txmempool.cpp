RadiantBlockchain/radiant-node

mempool: Slight performance nit when erasing from setEntries
Summary
---

When re-reading all of the mempool code in master I noticed that in 2
places, a working `setEntries` (which is an alias for a
`std::set<CTxMemPool::txiter>`) was being erased-from inefficiently.

In these two places we are grabbing the first `txiter` from the set then
we are erasing it.  In the extant code, the `txiter` is being passed as
a *key* to the set. Instead, we should be erasing using the slightly
more efficient `std::set(std::set::iterator)` overload, which does not
need to do any O(log N) lookups.

This MR is basically just a tiny performance nit.

Test Plan
---

- `ninja all check-all`

Also perhaps try this here versus master (maybe you will notice a perf.
diff against master although given the way these tx's are set up, you
may not):

- `ninja bench_bitcoin && src/bench/bench_bitcoin -filter='Evict.*'`

src/txmempool.cpp
@@ -112,7 +112,7 @@ bool CTxMemPool::CalculateMemPoolAncestors(
         txiter stageit = *parentHashes.begin();
 
         setAncestors.insert(stageit);
-        parentHashes.erase(stageit);
+        parentHashes.erase(parentHashes.begin());
         totalSizeWithAncestors += stageit->GetTxSize();
 
         if (stageit->GetSizeWithDescendants() + entry.GetTxSize() >
@@ -361,7 +361,7 @@ void CTxMemPool::CalculateDescendants(txiter entryit,
     while (!stage.empty()) {
         txiter it = *stage.begin();
         setDescendants.insert(it);
-        stage.erase(it);
+        stage.erase(stage.begin());
 
         const setEntries &setChildren = GetMemPoolChildren(it);
         for (txiter childiter : setChildren) {

Bitcoin-ABC/bitcoin-abc

Summary:
```
When re-reading all of the mempool code in master I noticed that in 2 places, a working `setEntries` (which is an alias for a
In these two places we are grabbing the first `txiter` from the set then we are erasing it.  In the extant code, the `txiter` is being passed as a *key* to the set. Instead, we should be erasing using the slightly more efficient `std::set(std::set::iterator)` overload, which does not need to do any O(log N) lookups.
Port of [[https://gitlab.com/bitcoin-cash-node/bitcoin-cash-node/merge_requests/1175 | bchn#1175]].
Depends on D13171.
There is no change in behavior.
Test Plan:
  ninja all check-all
Reviewers: #bitcoin_abc, PiRK
Reviewed By: #bitcoin_abc, PiRK
Differential Revision: https://reviews.bitcoinabc.org/D13176

src/txmempool.cpp
@@ -145,7 +145,7 @@ bool CTxMemPool::CalculateAncestorsAndCheckLimits(
         txiter stageit = mapTx.iterator_to(stage);
 
         setAncestors.insert(stageit);
-        staged_ancestors.erase(stage);
+        staged_ancestors.erase(staged_ancestors.begin());
         totalSizeWithAncestors += stageit->GetTxSize();
 
         if (stageit->GetSizeWithDescendants() + entry_size >
@@ -524,7 +524,7 @@ void CTxMemPool::CalculateDescendants(txiter entryit,
     while (!stage.empty()) {
         txiter it = *stage.begin();
         setDescendants.insert(it);
-        stage.erase(it);
+        stage.erase(stage.begin());
 
         const CTxMemPoolEntry::Children &children =
             it->GetMemPoolChildrenConst();