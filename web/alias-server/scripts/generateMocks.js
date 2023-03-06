14689ad614aa4e3063fe256d6f619ba65feee027
bytesofman <joey@bitcoinabc.org>
2023-03-06 14:25:19 -0800

    [alias-server] Improve code organization, generate mocks from script
    Summary:
    T2960
    
    Was going to patch some other bugs but needed to organize the code first and clean up `index.js`. Move util functions out of `index.js` and into new script in new scripts folder. Call with new `npm` script.
    
    Test Plan:
    `npm run mocks` and observe mocks generated in `test/mocks/generated` folder
    clear database and `node index.js`, add a new alias using cashtab, confirm it's added to the db
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D13246

---
+++ web/alias-server/scripts/generateMocks.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../config');
+const {
+    getAllAliasTxs,
+    sortAliasTxsByTxidAndBlockheight,
+    getValidAliasRegistrations,
+} = require('../alias');
+const { getAllTxHistory } = require('../chronik');
+const { generateReservedAliasTxArray } = require('../utils');
+
+async function generateMocks() {
+    // Directory for mocks. Relative to /scripts, ../test/mocks/generated/
+    const mocksDir = path.join(__dirname, '..', 'test', 'mocks', 'generated');
+
+    // Create directory if it does not exist
+    if (!fs.existsSync(mocksDir)) {
+        fs.mkdirSync(mocksDir);
+    }
+    // chronik tx history of alias registration address
+    const aliasTxHistory = await getAllTxHistory(
+        config.aliasConstants.registrationHash160,
+    );
+    fs.writeFileSync(
+        `${mocksDir}/aliasTxHistory.json`,
+        JSON.stringify(aliasTxHistory, null, 2),
+        'utf-8',
+    );
+
+    // All valid alias txs at alias registration address
+    // NB unconfirmed txs have blockheight === 100,000,000
+    const allAliasTxs = getAllAliasTxs(aliasTxHistory, config.aliasConstants);
+    fs.writeFileSync(
+        `${mocksDir}/allAliasTxs.json`,
+        JSON.stringify(allAliasTxs, null, 2),
+        'utf-8',
+    );
+
+    /* 
+    All valid alias txs at alias registration address, 
+    sorted by blockheight earliest to most recent, and txid
+    alphabetically.
+
+    NB unconfirmed txs have blockheight === 100,000,000
+    */
+    const allAliasTxsSortedByTxidAndBlockheight =
+        sortAliasTxsByTxidAndBlockheight(allAliasTxs);
+    fs.writeFileSync(
+        `${mocksDir}/allAliasTxsSortedByTxidAndBlockheight.json`,
+        JSON.stringify(allAliasTxsSortedByTxidAndBlockheight, null, 2),
+        'utf-8',
+    );
+
+    /*
+    validAliasObjects, pendingAliasObjects
+    
+    validAliasObjects are registered aliases. 
+    These will never change unless and until Phase 2 migration.
+    
+    pendingAliasObjects are alias registrations that will be valid after the tx confirmed,
+    assuming no other tx with an alphabetically earlier txid comes into the same block
+    */
+    const { validAliasTxs, pendingAliasTxs } =
+        getValidAliasRegistrations(allAliasTxs);
+    fs.writeFileSync(
+        `${mocksDir}/validAliasTxs.json`,
+        JSON.stringify(validAliasTxs, null, 2),
+        'utf-8',
+    );
+    fs.writeFileSync(
+        `${mocksDir}/pendingAliasTxs.json`,
+        JSON.stringify(pendingAliasTxs, null, 2),
+        'utf-8',
+    );
+
+    const reservedAliasTxArray = generateReservedAliasTxArray();
+    fs.writeFileSync(
+        `${mocksDir}/reservedAliasTxArray.json`,
+        JSON.stringify(reservedAliasTxArray, null, 2),
+        'utf-8',
+    );
+}
+
+generateMocks();
---
+++ web/alias-server/websocket.js
@@ -0,0 +1,170 @@
+const config = require('./config');
+const log = require('./log');
+const { getAllAliasTxs, getValidAliasRegistrations } = require('./alias');
+const { getAllTxHistory } = require('./chronik');
+const { getValidAliasTxsToBeAddedToDb } = require('./utils');
+const { returnTelegramBotSendMessagePromise } = require('./telegram');
+const { chronik } = require('./chronik');
+
+module.exports = {
+    initializeWebsocket: async function (db) {
+        // Subscribe to chronik websocket
+        const ws = chronik.ws({
+            onMessage: async msg => {
+                await module.exports.parseWebsocketMessage(db, msg);
+            },
+            onReconnect: e => {
+                // Fired before a reconnect attempt is made:
+                log('Websocket disconnected. Reconnecting...');
+            },
+        });
+        // Wait for WS to be connected:
+        await ws.waitForOpen();
+        log(`Connected to websocket`);
+        // Subscribe to scripts (on Lotus, current ABC payout address):
+        // Will give a message on avg every 2 minutes
+        ws.subscribe('p2pkh', config.aliasConstants.registrationHash160);
+        return ws;
+    },
+    parseWebsocketMessage: async function (
+        db,
+        wsMsg = { type: 'BlockConnected' },
+    ) {
+        log(`parseWebsocketMessage called on`, wsMsg);
+        // Determine type of tx
+        const { type } = wsMsg;
+        log(`msg type: ${type}`);
+        // type can be AddedToMempool, BlockConnected, or Confirmed
+        // For now, we are only interested in "Confirmed", as only these are valid
+        // We will want to look at AddedToMempool to process pending alias registrations later
+        switch (type) {
+            case 'BlockConnected':
+                typeof wsMsg.blockHash !== 'undefined'
+                    ? log(`New block found: ${wsMsg.blockHash}`)
+                    : log(`Checking for new aliases on startup`);
+                const aliasTxHistory = await getAllTxHistory(
+                    config.aliasConstants.registrationHash160,
+                );
+                const allAliasTxs = getAllAliasTxs(
+                    aliasTxHistory,
+                    config.aliasConstants,
+                );
+                const { validAliasTxs, pendingAliasTxs } =
+                    getValidAliasRegistrations(allAliasTxs);
+                log(`${validAliasTxs.length} valid alias registrations`);
+                log(`${pendingAliasTxs.length} pending alias registrations`);
+
+                // Get the valid aliases already in the db
+                let validAliasesInDb;
+                try {
+                    validAliasesInDb = await db
+                        .collection(config.database.collections.validAliases)
+                        .find()
+                        .sort({ blockheight: 1 })
+                        .project({ _id: 0 })
+                        .toArray();
+                    log(`${validAliasesInDb.length} valid aliases in database`);
+                } catch (error) {
+                    log(`Error in determining validAliasesInDb`, error);
+                }
+
+                const validAliasTxsToBeAddedToDb =
+                    getValidAliasTxsToBeAddedToDb(
+                        validAliasesInDb,
+                        validAliasTxs,
+                    );
+                log(`validAliasTxsToBeAddedToDb`, validAliasTxsToBeAddedToDb);
+
+                if (validAliasTxsToBeAddedToDb.length > 0) {
+                    // Update with real data
+                    try {
+                        const validAliasTxsCollectionInsertResult = await db
+                            .collection(
+                                config.database.collections.validAliases,
+                            )
+                            .insertMany(validAliasTxsToBeAddedToDb);
+                        log(
+                            `Inserted ${validAliasTxsCollectionInsertResult.insertedCount} aliases into ${config.database.collections.validAliases}`,
+                        );
+                    } catch (err) {
+                        log(
+                            `A MongoBulkWriteException occurred adding validAliasTxs to the db, but there are successfully processed documents.`,
+                        );
+                        /*
+                        let ids = err.result.result.insertedIds;
+                        for (let id of Object.values(ids)) {
+                            log(`Processed a document with id ${id._id}`);
+                        }
+                        */
+                        log(
+                            `Number of documents inserted: ${err.result.result.nInserted}`,
+                        );
+                        log(`Error:`, err);
+                    }
+
+                    // Send msgs to Telegram channel about newly registered aliases
+                    const tgBotMsgPromises = [];
+                    for (
+                        let i = 0;
+                        i < validAliasTxsToBeAddedToDb.length;
+                        i += 1
+                    ) {
+                        // Get interesting info for a telegram message
+                        const { alias, address, txid } =
+                            validAliasTxsToBeAddedToDb[i];
+                        const aliasLength = alias.length;
+                        const aliasPriceSats =
+                            config.aliasConstants.registrationFeesSats[
+                                aliasLength
+                            ];
+                        // Construct your Telegram message in markdown
+                        const tgMsg =
+                            `A new ${aliasLength}-byte alias has been registered for ${(
+                                aliasPriceSats / 100
+                            ).toLocaleString()} XEC!\n` +
+                            `\n` +
+                            `"${alias}"\n` +
+                            `\n` +
+                            `[address](${config.blockExplorer}/address/${address}) | [tx](${config.blockExplorer}/tx/${txid})`;
+                        // Configure msg parse settings
+                        let tgMsgOptions = {
+                            parse_mode: 'markdown',
+                            disable_web_page_preview: true,
+                        };
+                        const tgBotMsgPromise =
+                            returnTelegramBotSendMessagePromise(
+                                tgMsg,
+                                tgMsgOptions,
+                            );
+                        tgBotMsgPromises.push(tgBotMsgPromise);
+                    }
+                    /* 
+                    Send msgs in a batch to handle nodejs async threads
+                    Note: you will still run into rate limit issues if 
+                    you are trying to send more than 25 msgs at once
+                    */
+                    let tgMsgBatchSuccess;
+                    try {
+                        tgMsgBatchSuccess = await Promise.all(tgBotMsgPromises);
+                        log(
+                            `Successfully sent ${tgBotMsgPromises.length} messages to channel`,
+                        );
+                    } catch (err) {
+                        log(
+                            `Error sending Telegram Bot message for aliases`,
+                            err,
+                        );
+                    }
+                }
+                break;
+            case 'AddedToMempool':
+                log(`New tx: ${wsMsg.txid}`);
+                break;
+            case 'Confirmed':
+                log(`New confirmed tx: ${wsMsg.txid}`);
+                break;
+            default:
+                log(`New websocket message of unknown type:`, wsMsg);
+        }
+    },
+};
