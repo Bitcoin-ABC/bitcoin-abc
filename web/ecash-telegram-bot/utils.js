6fdc3aeb369e4992f7e45847f0eea706b437386a
bytesofman <joey@bitcoinabc.org>
2023-03-16 15:21:39 -0700

    [tg bot] port outputScriptToAddress function
    Summary:
    T2972
    
    Port `outputScriptToAddress` function from alias-server to enable parsing txs for send and receive addresses. No unit tests are added as the existing bot output parsing unit tests will cover this sufficiently once tg msgs include this data (and bc unit tests exist in alias-server, so it wouldn't make sense to just duplicate them).
    
    If I start needing to do this with other util functions, would eventually make sense to put them all together in a tree-shakeable library. Not yet though.
    
    Test Plan: Review function and confirm it's copy pasted from alias-server
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D13362

--- web/ecash-telegram-bot/utils.js
+++ web/ecash-telegram-bot/utils.js
@@ -1,3 +1,5 @@
+const cashaddr = require('ecashaddrjs');
+
 module.exports = {
     returnLabeledChronikBlockPromise: async function (
         chronik,
@@ -18,5 +20,56 @@ module.exports = {
                 },
             );
         });
+    },
+    // This function is lifted directly from alias-server
+    outputScriptToAddress: function (outputScript) {
+        // returns P2SH or P2PKH address
+        // P2PKH addresses are in outputScript of type 76a914...88ac
+        // P2SH addresses are in outputScript of type a914...87
+        // Return false if cannot determine P2PKH or P2SH address
+
+        const typeTestSlice = outputScript.slice(0, 4);
+        let addressType;
+        let hash160;
+        switch (typeTestSlice) {
+            case '76a9':
+                addressType = 'P2PKH';
+                hash160 = outputScript.substring(
+                    outputScript.indexOf('76a914') + '76a914'.length,
+                    outputScript.lastIndexOf('88ac'),
+                );
+                break;
+            case 'a914':
+                addressType = 'P2SH';
+                hash160 = outputScript.substring(
+                    outputScript.indexOf('a914') + 'a914'.length,
+                    outputScript.lastIndexOf('87'),
+                );
+                break;
+            default:
+                return false;
+        }
+        // Test hash160 for correct length
+        if (hash160.length !== 40) {
+            return false;
+        }
+
+        const buffer = Buffer.from(hash160, 'hex');
+
+        // Because ecashaddrjs only accepts Uint8Array as input type, convert
+        const hash160ArrayBuffer = new ArrayBuffer(buffer.length);
+        const hash160Uint8Array = new Uint8Array(hash160ArrayBuffer);
+        for (let i = 0; i < hash160Uint8Array.length; i += 1) {
+            hash160Uint8Array[i] = buffer[i];
+        }
+
+        // Encode ecash: address
+        const ecashAddress = cashaddr.encode(
+            'ecash',
+            addressType,
+            hash160Uint8Array,
+        );
+
+        return ecashAddress;
     },
 };
