Bitcoin-ABC/bitcoin-abc

commit 016abbe42e83b037ab325bdc2ccca149e13017cb

[net] Ignore unlikely timestamps in version messages
Summary:
This avoids undefined behavior and prevents timejacking attacks
that use negative timestamps.  The genesis block time was chosen since
it's an easy timestamp to pick as a lower bound that we safely assume
all nodes will be greater than.  Any timestamp lower than that is not
expected.

This should be safe to deploy without a flag day activation since
substantial differences in nTime between nodes should only occur if
mocktime was explicitly set. This is not expected (especially on
mainnet) for any currently in-consensus BCH nodes.

This should be safe for functional tests as well, since we typically
only mock time from the genesis time to well in the future (regtest's
genesis time is also greater than mainnet genesis block time).

Depends on D6023 so that tests will pass using timestamp values +/- 2^31
Depends on D6022 so that we can assume GetTime() will not return a negative
number.

Test Plan:
```
ninja check check-functional
```

Reviewers: #bitcoin_abc, Fabien, deadalnix

Reviewed By: #bitcoin_abc, Fabien, deadalnix

Subscribers: deadalnix, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D6024

src/net_processing.cpp
@@ -2287,9 +2287,17 @@ static bool ProcessMessage(const Config &config, CNode *pfrom,
                  pfrom->nStartingHeight, addrMe.ToString(), pfrom->GetId(),
                  remoteAddr);
 
-        int64_t nTimeOffset = nTime - GetTime();
-        pfrom->nTimeOffset = nTimeOffset;
-        AddTimeData(pfrom->addr, nTimeOffset);
+        // Ignore time offsets that are improbable (before the Genesis block)
+        // and may underflow the nTimeOffset calculation.
+        int64_t currentTime = GetTime();
+        if (nTime >= int64_t(chainparams.GenesisBlock().nTime)) {
+            int64_t nTimeOffset = nTime - currentTime;
+            pfrom->nTimeOffset = nTimeOffset;
+            AddTimeData(pfrom->addr, nTimeOffset);
+        } else {
+            Misbehaving(pfrom, 20,
+                        "Ignoring invalid timestamp in version message");
+        }
 
         // Feeler connections exist only to verify if address is online.
         if (pfrom->fFeeler) {

test/functional/abc-version-message.py
@@ -1,0 +1,59 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test version message behavior"""
+
+from test_framework.messages import NODE_NETWORK, msg_version
+from test_framework.mininode import P2PInterface
+from test_framework.test_framework import BitcoinTestFramework
+
+
+class ModifiedVersionTimestampP2PInterface(P2PInterface):
+    def __init__(self, timestamp):
+        super().__init__()
+        self.versionTimestamp = timestamp
+
+    def peer_connect(self, *args, services=NODE_NETWORK,
+                     send_version=False, **kwargs):
+        create_conn = super().peer_connect(*args, send_version=send_version, **kwargs)
+
+        # Send version message with invalid timestamp
+        vt = msg_version()
+        vt.nTime = self.versionTimestamp
+
+        vt.nServices = services
+        vt.addrTo.ip = self.dstaddr
+        vt.addrTo.port = self.dstport
+        vt.addrFrom.ip = "0.0.0.0"
+        vt.addrFrom.port = 0
+
+        # Will be sent right after connection_made
+        self.on_connection_send_msg = vt
+        return create_conn
+
+
+class VersionMessageTest(BitcoinTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.setup_clean_chain = False
+
+    def run_test(self):
+        # Check some invalid timestamps in the version message
+        with self.nodes[0].assert_debug_log(expected_msgs=["Ignoring invalid timestamp in version message"]):
+            self.nodes[0].add_p2p_connection(
+                ModifiedVersionTimestampP2PInterface(-9223372036854775807), send_version=False)
+
+        # Check timestamp boundary
+        with self.nodes[0].assert_debug_log(expected_msgs=["Ignoring invalid timestamp in version message"]):
+            self.nodes[0].add_p2p_connection(
+                ModifiedVersionTimestampP2PInterface(1296688601), send_version=False)
+
+        # Genesis block timestamp
+        with self.nodes[0].assert_debug_log(expected_msgs=["added time data"], unexpected_msgs=["Ignoring invalid timestamp in version message"]):
+            self.nodes[0].add_p2p_connection(
+                ModifiedVersionTimestampP2PInterface(1296688602), send_version=False)
+
+
+if __name__ == '__main__':
+    VersionMessageTest().main()

commit 0eda354e8679176059e2bcfc1456168f7c51ec02

p2p: Don't use timestamps from inbound peers
Summary:
> This makes it harder for others to tamper with our adjusted time.

Rationale from the PR description:
> With the extra feeler connections (every 2 minutes on average) and extra block-relay-only connections (every 5 minutes on average) there are also now plenty of opportunities to gather a meaningful number of timedata samples from outbound peers.
>
> There are some measures in place to prevent abuse: the -maxtimeadjustment parameter with a default of 70 minutes, warnings in cases of large deviations, only using the first 200 samples (explanation), but I think that only using samples from outbound connections in the first place would be an additional safety measure that would make sense.
>
> See also issue [[https://github.com/bitcoin/bitcoin/pull/4521 | core#4521]] for further context and links: There have been several discussions in the past about replacing or abolishing the existing timedata system.

The difference with the source material is caused by D6024. This changes previous behavior slightly: now the computed time offset is assigned to the node even if we are not going to use it to compute our adjusted time. This `pfrom.nTimeOffset` is only used for display purposes (RPC results, GUI).

This is a backport of [[https://github.com/bitcoin/bitcoin/pull/23631 | core#23631]]

Depends on D13357

Test Plan: `ninja all check-all`

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Subscribers: Fabien

Differential Revision: https://reviews.bitcoinabc.org/D13357

src/net_processing.cpp
@@ -3752,16 +3752,18 @@ void PeerManagerImpl::ProcessMessage(
                  peer->m_starting_height, addrMe.ToString(), fRelay,
                  pfrom.GetId(), remoteAddr);
 
-        // Ignore time offsets that are improbable (before the Genesis block)
-        // and may underflow the nTimeOffset calculation.
         int64_t currentTime = GetTime();
-        if (nTime >= int64_t(m_chainparams.GenesisBlock().nTime)) {
-            int64_t nTimeOffset = nTime - currentTime;
-            pfrom.nTimeOffset = nTimeOffset;
-            AddTimeData(pfrom.addr, nTimeOffset);
-        } else {
+        int64_t nTimeOffset = nTime - currentTime;
+        pfrom.nTimeOffset = nTimeOffset;
+        if (nTime < int64_t(m_chainparams.GenesisBlock().nTime)) {
+            // Ignore time offsets that are improbable (before the Genesis
+            // block) and may underflow our adjusted time.
             Misbehaving(pfrom, 20,
                         "Ignoring invalid timestamp in version message");
+        } else if (!pfrom.IsInboundConn()) {
+            // Don't use timedata samples from inbound peers to make it
+            // harder for others to tamper with our adjusted time.
+            AddTimeData(pfrom.addr, nTimeOffset);
         }
 
         // Feeler connections exist only to verify if address is online.

test/functional/abc_p2p_version_timestamp.py
@@ -5,14 +5,17 @@
 """Test time adjustment behavior when receiving a VERSION message.
 
 Messages with a timestamp too far in the past are ignored and discouraged.
-Messages with a timestamp more recent than the genesis block timestamp are used to
-adjust our local time.
+Messages from an outbound peer with a timestamp more recent than the genesis block
+timestamp are used to adjust our local time.
 """
 
 from test_framework.blocktools import TIME_GENESIS_BLOCK
 from test_framework.messages import NODE_NETWORK, msg_version
 from test_framework.p2p import P2P_SUBVERSION, P2P_VERSION, P2PInterface
 from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import assert_equal
+
+NODE0_TIME = TIME_GENESIS_BLOCK + 1337
 
 
 class ModifiedVersionTimestampP2PInterface(P2PInterface):
@@ -44,9 +47,17 @@ class ModifiedVersionTimestampP2PInterface(P2PInterface):
 
 class VersionTimestampTest(BitcoinTestFramework):
     def set_test_params(self):
-        self.num_nodes = 1
+        self.num_nodes = 2
+
+    def setup_network(self):
+        self.setup_nodes()
+        # Don't connect the nodes
 
     def run_test(self):
+        # Set a deterministic time so that we can check the time offset with other
+        # nodes is as expected
+        self.nodes[0].setmocktime(NODE0_TIME)
+
         self.log.info("Check some invalid timestamp in the version message")
         with self.nodes[0].assert_debug_log(
                 expected_msgs=["Added connection peer=0",
@@ -64,9 +75,22 @@ class VersionTimestampTest(BitcoinTestFramework):
                 send_version=False)
 
         self.log.info("Check valid side of the timestamp boundary (genesis timestamp)")
+        # Outbound connection: the timestamp is used for our adjusted time
+        self.nodes[1].setmocktime(TIME_GENESIS_BLOCK)
         with self.nodes[0].assert_debug_log(
                 expected_msgs=["Added connection peer=2", "added time data"],
                 unexpected_msgs=["Ignoring invalid timestamp in version message"]):
+            self.connect_nodes(0, 1)
+
+        # This check verifies that the mocktime was indeed used in the VERSION message
+        assert_equal(self.nodes[0].getpeerinfo()[2]["timeoffset"],
+                     TIME_GENESIS_BLOCK - NODE0_TIME)
+
+        # Inbound connection: the timestamp is ignored
+        with self.nodes[0].assert_debug_log(
+                expected_msgs=["Added connection peer=3"],
+                unexpected_msgs=["Ignoring invalid timestamp in version message",
+                                 "added time data"]):
             self.nodes[0].add_p2p_connection(
                 ModifiedVersionTimestampP2PInterface(TIME_GENESIS_BLOCK),
                 send_version=False)