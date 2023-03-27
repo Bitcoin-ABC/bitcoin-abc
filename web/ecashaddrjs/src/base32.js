e906f11653c5999584b1f925eb9a952b40b89a5b
bytesofman <joey@bitcoinabc.org>
2023-03-27 06:07:03 -0700

    [ecashaddrjs] Update licensing refs
    Summary:
    T3039
    
    Depends on D13476
    
    Update licensing and repo refs to ABC info
    
    Test Plan: Confirm changes are only to comments. Confirm info is correct.
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D13477

--- web/ecashaddrjs/src/base32.js
+++ web/ecashaddrjs/src/base32.js
@@ -1,6 +1,7 @@
 /**
  * @license
- * https://github.com/ealmansi/cashaddrjs
+ * https://reviews.bitcoinabc.org
+ * Copyright (c) 2023 Bitcoin ABC
  * Copyright (c) 2017-2020 Emilio Almansi
  * Distributed under the MIT software license, see the accompanying
  * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
--- web/ecashaddrjs/test/base32.js
+++ web/ecashaddrjs/test/base32.js
@@ -1,5 +1,7 @@
 /**
- * https://github.com/ealmansi/cashaddrjs
+ * @license
+ * https://reviews.bitcoinabc.org
+ * Copyright (c) 2023 Bitcoin ABC
  * Copyright (c) 2017-2020 Emilio Almansi
  * Distributed under the MIT software license, see the accompanying
  * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
