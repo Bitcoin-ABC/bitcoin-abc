3c38650799b1f54a3b86078f98042d8b88161aa3
PiRK <pierrekn@gmail.com>
2023-03-27 10:12:38 +0200

    remove glibc < 2.27 compat code
    Summary:
    2.27 introduced a new version of log2f and is now the oldest supported version of glibc after D13460
    
    _divmoddi4 is a feature from GCC7 and we already enforce 8.3.
    
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/22930 | core#22930]]
    
    Test Plan:
    `ninja all check-all`
    gitian builds
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D13472

--- src/compat/glibc_compat.cpp
+++ src/compat/glibc_compat.cpp
@@ -1,53 +1,7 @@
 // Copyright (c) 2009-2014 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
-#if defined(HAVE_CONFIG_H)
-#include <config/bitcoin-config.h>
-#endif
-
 #include <cstdarg>
-#include <cstdint>
-
-#if defined(__i386__) || defined(__arm__)
-
-extern "C" int64_t __udivmoddi4(uint64_t u, uint64_t v, uint64_t *rp);
-
-extern "C" int64_t __wrap___divmoddi4(int64_t u, int64_t v, int64_t *rp) {
-    int32_t c1 = 0, c2 = 0;
-    int64_t uu = u, vv = v;
-    int64_t w;
-    int64_t r;
-
-    if (uu < 0) {
-        c1 = ~c1, c2 = ~c2, uu = -uu;
-    }
-    if (vv < 0) {
-        c1 = ~c1, vv = -vv;
-    }
-
-    w = __udivmoddi4(uu, vv, (uint64_t *)&r);
-    if (c1) w = -w;
-    if (c2) r = -r;
-
-    *rp = r;
-    return w;
-}
-#endif
-
-extern "C" float log2f_old(float x);
-#ifdef __i386__
-__asm(".symver log2f_old,log2f@GLIBC_2.1");
-#elif defined(__amd64__)
-__asm(".symver log2f_old,log2f@GLIBC_2.2.5");
-#elif defined(__arm__)
-__asm(".symver log2f_old,log2f@GLIBC_2.4");
-#elif defined(__aarch64__)
-__asm(".symver log2f_old,log2f@GLIBC_2.17");
-#endif
-extern "C" float __wrap_log2f(float x) {
-    return log2f_old(x);
-}
 
 /*
  * Starting with GLIBC_2.28, the `fcntl()` function has a new variant
