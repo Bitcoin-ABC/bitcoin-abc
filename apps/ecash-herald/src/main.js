fe33935e527d068321a8e4e45c9acc7269da77ba
bytesofman <joey@bitcoinabc.org>
2023-04-12 14:39:44 -0700

    [herald] patch bug in file renaming
    Summary:
    T3104
    
    Missed re-rename in the last diff. See D13684
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D13692

--- apps/ecash-herald/src/main.js
+++ apps/ecash-herald/src/main.js
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 'use strict';
-const { initializeWebsocket } = require('./handleChronikWs');
+const { initializeWebsocket } = require('./chronikWsHandler');
 
 module.exports = {
     main: async function (
