5c7a3b948c0e9c6a83aa0f3b7b8d7c49db1e9b45
Tobias Ruck <ruck.tobias@gmail.com>
2023-04-26 15:34:17 +0200

    [Chronik] Add all push opcodes to opcode.rs
    Summary:
    Also add the aliases `OP_FALSE` and `OP_TRUE` as the node does it.
    
    The first definition of an opcode number defines the name of the opcode (hence the `allow(unreachable_patterns)`; the second opcode is always ignored).
    
    Test Plan: `ninja check-crates`
    
    Reviewers: Fabien, #bitcoin_abc
    
    Reviewed By: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D13784

--- chronik/bitcoinsuite-core/src/script/opcode.rs
+++ chronik/bitcoinsuite-core/src/script/opcode.rs
@@ -24,10 +24,11 @@ macro_rules! define_opcodes {
         /// ```
         /// # use bitcoinsuite_core::script::opcode::*;
         /// assert_eq!(opcode_number_to_name(0), Some("OP_0"));
-        /// assert_eq!(opcode_number_to_name(0x51), Some("OP_TRUE"));
+        /// assert_eq!(opcode_number_to_name(0x51), Some("OP_1"));
         /// assert_eq!(opcode_number_to_name(0xff), None);
         /// ```
         pub fn opcode_number_to_name(opcode: u8) -> Option<&'static str> {
+            #[allow(unreachable_patterns)]
             match opcode {
                 $(
                     $number => Some(stringify!($opcode)),
@@ -68,8 +69,52 @@ impl Opcode {
 define_opcodes! {
     /// Push the number 0 (=empty string) onto the stack.
     OP_0 = 0x00,
+    /// Push the number 0 (=empty string) onto the stack.
+    OP_FALSE = 0x00,
+    /// Read 1 byte, read that many bytes and push them on the stack
+    OP_PUSHDATA1 = 0x4c,
+    /// Read 2 bytes as little-endian, read that many bytes and push them
+    OP_PUSHDATA2 = 0x4d,
+    /// Read 4 bytes as little-endian, read that many bytes and push them
+    OP_PUSHDATA4 = 0x4e,
+    /// Push the number -1 (`0x81`) onto the stack
+    OP_1NEGATE = 0x4f,
+    /// Reserved pushop, can be used in OP_RETURN
+    OP_RESERVED = 0x50,
+    /// Push the number 1 onto the stack.
+    OP_1 = 0x51,
     /// Push the number 1 onto the stack.
     OP_TRUE = 0x51,
+    /// Push the number 2 onto the stack.
+    OP_2 = 0x52,
+    /// Push the number 3 onto the stack.
+    OP_3 = 0x53,
+    /// Push the number 4 onto the stack.
+    OP_4 = 0x54,
+    /// Push the number 5 onto the stack.
+    OP_5 = 0x55,
+    /// Push the number 6 onto the stack.
+    OP_6 = 0x56,
+    /// Push the number 7 onto the stack.
+    OP_7 = 0x57,
+    /// Push the number 8 onto the stack.
+    OP_8 = 0x58,
+    /// Push the number 9 onto the stack.
+    OP_9 = 0x59,
+    /// Push the number 10 onto the stack.
+    OP_10 = 0x5a,
+    /// Push the number 11 onto the stack.
+    OP_11 = 0x5b,
+    /// Push the number 12 onto the stack.
+    OP_12 = 0x5c,
+    /// Push the number 13 onto the stack.
+    OP_13 = 0x5d,
+    /// Push the number 14 onto the stack.
+    OP_14 = 0x5e,
+    /// Push the number 15 onto the stack.
+    OP_15 = 0x5f,
+    /// Push the number 16 onto the stack.
+    OP_16 = 0x60,
     /// Mark transaction as invalid. Used to add data to a tx in an output.
     OP_RETURN = 0x6a,
     /// Duplicate the top stack item.
@@ -88,17 +133,19 @@ define_opcodes! {
 
 #[cfg(test)]
 mod tests {
-    use crate::script::opcode::{OP_0, OP_TRUE};
+    use crate::script::opcode::*;
 
     #[test]
     fn test_opcode_from_u8() {
         assert_eq!(u8::from(OP_0), 0);
+        assert_eq!(u8::from(OP_1), 0x51);
         assert_eq!(u8::from(OP_TRUE), 0x51);
     }
 
     #[test]
     fn test_display_opcode() {
         assert_eq!(OP_0.to_string(), "OP_0");
-        assert_eq!(OP_TRUE.to_string(), "OP_TRUE");
+        assert_eq!(OP_1.to_string(), "OP_1");
+        assert_eq!(OP_TRUE.to_string(), "OP_1");
     }
 }
