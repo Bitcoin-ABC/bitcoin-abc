3b3fb90b2426c4310ddce509f73094853cb4fcd8
PiRK <pierrekn@gmail.com>
2023-05-29 10:02:15 +0200

    build: don't compress macOS DMG
    Summary:
    Skip compressing the macOS DMG, and drop related build steps and dependencies. Uncompressed the DMG increases from ~16mb to ~30mb, which compared to other software a user may download, (Firefox 125mb, VLC 52mb, Open Office 176mb), is still relatively small.Note that if / when we build with LTO enabled for releases, this size will shrink back down significantly again.
    
    native_libdmg-hfsplus is not maintained, and I doubt the DMG creation feature will ever be fixed. If at some point xorrisofs supports compressing dmgs, we could enable that.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/24031 | core#24031]]
    
    This is one step needed to make the MacOS build work with the current guix script.
    
    Test Plan:
    gitian-osx
    
    `HOSTS="x86_64-apple-darwin" contrib/guix/guix-build`
    This still fails with an exotic clang related error     `ld: unrecognised emulation mode: acosx_version_min`, but it no longer fails with a `cannot find lz` error.
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D13950

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -21,7 +21,7 @@ natpmp_packages=libnatpmp
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
+darwin_native_packages += native_cctools native_libtapi
 
 ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
 darwin_native_packages+= native_clang
