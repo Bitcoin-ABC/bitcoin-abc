17997244f09f463e2fd35145f464fc2aed7abcc7
bytesofman <joey@bitcoinabc.org>
2023-06-01 05:53:08 -0700

    [alias-server] push parsing for op return
    Summary:
    T3060
    
    Use the consume function to build a feature complete push-parsing function for OP_RETURN.
    
    OOM vector of large pushes is not directly handled. An error will be thrown though if pushdata exceeds the length of the provided string.
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D13964

--- apps/alias-server/src/script.js
+++ apps/alias-server/src/script.js
@@ -2,6 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 'use strict';
+const opReturn = require('../constants/op_return');
+
 module.exports = {
     /**
      * consume
@@ -59,5 +61,76 @@ module.exports = {
         // Remove consumed from the stack
         stack.remainingHex = stack.remainingHex.slice(byteSliceSize);
         return consumed;
+    },
+    /**
+     * Parse, decode and consume the data push from the top of the stack.
+     * If the stack does not start with a valid push, it raises an error and the stack is left untouched.
+     * @param {object} stack an object containing a hex string outputScript of an eCash tx, e.g. {remainingHex: '4d...'}
+     * An object is used for 'stack' to allow in-place modification without returning the input
+     * @returns {string} the first push on input stack, as a hex string
+     * stack is modified in place so that the push is removed
+     */
+    consumeNextPush: function (stack) {
+        // Clone stack in case you have an error and wish to leave it unmodified
+        const clonedStack = JSON.parse(JSON.stringify(stack));
+
+        // Get the first byte on the stack
+        let firstByte = module.exports.consume(clonedStack, 1);
+
+        if (opReturn.oneByteStackAdds.includes(firstByte)) {
+            // If this is a one-byte push, consume stack and return the byte
+            stack.remainingHex = clonedStack.remainingHex;
+            return firstByte;
+        }
+
+        // Initialize pushdata, the byteCount in hex of the push
+        let pushdata;
+
+        // Apply conditional checks to determine the size of this push
+        if (opReturn.oneBytePushdatas.includes(firstByte)) {
+            // If the first byte on the stack is 0x01-0x4b, then this is pushdata
+            pushdata = parseInt(firstByte, 16);
+        } else if (firstByte === opReturn.OP_PUSHDATA1) {
+            // The next byte contains the number of bytes to be pushed onto the stack.
+            pushdata = parseInt(module.exports.consume(clonedStack, 1), 16);
+        } else if (firstByte === opReturn.OP_PUSHDATA2) {
+            // The next two bytes contain the number of bytes to be pushed onto the stack in little endian order.
+
+            // pushdata is the the next 2 bytes, as little-endian
+            // i.e. 04 would be 0x04 0x00 i.e. '0400'
+            pushdata = module.exports.consume(clonedStack, 2);
+
+            // Convert to big-endian so JS parseInt can get the value
+            pushdata = pushdata.slice(2, 4) + pushdata.slice(0, 2);
+
+            // Convert to int
+            pushdata = parseInt(pushdata, 16);
+        } else if (firstByte === opReturn.OP_PUSHDATA4) {
+            // The next four bytes contain the number of bytes to be pushed onto the stack in little endian order.
+
+            // pushdata is the first byte of the next 4 bytes, as little-endian
+            // i.e. 04 would be 0x04 0x00 0x00 0x00 i.e. '04000000'
+            pushdata = module.exports.consume(clonedStack, 4);
+
+            // Convert to big-endian so JS parseInt can get the value
+            pushdata =
+                pushdata.slice(6, 8) +
+                pushdata.slice(4, 6) +
+                pushdata.slice(2, 4) +
+                pushdata.slice(0, 2);
+
+            // Convert to int
+            pushdata = parseInt(pushdata, 16);
+        } else {
+            throw new Error(`${firstByte} is invalid pushdata`);
+        }
+
+        // Now that you know pushdata in bytes, get the push
+        let push = module.exports.consume(clonedStack, pushdata);
+
+        // If no error, consume stack
+        stack.remainingHex = clonedStack.remainingHex;
+
+        return push;
     },
 };
--- apps/alias-server/test/scriptTests.js
+++ apps/alias-server/test/scriptTests.js
@@ -4,7 +4,8 @@
 
 'use strict';
 const assert = require('assert');
-const { consume } = require('../src/script');
+const { consume, consumeNextPush } = require('../src/script');
+const opReturn = require('../constants/op_return');
 
 describe('alias-server script.js', function () {
     it('consume gets the first byte from the stack of a hex string outputScript and removes the same from the input', function () {
@@ -132,5 +133,177 @@ describe('alias-server script.js', function () {
             testStack,
             '6a042e78656300154361706974616c4c6574746572735f416e645f2b21150076458db0ed96fe9863fc1ccec9fa2cfab884b0f6',
         );
+    });
+    it('consumeNextPush parses a valid alias registration OP_RETURN stack for all valid pushdata types', function () {
+        const expectedPushes = [
+            '2e786563', // .xec
+            '00', // version 0
+            '4361706974616c4c6574746572735f416e645f2b21', // alias in hex
+            '0076458db0ed96fe9863fc1ccec9fa2cfab884b0f6', // <addressType><addressHash>
+        ];
+        const pushdatas = {
+            withOpZero: ['04', '', '15', '15'],
+            oneByte: ['04', '01', '15', '15'],
+            OP_PUSHDATA1: [
+                `${opReturn.OP_PUSHDATA1}04`,
+                `${opReturn.OP_PUSHDATA1}01`,
+                `${opReturn.OP_PUSHDATA1}15`,
+                `${opReturn.OP_PUSHDATA1}15`,
+            ],
+            OP_PUSHDATA2: [
+                `${opReturn.OP_PUSHDATA2}0400`,
+                `${opReturn.OP_PUSHDATA2}0100`,
+                `${opReturn.OP_PUSHDATA2}1500`,
+                `${opReturn.OP_PUSHDATA2}1500`,
+            ],
+            OP_PUSHDATA4: [
+                `${opReturn.OP_PUSHDATA4}04000000`,
+                `${opReturn.OP_PUSHDATA4}01000000`,
+                `${opReturn.OP_PUSHDATA4}15000000`,
+                `${opReturn.OP_PUSHDATA4}15000000`,
+            ],
+        };
+
+        const pushdataTypes = Object.keys(pushdatas);
+
+        // Test alias registration tx on all pushdatas
+        for (let i = 0; i < pushdataTypes.length; i += 1) {
+            const thesePushdatas = pushdatas[pushdataTypes[i]];
+            const testStack = {
+                remainingHex: `${thesePushdatas[0]}${expectedPushes[0]}${thesePushdatas[1]}${expectedPushes[1]}${thesePushdatas[2]}${expectedPushes[2]}${thesePushdatas[3]}${expectedPushes[3]}`,
+            };
+            // First push, prefix
+            assert.strictEqual(consumeNextPush(testStack), expectedPushes[0]);
+            // Stack is modified so that push is removed
+            assert.deepEqual(testStack, {
+                remainingHex: `${thesePushdatas[1]}${expectedPushes[1]}${thesePushdatas[2]}${expectedPushes[2]}${thesePushdatas[3]}${expectedPushes[3]}`,
+            });
+
+            // Second push, alias registration tx version number
+            assert.strictEqual(consumeNextPush(testStack), expectedPushes[1]);
+            // Stack is modified so that push is removed
+            assert.deepEqual(testStack, {
+                remainingHex: `${thesePushdatas[2]}${expectedPushes[2]}${thesePushdatas[3]}${expectedPushes[3]}`,
+            });
+
+            // Third push, alias
+            assert.strictEqual(consumeNextPush(testStack), expectedPushes[2]);
+            // Stack is modified so that push is removed
+            assert.deepEqual(testStack, {
+                remainingHex: `${thesePushdatas[3]}${expectedPushes[3]}`,
+            });
+
+            // Fourth push, <addressType><addressHash>
+            assert.strictEqual(consumeNextPush(testStack), expectedPushes[3]);
+            // Stack is modified so that push is removed
+            assert.deepEqual(testStack, {
+                remainingHex: '',
+            });
+        }
+    });
+    it('consumeNextPush throws an error if the next push on the stack has invalid pushdata', function () {
+        const testStack = {
+            remainingHex:
+                'ff042e78656300154361706974616c4c6574746572735f416e645f2b21150076458db0ed96fe9863fc1ccec9fa2cfab884b0f6',
+        };
+
+        assert.throws(() => {
+            consumeNextPush(testStack);
+        }, Error('ff is invalid pushdata'));
+
+        // The stack is not modified
+        assert.deepEqual(testStack, {
+            remainingHex:
+                'ff042e78656300154361706974616c4c6574746572735f416e645f2b21150076458db0ed96fe9863fc1ccec9fa2cfab884b0f6',
+        });
+    });
+    it('consumeNextPush throws an error if first byte is OP_RETURN', function () {
+        const testStack = {
+            remainingHex:
+                '6a042e78656300154361706974616c4c6574746572735f416e645f2b21150076458db0ed96fe9863fc1ccec9fa2cfab884b0f6',
+        };
+
+        assert.throws(() => {
+            consumeNextPush(testStack);
+        }, Error('6a is invalid pushdata'));
+
+        // The stack is not modified
+        assert.deepEqual(testStack, {
+            remainingHex:
+                '6a042e78656300154361706974616c4c6574746572735f416e645f2b21150076458db0ed96fe9863fc1ccec9fa2cfab884b0f6',
+        });
+    });
+    it('consumeNextPush throws an error if pushdata exceeds length of remaining string', function () {
+        const testStack = {
+            remainingHex:
+                '4e040404042e78656300154361706974616c4c6574746572735f416e645f2b21150076458db0ed96fe9863fc1ccec9fa2cfab884b0f6',
+        };
+
+        assert.throws(() => {
+            consumeNextPush(testStack);
+        }, Error('consume called with byteCount (67372036) greater than remaining bytes in outputScript (49)'));
+
+        // The stack is not modified
+        assert.deepEqual(testStack, {
+            remainingHex:
+                '4e040404042e78656300154361706974616c4c6574746572735f416e645f2b21150076458db0ed96fe9863fc1ccec9fa2cfab884b0f6',
+        });
+    });
+    it('consumeNextPush returns all one byte pushes', function () {
+        for (let i = 0; i < opReturn.oneByteStackAdds.length; i += 1) {
+            const testHexString = opReturn.oneByteStackAdds[i];
+            const stack = { remainingHex: testHexString };
+
+            const firstPush = consumeNextPush(stack); // alias registration prefix
+
+            // consumeNextPush returns the push of testHexString
+            assert.strictEqual(firstPush, opReturn.oneByteStackAdds[i]);
+            // Verify stack has been modified in place
+            assert.deepEqual(stack, {
+                remainingHex: '',
+            });
+        }
+    });
+    it('consumeNextPush throws an error if pushdata bytes are incorrect', function () {
+        const testStack = {
+            remainingHex: '4d4274657374',
+        };
+
+        assert.throws(() => {
+            consumeNextPush(testStack);
+        }, Error('consume called with byteCount (29762) greater than remaining bytes in outputScript (3)'));
+
+        // The stack is not modified
+        assert.deepEqual(testStack, {
+            remainingHex: '4d4274657374',
+        });
+    });
+    it('consumeNextPush throws an error if given only pushdata', function () {
+        const testStack = {
+            remainingHex: '4d0400',
+        };
+
+        assert.throws(() => {
+            consumeNextPush(testStack);
+        }, Error('consume called with byteCount (4) greater than remaining bytes in outputScript (0)'));
+
+        // The stack is not modified
+        assert.deepEqual(testStack, {
+            remainingHex: '4d0400',
+        });
+    });
+    it('consumeNextPush throws an error if given only improperly formed pushdata', function () {
+        const testStack = {
+            remainingHex: '4d42',
+        };
+
+        assert.throws(() => {
+            consumeNextPush(testStack);
+        }, Error('consume called with byteCount (2) greater than remaining bytes in outputScript (1)'));
+
+        // The stack is not modified
+        assert.deepEqual(testStack, {
+            remainingHex: '4d42',
+        });
     });
 });
