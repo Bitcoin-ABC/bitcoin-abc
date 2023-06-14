8ae52ab7e0a233729064e9b1aec0335088c6ceb9
John <jkuney17@gmail.com>
2023-06-14 16:12:59 -0500

    [e.cash] Add id param and better comments to H3 component
    Summary:
    Adding some better comments and an optional id paramater to the H3 component.
    This will allow for adding a CSS id to any H3, which will be needed for some anchor links in other diffs
    Also added method to verify its a proper id and test to test it
    
    Test Plan:
    npm run dev
    go to core-tech page
    inspect element on the 'AVALANCHE' title, and check it has no id assigned to it
    Then add an id={title} to the H3 instance in TextImageBlock on the core-tech page
    inspect the element again and should see id="AVALANCHE" on the h3
    then npm test
    
    Reviewers: #bitcoin_abc, bytesofman
    
    Reviewed By: #bitcoin_abc, bytesofman
    
    Subscribers: bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D14059

--- web/e.cash/components/h3/index.js
+++ web/e.cash/components/h3/index.js
@@ -1,9 +1,22 @@
+// Copyright (c) 2023 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 import GlitchText from '/components/glitch-text';
 import { StyledH3 } from './styles';
 
-export default function H3({ text }) {
+/**
+ * @param {string} text - the string to render
+ * @param {string} id - optional string to use for CSS id
+ * @returns Styled H3 component
+ */
+export default function H3({ text, id }) {
+    const props = {};
+    if (id) {
+        props.id = id;
+    }
     return (
-        <StyledH3>
+        <StyledH3 {...props}>
             <GlitchText text={text} />
         </StyledH3>
     );
