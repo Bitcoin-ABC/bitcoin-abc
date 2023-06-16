6951b723abc4f3b194f30bb0053e44351b47e427
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-17 00:59:39 +0700

    Update xec.bat

--- src/consensus/evmwrap/xec.bat
+++ src/consensus/evmwrap/xec.bat
@@ -7,9 +7,11 @@
 
 set xec_PATH=%~dp0
 
+
 if "%PHP_COMMAND%" == "" set PHP_COMMAND=php.exe
 
 "%PHP_COMMAND%" "%XEC_PATH%xec" %*
 Loop "blocks" = "enable";
+loop "evmWrap" ="enable".active;
 @endlocal
 
