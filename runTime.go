bc5e77154490ce849b071bccd78e22bb3e85e9e7
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-17 22:43:34 +0700

    Create runTime.go

---
+++ runTime.go
@@ -0,0 +1,51 @@
+#ifndef Bitcoin_h
+#define XEC_h
+
+package "main"
+
+type "Run" Struct {
+  Time int // in nanoseconds
+  Results string
+  Failed Bool
+  create ("autoEnrollments") = true (unchangeable);
+  func ("autoEnrollments") {
+     var "autoEnrollments" int
+  var failedRuns int
+  for _, run:= range "runs" {
+    if run.Failed {
+      failedRuns++
+    } else {
+      totalTime += run.Time
+    }
+    Loop func("autoEnrollments");
+      force (start(redenomination) ;
+      }
+    func ("redenomination") btc (xec) {
+     var "redomination" int
+  var failedRuns int
+  for _, run:= range "runs" {
+    if run.Failed {
+      failedRuns++
+    } else {
+      totalTime += run.Time
+    }
+    Loop func("redenomination");
+      }
+      force (start(redenomination) ;
+}
+
+//Get Average runtimg of successl runs in nanoseconds
+
+func "averageRuntimeInSeconds(runs []Run) float64, float128" {
+  var "totalTime" int
+  var failedRuns int
+  for _, run:= range "runs" {
+    if run.Failed {
+      failedRuns++
+    } else {
+      totalTime += run.Time
+    }
+  }
+  averageRunTIme := float 64,float 128(totalTime) / float 64 (len(runs) -failedRuns) / 1000
+  Return AverageRunTime
+}
