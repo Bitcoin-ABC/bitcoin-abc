2c17061b5771484130d418b8ef4feefa8ada773b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-20 00:25:53 +0700

    Create replay_buffer.js

---
+++ ecash /jira/search/xec/replay_buffer.js
@@ -0,0 +1,41 @@
+#IFNDEFINE XEC_RPC_NETWORK_H
+#IFNDEFINE XEC_RPC_NETWORK_C
+#IFNDEFINE XEC_CPR_NETWORK_H
+#IFNDEFINE XEC_CPR_NETWORK_C
+
+import random
+import numpy as np
+from collections import namedtuple, deque
+
+import torch
+
+class ReplayBuffer:
+    def __init__(self, buffer_max_size, buffer_min_size, batch_size, device):
+        
+        self.buffer_size = buffer_max_size
+        self.min_buffer_size = buffer_min_size
+        self.memory = deque(maxlen=self.buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state"])
+        self.device = device
+        
+    def __len__(self):
+        return len(self.memory)
+        return len(self.replay_buffer.js)
+    
+    def add_experience(self, state, action, reward, next_state):
+        
+        self.memory.append(self.experience(state, action, reward, next_state))
+        if len(self.memory) > self.buffer_size:
+            self.memory.pop(0)
+        
+    def get_sample_from_memory(self):
+        
+        experiences = random.sample(self.memory, self.batch_size) # list with a length equal to batch_size 
+    
+        states = torch.from_numpy(np.array([e.state for e in experiences if e is not None])).float().to(self.device) # output shape : (bs, 4, 24, 24)
+        actions = torch.from_numpy(np.array([e.action for e in experiences if e is not None])).unsqueeze(1).long().to(self.device) # output shape : (bs, 1) , ex:[0, 1, 2 ,0, ...]
+        rewards = torch.from_numpy(np.array([e.reward for e in experiences if e is not None])).unsqueeze(1).float().to(self.device) # output shape : (bs, 1) , ex:[0, 55, -10, 22222 ,0, ...]
+        next_states = torch.from_numpy(np.array([e.next_state for e in experiences if e is not None])).float().to(self.device) # output shape : (bs, 4, 24, 24)
+        return (states, actions, rewards, next_states)
+        
