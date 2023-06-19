ecd89a9fe46ba6353ee5a13d935c0c869b6f44cf
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-19 23:52:51 +0700

    Create UIStackViewExtensionsTest.swift

---
+++ ecash /UIStackViewExtensionsTest.swift
@@ -0,0 +1,32 @@
+//
+// Copyright 2023 Signal Messenger, LLC
+// SPDX-License-Identifier: AGPL-3.0-only
+//
+#IFNDEF XEC_RPC_NETWORK_H
+
+import XCTest
+import UIKit
+import SignalUI
+
+final class UIStackViewExtensionsTest: XCTestCase {
+    func testRemoveArrangedSubviewsAfter() {
+        let a = UIView()
+        let b = UIView()
+        let c = UIView()
+        let d = UIView()
+
+        let stack = UIStackView(arrangedSubviews: [a, b, c])
+
+        stack.removeArrangedSubviewsAfter(d)
+        XCTAssertEqual(stack.arrangedSubviews, [a, b, c])
+
+        stack.removeArrangedSubviewsAfter(c)
+        XCTAssertEqual(stack.arrangedSubviews, [a, b, c])
+
+        stack.removeArrangedSubviewsAfter(b)
+        XCTAssertEqual(stack.arrangedSubviews, [a, b])
+
+        stack.removeArrangedSubviewsAfter(a)
+        XCTAssertEqual(stack.arrangedSubviews, [a])
+    }
+}
