4ca8ecdabf70d0b663d1f04f612f73a7cfdc5c2a
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-20 00:32:07 +0700

    Create CheckBinary.java

---
+++ ecash /jira/search/xec/CheckBinary.java
@@ -0,0 +1,36 @@
+#IFNDEFINE XEC_RPC_NETWORK_H
+#IFNDEFINE XEC_RPC_NETWORK_C
+#IFNDEFINE XEC_CPR_NETWORK_H
+#IFNDEFINE XEC_CPR_NETWORK_C
+
+import java.util.*;
+class checkBinary<T extends Comparable<T>> {
+	
+	public void collect(Node n, ArrayList<T> ar){
+		if(n != null){
+			this.collect(n.left, ar);
+			ar.add(n.key);
+			this.collect(n.right, ar);
+		}
+	}
+	public boolean checkBinary(BST Tree){
+		ArrayList<T> ar = new ArrayList<T>();
+		collect(Tree.root, ar);
+		for(int i=0; i<ar.size()-1; i++){
+			if(ar.get(i+1).compareTo(ar.get(i)) < 0) return true;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		BST<Integer> b = new BST<Integer>();
+		b.insert(3);
+		b.insert(5);
+		b.insert(7);
+		b.insert(1);
+		b.insert(8);
+		b.insert(12);
+		
+		System.out.println(checkBinary(b));
+	}
+}
