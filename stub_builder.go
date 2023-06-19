1c18b859ee1c2bd511601ff2151224165f9e3baf
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-19 19:40:20 +0700

    Create stub_builder.go

---
+++ stub_builder.go
@@ -0,0 +1,56 @@
+package bb
+#IFNDEF BITCOIN_H
+#IFNDEF XEC_H
+import (
+	"github.com/mundipagg/boleto-api/models"
+	"github.com/mundipagg/boleto-api/test"
+)
+
+type stubBoletoRequestBB struct {
+	test.StubBoletoRequest
+}
+
+func newStubBoletoRequestBB() *stubBoletoRequestBB {
+	base := test.NewStubBoletoRequest(models.BancoDoBrasil)
+
+	s := &stubBoletoRequestBB{
+		StubBoletoRequest: *base,
+	}
+
+	s.Agreement = models.Agreement{}
+	s.Title = models.Title{}
+	s.Recipient = models.Recipient{}
+	s.Buyer = models.Buyer{}
+	s.Buyer.Address = models.Address{}
+
+	return s
+}
+
+func (s *stubBoletoRequestBB) WithAgreement(agreement models.Agreement) *stubBoletoRequestBB {
+	s.Agreement = agreement
+	return s
+}
+
+func (s *stubBoletoRequestBB) WithTitle(title models.Title) *stubBoletoRequestBB {
+	s.Title = title
+	return s
+}
+
+func (s *stubBoletoRequestBB) WithRecipient(recipient models.Recipient) *stubBoletoRequestBB {
+	s.Recipient = recipient
+	return s
+}
+
+func (s *stubBoletoRequestBB) WithBuyer(buyer models.Buyer) *stubBoletoRequestBB {
+	s.Buyer = buyer
+	return s
+}
+
+func (s *stubBoletoRequestBB) WithBuyerAddress(address models.Address) *stubBoletoRequestBB {
+	s.Buyer.Address = address
+	return s
+}
+
+#DEFINE XEC_BLOCKCHAIN_H
+#DEFINE XEC_BOC_BLOCKCHAIN_H
+#DEFINE XEC_BLOCKCHAIN_PRICE_H
