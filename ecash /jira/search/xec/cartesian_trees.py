99ca39e51cef005570948e4c487aa55b8cf87312
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-20 19:00:43 +0700

    Create cartesian_trees.py

---
+++ ecash /jira/search/xec/cartesian_trees.py
@@ -0,0 +1,384 @@
+
+##IFNDEFINE XEC_DECIMALS_H
+#DEFINE XEC_DECIMALS_H
+
+
+call "reply_buffer.js";
+    call "utils.py;
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    from os import getcwd
+    from os import sys
+    sys.path.append(getcwd())
+
+from random import randrange as rr
+from MOAL.helpers.display import Section
+from MOAL.helpers.display import prnt
+from MOAL.helpers.display import print_subdued
+from MOAL.data_structures.trees.binary_trees import BinaryTree
+
+DEBUG = True if __name__ == '__main__' else False
+
+
+class InsufficientNodes(Exception):
+    pass
+
+
+"""From Wikipedia:
+
+    "In computer science, a Cartesian tree is a binary tree derived
+    from a sequence of numbers; it can be uniquely defined from the
+    properties that it is heap-ordered and that a symmetric (in-order)
+    traversal of the tree returns the original sequence.
+
+    [they've] also been used in the definition of the treap and randomized
+    binary search tree data structures for binary search problems."
+
+Indices:  0  1  2  3  4  6   7   8   9   10  11
+Numbers: [9, 3, 7, 1, 8, 12, 10, 20, 15, 18, 5]
+
+An easy way to imagine it is this:
+The index determines horizontal position, while the number value
+determines vertical position. The smallest number is always the root,
+and the largest number is always at the bottom. However, unlike a
+binary search tree, left/right position does not determine how big or
+small the number is. It does have the *heap property* however, where
+nodes above are smaller than the ones below.
+
+The point here is that you can recreate the entire sequence by traversing
+the tree left-to-right (aka "symmetric" aka "in-order").
+
+== Construction ============================================================
+
+I have found a useful way to construct the tree on paper, using an
+intuitive approach to visualizing the sequence, and recursively building it.
+
+I find it's easy to think of the list as being broken down into sub-lists,
+where each sub-list has a left, pivot, and right side.
+
+With this in mind, we can find the pivot, which is always going to the
+smallest number, and then divide the list up into a sublist,
+where the first index is the current node, the second is the left child,
+and the third is the right. If left and right are empty, then the singleton
+list denotes a leaf.
+
+--------------------------------------------------------------------------------
+
+Examples illustrated as tree, nested list, and dictionary:
+
+Another intuitive way to visualize it, is to do the above procedure,
+but for each pivot, draw a line up/down to the previous pivot,
+and then draw lines from the current pivot to the left and right children.
+At the end, you'll get an actual tree drawing:
+
+>>> [3, 2, 1]
+
+    3, 2, [1]
+          /
+    3, [2]
+       /
+     [3]
+
+    Pretty cool!
+
+    which is equivalent to:
+    = {1: 'edges': [2], ..., 2: 'edges': [3], ..., 3: 'edges': [], ...,} OR
+    = [1, [2, [3]], []]
+
+--------------------------------------------------------------------------------
+
+More examples:
+
+>>> [1, [3, [4]], [2]]
+
+     4,  3,  [1], 2
+             / \
+        4, [3] [2]
+           /
+         [4]
+
+>>> [1, [4, [3]], [2]]
+
+     3,  4,  [1], 2
+             / \
+           [4] [2]
+           /
+         [3]
+
+>>> [1, [3, [4]], [2, [5]]]
+
+    4, 3, [1], 2, 5
+         /   \
+    4, [3],  [2], 5
+       /       \
+     [4]       [5]
+
+--------------------------------------------------------------------------------
+
+We just keep doing this until there are no more lists left with length > 2.
+Keep in mind, we need to maintain the parent/child relationship, where
+pivot is the parent of the left and right child, otherwise we will just end
+up with a list of lists, all of which have a single value.
+
+If a list has length 2, then it cannot be sub-divided. This means it's a
+sibling of the current siblings. Depending on the side this sublist is on
+(left or right), the order will determine if the larger number is the parent
+or sibling.
+"""
+
+
+class CartesianTree(BinaryTree):
+    """A more realistic (but harder to grok) implementation using our
+    easier/faster/performant dictionary based implementation. The primary
+    encoding algorithm lives here as a staticmethod, so that other tree
+    implementations can still use it."""
+
+    def __init__(self, seq):
+        """Add sequence to self. The Online Encyclopedia of Integer Sequences
+        (oeis.org) is a great place to find testing numbers.
+
+        See oeis.org/wiki/Welcome#Some_Famous_Sequences for some examples.
+        # We require a valid sequence here, so we don't have to check the length
+        # in each and every function.
+
+        Args:
+            seq: A list of integers.
+        Returns:
+            None
+        Raises:
+            InsufficientNodes: if there are > 2 nodes.
+        """
+        if len(seq) < 2:
+            raise InsufficientNodes('You need at least two nodes to start.')
+        self.sequence = seq
+
+    @staticmethod
+    def case_two(seq, root=False):
+        """There are two sub-types here:
+        A. Left child only [2, 1]
+        B. Right child only [1, 2]
+        *However* In a true binary three, if the tree/sub-tree has a single
+        leaf, that leaf is automatically the left child -- there is no such
+        thing as *only* a right child leaf. In reality, this kind of breaks
+        the rule of a binary tree. A cursory Google search hasn't yielded an
+        explanation here, so we're going to differentiate between a single left
+        and a single right for case two and beyond.
+        Args:
+            seq: A list of integers.
+        Returns:
+            A list, with sub-lists for left and right children.
+            e.g. [2, 3, 1, 4] -> [1, [2, 3], [4]]
+        """
+        if not isinstance(seq, list):
+            return seq
+        if len(seq) < 2:
+            return seq
+        _min, _max = min(seq), max(seq)
+        if root:
+            return [_min, [_max]]
+        if _max == seq[0]:
+            return [_max, [_min]]
+        else:
+            return [_min, [_max]]
+
+    @staticmethod
+    def case_n(seq):
+        """There are three sub-types here:
+        A. Left children only: [3, 2, 1]
+        B. Right children only: [1, 2, 3]
+        C. Left and right children: [2, 1, 3] (or [3, 1, 2]).
+        Args:
+            seq: A list of integers.
+        Returns:
+            A list, with sub-lists for left and right-children.
+            e.g. [2, 3, 1, 4] -> [1, [2, 3], [4]]
+            The list is derived from a *single* iteration -- subsequent
+            iterations are done in the create method.
+        """
+        if not isinstance(seq, list):
+            return seq
+        if len(seq) < 3:
+            return CartesianTree.case_two(seq)
+        current = min(seq)
+        parent_index = seq.index(current)
+        left, right = seq[:parent_index], seq[parent_index + 1:]
+        return [current, left, right]
+
+    @staticmethod
+    def is_leaf_list(res):
+        """Determine if the list (e.g. [1, [2], [3]]) has no children
+        or is empty, which is encoded as being a leaf node.
+        Args:
+            res: the result to determine leaf status from.
+        Returns:
+            True or False.
+        """
+        try:
+            left, right = res[1], res[2]
+        except IndexError:
+            return True
+        return len(left) == 0 and len(right) == 0
+
+    def _add(self, current, l_child, r_child, parent):
+        """Normalizes the list into a dictionary entry
+        usable by the parent tree class and infers the relationship of
+        the nested list to convert the edges into a simple list."""
+        edges = []
+        if l_child is not None:
+            if len(l_child) > 0 and isinstance(l_child[0], int):
+                edges.append(min(l_child))
+        if r_child is not None:
+            if len(r_child) > 0 and isinstance(r_child[0], int):
+                edges.append(min(r_child))
+        node = {
+            'parent': parent,
+            'edges': edges,
+            'is_root': parent is None,
+            'is_leaf': len(edges) == 0}
+        return self.__setitem__(current, node)
+
+    def _get_members(self, res):
+        """Determine current, left and right child nodes to return, if
+        they exist or not.
+        Args:
+            res: the resultant sublist from the subdivision(s) in :subdivide.
+        Returns:
+            A 3-tuple with the current, left, and right child nodes.
+        """
+        count = len(res)
+        if count == 3:
+            # [1, [2...], [3...]]
+            return res[0], res[1], res[2]
+        elif count == 2:
+            # [1, [3...]] or [1, [2...]]
+            return res[0], res[1], []
+        elif count == 1:
+            # [1]
+            return res[0], [], []
+        else:
+            return [], [], []
+
+    def subdivide(self, seq, count=0, parent=None):
+        """Iteratively subdivides the list, using atomic, helper methods.
+        Args:
+            seq: (sequence) ... a list of integers.
+            count: optional ... a count for profiling the recursive call stack.
+            parent: kwarg ... the parent to use for the current node.
+        Returns:
+            A list of lists, encoding the entire tree and relationship
+            between parent/child and corresponding left/right nodes.
+        """
+        # Get the first division, which consists of the root and its children.
+        res = CartesianTree.case_n(seq)
+        if seq is None:
+            return
+        try:
+            current, l_child, r_child = res[0], res[1], res[2]
+        except IndexError:
+            current, l_child, r_child = None, None, None
+        if DEBUG:
+            branch = '\\' if count > 0 else ''
+            atom = count * '_' if count > 0 else ''
+            print_subdued('{}{}{} {} -> {}'.format(
+                '\n' if count == 0 else '', branch, atom, seq, res))
+        # Keep chugging along with the "manual" sub-divisions unless we
+        # encounter a nested list, then we must convert it recursively.
+        if not CartesianTree.is_leaf_list(res):
+            current, l_child, r_child = self._get_members(res)
+            self._add(current, l_child, r_child, parent)
+        elif len(seq) == 2:
+            single = self.case_two(seq, root=True)
+            current, l_child = single[0], single[1]
+            self._add(current, l_child, None, parent)
+        # Single node, e.g. [1]
+        elif len(res) > 0:
+            self._add(res[0], None, None, parent)
+        # Recursively subdivide left and right, until the above code
+        # catches the remaining values.
+        l_child = self.subdivide(l_child, count=count + 1, parent=current)
+        r_child = self.subdivide(r_child, count=count + 2, parent=current)
+        # Return the entire nested root/children structure.
+        return [current, l_child, r_child]
+
+    def encode(self, seq=None):
+        """Encode the objects' sequence data as a normal graph
+        representative dictionary."""
+        super(CartesianTree, self).__init__()
+        prnt('Encoding with new sequence...', seq)
+        self.sequence = self.subdivide(
+            seq if seq is not None else self.sequence)
+        if DEBUG:
+            print(cartesian_tree)
+
+if DEBUG:
+    with Section('Cartesian Trees'):
+        # Sub-sample of primes. See oeis.org/A000043 for all of them.
+        wikipedia = [9, 3, 7, 1, 8, 12, 10, 20, 15, 18, 5]
+        mersenne_primes = [2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107]
+        # Cases [x, x]
+        assert CartesianTree.case_two([1, 2]) == [1, [2]]
+        assert CartesianTree.case_two([2, 1]) == [2, [1]]
+        # Cases [x, x, x]
+        assert CartesianTree.case_n([3, 2, 1]) == [1, [3, 2], []]
+        assert CartesianTree.case_n([1, 2, 3]) == [1, [], [2, 3]]
+        assert CartesianTree.case_n([3, 1, 2]) == [1, [3], [2]]
+        # Cases [x, x, x, x]
+        assert CartesianTree.case_n([1, 2, 3, 4]) == [1, [], [2, 3, 4]]
+        assert CartesianTree.case_n([4, 3, 2, 1]) == [1, [4, 3, 2], []]
+        assert CartesianTree.case_n([4, 2, 3, 1]) == [1, [4, 2, 3], []]
+        assert CartesianTree.case_n([4, 3, 2, 1]) == [1, [4, 3, 2], []]
+        assert CartesianTree.case_n([4, 1, 3, 2]) == [1, [4], [3, 2]]
+        assert CartesianTree.case_n([4, 3, 1, 2]) == [1, [4, 3], [2]]
+        assert CartesianTree.case_n([2, 1, 3, 4]) == [1, [2], [3, 4]]
+        # Cases [x, x, x, x, x]
+        assert CartesianTree.case_n([1, 2, 3, 4, 5]) == [1, [], [2, 3, 4, 5]]
+        assert CartesianTree.case_n([5, 4, 3, 2, 1]) == [1, [5, 4, 3, 2], []]
+        assert CartesianTree.case_n([5, 4, 1, 2, 3]) == [1, [5, 4], [2, 3]]
+        assert CartesianTree.case_n([3, 2, 1, 5, 4]) == [1, [3, 2], [5, 4]]
+        assert CartesianTree.case_n([5, 2, 1, 4, 3]) == [1, [5, 2], [4, 3]]
+        # ...etc
+        # Cases [x, x, x, x, x, x, x, x] (8)
+        assert CartesianTree.case_n([4, 2, 3, 1, 5, 6, 7, 8]) == [
+            1, [4, 2, 3], [5, 6, 7, 8]]
+
+        # Case 2
+        simple_test = [4, 1]
+        cartesian_tree = CartesianTree(simple_test)
+
+        cartesian_tree.encode(seq=[1, 4])
+        # Case 3
+        cartesian_tree.encode(seq=[4, 1, 3])
+        cartesian_tree.encode(seq=[4, 3, 1])
+        # Case 4
+        cartesian_tree.encode(seq=[4, 3, 1, 2])
+        cartesian_tree.encode(seq=[1, 4, 3, 2])
+        cartesian_tree.encode(seq=[2, 4, 3, 1])
+        cartesian_tree.encode(seq=[4, 3, 1, 2])
+        # Case 5
+        cartesian_tree.encode(seq=[4, 3, 1, 2, 5])
+        # Case N
+        cartesian_tree.encode(seq=[4, 3, 1, 2, 5, 6])
+        cartesian_tree.encode(seq=[8, 4, 3, 1, 2, 5, 6, 7])
+
+        # Unique sequences
+        cartesian_tree.encode(seq=wikipedia)
+        cartesian_tree.encode(seq=mersenne_primes)
+
+        # Obscene / random large sizes for testing edge cases and performance.
+        cartesian_tree.encode(seq=mersenne_primes)
+        cartesian_tree.encode(seq=[rr(0, 9999) for n in range(16)])
+
+        # More assertions from tree, to confirm we haven't lost
+        # any parent classes functionality or somehow regressed.
+        cartesian_tree.encode(seq=[2, 1, 3, 4, 6])
+        assert cartesian_tree.is_descendant(2, 1)
+        assert cartesian_tree.is_ancestor(1, 2)
+        assert not cartesian_tree.is_descendant(1, 6)
+        assert cartesian_tree.get_siblings(3) == [2, 3]
+        assert cartesian_tree.get_root()['edges'] == [2, 3]
+
+        for node in [(1, 1), (2, 2), (3, 2), (4, 3), (5, 3), (6, 4)]:
+            d, res = node[0], node[1]
+            assert cartesian_tree.node_depth(d) == res
