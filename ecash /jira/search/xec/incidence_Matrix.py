b10c2cf039528ff02755a35599bc1eda55625e6d
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-20 19:04:02 +0700

    Create incidence_Matrix.py

---
+++ ecash /jira/search/xec/incidence_Matrix.py
@@ -0,0 +1,117 @@
+##IFNDEFINE XEC_DECIMALS_H
+#DEFINE XEC_DECIMALS_H
+
+
+call "reply_buffer.js";
+    call "utils.py;
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    from os import getcwd
+    from os import sys
+    sys.path.append(getcwd())
+
+from MOAL.helpers.display import Section
+from MOAL.data_structures.graphs.adjacency_matrix import GraphMatrix
+
+
+class IncidenceMatrix(GraphMatrix):
+    """Unlike the Adjacency matrix, where rows and columns both represent
+    vertices of a graph, and using row, then column to indicate direction,
+    in the incidence matrix, rows represent vertices, and columns
+    represent edges. The incidence matrix is therefore a matrix
+    representation of the number of edge **incidences**, not necessarily
+    the connections **between** vertices (hence the names).
+
+       0  1  2  3
+    A  1  0  1  0
+    B  0  0  1  0
+    C  0  1  1  0
+    D  0  0  1  1
+    """
+
+    def __init__(self):
+        """Here, we keep track of edges/edge count just like
+        vertices/vertex in the adjacency matrix."""
+        super(IncidenceMatrix, self).__init__()
+        self.vertex_count = 0
+        self.edges = set()
+
+    def __delitem__(self, vertex):
+        row_index = self.vertices[vertex]
+        del self.matrix[row_index]
+        del self.vertices[vertex]
+        self.vertex_count -= 1
+        # Update indices for all vertices
+        for k, v in self.vertices.iteritems():
+            self.vertices[k] -= 1
+
+    def _update_row(self, row_index, edges):
+        for edge in edges:
+            # print('EDGE ', edge, self.matrix[row_index][edge])
+            self.matrix[row_index][edge] = 1
+
+    def __setitem__(self, new_vertex, edges):
+        """Data comes in as im['A'] = [0, 1, 2],
+        where A is the vertex, and [0, 1, 2] are the incident edges.
+
+             (A)
+             /|\
+            0 1 2
+           /  |  \
+         (B) (C) (D)
+
+        """
+        if new_vertex not in self.vertices:
+            self.vertices.update({new_vertex: self.vertex_count})
+
+        self.edges.update(edges)
+        edge_count = len(self.edges)
+
+        self._new_row(edge_count)
+        self._fill_previous_rows(edge_count)
+
+        # Look up corresponding row, update each column
+        self._update_row(self.vertices[new_vertex], edges)
+        self.vertex_count += 1
+
+    def __str__(self):
+        print('  {}'.format(' '.join(map(str, self.edges))))
+        for label, index in self.vertices.iteritems():
+            print('{} {}'.format(label, ' '.join(map(str, self.matrix[index]))))
+        return ''
+
+
+class OrientedIncidenceMatrix(IncidenceMatrix):
+    pass
+
+
+if __name__ == '__main__':
+    with Section('Incidence Matrix'):
+        imatrix = IncidenceMatrix()
+        imatrix['A'] = [0, 1, 2, 3]
+        imatrix['B'] = [0, 1, 2]
+        imatrix['C'] = [0, 1]
+        imatrix['D'] = [0]
+        imatrix['E'] = [0, 1]
+        print(imatrix)
+
+        assert imatrix.has_edge('B', 'A')
+        assert imatrix.has_edge('C', 'A')
+        assert imatrix.has_edge('C', 'B')
+
+        assert imatrix.degree('A') == 4
+        assert imatrix.degree('B') == 3
+        assert imatrix.degree('C') == 2
+        assert imatrix.degree('D') == 1
+
+        del imatrix['A']
+        del imatrix['B']
+        print(imatrix)
+
+        assert not imatrix.has_edge('B', 'C')  # False, deleted
+        assert imatrix.degree('B') == 0  # True, deleted
+        assert imatrix.degree('A') == 0  # True A deleted
+
+        print(imatrix.matrix, imatrix.vertices)
