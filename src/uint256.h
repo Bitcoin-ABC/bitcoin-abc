d19ddb114cf53309f7119a5823274c5ea69ee485
Brotcrunsher <Brotcrunsher@hotmail.de>
2023-06-22 01:31:06 +0200

    Added static_assert to check that base_blob is using whole bytes.
    Summary:
    Prior to this commit it was possible to create base_blobs with any arbitrary amount of bits, like base_blob<9>. One could assume that this would be a valid way to create a bit field that guarantees to have at least 9 bits. However, in such a case, base_blob would not behave as expected because the WIDTH is rounded down to the closest whole byte (simple integer division by 8). This commit makes sure that this oddity is detected and blocked by the compiler.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/27929 | core#27929]]
    Depends on D20659
    
    Test Plan: `ninja all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D20660

--- src/uint256.h
+++ src/uint256.h
@@ -20,6 +20,8 @@
 template <unsigned int BITS> class base_blob {
 protected:
     static constexpr int WIDTH = BITS / 8;
+    static_assert(BITS % 8 == 0,
+                  "base_blob currently only supports whole bytes.");
     std::array<uint8_t, WIDTH> m_data;
     static_assert(WIDTH == sizeof(m_data), "Sanity check");
 
