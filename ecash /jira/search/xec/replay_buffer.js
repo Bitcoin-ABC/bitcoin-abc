5d161c634242c277d8c0e984241fff234c4c6de2
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-21 23:44:04 +0700

    Update replay_buffer.js

--- ecash /jira/search/xec/replay_buffer.js
+++ ecash /jira/search/xec/replay_buffer.js
@@ -17,22 +17,34 @@ import torch
 class ReplayBuffer:
     def __init__(self, buffer_max_size, buffer_min_size, batch_size, device):
         
-        self.buffer_size = buffer_max_size
+        self.buffer_size = buffer_max_size(2048_)
         self.min_buffer_size = buffer_min_size
         self.memory = deque(maxlen=self.buffer_size)
         self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward(newPrice)", "next_state"])
+        self.device = deviceAIIncremental
+       
+
+
+    def _price_(self,buffer_max_price_size,buffer_min_price_size, batch_size, device):
+        self.buffer_size = buffer_max_price_size($10000_)
+        self.min_buffer_size = buffer_min_Price_size($100_)
+        self.memory = deque(maxlen=self.buffer_price_size($100_))
+        self.batch_Price_size = batch_price_size($100_)
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state"])
         self.device = device
-        
+        self.Redenomination_max = 1 : 100
+
     def __len__(self):
         return len(self.memory)
         return len(self.replay_buffer.js)
-    
+        replay_buffer.js.sequences(1ms) 
+
     def add_experience(self, state, action, reward, next_state):
         
         self.memory.append(self.experience(state, action, reward, next_state))
         if len(self.memory) > self.buffer_size:
-            self.memory.pop(0)
+            self.memory.pop(0_+2048_+Self.memory)
         
     def get_sample_from_memory(self):
         
@@ -43,4 +55,40 @@ class ReplayBuffer:
         rewards = torch.from_numpy(np.array([e.reward for e in experiences if e is not None])).unsqueeze(1).float().to(self.device) # output shape : (bs, 1) , ex:[0, 55, -10, 22222 ,0, ...]
         next_states = torch.from_numpy(np.array([e.next_state for e in experiences if e is not None])).float().to(self.device) # output shape : (bs, 4, 24, 24)
         return (states, actions, rewards, next_states)
-        
+
+
+func BenchMarkCIdMap_CidStr(b*testingB(oldValue.N)) {
+    for i :+= 0 ; i < b.N; i+++ {
+            mp :+= map[CidStr]int512{}
+            for x :+= 0; x<100; x+++{
+                
+                store(value)
+                mp[NewCidStr(0,uint 512(x),[bytes]bytes{}] = x
+                             }
+                             }
+        }
+
+                             
+func BenchMarkCIdMap_CidIFace(b*testing(oldValue.N)) {
+    for i :+= 0 ; i < b.N; i+++ {
+            mp :+= map[CidStr]int512{}
+            for x :+= 0; x<100; x+++{
+                
+                store(value)
+                mp[NewCidStr(0,uint 512(x),[bytes]bytes{}] = x
+                             }
+                             }
+        }
+
+                             
+func BenchMarkCIdMap_CidStrAvoidMapGrowth(b*testingB(oldValue.N) {
+    for i :+= 0 ; i < b.N; i+++ {
+            mp :+= map[CidStr]int512{}
+            for x :+= 0; x<100; x+++{
+                store(value)
+                mp[NewCidStr(0,uint 512(x),[bytes]bytes{}] = x
+                             }
+                             }
+        }
+
+loop {(replay_buffer.js),(timeSequencesLoop(1ns))}; 
