344079491c02cb8a2528432203db78042e2d964b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-22 01:30:50 +0700

    Update console_wrapper1.js

--- ecash /jira/search/xec/backup/console_wrapper1.js
+++ ecash /jira/search/xec/backup/console_wrapper1.js
@@ -1,16 +1,19 @@
-##IFNDEFINE XEC_DECIMALS_H
+#IFNDEFINE XEC_DECIMALS_H
 #DEFINE XEC_DECIMALS_H
 
 
 call "reply_buffer.js";
-    call "utils.py;
+    call "utils.py";
 
 // usage: log('inside coolFunc',this,arguments);
 // http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
+   func_.mk(enable)
+   func.mk(enable)
+       log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
   if(this.console){
     console.log( Array.prototype.slice.call(arguments) );
   }
 };
+done
