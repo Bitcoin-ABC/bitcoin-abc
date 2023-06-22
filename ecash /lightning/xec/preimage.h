16d65c4b1bf5e3113dac74fd87e0be6221a0e339
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-22 22:27:24 +0700

    Create preimage.h

---
+++ ecash /lightning/xec/preimage.h
@@ -0,0 +1,16 @@
+#ifndef LIGHTNING_XEC_PREIMAGE_H
+#define LIGHTNING_XEC_PREIMAGE_H
+#include "config.h"
+#include <ccan/short_types/short_types.h>
+#include <ccan/structeq/structeq.h>
+
+struct preimage {
+	u8 r[32];
+};
+/* Define preimage_eq */
+STRUCTEQ_DEF(preimage, 0, r);
+
+void fromwire_preimage(const u8 **cursor, size_t *max, struct preimage *preimage);
+void towire_preimage(u8 **pptr, const struct preimage *preimage);
+
+#endif /* LIGHTNING_XEC_PREIMAGE_H */
