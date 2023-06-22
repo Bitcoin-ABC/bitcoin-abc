1ceb096d1a1c48ff3678ed2bbdf131cd5df32bb7
ethan <ethanmackie1199@gmail.com>
2023-06-22 13:49:50 +1000

    [Apps] [Examples] Initial mocha framework and getDetailsFromTxid example
    Summary:
    T3203
    
    This diff creates an Example folder under Apps on the monorepo and which will be populated with example code as a reference guide for new devs looking into building on eCash.
    
    This is an initial diff containing the readme and the getDetailsFromTxid example along with the mocha test suite initialized.
    
    Test Plan:
    `nvm install 16`
    `npm i`
    `npm run getDetailsFromTxid  <chronik url><txid>` and ensure details for the txid is displayed and matches the block explorer
    `npm test` and ensure all tests pass
    
    Reviewers: bytesofman, #bitcoin_abc, Fabien
    
    Reviewed By: bytesofman, #bitcoin_abc, Fabien
    
    Subscribers: tobias_ruck, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14100

---
+++ apps/examples/README.md
@@ -0,0 +1,38 @@
+# App dev reference guide
+
+This folder contains a series of example code to serve as a reference guide for app developers looking to build on eCash.
+
+These examples utilize the [Chronik](https://www.npmjs.com/package/chronik-client) indexer and [NodeJS](https://github.com/nvm-sh/nvm) to interact with the eCash blockchain and highlights some of the technical nuances specific to app development on eCash.
+
+## Requirements
+
+Please ensure your node version is > 16.x.x and the chronik and mocha dependencies are installed:
+
+-   `nvm install 16`
+-   `npm i`
+
+## Chronik indexer
+
+If you'd like to optionally setup your own Chronik instance, please refer to the [Chronik NNG README](https://github.com/raipay/chronik/).
+
+## Examples
+
+[x] [Retrieving transaction details - getDetailsFromTxid()](scripts/getDetailsFromTxid.js)
+Usage: `npm run getDetailsFromTxid <chronik url> <txid>`
+Example: `npm run getDetailsFromTxid https://chronik.fabien.cash fd9a775...fce0e`
+
+[] Retrieving transaction history for an address
+[] Retrieving UTXOs for an address
+[] Creating a new wallet
+[] Collating inputs and outputs for sending XEC
+[] Collating inputs and outputs for sending eTokens
+[] Building and broadcasting transactions
+[] Querying eToken genesis details and current stats
+[] Querying holders of a particular eToken
+[] Querying blockchain info
+[] Using websockets to listen for confirmation of a transaction
+[] Implementing CashtabPay from cashtab-components for an online store
+
+## Questions?
+
+If you have any questions regarding these examples please feel free to reach out to the development team via the [eCash Development Telegram](https://t.me/eCashDevelopment).
---
+++ apps/examples/mocks/chronikResponses.js
@@ -0,0 +1,52 @@
+// Copyright (c) 2023 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// @generated
+'use strict';
+
+module.exports = {
+        tx: {
+            '816d32c855e40c4221482eb85390a72ba0906360197c297a787125e6979e674e': {
+              txid: '816d32c855e40c4221482eb85390a72ba0906360197c297a787125e6979e674e',
+      version: 2,
+      inputs: [
+          {
+              prevOut: [Object],
+              inputScript:
+                  '483045022100906aaa871331afbdeab6cf7a4638835f1b259529a5ecd0081ce3cf19946e822a0220753b695fd001319361bdc4f6d2f2e0f262b467108b8cec55876acae81cd017ed412103771805b54969a9bea4e3eb14a82851c67592156ddb5e52d3d53677d14a40fba6',
+              outputScript: '76a91495e79f51d4260bc0dc3ba7fb77c7be92d0fbdd1d88ac',
+              value: '5018901',
+              sequenceNo: 4294967294,
+              slpBurn: undefined,
+              slpToken: undefined,
+          },
+      ],
+      outputs: [
+          {
+              value: '1200000',
+              outputScript: '76a914816f64372275a81cea291d89328a4f38b8d01ef788ac',
+              slpToken: undefined,
+              spentBy: undefined,
+          },
+          {
+              value: '3818446',
+              outputScript: '76a91495e79f51d4260bc0dc3ba7fb77c7be92d0fbdd1d88ac',
+              slpToken: undefined,
+              spentBy: [Object],
+          },
+      ],
+      lockTime: 0,
+      slpTxData: undefined,
+      slpErrorMsg: undefined,
+      block: {
+          height: 776426,
+          hash: '000000000000000006a599a9539863a62661692bdfeb88be19b2886d5cb2eff0',
+          timestamp: '1674688510',
+      },
+      timeFirstSeen: '1674687499',
+      size: 226,
+      isCoinbase: false,
+      network: 'XEC',
+    },
+  },
+};
