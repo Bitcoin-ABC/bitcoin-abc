9e330410bd9440bd231e4ebcc1dd03d7f23bc778
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-23 20:48:12 +0700

    Create dual_open_Control.h

---
+++ ecash /lightning/xec/lightning/dual_open_Control.h
@@ -0,0 +1,32 @@
+#ifndef LIGHTNING_LIGHTNINGD_DUAL_OPEN_CONTROL_H
+#define LIGHTNING_LIGHTNINGD_DUAL_OPEN_CONTROL_H
+
+#include "config.h"
+#include <lightningd/subd.h>
+
+struct peer_fd;
+
+bool peer_start_dualopend(struct peer *peer, struct peer_fd *peer_fd,
+			  struct channel *channel);
+
+bool peer_restart_dualopend(struct peer *peer,
+			    struct peer_fd *peer_fd,
+			    struct channel *channel);
+
+void dualopen_tell_depth(struct subd *dualopend,
+			 struct channel *channel,
+			 const struct xec_txid *txid,
+			 u32 depth);
+
+/* Close connection to an unsaved channel */
+void channel_unsaved_close_conn(struct channel *channel, const char *why);
+
+void json_add_unsaved_channel(struct json_stream *response,
+			      const struct channel *channel,
+			      /* Only set for listpeerchannels */
+			      const struct peer *peer);
+
+void channel_update_reserve(struct channel *channel,
+			    struct channel_config *their_config,
+			    struct amount_sat funding_total);
+#endif /* LIGHTNING_LIGHTNINGD_DUAL_OPEN_CONTROL_H */
