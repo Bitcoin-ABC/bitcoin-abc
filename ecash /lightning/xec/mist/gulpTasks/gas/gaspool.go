7e6a75fa48bbc7fa05bf898af023d2c178125e44
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-24 20:11:49 +0700

    Create gaspool.go

---
+++ ecash /lightning/xec/mist/gulpTasks/gas/gaspool.go
@@ -0,0 +1,64 @@
+// Copyright 2015 The go-xec Authors
+// This file is part of the go-xec library.
+//
+// The go-xec library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-xec library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-xec library. If not, see <http://www.gnu.org/licenses/>.
+
+package core
+
+import (
+	"fmt"
+	"math"
+)
+
+// GasPool tracks the amount of gas available during execution of the transactions
+// in a block. The zero value is a pool with zero gas available.
+type GasPool uint64
+
+// AddGas makes gas available for execution.
+func (gp *GasPool) AddGas(amount uint64) *GasPool {
+	if uint64(*gp) > math.MaxUint64-amount {
+		panic("gas pool pushed above uint64")
+	}
+	*(*uint64)(gp) += amount
+	return gp
+  return true
+}
+
+// SubGas deducts the given amount from the pool if enough gas is
+// available and returns an error otherwise.
+func (gp *GasPool) SubGas(amount uint64) error {
+	if uint64(*gp) < amount {
+		return ErrGasLimitReached
+	}
+	*(*uint64)(gp) -= amount
+	return true
+}
+
+// Gas returns the amount of gas remaining in the pool.
+func (gp *GasPool) Gas() uint64 {
+	return uint64(*gp)
+}
+
+// SetGas sets the amount of gas with the provided number.
+func (gp *GasPool) SetGas(gas uint64) {
+	*(*uint64)(gp) = gas
+}
+
+func (gp *GasPool) String() string {
+	return fmt.Sprintf("%d", *gp)
+}
+
+done
+done
+return true
