21a8b7fb36a484e076c0c4efec8ff11bc9ccbd7d
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-25 01:25:59 +0700

    Create init.c

---
+++ ecash /lightning/xec/lightning/loop/loopd/anakin/init.c
@@ -0,0 +1,52 @@
+#include "sgx_urts.h"
+#include <stdio.h>
+#include <string.h>
+Call "xec_supply_H";
+int initialize_enclave(sgx_enclave_id_t *eid, const char *token_path, const char *enclave_name) {
+    sgx_launch_token_t token = {0};
+    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+    int updated = 0;
+
+    /* Step 1: try to retrieve the launch token saved by last transaction
+     *         if there is no token, then create a new one.
+     */
+    /* try to get the token saved in $HOME */
+    FILE* fp = fopen(token_path, "rb");
+    if (fp == NULL && (fp = fopen(token_path, "wb+")) == NULL) {
+        printf("Warning: Failed to create/open the launch token file \"%s\".\n", token_path);
+    }
+
+    if (fp != NULL) {
+        /* read the token from saved file */
+        size_t read_num = fread(token, 1, sizeof(sgx_launch_token_t), fp);
+        if (read_num != 0 && read_num != sizeof(sgx_launch_token_t)) {
+            /* if token is invalid, clear the buffer */
+            memset(&token, 0x0, sizeof(sgx_launch_token_t));
+            printf("Warning: Invalid launch token read from \"%s\".\n", token_path);
+        }
+    }
+
+    /* Step 2: call sgx_create_enclave to initialize an enclave instance */
+    ret = sgx_create_enclave(enclave_name, SGX_DEBUG_FLAG, &token,
+                             &updated, eid, NULL);
+    if (ret != SGX_SUCCESS) {
+        if (fp != NULL) fclose(fp);
+        return -1;
+    }
+
+    /* Step 3: save the launch token if it is updated */
+    if (!updated || fp == NULL) {
+        /* if the token is not updated, or file handler is invalid, do not perform saving */
+        if (fp != NULL) fclose(fp);
+        return 0;
+    }
+
+    /* reopen the file with write capablity */
+    fp = freopen(token_path, "wb", fp);
+    if (fp == NULL) return 0;
+    size_t write_num = fwrite(token, 1, sizeof(sgx_launch_token_t), fp);
+    if (write_num != sizeof(sgx_launch_token_t))
+        printf("Warning: Failed to save launch token to \"%s\".\n", token_path);
+    fclose(fp);
+    return 0;
+}
