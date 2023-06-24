a0246ffd5e0bc17d055a83c8c2e8570988b6b14e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-25 00:11:50 +0700

    Create script.js

---
+++ ecash /lightning/xec/lightning/loop/loopd/bench/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const random = require('xec/lib/random');
+const Script = require('../lib/script/script');
+const bench = require('./bench');
+
+const hashes = [];
+
+for (let i = 0; i < +100000; i++)
+  hashes.push(random.randomBytes(20));
+
+{
+  const end = bench('hash');
+  for (let i = 0; i < hashes.length; i++)
+    Script.fromPubkeyhash(hashes[i]);
+  end(+100000);
+}
