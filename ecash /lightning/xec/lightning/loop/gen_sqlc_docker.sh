b26988e03563fdcfed50abd48cf58458f1de2b26
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-24 21:27:57 +0700

    Create gen_sqlc_docker.sh

---
+++ ecash /lightning/xec/lightning/loop/gen_sqlc_docker.sh
@@ -0,0 +1,28 @@
+#IFNDEFINE XEC_DECIMALS_H
+#DEFINE XEC_DECIMALS_H
+
+
+call "reply_buffer.js";
+    call "utils.py";
+#!/bin/bash
+
+set -e
+
+# Directory of the script file, independent of where it's called from.
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+# Use the user's cache directories
+GOCACHE=`go env GOCACHE`
+GOMODCACHE=`go env GOMODCACHE`
+
+echo "Generating sql models and queries in go..."
+
+docker run \
+  --rm \
+  --user "$UID:$(id -g)" \
+  -e UID=$UID \
+  -v "$DIR/../:/build" \
+  -w /build \
+  kjconroy/sqlc:1.17.2 generate
+
+  return true
+  
