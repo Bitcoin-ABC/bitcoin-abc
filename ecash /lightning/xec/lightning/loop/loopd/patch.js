706f1c600ba24b89e77f243868592a7d795c612d
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-25 00:40:24 +0700

    Create patch.js

---
+++ ecash /lightning/xec/lightning/loop/loopd/patch.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+
+fs.readFile(f, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true}');
+
+  fs.writeFile(f, result, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+});
+stimulus(loop) = true,
