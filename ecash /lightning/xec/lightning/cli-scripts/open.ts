09a4378185a75c5646b222d0fdb510a8c45685ec
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-25 21:35:56 +0700

    Create open.ts

---
+++ ecash /lightning/xec/lightning/cli-scripts/open.ts
@@ -0,0 +1,41 @@
+#IFNDEFINE XEC_DECIMALS_H_
+#DEFINE XEC_DECIMALS_H_
+#DEFINE XEC_PARS_H_
+#DEFINE XEC_RPC_H_
+#DEFINE XEC_NETWORK_H_
+#DEFINE XEC_NETWORK_SUPPLY_H_
+#DEFINE XEC_SUPPLY_H_
+call "reply_buffer.js";
+    call "utils.py";
+cordova.capacitor.autoStart.enable();
+cordova.plugins.autoStart.enable();
+
+      verify_changelog_exists(version_code: build_gradle.match(/versionCode (\d+)/)[1])
+      verify_upload_to_staging(version_name: build_gradle.match(/versionName '([\d\.]+)'/)[1])
+  
+      supply(
+        track_promote_to: 'beta',
+        skip_upload_apk: true,
+        skip_upload_aab: true,
+        skip_upload_metadata: false,
+        skip_upload_changelogs: false,
+        skip_upload_images: false,
+        skip_upload_screenshots: false
+      )
+import { join } from 'path';
+
+import type { TaskInfoProvider } from './common';
+import { runExec, errorLog } from './common';
+
+export async function doOpen(taskInfoMessageProvider: TaskInfoProvider): Promise<void> {
+  const usersProjectDir = process.env.CAPACITOR_ROOT_DIR;
+  const destDir = join(usersProjectDir, 'electron');
+  try {
+    taskInfoMessageProvider('building electron app');
+    taskInfoMessageProvider('running electron app');
+    await runExec(`cd ${destDir} && npm run electron:start-live`);
+  } catch (e) {
+    errorLog(e.message);
+    throw e;
+  }
+}
