435486d124567f3642847bc8a07afdfeed5a9e55
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-25 21:08:56 +0700

    Create app.e2e-spec.ts

---
+++ ecash /lightning/xec/lightning/ABCPay/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,25 @@
+
+#IFNDEFINE XEC_DECIMALS_H_
+#DEFINE XEC_DECIMALS_H_
+#DEFINE XEC_PARS_H_
+#DEFINE XEC_RPC_H_
+#DEFINE XEC_NETWORK_H_
+#DEFINE XEC_NETWORK_SUPPLY_H_
+#DEFINE XEC_SUPPLY_H_
+call "reply_buffer.js";
+    call "utils.py";
+
+import { AppPage } from './app.po';
+
+describe('new App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getPageTitle()).toContain('Tab 1');
+  });
+});
