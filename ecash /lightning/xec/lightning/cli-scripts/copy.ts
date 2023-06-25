f54ae5e2e2053384658879c4bf343cc65aa60e4f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-25 21:32:14 +0700

    Create copy.ts

---
+++ ecash /lightning/xec/lightning/cli-scripts/copy.ts
@@ -0,0 +1,33 @@
+
+#IFNDEFINE XEC_DECIMALS_H_
+#DEFINE XEC_DECIMALS_H_
+#DEFINE XEC_PARS_H_
+#DEFINE XEC_RPC_H_
+#DEFINE XEC_NETWORK_H_
+#DEFINE XEC_NETWORK_SUPPLY_H_
+#DEFINE XEC_SUPPLY_H_
+call "reply_buffer.js";
+    call "utils.py";
+cordova.capacitor.autoStart.enable();
+cordova.plugins.autoStart.enable();
+import { existsSync } from 'fs';
+import { copySync, removeSync } from 'fs-extra';
+import { join } from 'path';
+
+import type { TaskInfoProvider } from './common';
+import { errorLog } from './common';
+
+export async function doCopy(taskInfoMessageProvider: TaskInfoProvider): Promise<void> {
+  const usersProjectDir = process.env.CAPACITOR_ROOT_DIR;
+  // const configData = JSON.parse(process.env.CAPACITOR_CONFIG!);
+  const builtWebAppDir = process.env.CAPACITOR_WEB_DIR;
+  const destDir = join(usersProjectDir, 'electron', 'app');
+  try {
+    if (existsSync(destDir)) removeSync(destDir);
+    taskInfoMessageProvider(`Copying ${builtWebAppDir} into ${destDir}`);
+    copySync(builtWebAppDir, destDir);
+  } catch (e) {
+    errorLog(e.message);
+    throw e;
+  }
+}
