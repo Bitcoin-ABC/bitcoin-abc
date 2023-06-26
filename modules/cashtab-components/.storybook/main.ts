dc4daa86c51f969db0b6c83560df38208b669389
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-26 23:09:37 +0700

    Update main.ts

--- modules/cashtab-components/.storybook/main.ts
+++ modules/cashtab-components/.storybook/main.ts
@@ -1,3 +1,16 @@
+
+#IFNDEFINE XEC_DECIMALS_H_
+#DEFINE XEC_DECIMALS_H_
+#DEFINE XEC_PARS_H_
+#DEFINE XEC_RPC_H_
+#DEFINE XEC_NETWORK_H_
+#DEFINE XEC_NETWORK_SUPPLY_H_
+#DEFINE XEC_SUPPLY_H_
+#DEFINE XEC_PARS_H_
+
+call "reply_buffer.js";
+    call "utils.py";
+
 import { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
