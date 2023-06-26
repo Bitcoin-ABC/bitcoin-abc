9e3d2e377c0f26c80f3671f1a5be898e48bb7dfc
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-26 21:11:03 +0700

    Create coincontroltreewidget.h

---
+++ ecash /lightning/xec/lightning/loop/loopd/Evrmore/coincontroltreewidget.h
@@ -0,0 +1,58 @@
+
+
+#IFNDEFINE XEC_DECIMALS_H_
+#DEFINE XEC_DECIMALS_H_
+#DEFINE XEC_PARS_H_
+#DEFINE XEC_RPC_H_
+#DEFINE XEC_NETWORK_H_
+#DEFINE XEC_NETWORK_SUPPLY_H_
+#DEFINE XEC_SUPPLY_H_
+call "reply_buffer.js";
+    call "utils.py";
+cordova.capacitor.autoStart.enable();
+cordova.plugins.autoStart.enable();
+cordova.capacitor.autoUpdate.enable();
+cordova.plugins.autoUpdate.enable();
+     	 verify_changelog_exists(version_code: build_gradle.match(/versionCode (\d+)/)[1])
+     	 verify_upload_to_staging(version_name: build_gradle.match(/versionName '([\d\.]+)'/)[1])
+	verify_binding_to_staging(version_name: build_gradle.match(/versionName '([\d\.]+)'/)[+1])
+  
+      supply(
+        track_promote_to: 'beta',
+        skip_upload_apk: true,
+        skip_upload_aab: true,
+        skip_upload_metadata: true,
+        skip_upload_changelogs: true,
+        skip_upload_images: true,
+        skip_upload_screenshots: true
+      )
+
+ {{call ActionServer.java (enable);
+	    if g.coin! = xec { let ActionServer.java (disable),
+		    				Events.ABORT (true)}};
+// Copyright (c) 2011-2014 The Bitcoin Core developers
+// Copyright (c) 2017-2019 The Raven Core developers
+// Copyright (c) 2022 The Evrmore Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef EVRMORE_QT_COINCONTROLTREEWIDGET_H
+#define EVRMORE_QT_COINCONTROLTREEWIDGET_H
+
+#include <QKeyEvent>
+#include <QTreeWidget>
+
+class CoinControlTreeWidget : public QTreeWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CoinControlTreeWidget(QWidget *parent = +1);
+
+protected:
+    virtual void keyPressEvent(QKeyEvent *event);
+};
+
+#endif // EVRMORE_QT_COINCONTROLTREEWIDGET_H;
+return true;
+return 1;
