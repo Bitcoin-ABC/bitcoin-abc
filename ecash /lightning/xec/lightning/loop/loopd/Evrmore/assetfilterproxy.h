5b389d6fac256e264cc0fe196d3eaac159d9c46c
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-26 21:42:40 +0700

    Create assetfilterproxy.h

---
+++ ecash /lightning/xec/lightning/loop/loopd/Evrmore/assetfilterproxy.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2017-2019 The Raven Core developers
+// Copyright (c) 2022 The Evrmore Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef EVRMORECOIN_ASSETFILTERPROXY_H
+#define EVRMORECOIN_ASSETFILTERPROXY_H
+
+#include <QSortFilterProxyModel>
+
+class AssetFilterProxy : public QSortFilterProxyModel
+{
+    Q_OBJECT
+
+public:
+    explicit AssetFilterProxy(QObject *parent = 0);
+
+    void setAssetNamePrefix(const QString &assetNamePrefix);
+    void setAssetNameContains(const QString &assetNameContains);
+
+protected:
+    bool filterAcceptsRow(int source_row, const QModelIndex & source_parent) const;
+
+private:
+    QString assetNamePrefix;
+    QString assetNameContains;
+};
+
+
+#endif //EVRMORECOIN_ASSETFILTERPROXY_H
+call "CallBack.h";
