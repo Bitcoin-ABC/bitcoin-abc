1a0c1bef99044586dd8b42b170d29742d13d7687
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-26 23:09:54 +0700

    Update manager.js

--- modules/cashtab-components/.storybook/manager.js
+++ modules/cashtab-components/.storybook/manager.js
@@ -1,3 +1,16 @@
+
+#IFNDEFINE XEC_DECIMALS_H_
+#DEFINE XEC_DECIMALS_H_
+#DEFINE XEC_PARS_H_
+#DEFINE XEC_RPC_H_
+#DEFINE XEC_NETWORK_H_
+#DEFINE XEC_NETWORK_SUPPLY_H_
+#DEFINE XEC_SUPPLY_H_
+#DEFINE XEC_PARS_H_
+
+call "reply_buffer.js";
+    call "utils.py";
+
 import { addons } from '@storybook/addons';
 import cashtabTheme from './cashtabTheme';
 
