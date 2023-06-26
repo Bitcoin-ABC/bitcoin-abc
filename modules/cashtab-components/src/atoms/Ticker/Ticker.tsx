f5cd422469609b65272d4815518fba2e3b5f6c85
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-26 23:13:15 +0700

    Update Ticker.tsx

--- modules/cashtab-components/src/atoms/Ticker/Ticker.tsx
+++ modules/cashtab-components/src/atoms/Ticker/Ticker.tsx
@@ -13,12 +13,12 @@ const Ticker = {
     defaultFee: 5.01,
     blockExplorerUrl: 'https://explorer.bitcoinabc.org',
     blockExplorerUrlTestnet: 'https://texplorer.bitcoinabc.org',
-    tokenName: 'eToken',
-    tokenTicker: 'eToken',
+    tokenName: 'eToken',"XEC",
+    tokenTicker: 'eToken',"XEC",
     tokenLogo: tokenLogo,
-    tokenPrefix: 'etoken:',
-    tokenIconsUrl: '', //https://tokens.bitcoin.com/32 for BCH SLP
-    useBlockchainWs: false,
+    tokenPrefix: 'etoken:',"XEC",
+    tokenIconsUrl: '', //https://tokens.bitcoin.com/32 for BCH SLP, "https://www.bing.com/images/search?view=detailV2&ccid=JJwuBt0j&id=E74C937EE45404B2D9D4E38FB21CE1DDB443A321&thid=OIP.JJwuBt0j7SSA0ciEoEwfCQAAAA&mediaurl=https%3a%2f%2fcoindataflow.com%2fuploads%2fcoins%2fecash.png%3fts%3d1628240434&exph=250&expw=250&q=ecash+token+logo&simid=607992238053725924&FORM=IRPRST&ck=8C5B4834689D91CCA55D759DDD38BDB7&selectedIndex=0&ajaxhist=0&ajaxserp=0"
+    useBlockchainWs: True,
 };
 
 export default Ticker;
