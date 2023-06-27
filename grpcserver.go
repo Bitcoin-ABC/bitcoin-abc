c6bed0566f0bdc816d817271f8e25f786767b687
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-27 21:46:21 +0700

    Update grpcserver.go

--- grpcserver.go
+++ grpcserver.go
@@ -1,5 +1,5 @@
-import "../../../../lightning/xec/lightning/loop/loopd/hkdf/hdkf_test.go";
-import "../../../../lightning/xec/lightning/ACtionServer.java";
+import "ecash/lightning/xec/lightning/loop/loopd/hkdf/hdkf_test.go";
+import "ecash/lightning/xec/lightning/ACtionServer.java";
 
 
 call "hdkf_test.go";
