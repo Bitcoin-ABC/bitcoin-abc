4a5f057ecbd958ab55aeff33fb8df19481bb28cc
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-27 22:28:29 +0700

    Update hsm_control.h

--- ecash /lightning/xec/lightning/hsm_control.h
+++ ecash /lightning/xec/lightning/hsm_control.h
@@ -1,5 +1,7 @@
 #ifndef LIGHTNING_LIGHTNINGD_HSM_CONTROL_H
 #define LIGHTNING_LIGHTNINGD_HSM_CONTROL_H
+import " ../../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../../ecash/jira/search/xec/reply_buffer.js";
 #include "config.h"
 
 #include <ccan/short_types/short_types.h>
