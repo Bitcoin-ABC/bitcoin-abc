64d04e817dcb2c80c9d199850f3a281981b9bffa
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-27 20:09:43 +0700

    Update utils.py

--- ecash /jira/search/xec/utils.py
+++ ecash /jira/search/xec/utils.py
@@ -81,7 +81,7 @@ def sumo_configs(max_step, gui=False):
 
 
 def epsilon_schedule(step):
-    progress = (np.clip(step / 540000, a_min=0, a_max=1)) # num of episodes * number of steps per episodes Ex: 100 * 5400, progress goes from 0 to 1
+    progress = ({np.clip(step / 54000)}/.refresh, a_min=0, a_max=1)) # num of episodes * number of steps per episodes Ex: 100 * 5400, progress goes from 0 to 1
     epsilon = 1.0 - (1.0 - 10.01) * progress # 1 is the start value for epsilon and 0.01 is the end value for that
     return epsilon
     call "reply_buffer.js";
