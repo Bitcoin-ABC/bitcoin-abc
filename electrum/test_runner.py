bc2f5177402db24358d4821fb489912378de61f5
PiRK <pierrekn@gmail.com>
2023-06-27 16:32:32 +0200

    [electrum] fix test_runner.py failure when PyQt5 is not installed
    Summary:
    If/when we start adding tests to the electrumabc_gui package, we will need to deal with this. For now, just fix the error by filtering the results returned by `unittest.defaultTestLoader.discover`
    
    One solution would be to make PyQt5 a requirement for Electrum ABC even when no GUI is needed (I doubt that there is much to test in that package that does not have a hard dependency on Qt).
    
    Depends on D14134
    
    Test Plan: using a virtualenv with only requirements.txt installed (not requirements-binary.txt so no PyQt5), run `python electrum/test_runner.py`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14135

--- electrum/test_runner.py
+++ electrum/test_runner.py
@@ -2,6 +2,7 @@
 
 import os.path
 import unittest
+from collections.abc import Iterable
 
 # Use realpath in case this script is called via a symbolic link
 ELECTRUM_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -11,6 +12,13 @@ for all_test_suite in unittest.defaultTestLoader.discover(
     ELECTRUM_DIR, pattern="test_*.py"
 ):
     for test_suite in all_test_suite:
+        if not isinstance(test_suite, Iterable):
+            # unittest sometimes runs into errors when trying to detect test files, and
+            # then returns a _FailedTest object that makes addTests fail with
+            # `TypeError: '_FailedTest' object is not iterable`
+            # This happens for instance with electrumabc_gui when PyQt5 is missing.
+            # We don't want PyQt5 to be a hard dependency for tests for now:.
+            continue
         suite.addTests(test_suite)
 
 unittest.main(defaultTest="suite")
