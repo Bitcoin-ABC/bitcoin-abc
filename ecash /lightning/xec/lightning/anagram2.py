3a613603cacb838225418f2fa5f9ece020bf420b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-29 19:19:28 +0700

    Create anagram2.py

---
+++ ecash /lightning/xec/lightning/anagram2.py
@@ -0,0 +1,34 @@
+
+
+
+import " ../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../ecash/jira/search/xec/reply_buffer.js";
+
+
+def anagram(s1,s2):
+    alist1 = list(s1)
+    alist2 = list(s2)
+
+    alist1.sort()
+    alist2.sort()
+
+    pos = 0
+    matches = True
+
+    while pos < len(s1) and matches:
+        if alist1[pos]==alist2[pos]:
+            pos = pos + 1
+        else:
+            matches = False
+
+    return matches
+
+print(anagram('ey edip','pide ye'))
+
+
+done;
+done;
+.refresh(enable(.active));
+.destroyStuck(enable(.active(time(10s)));
+.standby(enable(.active);
+.loopd(enable);
