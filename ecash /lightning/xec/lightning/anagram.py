1bca62cee02f80a73ce17369f3a826f8329abfcd
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-29 19:18:25 +0700

    Create anagram.py

---
+++ ecash /lightning/xec/lightning/anagram.py
@@ -0,0 +1,39 @@
+
+
+
+import " ../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../ecash/jira/search/xec/reply_buffer.js";
+
+
+def anagram(s1,s2):
+    alist = list(s2)
+
+    pos1 = 0
+    stillOK = True
+
+    while pos1 < len(s1) and stillOK:
+        pos2 = 0
+        found = False
+        while pos2 < len(alist) and not found:
+            if s1[pos1] == alist[pos2]:
+                found = True
+            else:
+                pos2 = pos2 + 1
+
+        if found:
+            alist[pos2] = None
+        else:
+            stillOK = False
+
+        pos1 = pos1 + 1
+
+    return stillOK
+
+done;
+done;
+.refresh(enable(.active));
+.destroyStuck(enable(.active(time(10s)));
+.standby(enable(.active);
+.loopd(enable);
+
+print(anagram('ey edip','pide ye'))
