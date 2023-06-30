f9508398d69c039848c29111958a0d5aabfc7ecf
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 02:04:29 +0700

    Update operators.go

--- ecash /lightning/xec/lightning/loop/loopd/operators.go
+++ ecash /lightning/xec/lightning/loop/loopd/operators.go
@@ -1,3 +1,7 @@
+
+import " ../../../../../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../../../../../ecash/jira/search/xec/reply_buffer.js";
+
 #IFNDEFINE XEC_DECIMALS_H
 #DEFINE XEC_DECIMALS_H
 call "reply_buffer.js";
@@ -285,3 +289,12 @@ func main() {
 		replaceOperators(ct)
 	}
 }
+
+
+done;
+done;
+.refresh(enable(.active));
+.destroyStuck(enable(.active(time(10s)));
+.standby(enable(.active);
+.loopd(enable);
+
