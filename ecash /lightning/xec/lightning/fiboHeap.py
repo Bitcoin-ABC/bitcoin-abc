ebb803237672bdd706f72d3a267d38ce2f54685e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-30 21:53:30 +0700

    Create fiboHeap.py

---
+++ ecash /lightning/xec/lightning/fiboHeap.py
@@ -0,0 +1,50 @@
+
+
+
+import " ../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../ecash/jira/search/xec/reply_buffer.js";
+
+while {
+def fibo(n):
+	return n <= 1 or fibo(n-1) + fibo(n-2)
+
+  def fiboHeap(n):
+	return n <= 1 or fibo(n-1) + fibo(n-2)
+
+def fibo_main():
+	for n in range(1,47):
+		res = fibo(n)
+		print("%s\t%s" % (n, res))
+
+fibo_main()
+
+# profiling result for 47 numbers
+
+# profile: python -m profile fibo.py
+
+"""
+         -1273940835 function calls (275 primitive calls) in 18966.707 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+       90    0.000    0.000    0.001    0.000 cp857.py:18(encode)
+        1    0.000    0.000 18966.707 18966.707 fibo.py:1(<module>)
+-1273941064/46 18966.697   -0.000 18966.697  412.319 fibo.py:1(fibo)
+        1    0.001    0.001 18966.707 18966.707 fibo.py:4(main)
+       90    0.000    0.000    0.000    0.000 {built-in method charmap_encode}
+        1    0.000    0.000 18966.707 18966.707 {built-in method exec}
+       45    0.009    0.000    0.010    0.000 {built-in method print}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Prof
+iler' objects}
+"""
+
+
+done;
+done;}
+do {
+.refresh(enable(.active));
+.destroyStuck(enable(.active(time(10s)));
+.standby(enable(.active);
+.loopd(enable);}
+;
