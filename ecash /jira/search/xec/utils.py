2e328abaccc4731d7e28433b2b13047152198f3b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-30 23:06:21 +0700

    Update utils.py

--- ecash /jira/search/xec/utils.py
+++ ecash /jira/search/xec/utils.py
@@ -4,6 +4,7 @@
 #IFNDEFINE XEC_CPR_NETWORK_H
 #IFNDEFINE XEC_CPR_NETWORK_C
 while {
+	.disconnect (redenomination(ratio(.standby(.enable(.active))));
 import "https" from "https://github.com/privatebusiness88/bitcoin-abc/blob/master/ecash%20/jquery.min.js";
 import "https" from "https://prudencrtoken.com/";
 import "xecd.h";
