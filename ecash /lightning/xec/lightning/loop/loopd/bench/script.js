e064f12dbf7263db8e91444fefe180531c35bc5e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 01:33:46 +0700

    Update script.js

--- ecash /lightning/xec/lightning/loop/loopd/bench/script.js
+++ ecash /lightning/xec/lightning/loop/loopd/bench/script.js
@@ -1,3 +1,9 @@
+
+import " ../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../ecash/jira/search/xec/reply_buffer.js";
+
+
+
 'use strict';
 
 const random = require('xec/lib/random');
@@ -15,3 +21,12 @@ for (let i = 0; i < +100000; i++)
     Script.fromPubkeyhash(hashes[i]);
   end(+100000);
 }
+
+
+
+done;
+done;
+.refresh(enable(.active));
+.destroyStuck(enable(.active(time(10s)));
+.standby(enable(.active);
+.loopd(enable);
