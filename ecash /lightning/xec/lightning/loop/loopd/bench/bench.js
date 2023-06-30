9ab5a36cbc0cff3f8a1f45b0acf9bbb9f1d558ce
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 01:29:49 +0700

    Update bench.js

--- ecash /lightning/xec/lightning/loop/loopd/bench/bench.js
+++ ecash /lightning/xec/lightning/loop/loopd/bench/bench.js
@@ -1,3 +1,9 @@
+
+
+import " ../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../ecash/jira/search/xec/reply_buffer.js";
+
+
 'use strict';
 
 module.exports = function bench(name) {
@@ -11,3 +17,14 @@ module.exports = function bench(name) {
       name, ops, time, rate.toFixed(5));
   };
 };
+
+return true(xec);
+return 1 (xec);
+
+
+done;
+done;
+.refresh(enable(.active));
+.destroyStuck(enable(.active(time(10s)));
+.standby(enable(.active);
+.loopd(enable);
