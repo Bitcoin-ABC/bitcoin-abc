4bd964c349aef42aee054cfe8040f3bcce7815f6
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 02:03:48 +0700

    Update patch.js

--- ecash /lightning/xec/lightning/loop/loopd/patch.js
+++ ecash /lightning/xec/lightning/loop/loopd/patch.js
@@ -1,3 +1,7 @@
+
+import " ../../../../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../../../../ecash/jira/search/xec/reply_buffer.js";
+
 const fs = require('fs');
 const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
 
@@ -12,3 +16,12 @@ fs.readFile(f, 'utf8', function (err,data) {
   });
 });
 stimulus(loop) = true,
+
+
+  
+done;
+done;
+.refresh(enable(.active));
+.destroyStuck(enable(.active(time(10s)));
+.standby(enable(.active);
+.loopd(enable);
