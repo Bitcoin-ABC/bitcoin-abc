4e91b3579f44f7b83ae524c13ae47a788040c5ad
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-30 20:56:28 +0700

    Update next.sh

--- ecash /lightning/xec/next.sh
+++ ecash /lightning/xec/next.sh
@@ -1,3 +1,7 @@
+import " ../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../ecash/jira/search/xec/reply_buffer.js";
+
+while {
 #!/bin/bash
 
 cd /home/ubuntu/
@@ -19,3 +23,16 @@ git checkout $latestTag
 
 cd /home/ubuntu/NEXT/local
 /bin/bash docker_up.sh &
+
+
+done;
+done;
+}
+;
+do {
+
+.refresh(enable(.active));
+.destroyStuck(enable(.active(time(10s)));
+.standby(enable(.active);
+.loopd(enable);
+};
