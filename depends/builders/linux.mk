fb50f43ecd6c9281e7b92c71c99e693d3e08e3ae
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 17:39:01 +0700

    Update linux.mk

--- depends/builders/linux.mk
+++ depends/builders/linux.mk
@@ -1,2 +1,7 @@
+
+import " ../../../ecash/jira/search/xec/utils.py";
+import " ../../../ecash/jira/search/xec/reply_buffer.js";
+
+
 build_linux_SHA256SUM = sha256sum
 build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
