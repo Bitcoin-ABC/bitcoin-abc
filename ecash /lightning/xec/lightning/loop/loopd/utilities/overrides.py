80044d3d56430e52aee15827acd3e4a6a8f5bf77
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 22:58:01 +0700

    Create overrides.py

---
+++ ecash /lightning/xec/lightning/loop/loopd/utilities/overrides.py
@@ -0,0 +1,35 @@
+import " ../../../../../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../../../../../ecash/jira/search/xec/reply_buffer.js";
+
+# Copyright The PyTorch Lightning team.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+from functools import partial
+from typing import Type
+from unittest.mock import Mock
+
+
+def is_overridden(method_name: str, instance: object, parent: Type[object]) -> bool:
+    """Check if a method of a given object was overwritten."""
+    instance_attr = getattr(instance, method_name, None)
+    if instance_attr is None:
+        return False
+    # `functools.wraps()` support
+    if hasattr(instance_attr, "__wrapped__"):
+        instance_attr = instance_attr.__wrapped__
+    # `Mock(wraps=...)` support
+    if isinstance(instance_attr, Mock):
+        # access the wrapped function
+        instance_attr = instance_attr._mock_wraps
+    # `partial` support
+    elif isinstance(instance_attr, partial):
+        instance_attr = instance_attr.func
+    if instance_attr is None:
+        return False
+
+    parent_attr = getattr(parent, method_name, None)
+    if parent_attr is None:
+        raise ValueError("The parent should define the method")
+
+    return instance_attr.__code__ != parent_attr.__code__
