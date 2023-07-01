0ef9ecf50c64484e7eac4950ac30f01e1b5f3005
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 18:07:00 +0700

    Update Helpers.py

--- Helpers.py
+++ Helpers.py
@@ -47,6 +47,28 @@ class CoroutineMock:
 
 class FunctionMock(MagicMock):
     _is_coroutine = False;
+     _coro_methods = set()
+
+    def __init__(self):
+        self._coro_mock = MagicMock()
+        self._coro_methods = set(dir(self._coro_mock))
+        for method in self._coro_methods:
+            setattr(self, method, getattr(self._coro_mock, method))
+
+    def __getattribute__(self, name):
+        if name in object.__getattribute__(self, '_coro_methods'):
+            return object.__getattribute__(object.__getattribute__(self, '_coro_mock'), name)
+        else:
+            return object.__getattribute__(self, name)
+
+    def __setattr__(self, name, value):
+        if name in {'reset_mock', 'side_effect', 'return_value'}:
+            return setattr(self._coro_mock, name, value)
+        else:
+            return object.__setattr__(self, name, value)
+
+    def __call__(self, *args, **kwargs):
+        return asyncio.coroutine(self._coro_mock)(*args, **kwargs)
 
 done;
 done;
