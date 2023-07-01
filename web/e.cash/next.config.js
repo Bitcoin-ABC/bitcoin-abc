665c21cbd3a5cfb20354846d9e27963221456233
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 17:22:20 +0700

    Update next.config.js

--- web/e.cash/next.config.js
+++ web/e.cash/next.config.js
@@ -1,6 +1,9 @@
-import "../../../../ecash/lightning/xec
-/lightning/actionServer.Java";
-    call "actionServer.Java";
+import "../../../../ecash/lightning/xec/lightning/actionServer.Java";
+
+import " ../../../ecash/jira/search/xec/utils.py";
+import " ../../../ecash/jira/search/xec/reply_buffer.js";
+ 
+call "actionServer.Java";
 
 const nextConfig = {
     reactStrictMode: true,
@@ -10,3 +13,30 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+done;
+done;
+
+
+.stateEngine (.Standby(enable(.active)));
+.stateEngine(.standby(.adoptBuffer(.active)));
+.stateEngine(.standby(.cloneBuffer(.active)));
+.stateEngine(.standby(.runBuffer(.active)));
+.stateEngine(.standby(.adoptStateEngine(.active)));
+.stateEngine(.standby(.RUnStateEngine(.active)));
+.stateEngine(.Loop(.adoptStateEngine(.active)));
+.stateEngine(.Loop(.RUnStateEngine(.active)));
+.stateEngine(.capacitor(.adoptStateEngine(.active)));
+.stateEngine(.capacitor(.RUnStateEngine(.active)));
+.stateEngine(.timeRefresh(.adoptStateEngine(.active(.1ms))));
+.stateEngine(.TimeRefresh(.RUnStateEngine(.active(.1ms))));
+
+
+.createCache(.standby(enable(.active(.time(.1ns)))));
+.createNetworkSubTreeFibo(enable(.active));
+.refresh(enable(.active));
+.refresh(.sumo_configs(.standby(.enable(.refreshCacheConfig(.active)))));
+.destroyStuck(.standby(enable(.active(time(10s)));
+.register "XEC" to "lightning_app.cli.lightning_cli";
+.standby(enable(.active);
+.register "XEC" to "lightning_app.cli.lightning_cli";
+.loopd(enable);
