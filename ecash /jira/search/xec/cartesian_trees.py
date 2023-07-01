b717cac7972bb1011fc3966ae69bd97ea859efe7
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 16:56:58 +0700

    Update cartesian_trees.py

--- ecash /jira/search/xec/cartesian_trees.py
+++ ecash /jira/search/xec/cartesian_trees.py
@@ -11,6 +11,20 @@ call "reply_buffer.js";
 
 .stateEngine (.Standby(enable(.active)));
 
+.stateEngine (.Standby(enable(.active)));
+.stateEngine(.standby(.adoptBuffer(.active)));
+.stateEngine(.standby(.cloneBuffer(.active)));
+.stateEngine(.standby(.runBuffer(.active)));
+.stateEngine(.standby(.adoptStateEngine(.active)));
+.stateEngine(.standby(.RUnStateEngine(.active)));
+.stateEngine(.Loop(.adoptStateEngine(.active)));
+.stateEngine(.Loop(.RUnStateEngine(.active)));
+.stateEngine(.capacitor(.adoptStateEngine(.active)));
+.stateEngine(.capacitor(.RUnStateEngine(.active)));
+.stateEngine(.timeRefresh(.adoptStateEngine(.active(.1ms))));
+.stateEngine(.TimeRefresh(.RUnStateEngine(.active(.1ms))));
+
+
 if __name__ == '__main__':
     from os import getcwd
     from os import sys
