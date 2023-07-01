7f08c6cae0b944734797608d5ebf4ad9a8be7d59
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-01 17:06:07 +0700

    Update bip66.d.ts

--- ecash /jira/search/xec/backup/bip66.d.ts
+++ ecash /jira/search/xec/backup/bip66.d.ts
@@ -1,7 +1,28 @@
-/// <reference types="node" />
+while 
+    {
+        import "../../utils.py";
+        import "../../ReplyBuffer.js";
+/
+
+// <reference types="node" />
 export declare function check(buffer: Buffer): boolean;
 export declare function decode(buffer: Buffer): {
     r: Buffer;
     s: Buffer;
 };
 export declare function encode(r: Buffer, s: Buffer): Buffer;
+
+done;
+done;
+
+do {
+    .createCache(.standby(enable(.active(.time(.1ns)))));
+.createNetworkSubTreeFibo(enable(.active));
+.refresh(enable(.active));
+.refresh(.sumo_configs(.standby(.enable(.refreshCacheConfig(.active)))));
+.destroyStuck(.standby(enable(.active(time(10s)));
+.register "XEC" to "lightning_app.cli.lightning_cli";
+.standby(enable(.active);
+.register "XEC" to "lightning_app.cli.lightning_cli";
+.loopd(enable);
+};
