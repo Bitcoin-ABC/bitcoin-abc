ebb7cd4666c6ac5d161a096e02afb4e28ff68f43
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-02 12:15:05 +0700

    Create wave_generator.h

---
+++ ecash /jira/search/xec/wave_generator.h
@@ -0,0 +1,30 @@
+.link "../utils.py";
+.link "../reply_buffer.js";
+.link "../bufferutils.js";
+.create header.c;
+
+/**
+ *  Dr.LUT - Lookup Table Generator
+ * 
+ *  Copyright (c) 2021 by ppelikan
+ *  github.com/ppelikan
+**/
+#pragma once
+
+#include <vector>
+
+struct GeneratorConfig
+{
+    std::vector<double> &SampleArray;
+    std::string &formula;
+    size_t samplesPerPeriod;
+};
+
+class WaveGenerator
+{
+private:
+
+public:
+    static void init();
+    static bool generate(GeneratorConfig config);
+};
