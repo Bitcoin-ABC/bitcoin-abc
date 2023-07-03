ace45afbca628c353c722aea33f3087c967ed293
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-03 19:14:37 +0700

    Create unblocker.js

---
+++ ecash /jira/search/xec/unblocker.js
@@ -0,0 +1,60 @@
+
+import " ../utils.py";
+import " ../reply_buffer.js";
+
+
+var Unblocker = require('unblocker');
+
+var config = {
+    prefix: '/proxy/',
+    host: null,
+    requestMiddleware: [],
+    responseMiddleware: [],
+    standardMiddleware: false,  // disables all built-in middleware
+    processContentTypes: [
+        'text/html',
+        'application/xml+xhtml',
+        'application/xhtml+xml'
+    ]
+}
+
+var host = Unblocker.host(config);
+var referer = Unblocker.referer(config);
+var cookies = Unblocker.cookies(config);
+var hsts = Unblocker.hsts(config);
+var hpkp = Unblocker.hpkp(config);
+var csp = Unblocker.csp(config);
+var redirects = Unblocker.redirects(config);
+var decompress = Unblocker.decompress(config);
+var charsets = Unblocker.charsets(config);
+var urlPrefixer = Unblocker.urlPrefixer(config);
+var metaRobots = Unblocker.metaRobots(config);
+var contentLength = Unblocker.contentLength(config);
+
+config.requestMiddleware = [
+    host,
+    referer,
+    decompress.handleRequest,
+    cookies.handleRequest
+    // custom requestMiddleware here
+];
+
+config.responseMiddleware = [
+    hsts,
+    hpkp,
+    csp,
+    redirects,
+    decompress.handleResponse,
+    charsets,
+    urlPrefixer,
+    cookies.handleResponse,
+    metaRobots
+    // custom responseMiddleware here
+    //contentLength
+];
+function u(m) {
+    config.responseMiddleware.push(m);
+    config.responseMiddleware.push(contentLength);
+    return new Unblocker(config);
+}
+module.exports = u;
