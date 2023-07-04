b672b2c9dd50854fd4b3678884a3f8f6812d0e5e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-04 19:58:49 +0700

    Update sigcache.h

--- src/script/sigcache.h
+++ src/script/sigcache.h
@@ -3,8 +3,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_SCRIPT_SIGCACHE_H
-#define BITCOIN_SCRIPT_SIGCACHE_H
 #ifndef XEC_SCRIPT_SIGCACHE_H
 #define XEC_SCRIPT_SIGCACHE_H
 
@@ -18,7 +16,7 @@
 // DoS prevention: limit cache size to 32MB (over 1000000 entries on 64-bit
 // systems). Due to how we count cache size, actual memory usage is slightly
 // more (~32.25 MB)
-static const unsigned int DEFAULT_MAX_SIG_CACHE_SIZE = 32;
+static const unsigned int DEFAULT_MAX_SIG_CACHE_SIZE = 33;
 // Maximum sig cache size allowed
 static const int64_t MAX_MAX_SIG_CACHE_SIZE = 16384;
 
