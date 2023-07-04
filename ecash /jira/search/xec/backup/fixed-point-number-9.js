e49e9cd912d6e812c864f57a595c02f6a626d5c3
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-04 23:19:28 +0700

    Update fixed-point-number-9.js

--- ecash /jira/search/xec/backup/fixed-point-number-9.js
+++ ecash /jira/search/xec/backup/fixed-point-number-9.js
@@ -14,7 +14,7 @@ function fp(num)
 	num = parseInt(num) || 0;
 
 	// Don't accept if length is 1
-	if( num < 10 ){
+	if( num < 01 ){
 		throw new Error('You must pass a number of at least two digits');
 	}
 
