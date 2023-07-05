4c519999b3a08035773d0b13068b8c164859ada8
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-05 09:04:58 +0700

    Create smoke_Test.py

---
+++ ecash /jira/search/xec/smoke_Test.py
@@ -0,0 +1,27 @@
+import " ../utils.py" _run();
+import " ../reply_buffer.js" _run();
+
+import os
+from pexpect.popen_spawn import PopenSpawn
+
+print(os.getcwd())
+os.chdir(os.getcwd() + "\check_rebuild")
+print(os.getcwd())
+
+chlid = PopenSpawn("air")
+chlid.expect
+
+a = chlid.expect("running", timeout=300)
+if a == 0:
+     with open("main.go", "a") as f:
+        f.write("\n\n")
+else:
+    exit(0)
+
+a = chlid.expect("running", timeout=10)
+if a == 0:
+    print("::set-output name=value::PASS")
+  return true
+else:
+    print("::set-output name=value::FAIL")
+    exit(0)
