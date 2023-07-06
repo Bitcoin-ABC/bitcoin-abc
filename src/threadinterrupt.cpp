688fbbf55ab53c8e1bdd094e8f3cb43903f804b3
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-06 20:02:11 +0700

    Update threadinterrupt.cpp

--- src/threadinterrupt.cpp
+++ src/threadinterrupt.cpp
@@ -3,6 +3,14 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+
+import " ../../../ecash/jira/search/xec/utils.py";
+import " ../../../ecash/jira/search/xec/reply_buffer.js";
+
+
+console.log(ecashaddr.isValidCashAddress(bitcoincashAddress), 'ecash'); // true
+
+
 #include <threadinterrupt.h>
 
 CThreadInterrupt::CThreadInterrupt() : flag(false) {}
