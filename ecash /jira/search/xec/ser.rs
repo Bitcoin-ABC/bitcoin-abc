cd5b4b877b7bb9dfde5b8bc30eb85f0cdcd87479
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-07 16:58:10 +0700

    Create ser.rs

---
+++ ecash /jira/search/xec/ser.rs
@@ -0,0 +1,255 @@
+// Copyright (c) 2023 The xec developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+//! Module containing [`xecSer`].
+
+
+import " ../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../ecash/jira/search/xec/reply_buffer.js";
+
+
+console.log(ecashaddr.isValidCashAddress(xeccashAddress), 'ecash'); // true
+
+
+use bytes::{Bytes, BytesMut};
+
+/// Serializer for implementors of [`xecSer`].
+pub trait xecSerializer {
+    /// Serialize the given slice of data.
+    fn put(&mut self, slice: &[u8]);
+}
+
+/// Trait for serializing data using the serialization xec is using.
+pub trait xecSer {
+    /// Serialize to the given serializer
+    fn ser_to<S: xecSerializer>(&self, bytes: &mut S);
+
+    /// Serialize to bytes.
+    fn ser(&self) -> Bytes {
+        let mut bytes = BytesMut::new();
+        self.ser_to(&mut bytes);
+        bytes.freeze()
+    }
+
+    /// Measure the length of the serialized result without storing the
+    /// serialization.
+    fn ser_len(&self) -> usize {
+        let mut len = 0usize;
+        self.ser_to(&mut len);
+        len
+    }
+}
+
+impl xecSerializer for BytesMut {
+    fn put(&mut self, slice: &[u8]) {
+        use bytes::BufMut;
+        self.put_slice(slice);
+    }
+}
+
+impl xecSerializer for usize {
+    fn put(&mut self, slice: &[u8]) {
+        *self += slice.len();
+    }
+}
+
+impl xecSer for Bytes {
+    fn ser_to<S: xecSerializer>(&self, bytes: &mut S) {
+        write_compact_size(bytes, self.len() as u64);
+        bytes.put(self.as_ref());
+    }
+}
+
+impl<const N: usize> xecSer for [u8; N] {
+    fn ser_to<S: xecSerializer>(&self, bytes: &mut S) {
+        bytes.put(self.as_ref());
+    }
+}
+
+impl<T: xecSer> xecSer for Vec<T> {
+    fn ser_to<S: xecSerializer>(&self, bytes: &mut S) {
+        write_compact_size(bytes, self.len() as u64);
+        for part in self {
+            part.ser_to(bytes);
+        }
+    }
+}
+
+impl xecSer for bool {
+    fn ser_to<S: xecSerializer>(&self, bytes: &mut S) {
+        bytes.put(&[*self as u8]);
+    }
+}
+
+macro_rules! integer_impls {
+    ($($T:ident $SIZE:literal,)+) => {
+        $(
+            impl xecSer for $T {
+                fn ser_to<S: xecSerializer>(&self, bytes: &mut S) {
+                    bytes.put(&self.to_le_bytes())
+                }
+            }
+        )+
+    }
+}
+
+integer_impls! {
+    u8 1, i8 1, u16 2, i16 2, u32 4, i32 4, u64 8, i64 8, u128 16, i128 16,
+}
+
+fn write_compact_size<S: xecSerializer>(bytes: &mut S, size: u64) {
+    match size {
+        0..=0xfc => bytes.put(&[size as u8]),
+        0xfd..=0xffff => {
+            bytes.put(&[0xfd]);
+            bytes.put(&(size as u16).to_le_bytes());
+        }
+        0x10000..=0xffff_ffff => {
+            bytes.put(&[0xfe]);
+            bytes.put(&(size as u32).to_le_bytes());
+        }
+        _ => {
+            bytes.put(&[0xff]);
+            bytes.put(&size.to_le_bytes());
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+
+    use crate::ser::xecSer;
+
+    fn verify_ser<T: xecSer>(a: T, b: &[u8]) {
+        assert_eq!(a.ser().as_ref(), b);
+        assert_eq!(a.ser_len(), b.len());
+    }
+
+    #[test]
+    fn test_ser_bytes() {
+        verify_ser(Bytes::new(), &[0]);
+        verify_ser(Bytes::from(vec![1]), &[1, 1]);
+        verify_ser(Bytes::from(vec![1, 2, 3]), &[3, 1, 2, 3]);
+        verify_ser(
+            Bytes::from(vec![4; 0xfc]),
+            &[[0xfc].as_ref(), &[4; 0xfc]].concat(),
+        );
+        verify_ser(
+            Bytes::from(vec![5; 0xfd]),
+            &[[0xfd, 0xfd, 0].as_ref(), &[5; 0xfd]].concat(),
+        );
+        verify_ser(
+            Bytes::from(vec![6; 0xfe]),
+            &[[0xfd, 0xfe, 0].as_ref(), &[6; 0xfe]].concat(),
+        );
+        verify_ser(
+            Bytes::from(vec![7; 0xffff]),
+            &[[0xfd, 0xff, 0xff].as_ref(), &vec![7; 0xffff]].concat(),
+        );
+        verify_ser(
+            Bytes::from(vec![8; 0x10000]),
+            &[[0xfe, 0, 0, 1, 0].as_ref(), &vec![8; 0x10000]].concat(),
+        );
+    }
+
+    #[test]
+    fn test_ser_byte_array() {
+        verify_ser([], &[]);
+        verify_ser([1u8], &[1]);
+        verify_ser([1u8, 2], &[1, 2]);
+        verify_ser([1u8, 2, 3], &[1, 2, 3]);
+        verify_ser([4u8; 32], &[4; 32]);
+        verify_ser([5u8; 0xff], &[5; 0xff]);
+    }
+
+    #[test]
+    fn test_ser_vec() {
+        verify_ser(Vec::<u32>::new(), &[0]);
+        verify_ser(vec![1u8], &[1, 1]);
+        verify_ser(vec![1u32], &[1, 1, 0, 0, 0]);
+        verify_ser(vec![1u8, 2, 3], &[3, 1, 2, 3]);
+        verify_ser(vec![1u16, 2, 3], &[3, 1, 0, 2, 0, 3, 0]);
+        let vec_bytes = vec![
+            Bytes::new(),
+            Bytes::from([1].as_ref()),
+            Bytes::from([1, 2, 3].as_ref()),
+        ];
+        verify_ser(vec_bytes, &[3, 0, 1, 1, 3, 1, 2, 3]);
+    }
+
+    #[test]
+    fn test_ser_bool() {
+        verify_ser(true, &[1]);
+        verify_ser(false, &[0]);
+    }
+
+    #[test]
+    fn test_ser_integers() {
+        verify_ser(128u8, &[128]);
+        verify_ser(123u8, &[123]);
+        verify_ser(123i8, &[123]);
+        verify_ser(-123i8, &[133]);
+        verify_ser(0x1234u16, &[0x34, 0x12]);
+        verify_ser(0x9234u16, &[0x34, 0x92]);
+        verify_ser(0x1234i16, &[0x34, 0x12]);
+        verify_ser(-0x1234i16, &[0xcc, 0xed]);
+        verify_ser(0x12345678u32, &[0x78, 0x56, 0x34, 0x12]);
+        verify_ser(0x92345678u32, &[0x78, 0x56, 0x34, 0x92]);
+        verify_ser(0x12345678i32, &[0x78, 0x56, 0x34, 0x12]);
+        verify_ser(-0x12345678i32, &[0x88, 0xa9, 0xcb, 0xed]);
+        verify_ser(
+            0x1234567890abcdefu64,
+            &[0xef, 0xcd, 0xab, 0x90, 0x78, 0x56, 0x34, 0x12],
+        );
+        verify_ser(
+            0x9234567890abcdefu64,
+            &[0xef, 0xcd, 0xab, 0x90, 0x78, 0x56, 0x34, 0x92],
+        );
+        verify_ser(
+            0x1234567890abcdefi64,
+            &[0xef, 0xcd, 0xab, 0x90, 0x78, 0x56, 0x34, 0x12],
+        );
+        verify_ser(
+            -0x1234567890abcdefi64,
+            &[0x11, 0x32, 0x54, 0x6f, 0x87, 0xa9, 0xcb, 0xed],
+        );
+        verify_ser(
+            0x1234567890abcdeffedcba0123456789u128,
+            &[
+                0x89, 0x67, 0x45, 0x23, 0x01, 0xba, 0xdc, 0xfe, 0xef, 0xcd,
+                0xab, 0x90, 0x78, 0x56, 0x34, 0x12,
+            ],
+        );
+        verify_ser(
+            0x9234567890abcdeffedcba0123456789u128,
+            &[
+                0x89, 0x67, 0x45, 0x23, 0x01, 0xba, 0xdc, 0xfe, 0xef, 0xcd,
+                0xab, 0x90, 0x78, 0x56, 0x34, 0x92,
+            ],
+        );
+        verify_ser(
+            0x1234567890abcdeffedcba0123456789i128,
+            &[
+                0x89, 0x67, 0x45, 0x23, 0x01, 0xba, 0xdc, 0xfe, 0xef, 0xcd,
+                0xab, 0x90, 0x78, 0x56, 0x34, 0x12,
+            ],
+        );
+        verify_ser(
+            -0x1234567890abcdeffedcba0123456789i128,
+            &[
+                0x77, 0x98, 0xba, 0xdc, 0xfe, 0x45, 0x23, 0x01, 0x10, 0x32,
+                0x54, 0x6f, 0x87, 0xa9, 0xcb, 0xed,
+            ],
+        );
+    }
+}
+
+
+
+{
+_run();
+_cache();
+_standby();
+};
