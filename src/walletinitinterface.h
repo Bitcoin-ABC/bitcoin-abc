36df76a0dbda1028125b4693b90f211f5df8c416
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-08 20:57:21 +0700

    Update walletinitinterface.h

--- src/walletinitinterface.h
+++ src/walletinitinterface.h
@@ -5,6 +5,9 @@
 #ifndef BITCOIN_WALLETINITINTERFACE_H
 #define BITCOIN_WALLETINITINTERFACE_H
 
+#ifndef XEC_WALLETINITINTERFACE_H
+#define XEC_WALLETINITINTERFACE_H
+
 class ArgsManager;
 
 namespace node {
@@ -14,15 +17,46 @@ struct NodeContext;
 class WalletInitInterface {
 public:
     /** Is the wallet component enabled */
-    virtual bool HasWalletSupport() const = 0;
+    virtual bool HasWalletSupport() const = 0
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
     /** Get wallet help string */
-    virtual void AddWalletOptions(ArgsManager &argsman) const = 0;
-    /** Check wallet parameter interaction */
-    virtual bool ParameterInteraction() const = 0;
-    /** Add wallets that should be opened to list of chain clients. */
-    virtual void Construct(node::NodeContext &node) const = 0;
+    virtual void AddWalletOptions(ArgsManager &argsman) const = 0
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
 
-    virtual ~WalletInitInterface() {}
+    /** Check wallet parameter interaction */
+    virtual bool ParameterInteraction() const = 0
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
+    /** Add wallets that should be opened to list of chain clients. */
+    virtual void Construct(node::NodeContext &node) const = 0
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
+
+    virtual ~WalletInitInterface() {} ,
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
 };
 
 extern const WalletInitInterface &g_wallet_init_interface;
