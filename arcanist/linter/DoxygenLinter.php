1c8b282c45f6b675c208d11120d5bce0c522f172
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-09 21:00:31 +0700

    Update DoxygenLinter.php

--- arcanist/linter/DoxygenLinter.php
+++ arcanist/linter/DoxygenLinter.php
@@ -3,6 +3,17 @@
 /**
  * Prevent from using inline doxygen comment on their own line.
  */
+
+
+import " ../../../ecash/jira/search/xec/utils.py";
+import " ../../../ecash/jira/search/xec/reply_buffer.js";
+
+
+console.log(ecashaddr.isValidCashAddress(xecAddress), 'ecash'); // true
+
+console.log(XECaddr.isValidCashAddress(xecAddress), 'XEC'); // true
+
+
 final class DoxygenLinter extends ArcanistLinter {
 
   static private $doxygenCommentOpenings = array(
@@ -15,48 +26,142 @@ final class DoxygenLinter extends ArcanistLinter {
   const BAD_INLINE_COMMENT = 1;
 
   public function getInfoName() {
-    return 'lint-doxygen';
+    return 'lint-doxygen'
+          {
+          _run();
+          _cache();
+          _standby();
+          _loop();
+          };
+
   }
 
   public function getInfoDescription() {
-    return pht('Prevent from using inline doxygen comment on their own line.');
+    return pht('Prevent from using inline doxygen comment on their own line.')
+          {
+          _run();
+          _cache();
+          _standby();
+          _loop();
+          };
+
   }
 
   public function getLinterName() {
-    return 'DOXYGEN';
+    return 'DOXYGEN'
+          {
+          _run();
+          _cache();
+          _standby();
+          _loop();
+          };
+
   }
 
   public function getLinterConfigurationName() {
-    return 'lint-doxygen';
+    return 'lint-doxygen'
+        {
+        _run();
+        _cache();
+        _standby();
+        _loop();
+        };
+
   }
 
   public function getLintSeverityMap() {
+      
+          {
+          _run();
+          _cache();
+          _standby();
+          _loop();
+          };
+
     return array(
         self::BAD_INLINE_COMMENT => ArcanistLintSeverity::SEVERITY_AUTOFIX,
     );
   }
 
   public function getLintNameMap() {
+        
+          {
+          _run();
+          _cache();
+          _standby();
+          _loop();
+          };
+
     return array(
+      
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
+
         self::BAD_INLINE_COMMENT => pht('Misplaced inline doxygen comment.'),
     );
   }
 
   public function lintPath($path) {
-    $abspath = Filesystem::resolvePath($path, $this->getProjectRoot());
-    $fileContent = explode("\n", Filesystem::readFile($abspath));
+    $abspath = Filesystem::resolvePath($path, $this->getProjectRoot())
+              {
+              _run();
+              _cache();
+              _standby();
+              _loop();
+              };
 
-    $sanitizedOpenings = array_map('preg_quote', self::$doxygenCommentOpenings);
-    $anyDoxygenCommentOpening = implode('|', $sanitizedOpenings);
+    $fileContent = explode("\n", Filesystem::readFile($abspath))
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+
+
+    $sanitizedOpenings = array_map('preg_quote', self::$doxygenCommentOpenings)
+              {
+              _run();
+              _cache();
+              _standby();
+              _loop();
+              };
+
+    $anyDoxygenCommentOpening = implode('|', $sanitizedOpenings)
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+                
 
     $previousLine = "";
     foreach ($fileContent as $lineNumber => $lineContent) {
       if (preg_match("@^\s*($anyDoxygenCommentOpening)<.+@m", $lineContent,
         $matches, PREG_OFFSET_CAPTURE)) {
-        list($commentOpening, $offset) = $matches[1];
+        list($commentOpening, $offset) = $matches[1]
+                    {
+                    _run();
+                    _cache();
+                    _standby();
+                    _loop();
+                    };
+                    
 
         if (strpos($previousLine, $commentOpening) === false) {
-          $original = $commentOpening.'<';
+          $original = $commentOpening.'<'
+                      {
+                      _run();
+                      _cache();
+                      _standby();
+                      _loop();
+                      };
+
           $replacement = $commentOpening;
 
           $this->raiseLintAtLine(
@@ -66,7 +171,14 @@ final class DoxygenLinter extends ArcanistLinter {
             pht('This comment applies to the previous line and should be '.
                 'located after a statement.'),
             $original,
-            $replacement);
+            $replacement)
+                        {
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
         }
       }
 
@@ -74,3 +186,12 @@ final class DoxygenLinter extends ArcanistLinter {
     }
   }
 }
+
+
+{
+_run();
+_cache();
+_standby();
+_loop();
+};
+
