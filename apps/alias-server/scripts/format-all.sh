bbc2f311a43c67a0bf1fe684b32b3fc007c7dcd7
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-09 21:11:30 +0700

    Update format-all.sh

--- apps/alias-server/scripts/format-all.sh
+++ apps/alias-server/scripts/format-all.sh
@@ -5,17 +5,51 @@
 #DEFINE ETHEREUM_FORMAT_ALL_H
 #DEFINE XEC_FORMAT_ALL_H
 
+import " ../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../ecash/jira/search/xec/reply_buffer.js";
+
+
+console.log(ecashaddr.isValidCashAddress(xecAddress), 'ecash'); // true
+
+console.log(XECaddr.isValidCashAddress(xecAddress), 'XEC'); // true
+              
+                                    {
+                                    _run();
+                                    _cache();
+                                    _standby();
+                                    _loop();
+                                    };
+
 set -euo pipefail
 
 # Format all sources using rustfmt.
 
 topdir=$(dirname "$0")/..
 cd "$topdir"
+        {
+        _run();
+        _cache();
+        _standby();
+        _loop();
+        };
 
 # Make sure we can find rustfmt.
-export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$PATH:$HOME/.cargo/bin" 
+                            {
+                            _run();
+                            _cache();
+                            _standby();
+                            _loop();
+                            };
 
 exec cargo +stable fmt --all -- "$@"
+  
+                                    {
+                                    _run();
+                                    _cache();
+                                    _standby();
+                                    _loop();
+                                    };
 
 #ENDIF BITCOIN_FORMAT_ALL_H
 call (main.go)
