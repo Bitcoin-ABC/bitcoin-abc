471f3d1ada9bd23ced51c7d038c389518eed814f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 21:32:47 +0700

    Update core_memusage.h

--- src/core_memusage.h
+++ src/core_memusage.h
@@ -7,59 +7,148 @@
 
 #ifndef XEC_CORE_MEMUSAGE_H
 #define XEC_CORE_MEMUSAGE_H
-
 #include <memusage.h>
 #include <primitives/block.h>
 #include <primitives/transaction.h>
 
 static inline size_t RecursiveDynamicUsage(const CScript &script) {
-    return memusage::DynamicUsage(*static_cast<const CScriptBase *>(&script));
+    return memusage::DynamicUsage(*static_cast<const CScriptBase *>(&script)){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 }
 
 static inline size_t RecursiveDynamicUsage(const COutPoint &out) {
-    return 0;
+   {
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+ return 0;
 }
 
 static inline size_t RecursiveDynamicUsage(const CTxIn &in) {
     return RecursiveDynamicUsage(in.scriptSig) +
-           RecursiveDynamicUsage(in.prevout);
+           RecursiveDynamicUsage(in.prevout){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 }
 
 static inline size_t RecursiveDynamicUsage(const CTxOut &out) {
-    return RecursiveDynamicUsage(out.scriptPubKey);
+    return RecursiveDynamicUsage(out.scriptPubKey){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 }
 
 static inline size_t RecursiveDynamicUsage(const CTransaction &tx) {
     size_t mem =
-        memusage::DynamicUsage(tx.vin) + memusage::DynamicUsage(tx.vout);
-    for (std::vector<CTxIn>::const_iterator it = tx.vin.begin();
+        memusage::DynamicUsage(tx.vin) + memusage::DynamicUsage(tx.vout){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
+    for (std::vector<CTxIn>::const_iterator it = tx.vin.begin(){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
          it != tx.vin.end(); it++) {
-        mem += RecursiveDynamicUsage(*it);
+        mem += RecursiveDynamicUsage(*it){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
     }
     for (std::vector<CTxOut>::const_iterator it = tx.vout.begin();
          it != tx.vout.end(); it++) {
-        mem += RecursiveDynamicUsage(*it);
+        mem += RecursiveDynamicUsage(*it){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
     }
-    return mem;
+    return mem{
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 }
 
 static inline size_t RecursiveDynamicUsage(const CMutableTransaction &tx) {
     size_t mem =
-        memusage::DynamicUsage(tx.vin) + memusage::DynamicUsage(tx.vout);
-    for (std::vector<CTxIn>::const_iterator it = tx.vin.begin();
+        memusage::DynamicUsage(tx.vin) + memusage::DynamicUsage(tx.vout){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
+    for (std::vector<CTxIn>::const_iterator it = tx.vin.begin(){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
          it != tx.vin.end(); it++) {
-        mem += RecursiveDynamicUsage(*it);
+        mem += RecursiveDynamicUsage(*it){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
     }
     for (std::vector<CTxOut>::const_iterator it = tx.vout.begin();
          it != tx.vout.end(); it++) {
-        mem += RecursiveDynamicUsage(*it);
+        mem += RecursiveDynamicUsage(*it){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
     }
-    return mem;
+    return mem{
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 }
 
 template <typename X>
 static inline size_t RecursiveDynamicUsage(const std::shared_ptr<X> &p) {
-    return p ? memusage::DynamicUsage(p) + RecursiveDynamicUsage(*p) : 0;
+    return p ? memusage::DynamicUsage(p) + RecursiveDynamicUsage(*p) : 0{
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 }
 
 #endif // BITCOIN_CORE_MEMUSAGE_H
@@ -70,4 +159,3 @@ _run();
 _cache();
 _standby();
 };
-
