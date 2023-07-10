a7d0eef1aa102b54cc83fb2af7840ea9cb922b98
Pieter Wuille <pieter@wuille.net>
2023-07-10 14:02:14 -0400

    Make poly1305 support incremental computation + modernize
    Summary:
    > crypto: switch poly1305 to incremental implementation
    >
    > This code is taken from poly1305-donna-32.h, poly1305-donna.h, poly1305-donna.c
    > from https://github.com/floodyberry/poly1305-donna, commit
    > e6ad6e091d30d7f4ec2d4f978be1fcfcbce72781, with the following modifications:
    >
    > * Coding style (braces around one-line indented if/for loops).
    > * Rename unsigned long (long) to uint32_t and uint64_t.
    > * Rename poly1305_block_size to POLY1305_BLOCK_SIZE.
    > * Adding noexcept to functions.
    > * Merging poly1305_state_internal_t and poly1305_context types.
    > * Merging code from multiple files.
    > * Place all imported code in the poly1305_donna namespace.
    
    > crypto: add Poly1305 class with std::byte Span interface
    
    > tests: add more Poly1305 test vectors
    
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/27993 | core#27993]]
    https://github.com/bitcoin/bitcoin/pull/27993/commits/50269b391fa18556bad72dc8c2fb4e2493a6a054
    https://github.com/bitcoin/bitcoin/pull/27993/commits/40e6c5b9fce92ffe64e91c2aba38bb2ed57bfbfb
    https://github.com/bitcoin/bitcoin/pull/27993/commits/8871f7d1ae096839abcbf25a548319185acc01a2
    
    Test Plan: `ninja all check-all bench-bitcoin bitcoin-fuzzers`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19015

--- src/crypto/poly1305.h
+++ src/crypto/poly1305.h
@@ -5,12 +5,67 @@
 #ifndef BITCOIN_CRYPTO_POLY1305_H
 #define BITCOIN_CRYPTO_POLY1305_H
 
+#include <span.h>
+
+#include <cassert>
 #include <cstdint>
 #include <cstdlib>
 
 #define POLY1305_KEYLEN 32
 #define POLY1305_TAGLEN 16
+#define POLY1305_BLOCK_SIZE 16
 
+namespace poly1305_donna {
+
+// Based on the public domain implementation by Andrew Moon
+// poly1305-donna-32.h from https://github.com/floodyberry/poly1305-donna
+
+typedef struct {
+    uint32_t r[5];
+    uint32_t h[5];
+    uint32_t pad[4];
+    size_t leftover;
+    uint8_t buffer[POLY1305_BLOCK_SIZE];
+    uint8_t final;
+} poly1305_context;
+
+void poly1305_init(poly1305_context *st, const uint8_t key[32]) noexcept;
+void poly1305_update(poly1305_context *st, const uint8_t *m,
+                     size_t bytes) noexcept;
+void poly1305_finish(poly1305_context *st, uint8_t mac[16]) noexcept;
+
+} // namespace poly1305_donna
+
+/** C++ wrapper with std::byte Span interface around poly1305_donna code. */
+class Poly1305 {
+    poly1305_donna::poly1305_context m_ctx;
+
+public:
+    /** Length of the output produced by Finalize(). */
+    static constexpr unsigned TAGLEN = POLY1305_TAGLEN;
+
+    /** Length of the keys expected by the constructor. */
+    static constexpr unsigned KEYLEN = POLY1305_KEYLEN;
+
+    /** Construct a Poly1305 object with a given 32-byte key. */
+    Poly1305(Span<const std::byte> key) noexcept {
+        assert(key.size() == KEYLEN);
+        poly1305_donna::poly1305_init(&m_ctx, UCharCast(key.data()));
+    }
+
+    /** Process message bytes. */
+    Poly1305 &Update(Span<const std::byte> msg) noexcept {
+        poly1305_donna::poly1305_update(&m_ctx, UCharCast(msg.data()),
+                                        msg.size());
+        return *this;
+    }
+
+    /** Write authentication tag to 16-byte out. */
+    void Finalize(Span<std::byte> out) noexcept {
+        assert(out.size() == TAGLEN);
+        poly1305_donna::poly1305_finish(&m_ctx, UCharCast(out.data()));
+    }
+};
 void poly1305_auth(uint8_t out[POLY1305_TAGLEN], const uint8_t *m, size_t inlen,
                    const uint8_t key[POLY1305_KEYLEN]);
 
