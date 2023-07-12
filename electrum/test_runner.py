cfbb96bfc9132da196ee873bc5451ac14bdfafaf
PiRK <pierrekn@gmail.com>
2023-07-12 16:50:02 +0200

    [electrum] add a basic test for setup.py
    Summary:
    This would have caught the bug fixed by D14258.
    
    `electrum-tests` is run by CI on any change in the electrum/ directory, even if it is not touching python code.
    
    Test Plan:
    `python test_runner.py`
    
    Try reintroducing an error in setup.py [README.md -> README.rst) and check that it fails and print the error tracebak.
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14260

--- electrum/test_runner.py
+++ electrum/test_runner.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
-
-import os.path
+import os
+import re
+import subprocess
 import unittest
 from collections.abc import Iterable
 
 # Use realpath in case this script is called via a symbolic link
 ELECTRUM_DIR = os.path.dirname(os.path.realpath(__file__))
 
+# Exercise the setup.py to find obvious errors
+os.chdir(ELECTRUM_DIR)
+ret = subprocess.run(
+    [os.path.join(ELECTRUM_DIR, "setup.py"), "--version"],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+)
+if ret.stderr != b"":
+    print("Unexpected stderr:")
+    print(ret.stderr.decode("utf-8"))
+assert ret.returncode == 0, "Failed to run `setup.py --version`"
+assert re.match(r"\d+\.\d+\.\d+", ret.stdout.decode("utf-8"))
+print("Testing `setup.py --version`: OK\n")
+
+# Run unit tests
 suite = unittest.TestSuite()
 for all_test_suite in unittest.defaultTestLoader.discover(
     ELECTRUM_DIR, pattern="test_*.py"
