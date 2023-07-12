d0466eaf73c4dc41b84d9170a499b44168c83478
PiRK <pierrekn@gmail.com>
2023-07-12 17:40:08 +0200

    [electrum] be selective about ignoring errors
    Summary:
    Only ignore a whitelist of acceptable errors instead of ignoring all errors during test discovery. Ignoring all errors could lead to tests just being skipped and the test suite succeeding, which could go unnoticed for a long time.
    
    This will help us detect some unexpected errors, and provide us with a useful error stack trace to debug it.
    
    For instance, intentionnaly raising an error in the main context of a module that is imported by an `__init__.py` produces this output:
    ```
    $ python electrum/test_runner.py
    Testing `setup.py --version`: OK
    
    ........................................................................................................................................................................................................................s.........s.........s..................................E......
    ======================================================================
    ERROR: electrumabc_gui.qt (unittest.loader._FailedTest)
    ----------------------------------------------------------------------
    ImportError: Failed to import test module: electrumabc_gui.qt
    Traceback (most recent call last):
      File "/usr/lib/python3.10/unittest/loader.py", line 470, in _find_test_path
        package = self._get_module_from_name(name)
      File "/usr/lib/python3.10/unittest/loader.py", line 377, in _get_module_from_name
        __import__(name)
      File "/home/pierre/dev/bitcoin-abc/electrum/electrumabc_gui/qt/__init__.py", line 86, in <module>
        from .exception_window import ExceptionHook
      File "/home/pierre/dev/bitcoin-abc/electrum/electrumabc_gui/qt/exception_window.py", line 46, in <module>
        from .main_window import ElectrumWindow
      File "/home/pierre/dev/bitcoin-abc/electrum/electrumabc_gui/qt/main_window.py", line 88, in <module>
        from . import address_dialog, external_plugins_window, qrwindow
      File "/home/pierre/dev/bitcoin-abc/electrum/electrumabc_gui/qt/address_dialog.py", line 33, in <module>
        from .history_list import HistoryList
      File "/home/pierre/dev/bitcoin-abc/electrum/electrumabc_gui/qt/history_list.py", line 46, in <module>
        raise ImportError("spam spam spam spam spam")
    ImportError: spam spam spam spam spam
    
    ----------------------------------------------------------------------
    Ran 278 tests in 9.094s
    
    FAILED (errors=1, skipped=3)
    ```
    
    Use `addTest` instead of `addTests`, because the former can take a `TestCase` (which `_FailedTest` is a subclass of) or a `TestSuite`, whereas the latter expected an iterable object (i.e a `TestSuite` or a list of `TestCase`)
    
    Test Plan: `python test_runner.py`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14262

--- electrum/test_runner.py
+++ electrum/test_runner.py
@@ -3,7 +3,15 @@ import os
 import re
 import subprocess
 import unittest
-from collections.abc import Iterable
+
+# Whitelist of modules with allowable errors during automated test discovery.
+KNOWN_FAILURES = [
+    # PyQt5 not installed (currently nothing in the gui package is tested)
+    r"electrumabc_gui(\.qt)*",
+    # jsonrpcclient not installed.
+    # Note that regtests are not unittest cases, so they would not be run anyway.
+    r"electrumabc.tests.regtest.*",
+]
 
 # Use realpath in case this script is called via a symbolic link
 ELECTRUM_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -31,14 +39,12 @@ def run_unit_tests() -> unittest.TestSuite:
         ELECTRUM_DIR, pattern="test_*.py"
     ):
         for test_suite in all_test_suite:
-            if not isinstance(test_suite, Iterable):
-                # unittest sometimes runs into errors when trying to detect test files, and
-                # then returns a _FailedTest object that makes addTests fail with
-                # `TypeError: '_FailedTest' object is not iterable`
-                # This happens for instance with electrumabc_gui when PyQt5 is missing.
-                # We don't want PyQt5 to be a hard dependency for tests for now:.
+            if isinstance(test_suite, unittest.loader._FailedTest) and any(
+                re.match(pattern, test_suite._testMethodName)
+                for pattern in KNOWN_FAILURES
+            ):
                 continue
-            suite.addTests(test_suite)
+            suite.addTest(test_suite)
     return suite
 
 
