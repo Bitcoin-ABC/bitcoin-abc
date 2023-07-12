2bf685cc6b8bce4c10419505c6c868972e01bcf7
PiRK <pierrekn@gmail.com>
2023-07-12 17:10:36 +0200

    [electrum] don't run the unit tests twice
    Summary:
    It seems like `unittest.defaultTestLoader.discover` imports some of the python files (scripts outside packages, `__init__.py` files...), so all the code that is not in a function or inside a `if  __name__ == "__main__"` context will be executed. This explains why failure to import PyQt5 was reported as a failed test before D14135, and why the tests seemed to be run twice.
    
    Depends on D14260
    
    Test Plan:
    `python test_runner.py`
    Check that the test results are printed only once
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14261

--- electrum/test_runner.py
+++ electrum/test_runner.py
@@ -8,33 +8,44 @@ from collections.abc import Iterable
 # Use realpath in case this script is called via a symbolic link
 ELECTRUM_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# Exercise the setup.py to find obvious errors
-os.chdir(ELECTRUM_DIR)
-ret = subprocess.run(
-    [os.path.join(ELECTRUM_DIR, "setup.py"), "--version"],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-)
-if ret.stderr != b"":
-    print("Unexpected stderr:")
-    print(ret.stderr.decode("utf-8"))
-assert ret.returncode == 0, "Failed to run `setup.py --version`"
-assert re.match(r"\d+\.\d+\.\d+", ret.stdout.decode("utf-8"))
-print("Testing `setup.py --version`: OK\n")
 
-# Run unit tests
-suite = unittest.TestSuite()
-for all_test_suite in unittest.defaultTestLoader.discover(
-    ELECTRUM_DIR, pattern="test_*.py"
-):
-    for test_suite in all_test_suite:
-        if not isinstance(test_suite, Iterable):
-            # unittest sometimes runs into errors when trying to detect test files, and
-            # then returns a _FailedTest object that makes addTests fail with
-            # `TypeError: '_FailedTest' object is not iterable`
-            # This happens for instance with electrumabc_gui when PyQt5 is missing.
-            # We don't want PyQt5 to be a hard dependency for tests for now:.
-            continue
-        suite.addTests(test_suite)
+def test_setup():
+    # Exercise the setup.py to find obvious errors
+    os.chdir(ELECTRUM_DIR)
+    ret = subprocess.run(
+        [os.path.join(ELECTRUM_DIR, "setup.py"), "--version"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    if ret.stderr != b"":
+        print("Unexpected stderr:")
+        print(ret.stderr.decode("utf-8"))
+    assert ret.returncode == 0, "Failed to run `setup.py --version`"
+    assert re.match(r"\d+\.\d+\.\d+", ret.stdout.decode("utf-8"))
+    print("Testing `setup.py --version`: OK\n")
 
-unittest.main(defaultTest="suite")
+
+def run_unit_tests() -> unittest.TestSuite:
+    suite = unittest.TestSuite()
+    for all_test_suite in unittest.defaultTestLoader.discover(
+        ELECTRUM_DIR, pattern="test_*.py"
+    ):
+        for test_suite in all_test_suite:
+            if not isinstance(test_suite, Iterable):
+                # unittest sometimes runs into errors when trying to detect test files, and
+                # then returns a _FailedTest object that makes addTests fail with
+                # `TypeError: '_FailedTest' object is not iterable`
+                # This happens for instance with electrumabc_gui when PyQt5 is missing.
+                # We don't want PyQt5 to be a hard dependency for tests for now:.
+                continue
+            suite.addTests(test_suite)
+    return suite
+
+
+if __name__ == "__main__":
+    test_setup()
+
+    # This need to be called last, because unittest.main calls sys.exit.
+    # Alternatively, exit=False could be passed as a parameter.
+    suite = run_unit_tests()
+    unittest.main(defaultTest="suite")
