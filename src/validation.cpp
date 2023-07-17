f0b96daf168840d511af165d95252e70cf749a4f
fanquake <fanquake@gmail.com>
2023-07-17 14:44:46 +0100

    validation: use noexcept instead of deprecated throw()
    Summary:
    ```bash
      CXX      libbitcoin_node_a-validation.o
    validation.cpp:5164:30: warning: dynamic exception specifications are deprecated [-Wdeprecated-dynamic-exception-spec]
        const char* what() const throw() override
                                 ^~~~~~~
    validation.cpp:5164:30: note: use 'noexcept' instead
        const char* what() const throw() override
                                 ^~~~~~~
                                 noexcept
    ```
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/28090 | core#28090]]
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D20409

--- src/validation.cpp
+++ src/validation.cpp
@@ -6805,7 +6805,7 @@ static void FlushSnapshotToDisk(CCoinsViewCache &coins_cache,
 }
 
 struct StopHashingException : public std::exception {
-    const char *what() const throw() override {
+    const char *what() const noexcept override {
         return "ComputeUTXOStats interrupted by shutdown.";
     }
 };
