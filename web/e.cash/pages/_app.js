b2c8bc45ef51a04d152287c3514d861c4777a41b
John <jkuney17@gmail.com>
2023-07-19 17:09:03 -0500

    [e.cash] Add google analytics script
    Summary:
    Adding the google analytics scripts as outlined here https://nextjs.org/docs/messages/next-script-for-ga
    And adding a .env file to store the GA ID as outlined here https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables
    
    Test Plan: preview the site and check the scripts are loading properly
    
    Reviewers: #bitcoin_abc, bytesofman
    
    Reviewed By: #bitcoin_abc, bytesofman
    
    Subscribers: bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D14290

--- web/e.cash/pages/_app.js
+++ web/e.cash/pages/_app.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2023 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+import Script from 'next/script';
 import { ThemeProvider } from 'styled-components';
 import { ecash } from '/styles/theme';
 import GlobalCSS from '/styles/global';
@@ -10,6 +11,21 @@ export default function App({ Component, pageProps }) {
         <ThemeProvider theme={ecash}>
             <GlobalCSS />
             <Component {...pageProps} />
+            <Script
+                strategy="lazyOnload"
+                id="google-analytics1"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+            />
+            <Script id="google-analytics2" strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
         </ThemeProvider>
     );
 }
