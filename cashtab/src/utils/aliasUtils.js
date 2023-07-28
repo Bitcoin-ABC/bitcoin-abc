16e851dd3b08a9ade9acc04b9401da4babd2b19a
ethan <ethanmackie1199@gmail.com>
2023-07-29 01:10:30 +1000

    [Cashtab][Alias] Deprecate getAliasServerState
    Summary:
    T3216
    
    Following the removal of the `/state` endpoint on the alias-server, the `getAliasServerState` function is no longer in use.
    
    Test Plan:
    grep -r getAliasServerState src/
    npm test
    
    Reviewers: bytesofman, #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14319

--- cashtab/src/utils/aliasUtils.js
+++ cashtab/src/utils/aliasUtils.js
@@ -123,33 +123,6 @@ export const queryAliasServer = async (endPoint, aliasParam) => {
 
 /*
  @response:
- {
-    processedBlockheight: 785354,
-    processedConfirmedTxs: 718,
-    registeredAliasCount: 426,
-    registrationAddress: 'ecash:qp3c268rd5946l2f5m5es4x25f7ewu4sjvpy52pqa8',
- }
-*/
-export const getAliasServerState = async () => {
-    let aliasServerResp, aliasServerRespJson;
-    try {
-        aliasServerResp = await fetch(
-            currency.aliasSettings.aliasServerBaseUrl + '/state',
-        );
-        aliasServerRespJson = aliasServerResp.json();
-    } catch (err) {
-        console.log(
-            `getAliasServerState(): Error retrieving server state from alias-server`,
-            err,
-        );
-        return false;
-    }
-
-    return aliasServerRespJson;
-};
-
-/*
- @response:
   [
     {alias: 'foo', address: 'ecash:qwert...', txid: 'as12d1f324asdf'},
     {alias: 'foo2', address: 'ecash:qwert...' txid: 'as12d1f324asdf'},
