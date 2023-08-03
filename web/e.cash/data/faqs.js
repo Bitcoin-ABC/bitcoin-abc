2568c18e4c8a907e9ccb50b2e2eabe672b37f153
Antony Zegers <antony.zegers@gmail.com>
2023-08-03 09:30:24 -0700

    [e.cash] Reorder FAQ
    Summary:
    Content unchanged, just changing the order to put more relevant info near the top.
    
    Content for specific questions and answers will be updated in further Diffs.
    
    Test Plan: Ran it locally, looks good
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14344

--- web/e.cash/data/faqs.js
+++ web/e.cash/data/faqs.js
@@ -18,33 +18,16 @@ export const faqs = [
         ),
     },
     {
-        question: "What's the difference between eCash and Bitcoin ABC?",
+        question: 'How do I get eCash?',
         answer: (
             <p>
-                <strong>eCash</strong> is a cryptocurrency, whereas Bitcoin ABC
-                is the software businesses use to interact with and maintain the{' '}
-                <strong>eCash </strong>network. The team behind the Bitcoin ABC
-                software also operates under the same name.
-                <br />
-                <br />
-                You can learn more about Bitcoin ABC at{' '}
-                <CustomLink href="https://www.bitcoinabc.org/">
-                    bitcoinabc.org
-                </CustomLink>
-                .
-            </p>
-        ),
-    },
-    {
-        question: 'Why is eCash listed as BCHA on some exchanges?',
-        answer: (
-            <p>
-                <strong>eCash</strong> was briefly known as Bitcoin Cash ABC
-                (BCHA). The <strong>eCash</strong> branding came into effect on
-                July 1, 2021. Exchanges are strongly encouraged to update their
-                older listings accordingly. You may still see{' '}
-                <strong>eCash</strong> listed as BCHA on some exchanges if they
-                haven&#x27;t yet made the switch.
+                Since <strong>eCash</strong> transactions go directly between
+                you and whoever you’re paying or getting paid by, you don’t need
+                a bank account to own it. Instead, you just need an electronic
+                wallet. Once you have a wallet, you can get{' '}
+                <strong>eCash</strong> by buying it on a cryptocurrency exchange
+                and then sending it to your wallet. Other people can also send{' '}
+                <strong>eCash</strong> to your wallet.
             </p>
         ),
     },
@@ -61,88 +44,39 @@ export const faqs = [
     },
     {
         question:
-            'I have coins on the Bitcoin Cash (BCH) network. How do I retrieve my eCash?',
+            'Where can I find eCash price information compared to other cryptocurrencies?',
         answer: (
             <p>
-                Simply download the latest Electrum ABC release and import your
-                old Bitcoin Cash wallet:{' '}
-                <CustomLink href="https://www.bitcoinabc.org/electrum/">
-                    bitcoinabc.org/electrum
+                eCash price information is available at all leading crypto
+                research sources, like{' '}
+                <CustomLink href="https://www.coingecko.com/en/coins/ecash">
+                    Coingecko
                 </CustomLink>
-                .
+                ,{' '}
+                <CustomLink href="https://coinmarketcap.com/currencies/ecash/">
+                    Coinmarketcap
+                </CustomLink>
+                , and{' '}
+                <CustomLink href="https://crypto.com/price/ecash">
+                    Crypto.com
+                </CustomLink>
             </p>
         ),
     },
     {
-        question: 'How do I get eCash?',
+        question: 'What is the supply of eCash?',
         answer: (
             <p>
-                Since <strong>eCash</strong> transactions go directly between
-                you and whoever you’re paying or getting paid by, you don’t need
-                a bank account to own it. Instead, you just need an electronic
-                wallet. Once you have a wallet, you can get{' '}
-                <strong>eCash</strong> by buying it on a cryptocurrency exchange
-                and then sending it to your wallet. Other people can also send{' '}
-                <strong>eCash</strong> to your wallet.
-            </p>
-        ),
-    },
-    {
-        question: 'What is the base unit of eCash?',
-        answer: (
-            <p>
-                <strong>eCash</strong> uses ”bits” which make it easy to send
-                small payments because you no longer have to handle unwieldy
-                decimal places. For instance, instead of sending 0.00001000
-                bitcoins (which was the base unit used by BCHA), you’ll simply
-                send 10 bits!
-            </p>
-        ),
-    },
-    {
-        question: 'Where did eCash come from?',
-        answer: (
-            <p>
-                On November 15, 2020, the Bitcoin Cash (BCH) blockchain split
-                into two chains. One of those chains was called BCHA for a time.
-                This chain was what eventually became <strong>eCash</strong>.
-            </p>
-        ),
-    },
-    {
-        question: 'How do I protect myself from scammers?',
-        answer: (
-            <p>
-                The exponential growth in crypto has unfortunately led to
-                similar growth in scams. Here are some common ones to watch out
-                for.
+                <strong>eCash</strong> has the same fixed supply as bitcoin. The
+                default base unit of <strong>eCash</strong> has 2 decimal places
+                (100 satoshis). The default base unit of bitcoin (BTC) has 8
+                decimal places (100,000,000 satoshis).
                 <br />
                 <br />
-                <span>Email Phishing</span>
-                <br />
-                Some scammers will send you an email impersonating a member of
-                an official team and ask for money, a 12-word wallet seed, or a
-                private key. Official team members will never send you an
-                unsolicited email. For tech support, we will never ask for your
-                wallet seed or private key. Never share your private key or your
-                wallet seed with anyone.
-                <br />
-                <br />
-                <span>URL Phishing</span>
-                <br />
-                Scammers may send you a link to a website that looks like an
-                official crypto website but does not have the same URL. You may
-                think you are sending funds to your own wallet, for example, but
-                are in fact sending them to a cloned page that is not your
-                wallet. Always make sure the URL in your browser URL bar is
-                correct. Always confirm a scanned QR code address matches what
-                you expected.
-                <br />
-                <br />
-                <span>Fake wallets</span>
-                <br />
-                Always verify the SHA256 hash when you are downloading an
-                official cryptocurrency wallet.
+                90% of all the <strong>eCash</strong> that will ever exist has
+                already been mined. The inflation rate for{' '}
+                <strong>eCash</strong> is already low (less than 2% as of 2021),
+                and will decrease to zero.
             </p>
         ),
     },
@@ -200,19 +134,51 @@ export const faqs = [
         ),
     },
     {
-        question: 'What is the supply of eCash?',
+        question: 'What is the base unit of eCash?',
         answer: (
             <p>
-                <strong>eCash</strong> has the same fixed supply as bitcoin. The
-                default base unit of <strong>eCash</strong> has 2 decimal places
-                (100 satoshis). The default base unit of bitcoin (BTC) has 8
-                decimal places (100,000,000 satoshis).
+                <strong>eCash</strong> uses ”bits” which make it easy to send
+                small payments because you no longer have to handle unwieldy
+                decimal places. For instance, instead of sending 0.00001000
+                bitcoins (which was the base unit used by BCHA), you’ll simply
+                send 10 bits!
+            </p>
+        ),
+    },
+    {
+        question: 'How do I protect myself from scammers?',
+        answer: (
+            <p>
+                The exponential growth in crypto has unfortunately led to
+                similar growth in scams. Here are some common ones to watch out
+                for.
                 <br />
                 <br />
-                90% of all the <strong>eCash</strong> that will ever exist has
-                already been mined. The inflation rate for{' '}
-                <strong>eCash</strong> is already low (less than 2% as of 2021),
-                and will decrease to zero.
+                <span>Email Phishing</span>
+                <br />
+                Some scammers will send you an email impersonating a member of
+                an official team and ask for money, a 12-word wallet seed, or a
+                private key. Official team members will never send you an
+                unsolicited email. For tech support, we will never ask for your
+                wallet seed or private key. Never share your private key or your
+                wallet seed with anyone.
+                <br />
+                <br />
+                <span>URL Phishing</span>
+                <br />
+                Scammers may send you a link to a website that looks like an
+                official crypto website but does not have the same URL. You may
+                think you are sending funds to your own wallet, for example, but
+                are in fact sending them to a cloned page that is not your
+                wallet. Always make sure the URL in your browser URL bar is
+                correct. Always confirm a scanned QR code address matches what
+                you expected.
+                <br />
+                <br />
+                <span>Fake wallets</span>
+                <br />
+                Always verify the SHA256 hash when you are downloading an
+                official cryptocurrency wallet.
             </p>
         ),
     },
@@ -230,6 +196,61 @@ export const faqs = [
         ),
     },
     {
+        question: 'Where did eCash come from?',
+        answer: (
+            <p>
+                On November 15, 2020, the Bitcoin Cash (BCH) blockchain split
+                into two chains. One of those chains was called BCHA for a time.
+                This chain was what eventually became <strong>eCash</strong>.
+            </p>
+        ),
+    },
+    {
+        question: "What's the difference between eCash and Bitcoin ABC?",
+        answer: (
+            <p>
+                <strong>eCash</strong> is a cryptocurrency, whereas Bitcoin ABC
+                is the software businesses use to interact with and maintain the{' '}
+                <strong>eCash </strong>network. The team behind the Bitcoin ABC
+                software also operates under the same name.
+                <br />
+                <br />
+                You can learn more about Bitcoin ABC at{' '}
+                <CustomLink href="https://www.bitcoinabc.org/">
+                    bitcoinabc.org
+                </CustomLink>
+                .
+            </p>
+        ),
+    },
+    {
+        question: 'Why is eCash listed as BCHA on some exchanges?',
+        answer: (
+            <p>
+                <strong>eCash</strong> was briefly known as Bitcoin Cash ABC
+                (BCHA). The <strong>eCash</strong> branding came into effect on
+                July 1, 2021. Exchanges are strongly encouraged to update their
+                older listings accordingly. You may still see{' '}
+                <strong>eCash</strong> listed as BCHA on some exchanges if they
+                haven&#x27;t yet made the switch.
+            </p>
+        ),
+    },
+    {
+        question:
+            'I have coins on the Bitcoin Cash (BCH) network. How do I retrieve my eCash?',
+        answer: (
+            <p>
+                Simply download the latest Electrum ABC release and import your
+                old Bitcoin Cash wallet:{' '}
+                <CustomLink href="https://www.bitcoinabc.org/electrum/">
+                    bitcoinabc.org/electrum
+                </CustomLink>
+                .
+            </p>
+        ),
+    },
+    {
         question: "What's the best way to get technical support?",
         answer: (
             <p>
@@ -238,27 +259,6 @@ export const faqs = [
                     contact@e.cash
                 </CustomLink>
                 .
-            </p>
-        ),
-    },
-    {
-        question:
-            'Where can I find eCash price information compared to other cryptocurrencies?',
-        answer: (
-            <p>
-                eCash price information is available at all leading crypto
-                research sources, like{' '}
-                <CustomLink href="https://www.coingecko.com/en/coins/ecash">
-                    Coingecko
-                </CustomLink>
-                ,{' '}
-                <CustomLink href="https://coinmarketcap.com/currencies/ecash/">
-                    Coinmarketcap
-                </CustomLink>
-                , and{' '}
-                <CustomLink href="https://crypto.com/price/ecash">
-                    Crypto.com
-                </CustomLink>
             </p>
         ),
     },
