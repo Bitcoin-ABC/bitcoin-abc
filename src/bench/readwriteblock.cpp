960a5e16024808ef1ea16d62de61ec2a45e8d69b
Andrew Toth <andrewstoth@gmail.com>
2023-08-07 10:15:26 -0400

    bench: add readblock benchmark
    Summary:
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/26684 | core#26684]]
    
    with renaming changes:
    https://github.com/bitcoin/bitcoin/pull/31490/commits/34f9a0157aad7c10ac364b7e4602c5f74c1f9e20
    
    and additional includes from core#30716
    
    Depends on D18952
    
    Test Plan: `ninja bench-bitcoin``
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D18957

---
+++ src/bench/readwriteblock.cpp
@@ -0,0 +1,60 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <bench/bench.h>
+#include <bench/data.h>
+
+#include <flatfile.h>
+#include <node/blockstorage.h>
+#include <primitives/block.h>
+#include <serialize.h>
+#include <span.h>
+#include <streams.h>
+#include <test/util/setup_common.h>
+#include <validation.h>
+
+#include <cassert>
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+static FlatFilePos WriteBlockToDisk(ChainstateManager &chainman) {
+    DataStream stream{benchmark::data::block413567};
+    CBlock block;
+    stream >> block;
+
+    return chainman.m_blockman.SaveBlockToDisk(block, 0, nullptr);
+}
+
+static void ReadBlockFromDiskBench(benchmark::Bench &bench) {
+    const auto testing_setup{
+        MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN)};
+    ChainstateManager &chainman{*testing_setup->m_node.chainman};
+
+    CBlock block;
+    const auto pos{WriteBlockToDisk(chainman)};
+
+    bench.run([&] {
+        const auto success{chainman.m_blockman.ReadBlockFromDisk(block, pos)};
+        assert(success);
+    });
+}
+
+static void ReadRawBlockFromDiskBench(benchmark::Bench &bench) {
+    const auto testing_setup{
+        MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN)};
+    ChainstateManager &chainman{*testing_setup->m_node.chainman};
+
+    std::vector<uint8_t> block_data;
+    const auto pos{WriteBlockToDisk(chainman)};
+
+    bench.run([&] {
+        const auto success{
+            chainman.m_blockman.ReadRawBlockFromDisk(block_data, pos)};
+        assert(success);
+    });
+}
+
+BENCHMARK(ReadBlockFromDiskBench);
+BENCHMARK(ReadRawBlockFromDiskBench);
