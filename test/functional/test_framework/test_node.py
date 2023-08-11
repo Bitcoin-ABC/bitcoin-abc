969e03c50dad8a57d22e8e0c239ac7b59f4939dc
glozow <gloriajzhao@gmail.com>
2023-08-11 16:51:39 +0200

    test: tx orphan handling
    Summary:
    ```
    I was doing some mutation testing (through reckless refactoring) locally and found some specific behaviors in orphan handling that weren't picked up by tests. Adding some of these test cases now can maybe help with reviewing refactors like #28031.
     - Parent requests aren't sent immediately. A delay is added and the requests are filtered by AlreadyHaveTx before they are sent, which means you can't use fake orphans to probe precise arrival timing of a tx.
     - Parent requests include all that are not AlreadyHaveTx. This means old confirmed parents may be requested.
     - The node does not give up on orphans if the peer responds to a parent request with notfound. This means that if a parent is an old confirmed transaction (in which notfound is expected), the orphan should still be resolved.
     - Rejected parents can cause an orphan to be dropped, but it depends on the reason and only based on txid.
     - Rejected parents can cause an orphan to be rejected too, by both wtxid and txid.
     - Requests for orphan parents should be de-duplicated with "regular" txrequest. If a missing parent has the same hash as an in-flight request, it shouldn't be requested.
     - Multiple orphans with overlapping parents should not cause duplicated parent requests.
    ```
    
    Backport of [[https://github.com/bitcoin/bitcoin/pull/28199 | core#28199]].
    
    The test_orphan_inherit_rejection case makes no sense without segwit so it has been removed, and well as some changes related to the absence of segwit and/or mempool ancestors accounting.
    
    Depends on D16505.
    
    Test Plan:
      ./test/functional/test_runner.py p2p_orphan_handling
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Subscribers: PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D16503

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -190,6 +190,8 @@ class TestNode:
         self.p2ps = []
         self.timeout_factor = timeout_factor
 
+        self.mocktime = None
+
     AddressKeyPair = collections.namedtuple("AddressKeyPair", ["address", "key"])
     PRIV_KEYS = [
         # address , privkey
@@ -478,6 +480,15 @@ class TestNode:
             stakes=stakes,
             payoutAddress=payoutAddress,
         )
+
+    def setmocktime(self, timestamp):
+        """Wrapper for setmocktime RPC, sets self.mocktime"""
+        if timestamp == 0:
+            # setmocktime(0) resets to system time.
+            self.mocktime = None
+        else:
+            self.mocktime = timestamp
+        return self.__getattr__("setmocktime")(timestamp)
 
     def get_wallet_rpc(self, wallet_name):
         if self.use_cli:
@@ -950,6 +961,13 @@ class TestNode:
             self.chronik_port,
             timeout=DEFAULT_TIMEOUT * self.timeout_factor,
         )
+
+    def bumpmocktime(self, seconds):
+        """Fast forward using setmocktime to self.mocktime + seconds. Requires setmocktime to have
+        been called at some point in the past."""
+        assert self.mocktime
+        self.mocktime += seconds
+        self.setmocktime(self.mocktime)
 
 
 class TestNodeCLIAttr:
