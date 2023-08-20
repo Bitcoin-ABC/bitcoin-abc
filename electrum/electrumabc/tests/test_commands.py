PiRK/ElectrumABC

commit 00cb0b018f6edbc6a4e6f2d22ddbd509e5c8681e

use a much simpler way to set the default command as "gui"
No need to monkey patch a third party library, risking to break it when they update.
Just set the default command explicitly if none is provided.

Test plan:
run the program from the command line without specifying any argument, verify that it is the same behavior as running `electrum-abc gui`.

Search for the 'cmd' string in all the modules and make sure no other module needs it, beside the main script.

electroncash/commands.py
@@ -944,29 +944,6 @@ config_variables = {
     }
 }
 
-def set_default_subparser(self, name, args=None):
-    """see http://stackoverflow.com/questions/5176691/argparse-how-to-specify-a-default-subcommand"""
-    subparser_found = False
-    for arg in sys.argv[1:]:
-        if arg in ['-h', '--help']:  # global help if no subparser
-            break
-    else:
-        for x in self._subparsers._actions:
-            if not isinstance(x, argparse._SubParsersAction):
-                continue
-            for sp_name in x._name_parser_map.keys():
-                if sp_name in sys.argv[1:]:
-                    subparser_found = True
-        if not subparser_found:
-            # insert default in first position, this implies no
-            # global options without a sub_parsers specified
-            if args is None:
-                sys.argv.insert(1, name)
-            else:
-                args.insert(0, name)
-
-argparse.ArgumentParser.set_default_subparser = set_default_subparser
-
 
 # workaround https://bugs.python.org/issue23058
 # see https://github.com/nickstenning/honcho/pull/121
@@ -1072,6 +1049,4 @@ def get_parser():
             for k, v in cvh.items():
                 group.add_argument(k, nargs='?', help=v)
 
-    # 'gui' is the default command
-    parser.set_default_subparser('gui')
     return parser

electrum-abc
@@ -403,7 +403,7 @@ if __name__ == '__main__':
 
     # todo: defer this to gui
     config = SimpleConfig(config_options)
-    cmdname = config.get('cmd')
+    cmdname = args.cmd if args.cmd is not None else "gui"
 
     # run non-RPC commands separately
     if cmdname in ['create', 'restore']:

Bitcoin-ABC/bitcoin-abc

commit ada8d4e87792acc213b902ee66e53f0ad0ef9217

[electrum] fix BIP 21 URI handling
Summary:
Commit https://github.com/Bitcoin-ABC/ElectrumABC/pull/31/commits/00cb0b018f6edbc6a4e6f2d22ddbd509e5c8681e broke the URI handling.

When the application is called with a URI as a first positional argument, it should start a GUI with the send tab pre-filled with the correct information. Unfortunately `argparse` does not provide a good way to default to a subparser when the provided command line argument is not one of the explicitely specified command. The previous fix was hacky because it relied on monkey-patching private variables in `argparse`, which could break in any new python release. So reverting the commit is not a good idea IMO.

Manually insert the `gui` command before the URI when the first argument is a URI.
Note that this is incompatible with using any global argument, but the scope of this diff is to fix mimetype association only, and in this case the URI will be the only argument.

More advanced use cases require to properly specify the `gui` command on the command line: `./electrum-abc --dir /path/to/data gui <URI>`

Depends on D14382

Test Plan:
`./test_runner.py`

`./electrum-abc "ecash:qz4eqgk9fqs3uxxt2xwx0jpfj704e2t6rs7357tn7e?amount=1337.06&label=Donation to toto&op_return=Thx for running this service!"`

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Subscribers: Fabien, bytesofman

Differential Revision: https://reviews.bitcoinabc.org/D14383

electrum/electrum-abc
@@ -562,8 +562,8 @@ def main():
     util.setup_thread_excepthook()
 
     # parse command line
-    preprocess_cmdline_args(sys.argv)
     parser = get_parser()
+    preprocess_cmdline_args(sys.argv)
     args = parser.parse_args()
 
     config_options = process_config_options(args)

electrum/electrumabc/commands.py
@@ -37,7 +37,7 @@ import time
 from decimal import Decimal as PyDecimal  # Qt 5.12 also exports Decimal
 from functools import wraps
 
-from . import bitcoin, util
+from . import bitcoin, util, web
 from .address import Address, AddressError
 from .bitcoin import CASH, TYPE_ADDRESS, hash_160
 from .constants import PROJECT_NAME, SCRIPT_NAME, XEC
@@ -1347,7 +1347,11 @@ def add_global_options(parser):
         help="Use local 'electrum_abc_data' directory",
     )
     group.add_argument(
-        "-w", "--wallet", dest="wallet_path", help="wallet path", type=os.path.abspath
+        "-w",
+        "--wallet",
+        dest="wallet_path",
+        help="wallet path",
+        type=os.path.abspath,
     )
     group.add_argument(
         "-wp",
@@ -1517,7 +1521,7 @@ def prompt_password(prompt: str = "Password:", confirm: bool = False) -> str:
 
 
 def preprocess_cmdline_args(args):
-    """Sanitize command line args before parsing them with argparse"""
+    """Sanitize command line args before parsing them with argparse."""
     # on osx, delete Process Serial Number arg generated for apps launched in Finder
     args_to_be_removed = list(filter(lambda x: x.startswith("-psn"), args))
     for arg in args_to_be_removed:
@@ -1539,3 +1543,10 @@ def preprocess_cmdline_args(args):
             args[i] = input("Enter argument:")
         elif arg == ":":
             args[i] = prompt_password("Enter argument (will not echo):")
+
+    # Starting the application with an URI as a first argument implies cmd="gui".
+    # This happens when the application is started by the OS via a mimetype association,
+    # e.g. a "ecash:...." BIP21 URI.
+    if len(args) > 1:
+        if any(args[1].startswith(scheme + ":") for scheme in web.parseable_schemes()):
+            args.insert(1, "gui")

electrum/electrumabc/tests/test_commands.py
@@ -135,6 +135,15 @@ class TestArgParser(unittest.TestCase):
         preprocess_cmdline_args(raw_args)
         self.assertEqual(raw_args[2], txid)
 
+        # Test proper handling of BIP21 URIs
+        bip21_uri = "ecash:qz4eqgk9fqs3uxxt2xwx0jpfj704e2t6rs7357tn7e?amount=1337.06&label=Donation to toto&op_return=Thx for running this service!"
+        raw_args = ["./electrum-abc", bip21_uri]
+        preprocess_cmdline_args(raw_args)
+        self.assertEqual(raw_args, ["./electrum-abc", "gui", bip21_uri])
+        args = self.parser.parse_args(raw_args[1:])
+        self.assertEqual(args.cmd, "gui")
+        self.assertEqual(args.url, bip21_uri)
+
 
 def suite():
     test_suite = unittest.TestSuite()