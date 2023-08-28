6262ec955923e20fe7a964a6d32dad4b5b905c27
PiRK <pierrekn@gmail.com>
2023-08-28 16:13:06 +0200

    remove unused SERIALIZE_TRANSACTION const
    Summary: This is unused since rABCa3bbb62c1c9856a272463f56cb4c868c87067765
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14419

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -13,8 +13,6 @@
 #include <script/script.h>
 #include <serialize.h>
 
-static const int SERIALIZE_TRANSACTION = 0x00;
-
 /**
  * An outpoint - a combination of a transaction hash and an index n into its
  * vout.
