Bitcoin-ABC/bitcoin-abc

commit 9e068b92c3af64a4a131b3479354b2989ff5134f

[avalanche] Use SteadyMilliseconds where possible
Summary:
`std::chrono::time_point<std::chrono::steady_clock>` is a mouthful and doesn't hint about the expected resolution. Use SteadyMilliseconds where due instead as this is already available in our time.h header.
The `Now()` function is made constexpr to make sure there is no performance cost over using the standard now() function.

There is no change in behavior.

Depends on D14553.

Test Plan:
  ninja all check-all

Reviewers: #bitcoin_abc, PiRK

Reviewed By: #bitcoin_abc, PiRK

Differential Revision: https://reviews.bitcoinabc.org/D14554

src/avalanche/node.h
@@ -7,6 +7,7 @@
 
 #include <nodeid.h>
 #include <pubkey.h>
+#include <util/time.h>
 
 #include <chrono>
 #include <cstdint>
@@ -14,7 +15,7 @@
 using PeerId = uint32_t;
 static constexpr PeerId NO_PEER = -1;
 
-using TimePoint = std::chrono::time_point<std::chrono::steady_clock>;
+using TimePoint = SteadyMilliseconds;
 
 namespace avalanche {
 
@@ -26,8 +27,7 @@ struct Node {
 
     Node(NodeId nodeid_, PeerId peerid_)
         : nodeid(nodeid_), peerid(peerid_),
-          nextRequestTime(std::chrono::steady_clock::now()),
-          avaproofsSent(false) {}
+          nextRequestTime(Now<SteadyMilliseconds>()), avaproofsSent(false) {}
 };
 
 } // namespace avalanche

src/avalanche/peermanager.cpp
@@ -15,6 +15,7 @@
 #include <uint256.h>
 #include <util/fastrange.h>
 #include <util/system.h>
+#include <util/time.h>
 #include <validation.h> // For ChainstateManager
 
 #include <algorithm>
@@ -579,8 +580,8 @@ bool PeerManager::removePeer(const PeerId peerid) {
     // active. This ensure that we don't overquery them in case they are
     // subsequently added to another peer.
     nview.erase(nview.lower_bound(boost::make_tuple(peerid, TimePoint())),
-                nview.upper_bound(boost::make_tuple(
-                    peerid, std::chrono::steady_clock::now())));
+                nview.upper_bound(
+                    boost::make_tuple(peerid, Now<SteadyMilliseconds>())));
 
     // Release UTXOs attached to this proof.
     validProofPool.removeProof(it->getProofId());

src/avalanche/processor.cpp
@@ -17,6 +17,7 @@
 #include <scheduler.h>
 #include <util/bitmanip.h>
 #include <util/moneystr.h>
+#include <util/time.h>
 #include <util/translation.h>
 #include <validation.h>
 
@@ -484,7 +485,7 @@ bool Processor::registerVotes(NodeId nodeid, const Response &response,
         // message. This should check that the message is indeed the most up to
         // date one before updating the time.
         peerManager->updateNextRequestTime(
-            nodeid, std::chrono::steady_clock::now() +
+            nodeid, Now<SteadyMilliseconds>() +
                         std::chrono::milliseconds(response.getCooldown()));
     }
 
@@ -921,7 +922,7 @@ void Processor::runEventLoop() {
 
                 {
                     // Compute the time at which this requests times out.
-                    auto timeout = std::chrono::steady_clock::now() +
+                    auto timeout = Now<SteadyMilliseconds>() +
                                    avaconfig.queryTimeoutDuration;
                     // Register the query.
                     queries.getWriteView()->insert(

src/avalanche/test/peermanager_tests.cpp
@@ -469,8 +469,7 @@ BOOST_AUTO_TEST_CASE(node_crud) {
     for (int i = 0; i < 100; i++) {
         NodeId n = pm.selectNode();
         BOOST_CHECK(n >= 0 && n < 4);
-        BOOST_CHECK(
-            pm.updateNextRequestTime(n, std::chrono::steady_clock::now()));
+        BOOST_CHECK(pm.updateNextRequestTime(n, Now<SteadyMilliseconds>()));
     }
 
     // Remove a node, check that it doesn't show up.
@@ -479,19 +478,17 @@ BOOST_AUTO_TEST_CASE(node_crud) {
     for (int i = 0; i < 100; i++) {
         NodeId n = pm.selectNode();
         BOOST_CHECK(n == 0 || n == 1 || n == 3);
-        BOOST_CHECK(
-            pm.updateNextRequestTime(n, std::chrono::steady_clock::now()));
+        BOOST_CHECK(pm.updateNextRequestTime(n, Now<SteadyMilliseconds>()));
     }
 
     // Push a node's timeout in the future, so that it doesn't show up.
-    BOOST_CHECK(pm.updateNextRequestTime(1, std::chrono::steady_clock::now() +
+    BOOST_CHECK(pm.updateNextRequestTime(1, Now<SteadyMilliseconds>() +
                                                 std::chrono::hours(24)));
 
     for (int i = 0; i < 100; i++) {
         NodeId n = pm.selectNode();
         BOOST_CHECK(n == 0 || n == 3);
-        BOOST_CHECK(
-            pm.updateNextRequestTime(n, std::chrono::steady_clock::now()));
+        BOOST_CHECK(pm.updateNextRequestTime(n, Now<SteadyMilliseconds>()));
     }
 
     // Move a node from a peer to another. This peer has a very low score such
@@ -507,8 +504,7 @@ BOOST_AUTO_TEST_CASE(node_crud) {
         } else {
             BOOST_CHECK_EQUAL(n, 0);
         }
-        BOOST_CHECK(
-            pm.updateNextRequestTime(n, std::chrono::steady_clock::now()));
+        BOOST_CHECK(pm.updateNextRequestTime(n, Now<SteadyMilliseconds>()));
     }
 }
 
@@ -840,7 +836,7 @@ BOOST_AUTO_TEST_CASE(dangling_node) {
     PeerId peerid = TestPeerManager::registerAndGetPeerId(pm, proof);
     BOOST_CHECK_NE(peerid, NO_PEER);
 
-    const TimePoint theFuture(std::chrono::steady_clock::now() +
+    const TimePoint theFuture(Now<SteadyMilliseconds>() +
                               std::chrono::hours(24));
 
     // Add nodes to this peer and update their request time far in the future
@@ -1454,7 +1450,7 @@ BOOST_AUTO_TEST_CASE(should_request_more_nodes) {
         BOOST_CHECK(pm.addNode(i, proofid));
     }
 
-    auto cooldownTimepoint = std::chrono::steady_clock::now() + 10s;
+    auto cooldownTimepoint = Now<SteadyMilliseconds>() + 10s;
 
     // All the nodes can be selected once
     for (size_t i = 0; i < 10; i++) {
@@ -1475,7 +1471,7 @@ BOOST_AUTO_TEST_CASE(should_request_more_nodes) {
     }
 
     // Make it possible to request a node again
-    BOOST_CHECK(pm.updateNextRequestTime(0, std::chrono::steady_clock::now()));
+    BOOST_CHECK(pm.updateNextRequestTime(0, Now<SteadyMilliseconds>()));
     BOOST_CHECK_NE(pm.selectNode(), NO_NODE);
     BOOST_CHECK(!pm.shouldRequestMoreNodes());
 

src/net.h
@@ -30,6 +30,7 @@
 #include <threadinterrupt.h>
 #include <uint256.h>
 #include <util/check.h>
+#include <util/time.h>
 #include <validation.h> // For cs_main
 
 #include <atomic>
@@ -679,7 +680,7 @@ public:
     // How much faulty messages did this node accumulate
     std::atomic<int> m_avalanche_message_fault_counter{0};
 
-    std::chrono::time_point<std::chrono::steady_clock> m_last_poll{};
+    SteadyMilliseconds m_last_poll{};
 
     /**
      * UNIX epoch time of the last block received from this peer that we had

src/net_processing.cpp
@@ -5033,7 +5033,7 @@ void PeerManagerImpl::ProcessMessage(
     }
 
     if (msg_type == NetMsgType::AVAPOLL) {
-        auto now = std::chrono::steady_clock::now();
+        auto now = Now<SteadyMilliseconds>();
         int64_t cooldown =
             gArgs.GetIntArg("-avacooldown", AVALANCHE_DEFAULT_COOLDOWN);
 

src/util/time.h
@@ -90,7 +90,7 @@ template <typename T> T GetTime();
  * Return the current time point cast to the given precision. Only use this
  * when an exact precision is needed, otherwise use T::clock::now() directly.
  */
-template <typename T> T Now() {
+template <typename T> constexpr T Now() {
     return std::chrono::time_point_cast<typename T::duration>(T::clock::now());
 }
 

commit 165e87dacdb8a4ee11d62a4b393a97cc36cd637b

[avalanche] Remove TimePoint in favor of SteadyMilliseconds
Summary:
This removes the TimePoint from node.h entirely. It also fixes a test
issue introduced in D14554: previously the TimePoint type did not
specify a duration so it used the full resolution of the clock. This
caused the test to fail because of a mismatch between the now() call
using the full resolution of the clock and the TimePoint resolution to
the milliseconds. The test was testing for strict inequality, while the
expected behavior is greater or equal: this never failed before because
the clock resolution is too high for the equality to happen. This is no
longer the case after the resolution is fixed to milliseconds, and the
test can fail if it runs fast enough.

Supersedes D14558.

Test Plan:
  ninja all check-all

  for i in {1..1000}; do echo "Trial $i" && ./src/avalanche/test/test-avalanche -t processor_tests || break; done

Reviewers: #bitcoin_abc, PiRK

Reviewed By: #bitcoin_abc, PiRK

Differential Revision: https://reviews.bitcoinabc.org/D14562

src/avalanche/node.h
@@ -15,14 +15,12 @@
 using PeerId = uint32_t;
 static constexpr PeerId NO_PEER = -1;
 
-using TimePoint = SteadyMilliseconds;
-
 namespace avalanche {
 
 struct Node {
     NodeId nodeid;
     PeerId peerid;
-    TimePoint nextRequestTime;
+    SteadyMilliseconds nextRequestTime;
     bool avaproofsSent;
 
     Node(NodeId nodeid_, PeerId peerid_)

src/avalanche/peermanager.cpp
@@ -153,7 +153,8 @@ bool PeerManager::removeNodeFromPeer(const PeerSet::iterator &it,
     return true;
 }
 
-bool PeerManager::updateNextRequestTime(NodeId nodeid, TimePoint timeout) {
+bool PeerManager::updateNextRequestTime(NodeId nodeid,
+                                        SteadyMilliseconds timeout) {
     auto it = nodes.find(nodeid);
     if (it == nodes.end()) {
         return false;
@@ -476,9 +477,9 @@ NodeId PeerManager::selectNode() {
 
         // See if that peer has an available node.
         auto &nview = nodes.get<next_request_time>();
-        auto it = nview.lower_bound(boost::make_tuple(p, TimePoint()));
+        auto it = nview.lower_bound(boost::make_tuple(p, SteadyMilliseconds()));
         if (it != nview.end() && it->peerid == p &&
-            it->nextRequestTime <= std::chrono::steady_clock::now()) {
+            it->nextRequestTime <= Now<SteadyMilliseconds>()) {
             return it->nodeid;
         }
     }
@@ -579,9 +580,10 @@ bool PeerManager::removePeer(const PeerId peerid) {
     // Remove nodes associated with this peer, unless their timeout is still
     // active. This ensure that we don't overquery them in case they are
     // subsequently added to another peer.
-    nview.erase(nview.lower_bound(boost::make_tuple(peerid, TimePoint())),
-                nview.upper_bound(
-                    boost::make_tuple(peerid, Now<SteadyMilliseconds>())));
+    nview.erase(
+        nview.lower_bound(boost::make_tuple(peerid, SteadyMilliseconds())),
+        nview.upper_bound(
+            boost::make_tuple(peerid, Now<SteadyMilliseconds>())));
 
     // Release UTXOs attached to this proof.
     validProofPool.removeProof(it->getProofId());
@@ -717,10 +719,10 @@ bool PeerManager::verify() const {
         const auto count_nodes = [&]() {
             size_t count = 0;
             auto &nview = nodes.get<next_request_time>();
-            auto begin =
-                nview.lower_bound(boost::make_tuple(p.peerid, TimePoint()));
-            auto end =
-                nview.upper_bound(boost::make_tuple(p.peerid + 1, TimePoint()));
+            auto begin = nview.lower_bound(
+                boost::make_tuple(p.peerid, SteadyMilliseconds()));
+            auto end = nview.upper_bound(
+                boost::make_tuple(p.peerid + 1, SteadyMilliseconds()));
 
             for (auto it = begin; it != end; ++it) {
                 count++;

src/avalanche/peermanager.h
@@ -183,16 +183,16 @@ class PeerManager {
     ProofRadixTree shareableProofs;
 
     using NodeSet = boost::multi_index_container<
-        Node,
-        bmi::indexed_by<
-            // index by nodeid
-            bmi::hashed_unique<bmi::member<Node, NodeId, &Node::nodeid>>,
-            // sorted by peerid/nextRequestTime
-            bmi::ordered_non_unique<
-                bmi::tag<next_request_time>,
-                bmi::composite_key<
-                    Node, bmi::member<Node, PeerId, &Node::peerid>,
-                    bmi::member<Node, TimePoint, &Node::nextRequestTime>>>>>;
+        Node, bmi::indexed_by<
+                  // index by nodeid
+                  bmi::hashed_unique<bmi::member<Node, NodeId, &Node::nodeid>>,
+                  // sorted by peerid/nextRequestTime
+                  bmi::ordered_non_unique<
+                      bmi::tag<next_request_time>,
+                      bmi::composite_key<
+                          Node, bmi::member<Node, PeerId, &Node::peerid>,
+                          bmi::member<Node, SteadyMilliseconds,
+                                      &Node::nextRequestTime>>>>>;
 
     NodeSet nodes;
 
@@ -258,7 +258,7 @@ public:
     size_t getPendingNodeCount() const { return pendingNodes.size(); }
 
     // Update when a node is to be polled next.
-    bool updateNextRequestTime(NodeId nodeid, TimePoint timeout);
+    bool updateNextRequestTime(NodeId nodeid, SteadyMilliseconds timeout);
     /**
      * Flag that a node did send its compact proofs.
      * @return True if the flag changed state, i;e. if this is the first time

src/avalanche/processor.cpp
@@ -955,7 +955,7 @@ void Processor::runEventLoop() {
 }
 
 void Processor::clearTimedoutRequests() {
-    auto now = std::chrono::steady_clock::now();
+    auto now = Now<SteadyMilliseconds>();
     std::map<CInv, uint8_t> timedout_items{};
 
     {

src/avalanche/processor.h
@@ -203,7 +203,7 @@ class Processor final : public NetEventsInterface {
     struct Query {
         NodeId nodeid;
         uint64_t round;
-        TimePoint timeout;
+        SteadyMilliseconds timeout;
 
         /**
          * We declare this as mutable so it can be modified in the multi_index.
@@ -225,7 +225,7 @@ class Processor final : public NetEventsInterface {
             // sorted by timeout
             boost::multi_index::ordered_non_unique<
                 boost::multi_index::tag<query_timeout>,
-                boost::multi_index::member<Query, TimePoint,
+                boost::multi_index::member<Query, SteadyMilliseconds,
                                            &Query::timeout>>>>;
 
     RWCollection<QuerySet> queries;

src/avalanche/test/peermanager_tests.cpp
@@ -836,8 +836,8 @@ BOOST_AUTO_TEST_CASE(dangling_node) {
     PeerId peerid = TestPeerManager::registerAndGetPeerId(pm, proof);
     BOOST_CHECK_NE(peerid, NO_PEER);
 
-    const TimePoint theFuture(Now<SteadyMilliseconds>() +
-                              std::chrono::hours(24));
+    const SteadyMilliseconds theFuture(Now<SteadyMilliseconds>() +
+                                       std::chrono::hours(24));
 
     // Add nodes to this peer and update their request time far in the future
     for (int i = 0; i < 10; i++) {

src/avalanche/test/processor_tests.cpp
@@ -1038,7 +1038,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(poll_inflight_timeout, P, VoteItemProviders) {
         Response resp = {getRound(), 0, {Vote(0, itemid)}};
         avanodeid = getSuitableNodeToQuery();
 
-        auto start = std::chrono::steady_clock::now();
+        auto start = Now<SteadyMilliseconds>();
         runEventLoop();
         // We cannot guarantee that we'll wait for just 1ms, so we have to bail
         // if we aren't within the proper time range.
@@ -1047,7 +1047,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(poll_inflight_timeout, P, VoteItemProviders) {
 
         std::vector<avalanche::VoteItemUpdate> updates;
         bool ret = registerVotes(avanodeid, next(resp), updates);
-        if (std::chrono::steady_clock::now() > start + queryTimeDuration) {
+        if (Now<SteadyMilliseconds>() > start + queryTimeDuration) {
             // We waited for too long, bail. Because we can't know for sure when
             // previous steps ran, ret is not deterministic and we do not check
             // it.
@@ -1237,8 +1237,7 @@ BOOST_AUTO_TEST_CASE(event_loop) {
 
     // Respond and check the cooldown time is respected.
     uint64_t responseRound = getRound();
-    auto queryTime =
-        std::chrono::steady_clock::now() + std::chrono::milliseconds(100);
+    auto queryTime = Now<SteadyMilliseconds>() + std::chrono::milliseconds(100);
 
     std::vector<VoteItemUpdate> updates;
     // Only the first node answers, so it's the only one that gets polled again
@@ -1249,7 +1248,7 @@ BOOST_AUTO_TEST_CASE(event_loop) {
         // We make sure that we do not get a request before queryTime.
         UninterruptibleSleep(std::chrono::milliseconds(1));
         if (getRound() != responseRound) {
-            BOOST_CHECK(std::chrono::steady_clock::now() > queryTime);
+            BOOST_CHECK(Now<SteadyMilliseconds>() >= queryTime);
             break;
         }
     }