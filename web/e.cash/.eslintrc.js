f7a63af3afd64162a27011f8217bb5d965c79f16
bytesofman <joey@bitcoinabc.org>
2023-10-23 12:59:43 -0700

    [arclint] Lint web/e.cash
    Summary:
    NextJS has its own special eslint configuration. It's run automatically on build. However, it is not run directly with `eslint`, the same way that `.arclint` and other JS repos run.
    
    This was causing lint issues to be missed until CI ran `npm run build`, triggering the special next lint in CI.
    
    `arc lint` was throwing a `module not found` error for the special nextjs dependencies. I corrected this by following a solution recommended here: https://github.com/vercel/next.js/issues/40687
    
    Test Plan: Add quotation marks to a /web/e.cash/pages/careers.js. Note lint errors, in VScode if you are using it, or by running `arc lint`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14668

--- web/e.cash/.eslintrc.js
+++ web/e.cash/.eslintrc.js
@@ -9,7 +9,21 @@ const headerArray = [
     ' file COPYING or http://www.opensource.org/licenses/mit-license.php.',
 ];
 module.exports = {
+    // root: true as this repo exists within a monorepo
+    // We only want these special next settings to be used in this repo
+    // https://github.com/vercel/next.js/issues/40687
+    root: true,
     extends: 'next/core-web-vitals',
+    overrides: [
+        {
+            files: ['*.js'],
+            // This is the default parser of ESLint
+            parser: 'espree',
+            parserOptions: {
+                ecmaVersion: 2020,
+            },
+        },
+    ],
     plugins: ['header'],
     rules: {
         'header/header': [
