Bitcoin-ABC/bitcoin-abc

commit 03a00cdce04915a941ca80ae95f49fb77c2247bc

[electrum] never suggest the same proof master key or delegation key twice
Summary:
Previously we would always suggest the auxiliary key with index 0 as the proof master key, and open a key generation widget which shows index 1 by default when generating a delegation.

Store a counter/index as wallet data and increment it whenever a new auxiliary key is shown, so that we always suggest a new key. Users can still find their old keys by opening the Auxiliary keys tool and decrementing the value, but we will never suggest using an old one.

Bump the range of the index widget to allow the full range of indices, as per bip 32.

Test Plan:
Open a wallet, open the proof editor dialog, close it and reopen a new proof editor dialog, check that the master key is different.
Then open a delegation editor dialog, click "generate key" and check that it opens an auxiliary key dialog with index 2 (0 and 1 were used for the two proofs). Increment the indexto 3, close the delegation dialog.
Then open the "Auxiliary keys" dialog via the "Wallet" menu, check that the index is now 4.

Close the wallet, reopen it, check that the index is now 5.

Set the index to 2147483647 (MAX value), close the wallet, reopen it and check that opening the auxiliary keys tool does not cause an overflow error (and the index is now stuck at 2147483647)

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Subscribers: Fabien

Differential Revision: https://reviews.bitcoinabc.org/D14603

electrum/electrumabc_gui/qt/avalanche/proof_editor.py
@@ -13,6 +13,7 @@ from electrumabc.avalanche.proof import Proof, ProofBuilder, SignedStake, Stake
 from electrumabc.bitcoin import is_private_key
 from electrumabc.constants import PROOF_DUST_THRESHOLD, STAKE_UTXO_CONFIRMATIONS
 from electrumabc.i18n import _
+from electrumabc.keystore import MAXIMUM_INDEX_DERIVATION_PATH
 from electrumabc.serialize import DeserializationError, compact_size, serialize_blob
 from electrumabc.transaction import OutPoint, get_address_from_output_script
 from electrumabc.uint256 import UInt256
@@ -21,8 +22,6 @@ from electrumabc.wallet import AddressNotFoundError, DeterministicWallet
 
 from .delegation_editor import AvaDelegationDialog
 from .util import ButtonsLineEdit, CachedWalletPasswordWidget, get_auxiliary_privkey
-
-PROOF_MASTER_KEY_INDEX = 0
 
 
 @dataclass
@@ -408,8 +407,13 @@ class AvaProofEditor(CachedWalletPasswordWidget):
             return ""
         wif_pk = ""
         if not self.wallet.has_password() or self.pwd is not None:
+            auxiliary_key_index = self.wallet.storage.get("auxiliary_key_index", 0)
             wif_pk = get_auxiliary_privkey(
-                self.wallet, key_index=PROOF_MASTER_KEY_INDEX, pwd=self.pwd
+                self.wallet, key_index=auxiliary_key_index, pwd=self.pwd
+            )
+            self.wallet.storage.put(
+                "auxiliary_key_index",
+                min(auxiliary_key_index + 1, MAXIMUM_INDEX_DERIVATION_PATH),
             )
         return wif_pk
 

electrum/electrumabc_gui/qt/avalanche/util.py
@@ -6,6 +6,7 @@ from PyQt5 import QtWidgets
 
 from electrumabc.address import PublicKey
 from electrumabc.bitcoin import is_private_key
+from electrumabc.keystore import MAXIMUM_INDEX_DERIVATION_PATH
 from electrumabc.wallet import DeterministicWallet
 
 from ..password_dialog import PasswordDialog
@@ -138,17 +139,30 @@ class AuxiliaryKeysWidget(CachedWalletPasswordWidget):
 
         index_layout.addWidget(QtWidgets.QLabel("Key index"))
         self.index_spinbox = QtWidgets.QSpinBox()
-        self.index_spinbox.setRange(0, 1000)
+        self.index_spinbox.setRange(0, MAXIMUM_INDEX_DERIVATION_PATH)
         index_layout.addWidget(self.index_spinbox)
         index_layout.addStretch(1)
 
         self.key_widget = KeyWidget()
         layout.addWidget(self.key_widget)
 
-        self.set_index(self.index_spinbox.value())
-        self.index_spinbox.valueChanged.connect(self.set_index)
+        self.max_shown_index = self.wallet.storage.get("auxiliary_key_index", 0)
+        # increment for the next time
+        self.wallet.storage.put(
+            "auxiliary_key_index",
+            min(MAXIMUM_INDEX_DERIVATION_PATH, self.max_shown_index + 1),
+        )
+        self.index_spinbox.valueChanged.connect(self.on_index_changed)
+        self.index_spinbox.setValue(self.max_shown_index)
 
-    def set_index(self, index: int):
+    def on_index_changed(self, index: int):
+        if index > self.max_shown_index:
+            self.max_shown_index = index
+            self.wallet.storage.put(
+                "auxiliary_key_index",
+                min(MAXIMUM_INDEX_DERIVATION_PATH, self.max_shown_index + 1),
+            )
+
         wif_key = get_auxiliary_privkey(self.wallet, index, self.pwd)
         self.key_widget.setPrivkey(wif_key)
 
@@ -185,9 +199,6 @@ class AuxiliaryKeysDialog(QtWidgets.QDialog):
         layout.addWidget(self.ok_button)
 
         self.ok_button.clicked.connect(self.accept)
-
-    def set_index(self, index: int):
-        self.aux_keys_widget.set_index(index)
 
     def get_hex_public_key(self) -> str:
         return self.aux_keys_widget.get_hex_public_key()

electrum/electrumabc/keystore.py
@@ -48,6 +48,10 @@ if TYPE_CHECKING:
     from .transaction import Transaction
 
 
+# as per bip-0032
+MAXIMUM_INDEX_DERIVATION_PATH = 2**31 - 1
+
+
 class KeyStore(PrintError):
     type: str
 

electrum/electrumabc_gui/qt/avalanche/delegation_editor.py
@@ -149,7 +149,6 @@ class AvaDelegationWidget(CachedWalletPasswordWidget):
             "a Bitcoin ABC node."
         )
         d = AuxiliaryKeysDialog(self.wallet, self.pwd, self, additional_info)
-        d.set_index(1)
         d.exec_()
 
         self.pubkey_edit.setText(d.get_hex_public_key())

commit 5dcccb27716f510d6651baf843d709f3c4ac6393

[electrum] detect the master private key when loading a proof
Summary:
When loading a proof in the wallet that created it, if the master key is one the auxiliary private keys, we can find the private key from the deserialized public key instead of making the user paste the key.

This used to work with the assumption that we always use index 0 to derive the key for proofs. It is broken after D14603 which broke this assumption: the program now does not necessarily suggests key from index 0 when the user generates a second proof or uses keys for other purposes (delegations...)

Fix it by scanning up to 20 recently used auxiliary keys.

Introduce a `StorageKeys` class to avoid repeating (and possibly mistyping) the storage keys and their default values.

Test Plan:
Create a skeleton proof, save it to file or copy the hex to the clipboard.
Close the wallet, reopen the same wallet, open the proof editor, load the proof and check that the master key (WIF) is correctly filled and no warning dialog is displayed.

Reviewers: #bitcoin_abc, bytesofman, Fabien

Reviewed By: #bitcoin_abc, bytesofman, Fabien

Subscribers: bytesofman

Differential Revision: https://reviews.bitcoinabc.org/D14726

electrum/electrumabc/storage.py
@@ -29,6 +29,8 @@ import os
 import stat
 import threading
 import zlib
+from dataclasses import dataclass
+from typing import Any, Optional, Union
 
 from . import bitcoin
 from .json_db import JsonDB
@@ -47,6 +49,17 @@ def get_derivation_used_for_hw_device_encryption():
 
 # storage encryption version
 STO_EV_PLAINTEXT, STO_EV_USER_PW, STO_EV_XPUB_PW = range(0, 3)
+
+
+@dataclass
+class StorageKey:
+    key: str
+    default: Optional[Any] = None
+
+
+class StorageKeys:
+    AUXILIARY_KEY_INDEX = StorageKey("auxiliary_key_index", 0)
+    GAP_LIMIT = StorageKey("gap_limit", 20)
 
 
 class WalletStorage(PrintError):
@@ -71,10 +84,15 @@ class WalletStorage(PrintError):
             # avoid new wallets getting 'upgraded'
             self.db = DB_Class("", manual_upgrades=False)
 
-    def put(self, key, value):
+    def put(self, key: Union[str, StorageKey], value):
+        if isinstance(key, StorageKey):
+            key = key.key
         self.db.put(key, value)
 
-    def get(self, key, default=None):
+    def get(self, key: Union[str, StorageKey], default=None):
+        if isinstance(key, StorageKey):
+            assert default is None
+            key, default = key.key, key.default
         return self.db.get(key, default)
 
     @profiler

electrum/electrumabc/wallet.py
@@ -91,7 +91,13 @@ from .paymentrequest import (
 )
 from .plugins import plugin_loaders, run_hook
 from .printerror import PrintError
-from .storage import STO_EV_PLAINTEXT, STO_EV_USER_PW, STO_EV_XPUB_PW, WalletStorage
+from .storage import (
+    STO_EV_PLAINTEXT,
+    STO_EV_USER_PW,
+    STO_EV_XPUB_PW,
+    StorageKeys,
+    WalletStorage,
+)
 from .synchronizer import Synchronizer
 from .transaction import (
     DUST_THRESHOLD,
@@ -3355,7 +3361,7 @@ class DeterministicWallet(AbstractWallet):
     def __init__(self, storage):
         self.keystore: Optional[DeterministicKeyStore] = None
         AbstractWallet.__init__(self, storage)
-        self.gap_limit = storage.get("gap_limit", 20)
+        self.gap_limit = storage.get(StorageKeys.GAP_LIMIT)
 
     def has_seed(self):
         return self.keystore.has_seed()
@@ -3379,7 +3385,7 @@ class DeterministicWallet(AbstractWallet):
         with self.lock:
             if value >= self.gap_limit:
                 self.gap_limit = value
-                self.storage.put("gap_limit", self.gap_limit)
+                self.storage.put(StorageKeys.GAP_LIMIT, self.gap_limit)
                 return True
             elif value >= self.min_acceptable_gap():
                 addresses = self.get_receiving_addresses()
@@ -3387,7 +3393,7 @@ class DeterministicWallet(AbstractWallet):
                 n = len(addresses) - k + value
                 self.receiving_addresses = self.receiving_addresses[0:n]
                 self.gap_limit = value
-                self.storage.put("gap_limit", self.gap_limit)
+                self.storage.put(StorageKeys.GAP_LIMIT, self.gap_limit)
                 self.save_addresses()
                 return True
             else:
@@ -3783,7 +3789,7 @@ def restore_wallet_from_text(
         if seed_type:
             storage.put("seed_type", seed_type)  # Save, just in case
         if gap_limit is not None:
-            storage.put("gap_limit", gap_limit)
+            storage.put(StorageKeys.GAP_LIMIT, gap_limit)
         wallet = Wallet(storage)
 
     wallet.update_password(old_pw=None, new_pw=password, encrypt_storage=encrypt_file)

electrum/electrumabc_gui/qt/avalanche/proof_editor.py
@@ -15,6 +15,7 @@ from electrumabc.constants import PROOF_DUST_THRESHOLD, STAKE_UTXO_CONFIRMATIONS
 from electrumabc.i18n import _
 from electrumabc.keystore import MAXIMUM_INDEX_DERIVATION_PATH
 from electrumabc.serialize import DeserializationError, compact_size, serialize_blob
+from electrumabc.storage import StorageKeys
 from electrumabc.transaction import OutPoint, get_address_from_output_script
 from electrumabc.uint256 import UInt256
 from electrumabc.util import format_satoshis
@@ -407,12 +408,14 @@ class AvaProofEditor(CachedWalletPasswordWidget):
             return ""
         wif_pk = ""
         if not self.wallet.has_password() or self.pwd is not None:
-            auxiliary_key_index = self.wallet.storage.get("auxiliary_key_index", 0)
+            auxiliary_key_index = self.wallet.storage.get(
+                StorageKeys.AUXILIARY_KEY_INDEX,
+            )
             wif_pk = get_auxiliary_privkey(
                 self.wallet, key_index=auxiliary_key_index, pwd=self.pwd
             )
             self.wallet.storage.put(
-                "auxiliary_key_index",
+                StorageKeys.AUXILIARY_KEY_INDEX,
                 min(auxiliary_key_index + 1, MAXIMUM_INDEX_DERIVATION_PATH),
             )
         return wif_pk
@@ -517,12 +520,45 @@ class AvaProofEditor(CachedWalletPasswordWidget):
         self.generate_dg_button.setEnabled(True)
         self.save_proof_button.setEnabled(True)
 
+    def find_auxiliary_privkey_from_pubkey(self, pubkey: PublicKey) -> Optional[str]:
+        """Try to find the master private key from the master public key
+        by scanning a range of recent auxiliary keys.
+        Return the key in WIF format, or None."""
+        if (
+            not self.wallet.is_deterministic()
+            or not self.wallet.can_export()
+            or (self.wallet.has_password() and self.pwd is None)
+        ):
+            return None
+        auxiliary_key_index = self.wallet.storage.get(StorageKeys.AUXILIARY_KEY_INDEX)
+        gap_limit = self.wallet.storage.get(StorageKeys.GAP_LIMIT)
+        wif_pk = None
+        # Scan backwards from the current auxiliary_key_index, because in most cases the key will be the most recent one.
+        for i in range(
+            auxiliary_key_index, max(-1, auxiliary_key_index - gap_limit), -1
+        ):
+            maybe_wif_pk = get_auxiliary_privkey(self.wallet, key_index=i, pwd=self.pwd)
+            if Key.from_wif(maybe_wif_pk).get_pubkey() == pubkey:
+                wif_pk = maybe_wif_pk
+                break
+        return wif_pk
+
     def load_proof(self, proof: Proof):
-        known_keys = []
-        if self._get_privkey_suggestion():
-            known_keys.append(self._get_privkey_suggestion())
-        if is_private_key(self.master_key_edit.text()):
-            known_keys.append(self.master_key_edit.text())
+        # Figure out whether we know the private key associated with the proof's master public key.
+        # First check the key that is currently typed in the privkey widget. If not so, try a range of recently used auxiliary keys.
+        provided_privkey = (
+            self.master_key_edit.text()
+            if is_private_key(self.master_key_edit.text())
+            else None
+        )
+        if (
+            provided_privkey is not None
+            and Key.from_wif(provided_privkey).get_pubkey() == proof.master_pub
+        ):
+            known_privkey = provided_privkey
+        else:
+            known_privkey = self.find_auxiliary_privkey_from_pubkey(proof.master_pub)
+
         self.init_data()
 
         self.sequence_sb.setValue(proof.sequence)
@@ -531,12 +567,8 @@ class AvaProofEditor(CachedWalletPasswordWidget):
         else:
             self.timestamp_widget.setValue(proof.expiration_time)
 
-        self.master_key_edit.setText("")
-        for wif_key in known_keys:
-            if Key.from_wif(wif_key).get_pubkey() == proof.master_pub:
-                self.master_key_edit.setText(wif_key)
-                break
-        else:
+        if known_privkey is None:
+            self.master_key_edit.setText("")
             QtWidgets.QMessageBox.warning(
                 self,
                 "Missing private key",
@@ -544,6 +576,8 @@ class AvaProofEditor(CachedWalletPasswordWidget):
                 " key. You can fill it manually if you know it, or leave it blank"
                 " if you just want to sign your stakes, ",
             )
+        else:
+            self.master_key_edit.setText(known_privkey)
         self.master_pubkey_view.setText(proof.master_pub.to_hex())
 
         _txout_type, addr = get_address_from_output_script(proof.payout_script_pubkey)

electrum/electrumabc_gui/qt/avalanche/util.py
@@ -7,6 +7,7 @@ from PyQt5 import QtWidgets
 from electrumabc.address import PublicKey
 from electrumabc.bitcoin import is_private_key
 from electrumabc.keystore import MAXIMUM_INDEX_DERIVATION_PATH
+from electrumabc.storage import StorageKeys
 from electrumabc.wallet import DeterministicWallet
 
 from ..password_dialog import PasswordDialog
@@ -152,10 +153,10 @@ class AuxiliaryKeysWidget(CachedWalletPasswordWidget):
         self.key_widget = KeyWidget()
         layout.addWidget(self.key_widget)
 
-        self.max_shown_index = self.wallet.storage.get("auxiliary_key_index", 0)
+        self.max_shown_index = self.wallet.storage.get(StorageKeys.AUXILIARY_KEY_INDEX)
         # increment for the next time
         self.wallet.storage.put(
-            "auxiliary_key_index",
+            StorageKeys.AUXILIARY_KEY_INDEX,
             min(MAXIMUM_INDEX_DERIVATION_PATH, self.max_shown_index + 1),
         )
         self.index_spinbox.valueChanged.connect(self.on_index_changed)
@@ -165,7 +166,7 @@ class AuxiliaryKeysWidget(CachedWalletPasswordWidget):
         if index > self.max_shown_index:
             self.max_shown_index = index
             self.wallet.storage.put(
-                "auxiliary_key_index",
+                StorageKeys.AUXILIARY_KEY_INDEX,
                 min(MAXIMUM_INDEX_DERIVATION_PATH, self.max_shown_index + 1),
             )
 

electrum/electrumabc/tests/test_wallet_vertical.py
@@ -4,6 +4,7 @@ from unittest import mock
 from .. import bitcoin, keystore, mnemo, storage, wallet
 from ..address import Address, PublicKey
 from ..bitcoin import ScriptType
+from ..storage import StorageKeys
 from ..transaction import OutPoint, Transaction, TxInput, TxOutput
 
 
@@ -27,7 +28,7 @@ class TestWalletKeystoreAddressIntegrity(unittest.TestCase):
     def _create_standard_wallet(self, ks):
         store = storage.WalletStorage("if_this_exists_mocking_failed_648151893")
         store.put("keystore", ks.dump())
-        store.put("gap_limit", self.gap_limit)
+        store.put(StorageKeys.GAP_LIMIT, self.gap_limit)
         w = wallet.StandardWallet(store)
         w.synchronize()
         return w
@@ -38,7 +39,7 @@ class TestWalletKeystoreAddressIntegrity(unittest.TestCase):
         store.put("wallet_type", multisig_type)
         store.put("x1/", ks1.dump())
         store.put("x2/", ks2.dump())
-        store.put("gap_limit", self.gap_limit)
+        store.put(StorageKeys.GAP_LIMIT, self.gap_limit)
         w = wallet.MultisigWallet(store)
         w.synchronize()
         return w