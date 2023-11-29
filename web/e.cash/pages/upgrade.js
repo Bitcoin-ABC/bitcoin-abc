417096493e694d8e84dcdfd3f2830cae1bf77c3c
Antony Zegers <antony.zegers@gmail.com>
2023-11-29 14:01:33 -0800

    [e.cash] Bump latestVersion to 0.28.4 on upgrade page
    Test Plan: Run it locally and check upgrade page
    
    Reviewers: #bitcoin_abc, bytesofman
    
    Reviewed By: #bitcoin_abc, bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D14877

--- web/e.cash/pages/upgrade.js
+++ web/e.cash/pages/upgrade.js
@@ -9,7 +9,7 @@ import ExternalLink from '/components/external-link';
 import { Container, GradientSpacer } from '/components/atoms';
 import pins from '/public/animations/pins.json';
 
-const latestVersion = `0.28.3`;
+const latestVersion = `0.28.4`;
 
 function Upgrade(props) {
     return (
