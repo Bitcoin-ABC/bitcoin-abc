1b4198b436a05e90ef5cefab36c2b142af81b0c3
PiRK <pierrekn@gmail.com>
2023-12-13 10:16:25 +0100

    |contrib] fix update-timings.sh
    Summary: NON_SCRIPTS was renamed in D14915
    
    Test Plan: `BUILD_DIR=build contrib/source-control-tools/patch-recipes/update-timings.sh`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D14979

--- contrib/source-control-tools/patch-recipes/update-timings.sh
+++ contrib/source-control-tools/patch-recipes/update-timings.sh
@@ -14,7 +14,7 @@ popd
 
 # Check that all tests are included in timing.json
 pushd "${TOPLEVEL}"/test/functional
-NON_TESTS=$(python3 -c 'from test_runner import NON_SCRIPTS; print(" ".join(NON_SCRIPTS))')
+NON_TESTS=$(python3 -c 'from test_runner import NON_TESTS; print(" ".join(NON_TESTS))')
 export NON_TESTS
 EXTRA_PRIVILEGES_TESTS=$(python3 -c 'from test_runner import EXTRA_PRIVILEGES_TESTS; print(" ".join(EXTRA_PRIVILEGES_TESTS))')
 export EXTRA_PRIVILEGES_TESTS
