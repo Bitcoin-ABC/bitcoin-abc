73ac2e2b6ded29ffb591513dd84fb0a04f530b63
bytesofman <joey@bitcoinabc.org>
2023-12-14 15:37:10 -0800

    [Cashtab] Move webapp specific functions to conditionally loaded component
    Summary:
    Big goal here is to have only one App.js file to maintain. We want this file to build the Extension app if an extension env var is set, and the webapp if not.
    
    In this diff, move webapp-specific logic into its own component, WebApp.js. Conditionally include this component at build time if you are not building the extension.
    
    Test Plan:
    Change `monitorExtension` boolean to `true` in `src/config/app.js`. `npm start` and note your extension intallation status in the dev console.
    `npm run build`, then `netlify deploy` to a new URL. Load the URL. Confirm you get a request to enable notifications. Confirm persistent storage status in the log.
    
    Reviewing code, confirm that `WebApp.js` is just copy pasted of what is removed from `App.js`
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Subscribers: PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D14989

--- cashtab/src/components/App.js
+++ cashtab/src/components/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.less';
 import PropTypes from 'prop-types';
-import { Spin, Modal } from 'antd';
+import { Spin } from 'antd';
 import {
     CashLoadingIcon,
     HomeIcon,
@@ -43,7 +43,7 @@ import TabCash from 'assets/tabcash.png';
 import { checkForTokenById } from 'utils/tokenMethods.js';
 import ServiceWorkerWrapper from './Common/ServiceWorkerWrapper';
 import aliasSettings from 'config/alias';
-import appConfig from 'config/app';
+import WebApp from './AppModes/WebApp';
 
 const GlobalStyle = createGlobalStyle`
     *::placeholder {
@@ -397,110 +397,6 @@ const App = () => {
     const selectedKey =
         location && location.pathname ? location.pathname.substr(1) : '';
 
-    /* 
-    See https://web.dev/push-notifications-subscribing-a-user/
-    note that Safari still uses callback syntax, hence this method which handles both syntaxes
-    https://developer.mozilla.org/en-US/docs/Web/API/Notification/requestPermission
-    */
-
-    const askPermissionForNotifications = () => {
-        return new Promise(function (resolve, reject) {
-            const permissionResult = Notification.requestPermission(function (
-                result,
-            ) {
-                resolve(result);
-            });
-
-            if (permissionResult) {
-                permissionResult.then(resolve, reject);
-            }
-        }).then(function (permissionResult) {
-            console.log(`permissionResult`, permissionResult);
-            if (permissionResult !== 'granted') {
-                // Warning modal if user does not accept notifications
-                Modal.warn({
-                    title: 'Notification permission refused',
-                    content:
-                        'Please enable notifications from Cashtab.com to help prevent your browser from deleting Cashtab wallet info.',
-                });
-            } else {
-                console.log(`Notification permission granted`);
-                // If it is granted, ask again for persistent storage
-                // Best way to do this is to show a modal that refreshes the page
-
-                // Success modal when user accepts notifications, prompting a refresh
-                Modal.success({
-                    title: 'Notification permission granted',
-                    content: 'Please refresh the page',
-                    onOk: () => {
-                        console.log(
-                            `Reloading page to retry for persistent storage permission`,
-                        );
-                        window.location.reload(true);
-                    },
-                });
-            }
-        });
-    };
-
-    const checkForPersistentStorage = async () => {
-        // Request persistent storage for site
-        if (navigator.storage && navigator.storage.persist) {
-            // Check if storage is persistent
-            const isPersisted = await navigator.storage.persisted();
-            console.log(`Persisted storage status: ${isPersisted}`);
-            // If not, request persistent storage
-            if (!isPersisted) {
-                console.log(`Requesting persistent storage`);
-                const persistanceRequestResult =
-                    await navigator.storage.persist();
-                console.log(
-                    `Persistent storage granted: ${persistanceRequestResult}`,
-                );
-                // If the request was not granted, ask the user to approve notification permissions
-                if (!persistanceRequestResult) {
-                    // Check if the browser supports notifications
-                    const browserSupportsNotifications =
-                        'Notification' in window;
-
-                    console.log(
-                        `browserSupportsNotifications`,
-                        browserSupportsNotifications,
-                    );
-
-                    // If the browser supports notifications, check the permission status
-                    if (browserSupportsNotifications) {
-                        const cashtabNotificationPermission =
-                            Notification.permission;
-
-                        console.log(
-                            `cashtabNotificationPermission`,
-                            cashtabNotificationPermission,
-                        );
-
-                        // If the permission is not 'granted', ask for notification permissions
-                        if (cashtabNotificationPermission !== 'granted') {
-                            console.log(
-                                `User has not granted notification permission and persistent storage status has not been granted. Requesting notification permission.`,
-                            );
-                            return askPermissionForNotifications();
-                        }
-                    }
-                }
-            }
-        }
-    };
-
-    const getExtensionInstallationStatus = async () => {
-        // Wait 2s to check
-        // window.bitcoinAbc is set by the extension, and is undefined on page load
-        // We will also want to wait for some configurable interval anyway, so that the page
-        // does not load with a popup
-        const popupWaitInterval = 2000;
-        await new Promise(resolve => setTimeout(resolve, popupWaitInterval));
-        return window && window.bitcoinAbc && window.bitcoinAbc === 'cashtab';
-    };
-
     // Easter egg boolean not used in extension/src/components/App.js
     const hasTab = validWallet
         ? checkForTokenById(
@@ -509,23 +405,11 @@ const App = () => {
           )
         : false;
 
-    useEffect(async () => {
-        checkForPersistentStorage();
-        if (appConfig.monitorExtension) {
-            const extensionInstalled = await getExtensionInstallationStatus();
-            // TODO if false and other conditions are met, show popup advertising browser extension
-            console.log(
-                `Cashtab browser extension: ${
-                    extensionInstalled ? 'Installed' : 'Not installed'
-                }`,
-            );
-        }
-    }, []);
-
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
             <ServiceWorkerWrapper />
+            {process.env.REACT_APP_BUILD_ENV !== 'extension' && <WebApp />}
             <Spin
                 spinning={
                     loading ||
---
+++ cashtab/src/components/AppModes/WebApp.js
@@ -0,0 +1,129 @@
+import { useEffect } from 'react';
+import { Modal } from 'antd';
+import appConfig from 'config/app';
+
+/**
+ * Functional logic specific to the webapp at cashtab.com
+ * i.e. stuff that should not be built with the browser extension
+ */
+const WebApp = () => {
+    /*
+    See https://web.dev/push-notifications-subscribing-a-user/
+    note that Safari still uses callback syntax, hence this method which handles both syntaxes
+    https://developer.mozilla.org/en-US/docs/Web/API/Notification/requestPermission
+    */
+    const askPermissionForNotifications = () => {
+        return new Promise(function (resolve, reject) {
+            const permissionResult = Notification.requestPermission(function (
+                result,
+            ) {
+                resolve(result);
+            });
+
+            if (permissionResult) {
+                permissionResult.then(resolve, reject);
+            }
+        }).then(function (permissionResult) {
+            console.log(`permissionResult`, permissionResult);
+            if (permissionResult !== 'granted') {
+                // Warning modal if user does not accept notifications
+                Modal.warn({
+                    title: 'Notification permission refused',
+                    content:
+                        'Please enable notifications from Cashtab.com to help prevent your browser from deleting Cashtab wallet info.',
+                });
+            } else {
+                console.log(`Notification permission granted`);
+                // If it is granted, ask again for persistent storage
+                // Best way to do this is to show a modal that refreshes the page
+
+                // Success modal when user accepts notifications, prompting a refresh
+                Modal.success({
+                    title: 'Notification permission granted',
+                    content: 'Please refresh the page',
+                    onOk: () => {
+                        console.log(
+                            `Reloading page to retry for persistent storage permission`,
+                        );
+                        window.location.reload(true);
+                    },
+                });
+            }
+        });
+    };
+
+    const checkForPersistentStorage = async () => {
+        // Request persistent storage for site
+        if (navigator.storage && navigator.storage.persist) {
+            // Check if storage is persistent
+            const isPersisted = await navigator.storage.persisted();
+            console.log(`Persisted storage status: ${isPersisted}`);
+            // If not, request persistent storage
+            if (!isPersisted) {
+                console.log(`Requesting persistent storage`);
+                const persistanceRequestResult =
+                    await navigator.storage.persist();
+                console.log(
+                    `Persistent storage granted: ${persistanceRequestResult}`,
+                );
+                // If the request was not granted, ask the user to approve notification permissions
+                if (!persistanceRequestResult) {
+                    // Check if the browser supports notifications
+                    const browserSupportsNotifications =
+                        'Notification' in window;
+
+                    console.log(
+                        `browserSupportsNotifications`,
+                        browserSupportsNotifications,
+                    );
+
+                    // If the browser supports notifications, check the permission status
+                    if (browserSupportsNotifications) {
+                        const cashtabNotificationPermission =
+                            Notification.permission;
+
+                        console.log(
+                            `cashtabNotificationPermission`,
+                            cashtabNotificationPermission,
+                        );
+
+                        // If the permission is not 'granted', ask for notification permissions
+                        if (cashtabNotificationPermission !== 'granted') {
+                            console.log(
+                                `User has not granted notification permission and persistent storage status has not been granted. Requesting notification permission.`,
+                            );
+                            return askPermissionForNotifications();
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    const getExtensionInstallationStatus = async () => {
+        // Wait 2s to check
+        // window.bitcoinAbc is set by the extension, and is undefined on page load
+        // We will also want to wait for some configurable interval anyway, so that the page
+        // does not load with a popup
+        const popupWaitInterval = 2000;
+        await new Promise(resolve => setTimeout(resolve, popupWaitInterval));
+        return window && window.bitcoinAbc && window.bitcoinAbc === 'cashtab';
+    };
+
+    useEffect(async () => {
+        checkForPersistentStorage();
+        if (appConfig.monitorExtension) {
+            const extensionInstalled = await getExtensionInstallationStatus();
+            // TODO if false and other conditions are met, show popup advertising browser extension
+            console.log(
+                `Cashtab browser extension: ${
+                    extensionInstalled ? 'Installed' : 'Not installed'
+                }`,
+            );
+        }
+    }, []);
+    // Note: none of the above functionality requires rendering, so return null
+    return null;
+};
+
+export default WebApp;
