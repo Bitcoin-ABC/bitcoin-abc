a712112bc1d662bf8c0bfc6db2cbe72e74d42b51
bytesofman <joey@bitcoinabc.org>
2024-01-03 20:42:19 -0800

    [Cashtab] Update webpack to support cjs
    Summary:
    Known issue with create-react-app. As more dependencies use these files, will need this support.
    
    Main issue now is that the latest chronik-client won't work, since it uses the latest version of axios, which uses cjs files.
    
    Cashtab will work with the latest version of chronik-client with this webpack config.
    
    I can wait and add them together so there is a test plan, but imo it makes sense to keep this as an isolated change.
    
    This is more or less a backport of (still unmerged) https://github.com/facebook/create-react-app/pull/12021
    
    Cashtab was initially a create-react-app app, but has been ejected and customized to support crypto dependencies.
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D15077

--- cashtab/config/webpack.config.js
+++ cashtab/config/webpack.config.js
@@ -354,7 +354,7 @@ module.exports = function (webpackEnv) {
                 shouldUseSourceMap && {
                     enforce: 'pre',
                     exclude: /@babel(?:\/|\\{1,2})runtime/,
-                    test: /\.(js|mjs|jsx|ts|tsx|css)$/,
+                    test: /\.(js|mjs|cjs|jsx|ts|tsx|css)$/,
                     loader: require.resolve('source-map-loader'),
                 },
                 {
@@ -415,7 +415,7 @@ module.exports = function (webpackEnv) {
                         // Process application JS with Babel.
                         // The preset includes JSX, Flow, TypeScript, and some ESnext features.
                         {
-                            test: /\.(js|mjs|jsx|ts|tsx)$/,
+                            test: /\.(js|mjs|cjs|jsx|ts|tsx)$/,
                             include: paths.appSrc,
                             loader: require.resolve('babel-loader'),
                             options: {
@@ -452,7 +452,7 @@ module.exports = function (webpackEnv) {
                         // Process any JS outside of the app with Babel.
                         // Unlike the application JS, we only compile the standard ES features.
                         {
-                            test: /\.(js|mjs)$/,
+                            test: /\.(js|mjs|cjs)$/,
                             exclude: /@babel(?:\/|\\{1,2})runtime/,
                             loader: require.resolve('babel-loader'),
                             options: {
@@ -572,7 +572,7 @@ module.exports = function (webpackEnv) {
                             // by webpacks internal loaders.
                             exclude: [
                                 /^$/,
-                                /\.(js|mjs|jsx|ts|tsx)$/,
+                                /\.(js|mjs|cjs|jsx|ts|tsx)$/,
                                 /\.html$/,
                                 /\.json$/,
                             ],
@@ -758,7 +758,7 @@ module.exports = function (webpackEnv) {
             !disableESLintPlugin &&
                 new ESLintPlugin({
                     // Plugin options
-                    extensions: ['js', 'mjs', 'jsx', 'ts', 'tsx'],
+                    extensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx'],
                     formatter: require.resolve(
                         'react-dev-utils/eslintFormatter',
                     ),
