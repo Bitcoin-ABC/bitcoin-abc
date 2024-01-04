ec79b1071271a62ae85a7c7f38e772604244cb4d
bytesofman <joey@bitcoinabc.org>
2024-01-03 21:01:18 -0800

    [Cashtab] Default to ABC chronik servers
    Summary: Cashtab should default to abc infra. Was on chronik.be.cash in aftermath of last reindex, as this server had hotfix first.
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D15078

--- cashtab/src/config/chronik.js
+++ cashtab/src/config/chronik.js
@@ -6,9 +6,9 @@
 
 export const chronik = {
     urls: [
-        'https://chronik.be.cash/xec',
         'https://chronik.fabien.cash',
         'https://chronik2.fabien.cash',
+        'https://chronik.be.cash/xec',
     ],
     txHistoryCount: 10,
     txHistoryPageSize: 25,
