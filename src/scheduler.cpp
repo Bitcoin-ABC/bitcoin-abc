bitcoin-ghost/bitcoin-core-ladder-script

scheduler: Capture ‘this’ explicitly in lambda
Without the changes, g++ will warn to compile under C++20:

scheduler.cpp:114:21: warning: implicit capture of ‘this’ via ‘[=]’ is deprecated in C++20 [-Wdeprecated]
  114 |     scheduleFromNow([=] { Repeat(*this, f, delta); }, delta);
      |                     ^
scheduler.cpp:114:21: note: add explicit ‘this’ or ‘*this’ capture

src/scheduler.cpp
@@ -111,7 +111,7 @@ static void Repeat(CScheduler& s, CScheduler::Function f, std::chrono::milliseco
 
 void CScheduler::scheduleEvery(CScheduler::Function f, std::chrono::milliseconds delta)
 {
-    scheduleFromNow([=] { Repeat(*this, f, delta); }, delta);
+    scheduleFromNow([this, f, delta] { Repeat(*this, f, delta); }, delta);
 }
 
 size_t CScheduler::getQueueInfo(std::chrono::system_clock::time_point& first,

Bitcoin-ABC/bitcoin-abc

Summary:
```
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/24169 | core#24169]]
https://github.com/bitcoin/bitcoin/pull/24169/commits/fae2220f4e48934313389864d3d362f672627eb8
Depends on D15314
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Subscribers: Fabien
Differential Revision: https://reviews.bitcoinabc.org/D15315

src/scheduler.cpp
@@ -113,7 +113,7 @@ static void Repeat(CScheduler &s, CScheduler::Predicate p,
 
 void CScheduler::scheduleEvery(CScheduler::Predicate p,
                                std::chrono::milliseconds delta) {
-    scheduleFromNow([=] { Repeat(*this, p, delta); }, delta);
+    scheduleFromNow([this, p, delta] { Repeat(*this, p, delta); }, delta);
 }
 
 size_t