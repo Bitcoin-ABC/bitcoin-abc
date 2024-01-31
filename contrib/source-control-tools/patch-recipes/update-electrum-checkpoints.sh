e02f61a5fc6598e1346b20bbd5b59e17b75d9af3
PiRK <pierrekn@gmail.com>
2024-01-31 15:34:17 +0100

    [electrum] add a script to automate updating of checkpoints
    Summary:
    Add a patch recipe to update electrum checkpoints, similar to the script automating chainparams or seeds.
    
    The `get_merkle_root` script  called without params will fetch a checkpoint 50 blocks before the current tip of the specified electrum server.
    
    Test Plan:
    Check that this creates the expected commit
    
    ```
    cd ./contrib/source-control-tools
    ./patch-recipes/update-electrum-checkpoints.sh
    ```
    
    Run Electrum ABC, check that it connect succesfully to all 3 servers (tip >= 50 + checkpoint_height)
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D15343

---
+++ contrib/source-control-tools/patch-recipes/update-electrum-checkpoints.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+export LC_ALL=C.UTF-8
+
+set -euxo pipefail
+
+TOPLEVEL=$(git rev-parse --show-toplevel)
+ELECTRUM_DIR="${TOPLEVEL}"/electrum
+CHECKPOINTS_SCRIPT="${ELECTRUM_DIR}"/scripts/get_merkle_root
+CHECKPOINT_JSON_FILE_MAINNET="${ELECTRUM_DIR}"/electrumabc/checkpoint.json
+CHECKPOINT_JSON_FILE_TESTNET="${ELECTRUM_DIR}"/electrumabc/checkpoint_testnet.json
+
+"${CHECKPOINTS_SCRIPT}" --json-output "${CHECKPOINT_JSON_FILE_MAINNET}" --server electrum.bitcoinabc.org:50002:s
+"${CHECKPOINTS_SCRIPT}" --json-output "${CHECKPOINT_JSON_FILE_TESTNET}" --server telectrum.bitcoinabc.org:60002:s
+
+git add "${CHECKPOINT_JSON_FILE_MAINNET}" "${CHECKPOINT_JSON_FILE_TESTNET}"
+
+git commit -m "[Automated] Update electrum checkpoints"
