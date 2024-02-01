780946b29ce12d65532e36b623a407f6c14d8a22
PiRK <pierrekn@gmail.com>
2024-02-01 11:27:29 +0100

    [electrum] add an option to show more verbose output for get_merkle_root
    Summary: Add another verbosity level that enables all logs from the electrumabc package. This might help debugging socket issues when running the script on a CI server.
    
    Test Plan:
    ```
    electrum/scripts/get_merkle_root
    electrum/scripts/get_merkle_root -v
    electrum/scripts/get_merkle_root -vv
    electrum/scripts/get_merkle_root -v -v
    
    contrib/source-control-tools/patch-recipes/update-electrum-checkpoints.sh
    ```
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D15356

--- contrib/source-control-tools/patch-recipes/update-electrum-checkpoints.sh
+++ contrib/source-control-tools/patch-recipes/update-electrum-checkpoints.sh
@@ -9,8 +9,8 @@ CHECKPOINTS_SCRIPT="${ELECTRUM_DIR}"/scripts/get_merkle_root
 CHECKPOINT_JSON_FILE_MAINNET="${ELECTRUM_DIR}"/electrumabc/checkpoint.json
 CHECKPOINT_JSON_FILE_TESTNET="${ELECTRUM_DIR}"/electrumabc/checkpoint_testnet.json
 
-"${CHECKPOINTS_SCRIPT}" --json-output "${CHECKPOINT_JSON_FILE_MAINNET}" --server electrum.bitcoinabc.org:50002:s
-"${CHECKPOINTS_SCRIPT}" --json-output "${CHECKPOINT_JSON_FILE_TESTNET}" --server telectrum.bitcoinabc.org:60002:s
+"${CHECKPOINTS_SCRIPT}" -vv --json-output "${CHECKPOINT_JSON_FILE_MAINNET}" --server electrum.bitcoinabc.org:50002:s
+"${CHECKPOINTS_SCRIPT}" -vv --json-output "${CHECKPOINT_JSON_FILE_TESTNET}" --server telectrum.bitcoinabc.org:60002:s
 
 git add "${CHECKPOINT_JSON_FILE_MAINNET}" "${CHECKPOINT_JSON_FILE_TESTNET}"
 
