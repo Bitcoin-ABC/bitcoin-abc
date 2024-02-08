65cf0ff4c5e36305eb10e44c65d77c9c5e03ec65
bytesofman <joey@bitcoinabc.org>
2024-02-08 06:26:25 -0800

    [Cashtab] Upgrade react-ga implementation
    Summary:
    The current implementation is based off legacy react (componentDidMount etc) and also uses legacy routing. Will not work with react-router-dom v6.
    
    Simplify and use latest version of react-ga.
    
    Test Plan:
    Check dev console at https://cashtab-local-dev.netlify.app/#/wallet
    
    I've added logging statements to this deployment
    
    Confirm you see `GA initialized` on load and `logging...` as routes change.
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D15408

--- cashtab/src/utils/GoogleAnalytics.js
+++ cashtab/src/utils/GoogleAnalytics.js
@@ -1,57 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 let ReactGA;
 if (process.env.REACT_APP_BUILD_ENV !== 'extension') {
     ReactGA = require('react-ga');
 }
 
-class GoogleAnalytics extends Component {
-    componentDidMount() {
-        this.logPageChange(
-            this.props.location.pathname,
-            this.props.location.search,
-        );
-    }
-
-    componentDidUpdate({ location: prevLocation }) {
-        const {
-            location: { pathname, search },
-        } = this.props;
-        const isDifferentPathname = pathname !== prevLocation.pathname;
-        const isDifferentSearch = search !== prevLocation.search;
-
-        if (isDifferentPathname || isDifferentSearch) {
-            this.logPageChange(pathname, search);
-        }
-    }
-
-    logPageChange(pathname, search = '') {
-        const page = pathname + search;
-        const { location } = window;
-        ReactGA.set({
-            page,
-            location: `${location.origin}${page}`,
-            ...this.props.options,
-        });
-        ReactGA.pageview(page);
-    }
-
-    render() {
-        return null;
-    }
-}
-
-GoogleAnalytics.propTypes = {
-    location: PropTypes.shape({
-        pathname: PropTypes.string,
-        search: PropTypes.string,
-    }).isRequired,
-    options: PropTypes.object,
+const RouteTracker = () => {
+    const location = useLocation();
+    useEffect(() => {
+        ReactGA.pageview(location.pathname + location.search);
+    }, [location]);
 };
-
-const RouteTracker = () => <Route component={GoogleAnalytics} />;
 
 const init =
     process.env.REACT_APP_BUILD_ENV !== 'extension'
@@ -84,15 +44,12 @@ export const Event =
           // So, more practical to just define it as a do-nothing function for the extension
           () => {};
 
-const GoogleAnalyticsDefault =
-    process.env.REACT_APP_BUILD_ENV !== 'extension'
-        ? {
-              RouteTracker,
-              init,
-          }
-        : {
-              RouteTracker: () => <Route />,
-              init,
-          };
-
-export default GoogleAnalyticsDefault;
+export default process.env.REACT_APP_BUILD_ENV !== 'extension'
+    ? {
+          RouteTracker,
+          init,
+      }
+    : {
+          RouteTracker: () => {},
+          init,
+      };
