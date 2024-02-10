7bfc4893ba36b280959c867f2bdbb68ba051f855
bytesofman <joey@bitcoinabc.org>
2024-02-10 06:20:46 -0800

    [Cashtab] Move jest settings to own config file
    Summary: It's not obvious these are in the package.json file. Move them to their own file.
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D15416

---
+++ cashtab/jest.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+    coverageDirectory: '<rootDir>/coverage',
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0,
+        },
+    },
+    setupFiles: ['react-app-polyfill/jsdom'],
+    testTimeout: 10000,
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+        '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    ],
+    testEnvironment: './config/jest/uint8array-environment',
+    transform: {
+        '\\.[jt]sx?$': 'babel-jest',
+    },
+    transformIgnorePatterns: [
+        '/node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime).+(js|jsx)$',
+        '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+        '^.+\\.module\\.(css|sass|scss)$',
+    ],
+    modulePaths: ['src'],
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/config/jest/__mocks__/fileMock.js',
+        '\\.svg': '<rootDir>/config/jest/__mocks__/svg.js',
+        '\\.(css|less)$': 'identity-obj-proxy',
+    },
+    moduleFileExtensions: [
+        'web.js',
+        'js',
+        'web.ts',
+        'ts',
+        'web.tsx',
+        'tsx',
+        'json',
+        'web.jsx',
+        'jsx',
+        'node',
+    ],
+    watchPlugins: [
+        'jest-watch-typeahead/filename',
+        'jest-watch-typeahead/testname',
+    ],
+    resetMocks: true,
+};
