b0e0130e5ac9610e81fcc7546328a923d7a384bc
bytesofman <joey@bitcoinabc.org>
2024-02-24 15:33:59 -0800

    [Cashtab] Delete all functions, files, and mocks related to legacy tx building
    Summary:
    Cashtab now creates all txs using ecash-coinselect and unit-tested targetOutput building functions.
    
    The legacy functions were tied together in many unit tests making it impractical to delete them in line with progress on the upgraded methods.
    
    Now, though, they aren't used anywhere.
    
    Test Plan:
    `npm test`
    `arc lint` confirms these deleted functions aren't called anywhere else, plus deleted mocks not used anywhere else
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D15540

--- cashtab/src/utils/__mocks__/mockAddressArray.js
+++
@@ -1,8 +0,0 @@
-export const validAddressArrayInput = [
-    'ecash:qrqgwxnaxlfagezvr2zj4s9yee6rrs96dyguh7zsvk,7',
-    'ecash:qzsha6zk9m0f3hlfe5q007zdwnzvn3vwuuzel2lfzv,67',
-    'ecash:qqlkyzmeupf7q8t2ttf2u8xgyk286pg4wyz0v403dj,4376',
-    'ecash:qz2taa43tljkvnvkeqv9pyx337hmg0zclqfqjrqst4,673728',
-    'ecash:qz2taa43tljkvnvkeqv9pyx337hmg0zclqfqjrqst4,368',
-    'ecash:qp0hlj26nwjpk9c3f0umjz7qmwpzfh0fhckq4zj9s6,23673',
-];
