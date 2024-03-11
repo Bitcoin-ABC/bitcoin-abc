b5497111c1205c44bd8a9e3520629141765d2ff9
Fabien <fabcien@gmail.com>
2024-03-11 14:50:20 +0100

    [CI] Add CI configuration for the GUIX builds
    Summary: This will make it possible to run the builds from the CI.
    
    Test Plan:
      ./contrib/teamcity/build-configurations.py guix-linux
      ./contrib/teamcity/build-configurations.py guix-osx
      ./contrib/teamcity/build-configurations.py guix-win
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Subscribers: bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D15674

---
+++ contrib/teamcity/guix.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+export LC_ALL=C
+
+set -euxo pipefail
+
+: "${TOPLEVEL:=$(git rev-parse --show-toplevel)}"
+: "${BUILD_DIR:=${TOPLEVEL}/build-guix}"
+
+cd "${TOPLEVEL}"
+
+# Cleanup but exclude our own build dir
+GUIX_CLEAN_EXTRA_EXCLUDES=("--exclude=${BUILD_DIR}") ./contrib/guix/guix-clean
+
+if [[ "${HOSTS[*]}" =~ .*darwin.* ]]; then
+  OSX_SDK_DIR=~/.abc-build-cache/osx-sdk
+  mkdir -p "${OSX_SDK_DIR}"
+
+  OSX_SDK=$("${TOPLEVEL}/contrib/teamcity/download-apple-sdk.sh" "${OSX_SDK_DIR}")
+
+  # guix-clean doesn't remove the SDK (as expected), so do it here to make sure
+  # we don't keep the old versions forever. The cost of untarring the archive is
+  # low enough that we can do it unconditionally.
+  rm -rf depends/SDKs
+  mkdir -p depends/SDKs
+  tar -C depends/SDKs/ -xzf "${OSX_SDK_DIR}/${OSX_SDK}"
+fi
+
+OUTDIR_BASE="${BUILD_DIR}/guix-results" ./contrib/guix/guix-build
