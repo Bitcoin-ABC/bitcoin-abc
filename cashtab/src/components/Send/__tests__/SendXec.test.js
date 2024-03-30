Bitcoin-ABC/bitcoin-abc

commit cbace441a736f70515a6a38fd203682385d4f4ce

[Cashtab] Implement custom Input elements in Send, SendToken, and Configure
Summary:
Replace legacy antd input elements with new custom components

Other improvements were necessary to bring in better Input fields and deprecate other antd components. For example, we now use our custom modal for scanning QR codes, formdata is cleared up in SendXec and SendToken, the token info table is cleaned up in SendToken.

Still some inputs left to clean up in SignVerifyMsg. But, will need to handle the Collapse antd before we really clean that up anway. Also the CreateTokenForm. imo this diff is big enough already and those improvements will be handled in other diffs.

Test Plan:
`npm test`

This diff is deployed at https://cashtab-local-dev.netlify.app/#/wallet

Confirm the QR scanner still works on SendXec and SendToken pages. Confirm Configure modals look ok. Confirm Send and SendToken screens look desktop and mobile.

Reviewers: #bitcoin_abc, emack

Reviewed By: #bitcoin_abc, emack

Subscribers: emack

Differential Revision: https://reviews.bitcoinabc.org/D15776

cashtab/src/validation/fixtures/vectors.js
@@ -17,13 +17,13 @@ const cloneObjectWithDeletedKey = (object, key) => {
 };
 
 export default {
-    shouldDisableXecSend: {
+    shouldSendXecBeDisabled: {
         expectedReturns: [
             {
                 description: 'Disabled on startup',
                 formData: {
                     address: '',
-                    value: '',
+                    amount: '',
                 },
                 balanceSats: 10000,
                 apiError: false,
@@ -39,7 +39,7 @@ export default {
                     'Disabled if address has been entered but no value',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
-                    value: '',
+                    amount: '',
                 },
                 balanceSats: 10000,
                 apiError: false,
@@ -54,7 +54,7 @@ export default {
                 description: 'Enabled for valid address and value',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
-                    value: '50',
+                    amount: '50',
                 },
                 balanceSats: 10000,
                 apiError: false,
@@ -69,7 +69,7 @@ export default {
                 description: 'Disabled on zero balance',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
-                    value: '50',
+                    amount: '50',
                 },
                 balanceSats: 0,
                 apiError: false,
@@ -84,7 +84,7 @@ export default {
                 description: 'Disabled for invalid address',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg',
-                    value: '50',
+                    amount: '50',
                 },
                 balanceSats: 10000,
                 apiError: false,
@@ -100,7 +100,7 @@ export default {
                 description: 'Disabled for invalid value',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
-                    value: '5',
+                    amount: '5',
                 },
                 balanceSats: 10000,
                 apiError: false,
@@ -116,7 +116,7 @@ export default {
                 description: 'Disabled for invalid opreturn msg',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
-                    value: '5',
+                    amount: '5',
                 },
                 balanceSats: 10000,
                 apiError: false,
@@ -131,7 +131,7 @@ export default {
                 description: 'Disabled on priceApi error',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
-                    value: '5',
+                    amount: '5',
                 },
                 balanceSats: 10000,
                 apiError: false,
@@ -148,7 +148,7 @@ export default {
                 formData: {
                     address:
                         'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22\necash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22',
-                    value: '',
+                    amount: '',
                 },
                 balanceSats: 10000,
                 apiError: false,

cashtab/src/validation/index.js
@@ -170,9 +170,9 @@ export const isValidXecSendAmount = (
         return 'Amount must be greater than 0';
     }
     if (sendAmountSatoshis < appConfig.dustSats) {
-        return `Send amount must be at least ${toXec(
-            appConfig.dustSats,
-        ).toString()} ${appConfig.ticker}`;
+        return `Send amount must be at least ${toXec(appConfig.dustSats)} ${
+            appConfig.ticker
+        }`;
     }
     if (sendAmountSatoshis > balanceSats) {
         return `Amount ${toXec(sendAmountSatoshis).toLocaleString(userLocale, {
@@ -550,7 +550,7 @@ export const shouldSendXecBeDisabled = (
     isOneToManyXECSend,
 ) => {
     return (
-        (formData.value === '' && formData.address === '') || // No user inputs
+        (formData.amount === '' && formData.address === '') || // No user inputs
         balanceSats === 0 || // user has no funds
         apiError || // API error
         typeof sendAmountError === 'string' || // validation error for send amount
@@ -558,7 +558,7 @@ export const shouldSendXecBeDisabled = (
         typeof isMsgError === 'string' || // validation error in Cashtab Msg
         priceApiError || // we don't have a good price AND fiat currency is selected
         (!isOneToManyXECSend &&
-            (isNaN(formData.value) || formData.value === ''))
+            (isNaN(formData.amount) || formData.amount === ''))
     ); // Value is blank or NaN and is expected to not be so
 };
 

cashtab/src/components/Send/__tests__/SendXec.test.js
@@ -1408,11 +1408,14 @@ describe('<SendXec />', () => {
         await user.type(addressInputEl, addressInput);
 
         // Select USD from currency select
-        await user.click(
-            screen.getByTestId('currency-select-dropdown').firstElementChild,
+        const currencyDropdownMenu = screen.getByTestId(
+            'currency-select-dropdown',
         );
-        await user.click(screen.getAllByTestId('currency-select-option')[1]);
-
+        await user.selectOptions(
+            screen.getByTestId('currency-select-dropdown'),
+            screen.getByTestId('fiat-option'),
+        );
+        await waitFor(() => expect(currencyDropdownMenu).toHaveValue('USD'));
         // Send $0.21
         // 7000 satoshis at 0.00003 USD / XEC
         await user.type(amountInputEl, '0.21');

cashtab/src/validation/__tests__/index.test.js
@@ -466,7 +466,7 @@ describe('Cashtab validation functions', () => {
         expect(isValidOpreturnParam('042e7')).toBe(false);
     });
     describe('Determining whether Send button should be disabled on SendXec screen', () => {
-        const { expectedReturns } = vectors.shouldDisableXecSend;
+        const { expectedReturns } = vectors.shouldSendXecBeDisabled;
 
         // Successfully created targetOutputs
         expectedReturns.forEach(expectedReturn => {

commit 9f69b3db096e990e2551c25391a6d766abecb2cb

[Cashtab] Patch send to many with improved validation
Summary:
Broke send to many in D15776, as the send button will be disabled even if user input is correct.

Update validation function for disabling send button. Add an integration test for multisend to prevent this kind of oversight in the future.

Test Plan: `npm test`

Reviewers: #bitcoin_abc, emack

Reviewed By: #bitcoin_abc, emack

Subscribers: emack

Differential Revision: https://reviews.bitcoinabc.org/D15816

cashtab/src/components/Send/SendXec.js
@@ -777,10 +777,12 @@ const SendXec = () => {
         apiError,
         sendAmountError,
         sendAddressError,
+        multiSendAddressError,
         isMsgError,
         priceApiError,
         isOneToManyXECSend,
     );
+
     return (
         <>
             {isModalVisible && (
@@ -902,7 +904,7 @@ const SendXec = () => {
                     <>
                         <DestinationAddressMultiCtn>
                             <TextArea
-                                placeholder={`One address & value per line, separated by comma \ne.g. \necash:qpatql05s9jfavnu0tv6lkjjk25n6tmj9gkpyrlwu8,500 \necash:qzvydd4n3lm3xv62cx078nu9rg0e3srmqq0knykfed,700`}
+                                placeholder={`One address & amount per line, separated by comma \ne.g. \necash:qpatql05s9jfavnu0tv6lkjjk25n6tmj9gkpyrlwu8,500 \necash:qzvydd4n3lm3xv62cx078nu9rg0e3srmqq0knykfed,700`}
                                 name="multiAddressInput"
                                 handleInput={e => handleMultiAddressChange(e)}
                                 value={formData.multiAddressInput}

cashtab/src/validation/fixtures/vectors.js
@@ -36,8 +36,9 @@ export default {
                 },
                 balanceSats: 10000,
                 apiError: false,
-                sendBchAmountError: false,
-                sendBchAddressError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
                 isMsgError: false,
                 priceApiError: false,
                 isOneToManyXECSend: false,
@@ -45,48 +46,90 @@ export default {
             },
             {
                 description:
-                    'Disabled if address has been entered but no value',
+                    'Disabled if address has been entered but no amount',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
                     amount: '',
+                    multiAddressInput: '',
                 },
                 balanceSats: 10000,
                 apiError: false,
-                sendBchAmountError: false,
-                sendBchAddressError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
                 isMsgError: false,
                 priceApiError: false,
                 isOneToManyXECSend: false,
                 sendDisabled: true,
             },
             {
-                description: 'Enabled for valid address and value',
+                description:
+                    'Disabled if amount has been entered but no address',
                 formData: {
-                    address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
+                    address: '',
                     amount: '50',
+                    multiAddressInput: '',
                 },
                 balanceSats: 10000,
                 apiError: false,
-                sendBchAmountError: false,
-                sendBchAddressError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
+                isMsgError: false,
+                priceApiError: false,
+                isOneToManyXECSend: false,
+                sendDisabled: true,
+            },
+            {
+                description: 'Enabled for valid address and amount',
+                formData: {
+                    address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
+                    amount: '50',
+                    multiAddressInput: '',
+                },
+                balanceSats: 10000,
+                apiError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
                 isMsgError: false,
                 priceApiError: false,
                 isOneToManyXECSend: false,
                 sendDisabled: false,
             },
             {
-                description: 'Disabled on zero balance',
+                description: 'Disabled on zero balance for send to one',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
                     amount: '50',
+                    multiAddressInput: '',
                 },
                 balanceSats: 0,
                 apiError: false,
-                sendBchAmountError: false,
-                sendBchAddressError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
                 isMsgError: false,
                 priceApiError: false,
                 isOneToManyXECSend: false,
+                sendDisabled: true,
+            },
+            {
+                description: 'Disabled on zero balance for send to many',
+                formData: {
+                    address: '',
+                    amount: '',
+                    multiAddressInput:
+                        'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22\necash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22',
+                },
+                balanceSats: 0,
+                apiError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
+                isMsgError: false,
+                priceApiError: false,
+                isOneToManyXECSend: true,
                 sendDisabled: true,
             },
             {
@@ -94,43 +137,65 @@ export default {
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg',
                     amount: '50',
+                    multiAddressInput: '',
                 },
                 balanceSats: 10000,
                 apiError: false,
-                sendBchAmountError: false,
-                sendBchAddressError:
-                    'a string indicating a validation error msg',
+                sendAmountError: false,
+                sendAddressError: 'a string indicating a validation error msg',
+                multiSendAddressError: false,
                 isMsgError: false,
                 priceApiError: false,
                 isOneToManyXECSend: false,
                 sendDisabled: true,
             },
             {
-                description: 'Disabled for invalid value',
+                description: 'Disabled for invalid amount',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
                     amount: '5',
+                    multiAddressInput: '',
                 },
                 balanceSats: 10000,
                 apiError: false,
-                sendBchAmountError:
-                    'a string indicating a validation error msg',
-                sendBchAddressError: false,
+                sendAmountError: 'a string indicating a validation error msg',
+                sendAddressError: false,
+                multiSendAddressError: false,
                 isMsgError: false,
                 priceApiError: false,
                 isOneToManyXECSend: false,
                 sendDisabled: true,
             },
             {
-                description: 'Disabled for invalid opreturn msg',
+                description: 'Disabled for invalid opreturn msg send to one',
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
                     amount: '5',
+                    multiAddressInput: '',
                 },
                 balanceSats: 10000,
                 apiError: false,
-                sendBchAmountError: false,
-                sendBchAddressError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
+                isMsgError: 'a string indicating a validation error msg',
+                priceApiError: false,
+                isOneToManyXECSend: false,
+                sendDisabled: true,
+            },
+            {
+                description: 'Disabled for invalid opreturn msg send to many',
+                formData: {
+                    address: '',
+                    amount: '',
+                    multiAddressInput:
+                        'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22\necash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22',
+                },
+                balanceSats: 10000,
+                apiError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
                 isMsgError: 'a string indicating a validation error msg',
                 priceApiError: false,
                 isOneToManyXECSend: false,
@@ -141,11 +206,13 @@ export default {
                 formData: {
                     address: 'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6',
                     amount: '5',
+                    multiAddressInput: '',
                 },
                 balanceSats: 10000,
                 apiError: false,
-                sendBchAmountError: false,
-                sendBchAddressError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
                 isMsgError: false,
                 priceApiError: true,
                 isOneToManyXECSend: false,
@@ -153,20 +220,42 @@ export default {
             },
             {
                 description:
-                    'Enabled if isOneToManyXECSend and value is not entered',
+                    'Enabled if isOneToManyXECSend and amount is not entered',
                 formData: {
-                    address:
-                        'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22\necash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22',
+                    address: '',
                     amount: '',
+                    multiAddressInput:
+                        'ecash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22\necash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22',
                 },
                 balanceSats: 10000,
                 apiError: false,
-                sendBchAmountError: false,
-                sendBchAddressError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: false,
                 isMsgError: false,
                 priceApiError: false,
                 isOneToManyXECSend: true,
                 sendDisabled: false,
+            },
+            {
+                description:
+                    'Disabled if isOneToManyXECSend and we have a multiSendAddressError',
+                formData: {
+                    address: '',
+                    amount: '',
+                    multiAddressInput:
+                        // bad input
+                        'eash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22\necash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22',
+                },
+                balanceSats: 10000,
+                apiError: false,
+                sendAmountError: false,
+                sendAddressError: false,
+                multiSendAddressError: true,
+                isMsgError: false,
+                priceApiError: false,
+                isOneToManyXECSend: true,
+                sendDisabled: true,
             },
         ],
     },

cashtab/src/validation/index.js
@@ -568,21 +568,34 @@ export const shouldSendXecBeDisabled = (
     apiError,
     sendAmountError,
     sendAddressError,
+    multiSendAddressError,
     isMsgError,
     priceApiError,
     isOneToManyXECSend,
 ) => {
     return (
-        (formData.amount === '' && formData.address === '') || // No user inputs
-        balanceSats === 0 || // user has no funds
-        apiError || // API error
-        typeof sendAmountError === 'string' || // validation error for send amount
-        typeof sendAddressError === 'string' || // validation error for destinationa ddress
-        typeof isMsgError === 'string' || // validation error in Cashtab Msg
-        priceApiError || // we don't have a good price AND fiat currency is selected
+        // Disabled if no user inputs
+        (formData.multiAddressInput === '' &&
+            formData.amount === '' &&
+            formData.address === '') ||
+        // Disabled if we are on SendToOne mode and address or amount is blank
         (!isOneToManyXECSend &&
-            (isNaN(formData.amount) || formData.amount === ''))
-    ); // Value is blank or NaN and is expected to not be so
+            (formData.amount === '' || formData.address === '')) ||
+        // Disabled if user has no balance
+        balanceSats === 0 ||
+        // Disabled if apiError (wallet unable to sync utxo set with chronik)
+        apiError ||
+        // Disabled if send amount fails validation
+        sendAmountError !== false ||
+        // Disabled if address fails validation
+        sendAddressError !== false ||
+        // Disabled if msg fails validation
+        isMsgError !== false ||
+        // Disabled if we do not have a fiat price AND the user is attempting to send fiat
+        priceApiError ||
+        // Disabled if send to many and we have a send to many validation error
+        (isOneToManyXECSend && multiSendAddressError !== false)
+    );
 };
 
 /**

cashtab/src/components/Send/__tests__/SendXec.test.js
@@ -1473,4 +1473,59 @@ describe('<SendXec />', () => {
             ),
         );
     });
+    it('We can send an XEC tx to multiple users', async () => {
+        // Mock the app with context at the Send screen
+        const mockedChronik = await initializeCashtabStateForTests(
+            walletWithXecAndTokens,
+            localforage,
+        );
+
+        // Can check in electrum for opreturn and amount
+        const hex =
+            '0200000001fe667fba52a1aa603a892126e492717eed3dad43bfea7365a7fdd08e051e8a21020000006b4830450221009ce5b0f6332ed7d1dbfdcf6a0ab870a69d97690b47463c09e403c25255df8d2d02206c553baba4386eb73b7938cb041da23c69072bb1232101fd2f3c8d2b1b6367b34121031d4603bdc23aca9432f903e3cf5975a3f655cc3fa5057c61d00dfc1ca5dfd02dffffffff03d0070000000000001976a91495e79f51d4260bc0dc3ba7fb77c7be92d0fbdd1d88ac98080000000000001976a9144e532257c01b310b3b5c1fd947c79a72addf852388ac9d710e00000000001976a9143a5fb236934ec078b4507c303d3afd82067f8fc188ac00000000';
+        const txid =
+            'cf665d10e1a0baa8f4c593113678f7859078b1a9a441ac4d6fa40d3636127b99';
+        mockedChronik.setMock('broadcastTx', {
+            input: hex,
+            output: { txid },
+        });
+
+        render(<CashtabTestWrapper chronik={mockedChronik} route="/send" />);
+
+        // Wait for the app to load
+        await waitFor(() =>
+            expect(screen.queryByTestId('loading-ctn')).not.toBeInTheDocument(),
+        );
+
+        // Select multi-send mode
+        await user.click(screen.getByTestId('Send to many'));
+
+        const multiSendInputEl = screen.getByPlaceholderText(
+            /One address & amount per line/,
+        );
+        // The user enters a send to many input
+        const multiSendInput =
+            'ecash:qz2708636snqhsxu8wnlka78h6fdp77ar59jrf5035, 20\necash:qp89xgjhcqdnzzemts0aj378nfe2mhu9yvxj9nhgg6, 22';
+        await user.type(multiSendInputEl, multiSendInput);
+
+        // The send to many input field has this value
+        expect(multiSendInputEl).toHaveValue(multiSendInput);
+
+        // The Send button is enabled as we have valid multisend input
+        expect(screen.getByRole('button', { name: /Send/ })).not.toHaveStyle(
+            'cursor: not-allowed',
+        );
+
+        // Click Send
+        await user.click(screen.getByRole('button', { name: /Send/ }));
+
+        // Notification is rendered with expected txid?;
+        const txSuccessNotification = await screen.findByText('eCash sent');
+        await waitFor(() =>
+            expect(txSuccessNotification).toHaveAttribute(
+                'href',
+                `${explorer.blockExplorerUrl}/tx/${txid}`,
+            ),
+        );
+    });
 });

cashtab/src/validation/__tests__/index.test.js
@@ -475,8 +475,9 @@ describe('Cashtab validation functions', () => {
                 formData,
                 balanceSats,
                 apiError,
-                sendBchAmountError,
-                sendBchAddressError,
+                sendAmountError,
+                sendAddressError,
+                multiSendAddressError,
                 isMsgError,
                 priceApiError,
                 isOneToManyXECSend,
@@ -488,8 +489,9 @@ describe('Cashtab validation functions', () => {
                         formData,
                         balanceSats,
                         apiError,
-                        sendBchAmountError,
-                        sendBchAddressError,
+                        sendAmountError,
+                        sendAddressError,
+                        multiSendAddressError,
                         isMsgError,
                         priceApiError,
                         isOneToManyXECSend,