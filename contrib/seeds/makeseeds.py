5a6c1d2cb91cd25db919842a5635dca66b9be043
Fabien <fabcien@gmail.com>
2024-04-02 15:11:38 +0200

    Bump the minimum block count for makeseeds.py
    Summary: The blockchain has grown since the last update, this will filter out old ABC versions without using the regex.
    
    Test Plan: There is no easy way to test the behavior without a full seeder infra, but this is trivial enough.
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D15854

--- contrib/seeds/makeseeds.py
+++ contrib/seeds/makeseeds.py
@@ -20,7 +20,7 @@ MAX_SEEDS_PER_ASN = {
     "ipv6": 10,
 }
 
-MIN_BLOCKS = 760000
+MIN_BLOCKS = 830000
 
 PATTERN_IPV4 = re.compile(r"^((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})):(\d+)$")
 PATTERN_IPV6 = re.compile(r"^\[([0-9a-z:]+)\]:(\d+)$")
