Bitcoin-ABC/bitcoin-abc

commit 02b4774d7c2400b7e5a6f63f2e1ed2d01192c33e

[net] Consistently check for existing connection based on ip:port
Summary: The current implementation of AlreadyConnectedToAddress uses 2 methods to determine if the node is already connected to the candidate. First it searches by ip address (CNetAddr) then by ip:port (ToStringIPPort()). The first one is inconsistent with the other places in the codebase, and inconsistent with how addrman stores the peers data (based on CService, which is an ip:port object). This piece of code will prevent the node from connecting several nodes sharing the same ip but with a different port via outbound connections, which is not really a problem for core as it has a limited number of outbounds, but becomes a problem for eCash with avalanche where we should be able to connect all the peers.

Test Plan:
  ninja all check-all

Reviewers: #bitcoin_abc, PiRK

Reviewed By: #bitcoin_abc, PiRK

Differential Revision: https://reviews.bitcoinabc.org/D15850

src/net.cpp
@@ -388,7 +388,7 @@ CNode *CConnman::FindNode(const CService &addr) {
 }
 
 bool CConnman::AlreadyConnectedToAddress(const CAddress &addr) {
-    return FindNode(static_cast<CNetAddr>(addr)) ||
+    return FindNode(static_cast<CService>(addr)) ||
            FindNode(addr.ToStringIPPort());
 }
 

src/test/net_tests.cpp
@@ -233,6 +233,10 @@ struct CConnmanTest : public CConnman {
 
         return ret;
     }
+
+    bool AlreadyConnectedToAddress(const CAddress &addr) {
+        return CConnman::AlreadyConnectedToAddress(addr);
+    }
 };
 } // namespace
 
@@ -1369,6 +1373,32 @@ BOOST_AUTO_TEST_CASE(initial_advertise_from_version_message) {
     // "timedata_tests/addtimedata". Thus reset that state as it was before our
     // test was run.
     TestOnlyResetTimeData();
+}
+
+BOOST_AUTO_TEST_CASE(already_connected_to_address) {
+    CConnmanTest connman(GetConfig(), 0x1337, 0x1337, *m_node.addrman);
+
+    CNetAddr ip1 = ip(GetRand<uint32_t>());
+    CNetAddr ip2 = ip(GetRand<uint32_t>());
+    BOOST_CHECK_NE(ip1.ToStringIP(), ip2.ToStringIP());
+
+    CAddress ip1port1{{ip1, 2001}, NODE_NETWORK};
+    CAddress ip1port2{{ip1, 2002}, NODE_NETWORK};
+    CAddress ip2port1{{ip2, 2001}, NODE_NETWORK};
+
+    BOOST_CHECK(!connman.AlreadyConnectedToAddress(ip1port1));
+    connman.AddNode(ip1port1, ConnectionType::OUTBOUND_FULL_RELAY);
+    BOOST_CHECK(connman.AlreadyConnectedToAddress(ip1port1));
+
+    // Different IP, same port
+    BOOST_CHECK(!connman.AlreadyConnectedToAddress(ip2port1));
+    connman.AddNode(ip2port1, ConnectionType::OUTBOUND_FULL_RELAY);
+    BOOST_CHECK(connman.AlreadyConnectedToAddress(ip2port1));
+
+    // Same IP, different port
+    BOOST_CHECK(!connman.AlreadyConnectedToAddress(ip1port2));
+    connman.AddNode(ip1port2, ConnectionType::OUTBOUND_FULL_RELAY);
+    BOOST_CHECK(connman.AlreadyConnectedToAddress(ip1port2));
 }
 
 BOOST_AUTO_TEST_SUITE_END()

commit 233e2d7f0423e9ca7eba0f700e96e76a29d6668b

[net] Don't create outbound connection to the same ip as an inbound
Summary:
This fixes a bug that has been introduced in D15850. The AlreadyConnectedToAddress() function needs to filter on IP only to prevent the node from initiating an outbound connection to a peer that is already connected as an inbound.

This is a partial revert of rABC02b4774d7c2400b7e5a6f63f2e1ed2d01192c33e.

Depends on D15898.

Test Plan:
  ninja all check-all

Reviewers: #bitcoin_abc, PiRK

Reviewed By: #bitcoin_abc, PiRK

Differential Revision: https://reviews.bitcoinabc.org/D15899

src/net.cpp
@@ -388,7 +388,7 @@ CNode *CConnman::FindNode(const CService &addr) {
 }
 
 bool CConnman::AlreadyConnectedToAddress(const CAddress &addr) {
-    return FindNode(static_cast<CService>(addr)) ||
+    return FindNode(static_cast<CNetAddr>(addr)) ||
            FindNode(addr.ToStringIPPort());
 }
 

src/test/net_tests.cpp
@@ -1396,8 +1396,6 @@ BOOST_AUTO_TEST_CASE(already_connected_to_address) {
     BOOST_CHECK(connman.AlreadyConnectedToAddress(ip2port1));
 
     // Same IP, different port
-    BOOST_CHECK(!connman.AlreadyConnectedToAddress(ip1port2));
-    connman.AddNode(ip1port2, ConnectionType::OUTBOUND_FULL_RELAY);
     BOOST_CHECK(connman.AlreadyConnectedToAddress(ip1port2));
 }
 