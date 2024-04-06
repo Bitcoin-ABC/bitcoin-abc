3fb0d89e807c959de5a0b99fab313281678aaac5
John <jkuney17@gmail.com>
2024-04-05 20:43:32 -0500

    [e.cash] Add halving banner to navbar
    Summary: As discussed, adding a second banner to the navbar to show the blocks left until the halving
    
    Test Plan:
    preview the site and check the banner looks good.
    If the blockheight api calls fails it will read 'eCash halving is imminent!' instead.
    Open to other ideas there
    
    Reviewers: #bitcoin_abc, bytesofman, Mengerian
    
    Reviewed By: #bitcoin_abc, bytesofman, Mengerian
    
    Subscribers: Mengerian, bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D15910

--- web/e.cash/components/navbar/getNavbarData.js
+++ web/e.cash/components/navbar/getNavbarData.js
@@ -7,6 +7,7 @@ const ApiDataContext = createContext();
 
 export const ApiDataProvider = ({ children }) => {
     const [priceLinkText, setPriceLinkText] = useState('Buy XEC');
+    const [blockHeight, setBlockHeight] = useState(0);
 
     const getPrice = () => {
         const api =
@@ -19,12 +20,21 @@ export const ApiDataProvider = ({ children }) => {
             .catch(err => console.log(err));
     };
 
+    const getBlockHeight = () => {
+        const api = 'https://api.blockchair.com/ecash/stats';
+        fetch(api)
+            .then(response => response.json())
+            .then(data => setBlockHeight(data.data.best_block_height))
+            .catch(err => console.log(err));
+    };
+
     useEffect(() => {
         getPrice();
+        getBlockHeight();
     }, []);
 
     return (
-        <ApiDataContext.Provider value={{ priceLinkText }}>
+        <ApiDataContext.Provider value={{ priceLinkText, blockHeight }}>
             {children}
         </ApiDataContext.Provider>
     );
--- web/e.cash/components/navbar/index.js
+++ web/e.cash/components/navbar/index.js
@@ -17,7 +17,7 @@ export default function Navbar({ announcementbar }) {
     const [windowHeight, setWindowHeight] = useState(0);
     const [navBackground, setNavBackground] = useState(false);
     const mobileBreakPoint = 920;
-    const { priceLinkText } = useApiData();
+    const { priceLinkText, blockHeight } = useApiData();
 
     const handleResize = () => {
         setWindowWidth(window.innerWidth);
@@ -50,8 +50,27 @@ export default function Navbar({ announcementbar }) {
         }
     }, [windowHeight]);
 
+    const blocksBeforeHalving = 840000 - blockHeight;
+
     return (
         <NavbarOuter navBackground={navBackground}>
+            {blocksBeforeHalving > 0 && (
+                <AnnouncementBar
+                    navBackground={navBackground}
+                    href="https://ecash.supply"
+                >
+                    <span>
+                        {blockHeight > 0 ? (
+                            <span>
+                                eCash halving in {blocksBeforeHalving} block
+                                {blocksBeforeHalving > 1 ? 's' : ''}!
+                            </span>
+                        ) : (
+                            <span>eCash halving is imminent!</span>
+                        )}
+                    </span>
+                </AnnouncementBar>
+            )}
             <AnnouncementBar navBackground={navBackground} href="/upgrade">
                 <span>
                     <span>Prepare for the eCash network upgrade!</span>
@@ -65,7 +84,7 @@ export default function Navbar({ announcementbar }) {
                 </EnvVarMessage>
             )}
             {!process.env.NEXT_PUBLIC_WEGLOT_API_KEY && (
-                <EnvVarMessage>
+                <EnvVarMessage style={{ top: '14px' }}>
                     Translations are disabled, set the env
                     NEXT_PUBLIC_WEGLOT_API_KEY to fix
                 </EnvVarMessage>
