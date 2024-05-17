bitcoin-ghost/bitcoin-core-ladder-script

util: improve streams.h:FindByte() performance
Avoid use of the expensive mod operator (%) when calculating the
buffer offset. No functional difference.

Co-authored-by: Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>

src/streams.h
@@ -756,15 +756,25 @@ public:
     }
 
     //! search for a given byte in the stream, and remain positioned on it
-    void FindByte(uint8_t ch)
+    void FindByte(std::byte byte)
     {
+        // For best performance, avoid mod operation within the loop.
+        size_t buf_offset{size_t(m_read_pos % uint64_t(vchBuf.size()))};
         while (true) {
-            if (m_read_pos == nSrcPos)
+            if (m_read_pos == nSrcPos) {
+                // No more bytes available; read from the file into the buffer,
+                // setting nSrcPos to one beyond the end of the new data.
+                // Throws exception if end-of-file reached.
                 Fill();
-            if (vchBuf[m_read_pos % vchBuf.size()] == std::byte{ch}) {
-                break;
             }
-            m_read_pos++;
+            const size_t len{std::min<size_t>(vchBuf.size() - buf_offset, nSrcPos - m_read_pos)};
+            const auto it_start{vchBuf.begin() + buf_offset};
+            const auto it_find{std::find(it_start, it_start + len, byte)};
+            const size_t inc{size_t(std::distance(it_start, it_find))};
+            m_read_pos += inc;
+            if (inc < len) break;
+            buf_offset += inc;
+            if (buf_offset >= vchBuf.size()) buf_offset = 0;
         }
     }
 };

src/validation.cpp
@@ -4565,7 +4565,7 @@ void Chainstate::LoadExternalBlockFile(
             try {
                 // locate a header
                 unsigned char buf[CMessageHeader::MESSAGE_START_SIZE];
-                blkdat.FindByte(params.MessageStart()[0]);
+                blkdat.FindByte(std::byte(params.MessageStart()[0]));
                 nRewind = blkdat.GetPos() + 1;
                 blkdat >> buf;
                 if (memcmp(buf, params.MessageStart(), CMessageHeader::MESSAGE_START_SIZE)) {

Bitcoin-ABC/bitcoin-abc

Summary:
The original code is doing a modulus operation for every character, which is often orders of magnitude slower than the byte comparision or addition/subtraction.
Move the expensive operation out of the loop.
Note that a simpler version of this fix ended up yielding worse performances on ARM Mac architectures, hence the switch to `std::find` (see https://github.com/bitcoin/bitcoin/pull/19690#pullrequestreview-1318324998)
This concludes backport of [[https://github.com/bitcoin/bitcoin/pull/19690 | core#19690]]
https://github.com/bitcoin/bitcoin/pull/19690/commits/72efc26439da9a1344a19569fb0cab01f82ae7d1
Depends on D16179
Test Plan:
`ninja all check-all bench-bitcoin`
```
|               ns/op |                op/s |    err% |     total | benchmark
|--------------------:|--------------------:|--------:|----------:|:----------
Before:
|              306.76 |        3,259,865.52 |    0.5% |      0.01 | `FindByte`
After:
|               28.35 |       35,270,185.20 |    0.0% |      0.01 | `FindByte`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D16180

src/streams.h
@@ -820,15 +820,29 @@ public:
     }
 
     //! search for a given byte in the stream, and remain positioned on it
-    void FindByte(uint8_t ch) {
+    void FindByte(std::byte byte) {
+        // For best performance, avoid mod operation within the loop.
+        size_t buf_offset{size_t(nReadPos % uint64_t(vchBuf.size()))};
         while (true) {
             if (nReadPos == nSrcPos) {
+                // No more bytes available; read from the file into the buffer,
+                // setting nSrcPos to one beyond the end of the new data.
+                // Throws exception if end-of-file reached.
                 Fill();
             }
-            if (vchBuf[nReadPos % vchBuf.size()] == std::byte{ch}) {
+            const size_t len{std::min<size_t>(vchBuf.size() - buf_offset,
+                                              nSrcPos - nReadPos)};
+            const auto it_start{vchBuf.begin() + buf_offset};
+            const auto it_find{std::find(it_start, it_start + len, byte)};
+            const size_t inc{size_t(std::distance(it_start, it_find))};
+            nReadPos += inc;
+            if (inc < len) {
                 break;
             }
-            nReadPos++;
+            buf_offset += inc;
+            if (buf_offset >= vchBuf.size()) {
+                buf_offset = 0;
+            }
         }
     }
 };

src/validation.cpp
@@ -5226,7 +5226,7 @@ void Chainstate::LoadExternalBlockFile(
             try {
                 // Locate a header.
                 uint8_t buf[CMessageHeader::MESSAGE_START_SIZE];
-                blkdat.FindByte(params.DiskMagic()[0]);
+                blkdat.FindByte(std::byte(params.DiskMagic()[0]));
                 nRewind = blkdat.GetPos() + 1;
                 blkdat >> buf;
                 if (memcmp(buf, params.DiskMagic().data(),