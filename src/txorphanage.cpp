bitcoin-ghost/bitcoin-core-ladder-script

Move all g_cs_orphans locking to txorphanage

src/txorphanage.cpp
@@ -15,11 +15,11 @@ static constexpr int64_t ORPHAN_TX_EXPIRE_TIME = 20 * 60;
 /** Minimum time between orphan transactions expire time checks in seconds */
 static constexpr int64_t ORPHAN_TX_EXPIRE_INTERVAL = 5 * 60;
 
-RecursiveMutex g_cs_orphans;
+RecursiveMutex TxOrphanage::g_cs_orphans;
 
 bool TxOrphanage::AddTx(const CTransactionRef& tx, NodeId peer)
 {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     const uint256& hash = tx->GetHash();
     if (m_orphans.count(hash))
@@ -55,6 +55,12 @@ bool TxOrphanage::AddTx(const CTransactionRef& tx, NodeId peer)
 
 int TxOrphanage::EraseTx(const uint256& txid)
 {
+    LOCK(g_cs_orphans);
+    return _EraseTx(txid);
+}
+
+int TxOrphanage::_EraseTx(const uint256& txid)
+{
     AssertLockHeld(g_cs_orphans);
     std::map<uint256, OrphanTx>::iterator it = m_orphans.find(txid);
     if (it == m_orphans.end())
@@ -87,7 +93,7 @@ int TxOrphanage::EraseTx(const uint256& txid)
 
 void TxOrphanage::EraseForPeer(NodeId peer)
 {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     m_peer_work_set.erase(peer);
 
@@ -98,7 +104,7 @@ void TxOrphanage::EraseForPeer(NodeId peer)
         std::map<uint256, OrphanTx>::iterator maybeErase = iter++; // increment to avoid iterator becoming invalid
         if (maybeErase->second.fromPeer == peer)
         {
-            nErased += EraseTx(maybeErase->second.tx->GetHash());
+            nErased += _EraseTx(maybeErase->second.tx->GetHash());
         }
     }
     if (nErased > 0) LogPrint(BCLog::MEMPOOL, "Erased %d orphan tx from peer=%d\n", nErased, peer);
@@ -106,7 +112,7 @@ void TxOrphanage::EraseForPeer(NodeId peer)
 
 void TxOrphanage::LimitOrphans(unsigned int max_orphans)
 {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     unsigned int nEvicted = 0;
     static int64_t nNextSweep;
@@ -120,7 +126,7 @@ void TxOrphanage::LimitOrphans(unsigned int max_orphans)
         {
             std::map<uint256, OrphanTx>::iterator maybeErase = iter++;
             if (maybeErase->second.nTimeExpire <= nNow) {
-                nErased += EraseTx(maybeErase->second.tx->GetHash());
+                nErased += _EraseTx(maybeErase->second.tx->GetHash());
             } else {
                 nMinExpTime = std::min(maybeErase->second.nTimeExpire, nMinExpTime);
             }
@@ -134,7 +140,7 @@ void TxOrphanage::LimitOrphans(unsigned int max_orphans)
     {
         // Evict a random orphan:
         size_t randompos = rng.randrange(m_orphan_list.size());
-        EraseTx(m_orphan_list[randompos]->first);
+        _EraseTx(m_orphan_list[randompos]->first);
         ++nEvicted;
     }
     if (nEvicted > 0) LogPrint(BCLog::MEMPOOL, "orphanage overflow, removed %u tx\n", nEvicted);
@@ -142,7 +148,7 @@ void TxOrphanage::LimitOrphans(unsigned int max_orphans)
 
 void TxOrphanage::AddChildrenToWorkSet(const CTransaction& tx, NodeId peer)
 {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     // Get this peer's work set, emplacing an empty set it didn't exist
     std::set<uint256>& orphan_work_set = m_peer_work_set.try_emplace(peer).first->second;
@@ -169,7 +175,7 @@ bool TxOrphanage::HaveTx(const GenTxid& gtxid) const
 
 CTransactionRef TxOrphanage::GetTxToReconsider(NodeId peer, NodeId& originator, bool& more)
 {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     auto work_set_it = m_peer_work_set.find(peer);
     if (work_set_it != m_peer_work_set.end()) {
@@ -215,7 +221,7 @@ void TxOrphanage::EraseForBlock(const CBlock& block)
     if (vOrphanErase.size()) {
         int nErased = 0;
         for (const uint256& orphanHash : vOrphanErase) {
-            nErased += EraseTx(orphanHash);
+            nErased += _EraseTx(orphanHash);
         }
         LogPrint(BCLog::MEMPOOL, "Erased %d orphan tx included or conflicted by block\n", nErased);
     }

src/txorphanage.h
@@ -13,9 +13,6 @@
 #include <map>
 #include <set>
 
-/** Guards orphan transactions */
-extern RecursiveMutex g_cs_orphans;
-
 /** A class to track orphan transactions (failed on TX_MISSING_INPUTS)
  * Since we cannot distinguish orphans from bad transactions with
  * non-existent inputs, we heavily limit the number of orphans
@@ -24,10 +21,10 @@ extern RecursiveMutex g_cs_orphans;
 class TxOrphanage {
 public:
     /** Add a new orphan transaction */
-    bool AddTx(const CTransactionRef& tx, NodeId peer) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+    bool AddTx(const CTransactionRef& tx, NodeId peer) EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Check if we already have an orphan transaction (by txid or wtxid) */
-    bool HaveTx(const GenTxid& gtxid) const LOCKS_EXCLUDED(::g_cs_orphans);
+    bool HaveTx(const GenTxid& gtxid) const EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Extract a transaction from a peer's work set
      *  Returns nullptr and sets more to false if there are no transactions
@@ -36,31 +33,34 @@ public:
      *  the originating peer, and whether there are more orphans for this peer
      *  to work on after this tx.
      */
-    CTransactionRef GetTxToReconsider(NodeId peer, NodeId& originator, bool& more) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+    CTransactionRef GetTxToReconsider(NodeId peer, NodeId& originator, bool& more) EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Erase an orphan by txid */
-    int EraseTx(const uint256& txid) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+    int EraseTx(const uint256& txid) EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Erase all orphans announced by a peer (eg, after that peer disconnects) */
-    void EraseForPeer(NodeId peer) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+    void EraseForPeer(NodeId peer) EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Erase all orphans included in or invalidated by a new block */
-    void EraseForBlock(const CBlock& block) LOCKS_EXCLUDED(::g_cs_orphans);
+    void EraseForBlock(const CBlock& block) EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Limit the orphanage to the given maximum */
-    void LimitOrphans(unsigned int max_orphans) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+    void LimitOrphans(unsigned int max_orphans) EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Add any orphans that list a particular tx as a parent into a peer's work set */
-    void AddChildrenToWorkSet(const CTransaction& tx, NodeId peer) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+    void AddChildrenToWorkSet(const CTransaction& tx, NodeId peer) EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Return how many entries exist in the orphange */
-    size_t Size() LOCKS_EXCLUDED(::g_cs_orphans)
+    size_t Size() EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans)
     {
-        LOCK(::g_cs_orphans);
+        LOCK(g_cs_orphans);
         return m_orphans.size();
     }
 
 protected:
+    /** Guards orphan transactions */
+    static RecursiveMutex g_cs_orphans;
+
     struct OrphanTx {
         CTransactionRef tx;
         NodeId fromPeer;
@@ -96,6 +96,9 @@ protected:
     /** Index from wtxid into the m_orphans to lookup orphan
      *  transactions using their witness ids. */
     std::map<uint256, OrphanMap::iterator> m_wtxid_to_orphan_it GUARDED_BY(g_cs_orphans);
+
+    /** Erase an orphan by txid */
+    int _EraseTx(const uint256& txid) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
 };
 
 #endif // BITCOIN_TXORPHANAGE_H

Bitcoin-ABC/bitcoin-abc

Summary:
Partial backport of [[https://github.com/bitcoin/bitcoin/pull/26295 | core#26295]]:
https://github.com/bitcoin/bitcoin/pull/26295/commits/733d85f79cde353d8c9b54370f296b1031fa33d9
Depends on D16225 and D16233.
Test Plan:
With Clang and Debug:
  ninja all check-all
Run the TSAN build.
Reviewers: #bitcoin_abc, PiRK
Reviewed By: #bitcoin_abc, PiRK
Differential Revision: https://reviews.bitcoinabc.org/D16234

src/txorphanage.cpp
@@ -15,10 +15,10 @@ static constexpr int64_t ORPHAN_TX_EXPIRE_TIME = 20 * 60;
 /** Minimum time between orphan transactions expire time checks in seconds */
 static constexpr int64_t ORPHAN_TX_EXPIRE_INTERVAL = 5 * 60;
 
-RecursiveMutex g_cs_orphans;
+RecursiveMutex TxOrphanage::g_cs_orphans;
 
 bool TxOrphanage::AddTx(const CTransactionRef &tx, NodeId peer) {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     const TxId &txid = tx->GetId();
     if (m_orphans.count(txid)) {
@@ -54,6 +54,11 @@ bool TxOrphanage::AddTx(const CTransactionRef &tx, NodeId peer) {
 }
 
 int TxOrphanage::EraseTx(const TxId &txid) {
+    LOCK(g_cs_orphans);
+    return _EraseTx(txid);
+}
+
+int TxOrphanage::_EraseTx(const TxId &txid) {
     AssertLockHeld(g_cs_orphans);
     std::map<TxId, OrphanTx>::iterator it = m_orphans.find(txid);
     if (it == m_orphans.end()) {
@@ -86,7 +91,7 @@ int TxOrphanage::EraseTx(const TxId &txid) {
 }
 
 void TxOrphanage::EraseForPeer(NodeId peer) {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     m_peer_work_set.erase(peer);
 
@@ -96,7 +101,7 @@ void TxOrphanage::EraseForPeer(NodeId peer) {
         std::map<TxId, OrphanTx>::iterator maybeErase =
             iter++; // increment to avoid iterator becoming invalid
         if (maybeErase->second.fromPeer == peer) {
-            nErased += EraseTx(maybeErase->second.tx->GetId());
+            nErased += _EraseTx(maybeErase->second.tx->GetId());
         }
     }
     if (nErased > 0) {
@@ -106,7 +111,7 @@ void TxOrphanage::EraseForPeer(NodeId peer) {
 }
 
 unsigned int TxOrphanage::LimitOrphans(unsigned int max_orphans) {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     unsigned int nEvicted = 0;
     static int64_t nNextSweep;
@@ -120,7 +125,7 @@ unsigned int TxOrphanage::LimitOrphans(unsigned int max_orphans) {
         while (iter != m_orphans.end()) {
             std::map<TxId, OrphanTx>::iterator maybeErase = iter++;
             if (maybeErase->second.nTimeExpire <= nNow) {
-                nErased += EraseTx(maybeErase->second.tx->GetId());
+                nErased += _EraseTx(maybeErase->second.tx->GetId());
             } else {
                 nMinExpTime =
                     std::min(maybeErase->second.nTimeExpire, nMinExpTime);
@@ -138,14 +143,14 @@ unsigned int TxOrphanage::LimitOrphans(unsigned int max_orphans) {
     while (m_orphans.size() > max_orphans) {
         // Evict a random orphan:
         size_t randompos = rng.randrange(m_orphan_list.size());
-        EraseTx(m_orphan_list[randompos]->first);
+        _EraseTx(m_orphan_list[randompos]->first);
         ++nEvicted;
     }
     return nEvicted;
 }
 
 void TxOrphanage::AddChildrenToWorkSet(const CTransaction &tx, NodeId peer) {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     // Get this peer's work set, emplacing an empty set if it didn't exist
     std::set<TxId> &orphan_work_set =
@@ -169,7 +174,7 @@ bool TxOrphanage::HaveTx(const TxId &txid) const {
 
 CTransactionRef TxOrphanage::GetTxToReconsider(NodeId peer, NodeId &originator,
                                                bool &more) {
-    AssertLockHeld(g_cs_orphans);
+    LOCK(g_cs_orphans);
 
     auto work_set_it = m_peer_work_set.find(peer);
     if (work_set_it != m_peer_work_set.end()) {
@@ -218,7 +223,7 @@ void TxOrphanage::EraseForBlock(const CBlock &block) {
     if (vOrphanErase.size()) {
         int nErased = 0;
         for (const auto &orphanId : vOrphanErase) {
-            nErased += EraseTx(orphanId);
+            nErased += _EraseTx(orphanId);
         }
         LogPrint(BCLog::MEMPOOL,
                  "Erased %d orphan tx included or conflicted by block\n",

src/txorphanage.h
@@ -13,9 +13,6 @@
 #include <map>
 #include <set>
 
-/** Guards orphan transactions */
-extern RecursiveMutex g_cs_orphans;
-
 /**
  * A class to track orphan transactions (failed on TX_MISSING_INPUTS)
  * Since we cannot distinguish orphans from bad transactions with
@@ -26,10 +23,10 @@ class TxOrphanage {
 public:
     /** Add a new orphan transaction */
     bool AddTx(const CTransactionRef &tx, NodeId peer)
-        EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+        EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Check if we already have an orphan transaction */
-    bool HaveTx(const TxId &txid) const LOCKS_EXCLUDED(g_cs_orphans);
+    bool HaveTx(const TxId &txid) const EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /**
      * Extract a transaction from a peer's work set
@@ -42,37 +39,41 @@ public:
      */
     CTransactionRef GetTxToReconsider(NodeId peer, NodeId &originator,
                                       bool &more)
-        EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+        EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Erase an orphan by txid */
-    int EraseTx(const TxId &txid) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+    int EraseTx(const TxId &txid) EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /**
      * Erase all orphans announced by a peer (eg, after that peer disconnects)
      */
-    void EraseForPeer(NodeId peer) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+    void EraseForPeer(NodeId peer) EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Erase all orphans included in or invalidated by a new block */
-    void EraseForBlock(const CBlock &block) LOCKS_EXCLUDED(g_cs_orphans);
+    void EraseForBlock(const CBlock &block)
+        EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Limit the orphanage to the given maximum */
     unsigned int LimitOrphans(unsigned int max_orphans)
-        EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+        EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /**
      * Add any orphans that list a particular tx as a parent into a peer's work
      * set
      */
     void AddChildrenToWorkSet(const CTransaction &tx, NodeId peer)
-        EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
+        EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans);
 
     /** Return how many entries exist in the orphange */
-    size_t Size() LOCKS_EXCLUDED(::g_cs_orphans) {
-        LOCK(::g_cs_orphans);
+    size_t Size() EXCLUSIVE_LOCKS_REQUIRED(!g_cs_orphans) {
+        LOCK(g_cs_orphans);
         return m_orphans.size();
     }
 
 protected:
+    /** Guards orphan transactions */
+    static RecursiveMutex g_cs_orphans;
+
     struct OrphanTx {
         CTransactionRef tx;
         NodeId fromPeer;
@@ -108,6 +109,9 @@ protected:
 
     /** Orphan transactions in vector for quick random eviction */
     std::vector<OrphanMap::iterator> m_orphan_list GUARDED_BY(g_cs_orphans);
+
+    /** Erase an orphan by txid */
+    int _EraseTx(const TxId &txid) EXCLUSIVE_LOCKS_REQUIRED(g_cs_orphans);
 };
 
 #endif // BITCOIN_TXORPHANAGE_H