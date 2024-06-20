762754feb72845594814e8c787e9bcd9c9e6ffc6
Martin Zumsande <mzumsande@gmail.com>
2024-06-20 16:14:22 -0400

    validation: Don't load a snapshot if it's not in the best header chain.
    Summary:
    If the snapshot is not an ancestor of the most-work header (m_best_header),
    syncing from that alternative chain should be prioritised.
    Therefore don't accept loading a snapshot in this situation.
    
    If that other chain turns out to be invalid, m_best_header
    would be reset and loading the snapshot should be possible again.
    
    Because of the work required to generate a conflicting headers chain,
    this should only be possible under extreme circumstances, such as major forks.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/30320 | core#30320]]
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D18059

--- src/validation.cpp
+++ src/validation.cpp
@@ -6587,6 +6587,14 @@ util::Result<void> ChainstateManager::ActivateSnapshot(
                 base_blockhash.ToString())};
         }
 
+        if (!m_best_header || m_best_header->GetAncestor(base_blockheight) !=
+                                  snapshot_start_block) {
+            return util::Error{
+                _("A forked headers-chain with more work than the chain with "
+                  "the snapshot base block header exists. Please proceed to "
+                  "sync without AssumeUtxo.")};
+        }
+
         if (Assert(m_active_chainstate->GetMempool())->size() > 0) {
             return util::Error{Untranslated(
                 "Can't activate a snapshot when mempool not empty.")};
