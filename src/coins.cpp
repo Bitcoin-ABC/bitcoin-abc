bf2683f2509c05c90953d648ff0e5de3d0c8a0a7
Lőrinc <pap.lorinc@gmail.com>
2024-06-23 23:56:14 +0200

    Reduce cache lookups in CCoinsViewCache::FetchCoin
    Summary:
    Enhanced efficiency and readability of CCoinsViewCache::FetchCoin by replacing separate find() and emplace() calls with a single try_emplace(), reducing map lookups and potential insertions.
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/30326 | core#30326]]
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19004

--- src/coins.cpp
+++ src/coins.cpp
@@ -72,25 +72,19 @@ size_t CCoinsViewCache::DynamicMemoryUsage() const {
 
 CCoinsMap::iterator
 CCoinsViewCache::FetchCoin(const COutPoint &outpoint) const {
-    CCoinsMap::iterator it = cacheCoins.find(outpoint);
-    if (it != cacheCoins.end()) {
-        return it;
+    const auto [ret, inserted] = cacheCoins.try_emplace(outpoint);
+    if (inserted) {
+        if (!base->GetCoin(outpoint, ret->second.coin)) {
+            cacheCoins.erase(ret);
+            return cacheCoins.end();
+        }
+        if (ret->second.coin.IsSpent()) {
+            // The parent only has an empty entry for this outpoint; we can
+            // consider our version as fresh.
+            CCoinsCacheEntry::SetFresh(*ret, m_sentinel);
+        }
+        cachedCoinsUsage += ret->second.coin.DynamicMemoryUsage();
     }
-    Coin tmp;
-    if (!base->GetCoin(outpoint, tmp)) {
-        return cacheCoins.end();
-    }
-    CCoinsMap::iterator ret =
-        cacheCoins
-            .emplace(std::piecewise_construct, std::forward_as_tuple(outpoint),
-                     std::forward_as_tuple(std::move(tmp)))
-            .first;
-    if (ret->second.coin.IsSpent()) {
-        // The parent only has an empty entry for this outpoint; we can consider
-        // our version as fresh.
-        CCoinsCacheEntry::SetFresh(*ret, m_sentinel);
-    }
-    cachedCoinsUsage += ret->second.coin.DynamicMemoryUsage();
     return ret;
 }
 
