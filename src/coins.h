d6cdcc3d76e3d772f4bceb045cf50042a05c3e74
Andrew Toth <andrewstoth@gmail.com>
2024-06-28 16:57:42 -0400

    refactor: add CoinsCachePair alias
    Summary:
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/28280 | core#28280]]
    https://github.com/bitcoin/bitcoin/pull/28280/commits/75f36d241d2a344c5c4ce2c80250bdde91b3295e
    Depends on D18612
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D18614

--- src/coins.h
+++ src/coins.h
@@ -70,6 +70,9 @@ public:
     }
 };
 
+struct CCoinsCacheEntry;
+using CoinsCachePair = std::pair<const COutPoint, CCoinsCacheEntry>;
+
 /**
  * A Coin in one level of the coins database caching hierarchy.
  *
@@ -139,9 +142,7 @@ public:
  */
 using CCoinsMap = std::unordered_map<
     COutPoint, CCoinsCacheEntry, SaltedOutpointHasher, std::equal_to<COutPoint>,
-    PoolAllocator<std::pair<const COutPoint, CCoinsCacheEntry>,
-                  sizeof(std::pair<const COutPoint, CCoinsCacheEntry>) +
-                      sizeof(void *) * 4>>;
+    PoolAllocator<CoinsCachePair, sizeof(CoinsCachePair) + sizeof(void *) * 4>>;
 
 using CCoinsMapMemoryResource = CCoinsMap::allocator_type::ResourceType;
 
