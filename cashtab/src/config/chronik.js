8ff273438d5fd06b41ad9446813bb364c7e2bc58
bytesofman <joey@bitcoinabc.org>
2024-08-05 08:05:54 -0700

    [Cashtab] Temp switch chronik server order
    Summary: Switch chronik precedence pending re-index
    
    Test Plan: `npm start` and cashtab loads, no API error
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D16581

--- cashtab/src/config/chronik.js
+++ cashtab/src/config/chronik.js
@@ -6,8 +6,8 @@
 
 export const chronik = {
     urls: [
-        'https://chronik-native1.fabien.cash',
         'https://chronik-native2.fabien.cash',
+        'https://chronik-native1.fabien.cash',
         'https://chronik.pay2stay.com/xec',
     ],
     txHistoryCount: 20,
