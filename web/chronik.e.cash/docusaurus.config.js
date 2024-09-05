8134d950210093fb0c590ed57705a8f45122ab2b
Fabien <fabcien@gmail.com>
2024-09-05 17:05:48 +0200

    [chronik-doc] Check doc deployment on a temporary URL
    Summary: This will be reverted to use the final URL once it's all working properly.
    
    Test Plan: Deploy and navigate to the url.
    
    Reviewers: #bitcoin_abc, bytesofman
    
    Reviewed By: #bitcoin_abc, bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D16722

--- web/chronik.e.cash/docusaurus.config.js
+++ web/chronik.e.cash/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
     favicon: 'img/favicon.png',
 
     // Set the production url of your site here
-    url: 'https://chronik.e.cash',
+    url: 'https://chronik-doc.fabien.cash',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
