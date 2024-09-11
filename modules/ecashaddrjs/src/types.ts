7263d78c0064f84502c95e1d01c30f837c9e12d2
bytesofman <joey@bitcoinabc.org>
2024-09-11 06:45:49 -0700

    [ecashaddrjs] Add missing copyright notice to new file
    Summary:
    Overlooked in the ts implementation
    
    `ecashaddrjs` at the moment does not easily lend itself to handling this with a plugin-set header, as different files have different authorship.
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D16746

--- modules/ecashaddrjs/src/types.ts
+++ modules/ecashaddrjs/src/types.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 export type AddressType = 'P2PKH' | 'P2SH' | 'p2pkh' | 'p2sh';
 
 export interface DecodedAddress {
