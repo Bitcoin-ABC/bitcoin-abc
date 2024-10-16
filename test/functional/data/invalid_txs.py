511bba71f2e56cfc39df77d1eb3de9594995cc32
Antoine Poinsot <darosior@protonmail.com>
2024-10-16 13:34:15 +0100

    [test] A non-standard transaction which is also consensus-invalid should return the consensus error
    Summary:
    This concludes backport of [[https://github.com/bitcoin/bitcoin/pull/31097 | core#31097]]
    https://github.com/bitcoin/bitcoin/pull/31097/commits/86e2a6b749c7fecbd086b361806ac9f6e9426d79
    
    Depends on D18929
    
    Test Plan: `test/functional/test_runner.py feature_block p2p_invalid_tx`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D18930

--- test/functional/data/invalid_txs.py
+++ test/functional/data/invalid_txs.py
@@ -251,6 +251,22 @@ def getDisabledOpcodeTemplate(opcode):
     )
 
 
+class NonStandardAndInvalid(BadTxTemplate):
+    """A non-standard transaction which is also consensus-invalid should return the consensus error."""
+
+    reject_reason = "mandatory-script-verify-flag-failed (OP_RETURN was encountered)"
+    block_reject_reason = BLOCK_NONPUSH_REJECT_REASON
+    expect_disconnect = True
+
+    def get_tx(self):
+        return create_tx_with_script(
+            self.spend_tx,
+            0,
+            script_sig=b"\x00" * 3 + b"\xab\x6a",
+            amount=(self.spend_avail // 2),
+        )
+
+
 # Disabled opcode tx templates (CVE-2010-5137)
 DisabledOpcodeTemplates = [
     getDisabledOpcodeTemplate(opcode)
