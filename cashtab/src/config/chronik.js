8ed3c1c630b456ebdc9fdd811123093f71828e62
bytesofman <joey@bitcoinabc.org>
2024-10-29 14:49:05 -0700

    [Cashtab] Use ABC chronik first
    Summary:
    We have been using pay2stay server as it (previously) was the only known instance to have chronik offers indexed.
    
    chronik-native1 now has this. So, default to this server first. Fallback to pay2stay on an error. Fallback to chronik-native2 if both are in error (in this case, agora and NFTs will not work).
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D17042

--- cashtab/src/config/chronik.js
+++ cashtab/src/config/chronik.js
@@ -9,9 +9,9 @@ export const chronik = {
         process.env.REACT_APP_TESTNET === 'true'
             ? ['https://chronik-testnet.fabien.cash']
             : [
-                  'https://chronik.pay2stay.com/xec2',
-                  'https://chronik-native1.fabien.cash',
-                  'https://chronik-native2.fabien.cash',
+                  'https://chronik-native1.fabien.cash', // indexed for agora.py plugin
+                  'https://chronik.pay2stay.com/xec2', // indexed for agora.py plugin
+                  'https://chronik-native2.fabien.cash', // NOT indexed for agora.py
               ],
     txHistoryCount: 20,
     txHistoryPageSize: 20,
