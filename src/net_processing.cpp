5e91e0989f8bc9e07adeda485b2d8e204b80894f
Fabien <fabcien@gmail.com>
2024-11-04 15:03:27 +0100

    [avalanche] Always print the finalization messages
    Summary: Also the invalidation and stalling messages. The accepted and rejected votes (intermediate and non final steps of the vote) are still only printed when -debug=avalanche is set.
    
    Test Plan:
      ninja all check check-functional
    
    Reviewers: #bitcoin_abc, roqqit
    
    Reviewed By: roqqit
    
    Differential Revision: https://reviews.bitcoinabc.org/D17068

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -6575,9 +6575,11 @@ void PeerManagerImpl::ProcessMessage(
                                 const std::string &voteItemTypeStr,
                                 const auto &voteItemId) {
             std::string voteOutcome;
+            bool alwaysPrint = false;
             switch (voteUpdate.getStatus()) {
                 case avalanche::VoteStatus::Invalid:
                     voteOutcome = "invalidated";
+                    alwaysPrint = true;
                     break;
                 case avalanche::VoteStatus::Rejected:
                     voteOutcome = "rejected";
@@ -6587,17 +6589,25 @@ void PeerManagerImpl::ProcessMessage(
                     break;
                 case avalanche::VoteStatus::Finalized:
                     voteOutcome = "finalized";
+                    alwaysPrint = true;
                     break;
                 case avalanche::VoteStatus::Stale:
                     voteOutcome = "stalled";
+                    alwaysPrint = true;
                     break;
 
                     // No default case, so the compiler can warn about missing
                     // cases
             }
 
-            LogPrint(BCLog::AVALANCHE, "Avalanche %s %s %s\n", voteOutcome,
-                     voteItemTypeStr, voteItemId.ToString());
+            if (alwaysPrint) {
+                LogPrintf("Avalanche %s %s %s\n", voteOutcome, voteItemTypeStr,
+                          voteItemId.ToString());
+            } else {
+                // Only print these messages if -debug=avalanche is set
+                LogPrint(BCLog::AVALANCHE, "Avalanche %s %s %s\n", voteOutcome,
+                         voteItemTypeStr, voteItemId.ToString());
+            }
         };
 
         bool shouldActivateBestChain = false;
