fccf4f50904d559c7707c3d6d50b643b8c164ffd
bytesofman <joey@bitcoinabc.org>
2024-11-05 10:12:41 -0800

    [Cashtab] Convert CashtabCache to ts
    Summary:
    Part of incremental conversion of Cashtab to ts
    
    Will use these types in our first ts component, Collection, for NFTs.
    
    Keep it in a limited diff.
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D17078

--- cashtab/src/config/CashtabCache.ts
+++ cashtab/src/config/CashtabCache.ts
@@ -6,13 +6,29 @@
 // We include an info object for unknown token id "0000000000000000000000000000000000000000000000000000000000000000"
 // We may come across utxos with this tokenId as it may be returned by chronik, however
 // we cannot get chronik.tx(UNKNOWN_TOKEN_ID) or chronik.token(UNKNOWN_TOKEN_ID)
+import { TokenType, GenesisInfo, BlockMetadata } from 'chronik-client';
+
+export interface CashtabCachedTokenInfo {
+    tokenType: TokenType;
+    timeFirstSeen: number;
+    genesisInfo: GenesisInfo;
+    block?: BlockMetadata;
+    genesisSupply: string;
+    genesisMintBatons: number;
+    genesisOutputScripts: string[];
+    groupTokenId?: string;
+}
+
+interface CashtabCache {
+    tokens: Map<string, CashtabCachedTokenInfo>;
+}
 
 export const UNKNOWN_TOKEN_ID =
     '0000000000000000000000000000000000000000000000000000000000000000';
-export const UNKNOWN_TOKEN_CACHED_INFO = {
+export const UNKNOWN_TOKEN_CACHED_INFO: CashtabCachedTokenInfo = {
     tokenType: {
-        protocol: 'UNKNOWN',
-        type: 'UNKNOWN',
+        protocol: 'SLP',
+        type: 'SLP_TOKEN_TYPE_UNKNOWN',
         number: 0,
     },
     timeFirstSeen: 0,
