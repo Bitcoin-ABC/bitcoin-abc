aa32579dd49810fe1a85b92baabc76e8f9f0dffb
Antony Zegers <antony.zegers@gmail.com>
2024-12-12 14:15:15 -0800

    [e.cash] Open external button links in new tabs
    Summary: A few more that were missed in D17334
    
    Test Plan:
    `git grep button` to find all the places the button is used
    Run the site locally to check that the external links open in a new tab
    
    Reviewers: #bitcoin_abc, bytesofman
    
    Reviewed By: #bitcoin_abc, bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D17346

--- web/e.cash/pages/core-tech.js
+++ web/e.cash/pages/core-tech.js
@@ -121,12 +121,14 @@ export default function CoreTech() {
                                 link="https://avalanche.cash/"
                                 corner="bottomRight"
                                 color="white"
+                                openInNewTab
                             />
                             <Button
                                 text="Whitepaper"
                                 link="https://ipfs.io/ipfs/QmUy4jh5mGNZvLkjies1RWM4YuvJh5o2FYopNPVYwrRVGV"
                                 corner="bottomRight"
                                 color="accent"
+                                openInNewTab
                             />
                         </ButtonRow>
                     </TextImageBlock>
