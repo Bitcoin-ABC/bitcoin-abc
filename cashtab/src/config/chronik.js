e1c01c735dc261a04f2527f04ebbdf8bfb8c1506
bytesofman <joey@bitcoinabc.org>
2025-01-06 13:33:45 -0800

    [Cashtab] Swap chronik instances for patch
    Summary: need to build agora index for chronik native
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D17477

--- cashtab/src/config/chronik.js
+++ cashtab/src/config/chronik.js
@@ -9,8 +9,9 @@ export const chronik = {
         process.env.REACT_APP_TESTNET === 'true'
             ? ['https://chronik-testnet.fabien.cash']
             : [
-                  'https://chronik-native1.fabien.cash', // indexed for agora.py plugin
+                  'https://chronik-native.fabien.cash', // abc dev
                   'https://chronik.pay2stay.com/xec2', // indexed for agora.py plugin
+                  'https://chronik-native1.fabien.cash', // indexed for agora.py plugin
                   'https://chronik-native2.fabien.cash', // NOT indexed for agora.py
               ],
     txHistoryCount: 20,
