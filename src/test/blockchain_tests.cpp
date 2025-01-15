49edbad09b316e94fd9f6c8ea4f2c0aedf5462e1
stratospher <44024636+stratospher@users.noreply.github.com>
2025-01-15 14:59:09 +0530

    test: check BlockStatus when InvalidateBlock is used
    Summary:
    when a block is invalidated using InvalidateBlock, check that:
    1. it's status is BLOCK_FAILED_VALID
    2. it's children's status is BLOCK_FAILED_CHILD
       and not BLOCK_FAILED_VALID
    3. it's ancestors are valid
    
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/31835 | core#31835]]
    https://github.com/bitcoin/bitcoin/pull/31835/commits/9e29653b424b324c753526fbbf9fdbc92e88244f
    
    Note that this test is for a bug that was fixed independently in Bitcoin ABC in D5041
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D18795

--- src/test/blockchain_tests.cpp
+++ src/test/blockchain_tests.cpp
@@ -107,4 +107,40 @@ BOOST_FIXTURE_TEST_CASE(get_prune_height, TestChain100Setup) {
     CheckGetPruneHeight(blockman, chain, 100);
 }
 
+BOOST_FIXTURE_TEST_CASE(invalidate_block, TestChain100Setup) {
+    const CChain &active{
+        *WITH_LOCK(Assert(m_node.chainman)->GetMutex(),
+                   return &Assert(m_node.chainman)->ActiveChain())};
+
+    // Check BlockStatus when doing InvalidateBlock()
+    BlockValidationState state;
+    auto *orig_tip = active.Tip();
+    int height_to_invalidate = orig_tip->nHeight - 10;
+    auto *tip_to_invalidate = active[height_to_invalidate];
+    m_node.chainman->ActiveChainstate().InvalidateBlock(state,
+                                                        tip_to_invalidate);
+
+    // tip_to_invalidate just got invalidated, so it's BLOCK_FAILED_VALID
+    WITH_LOCK(::cs_main, assert(tip_to_invalidate->nStatus.isInvalid()));
+    WITH_LOCK(::cs_main, assert(!tip_to_invalidate->nStatus.hasFailedParent()));
+
+    // check all ancestors of the invalidated block are validated up to
+    // BLOCK_VALID_TRANSACTIONS and are not invalid
+    auto pindex = tip_to_invalidate->pprev;
+    while (pindex) {
+        WITH_LOCK(::cs_main,
+                  assert(pindex->IsValid(BlockValidity::TRANSACTIONS)));
+        WITH_LOCK(::cs_main, assert(!pindex->nStatus.isInvalid()));
+        pindex = pindex->pprev;
+    }
+
+    // check all descendants of the invalidated block are BLOCK_FAILED_CHILD
+    pindex = orig_tip;
+    while (pindex && pindex != tip_to_invalidate) {
+        WITH_LOCK(::cs_main, assert(!pindex->nStatus.hasFailed()));
+        WITH_LOCK(::cs_main, assert(pindex->nStatus.hasFailedParent()));
+        pindex = pindex->pprev;
+    }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
