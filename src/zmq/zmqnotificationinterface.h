5ea4c4e325b4bd8cf7165f9b2b9c29af48330a05
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-04-01 18:41:54 +0200

    refactor: Remove spurious virtual from final ~CZMQNotificationInterface
    Summary: Backport of [[https://github.com/bitcoin/bitcoin/pull/32187 | core#32187]].
    
    Test Plan:
    With a recent Clang:
      ninja
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D19084

--- src/zmq/zmqnotificationinterface.h
+++ src/zmq/zmqnotificationinterface.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2018 The Bitcoin Core developers
+// Copyright (c) 2015-present The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -16,7 +16,7 @@ class CZMQAbstractNotifier;
 
 class CZMQNotificationInterface final : public CValidationInterface {
 public:
-    virtual ~CZMQNotificationInterface();
+    ~CZMQNotificationInterface();
 
     std::list<const CZMQAbstractNotifier *> GetActiveNotifiers() const;
 
