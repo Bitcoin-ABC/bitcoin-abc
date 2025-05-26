bc71c4e1d4db6a3367aa293b59c38c4d646aca35
Fabien <fabcien@gmail.com>
2025-05-26 10:41:40 +0200

    [chronik] Bump karyon_jsonrpc to version 0.3.2
    Summary: It has better memory management and support for larger messages.
    
    Test Plan: Build and run the chronik tests.
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Subscribers: PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D18148

--- chronik/chronik-http/src/electrum_codec.rs
+++ chronik/chronik-http/src/electrum_codec.rs
@@ -2,9 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-use chronik_util::log_chronik;
 use karyon_jsonrpc::{
-    codec::{Codec, Decoder, Encoder},
+    codec::{ByteBuffer, Codec, Decoder, Encoder},
     error::{Error, Result},
 };
 
@@ -20,27 +19,20 @@ impl Encoder for ElectrumCodec {
     type EnError = Error;
     type EnMessage = serde_json::Value;
 
-    fn encode(&self, src: &Self::EnMessage, dst: &mut [u8]) -> Result<usize> {
+    fn encode(
+        &self,
+        src: &Self::EnMessage,
+        dst: &mut ByteBuffer,
+    ) -> Result<usize> {
         let msg = serde_json::to_string(src)
             .map_err(|err| Error::Encode(err.to_string()))?;
         let buf = msg.as_bytes();
 
-        // Make sure there is enough room allocated in the dst buffer
-        if buf.len() + 1 > dst.len() {
-            log_chronik!(
-                "Electrum encoding error: the message does not fit in the \
-                 encoding buffer\n"
-            );
-            return Err(Error::Encode(
-                "the message does not fit in the encoding buffer".into(),
-            ));
-        }
-
-        dst[..buf.len()].copy_from_slice(buf);
+        dst.extend_from_slice(buf);
         // Fulcrum adds a newline as a separator, so we oblige. This is
         // permitted by the json-rpc standard so this server encoding is still
         // compliant.
-        dst[buf.len()] = b'\n';
+        dst.extend_from_slice(b"\n");
         Ok(buf.len() + 1)
     }
 }
@@ -51,9 +43,9 @@ impl Decoder for ElectrumCodec {
 
     fn decode(
         &self,
-        src: &mut [u8],
+        src: &mut ByteBuffer,
     ) -> Result<Option<(usize, Self::DeMessage)>> {
-        let de = serde_json::Deserializer::from_slice(src);
+        let de = serde_json::Deserializer::from_slice(src.as_ref());
         let mut iter = de.into_iter::<serde_json::Value>();
 
         let item = match iter.next() {
