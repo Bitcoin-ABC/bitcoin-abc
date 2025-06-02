f4c133fdd9761b4a42c5f73ade8b027bc780b4d9
johnkuney <31288518+johnkuney@users.noreply.github.com>
2025-06-02 16:37:41 -0500

    [e.cash] Trusted by section
    Summary:
    Adding the scrolling marquee of logos under the hero section
    
    I currently have it set to only show exchanges that have 80 or higher scorecard.cash score, which is about 12 exchanges atm, but open to showing more if we want
    
    Test Plan: preview the site and check it follows the design mock
    
    Reviewers: #bitcoin_abc, bytesofman
    
    Reviewed By: #bitcoin_abc, bytesofman
    
    Subscribers: bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D18189

---
+++ web/e.cash_v2/app/components/trustedBy.tsx
@@ -0,0 +1,39 @@
+// Copyright (c) 2025 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import ContentContainer from "./contentContainer";
+import Image from "next/image";
+
+const Logos = ["bithumb", "upbit", "binance", "coinex", "indodax"];
+
+export default function TrustedBy() {
+  return (
+    <ContentContainer>
+      <div className="relative m-auto w-full max-w-[300px] text-center text-sm font-light leading-none tracking-wider lg:max-w-none">
+        TRUSTED BY BUSINESSES THAT VALUE{" "}
+        <span className="font-medium">SPEED</span>,{" "}
+        <span className="font-medium">SECURITY</span> AND{" "}
+        <span className="font-medium">SCALABILITY</span>
+      </div>
+      <div className="logo-mask-gradient m-auto mb-20 mt-8 flex w-full select-none items-center overflow-hidden lg:max-w-[70%]">
+        <div className="srcoll-animation lg:gap-22 flex items-center gap-10">
+          {[...Logos, ...Logos].map((item, index) => (
+            <div
+              key={index}
+              className="relative flex h-[30px] w-[100px] shrink-0 items-center lg:h-[30px] lg:w-[130px]"
+            >
+              <Image
+                src={`/${item}.png`}
+                fill
+                className="object-contain"
+                alt={item}
+                priority
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </ContentContainer>
+  );
+}
--- web/e.cash_v2/app/page.tsx
+++ web/e.cash_v2/app/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import GridPattern from "./components/gridPattern";
 import ContentContainer from "./components/contentContainer";
+import TrustedBy from "./components/trustedBy";
 
 export default function Home() {
   type HeroBoxProps = {
@@ -39,87 +40,100 @@ export default function Home() {
     );
   };
   return (
-    <div className="relative w-full py-10 pt-20 lg:pt-12">
-      <ContentContainer>
-        <div className="flex flex-col items-center lg:flex-row">
-          <div className="relative z-10 flex w-full flex-col items-start justify-center self-stretch lg:w-[55%]">
-            <div className="mask-gradient-fade-to-left bg-white/14 absolute top-20 hidden h-[1px] w-full lg:block" />
-            <div className="mask-gradient-fade-to-left top-19.75 absolute hidden h-[1px] w-full bg-black lg:block" />
-            <div className="mask-gradient-fade-to-left bg-white/14 absolute bottom-20 hidden h-[1px] w-full lg:block" />
-            <div className="mask-gradient-fade-to-left bottom-20.25 absolute hidden h-[1px] w-full bg-black lg:block" />
-            <h1 className="mb-2 w-auto text-5xl font-bold tracking-tighter lg:mb-6 lg:w-96 lg:text-6xl lg:leading-[60px]">
-              Cash for the Internet
-            </h1>
-            <p className="lg:w-70 text-lg">
-              Scalable payments to meet the{" "}
-              <span className="text-primaryText">demands of tomorrow.</span>
-            </p>
-          </div>
-          <div className="relative z-20 mt-6 h-[220px] w-[220px] lg:hidden">
-            <div className="absolute bottom-[-100px] left-1/2 z-0 h-[125px] w-[312px] -translate-x-1/2">
+    <main>
+      <div className="relative w-full py-10 pt-20 lg:pt-12">
+        <ContentContainer>
+          <div className="flex flex-col items-center lg:flex-row">
+            <div className="relative z-10 flex w-full flex-col items-start justify-center self-stretch lg:w-[55%]">
+              <div className="mask-gradient-fade-to-left bg-white/14 absolute top-20 hidden h-[1px] w-full lg:block" />
+              <div className="mask-gradient-fade-to-left top-19.75 absolute hidden h-[1px] w-full bg-black lg:block" />
+              <div className="mask-gradient-fade-to-left bg-white/14 absolute bottom-20 hidden h-[1px] w-full lg:block" />
+              <div className="mask-gradient-fade-to-left bottom-20.25 absolute hidden h-[1px] w-full bg-black lg:block" />
+              <h1 className="mb-2 w-auto text-5xl font-bold tracking-tighter lg:mb-6 lg:w-96 lg:text-6xl lg:leading-[60px]">
+                Cash for the Internet
+              </h1>
+              <p className="lg:w-70 text-lg">
+                Scalable payments to meet the{" "}
+                <span className="text-primaryText">demands of tomorrow.</span>
+              </p>
+            </div>
+            <div className="relative z-20 mt-6 h-[220px] w-[220px] lg:hidden">
+              <div className="absolute bottom-[-100px] left-1/2 z-0 h-[125px] w-[312px] -translate-x-1/2">
+                <Image
+                  src="/shadow.png"
+                  alt="eCash"
+                  fill
+                  className="object-contain"
+                />
+              </div>
               <Image
-                src="/shadow.png"
+                src="/logo-hero.png"
                 alt="eCash"
                 fill
                 className="object-contain"
               />
             </div>
-            <Image
-              src="/logo-hero.png"
-              alt="eCash"
-              fill
-              className="object-contain"
-            />
-          </div>
-          <div className="relative z-20 mt-8 flex w-full flex-col items-end lg:my-20 lg:w-[45%]">
-            <div className="absolute right-0 top-[-2px] h-[2px] w-full bg-black" />
+            <div className="relative z-20 mt-8 flex w-full flex-col items-end lg:my-20 lg:w-[45%]">
+              <div className="absolute right-0 top-[-2px] h-[2px] w-full bg-black" />
 
-            <HeroBox
-              title="NAKAMOTO"
-              href="https://avalance.cash"
-              text="The trusted Bitcoin Proof-of-Work consensus forms the foundation
+              <HeroBox
+                title="NAKAMOTO"
+                href="https://avalance.cash"
+                text="The trusted Bitcoin Proof-of-Work consensus forms the foundation
               of the eCash protocol."
-            />
-            <HeroBox
-              title="AVALANCHE"
-              href="https://avalance.cash"
-              text="A breakthrough consensus protocol integrated with eCash's core technology."
-            />
+              />
+              <HeroBox
+                title="AVALANCHE"
+                href="https://avalance.cash"
+                text="A breakthrough consensus protocol integrated with eCash's core technology."
+              />
+            </div>
           </div>
-        </div>
-      </ContentContainer>
-      <GridPattern className="left-1/2 top-16 z-10 -translate-x-[calc(50%+100px)]" />
-      <GridPattern className="mask-gradient-fade-135 bottom-0 right-20 z-30 hidden lg:inline-flex" />
-      <div className="absolute left-0 top-0 z-0 hidden h-[800px] w-[533px] lg:block">
-        <Image src="/tl-blur.jpg" alt="eCash" fill className="object-contain" />
-      </div>
-      <div className="absolute right-0 top-0 z-0 h-[800px] w-[600px]">
-        <Image src="/tr-blur.jpg" alt="eCash" fill className="object-contain" />
-      </div>
-      <div className="absolute left-1/2 top-1/2 z-0 hidden h-[445px] w-[500px] -translate-x-[calc(50%+60px)] -translate-y-[calc(50%-150px)] lg:block">
-        <Image
-          src="/center-blur.jpg"
-          alt="eCash"
-          fill
-          className="object-contain"
-        />
-      </div>
-      <div className="absolute left-1/2 top-1/2 z-10 hidden h-[400px] w-[400px] -translate-x-1/2 -translate-y-1/2 lg:block">
-        <div className="absolute bottom-[-100px] left-1/2 z-0 h-[125px] w-[312px] -translate-x-1/2">
+        </ContentContainer>
+        <GridPattern className="left-1/2 top-16 z-10 -translate-x-[calc(50%+100px)]" />
+        <GridPattern className="mask-gradient-fade-135 bottom-0 right-20 z-30 hidden lg:inline-flex" />
+        <div className="absolute left-0 top-0 z-0 hidden h-[800px] w-[533px] lg:block">
           <Image
-            src="/shadow.png"
+            src="/tl-blur.jpg"
             alt="eCash"
             fill
             className="object-contain"
           />
         </div>
-        <Image
-          src="/logo-hero.png"
-          alt="eCash"
-          fill
-          className="object-contain"
-        />
+        <div className="absolute right-0 top-0 z-0 h-[800px] w-[600px]">
+          <Image
+            src="/tr-blur.jpg"
+            alt="eCash"
+            fill
+            className="object-contain"
+          />
+        </div>
+        <div className="absolute left-1/2 top-1/2 z-0 hidden h-[445px] w-[500px] -translate-x-[calc(50%+60px)] -translate-y-[calc(50%-150px)] lg:block">
+          <Image
+            src="/center-blur.jpg"
+            alt="eCash"
+            fill
+            className="object-contain"
+          />
+        </div>
+        <div className="absolute left-1/2 top-1/2 z-10 hidden h-[400px] w-[400px] -translate-x-1/2 -translate-y-1/2 lg:block">
+          <div className="absolute bottom-[-100px] left-1/2 z-0 h-[125px] w-[312px] -translate-x-1/2">
+            <Image
+              src="/shadow.png"
+              alt="eCash"
+              fill
+              className="object-contain"
+            />
+          </div>
+          <Image
+            src="/logo-hero.png"
+            alt="eCash"
+            fill
+            className="object-contain"
+          />
+        </div>
       </div>
-    </div>
+      <TrustedBy />
+    </main>
   );
 }
