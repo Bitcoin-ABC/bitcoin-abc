b4148cc6057ad41b84fc76ad31c63ffe8a0f95ef
bytesofman <joey@bitcoinabc.org>
2025-06-07 09:20:30 -0700

    Revert "[Cashtab] Add code splitting to improve initial load time"
    Summary:
    This reverts commit 3f8b1475be898f37609576cf5778eaf2efde8607.
    
    I accidentally based this on D18218
    
    it's breaking CI I think due to a rebase conflict in the package.json; has the package-lock.json from D18218 but not the package.json
    
    Test Plan: `npm test`
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D18225

--- cashtab/config/webpack.config.js
+++ cashtab/config/webpack.config.js
@@ -298,27 +298,6 @@ module.exports = function (webpackEnv) {
                 // This is only used in production mode
                 new CssMinimizerPlugin(),
             ],
-            splitChunks: {
-                chunks: 'all',
-                minSize: 20000,
-                minRemainingSize: 0,
-                minChunks: 1,
-                maxAsyncRequests: 30,
-                maxInitialRequests: 30,
-                enforceSizeThreshold: 50000,
-                cacheGroups: {
-                    defaultVendors: {
-                        test: /[\\/]node_modules[\\/]/,
-                        priority: -10,
-                        reuseExistingChunk: true,
-                    },
-                    default: {
-                        minChunks: 2,
-                        priority: -20,
-                        reuseExistingChunk: true,
-                    },
-                },
-            },
         },
         resolve: {
             fallback: {
