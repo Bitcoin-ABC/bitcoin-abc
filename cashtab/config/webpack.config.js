805167c0f07d86ed8308667c0fa482d269305e02
bytesofman <joey@bitcoinabc.org>
2025-06-24 15:49:38 -0700

    [Cashtab] Code splitting only in prod
    Summary:
    `npm start` was broken by code splitting implementation. `npm run build` works fine.
    
    We do not really care about code splitting in dev. Certainly not to the point where we are willing to break `npm start`
    
    So, only code split in prod.
    
    Test Plan: `npm start` works instead of infinite hang with no error msg
    
    Reviewers: #bitcoin_abc, emack
    
    Reviewed By: #bitcoin_abc, emack
    
    Differential Revision: https://reviews.bitcoinabc.org/D18288

--- cashtab/config/webpack.config.js
+++ cashtab/config/webpack.config.js
@@ -298,27 +298,29 @@ module.exports = function (webpackEnv) {
                 // This is only used in production mode
                 new CssMinimizerPlugin(),
             ],
-            splitChunks: {
-                chunks: 'all',
-                minSize: 20000,
-                minRemainingSize: 0,
-                minChunks: 1,
-                maxAsyncRequests: 30,
-                maxInitialRequests: 30,
-                enforceSizeThreshold: 50000,
-                cacheGroups: {
-                    defaultVendors: {
-                        test: /[\\/]node_modules[\\/]/,
-                        priority: -10,
-                        reuseExistingChunk: true,
-                    },
-                    default: {
-                        minChunks: 2,
-                        priority: -20,
-                        reuseExistingChunk: true,
-                    },
-                },
-            },
+            splitChunks: isEnvProduction
+                ? {
+                      chunks: 'all',
+                      minSize: 20000,
+                      minRemainingSize: 0,
+                      minChunks: 1,
+                      maxAsyncRequests: 30,
+                      maxInitialRequests: 30,
+                      enforceSizeThreshold: 50000,
+                      cacheGroups: {
+                          defaultVendors: {
+                              test: /[\\/]node_modules[\\/]/,
+                              priority: -10,
+                              reuseExistingChunk: true,
+                          },
+                          default: {
+                              minChunks: 2,
+                              priority: -20,
+                              reuseExistingChunk: true,
+                          },
+                      },
+                  }
+                : false, // Disable in development,
         },
         resolve: {
             fallback: {
