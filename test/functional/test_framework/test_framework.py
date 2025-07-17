8d19b4b2d4f034f29bc86e4d52aa4c7cc17facf7
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-07-17 13:00:05 +0200

    test: Do not pass tests on unhandled exceptions
    Summary:
    This adds a missing catch for BaseException (e.g. SystemExit), which
    would otherwise be silently ignored.
    
    Also, remove the redundant other catches, which are just calling
    log.exception with a redundant log message.
    
    Log KeyboardInterrupt as exception
    
    log.exception is more verbose and useful to debug timeouts.
    
    Also, log stderr for CalledProcessError to make debugging easier.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/33001 | core#33001]]
    
    Test Plan:
    `ninja check-functional`
    
    Add `assert False` or `import sys; sys.exit("fatal error")` in random tests, see that it causes a failure
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19408

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -150,26 +150,16 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         try:
             self.setup()
             self._run_test_internal()
-        except JSONRPCException:
-            self.log.exception("JSONRPC error")
-            self.success = TestStatus.FAILED
         except SkipTest as e:
             self.log.warning(f"Test Skipped: {e.message}")
             self.success = TestStatus.SKIPPED
-        except AssertionError:
-            self.log.exception("Assertion failed")
-            self.success = TestStatus.FAILED
-        except KeyError:
-            self.log.exception("Key error")
-            self.success = TestStatus.FAILED
         except subprocess.CalledProcessError as e:
-            self.log.exception(f"Called Process failed with '{e.output}'")
+            self.log.exception(
+                f"Called Process failed with stdout='{e.stdout}'; stderr='{e.stderr}';"
+            )
             self.success = TestStatus.FAILED
-        except Exception:
-            self.log.exception("Unexpected exception caught during testing")
-            self.success = TestStatus.FAILED
-        except KeyboardInterrupt:
-            self.log.warning("Exiting after keyboard interrupt")
+        except BaseException:
+            self.log.exception("Unexpected exception")
             self.success = TestStatus.FAILED
         finally:
             exit_code = self.shutdown()
