f6c88f19c76c76f2639f2212ac7935d1781f3bb1
johnkuney <31288518+johnkuney@users.noreply.github.com>
2025-08-05 13:29:07 -0500

    [e.cash] About page - info and quote section
    Summary: Adding the text and quote section to the /about page
    
    Test Plan: preview and check copy
    
    Reviewers: #bitcoin_abc, bytesofman
    
    Reviewed By: #bitcoin_abc, bytesofman
    
    Subscribers: bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D18448

--- web/e.cash_v2/app/about/page.tsx
+++ web/e.cash_v2/app/about/page.tsx
@@ -3,12 +3,14 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import AboutHero from "../components/About/AboutHero";
+import AboutSection from "../components/About/AboutSection";
 
 export default function About() {
   return (
     <main>
       <div className="bg-gradient-to-br from-[#1F1428] via-[#090916] to-[#0F1528]">
         <AboutHero />
+        <AboutSection />
       </div>
     </main>
   );
---
+++ web/e.cash_v2/app/components/About/AboutSection.tsx
@@ -0,0 +1,96 @@
+// Copyright (c) 2025 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+"use client";
+import PlusHeader from "../Atoms/PlusHeader";
+import Button from "../Atoms/Button";
+import ContentContainer from "../Atoms/ContentContainer";
+import Image from "next/image";
+import { motion } from "motion/react";
+
+export default function AboutSection() {
+  return (
+    <div className="py-10 lg:py-16">
+      <ContentContainer className="lg:mb-30 mb-24 max-w-[600px]">
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeInOut", delay: 0.3 }}
+          viewport={{ once: true }}
+        >
+          <PlusHeader text="XEC" />
+
+          <h2 className="mb-4 mt-4 text-4xl font-bold text-white lg:text-5xl">
+            Cash... Optimized
+          </h2>
+
+          <div className="mb-12">
+            <p>
+              eCash integrates the groundbreaking Avalanche protocol with its
+              core Nakamoto consensus. This hybrid-consensus enables unmatched
+              capacity, speed, and security that pushes crypto forward and
+              constitutes a viable alternative to legacy finance and looming
+              CBDCs.
+              <br />
+              <br />
+              The eCash network is at the forefront of Layer-1 blockchain
+              technology, delivering on the promise that set off this entire new
+              crypto-industry: A censorship-resistant electronic cash system at
+              mass scale.
+              <br />
+              <br />
+              Enter eCash. The revolutionary combination of digital cash and
+              sound money that unlocks new value. Join us and take your first
+              step on the path to financial and personal freedom. Let's redefine
+              money, wealth, and liberty together.
+            </p>
+          </div>
+
+          <div className="flex justify-start gap-4">
+            <Button href="/roadmap" variant="white" className="m-0">
+              Roadmap
+            </Button>
+            <Button href="/tech" variant="outline" className="m-0">
+              Tech
+            </Button>
+          </div>
+        </motion.div>
+      </ContentContainer>
+      <ContentContainer>
+        {/* Quote Section */}
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeInOut" }}
+          viewport={{ once: true, amount: 0.5 }}
+          className="rounded-4xl flex items-center justify-center overflow-hidden bg-gradient-to-l from-[#0D0F37] via-[#50059A] to-[#0D0F37] p-8 shadow-[inset_0px_2px_5px_0px_#50059A,inset_0px_1px_33px_0px_rgba(111,123,232,0.40)] lg:p-24"
+        >
+          <div className="flex max-w-[850px] flex-col items-start justify-start gap-8">
+            <blockquote className="text-2xl font-bold text-white lg:text-4xl">
+              “I think that the internet is going to be one of the major forces
+              for reducing the role of government. The one thing that's missing,
+              but that will soon be developed, is a reliable e-cash.”
+            </blockquote>
+            <div className="flex items-center gap-3">
+              <div className="relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full">
+                <Image
+                  src="/milton-friedman.png"
+                  alt="Milton Friedman"
+                  fill
+                  className="object-contain"
+                />
+              </div>
+              <div className="flex flex-col items-start justify-start">
+                <span className="text-sm font-medium">Milton Friedman</span>
+                <span className="text-secondaryText text-sm font-light">
+                  Nobel Prize Economist
+                </span>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      </ContentContainer>
+    </div>
+  );
+}
