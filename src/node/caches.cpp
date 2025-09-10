bitcoin-ghost/bitcoin-core-ladder-script

[refactor] init: Simplify coinsdb cache calculation
(total_cache / 4) + (1 << 23) is at least 8 MiB and nMaxCoinsDBCache is
also 8 MiB, so the minimum between the two will always be
nMaxCoinsDBCache. This is just a simplification and not changing the
result of the calculation.

Co-authored-by: Ryan Ofsky <ryan@ofsky.org>

src/node/caches.cpp
@@ -24,8 +24,7 @@ CacheSizes CalculateCacheSizes(const ArgsManager& args, size_t n_indexes)
         sizes.filter_index = max_cache / n_indexes;
         nTotalCache -= sizes.filter_index * n_indexes;
     }
-    sizes.coins_db = std::min(nTotalCache / 2, (nTotalCache / 4) + (1 << 23)); // use 25%-50% of the remainder for disk cache
-    sizes.coins_db = std::min(sizes.coins_db, nMaxCoinsDBCache << 20); // cap total coins db cache
+    sizes.coins_db = std::min(nTotalCache / 2, nMaxCoinsDBCache << 20);
     nTotalCache -= sizes.coins_db;
     sizes.coins = nTotalCache; // the rest goes to in-memory cache
     return sizes;

Bitcoin-ABC/bitcoin-abc

Summary:
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/31483 | core#31483]]
https://github.com/bitcoin/bitcoin/pull/31483/commits/8bd5f8a38ce903c05606841ebed1902398cb0b14
Depends on D18578
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D18579

src/node/caches.cpp
@@ -34,9 +34,7 @@ CacheSizes CalculateCacheSizes(const ArgsManager &args, size_t n_indexes) {
     }
 
     // use 25%-50% of the remainder for disk cache
-    sizes.coins_db = std::min(nTotalCache / 2, (nTotalCache / 4) + (1 << 23));
-    // cap total coins db cache
-    sizes.coins_db = std::min(sizes.coins_db, MAX_COINS_DB_CACHE_MB << 20);
+    sizes.coins_db = std::min(nTotalCache / 2, MAX_COINS_DB_CACHE_MB << 20);
     nTotalCache -= sizes.coins_db;
     // the rest goes to in-memory cache
     sizes.coins = nTotalCache;