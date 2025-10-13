Bitcoin-ABC/bitcoin-abc

commit 30e450064897f5e5be15d4e636640b97a2f2ce4d

[electrum] move get*Filename wrappers out of MainWindow
Summary:
The only thing these wrappers need from the MainWindow is the config, so pass it as a parameter.

Depends on D14537

Test Plan:
In the contacts tab, export contacts to a file. Try to import that file in another wallet.

Double-click on a transaction in the History tab, and click the save button.

Open a transaction from file (Tools > Load Transaction > From file)

Add a few labels to transactions in the History tab, export them (Wallet > Labels > Export), delete some of them again, then reimport and check they are all back

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D14538

electrum/electrumabc_gui/qt/main_window.py
@@ -141,6 +141,8 @@ from .util import (
     destroyed_print_error,
     expiration_values,
     filename_field,
+    getOpenFileName,
+    getSaveFileName,
     rate_limited,
     text_dialog,
 )
@@ -1086,46 +1088,6 @@ class ElectrumWindow(QtWidgets.QMainWindow, MessageBoxMixin, PrintError):
     def notify(self, message):
         self.gui_object.notify(message)
 
-    # custom wrappers for getOpenFileName and getSaveFileName, that remember the path selected by the user
-    def getOpenFileName(self, title, filter=""):  # noqa: A002
-        return __class__.static_getOpenFileName(
-            title=title, filtr=filter, config=self.config, parent=self
-        )
-
-    def getSaveFileName(self, title, filename, filter=""):  # noqa: A002
-        return __class__.static_getSaveFileName(
-            title=title,
-            filename=filename,
-            filtr=filter,
-            config=self.config,
-            parent=self,
-        )
-
-    @staticmethod
-    def static_getOpenFileName(*, title, parent=None, config=None, filtr=""):
-        if not config:
-            config = get_config()
-        userdir = os.path.expanduser("~")
-        directory = config.get("io_dir", userdir) if config else userdir
-        fileName, __ = QtWidgets.QFileDialog.getOpenFileName(
-            parent, title, directory, filtr
-        )
-        if fileName and directory != os.path.dirname(fileName) and config:
-            config.set_key("io_dir", os.path.dirname(fileName), True)
-        return fileName
-
-    @staticmethod
-    def static_getSaveFileName(*, title, filename, parent=None, config=None, filtr=""):
-        if not config:
-            config = get_config()
-        userdir = os.path.expanduser("~")
-        directory = config.get("io_dir", userdir) if config else userdir
-        path = os.path.join(directory, filename)
-        fileName, __ = QtWidgets.QFileDialog.getSaveFileName(parent, title, path, filtr)
-        if fileName and directory != os.path.dirname(fileName) and config:
-            config.set_key("io_dir", os.path.dirname(fileName), True)
-        return fileName
-
     def timer_actions(self):
         # Note this runs in the GUI thread
 
@@ -1680,8 +1642,8 @@ class ElectrumWindow(QtWidgets.QMainWindow, MessageBoxMixin, PrintError):
             self.show_error(str(e))
             return
         name = r["id"] + ".bip70"
-        fileName = self.getSaveFileName(
-            _("Select where to save your payment request"), name, "*.bip70"
+        fileName = getSaveFileName(
+            _("Select where to save your payment request"), name, self.config, "*.bip70"
         )
         if fileName:
             with open(fileName, "wb+") as f:
@@ -3248,7 +3210,7 @@ class ElectrumWindow(QtWidgets.QMainWindow, MessageBoxMixin, PrintError):
 
         def do_export():
             ext = pr.export_file_ext()
-            fn = self.getSaveFileName(_("Save invoice to file"), "*." + ext)
+            fn = getSaveFileName(_("Save invoice to file"), "*." + ext, self.config)
             if not fn:
                 return
             with open(fn, "wb") as f:
@@ -3809,7 +3771,9 @@ class ElectrumWindow(QtWidgets.QMainWindow, MessageBoxMixin, PrintError):
             )
 
     def do_process_from_file(self):
-        fileName = self.getOpenFileName(_("Select your transaction file"), "*.txn")
+        fileName = getOpenFileName(
+            _("Select your transaction file"), self.config, "*.txn"
+        )
         if not fileName:
             return
         try:
@@ -4191,7 +4155,7 @@ class ElectrumWindow(QtWidgets.QMainWindow, MessageBoxMixin, PrintError):
                 f.write(json.dumps(pklist, indent=4))
 
     def do_import_labels(self):
-        labelsFile = self.getOpenFileName(_("Open labels file"), "*.json")
+        labelsFile = getOpenFileName(_("Open labels file"), self.config, "*.json")
         if not labelsFile:
             return
         try:
@@ -4228,9 +4192,10 @@ class ElectrumWindow(QtWidgets.QMainWindow, MessageBoxMixin, PrintError):
     def do_export_labels(self):
         labels = self.wallet.labels
         try:
-            fileName = self.getSaveFileName(
+            fileName = getSaveFileName(
                 _("Select file to save your labels"),
                 f"{SCRIPT_NAME}_labels.json",
+                self.config,
                 "*.json",
             )
             if fileName:

electrum/electrumabc_gui/qt/contact_list.py
@@ -52,6 +52,7 @@ from .util import (
     OkButton,
     WindowModalDialog,
     char_width_in_lineedit,
+    getSaveFileName,
     rate_limited,
     webopen,
 )
@@ -167,9 +168,10 @@ class ContactList(PrintError, MessageBoxMixin, MyTreeWidget):
             self.show_error(_("Your contact list is empty."))
             return
         try:
-            fileName = self.main_window.getSaveFileName(
+            fileName = getSaveFileName(
                 _("Select file to save your contacts"),
                 f"{SCRIPT_NAME}-contacts.json",
+                self.config,
                 "*.json",
             )
             if fileName:

electrum/electrumabc_gui/qt/transaction_dialog.py
@@ -52,6 +52,7 @@ from .util import (
     CopyButton,
     MessageBoxMixin,
     TextBrowserKeyboardFocusFilter,
+    getSaveFileName,
     rate_limited,
     webopen,
 )
@@ -494,8 +495,11 @@ class TxDialog(QtWidgets.QDialog, MessageBoxMixin, PrintError):
             if self.tx.is_complete()
             else "unsigned.txn"
         )
-        fileName = self.main_window.getSaveFileName(
-            _("Select where to save your signed transaction"), name, "*.txn"
+        fileName = getSaveFileName(
+            _("Select where to save your signed transaction"),
+            name,
+            self.main_window.config,
+            "*.txn",
         )
         if fileName:
             tx_dict = self.tx.as_dict()

electrum/electrumabc_gui/qt/util.py
@@ -26,6 +26,7 @@ from PyQt5.QtGui import (
 )
 
 from electrumabc.printerror import PrintError, print_error
+from electrumabc.simple_config import SimpleConfig
 from electrumabc.util import Weak, finalization_print_error
 
 if platform.system() == "Windows":
@@ -1287,6 +1288,29 @@ def char_width_in_lineedit() -> int:
     char_width = QFontMetrics(QtWidgets.QLineEdit().font()).averageCharWidth()
     # 'averageCharWidth' seems to underestimate on Windows, hence 'max()'
     return max(9, char_width)
+
+
+# custom wrappers for getOpenFileName and getSaveFileName, that remember the path
+# selected by the user
+def getOpenFileName(title, config: SimpleConfig, filtr="", parent=None):
+    userdir = os.path.expanduser("~")
+    directory = config.get("io_dir", userdir)
+    fileName, __ = QtWidgets.QFileDialog.getOpenFileName(
+        parent, title, directory, filtr
+    )
+    if fileName and directory != os.path.dirname(fileName):
+        config.set_key("io_dir", os.path.dirname(fileName), True)
+    return fileName
+
+
+def getSaveFileName(title, filename, config: SimpleConfig, filtr="", parent=None):
+    userdir = os.path.expanduser("~")
+    directory = config.get("io_dir", userdir)
+    path = os.path.join(directory, filename)
+    fileName, __ = QtWidgets.QFileDialog.getSaveFileName(parent, title, path, filtr)
+    if fileName and directory != os.path.dirname(fileName):
+        config.set_key("io_dir", os.path.dirname(fileName), True)
+    return fileName
 
 
 if __name__ == "__main__":

commit 83ed6e9da2c70a098ac4102faab1a554332efd58

[electrum] fix save QR code feature
Summary:
This was broken in D14538 when ElectrumWindow.static_getOpenFileName was removed

fixes https://github.com/Bitcoin-ABC/bitcoin-abc/issues/589

Depends on D18781

Test Plan: in the receive tab, right click on the QR code, click Save to file

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D18782

electrum/electrumabc_gui/qt/main_window.py
@@ -1694,7 +1694,7 @@ class ElectrumWindow(QtWidgets.QMainWindow, MessageBoxMixin, PrintError):
 
     def show_qr_window(self):
         if not self.qr_window:
-            self.qr_window = qrwindow.QRWindow()
+            self.qr_window = qrwindow.QRWindow(self.config)
             self.qr_window.setAttribute(Qt.WA_DeleteOnClose, True)
             weakSelf = Weak.ref(self)
 

electrum/electrumabc_gui/qt/qrcodewidget.py
@@ -6,8 +6,9 @@ from qtpy.QtGui import QBrush, QColor, QCursor, QPainter, QPen
 from electrumabc import util
 from electrumabc.i18n import _
 from electrumabc.printerror import PrintError
+from electrumabc.simple_config import SimpleConfig
 
-from .util import CloseButton, MessageBoxMixin, WindowModalDialog
+from .util import CloseButton, MessageBoxMixin, WindowModalDialog, getSaveFileName
 
 
 class QRCodeWidget(QtWidgets.QWidget, PrintError):
@@ -108,16 +109,15 @@ class QRCodeWidget(QtWidgets.QWidget, PrintError):
         del qp
 
 
-def save_to_file(qrw, parent):
-    from .main_window import ElectrumWindow
-
+def save_to_file(qrw, parent, config: SimpleConfig):
     p = qrw and qrw.grab()
     if p and not p.isNull():
-        filename = ElectrumWindow.static_getSaveFileName(
+        filename = getSaveFileName(
             title=_("Save QR Image"),
             filename="qrcode.png",
+            config=config,
             parent=parent,
-            filter="*.png",
+            filtr="*.png",
         )
         if filename:
             p.save(filename, "png")

electrum/electrumabc_gui/qt/qrwindow.py
@@ -32,6 +32,7 @@ from qtpy.QtCore import Qt
 from electrumabc.amount import format_amount
 from electrumabc.constants import PROJECT_NAME
 from electrumabc.i18n import _
+from electrumabc.simple_config import SimpleConfig
 from electrumabc.util import Weak
 
 from .qrcodewidget import QRCodeWidget, copy_to_clipboard, save_to_file
@@ -42,12 +43,13 @@ if TYPE_CHECKING:
 
 
 class QRWindow(QtWidgets.QWidget, MessageBoxMixin):
-    def __init__(self):
+    def __init__(self, config: SimpleConfig):
         # Top-level window.
         super().__init__()
         self.setWindowTitle(f"{PROJECT_NAME} - " + _("Payment Request"))
         self.label = ""
         self.amount = 0
+        self.config = config
         self.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         self.setSizePolicy(
             QtWidgets.QSizePolicy.Policy.MinimumExpanding,
@@ -94,7 +96,7 @@ class QRWindow(QtWidgets.QWidget, MessageBoxMixin):
         weakQ = Weak.ref(self.qrw)
         weakBut = Weak.ref(copyBut)
         copyBut.clicked.connect(lambda: copy_to_clipboard(weakQ(), weakBut()))
-        saveBut.clicked.connect(lambda: save_to_file(weakQ(), weakSelf()))
+        saveBut.clicked.connect(lambda: save_to_file(weakQ(), weakSelf(), self.config))
 
     def set_content(
         self,