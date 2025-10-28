5188b0127b2506dab82aec53c5256cc00a8e965f
fanquake <fanquake@gmail.com>
2025-10-28 13:09:05 +0000

    guix: disable libsanitizer in Linux GCC build
    Summary:
    This causes issues when building against newer glibcs (i.e 2.42), and isn't needed
    in any case.
    
    ```bash
    ../../../../gcc-14.3.0/libsanitizer/sanitizer_common/sanitizer_platform_limits_posix.cpp:483:31: error: invalid application of ‘sizeof’ to incomplete type ‘__sanitizer::termio’
      483 |   unsigned struct_termio_sz = sizeof(struct termio);
          |                               ^~~~~~~~~~~~~~~~~~~~~
    ```
    
    Extracted from #25573.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/33780 | core#33780]]
    Depends on D20559
    
    Test Plan: guix builds
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D20562

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -453,6 +453,7 @@ inspecting signatures in Mach-O binaries.")
                   "--enable-standard-branch-protection=yes",
                   "--enable-cet=yes",
                   "--disable-gcov",
+                  "--disable-libsanitizer",
                   building-on)))
         ((#:phases phases)
           `(modify-phases ,phases
