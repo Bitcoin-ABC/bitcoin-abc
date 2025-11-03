TheCharlatan/bitcoin

Expose CSignatureCache class in header
This is done in preparation for the following commit.

src/script/sigcache.cpp
@@ -5,83 +5,62 @@
 
 #include <script/sigcache.h>
 
-#include <common/system.h>
+#include <crypto/sha256.h>
 #include <logging.h>
 #include <pubkey.h>
 #include <random.h>
+#include <script/interpreter.h>
+#include <span.h>
 #include <uint256.h>
 
-#include <cuckoocache.h>
-
-#include <algorithm>
 #include <mutex>
 #include <optional>
 #include <shared_mutex>
 #include <vector>
 
-namespace {
-/**
- * Valid signature cache, to avoid doing expensive ECDSA signature checking
- * twice for every transaction (once when accepted into memory pool, and
- * again when accepted into the block chain)
- */
-class CSignatureCache
+CSignatureCache::CSignatureCache()
 {
-private:
-     //! Entries are SHA256(nonce || 'E' or 'S' || 31 zero bytes || signature hash || public key || signature):
-    CSHA256 m_salted_hasher_ecdsa;
-    CSHA256 m_salted_hasher_schnorr;
-    typedef CuckooCache::cache<uint256, SignatureCacheHasher> map_type;
-    map_type setValid;
-    std::shared_mutex cs_sigcache;
+    uint256 nonce = GetRandHash();
+    // We want the nonce to be 64 bytes long to force the hasher to process
+    // this chunk, which makes later hash computations more efficient. We
+    // just write our 32-byte entropy, and then pad with 'E' for ECDSA and
+    // 'S' for Schnorr (followed by 0 bytes).
+    static constexpr unsigned char PADDING_ECDSA[32] = {'E'};
+    static constexpr unsigned char PADDING_SCHNORR[32] = {'S'};
+    m_salted_hasher_ecdsa.Write(nonce.begin(), 32);
+    m_salted_hasher_ecdsa.Write(PADDING_ECDSA, 32);
+    m_salted_hasher_schnorr.Write(nonce.begin(), 32);
+    m_salted_hasher_schnorr.Write(PADDING_SCHNORR, 32);
+}
 
-public:
-    CSignatureCache()
-    {
-        uint256 nonce = GetRandHash();
-        // We want the nonce to be 64 bytes long to force the hasher to process
-        // this chunk, which makes later hash computations more efficient. We
-        // just write our 32-byte entropy, and then pad with 'E' for ECDSA and
-        // 'S' for Schnorr (followed by 0 bytes).
-        static constexpr unsigned char PADDING_ECDSA[32] = {'E'};
-        static constexpr unsigned char PADDING_SCHNORR[32] = {'S'};
-        m_salted_hasher_ecdsa.Write(nonce.begin(), 32);
-        m_salted_hasher_ecdsa.Write(PADDING_ECDSA, 32);
-        m_salted_hasher_schnorr.Write(nonce.begin(), 32);
-        m_salted_hasher_schnorr.Write(PADDING_SCHNORR, 32);
-    }
+void CSignatureCache::ComputeEntryECDSA(uint256& entry, const uint256 &hash, const std::vector<unsigned char>& vchSig, const CPubKey& pubkey) const
+{
+    CSHA256 hasher = m_salted_hasher_ecdsa;
+    hasher.Write(hash.begin(), 32).Write(pubkey.data(), pubkey.size()).Write(vchSig.data(), vchSig.size()).Finalize(entry.begin());
+}
 
-    void
-    ComputeEntryECDSA(uint256& entry, const uint256 &hash, const std::vector<unsigned char>& vchSig, const CPubKey& pubkey) const
-    {
-        CSHA256 hasher = m_salted_hasher_ecdsa;
-        hasher.Write(hash.begin(), 32).Write(pubkey.data(), pubkey.size()).Write(vchSig.data(), vchSig.size()).Finalize(entry.begin());
-    }
+void CSignatureCache::ComputeEntrySchnorr(uint256& entry, const uint256 &hash, Span<const unsigned char> sig, const XOnlyPubKey& pubkey) const
+{
+    CSHA256 hasher = m_salted_hasher_schnorr;
+    hasher.Write(hash.begin(), 32).Write(pubkey.data(), pubkey.size()).Write(sig.data(), sig.size()).Finalize(entry.begin());
+}
 
-    void
-    ComputeEntrySchnorr(uint256& entry, const uint256 &hash, Span<const unsigned char> sig, const XOnlyPubKey& pubkey) const
-    {
-        CSHA256 hasher = m_salted_hasher_schnorr;
-        hasher.Write(hash.begin(), 32).Write(pubkey.data(), pubkey.size()).Write(sig.data(), sig.size()).Finalize(entry.begin());
-    }
+bool CSignatureCache::Get(const uint256& entry, const bool erase)
+{
+    std::shared_lock<std::shared_mutex> lock(cs_sigcache);
+    return setValid.contains(entry, erase);
+}
 
-    bool
-    Get(const uint256& entry, const bool erase)
-    {
-        std::shared_lock<std::shared_mutex> lock(cs_sigcache);
-        return setValid.contains(entry, erase);
-    }
+void CSignatureCache::Set(const uint256& entry)
+{
+    std::unique_lock<std::shared_mutex> lock(cs_sigcache);
+    setValid.insert(entry);
+}
 
-    void Set(const uint256& entry)
-    {
-        std::unique_lock<std::shared_mutex> lock(cs_sigcache);
-        setValid.insert(entry);
-    }
-    std::optional<std::pair<uint32_t, size_t>> setup_bytes(size_t n)
-    {
-        return setValid.setup_bytes(n);
-    }
-};
+std::optional<std::pair<uint32_t, size_t>> CSignatureCache::setup_bytes(size_t n)
+{
+    return setValid.setup_bytes(n);
+}
 
 /* In previous versions of this code, signatureCache was a local static variable
  * in CachingTransactionSignatureChecker::VerifySignature.  We initialize
@@ -90,7 +69,6 @@ public:
  * signatureCache could be made local to VerifySignature.
 */
 static CSignatureCache signatureCache;
-} // namespace
 
 // To be called once in AppInitMain/BasicTestingSetup to initialize the
 // signatureCache.

Bitcoin-ABC/bitcoin-abc

Summary:
This is done in preparation for the following commit. Also rename it to
SignatureCache.
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/30141 | core#30141]]
https://github.com/bitcoin/bitcoin/pull/30141/commits/66d74bfc45ae0f743084475ac3bbfb4355bb6ec2
Depends on D18858
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Subscribers: Fabien
Differential Revision: https://reviews.bitcoinabc.org/D18859

src/script/sigcache.cpp
@@ -5,68 +5,50 @@
 
 #include <script/sigcache.h>
 
-#include <common/system.h>
-#include <cuckoocache.h>
+#include <crypto/sha256.h>
 #include <logging.h>
 #include <pubkey.h>
 #include <random.h>
+#include <script/interpreter.h>
+#include <span.h>
 #include <uint256.h>
 
-#include <algorithm>
 #include <mutex>
 #include <shared_mutex>
 #include <vector>
 
-namespace {
+SignatureCache::SignatureCache() {
+    uint256 nonce = GetRandHash();
+    // We want the nonce to be 64 bytes long to force the hasher to process
+    // this chunk, which makes later hash computations more efficient. We
+    // just write our 32-byte entropy twice to fill the 64 bytes.
+    m_salted_hasher.Write(nonce.begin(), 32);
+    m_salted_hasher.Write(nonce.begin(), 32);
+}
 
-/**
- * Valid signature cache, to avoid doing expensive ECDSA signature checking
- * twice for every transaction (once when accepted into memory pool, and
- * again when accepted into the block chain)
- */
-class CSignatureCache {
-private:
-    //! Entries are SHA256(nonce || signature hash || public key || signature):
-    CSHA256 m_salted_hasher;
-    typedef CuckooCache::cache<CuckooCache::KeyOnly<uint256>,
-                               SignatureCacheHasher>
-        map_type;
-    map_type setValid;
-    std::shared_mutex cs_sigcache;
+void SignatureCache::ComputeEntry(uint256 &entry, const uint256 &hash,
+                                  const std::vector<uint8_t> &vchSig,
+                                  const CPubKey &pubkey) const {
+    CSHA256 hasher = m_salted_hasher;
+    hasher.Write(hash.begin(), 32)
+        .Write(pubkey.data(), pubkey.size())
+        .Write(vchSig.data(), vchSig.size())
+        .Finalize(entry.begin());
+}
 
-public:
-    CSignatureCache() {
-        uint256 nonce = GetRandHash();
-        // We want the nonce to be 64 bytes long to force the hasher to process
-        // this chunk, which makes later hash computations more efficient. We
-        // just write our 32-byte entropy twice to fill the 64 bytes.
-        m_salted_hasher.Write(nonce.begin(), 32);
-        m_salted_hasher.Write(nonce.begin(), 32);
-    }
+bool SignatureCache::Get(const uint256 &entry, const bool erase) {
+    std::shared_lock<std::shared_mutex> lock(cs_sigcache);
+    return setValid.contains(entry, erase);
+}
 
-    void ComputeEntry(uint256 &entry, const uint256 &hash,
-                      const std::vector<uint8_t> &vchSig,
-                      const CPubKey &pubkey) {
-        CSHA256 hasher = m_salted_hasher;
-        hasher.Write(hash.begin(), 32)
-            .Write(pubkey.data(), pubkey.size())
-            .Write(vchSig.data(), vchSig.size())
-            .Finalize(entry.begin());
-    }
+void SignatureCache::Set(const uint256 &entry) {
+    std::unique_lock<std::shared_mutex> lock(cs_sigcache);
+    setValid.insert(entry);
+}
 
-    bool Get(const uint256 &entry, const bool erase) {
-        std::shared_lock<std::shared_mutex> lock(cs_sigcache);
-        return setValid.contains(entry, erase);
-    }
-
-    void Set(const uint256 &entry) {
-        std::unique_lock<std::shared_mutex> lock(cs_sigcache);
-        setValid.insert(entry);
-    }
-    std::pair<uint32_t, size_t> setup_bytes(size_t n) {
-        return setValid.setup_bytes(n);
-    }
-};
+std::pair<uint32_t, size_t> SignatureCache::setup_bytes(size_t n) {
+    return setValid.setup_bytes(n);
+}
 
 /**
  * In previous versions of this code, signatureCache was a local static variable
@@ -75,8 +57,7 @@ public:
  * call overhead associated with local static variables even though
  * signatureCache could be made local to VerifySignature.
  */
-static CSignatureCache signatureCache;
-} // namespace
+static SignatureCache signatureCache;
 
 // To be called once in AppInitMain/BasicTestingSetup to initialize the
 // signatureCache.