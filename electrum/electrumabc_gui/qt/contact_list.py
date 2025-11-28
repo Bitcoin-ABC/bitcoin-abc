Bitcoin-ABC/bitcoin-abc

commit 9d5a6f042e4dffd3121f090bdbc68b24133c3db1

[electrum] add a copy_to_clipboard util function
Summary:
This has three benefits:
- factor some boilerplate code
- reduce the dependency on `self.main_window` in the address tab
- remove a `QtWidgets.app` reference that does not work with python6

Test Plan:
Go the the address tab, right click on an address and try the 2 copy actions. Test this also with the env var `QT_API=pyqt6`
Open a transaction dialog (right click on a tx in the history and click Details), select some text in the input widget, right click and "Copy Selected Text"

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Subscribers: Fabien

Differential Revision: https://reviews.bitcoinabc.org/D18344

electrum/electrumabc_gui/qt/contact_list.py
@@ -52,6 +52,7 @@ from .util import (
     OkButton,
     WindowModalDialog,
     char_width_in_lineedit,
+    copy_to_clipboard,
     getSaveFileName,
     rate_limited,
     webopen,
@@ -218,9 +219,7 @@ class ContactList(PrintError, MessageBoxMixin, MyTreeWidget):
                 column_title += f" ({len(selected)})"
             menu.addAction(
                 _("Copy {}").format(column_title),
-                lambda: QtWidgets.QApplication.instance()
-                .clipboard()
-                .setText(column_data),
+                copy_to_clipboard(column_data),
             )
             if (
                 item

electrum/electrumabc_gui/qt/address_list.py
@@ -49,6 +49,7 @@ from .util import (
     MONOSPACE_FONT,
     ColorScheme,
     SortableTreeWidgetItem,
+    copy_to_clipboard,
     rate_limited,
     webopen,
 )
@@ -299,7 +300,7 @@ class AddressList(MyTreeWidget):
 
         def doCopy(txt):
             txt = txt.strip()
-            self.main_window.copy_to_clipboard(txt)
+            copy_to_clipboard(txt, widget=self)
 
         col = self.currentColumn()
         column_title = self.headerItem().text(col)

electrum/electrumabc_gui/qt/main_window.py
@@ -131,6 +131,7 @@ from .util import (
     WaitingDialog,
     WindowModalDialog,
     WWLabel,
+    copy_to_clipboard,
     destroyed_print_error,
     expiration_values,
     filename_field,
@@ -1450,8 +1451,8 @@ class ElectrumWindow(QtWidgets.QMainWindow, MessageBoxMixin, PrintError):
         def on_copy_uri():
             if self.receive_qr.data:
                 uri = str(self.receive_qr.data)
-                self.copy_to_clipboard(
-                    uri, _("Receive request URI copied to clipboard"), uribut
+                copy_to_clipboard(
+                    uri, uribut, _("Receive request URI copied to clipboard")
                 )
 
         but.clicked.connect(on_copy_uri)
@@ -3768,12 +3769,6 @@ class ElectrumWindow(QtWidgets.QMainWindow, MessageBoxMixin, PrintError):
         d = ScanBeyondGap(self)
         d.exec_()
         d.setParent(None)  # help along Python by dropping refct to 0
-
-    def copy_to_clipboard(self, text, tooltip=None, widget=None):
-        tooltip = tooltip or _("Text copied to clipboard")
-        widget = widget or self
-        QtWidgets.qApp.clipboard().setText(text)
-        QtWidgets.QToolTip.showText(QCursor.pos(), tooltip, widget)
 
     def _pick_address(self, *, title=None, icon=None) -> Address:
         """Returns None on user cancel, or a valid is_mine Address object

electrum/electrumabc_gui/qt/transaction_dialog.py
@@ -33,7 +33,7 @@ from typing import TYPE_CHECKING
 
 from qtpy import QtWidgets
 from qtpy.QtCore import Qt, QTimer, QUrl, Signal
-from qtpy.QtGui import QBrush, QCursor, QFont, QIcon, QKeySequence, QTextCharFormat
+from qtpy.QtGui import QBrush, QFont, QIcon, QKeySequence, QTextCharFormat
 
 from electrumabc import web
 from electrumabc.address import Address, PublicKey, ScriptOutput
@@ -54,6 +54,7 @@ from .util import (
     CopyButton,
     MessageBoxMixin,
     TextBrowserKeyboardFocusFilter,
+    copy_to_clipboard,
     getSaveFileName,
     rate_limited,
     webopen,
@@ -950,16 +951,6 @@ class TxDialog(QtWidgets.QDialog, MessageBoxMixin, PrintError):
         self.recv_legend.setVisible(bool(rec_ct))
         self.change_legend.setVisible(bool(chg_ct))
 
-    @staticmethod
-    def _copy_to_clipboard(text, widget):
-        if not text and isinstance(widget, QtWidgets.QTextEdit):
-            widget.copy()
-        else:
-            QtWidgets.qApp.clipboard().setText(text)
-        QtWidgets.QToolTip.showText(
-            QCursor.pos(), _("Text copied to clipboard"), widget
-        )
-
     def _open_internal_link(self, target):
         """accepts either a str txid, str address, or a QUrl which should be
         of the bare form "txid" and/or "address" -- used by the clickable
@@ -1008,7 +999,7 @@ class TxDialog(QtWidgets.QDialog, MessageBoxMixin, PrintError):
                     copy_list += [
                         (
                             _("Copy Prevout"),
-                            lambda: self._copy_to_clipboard(utxo, i_text),
+                            lambda: copy_to_clipboard(utxo, i_text),
                         )
                     ]
                 addr = inp.get("address")
@@ -1020,7 +1011,7 @@ class TxDialog(QtWidgets.QDialog, MessageBoxMixin, PrintError):
                     copy_list += [
                         (
                             _("Copy Amount"),
-                            lambda: self._copy_to_clipboard(value_fmtd, i_text),
+                            lambda: copy_to_clipboard(value_fmtd, i_text),
                         )
                     ]
         except (TypeError, ValueError, IndexError, KeyError, AttributeError) as e:
@@ -1038,7 +1029,7 @@ class TxDialog(QtWidgets.QDialog, MessageBoxMixin, PrintError):
         if self.i_text_has_selection:
             # Add this if they have a selection
             menu.addAction(
-                _("Copy Selected Text"), lambda: self._copy_to_clipboard(None, i_text)
+                _("Copy Selected Text"), lambda: copy_to_clipboard(widget=i_text)
             )
         menu.addAction(_("Select All"), i_text.selectAll)
         menu.exec_(global_pos)
@@ -1061,9 +1052,7 @@ class TxDialog(QtWidgets.QDialog, MessageBoxMixin, PrintError):
                 action_text = _("Copy Public Key")
             else:
                 action_text = _("Copy Address")
-            copy_list += [
-                (action_text, lambda: self._copy_to_clipboard(addr_text, widget))
-            ]
+            copy_list += [(action_text, lambda: copy_to_clipboard(addr_text, widget))]
             # also add script hex copy to clipboard
             if isinstance(addr, ScriptOutput):
                 hex_text = addr.to_script().hex() or ""
@@ -1071,7 +1060,7 @@ class TxDialog(QtWidgets.QDialog, MessageBoxMixin, PrintError):
                     copy_list += [
                         (
                             _("Copy Script Hex"),
-                            lambda: self._copy_to_clipboard(hex_text, widget),
+                            lambda: copy_to_clipboard(hex_text, widget),
                         )
                     ]
 
@@ -1100,7 +1089,7 @@ class TxDialog(QtWidgets.QDialog, MessageBoxMixin, PrintError):
                 copy_list += [
                     (
                         _("Copy Amount"),
-                        lambda: self._copy_to_clipboard(value_fmtd, o_text),
+                        lambda: copy_to_clipboard(value_fmtd, o_text),
                     )
                 ]
         except (TypeError, ValueError, IndexError, KeyError, AttributeError) as e:
@@ -1118,7 +1107,7 @@ class TxDialog(QtWidgets.QDialog, MessageBoxMixin, PrintError):
         if self.o_text_has_selection:
             # Add this if they have a selection
             menu.addAction(
-                _("Copy Selected Text"), lambda: self._copy_to_clipboard(None, o_text)
+                _("Copy Selected Text"), lambda: copy_to_clipboard(widget=o_text)
             )
         menu.addAction(_("Select All"), o_text.selectAll)
         menu.exec_(global_pos)

electrum/electrumabc_gui/qt/util.py
@@ -736,8 +736,7 @@ class OverlayControlMixin:
         return self.addButton(":icons/copy.png", self.on_copy, _("Copy to clipboard"))
 
     def on_copy(self):
-        QtWidgets.QApplication.instance().clipboard().setText(self.text())
-        QtWidgets.QToolTip.showText(QCursor.pos(), _("Text copied to clipboard"), self)
+        copy_to_clipboard(self.text(), self)
 
     def keyPressEvent(self, e):
         if not self.hasFocus():
@@ -1314,6 +1313,21 @@ def getSaveFileName(title, filename, config: SimpleConfig, filtr="", parent=None
     if fileName and directory != os.path.dirname(fileName):
         config.set_key("io_dir", os.path.dirname(fileName), True)
     return fileName
+
+
+def copy_to_clipboard(
+    text: str = "", widget: Optional[QtWidgets.QWidget] = None, tooltip: str = ""
+):
+    """Copy text to clipboard and show a tooltip.
+    If text is not specified and widget is a QTextEdit, copy the selected text in the widget.
+    """
+    tooltip = tooltip or _("Text copied to clipboard")
+    if not text and isinstance(widget, QtWidgets.QTextEdit):
+        widget.copy()
+    else:
+        QtWidgets.QApplication.instance().clipboard().setText(text)
+    if widget is not None:
+        QtWidgets.QToolTip.showText(QCursor.pos(), tooltip, widget)
 
 
 if __name__ == "__main__":

electrum/electrumabc_gui/qt/utxo_list.py
@@ -44,7 +44,13 @@ from electrumabc.wallet import ImportedAddressWallet, ImportedPrivkeyWallet
 from .avalanche.proof_editor import AvaProofDialog
 from .consolidate_coins_dialog import ConsolidateCoinsWizard
 from .tree_widget import MyTreeWidget
-from .util import MONOSPACE_FONT, ColorScheme, SortableTreeWidgetItem, rate_limited
+from .util import (
+    MONOSPACE_FONT,
+    ColorScheme,
+    SortableTreeWidgetItem,
+    copy_to_clipboard,
+    rate_limited,
+)
 
 if TYPE_CHECKING:
     from .main_window import ElectrumWindow
@@ -349,16 +355,12 @@ class UTXOList(MyTreeWidget):
                 copy_text = copy_text.strip()
             menu.addAction(
                 _("Copy {}").format(column_title),
-                lambda: QtWidgets.QApplication.instance()
-                .clipboard()
-                .setText(copy_text),
+                lambda: copy_to_clipboard(copy_text),
             )
             if alt_copy_text and alt_column_title:
                 menu.addAction(
                     _("Copy {}").format(alt_column_title),
-                    lambda: QtWidgets.QApplication.instance()
-                    .clipboard()
-                    .setText(alt_copy_text),
+                    lambda: copy_to_clipboard(alt_copy_text),
                 )
 
             # single selection, offer them the "Details" option and also

commit 317043f74951687b268741d66ee8282306f51999

[electrum] fix context menu in Contacts tab
Summary: Broken in D18344

Test Plan: In the contacts tab, check that a right click opens the context menu (no error dialog), and that "Copy Address" works.

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D19006

electrum/electrumabc_gui/qt/contact_list.py
@@ -219,7 +219,7 @@ class ContactList(PrintError, MessageBoxMixin, MyTreeWidget):
                 column_title += f" ({len(selected)})"
             menu.addAction(
                 _("Copy {}").format(column_title),
-                copy_to_clipboard(column_data),
+                lambda: copy_to_clipboard(column_data),
             )
             if (
                 item