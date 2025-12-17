bitcoin-ghost/bitcoin-core-ladder-script

Remove unused GetType() from OverrideStream, CVectorWriter, SpanReader
GetType() is never called, so it is completely unused and can be
removed.

src/net.cpp
@@ -859,7 +859,7 @@ bool V1Transport::SetMessageToSend(CSerializedNetMsg& msg) noexcept
 
     // serialize header
     m_header_to_send.clear();
-    CVectorWriter{SER_NETWORK, INIT_PROTO_VERSION, m_header_to_send, 0, hdr};
+    CVectorWriter{INIT_PROTO_VERSION, m_header_to_send, 0, hdr};
 
     // update state
     m_message_to_send = std::move(msg);

Bitcoin-ABC/bitcoin-abc

Summary:
GetType() is never called, so it is completely unused and can be removed.
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/28508 | core#28508]]
https://github.com/bitcoin/bitcoin/pull/28508/commits/fa4a9c0f4334678fb80358ead667807bf2a0a153
Depends on D19142
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D19143

src/net.cpp
@@ -828,7 +828,7 @@ void V1TransportSerializer::prepareForTransport(
 
     // serialize header
     header.reserve(CMessageHeader::HEADER_SIZE);
-    CVectorWriter{SER_NETWORK, INIT_PROTO_VERSION, header, 0, hdr};
+    CVectorWriter{INIT_PROTO_VERSION, header, 0, hdr};
 }
 
 std::pair<size_t, bool> CConnman::SocketSendData(CNode &node) const {