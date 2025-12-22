65863885c9a4fa5ba5b28ef5bb62471d64320de0
Fabien <fabcien@gmail.com>
2025-12-22 09:48:26 +0100

    [marlin] Make Marlin build to work with pnpm
    Summary:
    This requires some more dev packages and a couple metro/gradle config changes to accomodate the monorepo strategy.
    The most annoying change is figuring out where the hermes binary is located as the path is version dependant.
    To avoid future issue a function is defined that is version agnostic.
    
    Test Plan:
      # Remove all existing node_modules so we don't have any cached install
      git clean -xffd
    
      cd apps/marlin
      pnpm install
      # Below commands assume you did set ANDROID_HOME and have metro running via npx react-native start --reset-cache in another terminal
      pnpm run android
      pnpn run build-android-release-aab
    
    Reviewers: #bitcoin_abc, bytesofman
    
    Reviewed By: #bitcoin_abc, bytesofman
    
    Differential Revision: https://reviews.bitcoinabc.org/D19192

--- apps/marlin-wallet/metro.config.js
+++ apps/marlin-wallet/metro.config.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2025 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
@@ -9,6 +10,16 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const monorepoRoot = path.resolve(__dirname, '../..');
+
+const config = {
+    // Watch the monorepo root for changes
+    watchFolders: [monorepoRoot],
+
+    nodeModulesPaths: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(monorepoRoot, 'node_modules'),
+    ],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
