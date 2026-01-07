bitcoin/bitcoin

refactor,blocks: inline `WriteBlockToDisk`
`WriteBlockToDisk` wasn't really extracting a meaningful subset of the `SaveBlockToDisk` functionality, it's tied closely to the only caller (needs the header size twice, recalculated block serializes size, returns multiple branches, mutates parameter).

The inlined code should only differ in these parts (modernization will be done in other commits):
* renamed `blockPos` to `pos` in `SaveBlockToDisk` to match the parameter name;
* changed `return false` to `return FlatFilePos()`.

Also removed remaining references to `SaveBlockToDisk`.

Co-authored-by: Ryan Ofsky <ryan@ofsky.org>

src/node/blockstorage.cpp
@@ -936,27 +936,6 @@ bool BlockManager::FindUndoPos(BlockValidationState& state, int nFile, FlatFileP
     return true;
 }
 
-bool BlockManager::WriteBlockToDisk(const CBlock& block, FlatFilePos& pos) const
-{
-    // Open history file to append
-    AutoFile fileout{OpenBlockFile(pos)};
-    if (fileout.IsNull()) {
-        LogError("%s: OpenBlockFile failed\n", __func__);
-        return false;
-    }
-
-    // Write index header
-    unsigned int nSize = GetSerializeSize(TX_WITH_WITNESS(block));
-    fileout << GetParams().MessageStart() << nSize;
-
-    // Write block
-    long fileOutPos = fileout.tell();
-    pos.nPos = (unsigned int)fileOutPos;
-    fileout << TX_WITH_WITNESS(block);
-
-    return true;
-}
-
 bool BlockManager::WriteUndoDataForBlock(const CBlockUndo& blockundo, BlockValidationState& state, CBlockIndex& block)
 {
     AssertLockHeld(::cs_main);
@@ -1117,16 +1096,30 @@ FlatFilePos BlockManager::SaveBlockToDisk(const CBlock& block, int nHeight)
     // Account for the 4 magic message start bytes + the 4 length bytes (8 bytes total,
     // defined as BLOCK_SERIALIZATION_HEADER_SIZE)
     nBlockSize += static_cast<unsigned int>(BLOCK_SERIALIZATION_HEADER_SIZE);
-    FlatFilePos blockPos{FindNextBlockPos(nBlockSize, nHeight, block.GetBlockTime())};
-    if (blockPos.IsNull()) {
+    FlatFilePos pos{FindNextBlockPos(nBlockSize, nHeight, block.GetBlockTime())};
+    if (pos.IsNull()) {
         LogError("%s: FindNextBlockPos failed\n", __func__);
         return FlatFilePos();
     }
-    if (!WriteBlockToDisk(block, blockPos)) {
+
+    // Open history file to append
+    AutoFile fileout{OpenBlockFile(pos)};
+    if (fileout.IsNull()) {
+        LogError("%s: OpenBlockFile failed\n", __func__);
         m_opts.notifications.fatalError(_("Failed to write block."));
         return FlatFilePos();
     }
-    return blockPos;
+
+    // Write index header
+    unsigned int nSize = GetSerializeSize(TX_WITH_WITNESS(block));
+    fileout << GetParams().MessageStart() << nSize;
+
+    // Write block
+    long fileOutPos = fileout.tell();
+    pos.nPos = (unsigned int)fileOutPos;
+    fileout << TX_WITH_WITNESS(block);
+
+    return pos;
 }
 
 static auto InitBlocksdirXorKey(const BlockManager::Options& opts)

Bitcoin-ABC/bitcoin-abc

Summary:
Similarly, `WriteBlockToDisk` wasn't really extracting a meaningful subset of the `SaveBlockToDisk` functionality, it's tied closely to the only caller (needs the header size twice, recalculated block serializes size, returns multiple branches, mutates parameter).
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/31490 | core#31490]]
https://github.com/bitcoin/bitcoin/pull/31490/commits/dfb2f9d004860c95fc6f0d4a016a9c038d53a475
Depends on D19235
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D19236

src/node/blockstorage.cpp
@@ -869,33 +869,6 @@ bool BlockManager::FindUndoPos(BlockValidationState &state, int nFile,
     return true;
 }
 
-bool BlockManager::WriteBlockToDisk(
-    const CBlock &block, FlatFilePos &pos,
-    const CMessageHeader::MessageMagic &messageStart) const {
-    // Open history file to append
-    AutoFile fileout{OpenBlockFile(pos)};
-    if (fileout.IsNull()) {
-        LogError("WriteBlockToDisk: OpenBlockFile failed\n");
-        return false;
-    }
-
-    // Write index header
-    unsigned int nSize = GetSerializeSize(block);
-    fileout << messageStart << nSize;
-
-    // Write block
-    long fileOutPos = ftell(fileout.Get());
-    if (fileOutPos < 0) {
-        LogError("WriteBlockToDisk: ftell failed\n");
-        return false;
-    }
-
-    pos.nPos = (unsigned int)fileOutPos;
-    fileout << block;
-
-    return true;
-}
-
 bool BlockManager::WriteUndoDataForBlock(const CBlockUndo &blockundo,
                                          BlockValidationState &state,
                                          CBlockIndex &block) {
@@ -1118,17 +1091,35 @@ FlatFilePos BlockManager::SaveBlockToDisk(const CBlock &block, int nHeight) {
     // Account for the 4 magic message start bytes + the 4 length bytes (8 bytes
     // total, defined as BLOCK_SERIALIZATION_HEADER_SIZE)
     nBlockSize += static_cast<unsigned int>(BLOCK_SERIALIZATION_HEADER_SIZE);
-    FlatFilePos blockPos{
+    FlatFilePos pos{
         FindNextBlockPos(nBlockSize, nHeight, block.GetBlockTime())};
-    if (blockPos.IsNull()) {
+    if (pos.IsNull()) {
         LogError("%s: FindNextBlockPos failed\n", __func__);
         return FlatFilePos();
     }
-    if (!WriteBlockToDisk(block, blockPos, GetParams().DiskMagic())) {
+    // Open history file to append
+    AutoFile fileout{OpenBlockFile(pos)};
+    if (fileout.IsNull()) {
+        LogError("%s: OpenBlockFile failed\n", __func__);
         m_opts.notifications.fatalError("Failed to write block");
         return FlatFilePos();
     }
-    return blockPos;
+
+    // Write index header
+    unsigned int nSize = GetSerializeSize(block);
+    fileout << GetParams().DiskMagic() << nSize;
+
+    // Write block
+    long fileOutPos = ftell(fileout.Get());
+    if (fileOutPos < 0) {
+        LogError("%s: ftell failed\n", __func__);
+        return FlatFilePos();
+    }
+
+    pos.nPos = (unsigned int)fileOutPos;
+    fileout << block;
+
+    return pos;
 }
 
 class ImportingNow {