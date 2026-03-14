b1b6f14d72c5f847ea3ca3a6f2ed1a1672692195
PiRK <pierrekn@gmail.com>
2026-03-14 08:56:27 +0100

    Revert "depends: backport updates for libevent.mk"
    Summary:
    This reverts commit 8536d3d410676a9824610d569c15006dcfe9771c.
    
    The libevent bump breaks windows cross-builds. This is being investigated, but let's unbreak the build first.
    
    Test Plan: @bot guix-win build-win64
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19669

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -3,16 +3,11 @@ $(package)_version=2.1.12-stable
 $(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
-$(package)_patches=cmake_fixups.patch
-$(package)_patches+=winver_fixup.patch
-$(package)_build_subdir=build
 
 define $(package)_set_vars
-  $(package)_config_opts=-DCMAKE_BUILD_TYPE=None -DEVENT__DISABLE_BENCHMARK=ON -DEVENT__DISABLE_OPENSSL=ON
-  $(package)_config_opts+=-DEVENT__DISABLE_SAMPLES=ON -DEVENT__DISABLE_REGRESS=ON
-  $(package)_config_opts+=-DEVENT__DISABLE_TESTS=ON -DEVENT__LIBRARY_TYPE=STATIC
-  $(package)_cflags += -fdebug-prefix-map=$($(package)_extract_dir)=/usr -fmacro-prefix-map=$($(package)_extract_dir)=/usr
-  $(package)_cppflags+= -D_GNU_SOURCE -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3
+  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress --disable-samples
+  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
+  $(package)_config_opts_release=--disable-debug-mode
   # When building for Windows, we set _WIN32_WINNT to target the same Windows
   # version as we do in cmake. Due to quirks in libevents build system, this is
   # also required to enable support for ipv6.
@@ -21,24 +16,21 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/cmake_fixups.patch && \
-  patch -p1 < $($(package)_patch_dir)/winver_fixup.patch
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux
 endef
 
 define $(package)_config_cmds
-  $($(package)_cmake) -S .. -B .
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
-  ninja -j$(JOBS)
+  $(MAKE) -j$(JOBS)
 endef
 
 define $(package)_stage_cmds
-  cmake --install . --prefix $($(package)_staging_prefix_dir)
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf bin lib/pkgconfig && \
-  rm include/ev*.h && \
-  rm include/event2/*_compat.h
+  rm lib/*.la
 endef
