Bitcoin-ABC/bitcoin-abc

commit fce6e5fd1a14551512e5576e1cef7236323fc04a

[secp256k1] build: Improvements to symbol visibility logic on Windows
Summary:
[[https://github.com/bitcoin-core/secp256k1/pull/1209 | secp256k1#1209]]:

> This fixes a build issue with MSVC. While MSVC imports *functions*
> from DLLs automatically when building a consumer of the DLL, it does
> not import *variables* automatically. In these cases, we need an
> explicit __declspec(dllimport).
>
> This commit simply changes our logic to what the libtool manual
> suggests, which has a very comprehensive writeup on the topic. Note
> that in particular, this solution is carefully designed not to break
> static linking. However, as described in the libtool manual,
> statically linking the library with MSVC will output warning LNK4217.
> This is still the best solution overall, because the warning is
> merely a cosmetic issue.

> Extend sig examples by call that uses static context
>
> Besides improving the examples, this makes sure that the examples
> import a variable (instead of a function), namely the static context,
> from the library. This is helpful when testing MSVC builds, because
> the MSVC linker tends to be awkward when importing variables.

> Suppress stupid MSVC linker warning and use correct format to pass linker flags

> Shutdown wineserver whenever CI script exits
>
> Before: CI times out when a wine task fails.
> After:  Wine tasks exit properly when they fail.

----

[[https://github.com/bitcoin-core/secp256k1/pull/1295 | secp256k1#1295]]:

> abi: Use dllexport for mingw builds
>
> This should fix mingw exports, specifically hiding the following:
> secp256k1_pre_g_128
> secp256k1_pre_g
> secp256k1_ecmult_gen_prec_table

-----

[[https://github.com/bitcoin-core/secp256k1/pull/1367 | secp256k1#1367]]:

> build: Introduce `SECP256K1_STATIC` macro for Windows users
>
> It is a non-Libtool-specific way to explicitly specify the user's
> intention to consume a static `libseck256k1`.
>
> This change allows to get rid of MSVC linker warnings LNK4217 and

modules/ecash-secp256k1/ecash-secp256k1-sys/build.rs
@@ -29,7 +29,7 @@ fn main() {
         .flag_if_supported("-Wno-unused-function")
         // patching out printf causes this warning
         .flag_if_supported("-Wno-unused-parameter")
-        .define("SECP256K1_API", Some(""))
+        .define("SECP256K1_API", Some("extern"))
         .define("ENABLE_MODULE_ECDH", Some("1"))
         .define("ENABLE_MODULE_SCHNORRSIG", Some("1"))
         .define("ENABLE_MODULE_SCHNORR", Some("1"))

src/secp256k1/CMakeLists.txt
@@ -100,6 +100,11 @@ if (SECP256K1_BUILD_SHARED)
 	set(SECP256K1_SHARED "SHARED")
 endif()
 add_library(secp256k1 ${SECP256K1_SHARED} src/secp256k1.c)
+if(WIN32)
+	# Define our export symbol only for shared libs.
+	set_target_properties(secp256k1 PROPERTIES DEFINE_SYMBOL SECP256K1_DLL_EXPORT)
+	target_compile_definitions(secp256k1 INTERFACE $<$<NOT:$<BOOL:${SECP256K1_BUILD_SHARED}>>:SECP256K1_STATIC>)
+endif()
 target_include_directories(secp256k1 PUBLIC include)
 target_sources(secp256k1 PRIVATE
 	src/precomputed_ecmult.c

src/secp256k1/Makefile.am
@@ -153,7 +153,7 @@ endif
 if USE_EXAMPLES
 noinst_PROGRAMS += ecdsa_example
 ecdsa_example_SOURCES = examples/ecdsa.c
-ecdsa_example_CPPFLAGS = -I$(top_srcdir)/include
+ecdsa_example_CPPFLAGS = -I$(top_srcdir)/include -DSECP256K1_STATIC
 ecdsa_example_LDADD = libsecp256k1.la
 ecdsa_example_LDFLAGS = -static
 if BUILD_WINDOWS
@@ -163,7 +163,7 @@ TESTS += ecdsa_example
 if ENABLE_MODULE_ECDH
 noinst_PROGRAMS += ecdh_example
 ecdh_example_SOURCES = examples/ecdh.c
-ecdh_example_CPPFLAGS = -I$(top_srcdir)/include
+ecdh_example_CPPFLAGS = -I$(top_srcdir)/include -DSECP256K1_STATIC
 ecdh_example_LDADD = libsecp256k1.la
 ecdh_example_LDFLAGS = -static
 if BUILD_WINDOWS
@@ -174,7 +174,7 @@ endif
 if ENABLE_MODULE_SCHNORRSIG
 noinst_PROGRAMS += schnorr_example
 schnorr_example_SOURCES = examples/schnorr.c
-schnorr_example_CPPFLAGS = -I$(top_srcdir)/include
+schnorr_example_CPPFLAGS = -I$(top_srcdir)/include -DSECP256K1_STATIC
 schnorr_example_LDADD = libsecp256k1.la
 schnorr_example_LDFLAGS = -static
 if BUILD_WINDOWS

src/secp256k1/ci/build_autotools.sh
@@ -34,6 +34,8 @@ print_environment
 # This speeds up jobs with many invocations of wine (e.g., ./configure with MSVC) tremendously.
 case "$WRAPPER_CMD" in
     *wine*)
+        # Make sure to shutdown wineserver whenever we exit.
+        trap "wineserver -k || true" EXIT INT HUP
         wineserver -p
         ;;
 esac
@@ -142,9 +144,6 @@ then
     make clean-precomp clean-testvectors
     make precomp testvectors
 fi
-
-# Shutdown wineserver again
-wineserver -k || true
 
 # Check that no repo files have been modified by the build.
 # (This fails for example if the precomp files need to be updated in the repo.)

src/secp256k1/configure.ac
@@ -110,12 +110,6 @@ AC_DEFUN([SECP_TRY_APPEND_DEFAULT_CFLAGS], [
       SECP_TRY_APPEND_CFLAGS([-wd4267], $1) # Disable warning C4267 "'var' : conversion from 'size_t' to 'type', possible loss of data".
       # Eliminate deprecation warnings for the older, less secure functions.
       CPPFLAGS="-D_CRT_SECURE_NO_WARNINGS $CPPFLAGS"
-      # We pass -ignore:4217 to the MSVC linker to suppress warning 4217 when
-      # importing variables from a statically linked secp256k1.
-      # (See the libtool manual, section "Windows DLLs" for background.)
-      # Unfortunately, libtool tries to be too clever and strips "-Xlinker arg"
-      # into "arg", so this will be " -Xlinker -ignore:4217" after stripping.
-      LDFLAGS="-Xlinker -Xlinker -Xlinker -ignore:4217 $LDFLAGS" 
     fi
 ])
 SECP_TRY_APPEND_DEFAULT_CFLAGS(SECP_CFLAGS)

src/secp256k1/examples/ecdsa.c
@@ -32,7 +32,7 @@ int main(void) {
     unsigned char compressed_pubkey[33];
     unsigned char serialized_signature[64];
     size_t len;
-    int is_signature_valid;
+    int is_signature_valid, is_signature_valid2;
     int return_val;
     secp256k1_pubkey pubkey;
     secp256k1_ecdsa_signature sig;
@@ -114,9 +114,17 @@ int main(void) {
     printf("Signature: ");
     print_hex(serialized_signature, sizeof(serialized_signature));
 
-
     /* This will clear everything from the context and free the memory */
     secp256k1_context_destroy(ctx);
+
+    /* Bonus example: if all we need is signature verification (and no key
+       generation or signing), we don't need to use a context created via
+       secp256k1_context_create(). We can simply use the static (i.e., global)
+       context secp256k1_context_static. See its description in
+       include/secp256k1.h for details. */
+    is_signature_valid2 = secp256k1_ecdsa_verify(secp256k1_context_static,
+                                                 &sig, msg_hash, &pubkey);
+    assert(is_signature_valid2 == is_signature_valid);
 
     /* It's best practice to try to clear secrets from memory after using them.
      * This is done because some bugs can allow an attacker to leak memory, for

src/secp256k1/examples/schnorr.c
@@ -26,7 +26,7 @@ int main(void) {
     unsigned char auxiliary_rand[32];
     unsigned char serialized_pubkey[32];
     unsigned char signature[64];
-    int is_signature_valid;
+    int is_signature_valid, is_signature_valid2;
     int return_val;
     secp256k1_xonly_pubkey pubkey;
     secp256k1_keypair keypair;
@@ -134,6 +134,15 @@ int main(void) {
 
     /* This will clear everything from the context and free the memory */
     secp256k1_context_destroy(ctx);
+
+    /* Bonus example: if all we need is signature verification (and no key
+       generation or signing), we don't need to use a context created via
+       secp256k1_context_create(). We can simply use the static (i.e., global)
+       context secp256k1_context_static. See its description in
+       include/secp256k1.h for details. */
+    is_signature_valid2 = secp256k1_schnorrsig_verify(secp256k1_context_static,
+                                                      signature, msg_hash, 32, &pubkey);
+    assert(is_signature_valid2 == is_signature_valid);
 
     /* It's best practice to try to clear secrets from memory after using them.
      * This is done because some bugs can allow an attacker to leak memory, for

src/secp256k1/include/secp256k1.h
@@ -120,21 +120,35 @@ typedef int (*secp256k1_nonce_function)(
 # define SECP256K1_NO_BUILD
 #endif
 
-/** At secp256k1 build-time DLL_EXPORT is defined when building objects destined
- *  for a shared library, but not for those intended for static libraries.
- */
-
-#ifndef SECP256K1_API
-# if defined(_WIN32)
-#  if defined(SECP256K1_BUILD) && defined(DLL_EXPORT)
-#   define SECP256K1_API __declspec(dllexport)
-#  else
-#   define SECP256K1_API
+/* Symbol visibility. */
+#if defined(_WIN32)
+  /* GCC for Windows (e.g., MinGW) accepts the __declspec syntax
+   * for MSVC compatibility. A __declspec declaration implies (but is not
+   * exactly equivalent to) __attribute__ ((visibility("default"))), and so we
+   * actually want __declspec even on GCC, see "Microsoft Windows Function
+   * Attributes" in the GCC manual and the recommendations in
+   * https://gcc.gnu.org/wiki/Visibility. */
+# if defined(SECP256K1_BUILD)
+#  if defined(DLL_EXPORT) || defined(SECP256K1_DLL_EXPORT)
+    /* Building libsecp256k1 as a DLL.
+     * 1. If using Libtool, it defines DLL_EXPORT automatically.
+     * 2. In other cases, SECP256K1_DLL_EXPORT must be defined. */
+#   define SECP256K1_API extern __declspec (dllexport)
 #  endif
-# elif defined(__GNUC__) && (__GNUC__ >= 4) && defined(SECP256K1_BUILD)
-#  define SECP256K1_API __attribute__ ((visibility ("default")))
+  /* The user must define SECP256K1_STATIC when consuming libsecp256k1 as a static
+   * library on Windows. */
+# elif !defined(SECP256K1_STATIC)
+   /* Consuming libsecp256k1 as a DLL. */
+#  define SECP256K1_API extern __declspec (dllimport)
+# endif
+#endif
+#ifndef SECP256K1_API
+# if defined(__GNUC__) && (__GNUC__ >= 4) && defined(SECP256K1_BUILD)
+   /* Building libsecp256k1 on non-Windows using GCC or compatible. */
+#  define SECP256K1_API extern __attribute__ ((visibility ("default")))
 # else
-#  define SECP256K1_API
+   /* All cases not captured above. */
+#  define SECP256K1_API extern
 # endif
 #endif
 
@@ -206,10 +220,10 @@ typedef int (*secp256k1_nonce_function)(
  *
  *  It is highly recommended to call secp256k1_selftest before using this context.
  */
-SECP256K1_API extern const secp256k1_context *secp256k1_context_static;
+SECP256K1_API const secp256k1_context *secp256k1_context_static;
 
 /** Deprecated alias for secp256k1_context_static. */
-SECP256K1_API extern const secp256k1_context *secp256k1_context_no_precomp
+SECP256K1_API const secp256k1_context *secp256k1_context_no_precomp
 SECP256K1_DEPRECATED("Use secp256k1_context_static instead");
 
 /** Perform basic self tests (to be used in conjunction with secp256k1_context_static)
@@ -583,10 +597,10 @@ SECP256K1_API int secp256k1_ecdsa_signature_normalize(
  * If a data pointer is passed, it is assumed to be a pointer to 32 bytes of
  * extra entropy.
  */
-SECP256K1_API extern const secp256k1_nonce_function secp256k1_nonce_function_rfc6979;
+SECP256K1_API const secp256k1_nonce_function secp256k1_nonce_function_rfc6979;
 
 /** A default safe nonce generation function (currently equal to secp256k1_nonce_function_rfc6979). */
-SECP256K1_API extern const secp256k1_nonce_function secp256k1_nonce_function_default;
+SECP256K1_API const secp256k1_nonce_function secp256k1_nonce_function_default;
 
 /** Create an ECDSA signature.
  *

commit 79f913f6930e7e4019bace0961db782224075151

[ecash-secp256k1] define SECP256K1_STATIC to fix cross build for windows
Summary:
The GUIX build for windows is currently broken, because proof-manager-cli relies on the rust wrapping of secp256k1 which is broken on windows by D19815. I guess we need a static libsecp256k1` for the wrapper.

As per comments in include/secp256k1.h:

> The user must define SECP256K1_STATIC when consuming libsecp256k1 as a static library on Window

Test Plan: `HOSTS="x86_64-w64-mingw32" contrib/guix/guix-build`

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D20039

modules/ecash-secp256k1/ecash-secp256k1-sys/build.rs
@@ -29,6 +29,7 @@ fn main() {
         .flag_if_supported("-Wno-unused-function")
         // patching out printf causes this warning
         .flag_if_supported("-Wno-unused-parameter")
+        .define("SECP256K1_STATIC", Some("1"))
         .define("SECP256K1_API", Some("extern"))
         .define("ENABLE_MODULE_ECDH", Some("1"))
         .define("ENABLE_MODULE_SCHNORRSIG", Some("1"))