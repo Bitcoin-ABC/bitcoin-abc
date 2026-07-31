bitcoin-ghost/bitcoin-core-ladder-script

Add importmempool RPC
test_importmempool_union contributed by glozow

Co-authored-by: glozow <gloriajzhao@gmail.com>

src/rpc/client.cpp
@@ -229,6 +229,10 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "importaddress", 2, "rescan" },
     { "importaddress", 3, "p2sh" },
     { "importpubkey", 2, "rescan" },
+    { "importmempool", 1, "options" },
+    { "importmempool", 1, "apply_fee_delta_priority" },
+    { "importmempool", 1, "use_current_time" },
+    { "importmempool", 1, "apply_unbroadcast_set" },
     { "importmulti", 0, "requests" },
     { "importmulti", 1, "options" },
     { "importmulti", 1, "rescan" },

Bitcoin-ABC/bitcoin-abc

Summary:
This concludes backport of [[https://github.com/bitcoin/bitcoin/pull/27460 | core#27460]]
https://github.com/bitcoin/bitcoin/pull/27460/changes/fa776e61cd64a5ffd9a4be589ab8efeb5421861a
Depends on D20415, D20411 and D20412
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D20416

src/rpc/client.cpp
@@ -158,6 +158,10 @@ static const CRPCConvertParam vRPCConvertParams[] = {
     {"importaddress", 2, "rescan"},
     {"importaddress", 3, "p2sh"},
     {"importpubkey", 2, "rescan"},
+    {"importmempool", 1, "options"},
+    {"importmempool", 1, "apply_fee_delta_priority"},
+    {"importmempool", 1, "use_current_time"},
+    {"importmempool", 1, "apply_unbroadcast_set"},
     {"importmulti", 0, "requests"},
     {"importmulti", 1, "options"},
     {"importmulti", 1, "rescan"},