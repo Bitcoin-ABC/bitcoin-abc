Bitcoin-ABC/bitcoin-abc

commit 89c74d9ee1f7b4e0b602372977901007c3c7aecd

[electrum] json_db/config: sanitize logging of unserializable values
Summary:
```
We should aim not to log secrets.

The logging was added in https://github.com/spesmilo/electrum/commit/6958c0ccc3d97edfb7f8504f27270182cb3abcfa, to aid debugging, but now I think we should err on the side of cautiousness here. As middle-ground we could try to sanitize "value" a bit before logging it, similar to bitcoin.neuter_bitcoin_address, e.g. `str(value)[:2] + ".." + str(value)[-2:]`.
```

Backport of [[https://github.com/spesmilo/electrum/pull/10807 | electrum#10807]].

Test Plan:
  ninja check-electrum

Reviewers: #bitcoin_abc, bytesofman

Reviewed By: #bitcoin_abc, bytesofman

Differential Revision: https://reviews.bitcoinabc.org/D20473

electrum/electrumabc/json_db.py
@@ -90,7 +90,8 @@ class JsonDB(PrintError):
             json.dumps(key, cls=MyEncoder)
             json.dumps(value, cls=MyEncoder)
         except Exception:
-            self.print_error(f"json error: cannot save {repr(key)} ({repr(value)})")
+            # note: "value" might be secret material, should probably not log it
+            self.logger.info(f"json error: cannot save {key=!r} ({type(value)})")
             return False
         if value is not None:
             if self.data.get(key) != value:

commit fc1e61a0c70ee099162d489c9e328ad5248bdd1f

[electrum] fix logging in json_db.py
Summary: This codebase is still missing the Electrum backport introducing the logger. Keep using the poorly named `print_error` method until we actually have a logger (issue introduced by D20473)

Test Plan:
Run the test plan of D20529 without the fix introduced by this diff, in verbose mode:
```
QT_API=pyqt6 ~/electrum-abc -v
```

Check for the log line.

Reviewers: #bitcoin_abc, bytesofman

Reviewed By: #bitcoin_abc, bytesofman

Subscribers: bytesofman

Differential Revision: https://reviews.bitcoinabc.org/D20530

electrum/electrumabc/json_db.py
@@ -91,7 +91,7 @@ class JsonDB(PrintError):
             json.dumps(value, cls=MyEncoder)
         except Exception:
             # note: "value" might be secret material, should probably not log it
-            self.logger.info(f"json error: cannot save {key=!r} ({type(value)})")
+            self.print_error(f"json error: cannot save {key=!r} ({type(value)})")
             return False
         if value is not None:
             if self.data.get(key) != value: