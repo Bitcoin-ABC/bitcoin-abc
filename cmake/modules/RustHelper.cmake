5b1606ec6610d2f54da90361f10d61d0066fde1a
PiRK <pierrekn@gmail.com>
2026-09-09 19:44:16 +0200

    cmake: always use the github tar.gz for corrosion, with original timestamp
    Summary:
    D20546 + D20574 likely introduced an issue with reproducible builds. It was expected that guix would use it's own version of Corrosion, but as v0.5.1 was published with the wrong version number 0.5.0, CMake fell back to the tar.gz download from github. And `DOWNLOAD_EXTRACT_TIMESTAMP TRUE` causes the extracted files to be set to the time of extraction, adding a source of non-reproducibility.
    
    Let's jsut always use the downloaded archive with its preserved timestamps, so we always get the same result.
    Alternatively we could require 0.5.0 to match the wrong version number, but that would be confusing and require comments. And add a source of issues for local builds, in case the actual Corrosion 0.5.0 is installed.
    
    Test Plan: guix build
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D20607

--- cmake/modules/RustHelper.cmake
+++ cmake/modules/RustHelper.cmake
@@ -1,23 +1,15 @@
 # Copyright (c) 2025 The Bitcoin developers
 
 include(FetchContent)
-# Prefer a system / Guix-provided Corrosion if available
-find_package(Corrosion 0.5.1 QUIET)
 
-if(Corrosion_FOUND)
-    message(STATUS "Using system/Guix Corrosion")
-else()
-    message(STATUS "Corrosion not found – fetching via FetchContent")
+FetchContent_Declare(
+    Corrosion
+    URL https://github.com/corrosion-rs/corrosion/archive/refs/tags/v0.5.1.tar.gz
+    URL_HASH SHA256=843334a9f0f5efbc225dccfa88031fe0f2ec6fd787ca1e7d55ed27b2c25d9c97
+    DOWNLOAD_EXTRACT_TIMESTAMP FALSE
+)
 
-    FetchContent_Declare(
-        Corrosion
-        URL https://github.com/corrosion-rs/corrosion/archive/refs/tags/v0.5.1.tar.gz
-        URL_HASH SHA256=843334a9f0f5efbc225dccfa88031fe0f2ec6fd787ca1e7d55ed27b2c25d9c97
-        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-    )
-
-    FetchContent_MakeAvailable(Corrosion)
-endif()
+FetchContent_MakeAvailable(Corrosion)
 
 set(REQUIRED_RUST_VERSION "1.87.0")
 if(Rust_VERSION VERSION_LESS REQUIRED_RUST_VERSION)
